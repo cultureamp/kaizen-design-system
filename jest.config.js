be253f82faa5c6a5e55fc88309ab17a224f7ad40
Louis Quinnell <zioroboco@gmail.com>
2019-07-14 12:04:30 +1000

    Set up babel, style and image loaders

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,8 @@ module.exports = {
   preset: "ts-jest",
   testRunner: "jest-circus/runner",
   testMatch: ["**/*.spec.ts?(x)"],
+  moduleNameMapper: {
+    "\\.(jpe?g|png)$": "jest-static-stubs/$1",
+    "\\.(sass|scss|css)$": "identity-obj-proxy",
+  },
 }
--- tools/webpack/index.ts
+++ tools/webpack/index.ts
@@ -1,15 +1,75 @@
-import { RuleSetRule } from "webpack"
+import { Loader, RuleSetRule } from "webpack"
 
-export const rules: RuleSetRule[] = [
-  {
-    test: /\.(ts|tsx)$/,
-    use: [
-      {
-        loader: require.resolve("babel-loader"),
-        options: {
-          presets: ["@babel/preset-typescript", "@babel/preset-react"],
-        },
+const excludeExternalModules = (rule: RuleSetRule): RuleSetRule => ({
+  exclude: /node_modules\/(?!\@kaizen).*/,
+  ...rule,
+})
+
+const babelRule: RuleSetRule = {
+  test: /\.(j|t)sx?$/,
+  use: [
+    {
+      loader: require.resolve("babel-loader"),
+      options: {
+        plugins: [
+          "@babel/plugin-proposal-numeric-separator",
+          "@babel/plugin-proposal-class-properties",
+          "@babel/plugin-proposal-object-rest-spread",
+        ],
+        presets: ["@babel/preset-typescript", "@babel/preset-react"],
       },
-    ],
+    },
+  ],
+}
+
+const imagesRule: RuleSetRule = {
+  test: [/\.jpe?g$/, /\.png$/],
+  loader: "file-loader",
+  options: {
+    name: "[name].[hash:8].[ext]",
+  },
+}
+
+const preprocessorLoaders: Loader[] = [
+  {
+    loader: "postcss-loader",
+    options: {
+      ident: "postcss",
+      plugins: () => [
+        require("postcss-flexbugs-fixes"),
+        require("postcss-preset-env")({
+          autoprefixer: { flexbox: "no-2009" },
+          stage: 3,
+        }),
+      ],
+    },
+  },
+  {
+    loader: "sass-loader",
+    options: {
+      sourceMap: true,
+    },
   },
 ]
+
+const stylesRule: RuleSetRule = {
+  test: /\.(sass|scss|css)$/,
+  use: [
+    "style-loader",
+    {
+      loader: "css-loader",
+      options: {
+        importLoaders: preprocessorLoaders.length,
+        sourceMap: true,
+        modules: {
+          localIdentName: "[folder]-[name]__[local]--[hash:base64:5]",
+        },
+      },
+    },
+    ...preprocessorLoaders,
+  ],
+}
+
+export const rules: RuleSetRule[] = [babelRule, stylesRule, imagesRule].map(
+  excludeExternalModules
+)
