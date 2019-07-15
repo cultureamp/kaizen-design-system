5c6bc9902b60bd1291fcc680c3f59047550f66b3
Louis Quinnell <zioroboco@gmail.com>
2019-07-15 10:54:54 +1000

    Refactor cli directory resolution

--- tools/cli/commands/scripts.command.ts
+++ tools/cli/commands/scripts.command.ts
@@ -2,15 +2,13 @@ import execa from "execa"
 import { readdirSync } from "fs"
 import { resolve } from "path"
 import { CommandModule } from "yargs"
+import { getPath } from "../util/paths"
 
 /** List of package script names to skip when generating aliases. */
 const ignore = ["cli"]
 
-const rootDir = resolve(__dirname, "../../..")
-const binDir = resolve(rootDir, "bin")
-
-const getScriptNames = () =>
-  readdirSync(binDir, { withFileTypes: true })
+const scriptNames = () =>
+  readdirSync(getPath("scripts"), { withFileTypes: true })
     .filter(dirent => !dirent.isDirectory())
     .map(dirent => dirent.name)
     .filter(name => !ignore.includes(name))
@@ -18,11 +16,11 @@ const getScriptNames = () =>
 const commandModule: CommandModule = {
   command: "<script>",
   describe: "Aliases for package scripts.",
-  aliases: getScriptNames(),
+  aliases: scriptNames(),
   handler: async ({ _ }) => {
     const [scriptName, ...args] = _
-    await execa(resolve(binDir, scriptName), args, {
-      cwd: rootDir,
+    await execa(resolve(getPath("scripts"), scriptName), args, {
+      cwd: getPath("root"),
       stdio: "inherit",
     }).catch(() => process.exit(1))
   },
