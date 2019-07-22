b3d1b2d8cbec212420403b0c7bf7db3f35181a2d
Seb Pearce <sebpearce@gmail.com>
2019-07-22 18:58:58 +1000

    Add source-filesystem and transformer-remark

--- site/gatsby-config.js
+++ site/gatsby-config.js
@@ -1,14 +1,25 @@
 module.exports = {
   plugins: [
-    "gatsby-plugin-react-helmet",
+    `gatsby-plugin-react-helmet`,
     {
-      resolve: "gatsby-plugin-sass",
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src`,
+        name: `src`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {},
+    },
+    {
+      resolve: `gatsby-plugin-sass`,
       options: {
         cssLoaderOptions: {
           modules: true,
         },
       },
     },
-    "gatsby-plugin-svg-sprite",
+    `gatsby-plugin-svg-sprite`,
   ],
 }
