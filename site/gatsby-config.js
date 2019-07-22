dfce28ef84c04d3100ed2e14a3b7bb11cf9e18cc
Seb Pearce <sebpearce@gmail.com>
2019-07-22 18:59:53 +1000

    Add site metadata

--- site/gatsby-config.js
+++ site/gatsby-config.js
@@ -1,4 +1,10 @@
 module.exports = {
+  siteMetadata: {
+    title: `Kaizen Design System`,
+    author: `Culture Amp`,
+    description: `Culture Amp’s design system.`,
+    siteUrl: `http://cultureamp.design/`,
+  },
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
