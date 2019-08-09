d351eb07da1994d84dd5cb8b12ed0fcb9bcc7e55
Seb Pearce <sebpearce@gmail.com>
2019-08-09 17:53:01 +1000

    Add Drop and Grid components

---
+++ site/src/components/Drop.tsx
@@ -0,0 +1,30 @@
+import React from "react"
+import classnames from "classnames"
+import styles from "./Drop.scss"
+
+type DropProps = {
+  color: string
+  classes: any
+  onClick: any
+  children: React.ReactNode
+}
+
+class Drop extends React.PureComponent<DropProps> {
+  render() {
+    const { color, classes, onClick, children } = this.props
+
+    return (
+      <div className={styles.droplet}>
+        <div className={styles.container} onClick={onClick}>
+          <div
+            className={classnames(classes, styles.example)}
+            style={{ background: color }}
+          />
+        </div>
+        {children}
+      </div>
+    )
+  }
+}
+
+export default Drop
---
+++ site/src/components/Grid.tsx
@@ -0,0 +1,17 @@
+import React from "react"
+import classnames from "classnames"
+import styles from "./Grid.scss"
+
+class Grid extends React.PureComponent {
+  render() {
+    const { children, className } = this.props
+
+    return (
+      <React.Fragment>
+        <div className={classnames(styles.grid, className)}>{children}</div>
+      </React.Fragment>
+    )
+  }
+}
+
+export default Grid
