ab0f0c5a5610337f4cfa68b09afe2fa443480b3c
Seb Pearce <sebpearce@gmail.com>
2019-08-26 13:02:22 +1000

    Move global styles into separate stylesheet

--- site/src/components/Layout.tsx
+++ site/src/components/Layout.tsx
@@ -1,4 +1,5 @@
 import * as React from "react"
+import "../styles/global.scss"
 import Footer from "./Footer"
 import Head from "./Head"
 // import { TitleBlock } from "@cultureamp/kaizen-component-library/draft"
