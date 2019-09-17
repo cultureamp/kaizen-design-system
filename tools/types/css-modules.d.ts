f0c3481231b84732d1ec90f0055fa85bd87e748b
Louis Quinnell <zioroboco@gmail.com>
2019-09-17 22:14:46 +1000

    Remove typescript css modules plugin
    This was not working with sass modules.

--- tools/types/css-modules.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "*.css" {
-  const classes: { [key: string]: string }
-  export default classes
-}
-
-declare module "*.scss" {
-  const classes: { [key: string]: string }
-  export default classes
-}
