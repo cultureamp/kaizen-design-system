bd57c4d6a0cad021e5f58c91547798e7a5cac52a
Louis Quinnell <zioroboco@gmail.com>
2019-09-20 00:25:19 +1000

    Force trailing slashes in site links
    Required for correct routing in s3/cloudfront.

--- site/gatsby-config.js
+++ site/gatsby-config.js
@@ -9,6 +9,7 @@ module.exports = {
     siteUrl: `http://cultureamp.design/`,
   },
   plugins: [
+    `gatsby-plugin-force-trailing-slashes`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
