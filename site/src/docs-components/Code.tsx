3d4214a4abe8474b9510528846acb8bc03cb5297
Seb Pearce <sebpearce@gmail.com>
2019-09-23 16:22:10 +1000

    Get prism-react-renderer showing correct styles

--- site/src/docs-components/Code.tsx
+++ site/src/docs-components/Code.tsx
@@ -1,4 +1,5 @@
 import Highlight, { defaultProps } from "prism-react-renderer"
+import theme from "prism-react-renderer/themes/oceanicNext"
 import * as React from "react"
 
 class Code extends React.Component<{
@@ -11,16 +12,27 @@ class Code extends React.Component<{
         {...defaultProps}
         code={this.props.children}
         language={this.props.language}
+        theme={theme}
       >
         {({ className, style, tokens, getLineProps, getTokenProps }) => (
           <pre className={className} style={style}>
-            {tokens.map((line, i) => (
-              <div {...getLineProps({ line, key: i })}>
-                {line.map((token, key) => (
-                  <span {...getTokenProps({ token, key })} />
-                ))}
-              </div>
-            ))}
+            {tokens.map((line, i) => {
+              // Ensure blank lines/spaces drop onto a new line, except the last line
+              if (line.length === 1 && line[0].content === "") {
+                if (i !== tokens.length - 1) {
+                  line[0].content = " "
+                } else {
+                  return
+                }
+              }
+              return (
+                <div {...getLineProps({ line, key: i })}>
+                  {line.map((token, key) => {
+                    return <span {...getTokenProps({ token, key })} />
+                  })}
+                </div>
+              )
+            })}
           </pre>
         )}
       </Highlight>
