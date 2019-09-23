be47edd6a7cf0e2f28d3e185230d4501c4fa4c4d
Louis Quinnell <zioroboco@gmail.com>
2019-09-23 13:42:27 +1000

    Add storybook to publish step

--- .buildkite/scripts/publish.sh
+++ .buildkite/scripts/publish.sh
@@ -2,6 +2,7 @@
 
 echo "🎋 Publishing to ${DOMAIN}${BASE_PATH}" \
   && aws s3 sync --delete site/public "s3://${DOMAIN}${BASE_PATH}" \
+  && aws s3 sync --delete storybook/public "s3://${DOMAIN}${BASE_PATH}/storybook" \
   && aws cloudfront create-invalidation \
       --distribution-id "${DISTRIBUTION}" \
       --paths "${BASE_PATH:-/}*" \
