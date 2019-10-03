5f343e2b6957dc1fda4a46075ab921e5ea4ff1d5
Louis Quinnell <zioroboco@gmail.com>
2019-10-03 22:44:49 +1000

    Set all pipeline parameters by branch (#31)
    Prefix all environment variables and set them via branch-specific upload steps defined in buildkite.

--- .buildkite/scripts/publish.sh
+++ .buildkite/scripts/publish.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-echo "🎋 Publishing to ${DOMAIN}${BASE_PATH}" \
-  && aws s3 sync --delete site/public "s3://${DOMAIN}${BASE_PATH}" \
-  && aws s3 sync --delete storybook/public "s3://${DOMAIN}${BASE_PATH}/storybook-static" \
+echo "🎋 Publishing to ${KAIZEN_DOMAIN_NAME}${KAIZEN_BASE_PATH}" \
+  && aws s3 sync --delete \
+      site/public "s3://${KAIZEN_DOMAIN_NAME}${KAIZEN_BASE_PATH}" \
+  && aws s3 sync --delete \
+      storybook/public "s3://${KAIZEN_DOMAIN_NAME}${KAIZEN_BASE_PATH}/storybook-static" \
   && aws cloudfront create-invalidation \
-      --distribution-id "${DISTRIBUTION}" \
-      --paths "${BASE_PATH:-/}*" \
+      --distribution-id "${KAIZEN_DISTRIBUTION_ID}" \
+      --paths "${KAIZEN_BASE_PATH:-/}*" \
       --output text
--- site/gatsby-config.js
+++ site/gatsby-config.js
@@ -1,7 +1,7 @@
 const { resolve } = require("path")
 
 module.exports = {
-  pathPrefix: process.env.BASE_PATH || "/",
+  pathPrefix: process.env.KAIZEN_BASE_PATH || "/",
   siteMetadata: {
     title: `Kaizen Design System`,
     author: `Culture Amp`,
