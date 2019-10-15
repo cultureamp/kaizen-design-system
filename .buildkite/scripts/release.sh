cb0076fca5be0c64016d90991b999d0d3211e801
Louis Quinnell <zioroboco@gmail.com>
2019-10-15 23:18:53 +1100

    ci: Set up git and npm credentials for release

--- .buildkite/scripts/release.sh
+++ .buildkite/scripts/release.sh
@@ -5,21 +5,48 @@ get_secret() {
   aws secretsmanager get-secret-value \
     --secret-id "kaizen-design-system/$1" \
     --query SecretString \
-    | tr -d '"'
+    | tr -d '"' \
+    | sed 's/\\n/\n/g'
+}
+
+setup_github() {
+  git config --global credential.helper cache
+  git config --global user.email "cultureamp-ci@cultureamp.com"
+  git config --global user.name "cultureamp-ci"
+  git config --global user.password "$GH_TOKEN"
+  git config --global commit.gpgsign false
+
+  eval "$(ssh-agent -s)"
+  echo "$GH_SSH_KEY" | ssh-add -
+
+  echo "Checking GitHub authentication..."
+  ssh -T git@github.com || true # exits non-zero
+}
+
+setup_npm() {
+  npm config set update-notifier false
+  npm config set email service-npm@cultureamp.com
+  npm config set username cultureamp-user
+
+  echo "Checking npm authentication..."
+  echo "Logged in as: $(npm whoami)"
 }
 
 main() {
+  export GH_SSH_KEY GH_TOKEN NPM_TOKEN
+
   printf "Fetching secrets... "
   GH_SSH_KEY=$(get_secret "github-ssh-key") || exit $?
   GH_TOKEN=$(get_secret "github-api-token") || exit $?
   NPM_TOKEN=$(get_secret "npm-token") || exit $?
   echo "(done)"
 
-  unset GH_SSH_KEY
-  unset GH_TOKEN
-  unset NPM_TOKEN
+  setup_github
+  setup_npm
+
+  unset GH_SSH_KEY GH_TOKEN NPM_TOKEN
 }
 
 main
 
-unset -f main get_secret
+unset -f main get_secret setup_github setup_npm
