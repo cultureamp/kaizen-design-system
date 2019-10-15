a5ec12084ff321586b32e513d1486c27abe4bd93
Louis Quinnell <zioroboco@gmail.com>
2019-10-16 02:45:24 +1100

    ci: Fix npm auth token in release step

--- .buildkite/scripts/release.sh
+++ .buildkite/scripts/release.sh
@@ -37,6 +37,7 @@ setup_npm() {
   npm config set update-notifier false
   npm config set email service-npm@cultureamp.com
   npm config set username cultureamp-user
+  npm config set //registry.npmjs.org/:_authToken "$NPM_TOKEN"
 
   echo "Checking npm authentication..."
   echo "Logged in as: $(npm whoami)"
