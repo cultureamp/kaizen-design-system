411a2a11eec5fb8d5705989851a3d6e2255041f8
Louis Quinnell <zioroboco@gmail.com>
2019-10-15 23:11:21 +1100

    chore: Prepare for first release
    BREAKING CHANGE: Initial release

--- packages/example-package/src/Example.tsx
+++ packages/example-package/src/Example.tsx
@@ -3,7 +3,7 @@ import * as React from "react"
 const styles = require("./styles.scss")
 
 const Example: React.FunctionComponent = props => (
-  <div className={styles.example}>🎋</div>
+  <div className={styles.example}>📦</div>
 )
 
 export default Example
