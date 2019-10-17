89f00e426edfbb4f4fda52b4924f1f7de53e357b
Louis Quinnell <zioroboco@gmail.com>
2019-10-17 23:05:49 +1100

    Add missing storybook addons

---
+++ storybook/addons.ts
@@ -0,0 +1,9 @@
+// tslint:disable:ordered-imports
+
+// Order determines tab order in addons panel:
+import "@storybook/addon-actions/register"
+
+// Do not use the addons panel:
+import "@storybook/addon-options/register"
+import "@storybook/addon-backgrounds/register"
+import "@storybook/addon-viewport/register"
