038404ed3100373544f15d3fcd99a3e02b7fb52c
Seb Pearce <sebpearce@gmail.com>
2019-10-28 15:58:24 +1100

    Make StorybookDemo adjust its own height

--- site/src/components/StorybookDemo.tsx
+++ site/src/components/StorybookDemo.tsx
@@ -1,9 +1,38 @@
 import * as React from "react"
+import ReactDOM from "react-dom"
 
 const styles = require("./StorybookDemo.scss")
 
-const StorybookDemo = ({ children }) => (
-  <div className={styles.container}>{children}</div>
-)
+type StorybookDemoProps = {
+  demoId: string
+}
 
-export default StorybookDemo
+export default class StorybookDemo extends React.Component<StorybookDemoProps> {
+  constructor(props) {
+    super(props)
+    this.iFrameRef = React.createRef()
+  }
+  state = {
+    iFrameHeight: "0px",
+  }
+
+  render() {
+    return (
+      <div className={styles.container}>
+        <iframe
+          src={`https://cultureamp.design/storybook-static/iframe.html?id=${this.props.demoId}`}
+          onLoad={() => {
+            const obj = this.iFrameRef.current
+            if (obj && obj.contentWindow) {
+              this.setState({
+                iFrameHeight: obj!.contentWindow.body.scrollHeight + "px",
+              })
+            }
+          }}
+          height={this.state.iFrameHeight}
+          ref={this.iFrameRef}
+        ></iframe>
+      </div>
+    )
+  }
+}
--- site/src/templates/componentPage.tsx
+++ site/src/templates/componentPage.tsx
@@ -1,6 +1,7 @@
 import { graphql } from "gatsby"
 import { MDXRenderer } from "gatsby-plugin-mdx"
 import * as React from "react"
+import ReactDOM from "react-dom"
 import ContentMarkdownSection from "../components/ContentMarkdownSection"
 import Footer from "../components/Footer"
 import Layout from "../components/Layout"
@@ -45,11 +46,7 @@ export default ({ data, pageContext, location }) => {
 
   const renderStorybookIFrame = () => {
     return md.frontmatter.demoStoryId ? (
-      <StorybookDemo>
-        <iframe
-          src={`https://cultureamp.design/storybook-static/iframe.html?id=${md.frontmatter.demoStoryId}`}
-        ></iframe>
-      </StorybookDemo>
+      <StorybookDemo demoId={md.frontmatter.demoStoryId} />
     ) : (
       undefined
     )
