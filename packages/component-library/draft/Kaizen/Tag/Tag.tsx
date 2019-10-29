3b86c8511777f6dab7de89dc23515ed5748e813b
Li Juen Chang <lijuen.chang@cultureamp.com>
2019-10-29 13:32:39 +1100

    Bring React Tag up to feature parity with Elm Tag

--- packages/component-library/draft/Kaizen/Tag/Tag.tsx
+++ packages/component-library/draft/Kaizen/Tag/Tag.tsx
@@ -1,37 +1,80 @@
+import { Icon } from "@cultureamp/kaizen-component-library"
 import classNames from "classnames"
 import * as React from "react"
+const clearIcon = require("@cultureamp/kaizen-component-library/icons/clear.icon.svg")
+  .default
+const exclamationIcon = require("@cultureamp/kaizen-component-library/icons/exclamation.icon.svg")
+  .default
 const styles = require("./Tag.scss")
 
-type Color = "default" | "peach" | "seedling" | "wisteria"
-
-type Props = {
-  color?: Color
-  text: string
-  withIcon?: boolean
-  disableAnimation?: boolean
+interface Props {
+  readonly children: React.ReactNode
+  readonly variant: "default" | "cautionary" | "negative"
+  readonly size?: "medium" | "small"
+  readonly inline?: boolean
+  readonly dismissible?: boolean
+  readonly onDismiss?: React.MouseEventHandler<HTMLSpanElement>
+  readonly onMouseDown?: React.MouseEventHandler<HTMLSpanElement>
+  readonly onMouseLeave?: React.MouseEventHandler<HTMLSpanElement>
+  readonly truncationWidth?: number
 }
 
 const Tag = (props: Props) => {
-  const { color = "default", text, withIcon, disableAnimation } = props
+  const {
+    children,
+    variant = "default",
+    size = "medium",
+    inline = false,
+    dismissible = false,
+    onDismiss,
+    onMouseDown,
+    onMouseLeave,
+    truncationWidth,
+  } = props
+
+  const isValidation = variant === "cautionary" || variant === "negative"
+  const isTruncated = truncationWidth && truncationWidth > 0
 
   return (
-    <div className={classNames(styles.root, styles[color])}>
-      {text}
-      {withIcon && (
-        <span className={styles.pulse}>
-          <span
-            className={classNames(styles.pulseRing, {
-              [styles.noAnimation]: disableAnimation,
-            })}
-          />
+    <div
+      className={classNames(styles.root, {
+        [styles.default]: variant === "default",
+        [styles.validation]: isValidation,
+        [styles.cautionary]: variant === "cautionary",
+        [styles.negative]: variant === "negative",
+        [styles.medium]: size === "medium",
+        [styles.small]: size === "small",
+        [styles.inline]: inline,
+        [styles.dismissible]: dismissible,
+      })}
+    >
+      <span
+        className={classNames(styles.textContent, {
+          [styles.truncate]: isTruncated,
+        })}
+        style={{
+          maxWidth: isTruncated ? truncationWidth : undefined,
+        }}
+      >
+        {children}
+      </span>
+      {dismissible && (
+        <span
+          className={styles.dismissIcon}
+          onClick={onDismiss}
+          onMouseDown={onMouseDown}
+          onMouseLeave={onMouseLeave}
+        >
+          <Icon icon={clearIcon} inheritSize role="img" title="Dismiss" />
+        </span>
+      )}
+      {size === "medium" && isValidation && (
+        <span className={styles.validationIcon}>
+          <Icon icon={exclamationIcon} inheritSize role="presentation" />
         </span>
       )}
     </div>
   )
-}
-
-Tag.defaultProps = {
-  withIcon: false,
 }
 
 export default Tag
--- packages/component-library/stories/Tag.stories.tsx
+++ packages/component-library/stories/Tag.stories.tsx
@@ -1,7 +1,76 @@
-import * as React from "react"
-
 import { loadElmStories } from "@cultureamp/elm-storybook"
 import { Tag } from "@cultureamp/kaizen-component-library/draft"
+import { storiesOf } from "@storybook/react"
+import * as React from "react"
+
+const StoryContainer = ({ children }: { children: React.ReactNode }) => (
+  <div style={{ width: "100%", display: "flex", justifyContent: "center" }}>
+    {children}
+  </div>
+)
+
+storiesOf("Tag", module)
+  .add("Default - Medium", () => (
+    <StoryContainer>
+      <Tag variant="default">Default</Tag>
+    </StoryContainer>
+  ))
+
+  .add("Default - Small", () => (
+    <StoryContainer>
+      <Tag variant="default" size="small">
+        Default
+      </Tag>
+    </StoryContainer>
+  ))
+
+  .add("Default - Dismissible", () => (
+    <StoryContainer>
+      <Tag variant="default" dismissible>
+        Default
+      </Tag>
+    </StoryContainer>
+  ))
+
+  .add("Validation - Medium, Cautionary, Dismissible", () => (
+    <StoryContainer>
+      <Tag variant="cautionary" dismissible>
+        Validation
+      </Tag>
+    </StoryContainer>
+  ))
+
+  .add("Validation - Small, Cautionary, Dismissible", () => (
+    <StoryContainer>
+      <Tag variant="cautionary" size="small" dismissible>
+        Validation
+      </Tag>
+    </StoryContainer>
+  ))
+
+  .add("Validation - Medium, Cautionary, Dismissible, Truncated", () => (
+    <StoryContainer>
+      <Tag variant="cautionary" dismissible truncationWidth={50}>
+        Truncated
+      </Tag>
+    </StoryContainer>
+  ))
+
+  .add("Validation - Small, Cautionary, Dismissible, Truncated", () => (
+    <StoryContainer>
+      <Tag variant="cautionary" size="small" dismissible truncationWidth={50}>
+        Truncated
+      </Tag>
+    </StoryContainer>
+  ))
+
+  .add("Validation - Medium, Negative, Dismissible, Truncated", () => (
+    <StoryContainer>
+      <Tag variant="negative" dismissible truncationWidth={50}>
+        Truncated
+      </Tag>
+    </StoryContainer>
+  ))
 
 loadElmStories("Tag (Elm)", module, require("./TagStories.elm"), [
   "Default - Medium",
