9ce92f93a55e537c69e77d3acf6fa21b6bfb1126
Ally Beaton <ally.beaton@cultureamp.com.au>
2019-11-22 10:55:28 +1100

    Added types for color tokens

---
+++ packages/design-tokens/tokens/theme.ts
@@ -0,0 +1,55 @@
+import * as tokens from './color.json';
+
+interface ExtendedColorScale {
+  "600": string
+  "700": string
+  "800": string
+}
+
+interface ColorScale { 
+  "100": string
+  "200": string
+  "300": string
+  "400": string
+  "500": string
+}
+
+type Wisteria = ColorScale | ExtendedColorScale
+type Cluny = ColorScale 
+type Seedling = ColorScale | Pick<ExtendedColorScale, '600'>
+type Yuzu = ColorScale
+type Coral = ColorScale | Pick<ExtendedColorScale, '600'>
+type Peach = ColorScale 
+type Ash = string
+type Stone = string
+type White = string
+type Lapis = string
+type Ocean = string
+type Ink = string
+type Paper = string
+
+export interface Color {
+  kz: {
+    color: {
+      wisteria: Wisteria,
+      cluny: Cluny,
+      seedling: Seedling,
+      yuzu: Yuzu,
+      coral: Coral,
+      peach: Peach,
+      ash: Ash,
+      stone: Stone,
+      white: White,
+    },
+    DEPRECATED: {
+      color: {
+        lapis: Lapis,
+        ocean: Ocean,
+        ink: Ink,
+        paper: Paper,
+      },
+    },
+  }
+}
+
+export const ColorTokens: Color = tokens;
