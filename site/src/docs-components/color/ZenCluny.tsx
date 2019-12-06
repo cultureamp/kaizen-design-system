7e02788fa2a5964b9ecdabda7498d2d509e30405
Seb Pearce <sebpearce@gmail.com>
2019-12-06 17:04:50 +1100

    Create components for Cluny, Neutrals, Wisteria

---
+++ site/src/docs-components/color/ZenCluny.tsx
@@ -0,0 +1,7 @@
+import colorTokens from "@cultureamp/kaizen-design-tokens/tokens/color.json"
+import * as React from "react"
+import ZenColorGroup from "./ZenColorGroup"
+
+const ZenCluny = () => <ZenColorGroup colors={colorTokens.kz.color.cluny} />
+
+export default ZenCluny
