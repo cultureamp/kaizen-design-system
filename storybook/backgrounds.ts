7b40e181e6adf429bb1bfab6be3f39a21dbb0394
Michael Bylstra <mbylstra@gmail.com>
2020-01-06 15:23:07 +1100

    update storybook background colors to use kaizen-design-tokens

--- storybook/backgrounds.ts
+++ storybook/backgrounds.ts
@@ -1,8 +1,12 @@
+import * as tokens from "../packages/design-tokens/tokens/color"
+
 export const backgrounds = [
-  { name: "wisteria", value: "#4B4D68" },
-  { name: "seedling", value: "#45ad8f" },
-  { name: "peach", value: "#f3786d" },
-  { name: "yuzu", value: "#ffce1e" },
-  { name: "stone", value: "#F6F6F6" },
-  { name: "ash", value: "#ECECEF" },
+  { name: "stone", value: tokens.kz.color.stone },
+  { name: "ash", value: tokens.kz.color.ash },
+  { name: "wisteria", value: tokens.kz.color.wisteria["700"] },
+  { name: "cluny", value: tokens.kz.color.cluny["500"] },
+  { name: "seedling", value: tokens.kz.color.seedling["500"] },
+  { name: "yuzu", value: tokens.kz.color.yuzu["500"] },
+  { name: "peach", value: tokens.kz.color.peach["500"] },
+  { name: "coral", value: tokens.kz.color.coral["500"] },
 ]
