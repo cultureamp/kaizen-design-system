18e4edd8b9e81e9e4211b254603c5e7462e157b1
Michael Bylstra <mbylstra@gmail.com>
2020-01-06 14:44:05 +1100

    added Zen stone and ash colours to Storybook background presets

--- storybook/backgrounds.ts
+++ storybook/backgrounds.ts
@@ -3,4 +3,6 @@ export const backgrounds = [
   { name: "seedling", value: "#45ad8f" },
   { name: "peach", value: "#f3786d" },
   { name: "yuzu", value: "#ffce1e" },
+  { name: "stone", value: "#F6F6F6" },
+  { name: "ash", value: "#ECECEF" },
 ]
