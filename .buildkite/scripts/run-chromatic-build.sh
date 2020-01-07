0af9f75210a752a73e9bbc8b62367fdac01ce6c5
Michael Bylstra <mbylstra@gmail.com>
2020-01-07 17:15:19 +1100

    use yarn run

--- .buildkite/scripts/run-chromatic-build.sh
+++ .buildkite/scripts/run-chromatic-build.sh
@@ -2,10 +2,12 @@
 
 # AWS_SECRETS=$(aws secretsmanager get-secret-value --secret-id /kaizen-design-system/chromatic-app-code | jq -r  '.SecretString')
 
-AWS_SECRETS='{ "CHROMATIC_APP_CODE": "q2uguq8te" }'
-CHROMATIC_APP_CODE=$(echo "$AWS_SECRETS" | jq -r '.CHROMATIC_APP_CODE')
+# AWS_SECRETS='{ "CHROMATIC_APP_CODE": "q2uguq8te" }'
+# CHROMATIC_APP_CODE=$(echo "$AWS_SECRETS" | jq -r '.CHROMATIC_APP_CODE') #TODO: jq needs to be installed in buildkite agent somehow
+
+CHROMATIC_APP_CODE="q2uguq8te"
 # echo $CHROMATIC_APP_CODE
 
-cd ../..
-# pwd
-./node_modules/.bin/chromatic --build-script-name=storybook:build --app-code=$CHROMATIC_APP_CODE
+yarn setup
+
+yarn run chromatic --build-script-name=storybook:build --app-code=$CHROMATIC_APP_CODE
