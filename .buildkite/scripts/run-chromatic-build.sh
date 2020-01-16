e945440e6bdfbf3fbdae0b6582f274d44ae621ae
Louis Quinnell <zioroboco@gmail.com>
2020-01-16 14:48:12 +1100

    Get the chromatic app code from secrets manager
    Added to the development build role in https://github.com/cultureamp/kaizen-design-system-ops/pull/24

--- .buildkite/scripts/run-chromatic-build.sh
+++ .buildkite/scripts/run-chromatic-build.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-CHROMATIC_APP_CODE="q2uguq8te"
+# shellcheck source=helpers/get-secret.sh
+. ".buildkite/scripts/helpers/get-secret.sh"
+
+CHROMATIC_APP_CODE=$(get_secret "chromatic-app-code") || exit $?
 
 yarn setup
 
-yarn run chromatic --build-script-name=storybook:build --app-code=$CHROMATIC_APP_CODE
+yarn run chromatic --build-script-name=storybook:build --app-code="$CHROMATIC_APP_CODE"
