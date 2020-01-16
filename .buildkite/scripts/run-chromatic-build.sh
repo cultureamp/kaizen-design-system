1464d90f3f969dece1165fb40a5633709b989126
Louis Quinnell <zioroboco@gmail.com>
2020-01-16 17:54:20 +1100

    Fix app code and run chromatic on pre-built assets

--- .buildkite/scripts/run-chromatic-build.sh
+++ .buildkite/scripts/run-chromatic-build.sh
@@ -3,8 +3,9 @@
 # shellcheck source=helpers/get-secret.sh
 . ".buildkite/scripts/helpers/get-secret.sh"
 
+export CHROMATIC_APP_CODE
 CHROMATIC_APP_CODE=$(get_secret "chromatic-app-code") || exit $?
 
-yarn setup
+npm i -g chromatic
 
-yarn -s chromatic --build-script-name=storybook:build --app-code="$CHROMATIC_APP_CODE"
+chromatic --storybook-build-dir storybook/public
