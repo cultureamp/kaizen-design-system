414e50ce0a1b0d1ccaa1c2bd96cf9cbe0aaceab7
Diana MacDonald <diana.macdonald@cultureamp.com>
2020-01-16 18:10:46 +1100

    Change "Standard" to "Default" in RadioGroup story
    … to match other story names

--- packages/component-library/stories/RadioGroup.stories.tsx
+++ packages/component-library/stories/RadioGroup.stories.tsx
@@ -38,7 +38,7 @@ export default class RadioGroupExample extends React.Component<Props> {
 }
 
 storiesOf("RadioGroup (React)", module)
-  .add("Standard (Kaizen Site Demo)", () => (
+  .add("Default (Kaizen Site Demo)", () => (
     <RadioGroupExample
       render={({ selectedOption, onChangeHandler }) => (
         <RadioGroup labelText="Radio group label">
