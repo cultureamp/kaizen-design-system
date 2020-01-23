67ec86541b315a60ed6a1cd5189191d295cf6f0a
Michael Bylstra <mbylstra@gmail.com>
2020-01-23 22:35:17 +1100

    add Knobs story for IconButton.stories.tsx

--- packages/component-library/stories/IconButton.stories.tsx
+++ packages/component-library/stories/IconButton.stories.tsx
@@ -2,10 +2,35 @@ import { IconButton } from "@kaizen/component-library"
 const configureIcon = require("@kaizen/component-library/icons/configure.icon.svg")
   .default
 import { action } from "@storybook/addon-actions"
+import { boolean, radios, text, withKnobs } from "@storybook/addon-knobs"
 import { storiesOf } from "@storybook/react"
 import * as React from "react"
 
 storiesOf("IconButton (React)", module)
+  .addDecorator(withKnobs)
+  .add("with Knobs", () => {
+    return (
+      <IconButton
+        icon={configureIcon}
+        id={text("id (optional)", "button-1")}
+        label={text("label", "Label")}
+        destructive={boolean("destructive (optional)", false)}
+        disabled={boolean("disabled (optional)", false)}
+        form={boolean("form (optional)", false)}
+        reversed={boolean("reversed (optional)", false)}
+        newTabAndIUnderstandTheAccessibilityImplications={boolean(
+          "newTabAndIUnderstandTheAccessibilityImplications (optional)",
+          false
+        )}
+        automationId={text("automationId (optional)", "button-1")}
+        fullWidth={boolean("fullWidth (optional)", false)}
+        disableTabFocusAndIUnderstandTheAccessibilityImplications={boolean(
+          "disableTabFocusAndIUnderstandTheAccessibilityImplications (optional)",
+          false
+        )}
+      />
+    )
+  })
   .add("Default", () => (
     <IconButton icon={configureIcon} label="Label" automationId="demo-button" />
   ))
