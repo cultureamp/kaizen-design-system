f16424c9b542f8e15e2d1c932a2627e6a555c79b
Michael Bylstra <mbylstra@gmail.com>
2020-01-30 21:01:00 +1100

    use the Storybook 5.3 main.js format for finding stories
    it seems necessary for CSF to work.
    note that this path is much less flexible than the current one:
    it only looks for stories in packages/component-library/
    I couldn't get the regex or glob to work in the new format, so this will
    fixed in a later release. Currently this is the only directory where stories are found.

--- storybook/preview.ts
+++ storybook/preview.ts
@@ -1,5 +1,5 @@
 import { withA11y } from "@storybook/addon-a11y"
-import { addDecorator, addParameters, configure } from "@storybook/react"
+import { addDecorator, addParameters } from "@storybook/react"
 import { create } from "@storybook/theming"
 import { backgrounds } from "./backgrounds"
 
@@ -22,10 +22,3 @@ addParameters({
     theme: create({ brandTitle: "🌱 Storybook", base: "light" }),
   },
 })
-
-const loadStories = () => {
-  const req = require.context("../packages", true, /\.stories.tsx?$/)
-  req.keys().forEach(filename => req(filename))
-}
-
-configure(loadStories, module)
