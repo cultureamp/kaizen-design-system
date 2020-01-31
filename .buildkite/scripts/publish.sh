26f30605e9abb5a114ad786c711166608ca299ed
Ally Beaton <ally.beaton@cultureamp.com.au>
2020-01-31 13:55:28 +1100

    chore: Call Github's checks API when the branch preview is successful

--- .buildkite/scripts/publish.sh
+++ .buildkite/scripts/publish.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 set -e
 
-echo "Publishing to ${KAIZEN_DOMAIN_NAME}${KAIZEN_BASE_PATH}"
+destination=${KAIZEN_DOMAIN_NAME}${KAIZEN_BASE_PATH}
+
+echo "Publishing to ${destination}"
 
 aws s3 sync --delete \
     "site/public" \
@@ -15,3 +17,10 @@ aws cloudfront create-invalidation \
     --distribution-id "${KAIZEN_DISTRIBUTION_ID}" \
     --paths "${KAIZEN_BASE_PATH:-/}*" \
     --output text
+
+echo "Reporting the deployed URL via the GitHub status API"
+
+commit_hash=$(git rev-parse --verify HEAD)
+curl --fail -s "https://api.github.com/repos/cultureamp/kaizen-design-system/statuses/$commit_hash" \
+  --header "authorization: Bearer $GITHUB_STATUS_TOKEN" \
+  --data '{"state": "success", "target_url": "https://'"$destination"'", "description": "Branch preview"}'
