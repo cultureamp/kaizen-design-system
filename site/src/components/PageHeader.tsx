d5d8f1a4a2949cabba579af6fda2a70a6b31246b
Seb Pearce <sebpearce@gmail.com>
2020-02-03 14:23:57 +1100

    feat: Add header images to docs pages

--- site/src/components/PageHeader.tsx
+++ site/src/components/PageHeader.tsx
@@ -12,6 +12,7 @@ type PageHeaderProps = {
   tags?: string[]
   image?: React.ReactNode
   headingOnly?: boolean
+  headerImageName?: string
 }
 
 const PageHeader: React.SFC<PageHeaderProps> = ({
@@ -19,8 +20,8 @@ const PageHeader: React.SFC<PageHeaderProps> = ({
   headingText,
   summaryParagraph,
   tags,
-  image,
   headingOnly,
+  headerImageName,
 }) => (
   <StaticQuery
     query={graphql`
@@ -28,60 +29,82 @@ const PageHeader: React.SFC<PageHeaderProps> = ({
         amoeba: file(name: { eq: "amoeba" }) {
           publicURL
         }
-        emptyState: file(name: { eq: "empty-state-placeholder" }) {
+        emptyStateHeaderImage: file(name: { eq: "empty-state-placeholder" }) {
           publicURL
+        }
+        headerImages: allFile(
+          filter: { sourceInstanceName: { eq: "headerImages" } }
+        ) {
+          edges {
+            node {
+              publicURL
+              name
+            }
+          }
         }
       }
     `}
-    render={data => (
-      <div
-        className={classnames({
-          [styles.pageHeader]: true,
-          [styles.headingOnly]: headingOnly,
-        })}
-      >
-        <div className={styles.pageHeaderInner}>
-          {!headingOnly && (
-            <div className={styles.sideSection}>
-              <div className={styles.image}>
-                <img
-                  src={data.amoeba.publicURL}
-                  className={styles.imageBackdrop}
-                />
-                {image ? image : <img src={data.emptyState.publicURL} />}
+    render={data => {
+      const foundImage = data.headerImages.edges.find(
+        edge => edge.node.name === headerImageName
+      )
+      const headerImageURL =
+        foundImage !== undefined ? foundImage.node.publicURL : undefined
+      const emptyStateHeaderImageURL = data.emptyStateHeaderImage.publicURL
+
+      return (
+        <div
+          className={classnames({
+            [styles.pageHeader]: true,
+            [styles.headingOnly]: headingOnly,
+          })}
+        >
+          <div className={styles.pageHeaderInner}>
+            {!headingOnly && (
+              <div className={styles.sideSection}>
+                <div className={styles.image}>
+                  <img
+                    src={data.amoeba.publicURL}
+                    className={styles.imageBackdrop}
+                  />
+                  <img
+                    src={headerImageURL || emptyStateHeaderImageURL}
+                    className={styles.headerImage}
+                  />
+                </div>
+                <div className={styles.tagsContainer}>
+                  {tags && (
+                    <>
+                      <div className={styles.tagsLabel}>Also known as:</div>
+                      <div className={styles.tags}>
+                        {tags.map((tagText, i) => (
+                          <Tag text={tagText} key={`tag-${i}`} />
+                        ))}
+                      </div>
+                    </>
+                  )}
+                </div>
               </div>
-              <div className={styles.tagsContainer}>
-                {tags && (
-                  <>
-                    <div className={styles.tagsLabel}>Also known as:</div>
-                    <div className={styles.tags}>
-                      {tags.map((tagText, i) => (
-                        <Tag text={tagText} key={`tag-${i}`} />
-                      ))}
-                    </div>
-                  </>
-                )}
-              </div>
-            </div>
-          )}
-          <div className={styles.mainSection}>
-            <div className={styles.headingTextContainer}>
-              <Heading
-                variant={headingOnly ? "display-0" : "heading-1"}
-                tag="h1"
-                classNameAndIHaveSpokenToDST={styles.headingTextOverrides}
-              >
-                {headingText}
-              </Heading>
-            </div>
-            {summaryParagraph && (
-              <h3 className={styles.summaryParagraph}>{summaryParagraph}</h3>
             )}
-            {children}
+            <div className={styles.mainSection}>
+              <div className={styles.headingTextContainer}>
+                <Heading
+                  variant={headingOnly ? "display-0" : "heading-1"}
+                  tag="h1"
+                  classNameAndIHaveSpokenToDST={styles.headingTextOverrides}
+                >
+                  {headingText}
+                </Heading>
+              </div>
+              {summaryParagraph && (
+                <h3 className={styles.summaryParagraph}>{summaryParagraph}</h3>
+              )}
+              {children}
+            </div>
           </div>
         </div>
-      </div>
-    )}
+      )
+    }}
   />
 )
 
