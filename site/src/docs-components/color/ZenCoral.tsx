73078bd24651fbd897c73e6e74c8477260abab2a
Seb Pearce <sebpearce@gmail.com>
2020-02-14 14:15:32 +1100

    Lint color group components

--- site/src/docs-components/color/ZenCoral.tsx
+++ site/src/docs-components/color/ZenCoral.tsx
@@ -2,8 +2,6 @@ import colorTokens from "@kaizen/design-tokens/tokens/color.json"
 import * as React from "react"
 import ZenColorGroup from "./ZenColorGroup"
 
-const ZenCoral = () => (
-  <ZenColorGroup colors={colorTokens.kz.color.coral} />
-)
+const ZenCoral = () => <ZenColorGroup colors={colorTokens.kz.color.coral} />
 
 export default ZenCoral
--- site/src/docs-components/color/ZenYuzu.tsx
+++ site/src/docs-components/color/ZenYuzu.tsx
@@ -2,8 +2,6 @@ import colorTokens from "@kaizen/design-tokens/tokens/color.json"
 import * as React from "react"
 import ZenColorGroup from "./ZenColorGroup"
 
-const ZenYuzu = () => (
-  <ZenColorGroup colors={colorTokens.kz.color.yuzu} />
-)
+const ZenYuzu = () => <ZenColorGroup colors={colorTokens.kz.color.yuzu} />
 
 export default ZenYuzu
