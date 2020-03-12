57f11257f143a72f1d5ebc826c9724947edbb9eb
Ally Beaton <ally.beaton@cultureamp.com.au>
2020-03-12 12:28:20 +1100

    Align the text of the paragraph stories to the names in Figma

--- packages/component-library/stories/Paragraph.stories.tsx
+++ packages/component-library/stories/Paragraph.stories.tsx
@@ -4,17 +4,19 @@ import { Paragraph } from "../components/Paragraph"
 export default { title: "typography/Paragraph" }
 
 export const IntroLede = () => (
-  <Paragraph variant="intro-lede">Intro Lede</Paragraph>
+  <Paragraph variant="intro-lede">Paragraph Intro Lede</Paragraph>
 )
 
 export const Body = () => (
   <Paragraph data-automation-id="test" variant="body">
-    Body
+    Paragraph Body
   </Paragraph>
 )
 
-export const Small = () => <Paragraph variant="small">Small</Paragraph>
+export const Small = () => (
+  <Paragraph variant="small">Paragraph Small</Paragraph>
+)
 
 export const ExtraSmall = () => (
-  <Paragraph variant="extra-small">Extra Small</Paragraph>
+  <Paragraph variant="extra-small">Paragraph Extra Small</Paragraph>
 )
