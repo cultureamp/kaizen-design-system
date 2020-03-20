36e8510fc03783fb2aca96fc802e6d6b12393ddc
Seb Pearce <sebpearce@gmail.com>
2020-03-20 13:06:17 +1100

    Add colors to Heading component

--- packages/component-library/stories/Heading.stories.tsx
+++ packages/component-library/stories/Heading.stories.tsx
@@ -1,4 +1,5 @@
 import { loadElmStories } from "@cultureamp/elm-storybook"
+import * as colorTokens from "@kaizen/design-tokens/tokens/color.json"
 import * as React from "react"
 import { Box } from "../components/Box"
 import { Heading } from "../components/Heading"
@@ -22,6 +23,76 @@ export const Heading4 = () => <Heading variant="heading-4">Heading 4</Heading>
 export const Heading5 = () => <Heading variant="heading-5">Heading 5</Heading>
 
 export const Heading6 = () => <Heading variant="heading-6">Heading 6</Heading>
+
+const Heading1DarkTranslucent = () => (
+  <Heading variant="heading-1" color="dark-translucent">
+    Heading 1 (dark, translucent)
+  </Heading>
+)
+
+const Heading1Light = () => (
+  <Heading variant="heading-1" color="light">
+    Heading 1 (light)
+  </Heading>
+)
+
+const Heading1LightTranslucent = () => (
+  <Heading variant="heading-1" color="light-translucent">
+    Heading 1 (light, translucent)
+  </Heading>
+)
+
+export const Heading1Positive = () => (
+  <Heading variant="heading-1" color="positive">
+    Heading 1 (positive)
+  </Heading>
+)
+
+export const Heading1Negative = () => (
+  <Heading variant="heading-1" color="negative">
+    Heading 1 (negative)
+  </Heading>
+)
+
+export const Heading3Positive = () => (
+  <Heading variant="heading-3" color="positive">
+    Heading 3 (positive)
+  </Heading>
+)
+
+export const Heading3Negative = () => (
+  <Heading variant="heading-3" color="negative">
+    Heading 3 (negative)
+  </Heading>
+)
+
+Heading1Light.story = {
+  name: "Heading 1 Light",
+  parameters: {
+    backgrounds: [
+      {
+        name: "Wisteria 700",
+        value: colorTokens.kz.color.wisteria["700"],
+        default: true,
+      },
+    ],
+  },
+}
+
+Heading1LightTranslucent.story = {
+  name: "Heading 1 Light Translucent",
+  parameters: {
+    backgrounds: [
+      {
+        name: "Wisteria 700",
+        value: colorTokens.kz.color.wisteria["700"],
+        default: true,
+      },
+    ],
+  },
+}
+
+export { Heading1Light, Heading1LightTranslucent, Heading1DarkTranslucent }
 
 loadElmStories("Heading (Elm)", module, require("./Heading.stories.elm"), [
   "Display0",
