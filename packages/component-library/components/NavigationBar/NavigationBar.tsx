6042908c279b4766f22a09eb4a53ee6a927a74e4
Casey Sinclair <casey@cultureamp.com>
2020-04-16 14:53:36 +1000

    Add a section renderer to separate primary, secondary and final link sections

--- packages/component-library/components/NavigationBar/NavigationBar.tsx
+++ packages/component-library/components/NavigationBar/NavigationBar.tsx
@@ -19,13 +19,19 @@ type SupportedChild =
   | React.ReactElement<LinkProps>
   | React.ReactElement<MenuProps>
 
+type Children = {
+  primary?: SupportedChild[]
+  secondary?: SupportedChild[]
+  final?: SupportedChild[]
+}
+
 type Props = {
   environment?: "production" | "staging" | "test" | "local"
   loading?: boolean
   colorScheme?: "cultureamp" | "kaizen" | "content"
   badgeHref?: string
   footerComponent?: React.ReactNode
-  children?: { primary?: any; secondary?: any; final?: any }
+  children?: Children
 }
 
 export default class NavigationBar extends React.Component<Props> {
@@ -37,45 +43,11 @@ export default class NavigationBar extends React.Component<Props> {
     loading: false,
     colorScheme: "cultureamp",
     badgeHref: "/",
-    primary: [],
-    secondary: [],
-    final: [],
   }
 
   render() {
-    const {
-      children,
-      colorScheme = "cultureamp",
-    } = this.props
-    const links: SupportedChild[] = []
-    if (!children) return null;
-
-    React.Children.toArray([children.primary, children.secondary, children.final]).forEach(child => {
-      if (!child) {
-        return
-      }
-
-      // Because react-hot-loader wraps the type, and uglify changes the type name,
-      // we compare the displayName rather than comparing the type of name directly.
-      const childElement = child as any
-
-      if (
-        childElement.type &&
-        childElement.type.displayName === Link.displayName
-      ) {
-        links.push(child as React.ReactElement<LinkProps>)
-      } else if (
-        childElement.type &&
-        childElement.type.displayName === Menu.displayName
-      ) {
-        links.push(child as React.ReactElement<MenuProps>)
-      } else {
-        // tslint:disable-next-line: no-console
-        console.warn(
-          "A child Element has been provided to <NavigationBar /> that is not of type `Menu` or `Link`."
-        )
-      }
-    })
+    const { children, colorScheme = "cultureamp" } = this.props
+    if (!children) return null
 
     return (
       <Media query={MOBILE_QUERY}>
@@ -84,7 +56,7 @@ export default class NavigationBar extends React.Component<Props> {
             <ControlledOffCanvas
               headerComponent={this.renderBadge()}
               footerComponent={this.props.footerComponent}
-              links={[...links]}
+              links={[children]}
               heading="Menu"
               menuId="menu"
             />
@@ -93,11 +65,35 @@ export default class NavigationBar extends React.Component<Props> {
               className={classNames(styles.navigationBar, styles[colorScheme])}
             >
               {this.renderBadge()}
-              {this.renderNavItems(links)}
+              {this.renderNav(children)}
             </header>
           )
         }
       </Media>
+    )
+  }
+
+  renderNav(children: Children) {
+    return (
+      <nav className={styles.links}>
+        {Object.keys(children).map(key =>
+          this.renderNavSection(key, children[key])
+        )}
+      </nav>
+    )
+  }
+
+  renderNavSection(section, items: SupportedChild[]) {
+    return (
+      <ul
+        className={classNames({
+          [styles.primary]: section === "primary",
+          [styles.secondary]: section === "secondary",
+          [styles.final]: section === "final",
+        })}
+      >
+        {items.map((item, index) => this.renderNavItem(item, index))}
+      </ul>
     )
   }
 
@@ -133,14 +129,6 @@ export default class NavigationBar extends React.Component<Props> {
 
     return (
       <Badge loading={loading} href={badgeHref} colorScheme={colorScheme} />
-    )
-  }
-
-  renderNavItems(links: SupportedChild[]) {
-    return (
-      <nav className={styles.links}>
-        <ul>{links.map((link, index) => this.renderNavItem(link, index))}</ul>
-      </nav>
     )
   }
 }
