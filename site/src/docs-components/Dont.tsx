181354f186a53f637e79ae30bfa9f56e2ecd07be
Diana MacDonald <diana.macdonald@cultureamp.com>
2020-04-17 15:36:09 +1000

    docs: add content Do and Dont components

---
+++ site/src/docs-components/Dont.tsx
@@ -0,0 +1,41 @@
+import classnames from "classnames"
+import * as React from "react"
+import DoOrDontItem from "./DoOrDontItem"
+
+const styles = require("./DoAndDontContainer.scss")
+
+const maxItems = 3
+
+export default ({ children }) => (
+  <div className={classnames(styles.tagAndCard)}>
+    <div className={classnames(styles.dontText)}>Don't:</div>
+    <div className={classnames(styles.card, styles.dontCard)}>
+      <ul className={classnames(styles.doOrDontList)}>
+        {React.Children.map(children, (ul) => {
+          if (React.Children.count(ul.props.children) > maxItems) {
+            return React.Children.map(ul.props.children, (li) => {
+              return (
+                <li className={classnames(styles.doOrDontListItem)}>
+                  <DoOrDontItem variant="dont" />
+                  {li.props.children}
+                </li>
+              );
+            }
+            ).concat(<li className={classnames(styles.doOrDontListItem, styles.tooManyItemsWarning)}>
+              We only want to show {maxItems} items. Please remove some.
+            </li>);
+          } else {
+            return React.Children.map(ul.props.children, (li) => {
+              return (
+                <li className={classnames(styles.doOrDontListItem)}>
+                  <DoOrDontItem variant="dont" />
+                  {li.props.children}
+                </li>
+              );
+            })
+          }
+        })}
+      </ul>
+    </div>
+  </div>
+)
