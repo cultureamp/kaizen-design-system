2de6c08992bde3c2239b532eb87a73e89140c912
Samuel Norris <samuel.norris@cultureamp.com>
2020-04-27 14:32:42 +1000

    fix: corrected click events (#418)
    * fix: corrected click events
    
    * fix: lint error
    
    * fix: lint error
    
    * fix: lint error

--- packages/component-library/draft/Kaizen/ZenNavigationBar/components/MenuGroup.tsx
+++ packages/component-library/draft/Kaizen/ZenNavigationBar/components/MenuGroup.tsx
@@ -1,26 +1,25 @@
 import React from "react"
 
 import classNames from "classnames"
-import { MenuItemProps } from "../types"
+import { MenuGroupProps, MenuItemProps } from "../types"
 import Link from "./Link"
 import MenuItem from "./MenuItem"
 
 const styles = require("./MenuGroup.module.scss")
 
-type Props = {
-  title: string
-  items: MenuItemProps[]
-  index: number
-  offCanvas?: boolean
-}
-
-const MenuGroup = ({ title, items, index, offCanvas }: Props) => {
+const MenuGroup = ({
+  title,
+  items,
+  first = false,
+  offCanvas,
+  onLinkClick,
+}: MenuGroupProps) => {
   const renderOffCanvasMenuItem = (item: MenuItemProps) => (
     <Link
       key={item.url}
       text={item.label}
       href={item.url}
-      onClick={item.onLinkClick}
+      onClick={onLinkClick}
     />
   )
 
@@ -29,7 +28,7 @@ const MenuGroup = ({ title, items, index, offCanvas }: Props) => {
       <ul
         className={classNames(styles.container, {
           [styles.offCanvas]: true,
-          [styles.firstMenuItem]: index === 0,
+          [styles.firstMenuItem]: first,
         })}
       >
         <h4 className={styles.title}>{title}</h4>
@@ -42,7 +41,7 @@ const MenuGroup = ({ title, items, index, offCanvas }: Props) => {
     <li className={styles.container}>
       <h4 className={styles.title}>{title}</h4>
       {items.map(item => (
-        <MenuItem {...item} />
+        <MenuItem {...item} onLinkClick={onLinkClick} />
       ))}
     </li>
   )
