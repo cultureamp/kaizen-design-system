d05bf2c62a598ca80e662b9cdc192cf9a4310e41
Casey Sinclair <casey@cultureamp.com>
2020-05-04 13:20:03 +1000

    Add two more guidance block variances

--- draft-packages/stories/GuidanceBlock.stories.tsx
+++ draft-packages/stories/GuidanceBlock.stories.tsx
@@ -1,8 +1,9 @@
 import * as colorTokens from "@kaizen/design-tokens/tokens/color.json"
 import * as React from "react"
 
-import { GuidanceBlock } from "@kaizen/component-library/draft"
+import { GuidanceBlock } from "@kaizen/draft-guidance-block"
 const bannerImg = require("@kaizen/component-library/stories/illustrations/team.png")
+const meetingImg = require("@kaizen/component-library/stories/illustrations/meeting-voices.png")
 
 export default {
   title: "GuidanceBlock (React)",
@@ -30,6 +31,56 @@ const Default = () => (
   </div>
 )
 
+const Persistent = () => (
+  <div style={{ display: "flex", justifyContent: "center", margin: "100px" }}>
+    <GuidanceBlock
+      img={{ src: meetingImg, alt: "Meeting illustration" }}
+      text={{
+        title: "You're using the updated navigation",
+        description:
+          "This beta is to test out the new navigation, read about our thinking behind the change. And let us know if you have any feedback",
+      }}
+      button={{
+        label: "Turn on for the whole org",
+        onClick: () => {
+          alert("tada: 🎉")
+        },
+      }}
+      onDismiss={() => {
+        /* do nothing */
+      }}
+      persistent
+    />
+  </div>
+)
+
+const SecondaryAction = () => (
+  <div style={{ display: "flex", justifyContent: "center", margin: "100px" }}>
+    <GuidanceBlock
+      img={{ src: meetingImg, alt: "Meeting illustration" }}
+      text={{
+        title: "You're using the updated navigation",
+        description:
+          "This beta is to test out the new navigation, read about our thinking behind the change. And let us know if you have any feedback",
+      }}
+      button={{
+        label: "Turn on for the whole org",
+        onClick: () => {
+          alert("tada: 🎉")
+        },
+      }}
+      secondaryAction={{
+        label: "Turn off new navigation",
+        onClick: () => alert("tada: 🎉"),
+      }}
+      onDismiss={() => {
+        /* do nothing */
+      }}
+      persistent
+    />
+  </div>
+)
+
 Default.story = {
   name: "Default",
   parameters: {
@@ -43,4 +94,30 @@ Default.story = {
   },
 }
 
-export { Default }
+Persistent.story = {
+  name: "Persistent",
+  parameters: {
+    backgrounds: [
+      {
+        name: "Stone",
+        value: colorTokens.kz.color.stone,
+        default: true,
+      },
+    ],
+  },
+}
+
+SecondaryAction.story = {
+  name: "Secondary Action",
+  parameters: {
+    backgrounds: [
+      {
+        name: "Stone",
+        value: colorTokens.kz.color.stone,
+        default: true,
+      },
+    ],
+  },
+}
+
+export { Default, Persistent, SecondaryAction }
