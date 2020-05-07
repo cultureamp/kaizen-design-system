d56c0107063df357fa649b67e804572e9268999d
Julio <send.to.julio@gmail.com>
2020-05-07 16:34:15 +1000

    make everything splitbutton on the same file

--- draft-packages/split-button/KaizenDraft/SplitButton/SplitButton.elm
+++ draft-packages/split-button/KaizenDraft/SplitButton/SplitButton.elm
@@ -1,59 +1,159 @@
-module KaizenDraft.SplitButton.SplitButton exposing (default, href, onClick, view)
+module KaizenDraft.SplitButton.SplitButton exposing (Msg(..), SplitButton, default, href, init, menuItems, update, view)
 
 import CssModules exposing (css)
-import Html exposing (Html, a, div, span, text)
-import Html.Attributes exposing (dir)
+import Html exposing (Html, a, button, div, span, text)
+import Html.Attributes exposing (dir, style)
+import Html.Events exposing (onClick)
+import Html.Extra exposing (static, viewIf)
+import Icon.Icon as Icon
+import Icon.SvgAsset exposing (svgAsset)
+import KaizenDraft.MenuList.MenuList as MenuList
 
 
 
--- Config
+-- MODEL
 
 
-type Config msg
-    = Config (ConfigValue msg)
+type alias SplitButtonId =
+    String
 
 
-type alias ConfigValue msg =
+type DropdownMenuVisibility
+    = Open SplitButtonId
+    | Closed
+
+
+type SplitButton
+    = SplitButton
+        { id : SplitButtonId
+        , dropdownMenuVisibility : DropdownMenuVisibility
+        , disabled : Bool
+        }
+
+
+init : SplitButton
+init =
+    SplitButton { id = "", dropdownMenuVisibility = Closed, disabled = False }
+
+
+
+-- CONFIG
+
+
+type Config menuItemMsg
+    = Config (ConfigValue menuItemMsg)
+
+
+type alias ConfigValue menuItemMsg =
     { href : String
-    , disabled : Bool
-    , onClick : Maybe msg
+    , menuItems : List (MenuList.MenuItemConfig menuItemMsg)
     , dir : String
     }
 
 
-default : Config msg
+default : Config menuItemMsg
 default =
-    Config { href = "", disabled = False, onClick = Nothing, dir = "ltr" }
+    Config { href = "", menuItems = [], dir = "ltr" }
+
+
+menuItems : List (MenuList.MenuItemConfig menuItemMsg) -> Config menuItemMsg -> Config menuItemMsg
+menuItems items (Config config) =
+    Config { config | menuItems = items }
 
 
 
 -- MODIFIERS
 
 
-href : String -> Config msg -> Config msg
+href : String -> Config menuItemMsg -> Config menuItemMsg
 href value (Config config) =
     Config { config | href = value }
 
 
-onClick : msg -> Config msg -> Config msg
-onClick value (Config config) =
-    Config { config | onClick = Just value }
+
+--UPDATE
+
+
+type Msg menuItemMsg
+    = ToggleDropdown
+    | MenuItem menuItemMsg
+
+
+update : Msg menuItemMsg -> SplitButton -> ( SplitButton, Cmd (Msg menuItemMsg) )
+update msg (SplitButton model) =
+    case msg of
+        ToggleDropdown ->
+            let
+                newVisibility =
+                    case model.dropdownMenuVisibility of
+                        Closed ->
+                            Open ""
+
+                        Open _ ->
+                            Closed
+            in
+            ( SplitButton { model | dropdownMenuVisibility = newVisibility }, Cmd.none )
+
+        MenuItem _ ->
+            ( SplitButton model, Cmd.none )
 
 
 
 -- VIEW
 
 
-view : Config msg -> String -> Html msg
-view (Config config) label =
+view : Config menuItemMsg -> SplitButton -> String -> Html (Msg menuItemMsg)
+view (Config config) (SplitButton splitButton) label =
+    let
+        mappedMenuItems : List (MenuList.MenuItemConfig (Msg menuItemMsg))
+        mappedMenuItems =
+            config.menuItems
+                |> List.map
+                    (\item -> { label = item.label, onClickMsg = MenuItem item.onClickMsg })
+    in
     div [ styles.class .root, dir config.dir ]
         [ div [ styles.class .buttonsContainer ]
-            [ a [ styles.class .default, Html.Attributes.href config.href ]
-                [ span [ styles.class .label ]
-                    [ text label ]
+            [ viewPrimaryActionButton config.href label
+            , viewDropdown
+            , viewIf (splitButton.dropdownMenuVisibility == Open "") <|
+                viewDropdownMenu mappedMenuItems
+            ]
+        ]
+
+
+viewPrimaryActionButton : String -> String -> Html (Msg menuItemMsg)
+viewPrimaryActionButton url label =
+    a [ styles.class .default, Html.Attributes.href url ]
+        [ span [ styles.class .label ]
+            [ text label ]
+        ]
+
+
+viewDropdown : Html (Msg msg)
+viewDropdown =
+    div [ styles.class .dropdown ]
+        [ button
+            [ onClick ToggleDropdown
+            , styles.classList [ ( .dropdownButton, True ), ( .dropdownButtonDefault, True ) ]
+            ]
+            [ span
+                [ styles.class .dropdownIcon ]
+                [ Icon.view Icon.presentation
+                    (svgAsset "@kaizen/component-library/icons/chevron-down.icon.svg")
+                    |> static
                 ]
             ]
         ]
+
+
+viewDropdownMenu : List (MenuList.MenuItemConfig (Msg menuItemMsg)) -> Html (Msg menuItemMsg)
+viewDropdownMenu menuItems_ =
+    div
+        [ styles.class .menuContainer
+        , style "right" "0px"
+        , style "top" "40px"
+        ]
+        [ MenuList.view { items = menuItems_ } ]
 
 
 styles =
@@ -62,4 +162,9 @@ styles =
         , buttonsContainer = "buttonsContainer"
         , label = "label"
         , default = "default"
+        , dropdown = "dropdown"
+        , dropdownButton = "dropdownButton"
+        , dropdownButtonDefault = "dropdownButtonDefault"
+        , dropdownIcon = "dropdownIcon"
+        , menuContainer = "menuContainer"
         }
--- draft-packages/stories/SplitButtonStories.elm
+++ draft-packages/stories/SplitButtonStories.elm
@@ -1,11 +1,66 @@
 module Main exposing (main)
 
-import ElmStorybook exposing (statelessStoryOf, storybook)
-import KaizenDraft.SplitButton.SplitButton as SplitButton
+import ElmStorybook exposing (storyOf, storybook)
+import Html
+import KaizenDraft.SplitButton.SplitButton as SplitButton exposing (SplitButton, menuItems)
+
+
+type alias Model =
+    { splitButton : SplitButton }
+
+
+type Msg
+    = Foo
+    | Bar
+    | SplitButtonMsg (SplitButton.Msg Msg)
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Foo ->
+            let
+                ( newSplitButton, splitButtonCmds ) =
+                    SplitButton.update (SplitButton.MenuItem Foo) model.splitButton
+            in
+            ( { model | splitButton = newSplitButton }, splitButtonCmds |> Cmd.map SplitButtonMsg )
+
+        Bar ->
+            let
+                ( newSplitButton, splitButtonCmds ) =
+                    SplitButton.update (SplitButton.MenuItem Foo) model.splitButton
+            in
+            ( { model | splitButton = newSplitButton }, splitButtonCmds |> Cmd.map SplitButtonMsg )
+
+        SplitButtonMsg subMsg ->
+            let
+                ( newSplitButton, splitButtonCmds ) =
+                    SplitButton.update subMsg model.splitButton
+            in
+            ( { model | splitButton = newSplitButton }, splitButtonCmds |> Cmd.map SplitButtonMsg )
 
 
 main =
+    let
+        menuItems_ =
+            [ { label = "Foo", onClickMsg = Foo }
+            , { label = "Bar", onClickMsg = Bar }
+            ]
+
+        config =
+            { update = update
+            , init = ( { splitButton = SplitButton.init }, Cmd.none )
+            , subscriptions = \_ -> Sub.none
+            }
+    in
     storybook
-        [ statelessStoryOf "Default" <|
-            SplitButton.view SplitButton.default "Label"
+        [ storyOf "Default" config <|
+            \model ->
+                Html.map SplitButtonMsg <|
+                    SplitButton.view
+                        (SplitButton.default
+                            |> menuItems menuItems_
+                        )
+                        model.splitButton
+                        "Label"
         ]
