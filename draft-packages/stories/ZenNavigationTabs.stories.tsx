4397b1a30909d8c8ad9f05903ffe8b59f1fc3fa4
Seb Pearce <sebpearce@gmail.com>
2020-05-08 12:28:52 +1000

    feat: Create ZenNavigationTabs component

--- draft-packages/stories/ZenNavigationTabs.stories.tsx
+++ draft-packages/stories/ZenNavigationTabs.stories.tsx
@@ -1,36 +1,40 @@
-import { ZenNavigationTabs } from "@kaizen/draft-zen-navigation-tabs"
+import * as colorTokens from "@kaizen/design-tokens/tokens/color.json"
 import * as React from "react"
-
-const surveyIllustration = require("../illustrations/survey.png")
+import { ZenNavigationTabs } from "../zen-navigation-tabs"
 
 const Container = ({ children }: { children: React.ReactNode }) => (
-  <div style={{ padding: "20px", display: "flex" }}>{children}</div>
-)
-
-const renderContent = () => (
   <div
     style={{
-      width: "560px",
-      height: "300px",
+      height: "84px",
+      background: colorTokens.kz.color.wisteria[700],
+      display: "flex",
     }}
   >
-    <p style={{ margin: "0" }}>
-      Understand diversity and inclusion in your organization with this set of
-      people science backed survey questions. Preview the questions to get a
-      feel for what the participants will see.
-    </p>
-    <Button label="Preview questions" />
+    {children}
   </div>
 )
 
 export default {
-  title: "HeroCard (React)",
+  title: "ZenNavigationTabs (React)",
 }
 
+// <Link  />,
 export const DefaultKaizenSiteDemo = () => (
-  <Container>
-    <HeroCard>{renderContent()}</HeroCard>
-  </Container>
+  <>
+    <Container>
+      <ZenNavigationTabs.Container>
+        <ZenNavigationTabs.Link text="Insight" href="#" />
+        <ZenNavigationTabs.Link text="Participation" href="#" active />
+        <ZenNavigationTabs.Link text="Questions" href="#" />
+        <ZenNavigationTabs.Link text="Heatmap" href="#" />
+        <ZenNavigationTabs.Link text="Comments" href="#" />
+      </ZenNavigationTabs.Container>
+    </Container>
+    {/* <p>
+      Note that this component does not set a background color – that is being
+      set by the container here.
+    </p> */}
+  </>
 )
 
 DefaultKaizenSiteDemo.story = {
@@ -39,89 +43,6 @@ DefaultKaizenSiteDemo.story = {
 
 export const Title = () => (
   <Container>
-    <HeroCard title={<h1>Preview the survey questions</h1>}>
-      {renderContent()}
-    </HeroCard>
+    <ZenNavigationTabs>"test"</ZenNavigationTabs>
   </Container>
 )
-
-export const Badge = () => (
-  <Container>
-    <HeroCard
-      title={<h1>Preview the survey questions</h1>}
-      badge={<span>1</span>}
-    >
-      {renderContent()}
-    </HeroCard>
-  </Container>
-)
-
-export const Image = () => (
-  <Container>
-    <HeroCard
-      title={<h1>Preview the survey questions</h1>}
-      badge={<span>1</span>}
-      image={
-        <img
-          src={surveyIllustration}
-          alt="survey-preview-image"
-          style={{
-            position: "absolute",
-            bottom: "15px",
-            left: "0",
-            width: "100%",
-          }}
-        />
-      }
-    >
-      {renderContent()}
-    </HeroCard>
-  </Container>
-)
-
-export const CustomLeftContent = () => (
-  <Container>
-    <HeroCard
-      title={<h1>Preview the survey questions</h1>}
-      leftContent={<p>Ta-dah</p>}
-    >
-      {renderContent()}
-    </HeroCard>
-  </Container>
-)
-
-CustomLeftContent.story = {
-  name: "Custom left content",
-}
-
-export const CustomLeftContentAndBadge = () => (
-  <Container>
-    <HeroCard
-      title={<h1>Preview the survey questions</h1>}
-      leftContent={<p>Ta-dah</p>}
-      badge="2"
-    >
-      {renderContent()}
-    </HeroCard>
-  </Container>
-)
-
-CustomLeftContentAndBadge.story = {
-  name: "Custom left content and badge",
-}
-
-export const FullWidth = () => (
-  <Container>
-    <HeroCard
-      title={<h1>Preview the survey questions</h1>}
-      leftContent={<p>Ta-dah</p>}
-      fullWidth
-    >
-      {renderContent()}
-    </HeroCard>
-  </Container>
-)
-
-FullWidth.story = {
-  name: "Full width",
-}
--- draft-packages/zen-navigation-tabs/ZenNavigationTabs.tsx
+++ draft-packages/zen-navigation-tabs/ZenNavigationTabs.tsx
@@ -1,3 +1,4 @@
+import classnames from "classnames"
 import * as React from "react"
 
 const styles = require("./styles.scss")
@@ -5,16 +6,54 @@ const styles = require("./styles.scss")
 export type ZenNavigationTabsProps = {
   id?: string
   automationId?: string
+
   //   onChange?: (event: React.ChangeEvent<HTMLInputElement>) => any
   //   disabled?: boolean
 }
 
-type ZenNavigationTabs = React.FunctionComponent<ZenNavigationTabsProps>
+export type ZenNavigationTabsLinkProps = {
+  text: string
+  href: string
+  active?: boolean
+}
 
-const ZenNavigationTabs: ZenNavigationTabs = ({
-  id,
-  automationId,
-  children,
-}) => <div className={styles.container}>{children}</div>
+const Link = (props: ZenNavigationTabsLinkProps) => (
+  <a className={styles.linkAnchor} href={props.href}>
+    <div
+      className={classnames(styles.linkLabel, {
+        [styles.active]: props.active,
+      })}
+    >
+      {props.text}
+    </div>
+  </a>
+)
+
+const Container = ({ children }) => (
+  <div className={styles.container}>{children}</div>
+)
+
+// type ZenNavigationTabs = React.FunctionComponent<ZenNavigationTabsProps>
+
+class ZenNavigationTabs extends React.Component<ZenNavigationTabsProps> {
+  static Link = Link
+  static Container = Container
+
+  constructor(props) {
+    super(props)
+  }
+
+  // render() {
+  //   const { children } = this.props
+
+  //   return
+  // }
+}
+
+// const ZenNavigationTabs: ZenNavigationTabs = ({
+//   id,
+//   automationId,
+//   children,
+// }) =>
 
 export default ZenNavigationTabs
