1470dd1b62b97610b43f6a25957c6e14a5de3d7c
Ally Beaton <allanna.beaton@gmail.com>
2020-05-20 11:31:20 +1000

    feat: React illustrations (#472)
    feat: Added scene and spot illustrations
    
    Co-authored-by: Seb Pearce <sebpearce@gmail.com>

---
+++ draft-packages/illustration/KaizenDraft/Illustration/Spot.tsx
@@ -0,0 +1,361 @@
+import * as React from "react"
+import { Base, BaseProps } from "./Base"
+
+export type SpotProps = Pick<BaseProps, "alt" | "classNameAndIHaveSpokenToDST">
+
+export const BenefitsSurvey = (props: SpotProps) => (
+  <Base
+    {...props}
+    name="illustrations/spot/template-library-benefits-survey.svg"
+  />
+)
+
+export const CustomSurvey = (props: SpotProps) => (
+  <Base
+    {...props}
+    name="illustrations/spot/template-library-custom-survey.svg"
+  />
+)
+
+export const CustomUnattributedSurvey = (props: SpotProps) => (
+  <Base
+    {...props}
+    name="illustrations/spot/template-library-custom-unattributed-survey.svg"
+  />
+)
+
+export const EngagementSurvey = (props: SpotProps) => (
+  <Base
+    {...props}
+    name="illustrations/spot/template-library-engagement-survey.svg"
+  />
+)
+
+export const InclusionSurvey = (props: SpotProps) => (
+  <Base
+    {...props}
+    name="illustrations/spot/template-library-inclusion-survey.svg"
+  />
+)
+
+export const QuickEngagementSurvey = (props: SpotProps) => (
+  <Base
+    {...props}
+    name="illustrations/spot/template-library-quick-engagement-survey.svg"
+  />
+)
+
+export const ValuesSurvey1 = (props: SpotProps) => (
+  <Base
+    {...props}
+    name="illustrations/spot/template-library-values-survey-1.svg"
+  />
+)
+
+export const ValuesSurvey2 = (props: SpotProps) => (
+  <Base
+    {...props}
+    name="illustrations/spot/template-library-values-survey-2.svg"
+  />
+)
+
+export const WellbeingSurvey1 = (props: SpotProps) => (
+  <Base
+    {...props}
+    name="illustrations/spot/template-library-wellbeing-survey-1.svg"
+  />
+)
+
+export const WellbeingSurvey2 = (props: SpotProps) => (
+  <Base
+    {...props}
+    name="illustrations/spot/template-library-wellbeing-survey-2.svg"
+  />
+)
+
+export const WellbeingSurvey3 = (props: SpotProps) => (
+  <Base
+    {...props}
+    name="illustrations/spot/template-library-wellbeing-survey-3.svg"
+  />
+)
+
+export const ChangeReadiness = (props: SpotProps) => (
+  <Base
+    {...props}
+    name="illustrations/spot/template-library-change-readiness.svg"
+  />
+)
+
+export const ChangeSuccess = (props: SpotProps) => (
+  <Base
+    {...props}
+    name="illustrations/spot/template-library-change-success.svg"
+  />
+)
+
+export const CandidateSurvey = (props: SpotProps) => (
+  <Base
+    {...props}
+    name="illustrations/spot/template-library-candidate-survey.svg"
+  />
+)
+
+export const CustomOnboardSurvey = (props: SpotProps) => (
+  <Base
+    {...props}
+    name="illustrations/spot/template-library-custom-onboard-survey.svg"
+  />
+)
+
+export const ExitSurvey = (props: SpotProps) => (
+  <Base {...props} name="illustrations/spot/template-library-exit-survey.svg" />
+)
+
+export const InternSurvey = (props: SpotProps) => (
+  <Base
+    {...props}
+    name="illustrations/spot/template-library-intern-survey.svg"
+  />
+)
+
+export const PhasedWeek1OnboardSurvey = (props: SpotProps) => (
+  <Base
+    {...props}
+    name="illustrations/spot/template-library-phased-week-1-onboard-survey.svg"
+  />
+)
+
+export const PhasedWeek5OnboardSurvey = (props: SpotProps) => (
+  <Base
+    {...props}
+    name="illustrations/spot/template-library-phased-week-5-onboard-survey.svg"
+  />
+)
+
+export const SinglePointOnboardSurvey = (props: SpotProps) => (
+  <Base
+    {...props}
+    name="illustrations/spot/template-library-single-point-onboard-survey.svg"
+  />
+)
+
+export const Individual360 = (props: SpotProps) => (
+  <Base
+    {...props}
+    name="illustrations/spot/template-library-individual-360.svg"
+  />
+)
+
+export const Leadership360 = (props: SpotProps) => (
+  <Base
+    {...props}
+    name="illustrations/spot/template-library-leadership-360.svg"
+  />
+)
+
+export const Manager360 = (props: SpotProps) => (
+  <Base {...props} name="illustrations/spot/template-library-manager-360.svg" />
+)
+
+export const TeamEffectiveness1 = (props: SpotProps) => (
+  <Base
+    {...props}
+    name="illustrations/spot/template-library-team-effectiveness-1.svg"
+  />
+)
+
+export const TeamEffectiveness2 = (props: SpotProps) => (
+  <Base
+    {...props}
+    name="illustrations/spot/template-library-team-effectiveness-2.svg"
+  />
+)
+
+export const AccountBasics = (props: SpotProps) => (
+  <Base {...props} name="illustrations/spot/new-account-account-basics.svg" />
+)
+
+export const CompanyDetails = (props: SpotProps) => (
+  <Base {...props} name="illustrations/spot/new-account-company-details.svg" />
+)
+
+export const EmployeeData = (props: SpotProps) => (
+  <Base {...props} name="illustrations/spot/new-account-employee-data.svg" />
+)
+
+export const Gdpr = (props: SpotProps) => (
+  <Base {...props} name="illustrations/spot/new-account-gdpr.svg" />
+)
+
+export const Timezone = (props: SpotProps) => (
+  <Base {...props} name="illustrations/spot/new-account-timezone.svg" />
+)
+
+export const AddUser = (props: SpotProps) => (
+  <Base {...props} name="illustrations/spot/new-account-add-user.svg" />
+)
+
+export const ViewReports = (props: SpotProps) => (
+  <Base {...props} name="illustrations/spot/miscellaneous-view-reports.svg" />
+)
+
+export const ReadArticle = (props: SpotProps) => (
+  <Base {...props} name="illustrations/spot/miscellaneous-read-article.svg" />
+)
+
+export const FastAction = (props: SpotProps) => (
+  <Base {...props} name="illustrations/spot/miscellaneous-fast-action.svg" />
+)
+
+export const BaselineSurvey = (props: SpotProps) => (
+  <Base
+    {...props}
+    name="illustrations/spot/miscellaneous-baseline-survey.svg"
+  />
+)
+
+export const SpreadsheetTemplate = (props: SpotProps) => (
+  <Base
+    {...props}
+    name="illustrations/spot/miscellaneous-spreadsheet-template.svg"
+  />
+)
+
+export const AddImage = (props: SpotProps) => (
+  <Base {...props} name="illustrations/spot/miscellaneous-add-image.svg" />
+)
+
+export const MeetingVoices = (props: SpotProps) => (
+  <Base {...props} name="illustrations/spot/miscellaneous-meeting-voices.svg" />
+)
+
+export const Workshop = (props: SpotProps) => (
+  <Base {...props} name="illustrations/spot/miscellaneous-workshop.svg" />
+)
+
+export const Video = (props: SpotProps) => (
+  <Base {...props} name="illustrations/spot/miscellaneous-video.svg" />
+)
+
+export const ReportSharing = (props: SpotProps) => (
+  <Base {...props} name="illustrations/spot/miscellaneous-report-sharing.svg" />
+)
+
+export const BlankSurvey = (props: SpotProps) => (
+  <Base {...props} name="illustrations/spot/miscellaneous-blank-survey.svg" />
+)
+
+export const TakeAim = (props: SpotProps) => (
+  <Base {...props} name="illustrations/spot/miscellaneous-take-aim.svg" />
+)
+
+export const Action = (props: SpotProps) => (
+  <Base {...props} name="illustrations/spot/miscellaneous-action.svg" />
+)
+
+export const Training1 = (props: SpotProps) => (
+  <Base {...props} name="illustrations/spot/miscellaneous-training-1.svg" />
+)
+
+export const Training2 = (props: SpotProps) => (
+  <Base {...props} name="illustrations/spot/miscellaneous-training-2.svg" />
+)
+
+export const Training3 = (props: SpotProps) => (
+  <Base {...props} name="illustrations/spot/miscellaneous-training-3.svg" />
+)
+
+export const ShareReport = (props: SpotProps) => (
+  <Base {...props} name="illustrations/spot/miscellaneous-share-report.svg" />
+)
+
+export const Team = (props: SpotProps) => (
+  <Base {...props} name="illustrations/spot/miscellaneous-team.svg" />
+)
+
+export const ExecutiveReportSharing = (props: SpotProps) => (
+  <Base
+    {...props}
+    name="illustrations/spot/miscellaneous-executive-report-sharing.svg"
+  />
+)
+
+export const ManagerReportSharing = (props: SpotProps) => (
+  <Base
+    {...props}
+    name="illustrations/spot/miscellaneous-manager-report-sharing.svg"
+  />
+)
+
+export const LeaderReportSharing = (props: SpotProps) => (
+  <Base
+    {...props}
+    name="illustrations/spot/miscellaneous-leader-report-sharing.svg"
+  />
+)
+
+export const Cautionary = (props: SpotProps) => (
+  <Base {...props} name="illustrations/spot/moods-cautionary.svg" />
+)
+
+export const Informative = (props: SpotProps) => (
+  <Base {...props} name="illustrations/spot/moods-informative.svg" />
+)
+
+export const Negative = (props: SpotProps) => (
+  <Base {...props} name="illustrations/spot/moods-negative.svg" />
+)
+
+export const PositiveMale = (props: SpotProps) => (
+  <Base {...props} name="illustrations/spot/moods-positive-male.svg" />
+)
+
+export const PositiveFemale = (props: SpotProps) => (
+  <Base {...props} name="illustrations/spot/moods-positive-female.svg" />
+)
+
+export const OneOnOne = (props: SpotProps) => (
+  <Base {...props} name="illustrations/spot/manager-learning-1-on-1.svg" />
+)
+
+export const Productivity = (props: SpotProps) => (
+  <Base
+    {...props}
+    name="illustrations/spot/manager-learning-productivity.svg"
+  />
+)
+
+export const Strategy = (props: SpotProps) => (
+  <Base {...props} name="illustrations/spot/manager-learning-strategy.svg" />
+)
+
+export const Resilience = (props: SpotProps) => (
+  <Base {...props} name="illustrations/spot/manager-learning-resilience.svg" />
+)
+
+export const Coaching = (props: SpotProps) => (
+  <Base {...props} name="illustrations/spot/manager-learning-coaching.svg" />
+)
+
+export const Feedback = (props: SpotProps) => (
+  <Base {...props} name="illustrations/spot/manager-learning-feedback.svg" />
+)
+
+export const RemoteManager = (props: SpotProps) => (
+  <Base
+    {...props}
+    name="illustrations/spot/manager-learning-remote-manager.svg"
+  />
+)
+
+export const ManagerLearning = (props: SpotProps) => (
+  <Base
+    {...props}
+    name="illustrations/spot/manager-learning-manager-learning.svg"
+  />
+)
+
+export const PerformanceDiagnostics = (props: SpotProps) => (
+  <Base {...props} name="illustrations/spot/template-library-diagnostics.svg" />
+)
