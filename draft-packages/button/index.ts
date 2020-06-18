32d3e5028b2afc1b99ba02cde7cb1e3a789d4874
Seb Pearce <sebpearce@gmail.com>
2020-06-18 17:51:20 +1000

    feat: Expose ButtonProps

--- draft-packages/button/index.ts
+++ draft-packages/button/index.ts
@@ -1,2 +1,3 @@
 export { default as Button } from "./KaizenDraft/Button/Button"
 export { default as IconButton } from "./KaizenDraft/Button/IconButton"
+export { ButtonProps } from "./KaizenDraft/Button/components/GenericButton"
