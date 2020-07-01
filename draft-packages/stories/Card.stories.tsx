1a0f98cbd6e77dfe2dfbb02f9f26a66bbc44cd16
ActuallyACat <allanna.beaton@gmail.com>
2020-07-01 14:11:44 +1000

    Removed story that misled people

--- draft-packages/stories/Card.stories.tsx
+++ draft-packages/stories/Card.stories.tsx
@@ -1,6 +1,5 @@
 import { Box, Paragraph } from "@kaizen/component-library"
 import * as colorTokens from "@kaizen/design-tokens/tokens/color.json"
-import { Tabs } from "@kaizen/draft-tabs"
 import * as React from "react"
 import { Card } from "../card/index"
 
@@ -28,62 +27,7 @@ export const CardWithSpace = () => (
 )
 
 CardWithSpace.story = {
-  name: "Formatting with box",
-  parameters: {
-    backgrounds: [
-      {
-        name: "Stone",
-        value: colorTokens.kz.color.stone,
-        default: true,
-      },
-    ],
-  },
-}
-
-export const CardAsArticle = () => (
-  <Box m={4}>
-    <Card tag="article" data-test="test">
-      <Box p={1.5}>
-        <Paragraph variant="body">
-          Change the tag of the card to whatever makes the most semantic sense
-          for your use case
-        </Paragraph>
-      </Box>
-    </Card>
-  </Box>
-)
-
-CardAsArticle.story = {
-  name: "As an article",
-  parameters: {
-    backgrounds: [
-      {
-        name: "Stone",
-        value: colorTokens.kz.color.stone,
-        default: true,
-      },
-    ],
-  },
-}
-
-export const CardAsTabContainer = () => {
-  const tabs = [
-    { label: "One" },
-    { label: "Two", active: true },
-    { label: "Three" },
-    { label: "Four" },
-  ]
-  return (
-    <Box m={4} pt={1}>
-      <Card tag="section" data-test="test">
-        <Tabs tabs={tabs} />
-      </Card>
-    </Box>
-  )
-}
-
-CardAsTabContainer.story = {
-  name: "As a tab container",
+  name: "Card, custom spacing with Box",
   parameters: {
     backgrounds: [
       {
