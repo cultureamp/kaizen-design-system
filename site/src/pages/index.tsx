e3d072f5ed7ebb1b8866daf603a365300bf39d52
Diana MacDonald <diana.macdonald@cultureamp.com>
2020-07-09 18:04:27 +1000

    docs: fix Kaizen homepage summary paragraph

--- site/src/pages/index.tsx
+++ site/src/pages/index.tsx
@@ -13,8 +13,8 @@ const styles = require("./index.scss")
 const HomePageHeader = (
   <PageHeader
     headingText="Kaizen"
-    summaryParagraph={`Kaizen is Culture Amp’s design system. It’s the single source of truth\n
-      for our UX guidelines, design assets, and front-end code to help\nCulture Amp’s teams rapidly 
+    summaryParagraph={`Kaizen is Culture Amp’s design system. It’s the single source of truth 
+      for our UX guidelines, design assets, and front-end code to help Culture Amp’s teams rapidly 
       create a world-class experience.`}
     headingOnly
   />
