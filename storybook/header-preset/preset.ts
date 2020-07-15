d41ab3e9b0dedcd554ef97db32149c0c38f9d40e
ActuallyACat <allanna.beaton@gmail.com>
2020-07-15 15:48:04 +1000

    rejigged the config

--- storybook/header-preset/preset.ts
+++ storybook/header-preset/preset.ts
@@ -1,30 +1,80 @@
 import path from "path"
-import { RuleSetRule as Rule, NormalModuleReplacementPlugin } from "webpack"
+import {
+  RuleSetRule as Rule,
+  Loader,
+  NormalModuleReplacementPlugin,
+} from "webpack"
 
 module.exports = {
   managerWebpack: async (config, options) => {
-    const newConfig = { ...config }
+    const replaceLayoutPlugin = new NormalModuleReplacementPlugin(
+      /^\.\/layout$/,
+      "custom-layout"
+    )
+
     const babel: Rule = {
       test: /\.(j|t)sx?$/,
       loader: require.resolve("babel-loader"),
       options: require("../../package.json").babel,
     }
-    const replaceLayoutPlugin = new NormalModuleReplacementPlugin(
-      /^\.\/layout$/,
-      "custom-layout"
-    )
-    // const resolvePaths = [
-    //   path.resolve("./storybook/header-preset/header"),
-    //   path.resolve("./site/src/components/MainNav")
-    // ]
 
-    newConfig.plugins.push(replaceLayoutPlugin)
-    newConfig.resolve.alias["custom-layout"] = path.resolve(
-      "./storybook/header-preset/header"
-    )
-    newConfig.module.rules.push(babel)
+    const stylePreprocessors: Loader[] = [
+      {
+        loader: "postcss-loader",
+        options: {
+          ident: "postcss",
+          plugins: () => [
+            require("postcss-flexbugs-fixes"),
+            require("postcss-preset-env")({
+              autoprefixer: { flexbox: "no-2009" },
+              stage: 3,
+            }),
+          ],
+        },
+      },
+      {
+        loader: "sass-loader",
+        options: {
+          sourceMap: true,
+        },
+      },
+    ]
 
-    return newConfig
+    const styles: Rule = {
+      test: /\.s?css$/,
+      use: [
+        {
+          loader: "style-loader",
+        },
+        {
+          loader: "css-loader",
+          options: {
+            importLoaders: stylePreprocessors.length,
+            sourceMap: true,
+            modules: {
+              localIdentName: "[folder]-[name]__[local]--[hash:base64:5]",
+            },
+          },
+        },
+        ...stylePreprocessors,
+      ],
+    }
+
+    return {
+      ...config,
+      plugins: [...config.plugins, replaceLayoutPlugin],
+      module: {
+        ...config.module,
+        rules: [...config.module.rules, babel, styles],
+      },
+      resolve: {
+        ...config.resolve,
+        alias: {
+          ...config.resolve.alias,
+          "custom-layout": path.resolve("./storybook/header-preset/header.tsx"),
+        },
+      },
+    }
   },
   webpackFinal: async (config, options) => config,
   addons: [],
