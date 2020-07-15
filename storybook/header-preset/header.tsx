53764ef3a5cf0ed6a6dec2a12ec0e2130337f340
ActuallyACat <allanna.beaton@gmail.com>
2020-07-15 16:50:14 +1000

    Updated navigation links

--- storybook/header-preset/header.tsx
+++ storybook/header-preset/header.tsx
@@ -9,16 +9,20 @@ export const SiteHeader = () => (
   <NavigationBar>
     {{
       primary: [
-        <NavLink text="Home" href="" />,
-        <NavLink text="Guidelines" href="" />,
-        <NavLink text="Language" href="" />,
-        <NavLink text="Components" href="" />,
-        <NavLink text="Storybook" href="" />,
+        <NavLink text="Home" href="/" />,
+        <NavLink text="Guidelines" href="/guidelines/overview/" />,
+        <NavLink text="Language" href="/language/overview/" />,
+        <NavLink text="Components" href="/components/overview/" />,
+        <NavLink text="Storybook" href="/storybook-static" />,
       ],
     }}
   </NavigationBar>
 )
 
+/**
+ * Inject the nav header before the root node that storybook
+ * uses to mount
+ */
 const node = document.createElement("div")
 render(<SiteHeader />, node)
 document.body.insertAdjacentElement("afterbegin", node)
