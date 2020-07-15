023f1030767277fcdf6d327d94ac3d4fe43a704e
Diana MacDonald <diana.macdonald@cultureamp.com>
2020-07-15 11:57:36 +1000

    fix: add working Loading Spinner

--- draft-packages/loading-spinner/KaizenDraft/LoadingSpinner/LoadingSpinner.tsx
+++ draft-packages/loading-spinner/KaizenDraft/LoadingSpinner/LoadingSpinner.tsx
@@ -1,14 +1,48 @@
 import * as React from "react"
+import colorTokens from "@kaizen/design-tokens/tokens/color.json"
+import spacingTokens from "@kaizen/design-tokens/tokens/spacing.json"
 const styles = require("./styles.module.scss")
 
+type size = "md" | "lg"
+
 export interface LoadingSpinnerProps {
-  /**
-   * Remember to annotate your props! The typehints make developers happy
-   * @default ""
-   */
+  accessibilityLabel: string
+  size: size
   children?: React.ReactNode
 }
 
-export const LoadingSpinner = ({ children }: LoadingSpinnerProps) => {
-  return <h1 className={styles.wrapper}>Hello, {children}</h1>
-}
+export const LoadingSpinner = ({
+  accessibilityLabel = "Loading",
+  size = "lg",
+  children,
+}: LoadingSpinnerProps) => (
+  <div
+    data-automation-id="loading-spinner"
+    className={styles.wrapper}
+    aria-label={accessibilityLabel}
+    role="status"
+  >
+    <svg
+      className={styles.spinner}
+      aria-hidden="true"
+      viewBox="0 0 48 48"
+      width={size === "lg" ? 48 : 36}
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <circle
+        cx="24"
+        cy="24"
+        r="22.5"
+        stroke={colorTokens.kz.color.wisteria[200]}
+        strokeWidth="3"
+      />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        fill={colorTokens.kz.color.seedling[400]}
+        d="M46.5 24c.8284 0 1.5049-.6734 1.4539-1.5002C47.21 10.44 37.5601.789989 25.5003.0461639 24.6734-.004835 24 .671607 24 1.50003c0 .82843.6738 1.49444 1.5002 1.55277 10.4023.73424 18.7128 9.0447 19.447 19.447C45.0056 23.3262 45.6716 24 46.5 24z"
+      />
+    </svg>
+  </div>
+)
--- draft-packages/stories/LoadingSpinner.stories.tsx
+++ draft-packages/stories/LoadingSpinner.stories.tsx
@@ -6,7 +6,17 @@ export default {
   title: "LoadingSpinner (React)",
 }
 
-export const DefaultStory = () => <LoadingSpinner>world!</LoadingSpinner>
+export const DefaultStory = () => (
+  <div
+    style={{
+      alignItems: "center",
+      display: "flex",
+      justifyContent: "center",
+    }}
+  >
+    <LoadingSpinner></LoadingSpinner>
+  </div>
+)
 
 DefaultStory.story = {
   name: "Default (Kaizen Site Demo)",
