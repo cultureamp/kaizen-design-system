a8f08dd5227b8a4030a7622a0a0e0b058a31b0f7
Diana MacDonald <diana.macdonald@cultureamp.com>
2020-07-15 13:18:38 +1000

    chore: ignore eslint warning for long path d

--- draft-packages/loading-spinner/KaizenDraft/LoadingSpinner/LoadingSpinner.tsx
+++ draft-packages/loading-spinner/KaizenDraft/LoadingSpinner/LoadingSpinner.tsx
@@ -41,6 +41,7 @@ export const LoadingSpinner = ({
         fillRule="evenodd"
         clipRule="evenodd"
         fill={colorTokens.kz.color.seedling[400]}
+        /* eslint-disable max-len */
         d="M46.5 24c.8284 0 1.5049-.6734 1.4539-1.5002C47.21 10.44 37.5601.789989 25.5003.0461639 24.6734-.004835 24 .671607 24 1.50003c0 .82843.6738 1.49444 1.5002 1.55277 10.4023.73424 18.7128 9.0447 19.447 19.447C45.0056 23.3262 45.6716 24 46.5 24z"
       />
     </svg>
