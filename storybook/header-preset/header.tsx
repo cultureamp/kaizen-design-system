f7ed95db69218040d261001252e309b856dccaa5
ActuallyACat <allanna.beaton@gmail.com>
2020-07-15 17:33:22 +1000

    more flexible url generation that works with dev environments

--- storybook/header-preset/header.tsx
+++ storybook/header-preset/header.tsx
@@ -7,22 +7,45 @@ import {
 
 const styles = require("./header.module.scss")
 
-export const SiteHeader = () => (
-  <div className={styles.wrapper}>
-    <NavigationBar>
-      {{
-        primary: [
-          <NavLink text="Home" href="/" />,
-          <NavLink text="Guidelines" href="/guidelines/overview/" />,
-          <NavLink text="Language" href="/language/overview/" />,
-          <NavLink text="Components" href="/components/overview/" />,
-          <NavLink text="Storybook" href="/storybook-static" active={true} />,
-        ],
-      }}
-    </NavigationBar>
-  </div>
-)
+export const SiteHeader = () => {
+  let branch = ""
+  if (window.location.hostname.includes("dev")) {
+    // the dev branch is appended before the rest of the pathname
+    // given this component will only ever mount on storybook-static
+    // strip that from the path
+    branch = window.location.pathname
+      .split("/")
+      .filter(curr => curr !== "" && curr !== "storybook-static")
+      .join("/")
+  }
+  const baseUrl = `${window.location.origin}/${branch}/`
 
+  return (
+    <div className={styles.wrapper}>
+      <NavigationBar>
+        {{
+          primary: [
+            <NavLink text="Home" href="/" />,
+            <NavLink
+              text="Guidelines"
+              href={`${baseUrl}/guidelines/overview/`}
+            />,
+            <NavLink text="Language" href={`${baseUrl}/language/overview/`} />,
+            <NavLink
+              text="Components"
+              href={`${baseUrl}/components/overview/`}
+            />,
+            <NavLink
+              text="Storybook"
+              href={`${baseUrl}storybook-static`}
+              active={true}
+            />,
+          ],
+        }}
+      </NavigationBar>
+    </div>
+  )
+}
 /**
  * Inject the nav header before the root node that storybook
  * uses to mount
