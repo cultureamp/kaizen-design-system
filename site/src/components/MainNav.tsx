3e7c792f286fce692502aeb4ff9fb73d60cd7384
ActuallyACat <allanna.beaton@gmail.com>
2020-07-16 14:24:05 +1000

    revert changes to mainNav

--- site/src/components/MainNav.tsx
+++ site/src/components/MainNav.tsx
@@ -6,46 +6,43 @@ type MainNavProps = {
   currentPath?: string
 }
 
-class MainNav extends React.Component<MainNavProps> {
-  render() {
-    const { currentPath = "" } = this.props
-    const currentPathStartsWith = (path: string) =>
-      currentPath.startsWith(withPrefix(path))
+const MainNav: React.SFC<MainNavProps> = ({ currentPath = "" }) => {
+  const currentPathStartsWith = (path: string) =>
+    currentPath.startsWith(withPrefix(path))
 
-    return (
-      <NavigationBar>
-        {{
-          primary: [
-            <NavLink
-              text="Home"
-              href={withPrefix("/")}
-              active={withPrefix(currentPath) == withPrefix("/")}
-            />,
-            <NavLink
-              text="Guidelines"
-              href={withPrefix("/guidelines/overview")}
-              active={currentPathStartsWith("/guidelines")}
-            />,
-            <NavLink
-              text="Language"
-              href={withPrefix("/language/overview")}
-              active={currentPathStartsWith("/language")}
-            />,
-            <NavLink
-              text="Components"
-              href={withPrefix("/components/overview")}
-              active={currentPathStartsWith("/components")}
-            />,
-            <NavLink
-              text="Storybook"
-              href={withPrefix("/storybook")}
-              active={currentPathStartsWith("/storybook")}
-            />,
-          ],
-        }}
-      </NavigationBar>
-    )
-  }
+  return (
+    <NavigationBar>
+      {{
+        primary: [
+          <NavLink
+            text="Home"
+            href={withPrefix("/")}
+            active={withPrefix(currentPath) == withPrefix("/")}
+          />,
+          <NavLink
+            text="Guidelines"
+            href={withPrefix("/guidelines/overview")}
+            active={currentPathStartsWith("/guidelines")}
+          />,
+          <NavLink
+            text="Language"
+            href={withPrefix("/language/overview")}
+            active={currentPathStartsWith("/language")}
+          />,
+          <NavLink
+            text="Components"
+            href={withPrefix("/components/overview")}
+            active={currentPathStartsWith("/components")}
+          />,
+          <NavLink
+            text="Storybook"
+            href={withPrefix("/storybook")}
+            active={currentPathStartsWith("/storybook")}
+          />,
+        ],
+      }}
+    </NavigationBar>
+  )
 }
 
 export default MainNav
