53895639dc7b8469a0ecafc25499de4b03e1c548
ActuallyACat <allanna.beaton@gmail.com>
2020-07-16 10:52:17 +1000

    avoid withPrefix on external route

--- site/src/components/MainNav.tsx
+++ site/src/components/MainNav.tsx
@@ -6,56 +6,71 @@ type MainNavProps = {
   currentPath?: string
 }
 
-const MainNav: React.SFC<MainNavProps> = ({ currentPath = "" }) => {
-  const currentPathStartsWith = (path: string) =>
-    currentPath.startsWith(withPrefix(path))
+type MainPropsState = {
+  externalUrl?: string
+}
 
-  let branch = ""
-  if (window.location.hostname.includes("dev")) {
-    // the dev branch is appended before the rest of the pathname
-    // given this component will only ever mount on storybook-static
-    // strip that from the path.
-    branch = window.location.pathname
-      .split("/")
-      .filter(curr => curr !== "" && curr !== "storybook-static")
-      .join("/")
+class MainNav extends React.Component<MainNavProps> {
+  public state = {
+    externalUrl: "",
   }
-  // storybook is hosted outside of Gatsby, so we cannot use `withPrefix`
-  const baseUrl = `${window.location.origin}/${branch}`
 
-  return (
-    <NavigationBar>
-      {{
-        primary: [
-          <NavLink
-            text="Home"
-            href={withPrefix("/")}
-            active={withPrefix(currentPath) == withPrefix("/")}
-          />,
-          <NavLink
-            text="Guidelines"
-            href={withPrefix("/guidelines/overview")}
-            active={currentPathStartsWith("/guidelines")}
-          />,
-          <NavLink
-            text="Language"
-            href={withPrefix("/language/overview")}
-            active={currentPathStartsWith("/language")}
-          />,
-          <NavLink
-            text="Components"
-            href={withPrefix("/components/overview")}
-            active={currentPathStartsWith("/components")}
-          />,
-          <NavLink
-            text="Storybook"
-            href={`${baseUrl}/storybook`}
-            active={currentPathStartsWith("/storybook")}
-          />,
-        ],
-      }}
-    </NavigationBar>
-  )
+  componentDidMount() {
+    let branch = ""
+    if (window.location.hostname.includes("dev")) {
+      // the dev branch is appended before the rest of the pathname
+      // given this component will only ever mount on storybook-static
+      // strip that from the path.
+      branch = window.location.pathname
+        .split("/")
+        .filter(curr => curr !== "" && curr !== "storybook-static")
+        .join("/")
+    }
+    // storybook is hosted outside of Gatsby, so we cannot use `withPrefix`
+    const baseUrl = `${window.location.origin}/${branch}`
+
+    this.state = { externalUrl: baseUrl }
+  }
+
+  render() {
+    const { currentPath = "" } = this.props
+    const currentPathStartsWith = (path: string) =>
+      currentPath.startsWith(withPrefix(path))
+
+    return (
+      <NavigationBar>
+        {{
+          primary: [
+            <NavLink
+              text="Home"
+              href={withPrefix("/")}
+              active={withPrefix(currentPath) == withPrefix("/")}
+            />,
+            <NavLink
+              text="Guidelines"
+              href={withPrefix("/guidelines/overview")}
+              active={currentPathStartsWith("/guidelines")}
+            />,
+            <NavLink
+              text="Language"
+              href={withPrefix("/language/overview")}
+              active={currentPathStartsWith("/language")}
+            />,
+            <NavLink
+              text="Components"
+              href={withPrefix("/components/overview")}
+              active={currentPathStartsWith("/components")}
+            />,
+            <NavLink
+              text="Storybook"
+              href={`${this.state.externalUrl}/storybook`}
+              active={currentPathStartsWith("/storybook")}
+            />,
+          ],
+        }}
+      </NavigationBar>
+    )
+  }
 }
 
 export default MainNav
