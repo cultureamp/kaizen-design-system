bc0d54482490d4fe9c229d1382031a5ee389aacb
Di <diana.macdonald@cultureamp.com>
2020-07-17 16:26:11 +1000

    feat: add loading spinner (#659)
    * feat: add Loading Spinner generated draft package
    
    * fix: add working Loading Spinner
    
    * docs: update Loading Spinner story
    
    * chore: ignore eslint warning for long path d
    
    * fix: adjust loading spinner sizes
    
    * docs: update links to loading spinner
    
    * docs: update Loading Spinner documentation
    
    - Add demo ID
    - Improve copy
    - Remove "not built" and similar
    - Add usage details
    - Add external links and remove broken link
    
    * fix: use sr-only loading spinner accessible label
    
    * fix: separate sm and md loading spinners
    
    * fix: use new animation tokens
    
    * fix: make Loading Spinner size prop optional

---
+++ draft-packages/stories/LoadingSpinner.stories.tsx
@@ -0,0 +1,45 @@
+import * as React from "react"
+
+import { LoadingSpinner } from "@kaizen/draft-loading-spinner"
+
+export default {
+  title: "LoadingSpinner (React)",
+}
+
+export const DefaultStory = () => (
+  <div
+    style={{
+      alignItems: "center",
+      display: "flex",
+      justifyContent: "center",
+    }}
+  >
+    <LoadingSpinner
+      accessibilityLabel="Loading comments"
+      size="md"
+    ></LoadingSpinner>
+  </div>
+)
+
+DefaultStory.story = {
+  name: "Default (Kaizen Site Demo)",
+}
+
+export const SizeStory = () => (
+  <div
+    style={{
+      alignItems: "center",
+      display: "flex",
+      justifyContent: "center",
+    }}
+  >
+    <LoadingSpinner
+      accessibilityLabel="Loading comments"
+      size="sm"
+    ></LoadingSpinner>
+  </div>
+)
+
+SizeStory.story = {
+  name: "Small",
+}
