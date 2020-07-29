6c391811410e1fc76b515e1f19be916d681c791c
Michael Bylstra <michael.bylstra@cultureamp.com>
2020-07-29 13:06:07 +1000

    feat: allow arbitrary Html in TextField description fields (Elm & React) (#671)
    * feat: add descriptionHtml prop to Elm TextField
    
    This allows arbitrary Html to be optionally used for the message. Currently
    it is restricted to just strings. The use case was putting a link in
    the description.
    
    * feat: loosen restriction of TextField message content (React)
    
    Previously it only allowed strings, now it allows arbitrary Html (React.ReactNode)
    
    * feat: change descriptionHtml from single element to list
    
    This is more like Elm's `div [] []`  convention, and is more convenient to use
    as you don't need to think up a wrapping element.
    
    * feat: add stories for Elm description and descriptionHtml TextField
    
    * feat: add "Default with HTML description" story for React TextField
    
    * feat: loosen type of description in TextAreaField
    
    to match TextField

--- draft-packages/form/KaizenDraft/Form/Primitives/FieldMessage/FieldMessage.elm
+++ draft-packages/form/KaizenDraft/Form/Primitives/FieldMessage/FieldMessage.elm
@@ -1,4 +1,4 @@
-module KaizenDraft.Form.Primitives.FieldMessage.FieldMessage exposing (FieldMessageStatus(..), automationId, default, id, message, reversed, status, view)
+module KaizenDraft.Form.Primitives.FieldMessage.FieldMessage exposing (FieldMessageStatus(..), automationId, default, id, message, messageHtml, reversed, status, view)
 
 import CssModules exposing (css)
 import Html exposing (..)
@@ -24,25 +24,27 @@ type FieldMessageStatus
 -- CONFIG
 
 
-type Config
-    = Config ConfigValue
+type Config msg
+    = Config (ConfigValue msg)
 
 
-type alias ConfigValue =
+type alias ConfigValue msg =
     { id : Maybe String
     , automationId : Maybe String
     , reversed : Bool
     , message : String
+    , messageHtml : Maybe (List (Html msg))
     , status : FieldMessageStatus
     }
 
 
-defaults : ConfigValue
+defaults : ConfigValue msg
 defaults =
     { id = Nothing
     , automationId = Nothing
     , reversed = False
     , message = ""
+    , messageHtml = Nothing
     , status = Default
     }
 
@@ -51,7 +53,7 @@ defaults =
 -- VARIANTS
 
 
-default : Config
+default : Config msg
 default =
     Config defaults
 
@@ -60,32 +62,37 @@ default =
 -- MODIFIERS
 
 
-id : String -> Config -> Config
+id : String -> Config msg -> Config msg
 id value (Config config) =
     Config { config | id = Just value }
 
 
-automationId : String -> Config -> Config
+automationId : String -> Config msg -> Config msg
 automationId value (Config config) =
     Config { config | automationId = Just value }
 
 
-reversed : Bool -> Config -> Config
+reversed : Bool -> Config msg -> Config msg
 reversed value (Config config) =
     Config { config | reversed = value }
 
 
-message : String -> Config -> Config
+message : String -> Config msg -> Config msg
 message value (Config config) =
     Config { config | message = value }
 
 
-status : FieldMessageStatus -> Config -> Config
+messageHtml : Maybe (List (Html msg)) -> Config msg -> Config msg
+messageHtml value (Config config) =
+    Config { config | messageHtml = value }
+
+
+status : FieldMessageStatus -> Config msg -> Config msg
 status value (Config config) =
     Config { config | status = value }
 
 
-view : Config -> Html msg
+view : Config msg -> Html msg
 view (Config config) =
     let
         idAttr =
@@ -118,4 +125,14 @@ view (Config config) =
                     ]
                ]
         )
-        [ text config.message ]
+        (case ( config.message, config.messageHtml ) of
+            ( _, Just html ) ->
+                -- ideally we'd only allow either a string or html, not both, but
+                -- we wanted to avoid a breaking change, so we need to deal with
+                -- nonsensical state where both are provided, so the html
+                -- aribitrarily wins out if both are supplied
+                html
+
+            ( messageString, Nothing ) ->
+                [ text messageString ]
+        )
