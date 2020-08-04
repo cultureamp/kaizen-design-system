61d9551d3da485894e0c8832b7848a6bcb439572
Dennis O'Keeffe <hello@dennisokeeffe.com>
2020-08-04 15:59:55 +1000

    fix: Extend TitleBlockZen API so NavigationTabs can take an automation ID

--- draft-packages/title-block-zen/KaizenDraft/TitleBlockZen/NavigationTabs.tsx
+++ draft-packages/title-block-zen/KaizenDraft/TitleBlockZen/NavigationTabs.tsx
@@ -4,17 +4,14 @@ import { NON_REVERSED_VARIANTS, Variant } from "./TitleBlockZen"
 
 const styles = require("./NavigationTabs.scss")
 
-export type NavigationTabsProps = {
-  id?: string
-  automationId?: string
-}
-
 export type NavigationTabProps = {
   text: string
   href: string
   active?: boolean
   handleClick?: (event: React.MouseEvent) => void
   variant?: Variant
+  id?: string
+  automationId?: string
 }
 
 const isLight = (variant: Variant | undefined): boolean =>
@@ -27,6 +24,8 @@ const NavigationTab = (props: NavigationTabProps) => (
     })}
     href={props.href}
     onClick={props.handleClick}
+    id={props.id}
+    data-automation-id={props.automationId}
   >
     <div
       className={classnames(styles.linkLabel, {
