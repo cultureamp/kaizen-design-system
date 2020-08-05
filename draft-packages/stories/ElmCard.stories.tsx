801810ebcd969dc38947c184ec5b708fa80782b7
Michael Bylstra <mbylstra@gmail.com>
2020-08-05 11:44:44 +1000

    fix: run yarn lint:fix

--- draft-packages/stories/ElmCard.stories.tsx
+++ draft-packages/stories/ElmCard.stories.tsx
@@ -1,5 +1,3 @@
 import { loadElmStories } from "@cultureamp/elm-storybook"
 
-loadElmStories("Card (Elm)", module, require("./Card.stories.elm"), [
-  "Default",
-])
+loadElmStories("Card (Elm)", module, require("./Card.stories.elm"), ["Default"])
