5a34b9269f2d01be8bfaecc16773320a66b23980
Samuel Norris <samuel.norris@cultureamp.com>
2020-08-18 10:49:29 +1000

    feat: avatar stories added

--- draft-packages/stories/Avatar.stories.tsx
+++ draft-packages/stories/Avatar.stories.tsx
@@ -7,9 +7,49 @@ export default {
 }
 
 export const DefaultStory = () => (
-  <Avatar>
-    world!
-  </Avatar>
+  <>
+    <Avatar size="large" fullName="John Smith" />
+    <br />
+    <Avatar size="medium" fullName="John Smith" />
+  </>
+)
+export const WithAvatarPath = () => (
+  <>
+    <Avatar
+      size="large"
+      avatarPath="https://www.fillmurray.com/200/300"
+      fullName="John Smith"
+    />
+    <br />
+    <Avatar
+      size="medium"
+      avatarPath="https://www.fillmurray.com/200/300"
+      fullName="John Smith"
+    />
+  </>
+)
+export const WithBrokenAvatarPath = () => (
+  <>
+    <Avatar
+      size="large"
+      avatarPath="https://broke.fillmurray.com/200/300"
+      fullName="Jane Doe"
+    />
+    <br />
+    <Avatar
+      size="medium"
+      avatarPath="https://broke.fillmurray.com/200/300"
+      fullName="Jane Doe"
+    />
+  </>
+)
+
+export const WithLongInitialFallback = () => (
+  <>
+    <Avatar size="large" fullName="Very Long Name Which Shows Initials" />
+    <br />
+    <Avatar size="medium" fullName="Very Long Name Which Shows Initials" />
+  </>
 )
 
 DefaultStory.story = {
