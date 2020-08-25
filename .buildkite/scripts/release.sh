aa4edf5a258cb85a667ea62e89b0be5ba6fdc53f
Louis Quinnell <_@ziorobo.co>
2020-08-25 13:53:19 +1000

    Add comments

--- .buildkite/scripts/release.sh
+++ .buildkite/scripts/release.sh
@@ -56,6 +56,8 @@ release() {
 release_canary() {
   yarn install --frozen-lockfile
 
+  # Replace illegal characters according to semver prerelease versioning
+  # https://semver.org/#spec-item-9
   CANARY_LABEL=$(echo "$BUILDKITE_BRANCH" | sed 's/[^0-9A-Za-z-]/-/g')
 
   yarn lerna publish --canary --preid "$CANARY_LABEL" --yes
