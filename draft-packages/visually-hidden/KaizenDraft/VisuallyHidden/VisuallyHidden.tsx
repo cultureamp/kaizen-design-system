baa2072836c7e3cb40f0fb490b06ddf04fb052a5
Jason O'Neil <jason.oneil@gmail.com>
2020-09-01 13:57:27 +0800

    Create the React implementation

--- draft-packages/visually-hidden/KaizenDraft/VisuallyHidden/VisuallyHidden.tsx
+++ draft-packages/visually-hidden/KaizenDraft/VisuallyHidden/VisuallyHidden.tsx
@@ -3,23 +3,30 @@ const styles = require("./styles.module.scss")
 
 export interface VisuallyHiddenProps {
   /**
-   * Remember to annotate your props! The typehints make developers happy
-   * @default ""
+   * Any child nodes will be visually hidden.
    */
   children?: React.ReactNode
 
   /**
    * Uniquely identify this component for testing purposes
-   * @default ""
    */
   automationId?: string
 }
 
+/**
+ * A component to make content visually hidden, but available to screen readers.
+ *
+ * Elements that are natively focusable will become visible when focused.
+ * This is useful for "Skip to content" links etc.
+ *
+ * This will create a span with a CSS class that visually hides content using the clip pattern.
+ * See https://www.a11yproject.com/posts/2013-01-11-how-to-hide-content/
+ */
 export const VisuallyHidden = ({
   children,
   automationId,
 }: VisuallyHiddenProps) => (
-  <h1 data-automation-id={automationId} className={styles.wrapper}>
-    Hello, {children}
-  </h1>
+  <span data-automation-id={automationId} className={styles.visuallyHidden}>
+    {children}
+  </span>
 )
