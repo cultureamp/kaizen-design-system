5af89862632742d56939e099f064f4704365ff90
Samuel Norris <samuel.norris@cultureamp.com>
2020-09-02 10:29:46 +1000

    feat: allow animation on badge, and add variant 'dark'

--- draft-packages/badge/KaizenDraft/Badge/Badge.tsx
+++ draft-packages/badge/KaizenDraft/Badge/Badge.tsx
@@ -1,28 +1,65 @@
+import React, { useEffect, useState } from "react"
 import classNames from "classnames"
-import * as React from "react"
+import { CSSTransition } from "react-transition-group"
 
 const styles = require("./styles.module.scss")
 
-type Variant = "default" | "active"
+type Variant = "default" | "active" | "dark"
 
 export interface BadgeProps {
   readonly children: string
   readonly variant?: Variant
   readonly reversed?: boolean
+  /**
+   * Enables an animation when the badge text change to draw users' attention.
+   * @default "false"
+   */
+  readonly animateChange?: boolean
 }
 
 export const Badge = (props: BadgeProps) => {
-  const { children, variant = "default", reversed } = props
+  const {
+    children,
+    variant = "default",
+    reversed,
+    animateChange = false,
+  } = props
+
+  const [isAnimating, setIsAnimating] = useState(false)
+
+  useEffect(() => {
+    setIsAnimating(true)
+  }, [children])
 
   return (
     <span
       className={classNames(styles.badge, {
         [styles.default]: variant === "default",
         [styles.active]: variant === "active",
-        [styles.reversed]: reversed === true,
+        [styles.dark]: variant === "dark",
+        [styles.reversed]: reversed,
       })}
     >
-      {children}
+      <>
+        {animateChange && (
+          <CSSTransition
+            in={isAnimating}
+            timeout={{
+              enter: 700,
+              exit: 0,
+            }}
+            classNames={{
+              enter: styles.enter,
+              enterActive: styles.enterActive,
+            }}
+            onEntered={() => setIsAnimating(false)}
+          >
+            <span className={styles.animate} />
+          </CSSTransition>
+        )}
+
+        {children}
+      </>
     </span>
   )
 }
--- draft-packages/stories/Badge.stories.tsx
+++ draft-packages/stories/Badge.stories.tsx
@@ -1,7 +1,8 @@
 import * as colorTokens from "@kaizen/design-tokens/tokens/color.json"
-import * as React from "react"
+import React, { useState } from "react"
 
 import { Badge } from "@kaizen/draft-badge"
+import { Button } from "@kaizen/component-library"
 
 export default {
   title: "Badge (React)",
@@ -61,4 +62,29 @@ export const LongerText = () => <Badge variant="default">2x</Badge>
 
 LongerText.story = {
   name: "Longer text",
+}
+
+export const Animated = () => {
+  const [badgeCount, setBadgeCount] = useState(1)
+
+  return (
+    <div style={{ padding: "20px" }}>
+      <Badge animateChange={true} variant="default">
+        {String(badgeCount)}
+      </Badge>
+      <span style={{ display: "inline-block", width: "100px" }} />
+      <Badge animateChange={true} variant="active">
+        {String(badgeCount)}
+      </Badge>
+      <br />
+      <Button
+        label="Add Notification"
+        onClick={() => setBadgeCount(bc => bc + 1)}
+      />
+    </div>
+  )
+}
+
+Animated.story = {
+  name: "Animate change",
 }
