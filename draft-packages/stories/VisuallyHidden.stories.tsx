d1ca9d40ee811ec5122eee3093264a1e7563257a
ActuallyACat <allanna.beaton@gmail.com>
2020-10-07 10:59:51 +1100

    docs: Added storybook stories, documentation and a test for VisuallyHidden

--- draft-packages/stories/VisuallyHidden.stories.tsx
+++ draft-packages/stories/VisuallyHidden.stories.tsx
@@ -1,6 +1,8 @@
 import * as React from "react"
+import { Label, Input } from "@kaizen/draft-form"
 
 import { VisuallyHidden } from "@kaizen/draft-visually-hidden"
+import { Paragraph } from "@kaizen/component-library"
 
 export default {
   title: "VisuallyHidden (React)",
@@ -9,13 +11,41 @@ export default {
     info: {
       text: `
         import { VisuallyHidden } from "@kaizen/draft-VisuallyHidden";
+
+        Use VisuallyHidden when you want to visually hide an element, but still 
+        make it available to screen reader users.
+
+        If the hidden element is focusable (e.g. a Link or Button), then when this element receives focus 
+        it will be visible. For more information, read [Hide Content](https://www.a11yproject.com/posts/2013-01-11-how-to-hide-content/).
       `,
     },
   },
 }
 
-export const DefaultStory = () => <VisuallyHidden>world!</VisuallyHidden>
+export const DefaultStory = () => (
+  <span>
+    <p>Below should read "Hello, World!" for screen reader users</p>
+    Hello, <VisuallyHidden>World!</VisuallyHidden>.
+  </span>
+)
 
 DefaultStory.story = {
   name: "Default (Kaizen Site Demo)",
+}
+
+export const HiddenLabel = () => (
+  <div>
+    <Paragraph variant="body">
+      The label for this text field is being visually hidden by the
+      VisuallyHidden component.
+    </Paragraph>
+    <VisuallyHidden>
+      <Label htmlFor="test-id">Enter your name</Label>
+    </VisuallyHidden>
+    <Input id="test-id" placeholder="Enter your name" />
+  </div>
+)
+
+HiddenLabel.story = {
+  name: "Hidden Label Example",
 }
