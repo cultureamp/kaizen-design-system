cb819a614410167d6cffbd0535fbdbcd3389f72d
Doug MacKenzie <doug.mackenzie@cultureamp.com>
2020-10-21 10:41:49 +1100

    feat: Allow removal of dismiss button via empty dismissLabel string

--- draft-packages/modal/KaizenDraft/Modal/Presets/InputEditModal.tsx
+++ draft-packages/modal/KaizenDraft/Modal/Presets/InputEditModal.tsx
@@ -41,38 +41,46 @@ const InputEditModal = ({
   automationId,
   children,
   submitDisabled = false,
-}: Props) => (
-  <GenericModal
-    isOpen={isOpen}
-    onEscapeKeyup={onDismiss}
-    onOutsideModalClick={onDismiss}
-    automationId={automationId}
-  >
-    <div className={styles.modal} dir={localeDirection}>
-      <ModalHeader unpadded onDismiss={onDismiss}>
-        <div className={styles.header}>
-          <ModalAccessibleLabel>
-            <Text tag="h1" style="zen-heading-3" inline>
-              {title}
-            </Text>
-          </ModalAccessibleLabel>
-        </div>
-      </ModalHeader>
-      <ModalBody unpadded>
-        <div className={styles.body} dir={localeDirection}>
-          {children}
-        </div>
-      </ModalBody>
-      <ModalFooter
-        actions={[
-          { label: submitLabel, action: onSubmit, disabled: submitDisabled },
-          { label: dismissLabel, action: onDismiss },
-        ]}
-        appearance={type === "negative" ? "destructive" : "primary"}
-        automationId={automationId}
-      />
-    </div>
-  </GenericModal>
-)
+}: Props) => {
+  const submitAction = {
+    label: submitLabel,
+    action: onSubmit,
+    disabled: submitDisabled,
+  }
+  const footerActions = dismissLabel
+    ? [submitAction, { label: dismissLabel, action: onDismiss }]
+    : [submitAction]
+
+  return (
+    <GenericModal
+      isOpen={isOpen}
+      onEscapeKeyup={onDismiss}
+      onOutsideModalClick={onDismiss}
+      automationId={automationId}
+    >
+      <div className={styles.modal} dir={localeDirection}>
+        <ModalHeader unpadded onDismiss={onDismiss}>
+          <div className={styles.header}>
+            <ModalAccessibleLabel>
+              <Text tag="h1" style="zen-heading-3" inline>
+                {title}
+              </Text>
+            </ModalAccessibleLabel>
+          </div>
+        </ModalHeader>
+        <ModalBody unpadded>
+          <div className={styles.body} dir={localeDirection}>
+            {children}
+          </div>
+        </ModalBody>
+        <ModalFooter
+          actions={footerActions}
+          appearance={type === "negative" ? "destructive" : "primary"}
+          automationId={automationId}
+        />
+      </div>
+    </GenericModal>
+  )
+}
 
 export default InputEditModal
