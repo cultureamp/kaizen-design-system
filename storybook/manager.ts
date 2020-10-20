6b62bbbc591579b92169bea92a173f20b395fc6e
ActuallyACat <allanna.beaton@gmail.com>
2020-10-20 18:28:09 +1100

    removed comments

--- storybook/manager.ts
+++ storybook/manager.ts
@@ -1,9 +1,5 @@
 // This file is used by storybook to register installed addons.
 // See: https://storybook.js.org/docs/addons/using-addons/
-
-// The following do not use the addons panel:
-// import "@storybook/addon-viewport/register"
-
 import { addons } from "@storybook/addons"
 import KaizenTheme from "./theme"
 
