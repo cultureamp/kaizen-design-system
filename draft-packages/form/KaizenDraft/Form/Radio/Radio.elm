4f37d6d11bff691d682ac014210a4ce6cb42e730
ActuallyACat <allanna.beaton@gmail.com>
2020-12-01 09:48:57 +1100

    feat: Moved CheckboxGroup, Radio, RadioGroup to form package

---
+++ draft-packages/form/KaizenDraft/Form/Radio/Radio.elm
@@ -0,0 +1,200 @@
+module KaizenDraft.Radio.Radio exposing
+    ( LabelProp(..)
+    , SelectedStatus(..)
+    , automationId
+    , default
+    , disabled
+    , inline
+    , labelText
+    , name
+    , onChange
+    , radioId
+    , selectedStatus
+    , value
+    , view
+    )
+
+import CssModules exposing (css)
+import Html exposing (Html, div)
+import Html.Attributes as Attributes
+import KaizenDraft.Form.Primitives.Label.Label as Label
+import KaizenDraft.Radio.Primitives.RadioInput as RadioInput
+
+
+labelSuffix : String
+labelSuffix =
+    "-field-label"
+
+
+radioSuffix : String
+radioSuffix =
+    "-radio-input"
+
+
+type LabelProp msg
+    = LabelString String
+    | LabelHtml (List (Html msg))
+
+
+type Config msg
+    = Config (Configuration msg)
+
+
+type SelectedStatus
+    = Selected
+    | Unselected
+
+
+type RadioId
+    = RadioId String
+
+
+type alias Configuration msg =
+    { selectedStatus : SelectedStatus
+    , onChange : Maybe (Bool -> msg)
+    , inline : Bool
+    , name : String
+    , value : String
+    , labelText : LabelProp msg
+    , disabled : Bool
+    , automationId : String
+    }
+
+
+defaults : Configuration msg
+defaults =
+    { selectedStatus = Unselected
+    , onChange = Nothing
+    , inline = False
+    , name = ""
+    , value = ""
+    , labelText = LabelString ""
+    , disabled = False
+    , automationId = ""
+    }
+
+
+view : Config msg -> RadioId -> Html msg
+view (Config config) (RadioId i) =
+    let
+        resolveLabelText =
+            case config.labelText of
+                LabelString s ->
+                    Label.LabelString s
+
+                LabelHtml h ->
+                    Label.LabelHtml h
+
+        resolveSelectedStatus =
+            case config.selectedStatus of
+                Selected ->
+                    RadioInput.Selected
+
+                _ ->
+                    RadioInput.Unselected
+
+        withOnChange cnfg =
+            case config.onChange of
+                Just onChangeMsg ->
+                    RadioInput.onChange onChangeMsg cnfg
+
+                _ ->
+                    cnfg
+    in
+    div
+        [ styles.classList
+            [ ( .container, True )
+            , ( .selected, config.selectedStatus == Selected )
+            , ( .disabled, config.disabled )
+            , ( .inline, config.inline )
+            ]
+        , Attributes.attribute "data-automation-id" config.automationId
+        ]
+        [ Label.view
+            (Label.default
+                |> Label.id (i ++ labelSuffix)
+                |> Label.htmlFor i
+                |> Label.automationId (i ++ labelSuffix)
+                |> Label.labelText resolveLabelText
+                |> Label.labelType Label.Radio
+                |> Label.children
+                    [ RadioInput.view
+                        (RadioInput.default
+                            |> RadioInput.automationId (i ++ radioSuffix)
+                            |> RadioInput.disabled config.disabled
+                            |> RadioInput.selectedStatus resolveSelectedStatus
+                            |> RadioInput.value config.value
+                            |> RadioInput.name config.name
+                            |> withOnChange
+                            |> RadioInput.id i
+                        )
+                    ]
+            )
+        ]
+
+
+radioId : String -> RadioId
+radioId i =
+    RadioId i
+
+
+
+-- CONFIG
+
+
+default : Config msg
+default =
+    Config defaults
+
+
+
+-- MODIFIERS
+
+
+onChange : (Bool -> msg) -> Config msg -> Config msg
+onChange msg (Config config) =
+    Config { config | onChange = Just msg }
+
+
+selectedStatus : SelectedStatus -> Config msg -> Config msg
+selectedStatus v (Config config) =
+    Config { config | selectedStatus = v }
+
+
+inline : Bool -> Config msg -> Config msg
+inline i (Config config) =
+    Config { config | inline = i }
+
+
+name : String -> Config msg -> Config msg
+name n (Config config) =
+    Config { config | name = n }
+
+
+value : String -> Config msg -> Config msg
+value v (Config config) =
+    Config { config | name = v }
+
+
+labelText : LabelProp msg -> Config msg -> Config msg
+labelText l (Config config) =
+    Config { config | labelText = l }
+
+
+disabled : Bool -> Config msg -> Config msg
+disabled d (Config config) =
+    Config { config | disabled = d }
+
+
+automationId : String -> Config msg -> Config msg
+automationId a (Config config) =
+    Config { config | automationId = a }
+
+
+styles =
+    css "@kaizen/draft-radio/KaizenDraft/Radio/styles.scss"
+        { container = "container"
+        , selected = "selected"
+        , disabled = "disabled"
+        , inline = "inline"
+        }
---
+++ draft-packages/form/KaizenDraft/Form/RadioGroup/RadioGroup.spec.tsx
@@ -0,0 +1,79 @@
+import { Radio } from "@kaizen/draft-radio"
+import { cleanup, render } from "@testing-library/react"
+import { fireEvent } from "@testing-library/dom"
+import * as React from "react"
+import RadioGroup, { RadioGroupProps } from "./RadioGroup"
+
+afterEach(cleanup)
+
+const defaultRadioGroupProps: RadioGroupProps = {
+  automationId: "RadioAutomationId",
+  labelText: "Label",
+}
+const renderRadioGroup = (props?: RadioGroupProps) => {
+  const mergedRadioGroupProps = { ...defaultRadioGroupProps, ...props }
+
+  return render(<RadioGroup {...mergedRadioGroupProps} />)
+}
+
+describe("<RadioGroup /> ", () => {
+  describe("snapshots", () => {
+    test("renders RadioGroup in a column and with a label", () => {
+      const { container } = render(
+        <RadioGroup labelText="Label" children={null} />
+      )
+
+      expect(container.firstChild).toMatchSnapshot()
+    })
+
+    test("renders RadioGroup in a row and with a label", () => {
+      const { container } = render(
+        <RadioGroup labelText="Label" children={null} />
+      )
+
+      expect(container.firstChild).toMatchSnapshot()
+    })
+    test("renders RadioGroup with radios", () => {
+      const { container } = render(
+        <RadioGroup labelText="Label">
+          <Radio
+            name="radio"
+            id="radio-1"
+            labelText="Label 1"
+            selectedStatus={false}
+            disabled={false}
+            value="radio-1"
+          />
+          <Radio
+            name="radio"
+            id="radio-2"
+            labelText="Label 2"
+            selectedStatus={false}
+            disabled={false}
+            value="radio-2"
+          />
+        </RadioGroup>
+      )
+
+      expect(container.firstChild).toMatchSnapshot()
+    })
+
+    test("renders RadioGroup in a column and with a label", () => {
+      const { container } = render(
+        <RadioGroup labelText="Label" children={null} />
+      )
+
+      expect(container.firstChild).toMatchSnapshot()
+    })
+  })
+  describe("presentational", () => {
+    it("should render a title", () => {
+      const title = "Label"
+
+      const { queryByText } = render(
+        <RadioGroup labelText={title} children={null} />
+      )
+      expect(queryByText(title)).toBeTruthy()
+    })
+  })
+})
