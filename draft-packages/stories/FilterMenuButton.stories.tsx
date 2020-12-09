2a7e156774af0a49a2dd79a4bfbbf64a6263c423
Martin David Valentino <martin.valentino@cultureamp.com>
2020-12-09 14:47:23 +1100

    feat: add more use case for FilterMenuButton
    Add story for the case of simple and advanced filter

--- draft-packages/stories/FilterMenuButton.stories.tsx
+++ draft-packages/stories/FilterMenuButton.stories.tsx
@@ -216,3 +216,293 @@ export const DefaultEmpty = () => {
   )
 }
 DefaultEmpty.storyName = "Default (empty)"
+
+export const DefaultWithChildrenSimpleFilter = () => {
+  const [furryCheckboxState, setFurryCheckboxState] = useState<CheckboxState>(
+    "off"
+  )
+  const [aquaticCheckboxState, setAquaticCheckboxState] = useState<
+    CheckboxState
+  >("off")
+  const [venomousCheckboxState, setVenomousCheckboxState] = useState<
+    CheckboxState
+  >("off")
+  const [egglayingCheckboxState, setEgglayingCheckboxState] = useState<
+    CheckboxState
+  >("off")
+  const [isDropdownVisible, setIsDropdownVisible] = useState(false)
+
+  const [appliedFilters, setAppliedFilters] = useState<AppliedFiltersState>({
+    furry: "off",
+    aquatic: "off",
+    venomous: "off",
+    egglaying: "off",
+  })
+
+  const checkedTraits: string = (appliedFilters.furry === "on" ? ["Furry"] : [])
+    .concat(appliedFilters.aquatic === "on" ? ["Aquatic"] : [])
+    .concat(appliedFilters.venomous === "on" ? ["Venomous"] : [])
+    .concat(appliedFilters.egglaying === "on" ? ["Egg-laying"] : [])
+    .join("・")
+
+  const toggleDropdown = () => {
+    clearChanges()
+    setIsDropdownVisible(!isDropdownVisible)
+  }
+
+  const hideDropdown = () => {
+    setIsDropdownVisible(false)
+  }
+
+  const applyChanges = () => {
+    setAppliedFilters({
+      furry: furryCheckboxState,
+      aquatic: aquaticCheckboxState,
+      venomous: venomousCheckboxState,
+      egglaying: egglayingCheckboxState,
+    })
+    hideDropdown()
+  }
+
+  const clearChanges = () => {
+    setFurryCheckboxState(appliedFilters.furry)
+    setAquaticCheckboxState(appliedFilters.aquatic)
+    setVenomousCheckboxState(appliedFilters.venomous)
+    setEgglayingCheckboxState(appliedFilters.egglaying)
+  }
+
+  const haveFiltersNotChanged = () =>
+    appliedFilters.furry == furryCheckboxState &&
+    appliedFilters.aquatic == aquaticCheckboxState &&
+    appliedFilters.venomous == venomousCheckboxState &&
+    appliedFilters.egglaying == egglayingCheckboxState
+
+  return (
+    <StoryWrapper>
+      <FilterMenuButton
+        id="filter-menu-button"
+        labelText="Filter"
+        metadata={checkedTraits}
+        isDropdownVisible={isDropdownVisible}
+        toggleDropdown={toggleDropdown}
+        hideDropdown={hideDropdown}
+      >
+        <>
+          <div className={styles.content}>
+            <CheckboxGroup labelText="Traits">
+              <CheckboxField
+                onCheck={() => {
+                  setFurryCheckboxState(
+                    furryCheckboxState === "on" ? "off" : "on"
+                  )
+                }}
+                id="checkbox-1"
+                checkedStatus={furryCheckboxState as any}
+                labelText="Furry"
+              />
+              <CheckboxField
+                onCheck={() => {
+                  setAquaticCheckboxState(
+                    aquaticCheckboxState === "on" ? "off" : "on"
+                  )
+                }}
+                id="checkbox-2"
+                checkedStatus={aquaticCheckboxState as any}
+                labelText="Aquatic"
+              />
+              <CheckboxField
+                onCheck={() => {
+                  setVenomousCheckboxState(
+                    venomousCheckboxState === "on" ? "off" : "on"
+                  )
+                }}
+                id="checkbox-3"
+                checkedStatus={venomousCheckboxState as any}
+                labelText="Venomous"
+              />
+              <CheckboxField
+                onCheck={() => {
+                  setEgglayingCheckboxState(
+                    egglayingCheckboxState === "on" ? "off" : "on"
+                  )
+                }}
+                id="checkbox-4"
+                checkedStatus={egglayingCheckboxState as any}
+                labelText="Egg-laying"
+              />
+            </CheckboxGroup>
+          </div>
+          <div className={styles.buttons}>
+            <Button
+              secondary={true}
+              label="Cancel"
+              onClick={() => {
+                toggleDropdown()
+              }}
+            />
+            <Button
+              primary={true}
+              label="Apply"
+              disabled={haveFiltersNotChanged()}
+              onClick={() => {
+                applyChanges()
+              }}
+            />
+          </div>
+        </>
+      </FilterMenuButton>
+    </StoryWrapper>
+  )
+}
+DefaultWithChildrenSimpleFilter.storyName =
+  "Default with children (Simple filter)"
+
+export const DefaultWithChildrenAdvancedFilter = () => {
+  const [furryCheckboxState, setFurryCheckboxState] = useState<CheckboxState>(
+    "off"
+  )
+  const [aquaticCheckboxState, setAquaticCheckboxState] = useState<
+    CheckboxState
+  >("off")
+  const [venomousCheckboxState, setVenomousCheckboxState] = useState<
+    CheckboxState
+  >("off")
+  const [egglayingCheckboxState, setEgglayingCheckboxState] = useState<
+    CheckboxState
+  >("off")
+  const [isDropdownVisible, setIsDropdownVisible] = useState(false)
+
+  const [appliedFilters, setAppliedFilters] = useState<AppliedFiltersState>({
+    furry: "off",
+    aquatic: "off",
+    venomous: "off",
+    egglaying: "off",
+  })
+
+  const checkedTraits: string = (appliedFilters.furry === "on" ? ["Furry"] : [])
+    .concat(appliedFilters.aquatic === "on" ? ["Aquatic"] : [])
+    .concat(appliedFilters.venomous === "on" ? ["Venomous"] : [])
+    .concat(appliedFilters.egglaying === "on" ? ["Egg-laying"] : [])
+    .join("・")
+
+  const toggleDropdown = () => {
+    clearChanges()
+    setIsDropdownVisible(!isDropdownVisible)
+  }
+
+  const hideDropdown = () => {
+    setIsDropdownVisible(false)
+  }
+
+  const applyChanges = () => {
+    setAppliedFilters({
+      furry: furryCheckboxState,
+      aquatic: aquaticCheckboxState,
+      venomous: venomousCheckboxState,
+      egglaying: egglayingCheckboxState,
+    })
+    hideDropdown()
+  }
+
+  const clearChanges = () => {
+    setFurryCheckboxState(appliedFilters.furry)
+    setAquaticCheckboxState(appliedFilters.aquatic)
+    setVenomousCheckboxState(appliedFilters.venomous)
+    setEgglayingCheckboxState(appliedFilters.egglaying)
+  }
+
+  const haveFiltersNotChanged = () =>
+    appliedFilters.furry == furryCheckboxState &&
+    appliedFilters.aquatic == aquaticCheckboxState &&
+    appliedFilters.venomous == venomousCheckboxState &&
+    appliedFilters.egglaying == egglayingCheckboxState
+
+  const clearFilter = () => {
+    setAppliedFilters({
+      furry: "off",
+      aquatic: "off",
+      venomous: "off",
+      egglaying: "off",
+    })
+  }
+
+  return (
+    <StoryWrapper>
+      <FilterMenuButton
+        id="filter-menu-button"
+        labelText="Filter"
+        metadata={checkedTraits}
+        isDropdownVisible={isDropdownVisible}
+        toggleDropdown={toggleDropdown}
+        hideDropdown={hideDropdown}
+        onFilterClear={clearFilter}
+      >
+        <>
+          <div className={styles.content}>
+            <CheckboxGroup labelText="Traits">
+              <CheckboxField
+                onCheck={() => {
+                  setFurryCheckboxState(
+                    furryCheckboxState === "on" ? "off" : "on"
+                  )
+                }}
+                id="checkbox-1"
+                checkedStatus={furryCheckboxState as any}
+                labelText="Furry"
+              />
+              <CheckboxField
+                onCheck={() => {
+                  setAquaticCheckboxState(
+                    aquaticCheckboxState === "on" ? "off" : "on"
+                  )
+                }}
+                id="checkbox-2"
+                checkedStatus={aquaticCheckboxState as any}
+                labelText="Aquatic"
+              />
+              <CheckboxField
+                onCheck={() => {
+                  setVenomousCheckboxState(
+                    venomousCheckboxState === "on" ? "off" : "on"
+                  )
+                }}
+                id="checkbox-3"
+                checkedStatus={venomousCheckboxState as any}
+                labelText="Venomous"
+              />
+              <CheckboxField
+                onCheck={() => {
+                  setEgglayingCheckboxState(
+                    egglayingCheckboxState === "on" ? "off" : "on"
+                  )
+                }}
+                id="checkbox-4"
+                checkedStatus={egglayingCheckboxState as any}
+                labelText="Egg-laying"
+              />
+            </CheckboxGroup>
+          </div>
+          <div className={styles.buttons}>
+            <Button
+              secondary={true}
+              label="Cancel"
+              onClick={() => {
+                toggleDropdown()
+              }}
+            />
+            <Button
+              primary={true}
+              label="Apply"
+              disabled={haveFiltersNotChanged()}
+              onClick={() => {
+                applyChanges()
+              }}
+            />
+          </div>
+        </>
+      </FilterMenuButton>
+    </StoryWrapper>
+  )
+}
+DefaultWithChildrenAdvancedFilter.storyName =
+  "Default with children (Advanced filter)"
