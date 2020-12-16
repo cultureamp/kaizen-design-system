cb1070643affa14a4d544cda33d96ceaa78f8217
ActuallyACat <allanna.beaton@gmail.com>
2020-12-16 13:23:44 +1100

    fixed test imports

--- draft-packages/form/KaizenDraft/Form/Primitives/Radio/Radio.spec.tsx
+++ draft-packages/form/KaizenDraft/Form/Primitives/Radio/Radio.spec.tsx
@@ -1,7 +1,7 @@
 import { cleanup, render } from "@testing-library/react"
 import { fireEvent } from "@testing-library/dom"
 import * as React from "react"
-import RadioInput, { RadioInputProps } from "./RadioInput"
+import RadioInput, { RadioInputProps } from "./Radio"
 
 afterEach(cleanup)
 
