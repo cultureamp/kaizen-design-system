d293cfeebce343b4a71209d76243c47ff506eb65
Max Wheeler <max@makenosound.com>
2020-12-18 17:25:12 +1100

    Rough out stories using real Skirt component

--- draft-packages/stories/Skirt.stories.tsx
+++ draft-packages/stories/Skirt.stories.tsx
@@ -1,23 +1,223 @@
-// import * as React from "react"
+import * as React from "react"
+import {
+  NavigationTab,
+  TitleBlockZen,
+} from "../title-block-zen/KaizenDraft/TitleBlockZen"
+import { Skirt, SkirtCard } from "@kaizen/draft-skirt"
+import { Box, Paragraph } from "@kaizen/component-library"
 
-// import { Skirt } from "@kaizen/draft-skirt"
+export default {
+  title: "Skirt (React)",
+  component: Skirt,
+  parameters: {
+    info: {
+      text: `
+        import { Skirt } from "@kaizen/draft-Skirt";
+      `,
+    },
+  },
+}
 
-// export default {
-//   title: "Skirt (React)",
-//   component: Skirt,
-//   parameters: {
-//     info: {
-//       text: `
-//         import { Skirt } from "@kaizen/draft-Skirt";
-//       `,
-//     },
-//   },
-// }
+export const DefaultStory = () => (
+  <>
+    <TitleBlockZen
+      title="Skirt"
+      breadcrumb={{
+        path: "#",
+        text: "Back to home",
+        handleClick: event => {
+          alert("breadcrumb clicked!")
+        },
+      }}
+      navigationTabs={[<NavigationTab text="Label" href="#" active />]}
+    />
+    <Skirt>
+      <SkirtCard>
+        <Box p={1}>
+          <Box mb={1}>
+            <Paragraph variant="body">
+              The Skirt component extends the background color from the Title
+              Block into the content area.
+            </Paragraph>
+          </Box>
+          <Box mb={1}>
+            <Paragraph variant="body">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+              semper odio vitae sem gravida rutrum. Praesent vel sapien eget
+              eros dictum luctus scelerisque eu nibh. Etiam ullamcorper lobortis
+              gravida. Suspendisse massa tortor, ultricies et ipsum at, iaculis
+              bibendum est. Mauris vestibulum interdum ultricies. Proin sed elit
+              lacinia, malesuada leo in, auctor enim. Suspendisse venenatis,
+              tortor vel eleifend cursus, metus sem luctus nunc, at maximus
+              magna metus at mi. Curabitur porttitor dignissim ante, sit amet
+              tincidunt nibh elementum convallis. Morbi accumsan rutrum elit
+              eget sagittis. Pellentesque id quam at enim dictum placerat sit
+              amet in purus. Pellentesque habitant morbi tristique senectus et
+              netus et malesuada fames ac turpis egestas. Suspendisse quam elit,
+              facilisis sit amet libero vitae, congue rutrum nisi. Ut non
+              aliquet ex.
+            </Paragraph>
+          </Box>
+          <Paragraph variant="body">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+            semper odio vitae sem gravida rutrum. Praesent vel sapien eget eros
+            dictum luctus scelerisque eu nibh. Etiam ullamcorper lobortis
+            gravida. Suspendisse massa tortor, ultricies et ipsum at, iaculis
+            bibendum est. Mauris vestibulum interdum ultricies. Proin sed elit
+            lacinia, malesuada leo in, auctor enim. Suspendisse venenatis,
+            tortor vel eleifend cursus, metus sem luctus nunc, at maximus magna
+            metus at mi. Curabitur porttitor dignissim ante, sit amet tincidunt
+            nibh elementum convallis. Morbi accumsan rutrum elit eget sagittis.
+            Pellentesque id quam at enim dictum placerat sit amet in purus.
+            Pellentesque habitant morbi tristique senectus et netus et malesuada
+            fames ac turpis egestas. Suspendisse quam elit, facilisis sit amet
+            libero vitae, congue rutrum nisi. Ut non aliquet ex.
+          </Paragraph>
+        </Box>
+      </SkirtCard>
+    </Skirt>
+  </>
+)
 
-// export const DefaultStory = () => (
-//   <Skirt>
-//     world!
-//   </Skirt>
-// )
+DefaultStory.storyName = "Default (Kaizen Site Demo)"
 
-// DefaultStory.storyName = "Default (Kaizen Site Demo)"
+export const EducationVariant = () => (
+  <>
+    <TitleBlockZen
+      variant="education"
+      title="Education variant"
+      breadcrumb={{
+        path: "#",
+        text: "Back to home",
+        handleClick: event => {
+          alert("breadcrumb clicked!")
+        },
+      }}
+      navigationTabs={[
+        <NavigationTab variant="education" text="Label" href="#" active />,
+      ]}
+    />
+    <Skirt variant="education">
+      <SkirtCard>
+        <Box p={1}>
+          <Paragraph variant="body">
+            The Skirt component extends the background color from the Title
+            Block into the content area.
+          </Paragraph>
+        </Box>
+      </SkirtCard>
+    </Skirt>
+  </>
+)
+
+EducationVariant.storyName = "Education variant"
+
+export const WithoutTitleBlockNavigation = () => (
+  <>
+    <TitleBlockZen
+      title="Without Title Block navigation"
+      breadcrumb={{
+        path: "#",
+        text: "Back to home",
+        handleClick: event => {
+          alert("breadcrumb clicked!")
+        },
+      }}
+      collapseNavigationAreaWhenPossible={true}
+    />
+    <Skirt titleBlockHasNavigation={false}>
+      <SkirtCard>
+        <Box p={1}>
+          <Box mb={1}>
+            <Box mb={1}>
+              <Paragraph variant="body">
+                When the navigation area for the Title Block is collapsed,
+                there’s an additional prop to ensure the maximum height of the
+                skirt is consistent when it moves up. If you navigate to the
+                default Skirt story (which has navigation) you should see the
+                bottom level of the Skirt in both that story and this one match.
+              </Paragraph>
+            </Box>
+            <Box mb={1}>
+              <Paragraph variant="body">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+                semper odio vitae sem gravida rutrum. Praesent vel sapien eget
+                eros dictum luctus scelerisque eu nibh. Etiam ullamcorper
+                lobortis gravida. Suspendisse massa tortor, ultricies et ipsum
+                at, iaculis bibendum est. Mauris vestibulum interdum ultricies.
+                Proin sed elit lacinia, malesuada leo in, auctor enim.
+                Suspendisse venenatis, tortor vel eleifend cursus, metus sem
+                luctus nunc, at maximus magna metus at mi. Curabitur porttitor
+                dignissim ante, sit amet tincidunt nibh elementum convallis.
+                Morbi accumsan rutrum elit eget sagittis. Pellentesque id quam
+                at enim dictum placerat sit amet in purus. Pellentesque habitant
+                morbi tristique senectus et netus et malesuada fames ac turpis
+                egestas. Suspendisse quam elit, facilisis sit amet libero vitae,
+                congue rutrum nisi. Ut non aliquet ex.
+              </Paragraph>
+            </Box>
+            <Paragraph variant="body">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+              semper odio vitae sem gravida rutrum. Praesent vel sapien eget
+              eros dictum luctus scelerisque eu nibh. Etiam ullamcorper lobortis
+              gravida. Suspendisse massa tortor, ultricies et ipsum at, iaculis
+              bibendum est. Mauris vestibulum interdum ultricies. Proin sed elit
+              lacinia, malesuada leo in, auctor enim. Suspendisse venenatis,
+              tortor vel eleifend cursus, metus sem luctus nunc, at maximus
+              magna metus at mi. Curabitur porttitor dignissim ante, sit amet
+              tincidunt nibh elementum convallis. Morbi accumsan rutrum elit
+              eget sagittis. Pellentesque id quam at enim dictum placerat sit
+              amet in purus. Pellentesque habitant morbi tristique senectus et
+              netus et malesuada fames ac turpis egestas. Suspendisse quam elit,
+              facilisis sit amet libero vitae, congue rutrum nisi. Ut non
+              aliquet ex.
+            </Paragraph>
+          </Box>
+        </Box>
+      </SkirtCard>
+    </Skirt>
+  </>
+)
+
+WithoutTitleBlockNavigation.storyName = "Title Block without navigation"
+
+export const WithoutSkirtCard = () => (
+  <>
+    <TitleBlockZen
+      title="Without Skirt Card"
+      breadcrumb={{
+        path: "#",
+        text: "Back to home",
+        handleClick: event => {
+          alert("breadcrumb clicked!")
+        },
+      }}
+      collapseNavigationAreaWhenPossible
+    />
+    <Skirt titleBlockHasNavigation={false}>
+      <Box mb={1}>
+        <Paragraph variant="body" color="white">
+          The SkirtCard component is provided as a convenience for the most
+          common design case, but it not required.
+        </Paragraph>
+      </Box>
+      <Paragraph variant="body">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla semper
+        odio vitae sem gravida rutrum. Praesent vel sapien eget eros dictum
+        luctus scelerisque eu nibh. Etiam ullamcorper lobortis gravida.
+        Suspendisse massa tortor, ultricies et ipsum at, iaculis bibendum est.
+        Mauris vestibulum interdum ultricies. Proin sed elit lacinia, malesuada
+        leo in, auctor enim. Suspendisse venenatis, tortor vel eleifend cursus,
+        metus sem luctus nunc, at maximus magna metus at mi. Curabitur porttitor
+        dignissim ante, sit amet tincidunt nibh elementum convallis. Morbi
+        accumsan rutrum elit eget sagittis. Pellentesque id quam at enim dictum
+        placerat sit amet in purus. Pellentesque habitant morbi tristique
+        senectus et netus et malesuada fames ac turpis egestas. Suspendisse quam
+        elit, facilisis sit amet libero vitae, congue rutrum nisi. Ut non
+        aliquet ex.
+      </Paragraph>
+    </Skirt>
+  </>
+)
+
+WithoutSkirtCard.storyName = "Without SkirtCard"
--- draft-packages/stories/TitleBlockZen.stories.tsx
+++ draft-packages/stories/TitleBlockZen.stories.tsx
@@ -1,11 +1,16 @@
-import { Box, Heading, Paragraph } from "@kaizen/component-library"
 import * as React from "react"
 import addIcon from "@kaizen/component-library/icons/add.icon.svg"
+import { Box, Heading, Paragraph } from "@kaizen/component-library"
 import commentIcon from "@kaizen/component-library/icons/comment.icon.svg"
 import starIcon from "@kaizen/component-library/icons/star-on.icon.svg"
 import reportSharingIcon from "@kaizen/component-library/icons/report-sharing.icon.svg"
 import arrowForwardIcon from "@kaizen/component-library/icons/arrow-forward.icon.svg"
 import { assetUrl } from "@kaizen/hosted-assets"
+import { Skirt, SkirtCard } from "@kaizen/draft-skirt"
+import {
+  ContentBlockContainer,
+  ContentBlock,
+} from "@kaizen/draft-content-block"
 import {
   NavigationTab,
   TitleBlockZen,
@@ -342,6 +347,27 @@ export const EducationVariant = () => (
         },
       }}
     />
+    <Skirt variant="education">
+      <SkirtCard>
+        <Box p={1}>
+          <Paragraph variant="body">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+            semper odio vitae sem gravida rutrum. Praesent vel sapien eget eros
+            dictum luctus scelerisque eu nibh. Etiam ullamcorper lobortis
+            gravida. Suspendisse massa tortor, ultricies et ipsum at, iaculis
+            bibendum est. Mauris vestibulum interdum ultricies. Proin sed elit
+            lacinia, malesuada leo in, auctor enim. Suspendisse venenatis,
+            tortor vel eleifend cursus, metus sem luctus nunc, at maximus magna
+            metus at mi. Curabitur porttitor dignissim ante, sit amet tincidunt
+            nibh elementum convallis. Morbi accumsan rutrum elit eget sagittis.
+            Pellentesque id quam at enim dictum placerat sit amet in purus.
+            Pellentesque habitant morbi tristique senectus et netus et malesuada
+            fames ac turpis egestas. Suspendisse quam elit, facilisis sit amet
+            libero vitae, congue rutrum nisi. Ut non aliquet ex.
+          </Paragraph>
+        </Box>
+      </SkirtCard>
+    </Skirt>
   </Container>
 )
 
@@ -694,6 +720,90 @@ export const DefaultNoSecondary = () => (
         <NavigationTab text="Label" href="#" />,
       ]}
     />
+    <Skirt>
+      <SkirtCard>
+        <Box p={1}>
+          <Box mb={1}>
+            <Heading variant="heading-1">Placeholder heading</Heading>
+          </Box>
+          <Box my={1}>
+            <Paragraph variant="body">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+              semper odio vitae sem gravida rutrum. Praesent vel sapien eget
+              eros dictum luctus scelerisque eu nibh. Etiam ullamcorper lobortis
+              gravida. Suspendisse massa tortor, ultricies et ipsum at, iaculis
+              bibendum est. Mauris vestibulum interdum ultricies. Proin sed elit
+              lacinia, malesuada leo in, auctor enim. Suspendisse venenatis,
+              tortor vel eleifend cursus, metus sem luctus nunc, at maximus
+              magna metus at mi. Curabitur porttitor dignissim ante, sit amet
+              tincidunt nibh elementum convallis. Morbi accumsan rutrum elit
+              eget sagittis. Pellentesque id quam at enim dictum placerat sit
+              amet in purus. Pellentesque habitant morbi tristique senectus et
+              netus et malesuada fames ac turpis egestas. Suspendisse quam elit,
+              facilisis sit amet libero vitae, congue rutrum nisi. Ut non
+              aliquet ex.
+            </Paragraph>
+          </Box>
+          <Box my={1}>
+            <Paragraph variant="body">
+              Aenean bibendum leo sit amet nisi pellentesque, vitae semper nunc
+              interdum. Nullam in felis porta, aliquam neque ut, viverra sapien.
+              Proin gravida diam sit amet bibendum viverra. Duis ligula erat,
+              pretium vel faucibus ac, vestibulum nec ligula. Nullam eleifend
+              leo non mauris facilisis, sed placerat lectus consectetur.
+              Curabitur rutrum elementum lobortis. Donec risus enim, tempus
+              rutrum venenatis et, mattis vel sem. Vivamus sed tempus justo,
+              quis ultricies metus. Etiam vitae ante interdum, tempor quam in,
+              auctor sem. Pellentesque blandit sem lorem, ut imperdiet velit
+              lobortis sed. Cras aliquam finibus suscipit. Vestibulum tempus,
+              est at egestas scelerisque, ante quam interdum risus, a ultricies
+              mi nibh eget est.
+            </Paragraph>
+          </Box>
+          <Box my={1}>
+            <Paragraph variant="body">
+              Nulla et malesuada ligula, at euismod turpis. Curabitur ut tempus
+              ante. Nulla sit amet tellus eu neque volutpat volutpat. Mauris
+              vitae fermentum eros. Aliquam semper nisl ac turpis vestibulum
+              mattis. Curabitur ultrices pellentesque sapien, vitae vulputate
+              ipsum. In imperdiet dapibus magna, non ultricies lorem dictum non.
+              Mauris pretium erat eu elit laoreet, ac egestas ipsum aliquet.
+              Nullam cursus massa felis, et luctus felis semper elementum. Nulla
+              facilisi. In hac habitasse platea dictumst. Curabitur urna nibh,
+              tincidunt nec facilisis sit amet, finibus nec lorem. Morbi
+              hendrerit volutpat augue eget condimentum. Ut a fringilla augue,
+              quis finibus dui.
+            </Paragraph>
+          </Box>
+          <Box my={1}>
+            <Paragraph variant="body">
+              Etiam fermentum cursus massa non fermentum. Proin quis pharetra
+              enim. Phasellus fringilla sollicitudin auctor. Nunc tempor orci ac
+              nunc convallis dictum vel eget est. Sed eget placerat ante, quis
+              venenatis felis. Nulla varius varius lacus, quis consequat ex
+              placerat et. In consequat vestibulum tincidunt. Vivamus nibh leo,
+              mollis nec venenatis ac, pharetra id dui. Cras ullamcorper ante a
+              neque sodales, non tempus nisl sagittis. Etiam in odio sagittis,
+              hendrerit arcu et, rutrum metus.
+            </Paragraph>
+          </Box>
+          <Box>
+            <Paragraph variant="body">
+              Morbi eu mollis mauris, eleifend convallis nisi. Quisque malesuada
+              nisi ut leo tincidunt sodales. Nulla in accumsan diam. Nullam
+              egestas sed eros at luctus. Fusce eget elit quis nunc viverra
+              efficitur at et sem. Sed eget nisi et orci commodo hendrerit nec
+              in quam. Suspendisse condimentum tristique dignissim. Nunc sit
+              amet tortor efficitur, pellentesque ex at, placerat risus. Cras
+              elit ex, venenatis sit amet purus eget, mollis viverra justo.
+              Quisque sollicitudin ipsum non mi rutrum, feugiat hendrerit enim
+              malesuada. Ut massa enim, commodo non dapibus pharetra, viverra
+              sed erat.
+            </Paragraph>
+          </Box>
+        </Box>
+      </SkirtCard>
+    </Skirt>
   </Container>
 )
 
@@ -838,6 +948,15 @@ export const DefaultNoLink = () => (
         <NavigationTab text="Label" href="#" />,
       ]}
     />
+    <Skirt titleBlockHasNavigation={false}>
+      <SkirtCard>
+        <Box p={1}>
+          <Paragraph variant="body" color="white-reduced-opacity">
+            I am some text that should feel nicely butted up
+          </Paragraph>
+        </Box>
+      </SkirtCard>
+    </Skirt>
   </Container>
 )
 
@@ -932,33 +1051,33 @@ DefaultCollapsedNavigationCard.story = {
 
 export const AdminVariantNavigation = () => (
   <Container>
-    <>
-      <TitleBlockZen
-        variant="admin"
-        title="Page title"
-        subtitle="Subtitle goes here"
-        handleHamburgerClick={() => {
-          alert("Hamburger clicked")
-        }}
-        breadcrumb={{
-          path: "#",
-          text: "Back to home",
-          handleClick: event => {
-            alert("breadcrumb clicked!")
-          },
-        }}
-        collapseNavigationAreaWhenPossible
-      />
-      <div className={styles.fakeSkirtContainer}>
-        <div className={styles.fakeSkirtContent}>
+    <TitleBlockZen
+      variant="admin"
+      title="Page title"
+      subtitle="Subtitle goes here"
+      handleHamburgerClick={() => {
+        alert("Hamburger clicked")
+      }}
+      breadcrumb={{
+        path: "#",
+        text: "Back to home",
+        handleClick: event => {
+          alert("breadcrumb clicked!")
+        },
+      }}
+      collapseNavigationAreaWhenPossible
+    />
+    <ContentBlockContainer>
+      <ContentBlock>
+        <Box p={1}>
           <Paragraph variant="body">
             I am some text that should feel tightly butted up to the nav space.
             The responsibility for the admin variant is to move things down
             yourself if you need to.
           </Paragraph>
-        </div>
-      </div>
-    </>
+        </Box>
+      </ContentBlock>
+    </ContentBlockContainer>
   </Container>
 )
 
