834f4a17e88d3133b4f9dd49099f34fc7bfbd499
Max Wheeler <max@makenosound.com>
2020-12-18 17:23:08 +1100

    Move files aorund

--- draft-packages/content-block/KaizenDraft/ContentBlock/ContentBlock.tsx
+++ draft-packages/content-block/KaizenDraft/ContentBlock/ContentBlock.tsx
@@ -1,6 +1,6 @@
 import * as React from "react"
 import classNames from "classnames"
-import styles from "./styles.module.scss"
+import styles from "./styles.scss"
 
 export interface ContentBlockProps {
   /**
