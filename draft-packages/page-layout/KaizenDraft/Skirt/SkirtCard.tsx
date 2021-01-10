aeecb1627b919a916c846a6d14876a185a1b448f
Max Wheeler <max.wheeler@cultureamp.com>
2021-01-11 10:45:31 +1100

    feat: Add draft-page-layout package (Content, Container, Skirt, SkirtCard components) (#910)
    * Add Content/Container
    
    * Skirt implementation
    
    * Add SkirtCard
    
    * Restrict allowed CSS attributes for Content components

---
+++ draft-packages/page-layout/KaizenDraft/Skirt/SkirtCard.tsx
@@ -0,0 +1,17 @@
+import * as React from "react"
+import { Card, CardProps } from "@kaizen/draft-card"
+import classNames from "classnames"
+import styles from "./SkirtCard.scss"
+
+export const SkirtCard = (props: CardProps) => {
+  const { classNameAndIHaveSpokenToDST } = props
+  return (
+    <Card
+      {...props}
+      classNameAndIHaveSpokenToDST={classNames(
+        styles.wrapper,
+        classNameAndIHaveSpokenToDST
+      )}
+    ></Card>
+  )
+}
