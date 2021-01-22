d12565d8f4dbf0738989c38aa8e4e9ebad495b0d
ActuallyACat <allanna.beaton@gmail.com>
2021-01-22 17:12:37 +1100

    Uplifted the navigation bar

--- packages/component-library/stories/NavigationBar.stories.tsx
+++ packages/component-library/stories/NavigationBar.stories.tsx
@@ -3,9 +3,7 @@ import * as React from "react"
 import { Icon, Link, Menu, NavigationBar } from "@kaizen/component-library"
 
 import academyIcon from "@kaizen/component-library/icons/academy.icon.svg"
-
 import caMonogramIcon from "@kaizen/component-library/icons/ca-monogram.icon.svg"
-
 import supportIcon from "@kaizen/component-library/icons/support.icon.svg"
 
 export default {
