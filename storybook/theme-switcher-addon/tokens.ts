45aec184016a335f719f6fb9864f3cdda5b10214
ActuallyACat <allanna.beaton@gmail.com>
2021-02-04 17:54:14 +1100

    Added theme switcher preset

---
+++ storybook/theme-switcher-addon/tokens.ts
@@ -0,0 +1,194 @@
+export const tokens = [
+  {
+    name: "--kz-color-wisteria-100",
+    value: "#f4edf8",
+  },
+  {
+    name: "--kz-color-wisteria-200",
+    value: "#dfc9ea",
+  },
+  {
+    name: "--kz-color-wisteria-300",
+    value: "#c9a5dd",
+  },
+  {
+    name: "--kz-color-wisteria-400",
+    value: "#ae67b1",
+  },
+  {
+    name: "--kz-color-wisteria-500",
+    value: "#844587",
+  },
+  {
+    name: "--kz-color-wisteria-600",
+    value: "#5f3361",
+  },
+  {
+    name: "--kz-color-wisteria-700",
+    value: "#4a234d",
+  },
+  {
+    name: "--kz-color-wisteria-800",
+    value: "#2f2438",
+  },
+  {
+    name: "--kz-color-cluny-100",
+    value: "#e6f6ff",
+  },
+  {
+    name: "--kz-color-cluny-200",
+    value: "#bde2f5",
+  },
+  {
+    name: "--kz-color-cluny-300",
+    value: "#73c0e8",
+  },
+  {
+    name: "--kz-color-cluny-400",
+    value: "#008bd6",
+  },
+  {
+    name: "--kz-color-cluny-500",
+    value: "#0168b3",
+  },
+  {
+    name: "--kz-color-cluny-600",
+    value: "#004970",
+  },
+  {
+    name: "--kz-color-cluny-700",
+    value: "#003157",
+  },
+  {
+    name: "--kz-color-seedling-100",
+    value: "#e8f8f4",
+  },
+  {
+    name: "--kz-color-seedling-200",
+    value: "#c4ede2",
+  },
+  {
+    name: "--kz-color-seedling-300",
+    value: "#8fdbc7",
+  },
+  {
+    name: "--kz-color-seedling-400",
+    value: "#5dcbad",
+  },
+  {
+    name: "--kz-color-seedling-500",
+    value: "#3caf90",
+  },
+  {
+    name: "--kz-color-seedling-600",
+    value: "#2c7d67",
+  },
+  {
+    name: "--kz-color-seedling-700",
+    value: "#22594a",
+  },
+  {
+    name: "--kz-color-yuzu-100",
+    value: "#fff9e4",
+  },
+  {
+    name: "--kz-color-yuzu-200",
+    value: "#ffeeb3",
+  },
+  {
+    name: "--kz-color-yuzu-300",
+    value: "#ffe36e",
+  },
+  {
+    name: "--kz-color-yuzu-400",
+    value: "#ffca4d",
+  },
+  {
+    name: "--kz-color-yuzu-500",
+    value: "#ffb600",
+  },
+  {
+    name: "--kz-color-yuzu-600",
+    value: "#c68600",
+  },
+  {
+    name: "--kz-color-yuzu-700",
+    value: "#876400",
+  },
+  {
+    name: "--kz-color-coral-100",
+    value: "#fdeaee",
+  },
+  {
+    name: "--kz-color-coral-200",
+    value: "#f9c2cb",
+  },
+  {
+    name: "--kz-color-coral-300",
+    value: "#f597a8",
+  },
+  {
+    name: "--kz-color-coral-400",
+    value: "#e0707d",
+  },
+  {
+    name: "--kz-color-coral-500",
+    value: "#c93b55",
+  },
+  {
+    name: "--kz-color-coral-600",
+    value: "#a82433",
+  },
+  {
+    name: "--kz-color-coral-700",
+    value: "#6c1e20",
+  },
+  {
+    name: "--kz-color-peach-100",
+    value: "#fff0e8",
+  },
+  {
+    name: "--kz-color-peach-200",
+    value: "#ffd1b9",
+  },
+  {
+    name: "--kz-color-peach-300",
+    value: "#ffb08a",
+  },
+  {
+    name: "--kz-color-peach-400",
+    value: "#ff9461",
+  },
+  {
+    name: "--kz-color-peach-500",
+    value: "#e96c2f",
+  },
+  {
+    name: "--kz-color-peach-600",
+    value: "#b74302",
+  },
+  {
+    name: "--kz-color-peach-700",
+    value: "#903c00",
+  },
+  {
+    name: "--kz-color-ash",
+    value: "#e6e5e5",
+  },
+  {
+    name: "--kz-color-stone",
+    value: "#f9f9f9",
+  },
+  {
+    name: "--kz-color-white",
+    value: "#ffffff",
+  },
+  {
+    name: "--kz-color-iron-500",
+    value: "#8c8c97",
+  },
+  {
+    name: "--kz-color-slate-700",
+    value: "#524e56",
+  },
+]
