23d45f412d5a2104441f293b9a0a293cade134f0
ActuallyACat <allanna.beaton@gmail.com>
2021-02-08 17:48:18 +1100

    Added rule to check for lockfile

--- .danger/dangerfile.ts
+++ .danger/dangerfile.ts
@@ -1,4 +1,11 @@
-import { message, danger } from "danger"
+import { message, fail, danger } from "danger"
 
 const modifiedMD = danger.git.modified_files.join("- ")
 message("Changed Files in this PR: \n - " + modifiedMD)
+
+const containsNpmLockFile = danger.git.modified_files.find(curr =>
+  curr.includes("package-lock.json")
+)
+if (containsNpmLockFile !== undefined) {
+  fail("Committed NPM lockfile")
+}
