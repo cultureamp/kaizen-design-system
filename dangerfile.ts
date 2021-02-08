8543a34d23cad00ba5da0576ca8d33c868cadf0e
ActuallyACat <allanna.beaton@gmail.com>
2021-02-08 18:04:23 +1100

    check added files

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,11 +1,12 @@
-import { message, fail, danger } from "danger"
+import { fail, danger } from "danger"
 
-const modifiedMD = danger.git.modified_files.join("- ")
-message("Changed Files in this PR: \n - " + modifiedMD)
-
-const containsNpmLockFile = danger.git.modified_files.find(curr =>
-  curr.includes("package-lock.json")
-)
-if (containsNpmLockFile !== undefined) {
-  fail("Committed NPM lockfile")
+const checkNewLockfileRule = () => {
+  const containsNpmLockFile = danger.git.created_files.find(curr =>
+    curr.includes("package-lock.json")
+  )
+  if (containsNpmLockFile !== undefined) {
+    fail("Committed NPM lockfile")
+  }
 }
+
+checkNewLockfileRule()
