1b715ab94edd0e7ea65dbcb82fde5469064aec73
Michael Bylstra <mbylstra@gmail.com>
2021-02-08 14:39:49 +1100

    (fix) upgrade all packages to use elm-storybook 0.3.0

--- draft-packages/well/docs/ElmWell.stories.tsx
+++ draft-packages/well/docs/ElmWell.stories.tsx
@@ -2,13 +2,6 @@ import { loadElmStories } from "elm-storybook"
 const compiledElm = require("../ElmStories/WellStories.elm").Elm.ElmStories
   .WellStories
 
-import React from "react"
-import { storiesOf } from "@storybook/react"
-
-storiesOf("ElmWell", module).add("with some emoji", () => (
-  <div> 😀 😎 👍 💯</div>
-))
-
 loadElmStories("Well (Elm)", module, compiledElm, [
   "Default with solid border",
   "Default with dashed border",
