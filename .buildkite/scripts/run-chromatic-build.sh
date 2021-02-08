6bfbf1412f860fc198b2e2b3a129fd0b7bcf63ca
Michael Bylstra <mbylstra@gmail.com>
2021-02-08 15:45:17 +1100

    --allow-console-errors to try to fix chomatic build
    chromatic reports 13 errors, the console log only has warnings.
    What are these erorrs?? I'm passing in --allow-console-errors
    to see if the the chromatic build works.

--- .buildkite/scripts/run-chromatic-build.sh
+++ .buildkite/scripts/run-chromatic-build.sh
@@ -9,4 +9,4 @@ CHROMATIC_APP_CODE=$(get_secret "chromatic-app-code") || exit $?
 
 yarn install --frozen-lockfile
 yarn storybook:build
-yarn chromatic --exit-zero-on-changes --storybook-build-dir storybook/public
+yarn chromatic --allow-console-errors --exit-zero-on-changes --storybook-build-dir storybook/public
