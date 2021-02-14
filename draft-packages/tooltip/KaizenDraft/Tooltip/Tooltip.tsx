b284dd3485e98687caca3d698ac0060d1a90cb17
Jeremy Casey <jeremy8883@gmail.com>
2021-02-15 09:54:58 +1100

    Split out the tooltip context into its own component, to clean the code, and to prevent the popper hooks from being used when not needed

--- draft-packages/tooltip/KaizenDraft/Tooltip/Tooltip.tsx
+++ draft-packages/tooltip/KaizenDraft/Tooltip/Tooltip.tsx
@@ -18,17 +18,7 @@ export type TooltipProps = {
 
 const arrowHeight = 10
 
-const Tooltip = ({
-  children,
-  text,
-  inline,
-  position,
-  classNameAndIHaveSpokenToDST,
-  portalSelector,
-}: TooltipProps) => {
-  const [isHover, setIsHover] = useState(false)
-  const [isFocus, setIsFocus] = useState(false)
-  const [referenceElement, setReferenceElement] = useState(null)
+const TooltipContent = ({ position, text, referenceElement }) => {
   const [popperElement, setPopperElement] = useState(null)
   const [arrowElement, setArrowElement] = useState(null)
   const { styles: popperStyles, attributes } = usePopper(
@@ -53,30 +43,51 @@ const Tooltip = ({
     }
   )
 
-  const tooltip = (
-    <AppearanceAnim isVisible={isHover || isFocus}>
+  return (
+    <div
+      ref={setPopperElement}
+      style={popperStyles.popper}
+      {...attributes.popper}
+      className={styles.tooltip}
+    >
+      <div className={classnames(styles.tooltipContent)}>{text}</div>
       <div
-        ref={setPopperElement}
-        style={popperStyles.popper}
-        {...attributes.popper}
-        className={styles.tooltip}
+        ref={setArrowElement}
+        className={classnames({
+          [styles.arrow]: true,
+          [styles.arrowAbove]: position === "above",
+          [styles.arrowBelow]: position === "below",
+        })}
+        style={popperStyles.arrow}
       >
-        <div className={classnames(styles.tooltipContent)}>{text}</div>
-        <div
-          ref={setArrowElement}
-          className={classnames({
-            [styles.arrow]: true,
-            [styles.arrowAbove]: position === "above",
-            [styles.arrowBelow]: position === "below",
-          })}
-          style={popperStyles.arrow}
-        >
-          <div className={styles.arrowInner}>
-            <div className={styles.arrowWhite} />
-            <div className={styles.arrowShadow} />
-          </div>
+        <div className={styles.arrowInner}>
+          <div className={styles.arrowWhite} />
+          <div className={styles.arrowShadow} />
         </div>
       </div>
+    </div>
+  )
+}
+
+const Tooltip = ({
+  children,
+  text,
+  inline,
+  position,
+  classNameAndIHaveSpokenToDST,
+  portalSelector,
+}: TooltipProps) => {
+  const [isHover, setIsHover] = useState(false)
+  const [isFocus, setIsFocus] = useState(false)
+  const [referenceElement, setReferenceElement] = useState(null)
+
+  const tooltip = (
+    <AppearanceAnim isVisible={isHover || isFocus}>
+      <TooltipContent
+        text={text}
+        position={position}
+        referenceElement={referenceElement}
+      />
     </AppearanceAnim>
   )
 
