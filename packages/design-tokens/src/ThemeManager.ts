13165ea1f80a90a48a154483d52b3b5bc139dd0e
Daniel Manning <dmisdm@users.noreply.github.com>
2021-02-15 10:36:55 +1100

    feat: New sibling CSS variable tokens, heart theme, and theme switching (#1030)
    * Sibling CSS variable tokens and theme switching.
    
    * Generate new token files containing CSS variable functions
    * Generate a css/default-theme.css containing the variables of the default theme, along with css/heart-theme.css and css/zen-theme.css
    * Add ThemeManager class to support subscribing to and switching the current theme
    * Expose a React context wrapper around ThemeManager, with supporting hook: `useTheme`.
    
    These code changes we're migrated from an older PR, #967.
    It was migrated using `git checkout jasono/css-var-token-switching -- packages/design-tokens`, so the precise (but brief) commit history from that branch was lost.
    The reason why cherry pick wasn't used is because there we're only a few commits related to design-tokens changes, and some had been mixed with changes to other directories too.
    
    Cross posting from slack for more context:
    We’re going to change the release path of the new tokens to make the migration of consumers and release of components much easier.
    Rather than making a breaking change of each $kz-blah: #ff0011 variable, we’ll initially introduce new sibling variables that contain the CSS variable version of the same thing, like $kzvar-blah: var(--kz-blah) , which means that consumers will be able to update to the new design tokens version without any issues.
    In the work done prior where we wanted this breaking change to happen, this was much more trouble than it was worth, mainly because we would have to make massive PRs which would be difficult to verify and review.
    This way, consumers can update kaizen dependencies and design tokens asynchronously.
    
    * Storybook theme switcher addon now using the new ThemeManager
    
    * Don't produce variables with `-default` suffixes
    
    * Typo in storybook theme.ts
    
    * Fixes css variable theme namespace, and storybook
    
    * Improve design token story
    
    * Add CSS variable fallback values

---
+++ packages/design-tokens/src/ThemeManager.ts
@@ -0,0 +1,80 @@
+import { defaultTheme } from "./themes"
+import { DeepMapObjectLeafs, Theme as BaseTheme } from "./types"
+import { cssVariableThemeNamespace, flattenObjectToCSSVariables } from "./utils"
+
+/**
+ * Use this class to set and apply themes, and to access or subscribe to the currently active one.
+ * This class fulfills the idea of theming and runtime theme switching by relying on CSS variables,
+ * and the ability to update them in JavaScript - a framework agnostic method.
+ *
+ * It works by converting a Theme interface to a flattened map of CSS variable keys and values, then calling `document.documentElement.style.setProperty(key, value)`.
+ */
+export class ThemeManager<Theme extends BaseTheme = BaseTheme> {
+  private themeChangeListeners = [] as Array<(theme: Theme) => void>
+  private theme: Theme
+  private rootElement = document.documentElement
+  constructor(
+    theme: Theme,
+    rootElement = document.documentElement,
+    /* This allows you to stop the  class from applying the theme automatically during construction. Defaults to true */
+    apply: boolean = true
+  ) {
+    /*
+      For some reason, storybook likes this way of defining class properties better.
+      If you use `constructor( private theme: Theme, ...)` - theme becomes undefined within the class's methods.
+    */
+    this.theme = theme
+    this.rootElement = rootElement
+    if (apply) this.applyCurrentTheme()
+  }
+
+  public getRootElement = () => this.rootElement
+  public getCurrentTheme = () => this.theme
+  public getCssVariableThemeKey = () => {
+    const justTheKey = flattenObjectToCSSVariables({
+      kz: { themeKey: this.theme.themeKey },
+    })
+    return this.rootElement.style.getPropertyValue(justTheKey["--kz-theme-key"])
+  }
+  public setRootElement = (element: HTMLElement) => {
+    this.rootElement = element
+  }
+  public setAndApplyTheme = (theme: Theme, force?: boolean) => {
+    if (!force) {
+      if (
+        this.theme === theme ||
+        // This case will happen if you load a theme initially using CSS.
+        theme.themeKey === this.getCssVariableThemeKey()
+      )
+        return
+    }
+    this.theme = theme
+    this.applyCurrentTheme()
+    this.notifyThemeChangeListeners(theme)
+  }
+
+  public addThemeChangeListener = (listener: (theme: Theme) => void) => {
+    this.themeChangeListeners.push(listener)
+  }
+  public removeThemeChangeListener = (listener: (theme: Theme) => void) => {
+    this.themeChangeListeners = this.themeChangeListeners.filter(
+      l => l !== listener
+    )
+  }
+  public applyCurrentTheme = () => {
+    const cssVariablesOfTheme = flattenObjectToCSSVariables({
+      [cssVariableThemeNamespace]: this.theme,
+    })
+    Object.entries(cssVariablesOfTheme).forEach(([key, value]) => {
+      this.rootElement.style.setProperty(key, value)
+    })
+  }
+
+  private notifyThemeChangeListeners = (theme: Theme) => {
+    this.themeChangeListeners.forEach(listener => listener(theme))
+  }
+}
+
+// I would like to expose this, but instantiating ThemeManager in some situations or runtimes (such as nodejs) cause exceptions.
+// For now it is not exposed, but we might come back to it, as it makes sense to have a singleton ThemeManager in almost all cases
+/* export const defaultThemeManager = new ThemeManager(defaultTheme) */
---
+++ packages/design-tokens/src/themes/index.ts
@@ -0,0 +1,4 @@
+import { zenTheme } from "./zen"
+export { heartTheme } from "./heart"
+export { zenTheme } from "./zen"
+export const defaultTheme = zenTheme
