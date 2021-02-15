d3869c839ae3f68e27c62104316f885a2c36fcfe
actuallyacat <allanna.beaton@gmail.com>
2021-02-15 16:09:40 +1100

    Remove deprecation warning

--- packages/component-library/bin/design-uplift/replace-add-alpha.sh
+++ packages/component-library/bin/design-uplift/replace-add-alpha.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -eux
 
-fastmod -m -d draft-packages/ -d legacy-packages/ -d packages/ --extensions scss,css \
+fastmod -m -d $(pwd) --extensions scss,css \
   'add-alpha\((.*)?\)' \
   'rgba($1)'
