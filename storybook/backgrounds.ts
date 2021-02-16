8c866286a79c457024cd6b3c0fdd34ba2f8cc966
Daniel Manning <dmisdm@users.noreply.github.com>
2021-02-16 16:26:48 +1100

    docs: Story aggregation and overview page (#1053)
    * Story aggregation page for smoke testing
    
    * Add rest to stories overview
    
    * Performance improvements
    
    * Update index.stories.tsx
    
    * Re-render ability
    
    * Disable chromatic for story board
    
    * More comments on design tokens story

--- storybook/backgrounds.ts
+++ storybook/backgrounds.ts
@@ -1,6 +1,7 @@
 /* eslint import/no-extraneous-dependencies: 0 */
 
 import * as tokens from "@kaizen/design-tokens/tokens/color.json"
+import * as variables from "@kaizen/design-tokens/tokens/color-vars.json"
 
 export const backgrounds = [
   { name: "White", value: tokens.kz.color.white },
@@ -12,4 +13,17 @@ export const backgrounds = [
   { name: "Yuzu 500", value: tokens.kz.color.yuzu["500"] },
   { name: "Peach 500", value: tokens.kz.color.peach["500"] },
   { name: "Coral 500", value: tokens.kz.color.coral["500"] },
+]
+
+// This is a temporary duplication until we find a way for storybook to like the above ^^ tokens with css variables rather than concretes.
+export const cssVarBackgrounds = [
+  { name: "White", value: variables["kz-var"].color.white },
+  { name: "Stone", value: variables["kz-var"].color.stone },
+  { name: "Ash", value: variables["kz-var"].color.ash },
+  { name: "Wisteria 700", value: variables["kz-var"].color.wisteria["700"] },
+  { name: "Cluny 500", value: variables["kz-var"].color.cluny["500"] },
+  { name: "Seedling 500", value: variables["kz-var"].color.seedling["500"] },
+  { name: "Yuzu 500", value: variables["kz-var"].color.yuzu["500"] },
+  { name: "Peach 500", value: variables["kz-var"].color.peach["500"] },
+  { name: "Coral 500", value: variables["kz-var"].color.coral["500"] },
 ]
--- types.d.ts
+++ types.d.ts
@@ -15,3 +15,23 @@ declare module "*.icon.svg" {
   }
   export default content
 }
+
+declare module "react-map-interaction" {
+  type InteractionState = {
+    scale: number
+    translation: { x: number; y: number }
+  }
+  type Props = {
+    showControls?: boolean
+    minScale?: number
+    maxScale?: number
+    value?: InteractionState
+    onChange?: (state: InteractionState) => void
+  }
+  export const MapInteractionCSS: import("react").ComponentType<Props>
+  export const MapInteraction: import("react").ComponentType<
+    Props & {
+      children: (params: InteractionState) => React.ReactNode
+    }
+  >
+}
