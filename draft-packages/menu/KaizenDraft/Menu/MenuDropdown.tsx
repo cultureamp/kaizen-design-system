1d58ba7c1c8947f24d32228df9396a43f322a588
Jeremy Casey <jeremy8883@gmail.com>
2021-02-24 10:50:57 +1100

    Convert `MenuDropdown` to a functional component, as so we can use the popper hooks Note: the positioning logic is now broken, but we're removing this anyway

--- draft-packages/menu/KaizenDraft/Menu/MenuDropdown.tsx
+++ draft-packages/menu/KaizenDraft/Menu/MenuDropdown.tsx
@@ -1,6 +1,5 @@
 import classnames from "classnames"
-import * as React from "react"
-
+import React, { useEffect, useRef } from "react"
 import styles from "./styles.scss"
 
 type MenuDropdownProps = {
@@ -15,68 +14,37 @@ type MenuDropdownProps = {
   align?: "left" | "right"
   width?: "default" | "contain"
   autoHide?: "on" | "outside-click-only" | "off"
+  children: React.ReactNode
 }
 
-class MenuDropdown extends React.Component<MenuDropdownProps> {
-  static displayName = "MenuDropdown"
+const MenuDropdown = ({
+  children,
+  position,
+  id,
+  hideMenuDropdown,
+  autoHide = "on",
+  align = "left",
+  width = "default",
+}: MenuDropdownProps) => {
+  const menu = useRef<HTMLDivElement | null>(null)
 
-  static defaultProps = {
-    autoHide: "on",
-  }
-
-  menu = React.createRef<HTMLDivElement>()
-
-  componentDidMount() {
-    const { autoHide } = this.props
-    if (autoHide !== "off") {
-      document.addEventListener(
-        "click",
-        this.handleDocumentClickForAutoHide,
-        false
-      )
-    }
-    window.addEventListener("resize", this.handleDocumentResize, false)
-    this.positionMenu()
-  }
-
-  componentWillUnmount() {
-    const { autoHide } = this.props
-    if (autoHide !== "off") {
-      document.removeEventListener(
-        "click",
-        this.handleDocumentClickForAutoHide,
-        false
-      )
-    }
-    window.removeEventListener("resize", this.handleDocumentResize, false)
-  }
-
-  componentWillUpdate(newProps) {
-    // Hm, I don't like hooks, but in this situation they would have been handy
-    if (this.props.autoHide === "off" && newProps.autoHide !== "off") {
-      document.addEventListener(
-        "click",
-        this.handleDocumentClickForAutoHide,
-        false
-      )
-    } else if (this.props.autoHide !== "off" && newProps.autoHide === "off") {
-      document.removeEventListener(
-        "click",
-        this.handleDocumentClickForAutoHide,
-        false
-      )
+  // This callback handler will not run when autoHide === "off"
+  const handleDocumentClickForAutoHide = (e: MouseEvent) => {
+    if (
+      menu?.current &&
+      e.target instanceof Node &&
+      !menu.current.contains(e.target)
+    ) {
+      hideMenuDropdown()
     }
   }
 
-  positionMenu() {
-    const menu = this.menu
-
-    if (!this.props.position || !menu) {
+  const positionMenu = () => {
+    if (!position || !menu) {
       return
     }
 
     if (menu.current) {
-      const pos = this.props.position
       const { innerHeight } = window
       const rect = menu.current.getBoundingClientRect()
       const offsetParentRect = menu.current.offsetParent?.getBoundingClientRect()
@@ -86,7 +54,7 @@ class MenuDropdown extends React.Component<MenuDropdownProps> {
       menu.current.style.bottom =
         // If the menu won't fit below the the menu button, show it above instead.
         // For some reason, a 5px buffer was needed.
-        pos.bottom + 5 > innerHeight - rect.height &&
+        position.bottom + 5 > innerHeight - rect.height &&
         // ...but, do not display it above the menu button, if there's not enough
         // room, otherwise the user won't even be able to scroll high enough to
         // see the menu items!
@@ -96,45 +64,49 @@ class MenuDropdown extends React.Component<MenuDropdownProps> {
     }
   }
 
-  // This callback handler will not run when autoHide === "off"
-  handleDocumentClickForAutoHide = (e: MouseEvent) => {
-    if (
-      this.menu?.current &&
-      e.target instanceof Node &&
-      !this.menu.current.contains(e.target)
-    ) {
-      this.props.hideMenuDropdown()
-    }
+  const handleDocumentResize = () => {
+    hideMenuDropdown()
   }
 
-  handleDocumentResize = () => {
-    this.props.hideMenuDropdown()
-  }
-
-  handleRootClick = (): void => {
-    const { autoHide, hideMenuDropdown } = this.props
+  const handleRootClick = (): void => {
     if (autoHide === "on") {
       // ie. is not equal to "off" | "outside-click-only"
       hideMenuDropdown()
     }
   }
 
-  render(): JSX.Element {
-    const { children, align = "left", width = "default" } = this.props
+  useEffect(() => {
+    if (autoHide !== "off") {
+      document.addEventListener("click", handleDocumentClickForAutoHide, false)
+    }
+    window.addEventListener("resize", handleDocumentResize, false)
+    positionMenu()
 
-    return (
-      <div
-        id={this.props.id}
-        className={classnames(styles.menuContainer, {
-          [styles.defaultWidth]: width == "default",
-          [styles.alignRight]: align == "right",
-        })}
-        ref={this.menu}
-        onClick={this.handleRootClick}
-      >
-        {children}
-      </div>
-    )
-  }
+    return () => {
+      if (autoHide !== "off") {
+        document.removeEventListener(
+          "click",
+          handleDocumentClickForAutoHide,
+          false
+        )
+      }
+      window.removeEventListener("resize", handleDocumentResize, false)
+    }
+  }, [autoHide])
+
+  return (
+    <div
+      id={id}
+      className={classnames(styles.menuContainer, {
+        [styles.defaultWidth]: width == "default",
+        [styles.alignRight]: align == "right",
+      })}
+      ref={menu}
+      onClick={handleRootClick}
+    >
+      {children}
+    </div>
+  )
 }
+
 export default MenuDropdown
