55161139752998a783d1c6e1a875e0812390e6f5
Daniel Manning <daniel.manning@cultureamp.com>
2021-03-02 15:36:21 +1100

    feat: Update Divider component to support Heart
    Updates the divider component's styles to support heart theme, according
    to https://docs.google.com/document/d/1G5iN3zZ0XHc0K5FKIFpa263nGW3l8d-jV
    2nWf6Gcucs/edit#heading=h.u8txzlzfikjh

--- draft-packages/divider/KaizenDraft/Divider/Divider.tsx
+++ draft-packages/divider/KaizenDraft/Divider/Divider.tsx
@@ -1,3 +1,4 @@
+import { useTheme } from "@kaizen/design-tokens"
 import classNames from "classnames"
 import * as React from "react"
 import styles from "./styles.module.scss"
@@ -7,12 +8,15 @@ export interface DividerProps {
   variant: "content" | "canvas"
 }
 
-export const Divider = ({ isReversed = false, variant }: DividerProps) => (
-  <hr
-    className={classNames(styles.wrapper, {
-      [styles.reversed]: isReversed,
-      [styles.content]: variant === "content",
-      [styles.canvas]: variant === "canvas",
-    })}
-  />
-)
+export const Divider = ({ isReversed = false, variant }: DividerProps) => {
+  const theme = useTheme()
+  return (
+    <hr
+      className={classNames(styles[theme.themeKey], styles.wrapper, {
+        [styles.reversed]: isReversed,
+        [styles.content]: variant === "content",
+        [styles.canvas]: variant === "canvas",
+      })}
+    />
+  )
+}
