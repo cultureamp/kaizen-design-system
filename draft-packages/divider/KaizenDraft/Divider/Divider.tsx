dac9e541846571c685dd4009e9e40f84723f52b2
Daniel Manning <danielmanning213@gmail.com>
2021-03-05 10:57:02 +1100

    Revert hook usage and use CSS var hack

--- draft-packages/divider/KaizenDraft/Divider/Divider.tsx
+++ draft-packages/divider/KaizenDraft/Divider/Divider.tsx
@@ -1,4 +1,3 @@
-import { useTheme } from "@kaizen/design-tokens"
 import classNames from "classnames"
 import * as React from "react"
 import styles from "./styles.module.scss"
@@ -8,15 +7,12 @@ export interface DividerProps {
   variant: "content" | "canvas"
 }
 
-export const Divider = ({ isReversed = false, variant }: DividerProps) => {
-  const theme = useTheme()
-  return (
-    <hr
-      className={classNames(styles[theme.themeKey], styles.wrapper, {
-        [styles.reversed]: isReversed,
-        [styles.content]: variant === "content",
-        [styles.canvas]: variant === "canvas",
-      })}
-    />
-  )
-}
+export const Divider = ({ isReversed = false, variant }: DividerProps) => (
+  <hr
+    className={classNames(styles.wrapper, {
+      [styles.reversed]: isReversed,
+      [styles.content]: variant === "content",
+      [styles.canvas]: variant === "canvas",
+    })}
+  />
+)
