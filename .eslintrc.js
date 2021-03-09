95aa74b12fe0afb352a43e55d1f02a5f9f48a12a
Daniel Manning <dmisdm@users.noreply.github.com>
2021-03-09 18:03:22 +1100

    docs: How to use and migrate design tokens  (#1059)
    * Initial documentation on how to use and migrate design tokens
    
    * Refactor design tokens stories
    
    * Update and improve design tokens docs
    
    * Split design token docs into how-tos
    
    Improvements to documentation after researching best practices https://documentation.divio.com/
    
    * Fixes to design tokens docs
    
    - Move all docs to the "Design Tokens" storybook section.
    - Remove "What are design tokens" and move some of its content to README
    - Minor content changes
    - Improve API/reference page
    - eslint > ignore extraneous deps for *.docsExample.tsx. This allows us to not include an eslint-disable comment within example code.
    
    * Add README as a storybook docs page
    
    * Apply suggestions from code review
    
    Co-authored-by: Jason O'Neil <jason.oneil@gmail.com>
    
    * Rename getting started doc file
    
    * Use css vars in Heading
    
    * Update how-to-use-with-sass.stories.mdx
    
    * Update how-to-use-with-sass.stories.mdx
    
    * Update how-to-use-with-sass.stories.mdx
    
    * Update api.stories.mdx
    
    * Update how-to-use-with-sass.stories.mdx
    
    * Update getting-started.stories.mdx
    
    * Update getting-started.stories.mdx
    
    * Update getting-started.stories.mdx
    
    * Undo Heading uplift for demo
    
    * Amend copy on getting-started page
    
    * How to use SASS > copy amendments
    
    * Update how to migrate to v3 page
    
    * Link to MDN instead of W3Schools
    
    * Fixup DocsComponent
    
    Co-authored-by: Jason O'Neil <jason.oneil@gmail.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -215,7 +215,7 @@ module.exports = {
   },
   overrides: [
     {
-      files: ["*.stories.tsx", "*.spec.tsx"],
+      files: ["*.stories.tsx", "*.spec.tsx", "*.docsExample.tsx"],
       rules: {
         "import/no-extraneous-dependencies": "off",
       },
