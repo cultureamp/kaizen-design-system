667d467b6927a94027f9e32ef4c25a9cee0880d8
Daniel Manning <danielmanning213@gmail.com>
2021-03-22 13:21:50 +1100

    Empty state illustrations update

--- draft-packages/empty-state/KaizenDraft/EmptyState/EmptyState.tsx
+++ draft-packages/empty-state/KaizenDraft/EmptyState/EmptyState.tsx
@@ -1,5 +1,7 @@
 import classnames from "classnames"
 import * as React from "react"
+import { assetUrl } from "@kaizen/hosted-assets"
+import { useTheme } from "@kaizen/design-tokens"
 import styles from "./styles.scss"
 
 const actionIllustration = require("./illustrations/action.png")
@@ -8,13 +10,17 @@ const negativeIllustration = require("./illustrations/negative.png")
 const neutralIllustration = require("./illustrations/neutral.png")
 const positiveIllustration = require("./illustrations/positive.png")
 
-const illustrations: { [key: string]: any } = {
+// These can be removed once the rebrand to Heart is finished.
+const deprecatedZenIllustrations: { [key: string]: any } = {
   positive: positiveIllustration as any,
   neutral: neutralIllustration as any,
   negative: negativeIllustration as any,
   informative: informativeIllustration as any,
   action: actionIllustration as any,
 }
+
+const getIllustrationUrl = (type: IllustrationType) =>
+  assetUrl(`illustrations/heart/scene/empty-states-${type}.svg`)
 
 type IllustrationType =
   | "positive"
@@ -30,7 +36,6 @@ export type EmptyStateProps = {
   automationId?: string
   headingText: string
   bodyText: string | React.ReactNode
-  straightCorners?: boolean
   illustrationType?: IllustrationType
   layoutContext?: LayoutContextType
   useZenStyles?: boolean
@@ -47,36 +52,44 @@ const EmptyState: EmptyState = ({
   headingText,
   bodyText,
   children,
-  straightCorners,
   useZenStyles,
-}) => (
-  <div
-    className={classnames([
-      styles[illustrationType],
-      styles.container,
-      styles.zen,
-      styles[layoutContext],
-      { [styles.straightCorners]: straightCorners },
-    ])}
-    id={id}
-    data-automation-id={automationId}
-  >
-    <div className={styles.illustrationSide}>
-      <img
-        src={illustrations[illustrationType]}
-        className={styles.illustration}
-      />
-    </div>
+}) => {
+  const theme = useTheme()
+  return (
     <div
-      className={classnames([styles.textSide, { [styles.zen]: useZenStyles }])}
+      className={classnames([
+        styles[illustrationType],
+        styles.container,
+        styles.zen,
+        styles[layoutContext],
+      ])}
+      id={id}
+      data-automation-id={automationId}
     >
-      <div className={styles.textSideInner}>
-        <div className={styles.heading}>{headingText}</div>
-        <div className={styles.description}>{bodyText}</div>
-        {children}
+      <div className={styles.illustrationSide}>
+        <img
+          src={
+            theme.themeKey === "zen"
+              ? deprecatedZenIllustrations[illustrationType]
+              : getIllustrationUrl(illustrationType)
+          }
+          className={styles.illustration}
+        />
+      </div>
+      <div
+        className={classnames([
+          styles.textSide,
+          { [styles.zen]: useZenStyles },
+        ])}
+      >
+        <div className={styles.textSideInner}>
+          <div className={styles.heading}>{headingText}</div>
+          <div className={styles.description}>{bodyText}</div>
+          {children}
+        </div>
       </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default EmptyState
