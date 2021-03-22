5605cf5441d7436db48f50417d1f768aedbde5d3
Michael Bylstra <mbylstra@gmail.com>
2021-03-22 13:39:30 +1100

    setup up ts compilation

--- packages/stylelint-config/index.ts
+++ packages/stylelint-config/index.ts
@@ -1,5 +1,5 @@
 module.exports = {
-  plugins: ["./src/use-deprecated-component-library-helpers-scss-imports"],
+  plugins: ["./plugins/use-deprecated-component-library-helpers-scss-imports"],
   rules: {
     "kaizen/use-deprecated-component-library-helpers-scss-imports": true,
   },
---
+++ packages/stylelint-config/plugins/use-deprecated-component-library-helpers-scss-imports.ts
@@ -0,0 +1,55 @@
+// Abbreviated example
+import stylelint from "stylelint"
+
+const ruleName = "kaizen/use-deprecated-component-library-helpers-scss-imports"
+const messages = stylelint.utils.ruleMessages(ruleName, {
+  deprecatedComponentLibraryColorImport:
+    "@kaizen/component-library/styles/color.scss has been deprecated",
+  deprecatedComponentLibraryTypeImport:
+    "@kaizen/component-library/styles/type.scss has been deprecated",
+  deprecatedComponentLibraryLayoutImport:
+    "@kaizen/component-library/styles/layout.scss has been deprecated",
+})
+
+module.exports = stylelint.createPlugin(
+  ruleName,
+  (primaryOption: any) => (postcssRoot: any, postcssResult: any) => {
+    const validOptions = stylelint.utils.validateOptions(
+      postcssResult,
+      ruleName
+    )
+
+    if (!validOptions) {
+      return
+    }
+
+    if (postcssRoot.source.lang !== "scss") {
+      return
+    }
+    // console.log(postcssRoot)
+    // console.log(postcssRoot.source.input.css)
+    // console.log(postcssRoot.source.lang)
+
+    // console.log("running stylelint plugin by michael")
+
+    for (const node of postcssRoot.nodes) {
+      if (
+        node.type === "atrule" &&
+        node.name === "import" &&
+        node.params === '"~@kaizen/component-library/styles/color"'
+      ) {
+        stylelint.utils.report({
+          message: messages.deprecatedComponentLibraryColorImport,
+          node,
+          result: postcssResult,
+          ruleName,
+        })
+      }
+    }
+
+    // ... some logic ...
+  }
+)
+
+module.exports.ruleName = ruleName
+module.exports.messages = messages
