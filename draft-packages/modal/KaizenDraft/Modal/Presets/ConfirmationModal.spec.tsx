d00d178cbfc9f2b29fa627bab9773ec17d096daf
actuallyacat <allanna.beaton@gmail.com>
2021-03-24 10:01:13 +1100

    Added tests to Confirmation modal

---
+++ draft-packages/modal/KaizenDraft/Modal/Presets/ConfirmationModal.spec.tsx
@@ -0,0 +1,97 @@
+import { cleanup, render, fireEvent } from "@testing-library/react"
+import * as React from "react"
+import ConfirmationModal, { ConfirmationModalProps } from "./ConfirmationModal"
+
+afterEach(cleanup)
+
+const ConfirmationModalWrapper = (props: Partial<ConfirmationModalProps>) => (
+  <ConfirmationModal
+    type="informative"
+    isOpen={true}
+    title="Example Modal Title"
+    onDismiss={() => undefined}
+    onConfirm={() => undefined}
+    children="Example Modal body"
+    {...props}
+  />
+)
+
+describe("<ConfirmationModal />", () => {
+  it("renders an open modal with the provided content", () => {
+    const { getByText } = render(
+      <ConfirmationModalWrapper>
+          Example modal body
+      </ConfirmationModalWrapper>
+    )
+    expect(getByText("Example modal body")).toBeTruthy()
+
+    // Confirm/Cancel are modal footer CTAs
+    expect(getByText("Confirm")).toBeTruthy()
+    expect(getByText("Cancel")).toBeTruthy()
+  })
+
+  it("renders a modal in a \"working\" state", () => {
+    const { getByText, queryByText } = render(
+      <ConfirmationModalWrapper
+        confirmWorking={{
+          label: "Submitting",
+          labelHidden: false
+        }}>
+          Example modal body
+      </ConfirmationModalWrapper>
+    )
+    expect(getByText("Example modal body")).toBeTruthy()
+
+    // Replaced by the user-submitted prop
+    expect(getByText("Submitting")).toBeTruthy()
+    expect(getByText("Cancel")).toBeTruthy()
+
+    // default "confirm" CTA should not be replaced by the above working state label
+    expect(queryByText("Confirm")).toBeNull()
+  })
+
+  it("closes the modal when escape key pressed", () => {
+    const handleConfirm = jest.fn()
+    const handleDismiss = jest.fn()
+    const document = render(
+      <ConfirmationModalWrapper
+        onDismiss={handleDismiss}
+        onConfirm={handleConfirm}>
+          Example modal body
+      </ConfirmationModalWrapper>
+    )
+    fireEvent.keyUp(document.container, { key: "Escape", code: "Escape" })
+    expect(handleDismiss).toHaveBeenCalledTimes(1)
+    expect(handleConfirm).toHaveBeenCalledTimes(0)
+  })
+
+  it("closes the modal when cancel button is pressed", () => {
+    const handleConfirm = jest.fn()
+    const handleDismiss = jest.fn()
+    const { getByText } = render(
+      <ConfirmationModalWrapper
+        onDismiss={handleDismiss}
+        onConfirm={handleConfirm}>
+          Example modal body
+      </ConfirmationModalWrapper>
+    )
+    fireEvent.click(getByText(/Cancel/i))
+    expect(handleConfirm).toHaveBeenCalledTimes(0)
+    expect(handleDismiss).toHaveBeenCalledTimes(1)
+  })
+
+  it("closes the modal when confirm button is pressed", () => {
+    const handleConfirm = jest.fn()
+    const handleDismiss = jest.fn()
+    const { getByText } = render(
+      <ConfirmationModalWrapper
+        onDismiss={handleDismiss}
+        onConfirm={handleConfirm}>
+          Example modal body
+      </ConfirmationModalWrapper>
+    )
+    fireEvent.click(getByText(/Confirm/i))
+    expect(handleConfirm).toHaveBeenCalledTimes(1)
+    expect(handleDismiss).toHaveBeenCalledTimes(0)
+  })
+})
