f62d33f342f2a165cc148caabfdf7aec0af0c4ed
Max Wheeler <max@makenosound.com>
2021-03-24 16:39:20 +1100

    Update stories/docs

--- packages/component-library/stories/ToastNotification.stories.tsx
+++ packages/component-library/stories/ToastNotification.stories.tsx
@@ -1,8 +1,18 @@
 import * as React from "react"
+import { Button } from "@kaizen/draft-button"
 import { Link, ZenNavigationBar } from "@kaizen/draft-zen-navigation-bar"
 import { TitleBlockZen } from "@kaizen/draft-title-block-zen"
-import { ToastNotification } from "@kaizen/component-library"
+import {
+  addToastNotification,
+  removeToastNotification,
+  clearToastNotifications,
+  Box,
+  ToastNotification,
+} from "@kaizen/component-library"
+import { Container, Content } from "@kaizen/draft-page-layout"
+import { ToastNotificationWithOptionals } from "@kaizen/component-library/components/Notification/types"
 import { withDesign } from "storybook-addon-designs"
+import { v4 } from "uuid"
 import { figmaEmbed } from "../../../storybook/helpers"
 import styles from "./ToastNotification.stories.scss"
 
@@ -38,13 +48,52 @@ const withNavigation = (Story: React.FunctionComponent) => (
   </>
 )
 
+const Triggers = ({
+  notifications,
+}: {
+  notifications: ToastNotificationWithOptionals[]
+}) => {
+  const [local] = React.useState(notifications.map(n => ({ ...n, id: v4() })))
+
+  return (
+    <Container>
+      <Content>
+        <Box py={1} classNameAndIHaveSpokenToDST={styles.triggerContainer}>
+          <Box mr={0.25}>
+            <Button
+              label={`Show notification${local.length > 1 ? "s" : ""}`}
+              onClick={() => {
+                local.forEach(addToastNotification)
+              }}
+            />
+          </Box>
+          <Box mr={0.25}>
+            <Button
+              label="Remove"
+              onClick={() => {
+                local.forEach(({ id }) => removeToastNotification(id))
+              }}
+            />
+          </Box>
+          <Button
+            label="Clear"
+            onClick={() => {
+              clearToastNotifications()
+            }}
+          />
+        </Box>
+      </Content>
+    </Container>
+  )
+}
+
 export default {
   title: "ToastNotification (React)",
   component: ToastNotification,
   parameters: {
     info: {
       text: `
-        import { ToastNotification } from "@kaizen/component-library";
+        import { addToastNotification, removeToastNotification, clearToastNotifications } from "@kaizen/component-library";
       `,
     },
     ...figmaEmbed(
@@ -54,117 +103,168 @@ export default {
   decorators: [withDesign, withNavigation],
 }
 
-export const PositiveKaizenSiteDemo = () => (
-  <ToastNotification
-    type="affirmative"
-    title="Success"
-    automationId="notification1"
-  >
-    New user data, imported by mackenzie@hooli.com has successfully uploaded.{" "}
-    <a href="/">Manage users is now available</a>
-  </ToastNotification>
-)
+export const PositiveKaizenSiteDemo = () => {
+  React.useEffect(() => {
+    addToastNotification({
+      type: "affirmative",
+      title: "Success",
+      automationId: "notification1",
+      message: (
+        <>
+          New user data, imported by mackenzie@hooli.com has successfully
+          uploaded. <a href="/">Manage users is now available</a>
+        </>
+      ),
+    })
+  })
+  return null
+}
 
 PositiveKaizenSiteDemo.storyName = "Positive (Kaizen Site Demo)"
 
 export const PositiveAutohide = () => (
-  <ToastNotification
-    type="affirmative"
-    title="Success"
-    autohide
-    automationId="notification1"
-  >
-    New user data, imported by mackenzie@hooli.com has successfully uploaded.{" "}
-    <a href="/">Manage users is now available</a>
-  </ToastNotification>
+  <Triggers
+    notifications={[
+      {
+        type: "affirmative",
+        title: "Success",
+        automationId: "notification1",
+        message: (
+          <>
+            New user data, imported by mackenzie@hooli.com has successfully
+            uploaded. <a href="/">Manage users is now available</a>
+          </>
+        ),
+      },
+    ]}
+  />
 )
 
 PositiveAutohide.storyName = "Positive, Autohide"
 
 export const PositiveAutohideHideCloseIcon = () => (
-  <ToastNotification
-    automationId="notification1"
-    type="affirmative"
-    title="Success"
-    autohide
-    hideCloseIcon
-  >
-    New user data, imported by mackenzie@hooli.com has successfully uploaded.{" "}
-    <a href="/">Manage users is now available</a>
-  </ToastNotification>
+  <Triggers
+    notifications={[
+      {
+        automationId: "notification1",
+        type: "affirmative",
+        title: "Success",
+        message: (
+          <>
+            New user data, imported by mackenzie@hooli.com has successfully
+            uploaded. <a href="/">Manage users is now available</a>
+          </>
+        ),
+        persistent: true,
+      },
+    ]}
+  />
 )
 
 PositiveAutohideHideCloseIcon.storyName = "Positive, Autohide, Hide Close Icon"
 
 export const Informative = () => (
-  <ToastNotification
-    type="informative"
-    title="Informative"
-    automationId="notification1"
-  >
-    New user data is currently being processed. We'll let you know when the
-    process is completed. <a href="/">Manage users</a>
-  </ToastNotification>
+  <Triggers
+    notifications={[
+      {
+        automationId: "notification1",
+        type: "informative",
+        title: "Informative",
+        message: (
+          <>
+            New user data is currently being processed. We’ll let you know when
+            the process is completed. <a href="/">Manage users</a>
+          </>
+        ),
+      },
+    ]}
+  />
 )
 
 export const Cautionary = () => (
-  <ToastNotification
-    type="cautionary"
-    title="Warning"
-    automationId="notification1"
-  >
-    New user data, imported by mackenzie@hooli.com has uploaded with some minor
-    issues. <a href="/">View issues</a>
-  </ToastNotification>
+  <Triggers
+    notifications={[
+      {
+        automationId: "notification1",
+        type: "cautionary",
+        title: "Warning",
+        message: (
+          <>
+            New user data, imported by mackenzie@hooli.com has uploaded with
+            some minor issues. <a href="/">View issues</a>
+          </>
+        ),
+      },
+    ]}
+  />
 )
 
 export const Negative = () => (
-  <ToastNotification
-    type="negative"
-    title="No network connection"
-    automationId="notification1"
-  >
-    Check your connection and try again. <a href="/">Refresh</a>.
-  </ToastNotification>
+  <Triggers
+    notifications={[
+      {
+        automationId: "notification1",
+        type: "negative",
+        title: "No network connection",
+        message: (
+          <>
+            Check your connection and try again. <a href="/">Refresh</a>.
+          </>
+        ),
+      },
+    ]}
+  />
 )
 
 export const MultipleNotifications = () => (
-  <div>
-    <ToastNotification
-      type="affirmative"
-      title="Success"
-      automationId="notification1"
-    >
-      New user data, imported by mackenzie@hooli.com has successfully uploaded.{" "}
-      <a href="/">Manage users is now available</a>
-    </ToastNotification>
-    <ToastNotification
-      type="informative"
-      title="Informative"
-      automationId="notification2"
-      autohide
-      hideCloseIcon
-    >
-      New user data is currently being processed. We'll let you know when the
-      process is completed. <a href="/">Manage users</a>
-    </ToastNotification>
-    <ToastNotification
-      type="cautionary"
-      title="Warning"
-      automationId="notification3"
-      autohide
-    >
-      New user data, imported by mackenzie@hooli.com has uploaded with some
-      minor issues. <a href="/">View issues</a>
-    </ToastNotification>
-    <ToastNotification
-      type="negative"
-      title="No network connection"
-      automationId="notification4"
-    >
-      Check your connection and try again. <a href="/">Refresh</a>.
-    </ToastNotification>
-  </div>
+  <Triggers
+    notifications={[
+      {
+        automationId: "notification1",
+        type: "affirmative",
+        title: "Success",
+        message: (
+          <>
+            New user data, imported by mackenzie@hooli.com has successfully
+            uploaded. <a href="/">Manage users is now available</a>
+          </>
+        ),
+      },
+      {
+        automationId: "notification2",
+        type: "informative",
+        title: "Informative",
+        message: (
+          <>
+            New user data is currently being processed. We’ll let you know when
+            the process is completed. <a href="/">Manage users</a>
+          </>
+        ),
+        persistent: true,
+      },
+      {
+        automationId: "notification3",
+        type: "cautionary",
+        title: "Warning",
+        message: (
+          <>
+            New user data, imported by mackenzie@hooli.com has uploaded with
+            some minor issues. <a href="/">View issues</a>
+          </>
+        ),
+      },
+      {
+        automationId: "notification4",
+        type: "negative",
+        title: "No network connection",
+        message: (
+          <>
+            Check your connection and try again. <a href="/">Refresh</a>.
+          </>
+        ),
+      },
+    ]}
+  />
 )
 
 MultipleNotifications.storyName = "Multiple notifications"
