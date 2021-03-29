8496aef333568e471019b9f73349b24d165a9c54
Max Wheeler <max.wheeler@cultureamp.com>
2021-03-30 10:58:09 +1100

    fix: Deprecate ToastNotification component (#1335)

--- packages/component-library/components/Notification/ToastNotification.tsx
+++ packages/component-library/components/Notification/ToastNotification.tsx
@@ -1,5 +1,6 @@
 import * as React from "react"
 import { v4 } from "uuid"
+import { withDeprecatedComponent } from "@kaizen/react-deprecate-warning"
 import { addToastNotification } from "./ToastNotificationManager"
 import { ToastNotificationWithOptionals } from "./types"
 
@@ -32,4 +33,7 @@ const ToastNotification = ({
   return null
 }
 
-export default ToastNotification
+export default withDeprecatedComponent(ToastNotification, {
+  warning:
+    "ToastNotification component API is deprecated. Use addToastNotification from @kaizen/component-library instead.",
+})
