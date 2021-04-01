9da508f03a87ae3077d4802b57d92ac0cf2a745a
Steph Westgarth <stephwestgarth@gmail.com>
2021-04-01 15:02:44 +1100

    Added documentation for new accessibility helpers

---
+++ site/src/docs-components/color/HeartAccessibilityHelpers.tsx
@@ -0,0 +1,11 @@
+import accessibilityHelpers from "!raw-loader!@kaizen/design-tokens/sass/accessibility.scss"
+import * as React from "react"
+import Code from "../Code"
+
+class HeartAccessibilityHelpers extends React.PureComponent {
+  render() {
+    return <Code language="scss">{accessibilityHelpers}</Code>
+  }
+}
+
+export default HeartAccessibilityHelpers
