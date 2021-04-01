c7130dd9f425a8494d17d4932639a34d0948c713
Steph Westgarth <stephwestgarth@gmail.com>
2021-04-01 15:04:32 +1100

    Satisfied linter in doc class for accessibility

--- site/src/docs-components/color/HeartAccessibilityHelpers.tsx
+++ site/src/docs-components/color/HeartAccessibilityHelpers.tsx
@@ -1,6 +1,6 @@
-import accessibilityHelpers from "!raw-loader!@kaizen/design-tokens/sass/accessibility.scss"
 import * as React from "react"
 import Code from "../Code"
+import accessibilityHelpers from "!raw-loader!@kaizen/design-tokens/sass/accessibility.scss"
 
 class HeartAccessibilityHelpers extends React.PureComponent {
   render() {
