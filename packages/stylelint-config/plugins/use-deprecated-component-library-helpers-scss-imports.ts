4182e1aca3e467b4cf63a9cd18d3f6d3696c5104
Michael Bylstra <mbylstra@gmail.com>
2021-04-12 13:48:02 +1000

    implement --fix mode of deprecated-component-library-helpers eslint plugin

--- packages/stylelint-config/plugins/use-deprecated-component-library-helpers-scss-imports.ts
+++ packages/stylelint-config/plugins/use-deprecated-component-library-helpers-scss-imports.ts
@@ -1,6 +1,6 @@
 // Abbreviated example
 import stylelint from "stylelint"
-
+import { AtRule } from "postcss"
 const ruleName = "kaizen/use-deprecated-component-library-helpers-scss-imports"
 const messages = stylelint.utils.ruleMessages(ruleName, {
   deprecatedComponentLibraryColorImport:
@@ -11,45 +11,92 @@ const messages = stylelint.utils.ruleMessages(ruleName, {
     "@kaizen/component-library/styles/layout.scss has been deprecated",
 })
 
-module.exports = stylelint.createPlugin(
-  ruleName,
-  (primaryOption: any) => (postcssRoot: any, postcssResult: any) => {
-    const validOptions = stylelint.utils.validateOptions(
-      postcssResult,
-      ruleName
-    )
+type ImportToReplace = {
+  oldImport: string
+  newImport: string
+  message: any
+}
 
-    if (!validOptions) {
-      return
-    }
+const importsToReplace: ImportToReplace[] = [
+  {
+    oldImport: '"~@kaizen/component-library/styles/type"',
+    newImport: '"~@kaizen/deprecated-component-library-helpers/styles/type"',
+    message: messages.deprecatedComponentLibraryTypeImport,
+  },
+  {
+    oldImport: '"~@kaizen/component-library/styles/color"',
+    newImport: '"~@kaizen/deprecated-component-library-helpers/styles/color"',
+    message: messages.deprecatedComponentLibraryColorImport,
+  },
+  {
+    oldImport: '"~@kaizen/component-library/styles/layout"',
+    newImport: '"~@kaizen/deprecated-component-library-helpers/styles/layout"',
+    message: messages.deprecatedComponentLibraryLayoutImport,
+  },
+]
 
-    if (postcssRoot.source.lang !== "scss") {
-      return
-    }
-    // console.log(postcssRoot)
-    // console.log(postcssRoot.source.input.css)
-    // console.log(postcssRoot.source.lang)
+const makeRuleFunction = (
+  primaryOption: any,
+  secondaryOptionsObject: any,
+  context: any
+) => (postcssRoot: any, postcssResult: any) => {
+  const validOptions = stylelint.utils.validateOptions(postcssResult, ruleName)
 
-    // console.log("running stylelint plugin by michael")
+  if (!validOptions) {
+    return
+  }
 
-    for (const node of postcssRoot.nodes) {
-      if (
-        node.type === "atrule" &&
-        node.name === "import" &&
-        node.params === '"~@kaizen/component-library/styles/color"'
-      ) {
-        stylelint.utils.report({
-          message: messages.deprecatedComponentLibraryColorImport,
-          node,
-          result: postcssResult,
-          ruleName,
-        })
+  if (postcssRoot.source.lang !== "scss") {
+    return
+  }
+
+  for (const importToReplace of importsToReplace) {
+    replaceImport(context, postcssRoot, postcssResult, importToReplace)
+  }
+}
+
+const replaceImport = (
+  context: any,
+  postcssRoot: any,
+  postcssResult: any,
+  { oldImport, newImport, message }: ImportToReplace
+) => {
+  const importsToProcess: AtRule[] = []
+  let containsOldImport = false
+  postcssRoot.walkAtRules((node: AtRule) => {
+    if (node.name === "import") {
+      if ([oldImport, newImport].includes(node.params)) {
+        importsToProcess.push(node)
+        if (node.params.includes(oldImport)) {
+          containsOldImport = true
+          if (!context.fix) {
+            stylelint.utils.report({
+              message,
+              node,
+              result: postcssResult,
+              ruleName,
+            })
+          }
+        }
       }
     }
+  })
 
-    // ... some logic ...
+  if (context.fix && containsOldImport) {
+    const [lastImport, ...importsToRemove] = importsToProcess.reverse()
+
+    // the last import, whether it's an old or new import, should be
+    // changed to a new import
+    lastImport.params = newImport
+
+    for (const importToRemove of importsToRemove) {
+      // we only want one import, so remove all earlier imports
+      importToRemove.remove()
+    }
   }
-)
+}
+
+module.exports = stylelint.createPlugin(ruleName, makeRuleFunction)
 
 module.exports.ruleName = ruleName
 module.exports.messages = messages
