950890ad75757891fa01890db76433742db41f50
Ally Beaton <allanna.beaton@gmail.com>
2021-04-13 18:07:55 +1000

    fix: Update environment check to not use optional chaining (#1427)

--- packages/react-deprecate-warning/src/util.ts
+++ packages/react-deprecate-warning/src/util.ts
@@ -1,5 +1,12 @@
+/**
+ * All our consumers use Webpack's DefinePlugin to emulate process.env
+ * This either generates a string, "process.env.NODE_ENV", or an object
+ * process.env with properties such that they can be referenced like so
+ * process.env["NODE_ENV"]. This makes it safe to not need to check if
+ * process.env is a valid object.
+ */
 export const shouldWarn =
-  process.env?.NODE_ENV !== undefined && process.env?.NODE_ENV !== "production"
+  process.env.NODE_ENV !== undefined && process.env.NODE_ENV !== "production"
 
 const log = (deprecatedType: "component" | "prop") => (
   name: string,
