1584b13d3df3cf4f4e76a59de12a1910cddd6534
actuallyacat <allanna.beaton@gmail.com>
2021-04-21 09:58:21 +1000

    Added iron to color palette

--- site/src/docs-components/color/ZenNeutrals.tsx
+++ site/src/docs-components/color/ZenNeutrals.tsx
@@ -8,6 +8,7 @@ const ZenNeutrals = () => {
       // this order will be reversed before being displayed
       colors={{
         Slate: color.slate,
+        Iron: color.iron,
         Ash: color.ash,
         Stone: color.stone,
         White: color.white,
