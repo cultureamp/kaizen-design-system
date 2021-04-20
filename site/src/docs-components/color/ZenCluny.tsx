8638bbf10712e9842fa447584302e3cc53292270
actuallyacat <allanna.beaton@gmail.com>
2021-04-20 16:25:54 +1000

    Update color in guideline page

--- site/src/docs-components/color/ZenCluny.tsx
+++ site/src/docs-components/color/ZenCluny.tsx
@@ -1,7 +1,9 @@
-import colorTokens from "@kaizen/design-tokens/tokens/color.json"
 import * as React from "react"
-import ZenColorGroup from "./ZenColorGroup"
+import ZenColorGroup, { ColorThemeContext } from "./ZenColorGroup"
 
-const ZenCluny = () => <ZenColorGroup colors={colorTokens.kz.color.cluny} />
+const ZenCluny = () => {
+  const { color } = React.useContext(ColorThemeContext)
+  return <ZenColorGroup colors={color.cluny} />
+}
 
 export default ZenCluny
--- site/src/docs-components/color/ZenColorGroup.tsx
+++ site/src/docs-components/color/ZenColorGroup.tsx
@@ -1,4 +1,5 @@
 import * as React from "react"
+import { heartTheme, Theme } from "@kaizen/design-tokens"
 import ZenColor from "./ZenColor"
 
 const colorGroupStyles: React.CSSProperties = {
@@ -8,14 +9,21 @@ const colorGroupStyles: React.CSSProperties = {
   justifyContent: "space-between",
 }
 
-const ZenColorGroup = ({ colors }) => (
-  <div style={colorGroupStyles}>
-    {Object.keys(colors)
-      .reverse()
-      .map(c => (
-        <ZenColor key={colors[c]} name={c} value={colors[c]} />
-      ))}
-  </div>
-)
+// Use custom theme context as we do not want to write CSS vars to the DOM
+export const ColorThemeContext = React.createContext<Theme>(heartTheme)
+
+const ZenColorGroup = ({ colors }) => {
+  return (
+    <ColorThemeContext.Provider value={heartTheme}>
+      <div style={colorGroupStyles}>
+        {Object.keys(colors)
+          .reverse()
+          .map(c => (
+            <ZenColor key={colors[c]} name={c} value={colors[c]} />
+          ))}
+      </div>
+    </ColorThemeContext.Provider>
+  )
+}
 
 export default ZenColorGroup
