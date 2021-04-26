14df2bef851f7f52e811e48fd507d03d665e3189
Ally Beaton <allanna.beaton@gmail.com>
2021-04-26 16:00:38 +1000

    feat: Added Heart spot illustrations (#1473)
    feat: Added Heart spot illustrations

---
+++ draft-packages/illustration/KaizenDraft/Illustration/illustration.spec.tsx
@@ -0,0 +1,38 @@
+import { cleanup, render } from "@testing-library/react"
+import * as React from "react"
+import * as SpotIllustrations from "./Spot"
+import * as SceneIllustrations from "./Scene"
+
+afterEach(cleanup)
+
+describe("<Illustration />", () => {
+  describe("Spot", () => {
+    Object.keys(SpotIllustrations).forEach(componentName => {
+      const Component: (props: SpotIllustrations.SpotProps) => JSX.Element =
+        SpotIllustrations[componentName]
+
+      it(`${componentName} should exist and render an alt tag`, () => {
+        const altTitle = "My accessible title"
+        const wrapper = render(<Component alt={altTitle} />)
+
+        expect(wrapper.getByAltText(altTitle)).toBeTruthy()
+        expect(wrapper.container).toMatchSnapshot()
+      })
+    })
+  })
+
+  describe("Scene", () => {
+    Object.keys(SceneIllustrations).forEach(componentName => {
+      const Component: (props: SceneIllustrations.SceneProps) => JSX.Element =
+        SceneIllustrations[componentName]
+
+      it(`${componentName} should exist and render an alt tag`, () => {
+        const altTitle = "My accessible title"
+        const wrapper = render(<Component alt={altTitle} />)
+
+        expect(wrapper.getByAltText(altTitle)).toBeTruthy()
+        expect(wrapper.container).toMatchSnapshot()
+      })
+    })
+  })
+})
