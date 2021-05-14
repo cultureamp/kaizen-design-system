783afcf8e1d42c9a8e86395325d18c70c5aab309
Oli Bridgman <oli.bridgman@cultureamp.com>
2021-05-15 11:04:46 +1200

    Add mock function for deprecated console warning.

--- packages/component-library/components/Notification/ToastNotification.spec.tsx
+++ packages/component-library/components/Notification/ToastNotification.spec.tsx
@@ -3,7 +3,9 @@ import * as React from "react"
 import ToastNotification from "./ToastNotification"
 
 afterEach(cleanup)
-
+beforeEach(() => {
+  jest.spyOn(global.console, "warn").mockImplementation(jest.fn())
+})
 test("The basic notification renders correctly", () => {
   const { getByRole } = render(
     <ToastNotification type="affirmative" title="Success">
