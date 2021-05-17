735389f62fa7896412e776f41a3d3ecb5bb0a1f1
OliBridgman <oli.bridgman@gmail.com>
2021-05-17 17:47:37 +1200

    fix: toast unmounted set state (#1553)
    * fix: Check component is mounted before we call setState
    
    * fix: remove the onHide timeout if we manually hide.
    
    If the user hides the notification with the cancel button, we need
    to clear the timeout that still exists.

--- packages/component-library/components/Notification/components/GenericNotification.spec.tsx
+++ packages/component-library/components/Notification/components/GenericNotification.spec.tsx
@@ -27,7 +27,7 @@ test('Begins "hidden" but transitions out of it immediately', async () => {
 
 test("The cancel button hides the notification and triggers the onHide callback", async () => {
   const onHide = jest.fn()
-  const { container } = render(
+  const { container, getByTitle } = render(
     <GenericNotification
       type="affirmative"
       style="inline"
@@ -42,12 +42,15 @@ test("The cancel button hides the notification and triggers the onHide callback"
   expect(container.querySelector(".hidden")).toBeTruthy()
 
   // After clicking, the element should fade out, but the onHide not trigger yet.
-  const cancelButton = container.querySelector(".cancel")
-  cancelButton && fireEvent.click(cancelButton)
+  const cancelButton = getByTitle("close notification")
+
+  fireEvent.click(cancelButton)
 
   const notification = container.querySelector(".notification")
 
-  await waitFor(() => expect(onHide).toHaveBeenCalledTimes(0))
+  await waitFor(() => {
+    expect(notification).toBeTruthy()
+  })
 
   // Cannot use @testing-library/react `fireEvent` as it relies on jsdom events
   // TransitionEvent has not been implemented yet, using `ReactTestUtils.Simulate` is a workaround
@@ -56,13 +59,14 @@ test("The cancel button hides the notification and triggers the onHide callback"
       propertyName: "margin-top",
     } as any)
 
-  // TODO - This test is really flakey. Needs to be fixed or re-written
-  // After the fade out animation has finished, the onHide handler should trigger.
-  // await waitFor(() => expect(onHide).toHaveBeenCalledTimes(1))
+  await waitFor(() => {
+    expect(container.querySelector(".notification")).toBeFalsy()
+  })
+  await waitFor(() => expect(onHide).toHaveBeenCalledTimes(1))
 })
 
 test("If autohide is specified, we should start hiding after 5s", async () => {
-  const { container, queryByText } = render(
+  const { container } = render(
     <GenericNotification
       type="affirmative"
       style="toast"
