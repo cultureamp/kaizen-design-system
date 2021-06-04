96a11788e9d529292141d6bc8282ec9ea53b4142
actuallyacat <allanna.beaton@gmail.com>
2021-06-04 10:35:13 +1000

    Abstract polyfils into separate file so they can be used independantly of the wrapper (if needed)

--- packages/bootstrap/lib/Wrapper.tsx
+++ packages/bootstrap/lib/Wrapper.tsx
@@ -2,22 +2,9 @@ import * as React from "react"
 import { ThemeManager } from "@kaizen/design-tokens"
 import { themeSwitcher, getCurrentTheme } from "@cultureamp/theme-switcher"
 
-// Polyfill for :focus-visible pseudo-selector
-// See: https://github.com/WICG/focus-visible
-import "focus-visible"
-
-// Standard base stylesheet used across Culture Amp products
-// See: https://github.com/necolas/normalize.css/
-import "normalize.css"
-
-// Global styles
-import "./globals.module.scss"
-
-// Supported / active face declarations
-import "./fonts.module.scss"
+import "./polyfils"
 
 // Initialise the ThemeManager, this is a singleton so this should not be redeclared
-export const themeManager = new ThemeManager(getCurrentTheme())
-themeSwitcher({ themeManager })
+export const themeManager = new ThemeManager()
 
 export default ({ children }: { children?: React.ReactNode }) => <>{children}</>
---
+++ packages/bootstrap/lib/polyfils.ts
@@ -0,0 +1,13 @@
+// Polyfill for :focus-visible pseudo-selector
+// See: https://github.com/WICG/focus-visible
+import "focus-visible"
+
+// Standard base stylesheet used across Culture Amp products
+// See: https://github.com/necolas/normalize.css/
+import "normalize.css"
+
+// Global styles
+import "./globals.module.scss"
+
+// Supported / active face declarations
+import "./fonts.module.scss"
