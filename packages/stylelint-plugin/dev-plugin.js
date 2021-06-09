5045ce205674b7c6928db0edfd9b6f78cafaa2c0
Jason O'Neil <jason.oneil@gmail.com>
2021-06-09 15:18:23 +0800

    Improve the comment on dev-plugin.js (matching dev-config.js)

--- packages/stylelint-plugin/dev-plugin.js
+++ packages/stylelint-plugin/dev-plugin.js
@@ -1,5 +1,5 @@
-// This is an entrypoint file that you can use in your stylelint.config.js plugins list for local development.
-// The main reason for this is so the plugin works in CI
+// This is an entrypoint file that you can use in Kaizen's stylelint config (see also dev-config.js).
+// This enables us to use the typescript source of the plugin in this repo, rather than relying on a compiled version.
 require("ts-node").register({
   project: require.resolve("./tsconfig.json"),
   transpileOnly: true,
