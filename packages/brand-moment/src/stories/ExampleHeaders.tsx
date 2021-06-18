10f857f56a8a5f4f36c99b3d72555d4044a15de4
Doug MacKenzie <doug.mackenzie@cultureamp.com>
2021-06-18 10:22:20 +1000

    Add example locale switcher to customer focused stories

--- packages/brand-moment/src/stories/ExampleHeaders.tsx
+++ packages/brand-moment/src/stories/ExampleHeaders.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import { assetUrl } from "@kaizen/hosted-assets"
+import { Select } from "@kaizen/draft-select"
 import styles from "./ExampleHeaders.scss"
 
 export const MinimalBasic = () => (
@@ -21,10 +22,26 @@ export const MinimalBasic = () => (
   </div>
 )
 
+const exampleLocales = [
+  { value: "a", label: "English (US English)" },
+  { value: "b", label: "العربية (Arabic)" },
+  { value: "c", label: "беларускі (Belarusian)" },
+  { value: "d", label: "български (Bulgarian)" },
+  { value: "e", label: "čeština (Czech)" },
+]
+
 export const MinimalCustomerFocused = () => (
-  <div className={`${styles.header} ${styles.customerFocused}`}>
+  <div className={styles.headerCustomerFocused}>
     <div className={styles.logoContainer}>
       <img src={assetUrl("brand/enso-default.svg")} alt="Culture Amp" />
+    </div>
+    <div className={styles.rightAlign}>
+      <Select
+        options={exampleLocales}
+        isSearchable={false}
+        defaultValue={exampleLocales[0]}
+        variant="secondary"
+      />
     </div>
   </div>
 )
