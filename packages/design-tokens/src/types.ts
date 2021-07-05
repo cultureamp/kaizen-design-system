37e00213e392a52f7a5403e8e70b07b0d3b22dd5
Daniel Manning <danielmanning213@gmail.com>
2021-07-05 15:49:05 +1000

    feat: Add new Heart color names to design tokens
    This adds new colors that are in sync with Heart/the rebrand (e.g. Wisteria -> Purple).
    It is part of the bigger plan to completely remove the old ones, which we'll do in a later commit.
    The rationale behind having both old and new in the same repo temporarily is that the alternative, pinning a certain version of design tokens in every package which depends on it and doing some release trickery, is a lot more awkward.

--- packages/design-tokens/src/types.ts
+++ packages/design-tokens/src/types.ts
@@ -72,6 +72,9 @@ export type Theme = {
     }
   }
   color: {
+    /**
+     * @deprecated
+     */
     wisteria: {
       "100": string
       "200": string
@@ -82,6 +85,19 @@ export type Theme = {
       "700": string
       "800": string
     }
+    purple: {
+      "100": string
+      "200": string
+      "300": string
+      "400": string
+      "500": string
+      "600": string
+      "700": string
+      "800": string
+    }
+    /**
+     * @deprecated
+     */
     cluny: {
       "100": string
       "200": string
@@ -91,6 +107,18 @@ export type Theme = {
       "600": string
       "700": string
     }
+    blue: {
+      "100": string
+      "200": string
+      "300": string
+      "400": string
+      "500": string
+      "600": string
+      "700": string
+    }
+    /**
+     * @deprecated
+     */
     seedling: {
       "100": string
       "200": string
@@ -100,6 +128,19 @@ export type Theme = {
       "600": string
       "700": string
     }
+
+    green: {
+      "100": string
+      "200": string
+      "300": string
+      "400": string
+      "500": string
+      "600": string
+      "700": string
+    }
+    /**
+     * @deprecated
+     */
     yuzu: {
       "100": string
       "200": string
@@ -109,6 +150,19 @@ export type Theme = {
       "600": string
       "700": string
     }
+
+    yellow: {
+      "100": string
+      "200": string
+      "300": string
+      "400": string
+      "500": string
+      "600": string
+      "700": string
+    }
+    /**
+     * @deprecated
+     */
     coral: {
       "100": string
       "200": string
@@ -118,7 +172,28 @@ export type Theme = {
       "600": string
       "700": string
     }
+    red: {
+      "100": string
+      "200": string
+      "300": string
+      "400": string
+      "500": string
+      "600": string
+      "700": string
+    }
+    /**
+     * @deprecated
+     */
     peach: {
+      "100": string
+      "200": string
+      "300": string
+      "400": string
+      "500": string
+      "600": string
+      "700": string
+    }
+    orange: {
       "100": string
       "200": string
       "300": string
