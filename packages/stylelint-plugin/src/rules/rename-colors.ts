6f8e867e1b3e64c263657b90c287bd6e941fb58d
Daniel Manning <danielmanning213@gmail.com>
2021-07-06 12:09:52 +1000

    Fixing issues and improving readability

--- packages/stylelint-plugin/src/rules/rename-colors.ts
+++ packages/stylelint-plugin/src/rules/rename-colors.ts
@@ -1,4 +1,5 @@
 import { AtRule, Declaration, Root } from "postcss"
+import postcssValueParser from "postcss-value-parser"
 import { usageOfOldColorName } from "../messages"
 import { Options, RuleDefinition } from "../types"
 import {
@@ -21,37 +22,54 @@ const renameRules = [
 export const renameColorsRule: RuleDefinition = {
   name: "rename-colors",
   ruleFunction: (stylesheet: Root, options: Options) => {
-    const handleDeclAndAtRule = (node: Declaration | AtRule) => {
-      const value = node.type === "decl" ? node.value : node.params
-      renameRules.forEach(renameRule => {
-        const [oldName, newName] = renameRule
-        if (value.indexOf(oldName) === -1) return
-        if (options.fix) {
-          const newNode =
-            node.type === "decl"
-              ? node.clone({
-                  value: value.replace(new RegExp(oldName, "g"), newName),
-                })
-              : node.clone({
-                  params: value.replace(new RegExp(oldName, "g"), newName),
-                })
-          node.replaceWith(newNode)
-        } else {
-          options.reporter({
-            message: usageOfOldColorName(oldName, newName),
-            node,
-            autofixAvailable: true,
-          })
-        }
+    const handleDeclOrAtRule = (
+      declOrAtRule: Declaration | AtRule,
+      parsedValue: postcssValueParser.ParsedValue
+    ) => {
+      let changesMade = false
+      // Walk through each node within the declaration value. e.g. If the declaration was "prop: $kz-color-wisteria-800 $kz-color-cluny-500", two word nodes would be visited.
+      parsedValue.walk(node => {
+        renameRules.forEach(renameRule => {
+          const [oldName, newName] = renameRule
+
+          // We don't care about nodes that don't contain old names.
+          if (node.value.indexOf(oldName) === -1) return
+
+          // Only make a report if we're not in fix mode.
+          if (options.fix) {
+            // This only changes the postcssValueParser node, which needs to be stringified and used as a new value for the postcss node for changes to be made.
+            node.value = node.value.replace(oldName, newName)
+            changesMade = true
+          } else {
+            options.reporter({
+              message: usageOfOldColorName(oldName, newName),
+              node: declOrAtRule,
+              autofixAvailable: true,
+            })
+          }
+        })
       })
+
+      // Here is where we replace the postcss node with a new modified value, using the modified postcssValueParser nodes.
+      if (options.fix && changesMade) {
+        const newNode =
+          declOrAtRule.type === "decl"
+            ? declOrAtRule.clone({
+                value: postcssValueParser.stringify(parsedValue.nodes),
+              })
+            : declOrAtRule.clone({
+                params: postcssValueParser.stringify(parsedValue.nodes),
+              })
+        declOrAtRule.replaceWith(newNode)
+      }
     }
 
-    walkDeclsWithKaizenTokens(stylesheet, ({ postcssNode }) => {
-      handleDeclAndAtRule(postcssNode)
+    walkDeclsWithKaizenTokens(stylesheet, ({ postcssNode, parsedValue }) => {
+      handleDeclOrAtRule(postcssNode, parsedValue)
     })
 
-    walkAtRulesWithKaizenTokens(stylesheet, ({ postcssNode }) => {
-      handleDeclAndAtRule(postcssNode)
+    walkAtRulesWithKaizenTokens(stylesheet, ({ postcssNode, parsedValue }) => {
+      handleDeclOrAtRule(postcssNode, parsedValue)
     })
   },
 }
