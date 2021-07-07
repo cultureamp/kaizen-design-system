264eaea1248df6a759ec4b8eb8577895d7d20df0
Daniel Manning <danielmanning213@gmail.com>
2021-07-07 15:41:13 +1000

    Fix ZenNeutrals on docs

--- site/src/docs-components/color/ZenNeutrals.tsx
+++ site/src/docs-components/color/ZenNeutrals.tsx
@@ -7,12 +7,12 @@ const ZenNeutrals = () => {
     <ZenColorGroup
       // this order will be reversed before being displayed
       colors={{
-        "600": color.neutral["600"],
-        "500": color.neutral["500"],
-        "400": color.neutral["400"],
-        "300": color.neutral["300"],
-        "250": color.neutral["250"],
-        "200": color.neutral["200"],
+        600: color.neutral["600"],
+        500: color.neutral["500"],
+        400: color.neutral["400"],
+        300: color.neutral["300"],
+        250: color.neutral["250"],
+        200: color.neutral["200"],
         "100 (White)": color.neutral["100"],
       }}
     />
