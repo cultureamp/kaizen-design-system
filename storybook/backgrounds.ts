b52d2214a73b1c809909622e93c867c432b35ab5
Daniel Manning <dmisdm@users.noreply.github.com>
2021-07-07 11:05:22 +1000

    Update backgrounds.ts

--- storybook/backgrounds.ts
+++ storybook/backgrounds.ts
@@ -3,9 +3,9 @@
 import * as tokens from "@kaizen/design-tokens/tokens/color.json"
 
 export const backgrounds = [
-  { name: "Neutral-100", value: tokens.kz.color.neutral["100"] },
-  { name: "Neutral-200", value: tokens.kz.color.neutral["200"] },
-  { name: "Neutral-300", value: tokens.kz.color.neutral["300"] },
+  { name: "Neutral 100 (White)", value: tokens.kz.color.neutral["100"] },
+  { name: "Neutral 200", value: tokens.kz.color.neutral["200"] },
+  { name: "Neutral 300", value: tokens.kz.color.neutral["300"] },
   { name: "Purple 700", value: tokens.kz.color.purple["700"] },
   { name: "Blue 500", value: tokens.kz.color.blue["500"] },
   { name: "Green 500", value: tokens.kz.color.green["500"] },
@@ -16,9 +16,9 @@ export const backgrounds = [
 
 // This is a temporary duplication until we find a way for storybook to like the above ^^ tokens with css variables rather than concretes.
 export const cssVarBackgrounds = [
-  { name: "Neutral-100", value: tokens["kz-var"].color.neutral["100"] },
-  { name: "Neutral-200", value: tokens["kz-var"].color.neutral["200"] },
-  { name: "Neutral-300", value: tokens["kz-var"].color.neutral["300"] },
+  { name: "Neutral 100 (White)", value: tokens["kz-var"].color.neutral["100"] },
+  { name: "Neutral 200", value: tokens["kz-var"].color.neutral["200"] },
+  { name: "Neutral 300", value: tokens["kz-var"].color.neutral["300"] },
   { name: "Purple 700", value: tokens["kz-var"].color.purple["700"] },
   { name: "Blue 500", value: tokens["kz-var"].color.blue["500"] },
   { name: "Green 500", value: tokens["kz-var"].color.green["500"] },
