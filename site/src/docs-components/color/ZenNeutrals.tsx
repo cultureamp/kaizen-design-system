be1ac9c8090fe7f0abf78d209dc52abb84fd7b6f
Daniel Manning <dmisdm@users.noreply.github.com>
2021-07-07 15:03:25 +1000

    Make neutral colors consistent on color guidelines

--- site/src/docs-components/color/ZenNeutrals.tsx
+++ site/src/docs-components/color/ZenNeutrals.tsx
@@ -7,13 +7,13 @@ const ZenNeutrals = () => {
     <ZenColorGroup
       // this order will be reversed before being displayed
       colors={{
-        "Neutral-600": color.neutral["600"],
-        "Neutral-500": color.neutral["500"],
-        "Neutral-400": color.neutral["400"],
-        "Neutral-300": color.neutral["300"],
-        "Neutral-250": color.neutral["250"],
-        "Neutral-200": color.neutral["200"],
-        "Neutral-100 (White)": color.neutral["100"],
+        "600": color.neutral["600"],
+        "500": color.neutral["500"],
+        "400": color.neutral["400"],
+        "300": color.neutral["300"],
+        "250": color.neutral["250"],
+        "200": color.neutral["200"],
+        "100 (White)": color.neutral["100"],
       }}
     />
   )
