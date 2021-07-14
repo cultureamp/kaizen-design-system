07d258a19a8c3be7a9824d3809e3d7b9af73d671
Ally Beaton <allanna.beaton@gmail.com>
2021-07-14 16:18:22 +1000

    docs: Remove unnecessary wrapping of stories (#1754)
    docs: Remove unnecessary wrapping of stories.
    * Where possible, we should use existing layout components (such as Box) to lay out components in stories.
    * Move collapsible stories from within the package, to sit within `docs` (not versioned + packaged)

--- draft-packages/vertical-progress-step/docs/VerticalProgressStep.stories.tsx
+++ draft-packages/vertical-progress-step/docs/VerticalProgressStep.stories.tsx
@@ -2,19 +2,6 @@ import { Paragraph } from "@kaizen/component-library"
 import { VerticalProgressStep } from "@kaizen/draft-vertical-progress-step"
 import * as React from "react"
 
-const StoryContainer = ({ children }: { children: React.ReactNode }) => (
-  <div
-    style={{
-      background: "#fff",
-      margin: "30px",
-      minWidth: "600px",
-      position: "relative",
-    }}
-  >
-    {children}
-  </div>
-)
-
 export default {
   title: "VerticalProgressStep (React)",
   component: VerticalProgressStep.CurrentStep,
@@ -28,59 +15,51 @@ export default {
 }
 
 export const CurrentStepActionable = () => (
-  <StoryContainer>
-    <VerticalProgressStep.CurrentStep
-      name="Stop!"
-      status="actionable"
-      position="start"
-      // eslint-disable-next-line no-console
-      onClick={console.log.bind(console)}
-    >
-      <Paragraph variant="body">Click here to commence the Stop!</Paragraph>
-    </VerticalProgressStep.CurrentStep>
-  </StoryContainer>
+  <VerticalProgressStep.CurrentStep
+    name="Stop!"
+    status="actionable"
+    position="start"
+    // eslint-disable-next-line no-console
+    onClick={console.log.bind(console)}
+  >
+    <Paragraph variant="body">Click here to commence the Stop!</Paragraph>
+  </VerticalProgressStep.CurrentStep>
 )
 
 CurrentStepActionable.storyName = "current step actionable"
 
 export const CurrentStepStarted = () => (
-  <StoryContainer>
-    <VerticalProgressStep.CurrentStep
-      name="Stop!"
-      status="started"
-      position="start"
-      // eslint-disable-next-line no-console
-      onClick={console.log.bind(console)}
-    >
-      <Paragraph variant="body">Stopping...</Paragraph>
-    </VerticalProgressStep.CurrentStep>
-  </StoryContainer>
+  <VerticalProgressStep.CurrentStep
+    name="Stop!"
+    status="started"
+    position="start"
+    // eslint-disable-next-line no-console
+    onClick={console.log.bind(console)}
+  >
+    <Paragraph variant="body">Stopping...</Paragraph>
+  </VerticalProgressStep.CurrentStep>
 )
 
 CurrentStepStarted.storyName = "current step started"
 
 // eslint-disable-next-line no-underscore-dangle
 export const _UpcomingStep = () => (
-  <StoryContainer>
-    <VerticalProgressStep.UpcomingStep name="Collaborate!" position="middle">
-      <Paragraph variant="body">
-        Only once you have stopped may you collaborate
-      </Paragraph>
-    </VerticalProgressStep.UpcomingStep>
-  </StoryContainer>
+  <VerticalProgressStep.UpcomingStep name="Collaborate!" position="middle">
+    <Paragraph variant="body">
+      Only once you have stopped may you collaborate
+    </Paragraph>
+  </VerticalProgressStep.UpcomingStep>
 )
 
 _UpcomingStep.storyName = "upcoming step"
 
 // eslint-disable-next-line no-underscore-dangle
 export const _CompletedStep = () => (
-  <StoryContainer>
-    <VerticalProgressStep.CompletedStep name="Listen!" position="end">
-      <Paragraph variant="body">
-        You have finished listening. Vanilla Ice is proud of you.
-      </Paragraph>
-    </VerticalProgressStep.CompletedStep>
-  </StoryContainer>
+  <VerticalProgressStep.CompletedStep name="Listen!" position="end">
+    <Paragraph variant="body">
+      You have finished listening. Vanilla Ice is proud of you.
+    </Paragraph>
+  </VerticalProgressStep.CompletedStep>
 )
 
 _CompletedStep.storyName = "completed step"
