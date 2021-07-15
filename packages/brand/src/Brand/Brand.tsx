b7fd50d1db152bd48a41531121638f36ebba9bfd
Doug MacKenzie <doug.mackenzie@cultureamp.com>
2021-07-15 16:26:28 +1000

    fix: Constrain brand width to 100% of container (#1763)

--- packages/brand/src/Brand/Brand.tsx
+++ packages/brand/src/Brand/Brand.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import { assetUrl } from "@kaizen/hosted-assets"
+import styles from "./Brand.scss"
 
 export interface BrandProps {
   variant:
@@ -18,6 +19,7 @@ export const Brand = (props: BrandProps) => {
     <img
       src={assetUrl(`brand/${props.variant}${brandTheme}.svg`)}
       alt={props.alt}
+      className={styles.img}
     />
   )
 }
