1dc426b29a3c16302a569c526fc0864534586216
Daniel Manning <danielmanning213@gmail.com>
2021-07-22 10:53:29 +1000

    Fix build

--- storybook/backgrounds.ts
+++ storybook/backgrounds.ts
@@ -1,17 +1,18 @@
 /* eslint import/no-extraneous-dependencies: 0 */
 
 import * as colors from "@kaizen/design-tokens/tokens/color.json"
+import { defaultTheme } from "@kaizen/design-tokens"
 
 export const backgrounds = [
-  { name: "White", value: colors.kz.color.white },
-  { name: "Gray 100", value: colors.kz.color.gray["100"] },
-  { name: "Gray 300", value: colors.kz.color.gray["300"] },
-  { name: "Purple 700", value: colors.kz.color.purple["700"] },
-  { name: "Blue 500", value: colors.kz.color.blue["500"] },
-  { name: "Green 500", value: colors.kz.color.green["500"] },
-  { name: "Yellow 500", value: colors.kz.color.yellow["500"] },
-  { name: "Orange 500", value: colors.kz.color.orange["500"] },
-  { name: "Red 500", value: colors.kz.color.red["500"] },
+  { name: "White", value: defaultTheme.color.white },
+  { name: "Gray 100", value: defaultTheme.color.gray["100"] },
+  { name: "Gray 300", value: defaultTheme.color.gray["300"] },
+  { name: "Purple 700", value: defaultTheme.color.purple["700"] },
+  { name: "Blue 500", value: defaultTheme.color.blue["500"] },
+  { name: "Green 500", value: defaultTheme.color.green["500"] },
+  { name: "Yellow 500", value: defaultTheme.color.yellow["500"] },
+  { name: "Orange 500", value: defaultTheme.color.orange["500"] },
+  { name: "Red 500", value: defaultTheme.color.red["500"] },
 ]
 
 // This is a temporary duplication until we find a way for storybook to like the above ^^ tokens with css variables rather than concretes.
