33411db1d02e09539dea98ec43e5183815c9974b
Daniel Manning <dmisdm@users.noreply.github.com>
2021-08-06 16:24:52 +1000

    feat: Deprecated and removed tokens rule uplift (#1743)
    * feat: Rename colors stylelint rule
    
    A rule that allows users to automatically detect and fix colors that will be renamed in the future.
    
    * Code dedup
    
    * Quick comment
    
    * Expose the rule
    
    * Expose rule in default config
    
    * Fixing issues and improving readability
    
    * Test file clean
    
    * Update packages/stylelint-plugin/src/rules/rename-colors.ts
    
    * Handle neutral colors
    
    * Update readme with new rule
    
    * Make rename rules more specific to prevent accidents
    
    * Forgot to add seedling!
    
    * Add extra abilities to rename tokens rule
    
    - Remove kz-var prefix
    - Replace -rgb-params with -rgb
    - Repalce kz-var-id-* with *-id
    
    * Update packages/stylelint-plugin/README.md
    
    Co-authored-by: Jason O'Neil <jason.oneil@gmail.com>
    
    * Update packages/stylelint-plugin/src/messages.ts
    
    Co-authored-by: Jason O'Neil <jason.oneil@gmail.com>
    
    * Remove test/play files
    
    * no-deprecated-tokens rule extended to support new gen
    
    * Impl method 4 of knowing about deprecated tokens
    
    Fix any flow on effects within rules
    
    * Cleanup
    
    * Allow console logs in test
    
    * Update readme
    
    * Add comment to buildRemovedTokens
    
    * Update packages/stylelint-plugin/README.md
    
    * Update packages/stylelint-plugin/README.md
    
    * Remove test.scss
    
    * Apply suggestions from code review
    
    * Update packages/stylelint-plugin/src/types.ts
    
    * Update packages/stylelint-plugin/src/util/variableUtils.ts
    
    * Format removedTokens during generation
    
    * Extra comments
    
    * Run yarn to ensure correct lockfile
    
    * Cleanup
    
    * More comments and cleanup
    
    * Update packages/stylelint-plugin/README.md
    
    Co-authored-by: Jason O'Neil <jason.oneil@gmail.com>
    
    * Clear up a comment
    
    * Merge invalid-funcs and deprecated-tokens
    
    Merge no-invalid-use-of-var-tokens-in-functions and no-deprecated-or-removed-tokens rules for better maintainability, and coherence in the rules' responsibilities.
    
    Also build the list of deprecated tokens and their replacements in a way that is more understandable.
    
    * Cleanup
    
    * README update
    
    * Polish buildDeprecatedTokens script
    
    * Move prettier to devDependencies
    
    * Fix typo in messages
    
    * Extra comment
    
    * Inline stylelint plugin test config
    
    * Support migrating deprecated tokens when the previous value was already a CSS variable or it's replacement is not
    
    * Update message
    
    * Cleanup separation of concerns
    
    no-invalid-use-of-var-tokens-in-equations should care only about whether a value is a CSS variable. Previously, it would label all Kaizen tokens as "not allowed within equations", but now we have a few new ones that are not CSS variables and therefore allowed.
    
    * Add extra catch-all for any missed deprecated tokens at the end of the rule.
    
    * Update kaizenTokens.ts
    
    * Cleanup KaizenToken type
    
    * Rename tokensInFunctions to fixAlphaModificationFunctions
    
    * Un-export stringContainsDeprecatedKaizenToken
    
    * Use the deny list for detecting functions that don't support CSS variables, for replacement tokens.
    
    * Comments and re-ordering of logic for readability
    
    * Type error fix
    
    Co-authored-by: Jason O'Neil <jason.oneil@gmail.com>

--- packages/stylelint-plugin/src/rules/no-invalid-use-of-var-tokens-in-equations.ts
+++ packages/stylelint-plugin/src/rules/no-invalid-use-of-var-tokens-in-equations.ts
@@ -1,4 +1,4 @@
-import { Declaration, Root } from "postcss"
+import { AtRule, Declaration, Root } from "postcss"
 import postcssValueParser, { WordNode } from "postcss-value-parser"
 import {
   kaizenVariableUsedNextToOperatorMessage,
@@ -14,8 +14,8 @@ import {
 } from "../util/variableUtils"
 import { walkDeclsWithKaizenTokens, walkWithParent } from "../util/walkers"
 
-const noInvalidEquationsOnDeclaraion = (
-  postcssNode: Declaration,
+const noInvalidEquations = (
+  postcssNode: Declaration | AtRule,
   parsedValue: postcssValueParser.ParsedValue,
   options: Options
 ) => {
@@ -45,7 +45,12 @@ const noInvalidEquationsOnDeclaraion = (
     if (node.type === "word") {
       // Detect the case of the node being a negated variable (because postcss-value-parser doesn't detect the operator and variable as seprate nodes)
       const parsedVariable = parseVariable(node)
-      if (parsedVariable?.negated && parsedVariable?.kaizenToken) {
+
+      if (
+        parsedVariable?.negated &&
+        parsedVariable?.kaizenToken &&
+        parsedVariable?.kaizenToken.cssVariable
+      ) {
         if (
           options.language === "scss" &&
           options.fix &&
@@ -87,10 +92,14 @@ const noInvalidEquationsOnDeclaraion = (
         // If there exists an operator and a variable next to each other, it's invalid
         if (
           (isOperator(lastTwoWordNodesNextToEachOther[0].value) &&
-            parsedVariable1?.kaizenToken) ||
+            parsedVariable1?.kaizenToken &&
+            parsedVariable1?.kaizenToken?.cssVariable) ||
           (isOperator(lastTwoWordNodesNextToEachOther[1].value) &&
-            parsedVariable0?.kaizenToken)
+            parsedVariable0?.kaizenToken &&
+            parsedVariable0?.kaizenToken?.cssVariable)
         ) {
+          // Go through all the cases where we can fix automatically
+
           const variableInEquation = parsedVariable0 || parsedVariable1
           if (isWithinCalcFunction) {
             // If the variable is interpolated within a calc function, its fine
@@ -148,7 +157,7 @@ export const noInvalidUseOfVarTokensInEquations: RuleDefinition = {
     walkDeclsWithKaizenTokens(
       stylesheetNode,
       ({ postcssNode, parsedValue }) => {
-        noInvalidEquationsOnDeclaraion(postcssNode, parsedValue, options)
+        noInvalidEquations(postcssNode, parsedValue, options)
       }
     )
   },
@@ -158,15 +167,15 @@ export const noInvalidUseOfVarTokensInEquations: RuleDefinition = {
  * Check if a declaration contains an equation that we cannot support with CSS Variables.
  * This is exported so other rules can avoid transforming tokens to CSS Variables versions when they are used in unsupported functions.
  **/
-export const declContainsInvalidEquations = (
-  postcssNode: Declaration,
+export const containsEquationThatDoesntWorkWithCSSVariables = (
+  postcssNode: Declaration | AtRule,
   parsedValue: postcssValueParser.ParsedValue,
   options: Options
 ) => {
   let reported = 0
-  noInvalidEquationsOnDeclaraion(postcssNode, parsedValue, {
+  noInvalidEquations(postcssNode.clone(), parsedValue, {
     ...options,
-    fix: false,
+    fix: true,
     reporter: () => {
       reported++
     },
