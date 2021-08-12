88438cd4550c907fff0171712d52322675710d15
Daniel Manning <danielmanning213@gmail.com>
2021-08-12 12:38:18 +1000

    Remove tokens/*.ts files

--- packages/design-tokens/tokens/spacing.ts
+++
@@ -1,9 +0,0 @@
-import * as spacingTokens from "./spacing.json"
-
-export type SpacingTokens = typeof spacingTokens.spacing
-
-export interface Spacing {
-  spacing: SpacingTokens
-}
-
-export const spacing: Spacing = spacingTokens
--- packages/design-tokens/tokens/typography.ts
+++
@@ -1,6 +0,0 @@
-import { DeepMapObjectLeafs, Theme } from "../src/types"
-import * as typographyTokens from "./typography.json"
-
-export const typography: {
-  typography: DeepMapObjectLeafs<Theme["typography"], string>
-} = typographyTokens
