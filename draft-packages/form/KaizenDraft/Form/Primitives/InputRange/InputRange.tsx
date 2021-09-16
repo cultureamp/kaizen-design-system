10138ffb65912be879ab3cefab18cf6c0a76cbc4
Aaron Knol <aaron.knol@gmail.com>
2021-09-16 16:32:30 +1000

    Added label low and label high props and story

--- draft-packages/form/KaizenDraft/Form/Primitives/InputRange/InputRange.tsx
+++ draft-packages/form/KaizenDraft/Form/Primitives/InputRange/InputRange.tsx
@@ -1,15 +1,24 @@
-import React, { InputHTMLAttributes } from "react"
+import React, { InputHTMLAttributes, ReactNode } from "react"
+import { Paragraph } from "@kaizen/component-library"
 import styles from "./styles.scss"
 
 export interface InputRangeProps extends InputHTMLAttributes<HTMLInputElement> {
   value?: number
+  labelLow?: ReactNode
+  labelHigh?: ReactNode
 }
 
 const InputRange: React.FunctionComponent<InputRangeProps> = (
   props: InputRangeProps
 ) => {
-  const { defaultValue = 1, value, ...genericInputProps } = props
-  console.log(props)
+  const {
+    defaultValue = 1,
+    value,
+    labelLow = "Not at all",
+    labelHigh = "Very",
+    ...genericInputProps
+  } = props
+
   return (
     <div>
       <input
@@ -25,6 +34,24 @@ const InputRange: React.FunctionComponent<InputRangeProps> = (
         aria-valuemax={10}
         {...genericInputProps}
       />
+      <div className={styles.labelsContainer}>
+        <div className={styles.sliderLabels}>
+          <Paragraph
+            variant="extra-small"
+            color="dark-reduced-opacity"
+            tag="span"
+          >
+            {labelLow}
+          </Paragraph>
+          <Paragraph
+            variant="extra-small"
+            color="dark-reduced-opacity"
+            tag="span"
+          >
+            {labelHigh}
+          </Paragraph>
+        </div>
+      </div>
     </div>
   )
 }
