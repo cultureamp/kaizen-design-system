57d51b3df9a3ea8d03dd72781fb963c9b4cd1976
Aaron Knol <aaron.knol@gmail.com>
2021-09-23 15:39:07 +1000

    Fixed syntax error on test page

--- draft-packages/form/KaizenDraft/Form/Primitives/InputRange/InputRange.spec.tsx
+++ draft-packages/form/KaizenDraft/Form/Primitives/InputRange/InputRange.spec.tsx
@@ -25,21 +25,21 @@ it("moves value to 6 after right arrow press, then 5 after left arrow press", as
 
   // screen.getByDisplayValue("6")
   // expect(onChange).toHaveBeenCalledTimes(1)
+})
+// it.todo("moves value to 10 after clicking end of slider", () => {
+//   // await fireEvent.mouseDown(sliderInput, { clientX: 162, clientY: 302 })
+// })
 
-  it.todo("moves value to 10 after clicking end of slider", () => {
-    // await fireEvent.mouseDown(sliderInput, { clientX: 162, clientY: 302 })
-  })
+it.todo("shows default value correctly when provided")
 
-  it.todo("shows default value correctly when provided")
+it.todo("fires onChange after interaction")
 
-  it.todo("fires onChange after interaction")
+it.todo("shows value correctly when provided")
 
-  it.todo("shows value correctly when provided")
+it.todo("custom low and high labels show when provided")
 
-  it.todo("custom low and high labels show when provided")
+it.todo("renders the screenreader help text")
 
-  it.todo("renders the screenreader help text")
+it.todo("doesn't change value when disabled and clicked")
 
-  it.todo("doesn't change value when disabled and clicked")
-
-  it.todo("renders disabled label when provided and disabled")
+it.todo("renders disabled label when provided and disabled")
