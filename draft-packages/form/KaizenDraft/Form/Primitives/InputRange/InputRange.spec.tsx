e64f09c6cddef941e14524936beb44f5986e1e80
reetaa <retdream@gmail.com>
2021-09-23 15:30:23 +1000

    Add initial tests

--- draft-packages/form/KaizenDraft/Form/Primitives/InputRange/InputRange.spec.tsx
+++ draft-packages/form/KaizenDraft/Form/Primitives/InputRange/InputRange.spec.tsx
@@ -1,23 +1,45 @@
-it.todo("renders value as 5.5 when no default value provided")
+import React from "react"
+import { render, screen, fireEvent } from "@testing-library/react"
+import InputRange from "./InputRange"
 
-it.todo(
-  "moves value to 6 after right arrow press, then 5 after left arrow press"
-)
-
-it.todo("moves value to 10 after clicking end of slider", () => {
-  // await fireEvent.mouseDown(sliderInput, { clientX: 162, clientY: 302 })
+it("renders value as 5.5 when no default value provided", async () => {
+  render(<InputRange />)
+  screen.getByDisplayValue("5.5")
 })
 
-it.todo("shows default value correctly when provided")
+it("moves value to 6 after right arrow press, then 5 after left arrow press", async () => {
+  const onChange = jest.fn()
+  // passes
+  render(<InputRange />)
+  const slider = await screen.findByRole("slider")
+  fireEvent.change(slider, { target: { value: 6 } })
+  screen.getByDisplayValue("6")
 
-it.todo("fires onChange after interaction")
+  // why is onchange not firing and value not changed
+  // render(<InputRange {...onChange} />)
+  // const slider = await screen.findByRole("slider")
+  // fireEvent.keyUp(await screen.findByRole("slider"), {
+  //   key: "ArrowRight",
+  //   code: 39,
+  // })
 
-it.todo("shows value correctly when provided")
+  // screen.getByDisplayValue("6")
+  // expect(onChange).toHaveBeenCalledTimes(1)
 
-it.todo("custom low and high labels show when provided")
+  it.todo("moves value to 10 after clicking end of slider", () => {
+    // await fireEvent.mouseDown(sliderInput, { clientX: 162, clientY: 302 })
+  })
 
-it.todo("renders the screenreader help text")
+  it.todo("shows default value correctly when provided")
 
-it.todo("doesn't change value when disabled and clicked")
+  it.todo("fires onChange after interaction")
 
-it.todo("renders disabled label when provided and disabled")
+  it.todo("shows value correctly when provided")
+
+  it.todo("custom low and high labels show when provided")
+
+  it.todo("renders the screenreader help text")
+
+  it.todo("doesn't change value when disabled and clicked")
+
+  it.todo("renders disabled label when provided and disabled")
