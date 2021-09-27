41459e906229966a724198253e8d424bd4590734
Aaron Knol <aaron.knol@gmail.com>
2021-09-27 15:28:21 +1000

    Removed tests that require userEvent

--- draft-packages/form/KaizenDraft/Form/Primitives/InputRange/InputRange.spec.tsx
+++ draft-packages/form/KaizenDraft/Form/Primitives/InputRange/InputRange.spec.tsx
@@ -8,33 +8,11 @@ it("renders value as 5.5 when no default value provided", async () => {
   screen.getByDisplayValue("5.5")
 })
 
-it("moves value to 6 after right arrow press, then 5 after left arrow press", async () => {
-  // passes - change descripton above?
-  render(<InputRange />)
-  const slider = await screen.findByRole("slider")
+it.todo(
+  "moves value to 6 after right arrow press, then 5 after left arrow press"
+)
 
-  fireEvent.change(slider, { target: { value: 6 } })
-
-  screen.getByDisplayValue("6")
-
-  // why is onChange not firing and value not changed
-  // const onChange = jest.fn()
-
-  // render(<InputRange onChange={onChange}} />)
-  // const slider = await screen.findByRole("slider")
-
-  // fireEvent.keyUp(await screen.findByRole("slider"), {
-  //   key: "ArrowRight",
-  //   code: 39,
-  // })
-
-  // screen.getByDisplayValue("6")
-  // expect(onChange).toHaveBeenCalledTimes(1)
-})
-
-// it.todo("moves value to 10 after clicking end of slider", () => {
-//   // await fireEvent.mouseDown(sliderInput, { clientX: 162, clientY: 302 })
-// })
+it.todo("moves value to 10 after clicking end of slider")
 
 it("shows default value correctly when provided", async () => {
   render(<InputRange defaultValue={9} />)
@@ -73,15 +51,7 @@ it("renders the screenreader help text", async () => {
   await screen.findAllByText(/1 is bad, 10 is good/i)
 })
 
-// it("doesn't change value when disabled and clicked", async () => {
-//   render(<InputRange disabled={true} />)
-//   const slider = await screen.findByRole("slider")
-
-//   fireEvent.change(slider, { target: { value: 8 } })
-
-// actually value changes to 8
-//   screen.getByDisplayValue("5.5")
-// })
+it.todo("doesn't change value when disabled and clicked")
 
 it("renders disabled label when provided and disabled", async () => {
   render(
