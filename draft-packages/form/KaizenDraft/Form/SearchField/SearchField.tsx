127aa1c26c7972c1ac2a089793ad67e53c8d42ac
Lloyd Stubber <lloyd@squareyes.info>
2021-10-01 14:43:33 +1000

    feat: add SearchField and InputSearch to Kaizen (#1975)
    * feat: add SearchField and InputSearch to Kaizen
    
    * update hover style of secondary reversed
    
    * fix TS and update test
    
    * update to have aria-label
    
    * update to have aria-label
    
    * pr feedback
    
    * update stories to have labelText

---
+++ draft-packages/form/KaizenDraft/Form/SearchField/SearchField.tsx
@@ -0,0 +1,57 @@
+import React from "react"
+
+import { InputSearch } from "../Primitives/InputSearch"
+import { FieldGroup, Label } from ".."
+export interface SearchFieldProps
+  extends Omit<
+    React.InputHTMLAttributes<HTMLInputElement>,
+    "className" | "defaultValue"
+  > {
+  reversed?: boolean
+  loading?: boolean
+  secondary?: boolean
+  labelText: string
+  onClear?: () => void
+}
+
+const SearchField: React.FunctionComponent<SearchFieldProps> = ({
+  id,
+  disabled = false,
+  placeholder,
+  reversed = false,
+  loading,
+  secondary,
+  value,
+  labelText,
+  onClear,
+  ...genericInputProps
+}) => {
+  const searchFieldId = `${id}-search-field-input`
+
+  return (
+    <FieldGroup inline={false}>
+      {secondary && (
+        <Label
+          htmlFor={searchFieldId}
+          labelText={labelText}
+          reversed={reversed}
+          disabled={disabled}
+        />
+      )}
+      <InputSearch
+        aria-label={secondary ? labelText : undefined}
+        id={searchFieldId}
+        placeholder={placeholder}
+        disabled={disabled}
+        value={value}
+        reversed={reversed}
+        secondary={secondary}
+        onClear={onClear}
+        loading={loading}
+        {...genericInputProps}
+      />
+    </FieldGroup>
+  )
+}
+
+export default SearchField
