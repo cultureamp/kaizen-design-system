b27f4f97077ee937a2c05b17b074210b4e53a62c
Chy Meng <chy.meng@cultureamp.com>
2021-10-22 14:56:06 +1100

    Add tests

--- packages/pagination/src/TruncateIndicator.tsx
+++ packages/pagination/src/TruncateIndicator.tsx
@@ -4,7 +4,10 @@ import { Icon } from "@kaizen/component-library"
 import styles from "./Pagination.scss"
 
 export const TruncateIndicator = () => (
-  <div className={styles.truncateIndicatorWrapper}>
+  <div
+    className={styles.truncateIndicatorWrapper}
+    data-testid="truncate-indicator"
+  >
     <Icon icon={ellipsis} role="presentation" />
   </div>
 )
