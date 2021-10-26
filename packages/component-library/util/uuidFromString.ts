a326aa21c056c92b11b646757718dc661298a51d
Doug MacKenzie <doug.mackenzie@cultureamp.com>
2021-10-27 09:25:37 +1100

    fix: Remove IDs from Icon elements (#2124)
    These were previously used for linking with aria-labelledby, but we were told they are not required, and they are creating issues for us with duplicate IDs.

--- packages/component-library/util/uuidFromString.ts
+++
@@ -1,8 +0,0 @@
-const getUniqueId = require("uuid/v5")
-
-export const uuidFromString = (value: string): string => {
-  // Random valid uuid namespace
-  const NAMESPACE = "1b671a64-40d5-491e-99b0-da01ff1f3341"
-
-  return getUniqueId(value, NAMESPACE)
-}
