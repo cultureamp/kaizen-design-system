219aa74925688d3334e3b7c765b1484278967608
Seb Pearce <sebpearce@gmail.com>
2021-11-02 12:50:36 +1100

    docs: Add Storybook documentation for Heading, Paragraph and Box (#1150)
    docs: Add Heading docs
    docs: Add Paragraph docs
    docs: Add Box docs
    
    Co-authored-by: Lloyd Stubber <lloyd@squareyes.info>
    Co-authored-by: ActuallyACat <allanna.beaton@gmail.com>

--- packages/component-library/stories/Heading.stories.tsx
+++ packages/component-library/stories/Heading.stories.tsx
@@ -1,4 +1,14 @@
 import * as React from "react"
+import { Paragraph, Box } from "@kaizen/component-library"
+import {
+  Title,
+  Subtitle,
+  Description,
+  Primary,
+  ArgsTable,
+  Stories,
+  PRIMARY_STORY,
+} from "@storybook/addon-docs"
 import { CATEGORIES, SUB_CATEGORIES } from "../../../storybook/constants"
 import { Heading } from "../components/Heading"
 
@@ -7,14 +17,74 @@ export default {
   component: Heading,
   parameters: {
     docs: {
-      description: {
-        component: 'import { Heading } from "@kaizen/component-library";',
-      },
+      page: () => (
+        <>
+          <Title />
+          <Subtitle />
+          <Description />
+          <Documentation />
+          <Primary />
+          <ArgsTable story={PRIMARY_STORY} />
+          <Stories />
+        </>
+      ),
     },
   },
 }
 
-export const Display0 = () => <Heading variant="display-0">Display 0</Heading>
+const Documentation = ({ reversed }: { reversed?: boolean }) => (
+  <Box mt={2}>
+    <Paragraph variant="body" color={reversed ? "white" : "dark"}>
+      <ul>
+        <li>
+          The <code>variant</code> prop is required, but the <code>tag</code>{" "}
+          prop is not. If no <code>tag</code> prop is provided,{" "}
+          <code>Heading</code> will attempt to render the appropriate tag based
+          on the chosen <code>variant</code>. This cannot be relied on to give
+          you the right tag in 100% of cases, so we recommend manually choosing
+          the appropriate tag for your situation.
+        </li>
+        <li>
+          Use the <code>color</code> prop to change the text color. This prop is
+          typed to restrict values to a designer-approved palette. Do not modify
+          color by targeting the element via CSS from outside the{" "}
+          <code>Heading</code> component, because this causes problems in future
+          rebrands when our colors change.
+        </li>
+        <li>
+          <strong>Semantic, logical HTML should always take priority</strong>{" "}
+          when deciding which <code>tag</code> to use:
+          <ul>
+            <li>
+              Heading elements should never skip a level, e.g. an{" "}
+              <code>h2</code> followed by an <code>h4</code> should be avoided.
+            </li>
+            <li>
+              The main heading on the page that accurately summarizes the page
+              should be an <code>h1</code>.
+            </li>
+            <li>
+              A <code>Heading</code> should not have a bigger <code>tag</code>{" "}
+              than another visually smaller <code>Heading</code>.
+            </li>
+            <li>
+              <strong>
+                Illogical heading orders will cause pages to fail WCAG
+                compliance tests &amp; audits.
+              </strong>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </Paragraph>
+  </Box>
+)
+
+export const Display0 = () => (
+  <>
+    <Heading variant="display-0">Display 0</Heading>
+  </>
+)
 
 export const Heading1 = () => (
   <Heading data-automation-id="test" variant="heading-1" id="make-me-unique">
@@ -22,56 +92,93 @@ export const Heading1 = () => (
   </Heading>
 )
 
-export const Heading2 = () => <Heading variant="heading-2">Heading 2</Heading>
+export const Heading2 = () => (
+  <>
+    <Heading variant="heading-2">Heading 2</Heading>
+  </>
+)
 
-export const Heading3 = () => <Heading variant="heading-3">Heading 3</Heading>
+export const Heading3 = () => (
+  <>
+    <Heading variant="heading-3">Heading 3</Heading>
+  </>
+)
 
-export const Heading4 = () => <Heading variant="heading-4">Heading 4</Heading>
+export const Heading4 = () => (
+  <>
+    {" "}
+    <Heading variant="heading-4">Heading 4</Heading>
+  </>
+)
 
-export const Heading5 = () => <Heading variant="heading-5">Heading 5</Heading>
+export const Heading5 = () => (
+  <>
+    <Heading variant="heading-5">Heading 5</Heading>
+  </>
+)
 
-export const Heading6 = () => <Heading variant="heading-6">Heading 6</Heading>
+export const Heading6 = () => (
+  <>
+    <Heading variant="heading-6">Heading 6</Heading>
+  </>
+)
 
 const Heading1DarkReducedOpacity = () => (
-  <Heading variant="heading-1" color="dark-reduced-opacity">
-    Heading 1 (dark, reduced opacity)
-  </Heading>
+  <>
+    <Heading variant="heading-1" color="dark-reduced-opacity">
+      Heading 1 (dark, reduced opacity)
+    </Heading>
+  </>
 )
 
 const Heading1White = () => (
-  <Heading variant="heading-1" color="white">
-    Heading 1 (white)
-  </Heading>
+  <>
+    <Heading variant="heading-1" color="white">
+      Heading 1 (white)
+    </Heading>
+    <Documentation reversed />
+  </>
 )
 
 const Heading1WhiteReducedOpacity = () => (
-  <Heading variant="heading-1" color="white-reduced-opacity">
-    Heading 1 (white, reduced opacity)
-  </Heading>
+  <>
+    <Heading variant="heading-1" color="white-reduced-opacity">
+      Heading 1 (white, reduced opacity)
+    </Heading>
+    <Documentation reversed />
+  </>
 )
 
 export const Heading1Positive = () => (
-  <Heading variant="heading-1" color="positive">
-    Heading 1 (positive)
-  </Heading>
+  <>
+    <Heading variant="heading-1" color="positive">
+      Heading 1 (positive)
+    </Heading>
+  </>
 )
 
 export const Heading1Negative = () => (
-  <Heading variant="heading-1" color="negative">
-    Heading 1 (negative)
-  </Heading>
+  <>
+    <Heading variant="heading-1" color="negative">
+      Heading 1 (negative)
+    </Heading>
+  </>
 )
 
 export const Heading3Positive = () => (
-  <Heading variant="heading-3" color="positive">
-    Heading 3 (positive)
-  </Heading>
+  <>
+    <Heading variant="heading-3" color="positive">
+      Heading 3 (positive)
+    </Heading>
+  </>
 )
 
 export const Heading3Negative = () => (
-  <Heading variant="heading-3" color="negative">
-    Heading 3 (negative)
-  </Heading>
+  <>
+    <Heading variant="heading-3" color="negative">
+      Heading 3 (negative)
+    </Heading>
+  </>
 )
 
 Heading1White.storyName = "Heading 1 White"
--- packages/component-library/stories/Paragraph.stories.tsx
+++ packages/component-library/stories/Paragraph.stories.tsx
@@ -1,19 +1,79 @@
 import * as colorTokens from "@kaizen/design-tokens/tokens/color.json"
 import * as React from "react"
+import { Box } from "@kaizen/component-library"
+import {
+  Title,
+  Subtitle,
+  Description,
+  Primary,
+  ArgsTable,
+  Stories,
+  PRIMARY_STORY,
+} from "@storybook/addon-docs"
 import { CATEGORIES, SUB_CATEGORIES } from "../../../storybook/constants"
 import { Paragraph } from "../components/Paragraph"
 
 export default {
   title: `${CATEGORIES.components}/${SUB_CATEGORIES.typography}/Paragraph`,
-  component: Paragraph,
+  component: Box,
   parameters: {
     docs: {
-      description: {
-        component: 'import { Paragraph } from "@kaizen/component-library"',
-      },
+      page: () => (
+        <>
+          <Title />
+          <Subtitle />
+          <Description />
+          <Documentation />
+          <Primary />
+          <ArgsTable story={PRIMARY_STORY} />
+          <Stories />
+        </>
+      ),
     },
   },
 }
+
+const Documentation = ({ reversed }: { reversed?: boolean }) => (
+  <Box mt={2}>
+    <Paragraph variant="body" color={reversed ? "white" : "dark"}>
+      <ul>
+        <li>
+          There is no variant for bold text. You can use{" "}
+          <code>&#x3C;strong&#x3E;</code> tags for this purpose and it will have
+          the correct font weight applied.
+        </li>
+        <li>
+          Use the <code>color</code> prop to change the text color. This prop is
+          typed to restrict values to a designer-approved palette. Do not modify
+          color by targeting the element via CSS from outside the{" "}
+          <code>Paragraph</code> component, because this causes problems in
+          future rebrands when our colors change.
+        </li>
+        <li>
+          If you want to apply margin or padding to a <code>Paragraph</code>,
+          you can either
+          <ul>
+            <li>wrap it in a new div with margin or padding applied, or</li>
+            <li>
+              wrap it in the{" "}
+              <a href="/?path=/story/box-react--box-default">
+                <code>Box</code>
+              </a>{" "}
+              component, which gives you a no-CSS way to create spacing.
+            </li>
+          </ul>
+        </li>
+        <li>
+          Design-related documentation is available on the{" "}
+          <a href="https://cultureamp.design/guidelines/typography/">
+            Typography guidlines on the Kaizen Site
+          </a>
+          .
+        </li>
+      </ul>
+    </Paragraph>
+  </Box>
+)
 
 export const IntroLede = () => (
   <Paragraph variant="intro-lede" id="make-me-unique">
@@ -22,9 +82,11 @@ export const IntroLede = () => (
 )
 
 export const Body = () => (
-  <Paragraph data-automation-id="test" variant="body">
-    Paragraph Body
-  </Paragraph>
+  <>
+    <Paragraph data-automation-id="test" variant="body">
+      Paragraph Body
+    </Paragraph>
+  </>
 )
 
 export const BodyBold = () => (
@@ -32,28 +94,28 @@ export const BodyBold = () => (
     <Paragraph data-automation-id="test" variant="body">
       <strong>Paragraph Body (bold)</strong>
     </Paragraph>
-    <Paragraph variant="body">
-      There is no variant for bold text. You can use{" "}
-      <code>&#x3C;strong&#x3E;</code> tags for this purpose and it will have the
-      correct font weight applied.
-    </Paragraph>
   </>
 )
 
 export const BodyDarkReducedOpacity = () => (
-  <Paragraph
-    data-automation-id="test"
-    variant="body"
-    color="dark-reduced-opacity"
-  >
-    Paragraph Body
-  </Paragraph>
+  <>
+    <Paragraph
+      data-automation-id="test"
+      variant="body"
+      color="dark-reduced-opacity"
+    >
+      Paragraph Body
+    </Paragraph>
+  </>
 )
 
 export const BodyWhite = () => (
-  <Paragraph variant="body" color="white">
-    Paragraph Body
-  </Paragraph>
+  <>
+    <Paragraph variant="body" color="white">
+      Paragraph Body
+    </Paragraph>
+    <Documentation reversed />
+  </>
 )
 
 BodyWhite.storyName = "Body White"
@@ -63,21 +125,29 @@ BodyWhite.parameters = {
 }
 
 export const BodyPositive = () => (
-  <Paragraph variant="body" color="positive">
-    Paragraph Body
-  </Paragraph>
+  <>
+    <Paragraph variant="body" color="positive">
+      Paragraph Body
+    </Paragraph>
+  </>
 )
 
 export const BodyNegative = () => (
-  <Paragraph variant="body" color="negative">
-    Paragraph Body
-  </Paragraph>
+  <>
+    <Paragraph variant="body" color="negative">
+      Paragraph Body
+    </Paragraph>
+  </>
 )
 
 export const Small = () => (
-  <Paragraph variant="small">Paragraph Small</Paragraph>
+  <>
+    <Paragraph variant="small">Paragraph Small</Paragraph>
+  </>
 )
 
 export const ExtraSmall = () => (
-  <Paragraph variant="extra-small">Paragraph Extra Small</Paragraph>
+  <>
+    <Paragraph variant="extra-small">Paragraph Extra Small</Paragraph>
+  </>
 )
