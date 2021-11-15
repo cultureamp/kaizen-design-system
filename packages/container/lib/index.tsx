8a2828d7b9a1f431f485e57eb3134ab00fea548a
ActuallyACat <allanna.beaton@gmail.com>
2021-11-15 16:11:59 +1100

    Improve JSDoc of the theme manager

--- packages/container/lib/index.tsx
+++ packages/container/lib/index.tsx
@@ -1,9 +1,15 @@
 import * as React from "react"
-import { ThemeManager, ThemeProvider, heartTheme } from "@kaizen/design-tokens"
+import { ThemeManager, ThemeProvider, heartTheme, defaultTheme } from "@kaizen/design-tokens"
 import "./polyfills"
 
-// Initialise the ThemeManager, this is a singleton so this should not be redeclared
-export const themeManager = new ThemeManager(heartTheme)
+/**
+ * Initialise the themeManager with the default theme. This is a singleton so this should not
+ * be redeclared in consuming apps.
+ *
+ * Should you need to need to modify the default theme, you can do so by importing and calling
+ * `themeManager.setAndApplyTheme` from this package
+ */
+export const themeManager = new ThemeManager(defaultTheme)
 
 export const Container = ({ children }: { children?: React.ReactNode }) => (
   <ThemeProvider themeManager={themeManager}>{children}</ThemeProvider>
