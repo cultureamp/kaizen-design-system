f16452edb0dda51b46cbba410dca16da252851f6
NatalieLoyzaga <48232362+NatalieLoyzaga@users.noreply.github.com>
2021-11-29 09:47:58 +1100

    fix: Update ConfirmationModal to new styles and variants (#2308)
    * fix: Update ConfirmationModal to new styles and variants
    
    * fix: Update Icons to use -white and story modal title

--- draft-packages/modal/KaizenDraft/Modal/Presets/ConfirmationModal.tsx
+++ draft-packages/modal/KaizenDraft/Modal/Presets/ConfirmationModal.tsx
@@ -1,13 +1,18 @@
 import classnames from "classnames"
 import * as React from "react"
 
-import { Heading } from "@kaizen/component-library"
+import { Heading, Icon } from "@kaizen/component-library"
 import {
+  Assertive,
   Cautionary,
   Informative,
   Negative,
-  PositiveFemale,
+  Positive,
 } from "@kaizen/draft-illustration"
+
+import exclamationIcon from "@kaizen/component-library/icons/exclamation-white.icon.svg"
+import informationIcon from "@kaizen/component-library/icons/information-white.icon.svg"
+import successIcon from "@kaizen/component-library/icons/success-white.icon.svg"
 
 import { ButtonProps } from "@kaizen/draft-button"
 import {
@@ -23,6 +28,8 @@ import styles from "./ConfirmationModal.scss"
 
 export interface ConfirmationModalProps {
   readonly isOpen: boolean
+  readonly unpadded?: boolean
+  readonly isProminent?: boolean
   readonly type: ModalType
   readonly title: string
   readonly onConfirm?: () => void
@@ -35,23 +42,53 @@ export interface ConfirmationModalProps {
 }
 
 type ConfirmationModal = React.FunctionComponent<ConfirmationModalProps>
-type ModalType = "positive" | "informative" | "negative" | "cautionary"
 
-const getIcon = (type: ModalType) => {
+type ModalType =
+  | "positive"
+  | "informative"
+  | "negative"
+  | "cautionary"
+  | "assertive"
+
+const getIcon = (type: ModalType, isProminent: boolean) => {
   switch (type) {
     case "cautionary":
-      return <Cautionary alt="" />
+      return isProminent ? (
+        <Cautionary alt="" isAnimated />
+      ) : (
+        <Icon icon={exclamationIcon} inheritSize role="presentation" />
+      )
     case "informative":
-      return <Informative alt="" />
+      return isProminent ? (
+        <Informative alt="" isAnimated />
+      ) : (
+        <Icon icon={informationIcon} inheritSize role="presentation" />
+      )
     case "negative":
-      return <Negative alt="" />
+      return isProminent ? (
+        <Negative alt="" isAnimated />
+      ) : (
+        <Icon icon={exclamationIcon} inheritSize role="presentation" />
+      )
     case "positive":
-      return <PositiveFemale alt="" />
+      return isProminent ? (
+        <Positive alt="" isAnimated />
+      ) : (
+        <Icon icon={successIcon} inheritSize role="presentation" />
+      )
+    case "assertive":
+      return isProminent ? (
+        <Assertive alt="" isAnimated />
+      ) : (
+        <Icon icon={exclamationIcon} inheritSize role="presentation" />
+      )
   }
 }
 
 const ConfirmationModal = ({
   isOpen,
+  isProminent = false,
+  unpadded = false,
   type,
   title,
   onConfirm,
@@ -92,27 +129,41 @@ const ConfirmationModal = ({
       automationId={automationId}
     >
       <div className={styles.modal}>
-        <ModalHeader unpadded onDismiss={onDismiss}>
+        <ModalHeader onDismiss={onDismiss}>
           <div
             className={classnames(styles.header, {
               [styles.cautionaryHeader]: type === "cautionary",
               [styles.informativeHeader]: type === "informative",
               [styles.negativeHeader]: type === "negative",
               [styles.positiveHeader]: type === "positive",
+              [styles.assertiveHeader]: type === "assertive",
+              [styles.prominent]: isProminent,
+              [styles.padded]: !unpadded,
             })}
           >
-            <div className={styles.iconContainer}>
-              <div className={styles.spotIcon}>{getIcon(type)}</div>
+            <div
+              className={classnames(styles.iconContainer, {
+                [styles.prominent]: isProminent,
+              })}
+            >
+              <div className={styles.spotIcon}>
+                {getIcon(type, isProminent)}
+              </div>
             </div>
-            <ModalAccessibleLabel>
-              <Heading tag="h1" variant="heading-1">
+            <ModalAccessibleLabel isProminent={isProminent}>
+              <Heading tag="h2" variant="heading-2">
                 {title}
               </Heading>
             </ModalAccessibleLabel>
           </div>
         </ModalHeader>
-        <ModalBody unpadded>
-          <div className={styles.body}>
+        <ModalBody>
+          <div
+            className={classnames(styles.body, {
+              [styles.prominent]: isProminent,
+              [styles.padded]: !unpadded,
+            })}
+          >
             <ModalAccessibleDescription>{children}</ModalAccessibleDescription>
           </div>
         </ModalBody>
@@ -120,6 +171,7 @@ const ConfirmationModal = ({
           actions={footerActions}
           appearance={type === "negative" ? "destructive" : "primary"}
           automationId={automationId}
+          unpadded={unpadded}
         />
       </div>
     </GenericModal>
