428506b3465f576ff1fc959ea0889f333596e4e1
Lloyd Stubber <lloyd@squareyes.info>
2021-12-07 11:05:23 +1100

    chore: update all figma embed to use heart theme

--- draft-packages/button/docs/IconButton.stories.tsx
+++ draft-packages/button/docs/IconButton.stories.tsx
@@ -21,7 +21,7 @@ export default {
       },
     },
     ...figmaEmbed(
-      "https://www.figma.com/file/GMxm8rvDCbj0Xw3TQWBZ8b/UI-Kit-Zen?node-id=13555%3A0"
+      "https://www.figma.com/file/eZKEE5kXbEMY3lx84oz8iN/%E2%9D%A4%EF%B8%8F-UI-Kit%3A-Heart?node-id=13555%3A0"
     ),
   },
   decorators: [withDesign],
