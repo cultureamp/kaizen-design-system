f8971d31e180cfb3656815eda56b744275273e52
Christian Klammer <christian.klammer@cultureamp.com>
2021-12-16 13:30:30 +1100

    Made ccopy changes to all story examples

--- packages/notification/docs/GlobalNotification.stories.tsx
+++ packages/notification/docs/GlobalNotification.stories.tsx
@@ -75,7 +75,7 @@ export const GlobalNotificationVariants = () => (
         automationId="notification4"
         persistent={true}
       >
-        Something went wrong while validating and analyzing user data.
+        Something went wrong while validating and analyzing user data.{" "}
         <a href="/">View all</a>.
       </GlobalNotification>
     </div>
@@ -89,26 +89,26 @@ export const GlobalNotificationVariants = () => (
 
       <ExampleHeading>Positive</ExampleHeading>
       <GlobalNotification type="positive" automationId="notification1">
-        New user data, imported by mackenzie@hooli.com has successfully
-        uploaded. <a href="/">Manage users is now available</a>
+        Emails will be sent notifying coaches and inviting reviewers to give
+        their feedback. <a href="/">View all</a>
       </GlobalNotification>
 
       <ExampleHeading>Informative</ExampleHeading>
       <GlobalNotification type="informative" automationId="notification2">
-        New user data is currently being processed. We'll let you know when the
-        process is completed. <a href="/">Manage users</a>
+        [Survey name]'s status has been changed to 'Archived'.{" "}
+        <a href="/">View all</a>
       </GlobalNotification>
 
       <ExampleHeading>Cautionary</ExampleHeading>
       <GlobalNotification type="cautionary" automationId="notification3">
-        New user data, imported by mackenzie@hooli.com has uploaded with some
-        minor issues. <a href="/">View issues</a>
+        The syncing process can take some time to complete. Keep this window
+        open until complete. <a href="/">View all</a>
       </GlobalNotification>
 
       <ExampleHeading>Negative</ExampleHeading>
       <GlobalNotification type="negative" automationId="notification4">
-        No network connection. Check your connection and try again.{" "}
-        <a href="/">Refresh</a>.
+        Something went wrong while validating and analyzing user data.{" "}
+        <a href="/">View all</a>.
       </GlobalNotification>
     </div>
   </div>
