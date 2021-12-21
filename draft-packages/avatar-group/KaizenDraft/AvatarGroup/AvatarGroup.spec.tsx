33555a8c12a3aa64a597f63b30084b9b9c754b4b
mcwinter07 <mcwinter07@gmail.com>
2021-12-22 10:47:26 +1100

    * feat: update Avatar stories and resolve linting errors

--- draft-packages/avatar-group/KaizenDraft/AvatarGroup/AvatarGroup.spec.tsx
+++ draft-packages/avatar-group/KaizenDraft/AvatarGroup/AvatarGroup.spec.tsx
@@ -2,58 +2,64 @@ import * as React from "react"
 import { render, screen } from "@testing-library/react"
 import { AvatarGroup } from "./AvatarGroup"
 
-const avatarGroupData = [
+const imgAvatars = [
   {
     fullName: "Adirana Aniseed",
-    disableInitials: false,
     avatarSrc:
       "https://www.cultureampcom-preview-1.usw2.wp-dev-us.cultureamp-cdn.com/assets/slices/main/assets/public/media/chapters-card-1@2x.05e547444387f29f14df0b82634bf2b6.png",
-    isCurrentUser: false,
+    size: "medium",
+  },
+  {
+    fullName: "Adirana Aniseed",
+    avatarSrc:
+      "https://www.cultureampcom-preview-1.usw2.wp-dev-us.cultureamp-cdn.com/assets/slices/main/assets/public/media/chapters-card-1@2x.05e547444387f29f14df0b82634bf2b6.png",
+    size: "medium",
+  },
+  {
+    fullName: "Adirana Aniseed",
+    avatarSrc:
+      "https://www.cultureampcom-preview-1.usw2.wp-dev-us.cultureamp-cdn.com/assets/slices/main/assets/public/media/chapters-card-1@2x.05e547444387f29f14df0b82634bf2b6.png",
+    size: "medium",
+  },
+  {
+    fullName: "Adirana Aniseed",
+    avatarSrc:
+      "https://www.cultureampcom-preview-1.usw2.wp-dev-us.cultureamp-cdn.com/assets/slices/main/assets/public/media/chapters-card-1@2x.05e547444387f29f14df0b82634bf2b6.png",
+    size: "medium",
+  },
+]
+
+const largeAvatars = [
+  {
+    fullName: "Adirana Aniseed",
+    avatarSrc:
+      "https://www.cultureampcom-preview-1.usw2.wp-dev-us.cultureamp-cdn.com/assets/slices/main/assets/public/media/chapters-card-1@2x.05e547444387f29f14df0b82634bf2b6.png",
     size: "large",
   },
   {
     fullName: "Adirana Aniseed",
-    disableInitials: false,
     avatarSrc:
       "https://www.cultureampcom-preview-1.usw2.wp-dev-us.cultureamp-cdn.com/assets/slices/main/assets/public/media/chapters-card-1@2x.05e547444387f29f14df0b82634bf2b6.png",
-    isCurrentUser: false,
-    size: "large",
-  },
-  {
-    fullName: "Adirana Aniseed",
-    disableInitials: false,
-    avatarSrc:
-      "https://www.cultureampcom-preview-1.usw2.wp-dev-us.cultureamp-cdn.com/assets/slices/main/assets/public/media/chapters-card-1@2x.05e547444387f29f14df0b82634bf2b6.png",
-    isCurrentUser: false,
-    size: "large",
-  },
-  {
-    fullName: "Adirana Aniseed",
-    disableInitials: false,
-    avatarSrc:
-      "https://www.cultureampcom-preview-1.usw2.wp-dev-us.cultureamp-cdn.com/assets/slices/main/assets/public/media/chapters-card-1@2x.05e547444387f29f14df0b82634bf2b6.png",
-    isCurrentUser: false,
     size: "large",
   },
 ]
 
 describe("<AvatarGroup />", () => {
   it("renders a list of avatars up to the maxVisible value", () => {
-    render(<AvatarGroup avatars={avatarGroupData} maxVisible={4} />)
-    expect(screen.getAllByRole("img")).toHaveLength(4)
+    render(<AvatarGroup avatars={imgAvatars} maxVisible={4} />)
+    expect(
+      screen.getAllByRole("img", { name: "Adirana Aniseed" })
+    ).toHaveLength(4)
+  })
+  it("Overides the size of individual Avatars with the size set on AvatarGroup", () => {
+    const { container } = render(
+      <AvatarGroup avatars={largeAvatars} maxVisible={4} size="small" />
+    )
+    const avatarsWithLargeClass = container.querySelectorAll(".large").length
+    expect(avatarsWithLargeClass).toBe(0)
   })
   it("renders a token with a counter of the remaining avatars is maxVisible is exceeded", () => {
-    // render(<AvatarGroup avatars={avatarGroupData} maxVisible={3} />)
-  })
-  it("sets the size of the avatars and renders them accordingly", () => {
-    // render(
-    //   <AvatarGroup avatars={avatarGroupData} size="large" maxVisible={3} />
-    // )
-  })
-  it("renders Company Avatars", () => {
-    // render(<AvatarGroup avatars={avatarGroupData} maxVisible={3} />)
-  })
-  it("renders Company Avatars", () => {
-    // render(<AvatarGroup avatars={avatarGroupData} maxVisible={3} />)
+    render(<AvatarGroup avatars={imgAvatars} maxVisible={2} />)
+    screen.getByText("+2")
   })
 })
