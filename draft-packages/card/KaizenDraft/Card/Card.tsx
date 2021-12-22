d0eeb81b16b205dda4993a106f8f9f0e1c3546b2
mcwinter07 <mcwinter07@gmail.com>
2021-12-22 13:52:41 +1100

    feat: update card component variants and render method

--- draft-packages/card/KaizenDraft/Card/Card.tsx
+++ draft-packages/card/KaizenDraft/Card/Card.tsx
@@ -1,9 +1,20 @@
 import * as React from "react"
+import cx from "classnames"
 import styles from "./styles.module.scss"
 
+export type CardVariants =
+  | "default"
+  | "informative"
+  | "positive"
+  | "cautionary"
+  | "destructive"
+  | "assertive"
+  | "highlight"
 export interface CardProps {
+  /**
+   * The children will
+   */
   children?: React.ReactNode
-
   /**
    * Not recommended. A short-circuit for overriding styles in a pinch
    * @default ""
@@ -15,18 +26,30 @@ export interface CardProps {
    * @default "div"
    */
   tag?: "div" | "article" | "header" | "main" | "section" | "li"
+
+  /**
+   * HTML elements that are allowed on Card.
+   * @default "default"
+   */
+  variant?: CardVariants
 }
 
 export const Card = ({
   children,
   tag = "div",
   classNameAndIHaveSpokenToDST,
+  variant = "default",
   ...otherProps
 }: CardProps) => {
-  const className =
-    (classNameAndIHaveSpokenToDST ? classNameAndIHaveSpokenToDST : "") +
-    " " +
-    styles.wrapper
-
-  return React.createElement(tag, { ...otherProps, className }, children)
+  const Tag = tag
+  return (
+    <Tag
+      className={cx(styles.wrapper, styles[variant], {
+        classNameAndIHaveSpokenToDST,
+      })}
+      {...otherProps}
+    >
+      {children}
+    </Tag>
+  )
 }
--- draft-packages/card/docs/Card.stories.tsx
+++ draft-packages/card/docs/Card.stories.tsx
@@ -11,35 +11,39 @@ export default {
   parameters: {
     docs: {
       description: {
-        component: 'import { Card } from "@kaizen/component-library"',
+        component:
+          'import { Card } from "@kaizen/component-library". The `Card` component is a flexible container used to wrap primary content. It has several variants (moods) to assist in displaying information to a user. In the UI toolkit you will find the `Card` component is referred to as `Container`.',
       },
     },
     ...figmaEmbed(
-      "https://www.figma.com/file/eZKEE5kXbEMY3lx84oz8iN/%E2%9D%A4%EF%B8%8F-UI-Kit%3A-Heart?node-id=1929%3A14085"
+      "https://www.figma.com/file/eZKEE5kXbEMY3lx84oz8iN/%E2%9D%A4%EF%B8%8F-UI-Kit%3A-Heart?node-id=1929%3A14084"
     ),
   },
   decorators: [withDesign],
 }
 
-export const DefaultStory = () => <Card>This is a default card</Card>
-
-DefaultStory.storyName = "Default (Kaizen Site Demo)"
-
-export const CardWithSpace = () => (
-  <Box m={4}>
-    <Card>
-      <Box p={1.5}>
-        <Paragraph variant="body">
-          Meaningful content—such as data, images, or paragraphs (but not
-          headings) — sit on cards.
-        </Paragraph>
-      </Box>
-    </Card>
-  </Box>
+export const DefaultStory = args => (
+  <Card {...args}>This is a default card</Card>
 )
 
-CardWithSpace.storyName = "Card, custom spacing with Box"
+DefaultStory.storyName = "Default (Kaizen Site Demo)"
+DefaultStory.args = {
+  tag: "div",
+  variant: "default",
+}
 
-CardWithSpace.parameters = {
-  backgrounds: { default: "Gray 100" },
+export const DesignSheetDefault = () => (
+  
+)
+
+DesignSheetDefault.storyName = "Design Sheet (default)"
+
+
+export const DesignSheetReversed = () => (
+
+)
+
+DesignSheetReversed.storyName = "Design Sheet (reversed)"
+DesignSheetReversed.parameters = {
+  backgrounds: { default: "Purple 700" },
 }
