d346ba3c9d9bb4be80eb602b30b46e7968d0d5c3
Lloyd Stubber <lloyd@squareyes.info>
2022-01-06 13:34:33 +1100

    wip: try to TS-ify the shim

--- packages/date-picker/src/DatePicker.tsx
+++ packages/date-picker/src/DatePicker.tsx
@@ -1,16 +1,36 @@
 import React, { useEffect, useRef } from "react"
 import { defineCustomElements } from "@duetds/date-picker/dist/loader"
+import { DuetDateAdapter } from "@duetds/date-picker/dist/types/components/duet-date-picker/date-adapter"
+import styles from "./DatePicker.scss"
 
 defineCustomElements(window)
 
-function useListener(ref, eventName, handler) {
-  useEffect(() => {
-    if (ref.current) {
-      const element = ref.current
-      element.addEventListener(eventName, handler)
-      return () => element.removeEventListener(eventName, handler)
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace JSX {
+    interface IntrinsicElements {
+      "duet-date-picker": React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement>,
+        HTMLElement
+      >
     }
-  }, [eventName, handler, ref])
+  }
+}
+
+export type DatePickerProps = {
+  onBlur: CustomEvent<{ component: "duet-date-picker" }>
+  onChange: CustomEvent<{
+    component: "duet-date-picker"
+    valueAsDate: Date
+    value: string
+  }>
+  onFocus: CustomEvent<{ component: "duet-date-picker" }>
+  onOpen: CustomEvent<{ component: "duet-date-picker" }>
+  onClose: CustomEvent<{ component: "duet-date-picker" }>
+  dateAdapter: DuetDateAdapter
+
+  // TODO: type this
+  localization: Record<string, unknown>
 }
 
 export const DatePickerWrapper = ({
@@ -22,7 +42,17 @@ export const DatePickerWrapper = ({
   dateAdapter,
   localization,
   ...props
-}) => {
+}: DatePickerProps) => {
+  function useListener(ref, eventName, handler) {
+    useEffect(() => {
+      if (ref.current) {
+        const element = ref.current
+        element.addEventListener(eventName, handler)
+        return () => element.removeEventListener(eventName, handler)
+      }
+    }, [eventName, handler, ref])
+  }
+
   const ref = useRef(null)
 
   useListener(ref, "duetChange", onChange)
@@ -32,13 +62,20 @@ export const DatePickerWrapper = ({
   useListener(ref, "duetClose", onClose)
 
   useEffect(() => {
-    ref.current.localization = localization
-    ref.current.dateAdapter = dateAdapter
+    if (ref.current) {
+      ref.current.localization = localization
+      ref.current.dateAdapter = dateAdapter
+    }
   }, [localization, dateAdapter])
 
   return <duet-date-picker ref={ref} {...props}></duet-date-picker>
 }
 
 export const DatePicker = () => (
-  <DatePickerWrapper value="2020-08-24" onChange={e => console.log(e.detail)} />
+  <div className={styles.wrapper}>
+    <DatePickerWrapper
+      value="2020-08-24"
+      onChange={e => console.log(e.detail)}
+    />
+  </div>
 )
