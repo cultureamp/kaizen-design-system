cfc20e5dbb1c654e34f5ebe92d4466e9a3bf986b
Viet Anh Tran <viet.anhtran@cultureamp.com>
2022-01-14 11:42:20 +1300

    feat: make ThemeManager prerendering friendly

--- packages/design-tokens/src/__tests__/ThemeManager.spec.ts
+++ packages/design-tokens/src/__tests__/ThemeManager.spec.ts
@@ -3,10 +3,10 @@ import { ThemeManager } from "../ThemeManager"
 import { zenTheme } from "../themes"
 import { Theme } from "../types"
 
-const assertThemeIsActive = (theme: Theme, rootElement: HTMLElement) => {
+const assertThemeIsActive = (theme: Theme, rootElement: HTMLElement | null) => {
   const variables = makeCssVariableDefinitionsMap(theme)
   Object.entries(variables).forEach(([key, value]) => {
-    expect(rootElement.style.getPropertyValue(key)).toBe(value)
+    expect(rootElement?.style.getPropertyValue(key)).toBe(value)
   })
 }
 
@@ -14,7 +14,7 @@ describe(ThemeManager.name, () => {
   // TODO: JSDom/CSSOM doesn't support CSS variables, as it seems. Figure out a way to test them. Maybe jest or jsdom just needs an upgrade.
   test.skip("activates initial theme on construction", () => {
     const rootElement = document.documentElement
-    new ThemeManager(zenTheme, rootElement)
-    assertThemeIsActive(zenTheme, rootElement)
+    const themeManager = new ThemeManager(zenTheme)
+    assertThemeIsActive(zenTheme, themeManager.getRootElement())
   })
 })
