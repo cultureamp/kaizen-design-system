4a14145d6edd30dc4d4d9441993ad06f28384c04
mcwinter07 <mcwinter07@gmail.com>
2022-01-17 18:27:40 +1100

    * feat: remove stories, add sticker sheet wrappers and add demo story

--- draft-packages/form/docs/TextAreaField.stories.tsx
+++ draft-packages/form/docs/TextAreaField.stories.tsx
@@ -1,57 +1,41 @@
 import { TextAreaField } from "@kaizen/draft-form"
 import { withDesign } from "storybook-addon-designs"
 import React from "react"
+import { Heading } from "@kaizen/component-library"
 import { figmaEmbed } from "../../../storybook/helpers"
 import { CATEGORIES, SUB_CATEGORIES } from "../../../storybook/constants"
 
-interface RenderProps {
-  controlledValue: string
-  updateValue: (event: React.ChangeEvent<HTMLTextAreaElement>) => any
-}
-
-interface Props {
-  render: (props: RenderProps) => JSX.Element
-  defaultValue: string
-}
-
-class WithState extends React.Component<Props> {
-  public state = {
-    controlledValue: this.props.defaultValue,
-  }
-
-  constructor(props: Props) {
-    super(props)
-    this.updateValue = this.updateValue.bind(this)
-  }
-
-  public updateValue(event: React.ChangeEvent<HTMLTextAreaElement>) {
-    this.setState({
-      controlledValue: event.target.value,
-    })
-  }
-
-  public render() {
-    const { render } = this.props
-    return (
-      <div>
-        {render({
-          controlledValue: this.state.controlledValue,
-          updateValue: this.updateValue,
-        })}
-      </div>
-    )
-  }
-}
-
-const ExampleContainer: React.FunctionComponent = ({ children }) => (
-  <div style={{ width: "98%", margin: "1%" }}>{children}</div>
+const StoryContainer: React.FunctionComponent = ({ children }) => (
+  <div
+    style={{ display: "inline-flex", width: "100%", flexDirection: "column" }}
+  >
+    {children}
+  </div>
 )
 
-const reversedBg = {
-  backgrounds: {
-    default: "Purple 700",
-  },
-}
+const StoryColumn: React.FunctionComponent = ({ children }) => (
+  <div
+    style={{
+      display: "grid",
+      rowGap: "25px",
+    }}
+  >
+    {children}
+  </div>
+)
+
+const StoryGrid: React.FunctionComponent = ({ children }) => (
+  <div
+    style={{
+      display: "grid",
+      gridTemplateColumns: "repeat(auto-fill, minmax(10px, 195px))",
+      columnGap: "45px",
+      rowGap: "12px",
+    }}
+  >
+    {children}
+  </div>
+)
 
 export default {
   title: `${CATEGORIES.components}/${SUB_CATEGORIES.form}/Text Area Field`,
@@ -69,226 +53,59 @@ export default {
   decorators: [withDesign],
 }
 
-export const DefaultKaizenSiteDemo = () => (
-  <ExampleContainer>
-    <TextAreaField
-      id="reply"
-      labelText="Your reply"
-      placeholder="Write your reply..."
-      onChange={() => undefined}
-    />
-  </ExampleContainer>
-)
+export const DefaultStory = args => <TextAreaField {...args} />
 
-DefaultKaizenSiteDemo.storyName = "Default (Kaizen Site Demo)"
-
-export const DefaultLarge = () => (
-  <ExampleContainer>
-    <TextAreaField
-      id="reply"
-      labelText="Your reply"
-      placeholder="Write your reply..."
-      onChange={() => undefined}
-      rows={7}
-    />
-  </ExampleContainer>
-)
-
-DefaultLarge.storyName = "Default, Large"
-
-export const DefaultError = () => (
-  <ExampleContainer>
-    <TextAreaField
-      id="reply"
-      labelText="Your reply"
-      placeholder="Write your reply..."
-      onChange={() => undefined}
-      status="error"
-      validationMessage="Enter a reply"
-    />
-  </ExampleContainer>
-)
-
-DefaultError.storyName = "Default, Error"
-
-export const DefaultPrefilled = () => (
-  <ExampleContainer>
-    <TextAreaField
-      id="reply"
-      labelText="Your reply"
-      onChange={() => undefined}
-      defaultValue="A prefilled value in uncontrolled mode"
-    />
-  </ExampleContainer>
-)
-
-DefaultPrefilled.storyName = "Default, Prefilled & uncontrolled"
-
-export const DefaultControlled = () => (
-  <ExampleContainer>
-    <WithState
-      defaultValue="A controlled value"
-      render={({ controlledValue, updateValue }) => (
-        <TextAreaField
-          id="reply"
-          labelText="Your reply"
-          value={controlledValue}
-          onChange={updateValue}
-        />
-      )}
-    />
-  </ExampleContainer>
-)
-
-DefaultControlled.storyName = "Default, Controlled"
-
-export const DefaultWithDesc = () => (
-  <ExampleContainer>
-    <TextAreaField
-      id="reply"
-      labelText="Your reply"
-      placeholder="Write your reply..."
-      onChange={() => undefined}
-      description="Your reply will only be seen by you"
-    />
-  </ExampleContainer>
-)
-
-DefaultWithDesc.storyName = "Default, With description"
-
-export const DefaultInline = () => (
-  <ExampleContainer>
-    <TextAreaField
-      id="reply"
-      labelText="Your reply"
-      placeholder="Write your reply..."
-      onChange={() => undefined}
-      inline={true}
-    />
-  </ExampleContainer>
-)
-
-DefaultInline.storyName = "Default, Inline"
-
-export const DefaultErrorAndDesc = () => (
-  <ExampleContainer>
-    <TextAreaField
-      id="reply"
-      labelText="Your reply"
-      placeholder="Write your reply..."
-      onChange={() => undefined}
-      status="error"
-      validationMessage="Enter a reply"
-      description="Your reply will only be seen by you"
-    />
-  </ExampleContainer>
-)
-
-DefaultErrorAndDesc.storyName = "Default, Error & Description"
-
-export const DefaultAutogrowControlled = () => (
-  <ExampleContainer>
-    <WithState
-      defaultValue={"This\ntext\narea\nwill\ngrow\nand\nshrink\nwith\ncontent"}
-      render={({ controlledValue, updateValue }) => (
-        <TextAreaField
-          id="reply"
-          labelText="Your reply"
-          rows={1}
-          value={controlledValue}
-          onChange={updateValue}
-          autogrow
-        />
-      )}
-    ></WithState>
-  </ExampleContainer>
-)
-
-DefaultAutogrowControlled.story = {
-  name: "Default, Autogrow Controlled",
+DefaultStory.args = {
+  id: "reply",
+  labelText: "Your reply",
+  autogrow: false,
+  inline: false,
+  reversed: false,
+  validationMessage: "",
+  description: "",
 }
 
-export const DefaultAutogrow = () => (
-  <ExampleContainer>
-    <TextAreaField
-      id="reply"
-      labelText="Your reply"
-      rows={1}
-      defaultValue="A prefilled value in uncontrolled mode"
-      autogrow
-    />
-  </ExampleContainer>
-)
-
-DefaultAutogrow.story = {
-  name: "Default, Autogrow",
+DefaultStory.argTypes = {
+  textAreaRef: {
+    control: {
+      disable: true,
+    },
+  },
+  autogrow: {
+    defaultValue: { summary: "false" },
+  },
+  status: {
+    defaultValue: "default",
+  },
+  variant: {
+    defaultValue: "default",
+  },
 }
 
-export const Reversed = () => (
-  <ExampleContainer>
-    <TextAreaField
-      id="reply"
-      labelText="Your reply"
-      placeholder="Write your reply..."
-      onChange={() => undefined}
-      reversed
-    />
-  </ExampleContainer>
+DefaultStory.storyName = "Default (Kaizen Demo)"
+
+export const StickerSheetDefault = () => (
+  <StoryGrid>
+    <StoryColumn>
+      <Heading variant="heading-5" tag="h3">
+        Base
+      </Heading>
+      <StoryContainer></StoryContainer>
+    </StoryColumn>
+  </StoryGrid>
 )
 
-Reversed.storyName = "Reversed"
-Reversed.parameters = { ...reversedBg }
+StickerSheetDefault.storyName = "Sticker Sheet (Default)"
 
-export const ReversedErrorAndDesc = () => (
-  <ExampleContainer>
-    <TextAreaField
-      id="reply"
-      labelText="Your reply"
-      placeholder="Write your reply..."
-      onChange={() => undefined}
-      status="error"
-      validationMessage="Enter a reply"
-      description="Your reply will only be seen by you"
-      reversed
-    />
-  </ExampleContainer>
+export const StickerSheetReversed = () => (
+  <StoryGrid>
+    <StoryColumn>
+      <Heading variant="heading-5" color="white" tag="h3">
+        Base
+      </Heading>
+      <StoryContainer></StoryContainer>
+    </StoryColumn>
+  </StoryGrid>
 )
 
-ReversedErrorAndDesc.storyName = "Reversed, Error & Description"
-ReversedErrorAndDesc.parameters = { ...reversedBg }
-
-export const DefaultProminent = () => (
-  <ExampleContainer>
-    <TextAreaField id="reply" labelText="Your reply" variant="prominent" />
-  </ExampleContainer>
-)
-
-DefaultProminent.storyName = "Default, Prominent"
-
-export const DefaultProminentDesc = () => (
-  <ExampleContainer>
-    <TextAreaField
-      id="reply"
-      labelText="Your reply"
-      variant="prominent"
-      description="Your reply will only be seen by you"
-    />
-  </ExampleContainer>
-)
-
-DefaultProminentDesc.storyName = "Default, Prominent with description"
-
-export const ReversedProminent = () => (
-  <ExampleContainer>
-    <TextAreaField
-      id="reply"
-      labelText="Your reply"
-      variant="prominent"
-      description="Your reply will only be seen by you"
-      reversed
-    />
-  </ExampleContainer>
-)
-
-ReversedProminent.storyName = "Reversed, Prominent"
-ReversedProminent.parameters = { ...reversedBg }
+StickerSheetReversed.storyName = "Sticker Sheet (Reversed)"
