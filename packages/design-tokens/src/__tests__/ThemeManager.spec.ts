34492ba325ae5b1dd86304bc279bd03eb90ccc87
Viet Anh Tran <viet.anhtran@cultureamp.com>
2022-01-18 15:37:26 +1300

    fix: remove redundant variable

--- packages/design-tokens/src/__tests__/ThemeManager.spec.ts
+++ packages/design-tokens/src/__tests__/ThemeManager.spec.ts
@@ -13,7 +13,6 @@ const assertThemeIsActive = (theme: Theme, rootElement: HTMLElement | null) => {
 describe(ThemeManager.name, () => {
   // TODO: JSDom/CSSOM doesn't support CSS variables, as it seems. Figure out a way to test them. Maybe jest or jsdom just needs an upgrade.
   test.skip("activates initial theme on construction", () => {
-    const rootElement = document.documentElement
     const themeManager = new ThemeManager(zenTheme)
     assertThemeIsActive(zenTheme, themeManager.getRootElement())
   })
