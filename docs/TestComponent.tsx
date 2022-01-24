aff97705946293ce5aa1699b4f299edc0008997e
Cassandra Tam <cassandra.tam@cultureamp.com>
2022-01-24 17:12:16 +1100

    fixup! docs: add boolean default discussion point and examples

--- docs/TestComponent.tsx
+++ docs/TestComponent.tsx
@@ -10,3 +10,8 @@ const Component: React.VFC<Props> = ({ isBool }) => {
   const v = doSomething(isBool);
   return <div />;
 }
+
+const AnotherComponent: React.VFC<Props> = ({ isBool = false }) => {
+  const v = doSomething(isBool);
+  return <div />;
+}
