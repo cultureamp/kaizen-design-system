6f08010a044518e44c36e83e3d8b378d6bd35d57
Cassandra Tam <cassandra.tam@cultureamp.com>
2022-01-24 17:14:17 +1100

    fixup! docs: add boolean default discussion point and examples

--- docs/TestComponent.tsx
+++ docs/TestComponent.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 type Props = {
   isBool?: boolean;
@@ -7,11 +7,14 @@ type Props = {
 const doSomething = (isSomething: boolean): boolean => isSomething;
 
 const Component: React.VFC<Props> = ({ isBool }) => {
+  // Argument of type 'boolean | undefined' is not assignable to parameter of type 'boolean'.
+  // Type 'undefined' is not assignable to type 'boolean'.
   const v = doSomething(isBool);
   return <div />;
 }
 
 const AnotherComponent: React.VFC<Props> = ({ isBool = false }) => {
+  // No error
   const v = doSomething(isBool);
   return <div />;
 }
