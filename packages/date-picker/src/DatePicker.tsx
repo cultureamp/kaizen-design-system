67b5e3f7045a160b6fadf9f9b8f012aa827143d7
Lloyd Stubber <lloyd@squareyes.info>
2022-01-27 16:43:42 +1100

    wip: start adding props and basic styling

--- packages/date-picker/src/DatePicker.tsx
+++ packages/date-picker/src/DatePicker.tsx
@@ -1,24 +1,37 @@
+import { Label } from "@kaizen/draft-form"
 import React from "react"
 import DayPickerInput from "react-day-picker/DayPickerInput"
 import "react-day-picker/lib/style.css"
+import styles from "./DatePicker.scss"
 
 type DatePickerProps = {
   selectedDate?: Date
   handleDayChange: (day: Date) => void
+  label?: string
 }
 
-export const DatePickerWrapper = (props: DatePickerProps) => (
+export const DatePickerWrapper: React.FunctionComponent<DatePickerProps> = ({
+  selectedDate,
+  handleDayChange,
+  label,
+}) => (
   <>
-    <p>this is the date: {props.selectedDate?.toString()}</p>
-    <DayPickerInput
-      value={props.selectedDate}
-      onDayChange={props.handleDayChange}
-      dayPickerProps={{
-        selectedDays: props.selectedDate,
-        disabledDays: {
-          daysOfWeek: [0, 6],
-        },
-      }}
-    />
+    {label && <Label>{label}</Label>}
+    <div className={styles.wrapper}>
+      <DayPickerInput
+        value={selectedDate}
+        onDayChange={handleDayChange}
+        dayPickerProps={{
+          selectedDays: selectedDate,
+          disabledDays: {
+            daysOfWeek: [0, 6],
+          },
+        }}
+      />
+
+      {/* Inputs aren't able to have pseudo elements like ::after on them,
+          so we have to create an element ourselves for the focus ring */}
+      <div className={styles.focusRing} />
+    </div>
   </>
 )
