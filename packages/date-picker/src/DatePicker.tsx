e015828c1f709ed1098f69cf75cf9a93f1531beb
Natalie Loyzaga <nloyzaga@gmail.com>
2022-02-03 10:54:59 +1100

    feat: update Navbar typing and overlay styles

--- packages/date-picker/src/DatePicker.tsx
+++ packages/date-picker/src/DatePicker.tsx
@@ -7,7 +7,6 @@ import arrowLeft from "@kaizen/component-library/icons/arrow-left.icon.svg"
 import classnames from "classnames"
 import DayPickerInput from "react-day-picker/DayPickerInput"
 import "react-day-picker/lib/style.css"
-// import DayPicker from "react-day-picker/DayPicker"
 import styles from "./DatePicker.scss"
 import calendarStyles from "./Calendar.scss"
 
@@ -23,46 +22,57 @@ export const DatePickerWrapper: React.FunctionComponent<DatePickerProps> = ({
   onDayChange,
   labelText,
   isDisabled = false,
-}) => (
-  <>
-    {labelText && <Label disabled={isDisabled} labelText={labelText} />}
-    <div
-      className={classnames(
-        styles.wrapper,
-        styles.withStartIconAdornment,
-        styles.withSearch,
-        {
-          [styles.withDisabled]: isDisabled,
-        }
-      )}
-    >
-      <div className={styles.startIconAdornment}>
-        <Icon icon={dateStart} role="presentation" />
+}) => {
+  const getNavbar = ({ ...navbarProps }: NavbarProps) => (
+    <Navbar {...navbarProps} />
+  )
+  return (
+    <>
+      {labelText && <Label disabled={isDisabled} labelText={labelText} />}
+      <div
+        className={classnames(
+          styles.wrapper,
+          styles.withStartIconAdornment,
+          styles.withSearch,
+          {
+            [styles.withDisabled]: isDisabled,
+          }
+        )}
+      >
+        <div className={styles.startIconAdornment}>
+          <Icon icon={dateStart} role="presentation" />
+        </div>
+        <DayPickerInput
+          value={selectedDate}
+          onDayChange={onDayChange}
+          classNames={defaultDayPickerInputClasses}
+          dayPickerProps={{
+            selectedDays: selectedDate,
+            disabledDays: {
+              daysOfWeek: [0, 6],
+            },
+            navbarElement: getNavbar,
+            classNames: defaultDatePickerClasses,
+            className: calendarStyles.calendar,
+            onDayClick: onDayChange,
+          }}
+          inputProps={{
+            disabled: isDisabled,
+            className: classnames(styles.input, {
+              [styles.disabled]: isDisabled,
+            }),
+          }}
+        />
       </div>
-      <DayPickerInput
-        value={selectedDate}
-        onDayChange={onDayChange}
-        overlayComponent={CustomOverlay}
-        dayPickerProps={{
-          selectedDays: selectedDate,
-          disabledDays: {
-            daysOfWeek: [0, 6],
-          },
-          navbarElement: ({ ...navbarProps }) => <Navbar {...navbarProps} />,
-          classNames: defaultDatePickerClasses,
-          className: calendarStyles.calendar,
-          onDayClick: onDayChange,
-        }}
-        inputProps={{
-          disabled: isDisabled,
-          className: classnames(styles.input, {
-            [styles.disabled]: isDisabled,
-          }),
-        }}
-      />
-    </div>
-  </>
-)
+    </>
+  )
+}
+
+export const defaultDayPickerInputClasses = {
+  container: "DayPickerInput-Container",
+  overlayWrapper: "DayPickerInput-OverlayWrapper",
+  overlay: `${styles.overlay} DayPickerInput-Overlay`,
+}
 
 export const defaultDatePickerClasses = {
   container: "DayPicker",
@@ -88,16 +98,6 @@ export const defaultDatePickerClasses = {
   selected: `${calendarStyles.selected} DayPicker-Day--selected`,
   disabled: `${calendarStyles.disabled} DayPicker-Day--disabled`,
   outside: `${calendarStyles.outside} DayPicker-Day--outside`,
-}
-
-function CustomOverlay({ classNames, selectedDay, children, ...props }) {
-  return (
-    <div className="DayPickerInput-OverlayWrapper" {...props}>
-      <div className={`${calendarStyles.overlay} DayPickerInput-Overlay`}>
-        {children}
-      </div>
-    </div>
-  )
 }
 
 type NavbarProps = {
@@ -126,22 +126,3 @@ const Navbar: React.FunctionComponent<NavbarProps> = ({
     </button>
   </div>
 )
-
-// type CalendarProps = {
-//   selectedDate?: Date
-//   handleDayChange: (day: Date) => void
-// }
-
-// export const Calendar: React.FunctionComponent<CalendarProps> = ({
-//   selectedDate,
-//   handleDayChange,
-// }) => (
-//   <DayPicker
-//     navbarElement={<Navbar />}
-//     classNames={defaultDatePickerClasses}
-//     className={calendarStyles.calendar}
-//     selectedDays={selectedDate}
-//     onDayClick={handleDayChange}
-//     disabledDays={[new Date(2022, 1, 16), { daysOfWeek: [0, 6] }]}
-//   />
-// )
