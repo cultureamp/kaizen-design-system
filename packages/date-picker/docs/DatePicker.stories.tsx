f9d6da6c857d201a0088619f8a75a7078c962cbc
Lloyd Stubber <lloyd@squareyes.info>
2022-02-17 09:14:58 +1100

    feat: DatePicker  (#2526)
    * feat: add basic files and simple date picker example
    
    * wip: start adding props and basic styling
    
    * Update styles
    
    * feat: clean up input styling for states
    
    * feat: Update package.json and stories
    
    * feat: Update input styling and prop names.
    
    * feat: date picker input and calendar styling (#2511)
    
    * WIP: calendar styles and stories
    
    * feat: update cal styles and add to DatePickerInput
    
    * remove Calendar component from stories
    
    * typescript fix for Navbar
    
    * feat: update Navbar typing and overlay styles
    
    * wip: save progress before trying to use our own input
    
    * wip: style calendar to suit ui kit
    
    * update calendar day spacing and variables
    
    * feat: clean up structure of component and remove uneeded logic
    
    * feat: update stories
    
    * fix: remove react-focus-lock
    
    * feat: update arrow hover styling and day font colour
    
    * feat: update today day colour
    
    * feat: pr feedback. add more essential props around aria
    
    * fix: pr feedback to change from FC to VFC
    
    * fix: pr feedback - update focus styles for moz
    
    Co-authored-by: Lloyd Stubber <lloyd@squareyes.info>
    
    * feat: add basic popup functionality and click outside close
    
    * feat: close datepicker on day selection
    
    * feat: add props and ability for consumers to disable days
    
    * feat: rejig file, fix types of day converting function and add comments
    
    * feat: add functionality of no events or styles for disables days
    
    * fix: restyle today + disabled to match design
    
    * feat: add a11y support for calendar popup
    
    * feat: add formatting to the date when rendering in input
    
    * feat: handle multiple ways of disabling dates and ranges
    
    * feat: clean up file, document disabled props and split out util
    
    * feat: add more description to props for consumer and rejig layout of component folder structure
    
    * feat: make component completely controlled and add basic test
    
    * feat: add interactivity tests for datepicker component
    
    * feat: add CalendarNav and dayToNumbers tests
    
    * feat: rebase and update yarn.lock
    
    * feat: remove unneeded comment
    
    * feat: update prop description
    
    * feat: add disabled dates story
    
    * feat: remove unneeded package
    
    * fix: pr feedback
    
    * pr feedback: update prop to be value instead of selectedDate
    
    * chore: update yarn lock
    
    Co-authored-by: Natalie Loyzaga <nloyzaga@gmail.com>
    Co-authored-by: Natalie Loyzaga <48232362+NatalieLoyzaga@users.noreply.github.com>

---
+++ packages/date-picker/docs/DatePicker.stories.tsx
@@ -0,0 +1,104 @@
+import React, { useState } from "react"
+import { CATEGORIES } from "../../../storybook/constants"
+import { DatePicker } from "../src/DatePicker"
+
+export default {
+  title: `${CATEGORIES.components}/Date Picker`,
+  component: DatePicker,
+  parameters: {
+    docs: {
+      description: {
+        component: 'import { DatePicker } from "@kaizen/date-picker"',
+      },
+    },
+  },
+}
+
+export const KaizenDefault = props => {
+  const [selectedDate, setSelectedDate] = useState<Date | undefined>()
+
+  const onDayChange = (day: Date) => {
+    setSelectedDate(day)
+  }
+
+  return (
+    <DatePicker
+      id="make-me-unique-1"
+      labelText="Label"
+      value={selectedDate}
+      onChange={onDayChange}
+    />
+  )
+}
+
+export const DefaultWithDisabledDates = () => {
+  const [selectedDate, setSelectedDate] = useState<Date | undefined>()
+
+  const onDayChange = (day: Date) => {
+    setSelectedDate(day)
+  }
+
+  return (
+    <DatePicker
+      id="make-me-unique-2"
+      labelText="Label"
+      onChange={onDayChange}
+      disabledRange={{ from: new Date(2022, 1, 14), to: new Date(2022, 1, 16) }}
+      disabledDates={[new Date(2022, 1, 25)]}
+      disabledBefore={new Date()}
+    />
+  )
+}
+
+export const DefaultInputWithValue = () => {
+  const [selectedDate, setSelectedDate] = useState<Date | undefined>()
+
+  const onDayChange = (day: Date) => {
+    setSelectedDate(day)
+  }
+
+  return (
+    <DatePicker
+      id="make-me-unique-3"
+      labelText="Label"
+      value={new Date()}
+      onChange={onDayChange}
+    />
+  )
+}
+
+export const DisabledInput = () => {
+  const [selectedDate, setSelectedDate] = useState<Date | undefined>()
+
+  const onDayChange = (day: Date) => {
+    setSelectedDate(day)
+  }
+
+  return (
+    <DatePicker
+      id="make-me-unique-4"
+      labelText="Label"
+      value={selectedDate}
+      onChange={onDayChange}
+      isDisabled
+    />
+  )
+}
+
+export const DisabledInputWithValue = () => {
+  const [selectedDate, setSelectedDate] = useState<Date | undefined>()
+
+  const onDayChange = (day: Date) => {
+    setSelectedDate(day)
+  }
+
+  return (
+    <DatePicker
+      id="make-me-unique-5"
+      labelText="Label"
+      value={new Date()}
+      onChange={onDayChange}
+      isDisabled
+    />
+  )
+}
