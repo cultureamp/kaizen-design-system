b28765b7b2c6bde57e604db727cffb2e4eac6258
Lloyd Stubber <lloyd@squareyes.info>
2022-02-22 14:18:05 +0800

    feat: add test to make sure focus is on selected day

--- packages/date-picker/src/DatePicker/components/Calendar/Calendar.spec.tsx
+++ packages/date-picker/src/DatePicker/components/Calendar/Calendar.spec.tsx
@@ -1,47 +1,27 @@
-// import { act, render, screen } from "@testing-library/react"
-// import React from "react"
-// import userEvent from "@testing-library/user-event"
-// import { Calendar, CalendarNavProps, CalendarProps } from "./Calendar"
-// import "@testing-library/jest-dom"
+import { render, screen, cleanup } from "@testing-library/react"
+import React from "react"
+import { Calendar, CalendarProps } from "./Calendar"
+import "@testing-library/jest-dom"
 
-// const defaultProps: CalendarProps = {
-//   handleKeyDown: jest.fn(),
-//   handleOnDayChange: jest.fn(),
-//   firstDayOfWeek: 1,
-//   setPopperElement: jest.fn(),
-//   styles: {},
-//   attributes: {},
-// }
+const defaultProps: CalendarProps = {
+  value: new Date(2022, 2, 1),
+  initialMonth: new Date(2022, 2),
+  handleKeyDown: jest.fn(),
+  handleOnDayChange: jest.fn(),
+  firstDayOfWeek: 1,
+  setPopperElement: jest.fn(),
+  styles: {},
+  attributes: {},
+}
 
-// const navbarProps: CalendarNavProps = {
-//   onPreviousClick: jest.fn(),
-//   onNextClick: jest.fn(),
-// }
+describe("<Calendar />", () => {
+  it("show focus on selected day", async () => {
+    render(<Calendar {...defaultProps} />)
 
-// describe("<Calendar />", () => {
-//   it("calls onPreviousClick and onNextClick when buttons are clicked", async () => {
-//     render(<Calendar {...defaultProps} {...navbarProps} />)
+    const selectedDate = screen.getByRole("gridcell", {
+      name: "Tue Mar 01 2022",
+    })
 
-//     const previousButton = screen.getByRole("button", {
-//       name: "Previous month",
-//     })
-
-//     const nextButton = screen.getByRole("button", {
-//       name: "Next month",
-//     })
-
-//     await act(async () => {
-//       previousButton.focus()
-//       userEvent.click(previousButton)
-//     })
-
-//     expect(navbarProps.onPreviousClick).toBeCalledTimes(1)
-
-//     await act(async () => {
-//       nextButton.focus()
-//       userEvent.click(nextButton)
-//     })
-
-//     expect(navbarProps.onNextClick).toBeCalledTimes(1)
-//   })
-// })
+    expect(selectedDate).toHaveFocus()
+  })
+})
