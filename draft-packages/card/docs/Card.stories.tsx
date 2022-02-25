5df1d154eb813b05585a3702b4d9741e6e419a4e
Natalie Loyzaga <nloyzaga@gmail.com>
2022-02-25 14:20:24 +1100

    feat: update Card to container and use storyWrapper

--- draft-packages/card/docs/Card.stories.tsx
+++ draft-packages/card/docs/Card.stories.tsx
@@ -1,13 +1,12 @@
 import React from "react"
-import { Box, Heading } from "@kaizen/component-library"
 import { withDesign } from "storybook-addon-designs"
-import { Card, CardProps } from ".."
+import { Card } from ".."
 import { figmaEmbed } from "../../../storybook/helpers"
 import { CATEGORIES } from "../../../storybook/constants"
-import cardStoriesData from "./card-stories-data.json"
+import { StoryWrapper } from "../../../storybook/components/StoryWrapper"
 
 export default {
-  title: `${CATEGORIES.components}/Card`,
+  title: `${CATEGORIES.components}/Container`,
   component: Card,
   parameters: {
     docs: {
@@ -24,7 +23,7 @@ export default {
 }
 
 export const DefaultStory = args => (
-  <Card {...args}>This is a default card</Card>
+  <Card {...args}>This is a default container</Card>
 )
 DefaultStory.storyName = "Default (Kaizen Site Demo)"
 DefaultStory.args = {
@@ -32,31 +31,38 @@ DefaultStory.args = {
   variant: "default",
 }
 
-export const DesignSheetDefault = () => (
-  <div
-    style={{
-      display: "grid",
-      gridTemplateColumns: "repeat(auto-fill, minmax(100px, max-content))",
-      gap: "2.5rem",
-    }}
-  >
-    {cardStoriesData?.map(story => (
-      <div>
-        <Heading tag="h4" variant="heading-4">
-          {story.title}
-        </Heading>
-        <br />
-        {(story.stories as CardProps[]).map(cardProps => (
-          <>
-            <Card {...cardProps}>
-              <Box p={1}>{/* child content here */}</Box>
-            </Card>
-            <br />
-          </>
-        ))}
-      </div>
-    ))}
-  </div>
+export const StickerSheetDefault = () => (
+  <StoryWrapper>
+    <StoryWrapper.RowHeader
+      headings={[
+        "Base",
+        "Informative",
+        "Positive",
+        "Cautionary",
+        "Destructive",
+        "Assertive",
+        "Highlight",
+      ]}
+    />
+    <StoryWrapper.Row rowTitle="Default">
+      <Card variant="default" tag="div" />
+      <Card variant="informative" tag="div" />
+      <Card variant="positive" tag="div" />
+      <Card variant="cautionary" tag="div" />
+      <Card variant="destructive" tag="div" />
+      <Card variant="assertive" tag="div" />
+      <Card variant="highlight" tag="div" />
+    </StoryWrapper.Row>
+    <StoryWrapper.Row rowTitle="Description">
+      <Card variant="default" tag="div" isElevated />
+      <Card variant="informative" tag="div" isElevated />
+      <Card variant="positive" tag="div" isElevated />
+      <Card variant="cautionary" tag="div" isElevated />
+      <Card variant="destructive" tag="div" isElevated />
+      <Card variant="assertive" tag="div" isElevated />
+      <Card variant="highlight" tag="div" isElevated />
+    </StoryWrapper.Row>
+  </StoryWrapper>
 )
-DesignSheetDefault.storyName = "Design Sheet (default)"
-DesignSheetDefault.parameters = { chromatic: { disable: false } }
+
+StickerSheetDefault.storyName = "Sticker Sheet (Default)"
