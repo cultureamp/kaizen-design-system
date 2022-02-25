6ba57648456e7498e291f27beedfc24f2dc7c154
Natalie Loyzaga <nloyzaga@gmail.com>
2022-02-25 12:35:19 +1100

    feat: allow for margin top and bottom

--- storybook/components/StoryWrapper/StoryWrapper.tsx
+++ storybook/components/StoryWrapper/StoryWrapper.tsx
@@ -11,6 +11,8 @@ export interface StoryWrapperProps {
   hasNoRowTitles?: boolean
   isReversed?: boolean
   children: Array<React.ReactElement<StoryRowHeaderProps | StoryRowProps>>
+  mt?: boolean
+  mb?: boolean
 }
 
 type Subcomponents = {
@@ -22,6 +24,8 @@ export const StoryWrapper: React.VFC<StoryWrapperProps> & Subcomponents = ({
   hasNoRowTitles = false,
   children,
   isReversed = false,
+  mt = false,
+  mb = false,
 }) => {
   const childrenCount: number = React.Children.count(children)
 
@@ -29,6 +33,8 @@ export const StoryWrapper: React.VFC<StoryWrapperProps> & Subcomponents = ({
     <div
       className={classNames(styles.storyRowContainer, {
         [styles.noRowTitles]: hasNoRowTitles,
+        [styles.marginTop]: mt,
+        [styles.marginBottom]: mb,
       })}
       style={{
         gridTemplateRows: `repeat(${childrenCount}, min-content)`,
