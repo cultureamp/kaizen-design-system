e8ce1600116de0bf41fe0ce843ea5fe28401d958
Lloyd Stubber <lloyd@squareyes.info>
2022-03-01 11:06:50 +0800

    feat: convert date picker input to button for better a11y support (#2543)
    * feat: convert date picker input to button for better a11y support, restructure and split a part components.
    
    * fix: remove unneeded packages
    
    * feat: add test to make sure focus is on selected day
    
    * feat: return focus to button element after closing popup
    
    * feat: return focus to button when popup closes
    
    * feat: cleanup return focus to use single button ref
    
    * fix: slim down condiitonal
    
    * feat: override inital month when date is selected
    
    * feat: refactor and update file structure
    
    * feat: handleDisableDays util
    
    * feat: move handleDisabledDays to util and include test
    
    * feat: set timezone for handleDsiabledDays tests
    
    * update tests to use UTC timing
    
    * feat: update stories to enable selectedDate state
    
    * fix: pr feedback - update scss and handler function names
    
    * fix: pr feedback - convert to VFC and update tests
    
    * fix: pr feedback - add displayName to components, update function name and move mixins
    
    Co-authored-by: Natalie Loyzaga <nloyzaga@gmail.com>
    Co-authored-by: Natalie Loyzaga <48232362+NatalieLoyzaga@users.noreply.github.com>

---
+++ packages/date-picker/src/DatePicker/components/Calendar/Calendar.spec.tsx
@@ -0,0 +1,27 @@
+import { render, screen, cleanup } from "@testing-library/react"
+import React from "react"
+import { Calendar, CalendarProps } from "./Calendar"
+import "@testing-library/jest-dom"
+
+const defaultProps: CalendarProps = {
+  value: new Date(2022, 2, 1),
+  initialMonth: new Date(2022, 2),
+  onKeyDown: jest.fn(),
+  onDayChange: jest.fn(),
+  firstDayOfWeek: 1,
+  setPopperElement: jest.fn(),
+  styles: {},
+  attributes: {},
+}
+
+describe("<Calendar />", () => {
+  it("show focus on selected day", async () => {
+    render(<Calendar {...defaultProps} />)
+
+    const selectedDate = screen.getByRole("gridcell", {
+      name: "Tue Mar 01 2022",
+    })
+
+    expect(selectedDate).toHaveFocus()
+  })
+})
