6c34bd0942a4db917448c21c5be6179752e31ea0
Natalie Loyzaga <nloyzaga@gmail.com>
2022-03-24 10:31:07 +1100

    feat: update stories to use storyWrapper

--- packages/loading-skeleton/docs/LoadingSkeleton.stories.tsx
+++ packages/loading-skeleton/docs/LoadingSkeleton.stories.tsx
@@ -1,8 +1,11 @@
 import React from "react"
+import { Story } from "@storybook/react"
 import { withDesign } from "storybook-addon-designs"
+import { Paragraph } from "@kaizen/typography"
 import { CATEGORIES, SUB_CATEGORIES } from "../../../storybook/constants"
 import { LoadingParagraph } from "../src/LoadingSkeleton"
 import { figmaEmbed } from "../../../storybook/helpers"
+import { StoryWrapper } from "../../../storybook/components/StoryWrapper"
 
 export default {
   title: `${CATEGORIES.components}/${SUB_CATEGORIES.loadingSkeleton}`,
@@ -24,3 +27,45 @@ export default {
 
 export const DefaultLoadingParagraph = args => <LoadingParagraph {...args} />
 DefaultLoadingParagraph.storyName = "Loading Paragraph"
+
+const StickerSheetTemplate: Story<{ isReversed: boolean }> = ({
+  isReversed,
+}) => (
+  <StoryWrapper isReversed={isReversed}>
+    <StoryWrapper.RowHeader headings={["Loading Skeleton", "Example"]} />
+    <StoryWrapper.Row rowTitle="Paragraph">
+      <div>
+        <LoadingParagraph isReversed={isReversed} />
+        <LoadingParagraph isReversed={isReversed} />
+      </div>
+      <Paragraph variant="body" color={isReversed ? "white" : "dark"}>
+        Kaizen is Culture Amp’s design system. It’s the single source of truth
+        for our UX guidelines, design assets, and front-end code to help Culture
+        Amp’s teams rapidly create a world-class experience.
+      </Paragraph>
+    </StoryWrapper.Row>
+    <StoryWrapper.Row rowTitle="Paragraph Link">
+      <div>
+        <LoadingParagraph isLink isReversed={isReversed} />
+        <LoadingParagraph isLink isReversed={isReversed} />
+      </div>
+      <Paragraph variant="body" color={isReversed ? "white" : "dark"}>
+        Kaizen is Culture Amp’s design system. It’s the single source of truth
+        for our UX guidelines, design assets, and front-end code to help Culture
+        Amp’s teams rapidly create a world-class experience.
+      </Paragraph>
+    </StoryWrapper.Row>
+  </StoryWrapper>
+)
+
+export const StickerSheetDefault = StickerSheetTemplate.bind({})
+StickerSheetDefault.storyName = "Sticker Sheet (Default)"
+StickerSheetDefault.parameters = { chromatic: { disable: false } }
+
+export const StickerSheetReversed = StickerSheetTemplate.bind({})
+StickerSheetReversed.storyName = "Sticker Sheet (Reversed)"
+StickerSheetReversed.args = { isReversed: true }
+StickerSheetReversed.parameters = {
+  backgrounds: { default: "Purple 700" },
+  chromatic: { disable: false },
+}
