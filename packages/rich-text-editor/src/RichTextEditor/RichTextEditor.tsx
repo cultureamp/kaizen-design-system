5e9deafa545623bd084b087312fd954e74e98ad6
Michael Winter <36558508+mcwinter07@users.noreply.github.com>
2022-04-08 13:49:04 +1000

    feat: Rich Text Editor toolbar integration (#2681)
    * feat: add toolbar to kaizen RTE
    
    * update create initial state fanctionality and add to RichTextEditor
    
    * update rte wrapper styles and focus placement
    
    * add tests for toolbar keyboard navigation
    
    * update focus visible state & update edtorwrapper styles
    
    * remove code comments from toolbar
    
    * update rte mark, type and schema to bold and italic
    
    * create row type
    
    * add uuid for content controls id and editor wrapper

--- packages/rich-text-editor/src/RichTextEditor/RichTextEditor.tsx
+++ packages/rich-text-editor/src/RichTextEditor/RichTextEditor.tsx
@@ -3,48 +3,36 @@ import { v4 } from "uuid"
 import classnames from "classnames"
 import { history } from "prosemirror-history"
 import { keymap } from "prosemirror-keymap"
-import { EditorState } from "prosemirror-state"
 import { Node, Schema } from "prosemirror-model"
+import { EditorState } from "prosemirror-state"
 import { Label } from "@kaizen/draft-form"
 import { baseKeymap } from "prosemirror-commands"
-import { useRichTextEditor } from "@cultureamp/rich-text-toolkit"
+import {
+  useRichTextEditor,
+  markIsActive,
+  removeMark,
+  addMark,
+} from "@cultureamp/rich-text-toolkit"
 import { OverrideClassName } from "@kaizen/component-base"
-import { EditorContentArray } from "./types"
+import { EditorContentArray, EditorRows } from "./types"
 import { createSchemaFromControls } from "./schema"
 import { buildKeymap } from "./keymap"
 import { buildInputRules } from "./inputrules"
 import styles from "./RichTextEditor.scss"
+import { Toolbar, ToolbarSection, ToggleIconButton } from "./"
 
+type ToolbarControls = "bold" | "italic" | "underline"
 export interface RichTextEditorProps
   extends OverrideClassName<Omit<HTMLAttributes<HTMLDivElement>, "onChange">> {
   onChange: (content: EditorContentArray) => void
   value: EditorContentArray
   labelText: ReactNode
+  controls?: ToolbarControls[][]
   /**
    * Sets a default min-height for the editable area in units of body paragraph line height, similar to the 'rows' attribute on <textarea>.
    * The editable area will autogrow, so this only affects the component when the content doesn't exceed this height.
    */
-  rows?:
-    | 1
-    | 2
-    | 3
-    | 4
-    | 5
-    | 6
-    | 7
-    | 8
-    | 9
-    | 10
-    | 11
-    | 12
-    | 13
-    | 14
-    | 15
-    | 16
-    | 17
-    | 18
-    | 19
-    | 20
+  rows?: EditorRows
 }
 
 export const RichTextEditor: React.VFC<RichTextEditorProps> = props => {
@@ -52,13 +40,17 @@ export const RichTextEditor: React.VFC<RichTextEditorProps> = props => {
     onChange,
     value,
     labelText,
-    rows = 3,
     classNameOverride,
+    controls,
+    rows = 3,
     ...restProps
   } = props
-  const [schema] = useState<Schema>(createSchemaFromControls([]))
+  const [schema] = useState<Schema>(
+    createSchemaFromControls(controls?.flat() || [])
+  )
   const [labelId] = useState<string>(v4())
-  const [editorRef, editorState] = useRichTextEditor(
+  const [editorId] = useState<string>(v4())
+  const [editorRef, editorState, dispatchTransaction] = useRichTextEditor(
     EditorState.create({
       doc: value
         ? Node.fromJSON(schema, {
@@ -76,7 +68,9 @@ export const RichTextEditor: React.VFC<RichTextEditorProps> = props => {
     }),
     { "aria-labelledby": labelId }
   )
-
+  const marksFromControls = controls?.map(controlGroup =>
+    controlGroup.map(control => schema.marks[control])
+  )
   useEffect(() => {
     onChange(editorState.toJSON().doc.content)
     // Including `onContentChange` in the dependencies here will cause a 'Maximum update depth exceeded' issue
@@ -86,15 +80,39 @@ export const RichTextEditor: React.VFC<RichTextEditorProps> = props => {
     <>
       {labelText && <Label id={labelId} labelText={labelText} />}
       {/* TODO: add a bit of margin here once we have a classNameOverride on Label */}
-      <div
-        ref={editorRef}
-        className={classnames(
-          styles.editor,
-          styles[`rows${rows}`],
-          classNameOverride
+      <div className={styles.editorWrapper}>
+        {controls && (
+          <Toolbar aria-controls={editorId} aria-label="Text formatting">
+            {marksFromControls?.map((controlSection, sectionIndex) => (
+              <ToolbarSection key={sectionIndex}>
+                {controlSection.map((mark, markIndex) => {
+                  const isActive = markIsActive(editorState, mark) || false
+                  const action = isActive ? removeMark(mark) : addMark(mark)
+                  return (
+                    <ToggleIconButton
+                      key={markIndex}
+                      icon={mark.spec.control.icon}
+                      label={mark.spec.control.label}
+                      isActive={isActive}
+                      onClick={() => dispatchTransaction(action)}
+                    />
+                  )
+                })}
+              </ToolbarSection>
+            ))}
+          </Toolbar>
         )}
-        {...restProps}
-      />
+        <div
+          id={editorId}
+          ref={editorRef}
+          className={classnames(
+            styles.editor,
+            styles[`rows${rows}`],
+            classNameOverride
+          )}
+          {...restProps}
+        />
+      </div>
     </>
   )
 }
