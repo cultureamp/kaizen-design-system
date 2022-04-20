cbf74612994a5ee81aba5a6a0bd8d8f4d44c035b
Natalie Loyzaga <nloyzaga@gmail.com>
2022-04-20 11:00:29 +1000

    feat: add DateInput tests and clean up

--- packages/date-picker/src/DatePicker/components/DateInput/DateInput.spec.tsx
+++ packages/date-picker/src/DatePicker/components/DateInput/DateInput.spec.tsx
@@ -1,40 +1,42 @@
 import React from "react"
 import { render, screen } from "@testing-library/react"
 import "@testing-library/jest-dom"
+import dateStart from "@kaizen/component-library/icons/date-start.icon.svg"
 
-// import { TextField } from "./DateInput"
+import { DateInput } from "./DateInput"
 
-// const defaultProps = {
-//   id: "text-field-test",
-//   labelText: "Label",
-//   description: "Description text",
-// }
+const validationMessages = {
+  success: "This is a success message",
+  caution: "This is a cautionary message",
+  error: "This is an error message",
+}
 
-// describe("<TextField />", () => {
-//   it("renders a description when provided", () => {
-//     render(<TextField {...defaultProps} />)
-//     screen.getByText("Description text")
-//   })
-//   it("renders a validation message when provided", () => {
-//     render(
-//       <TextField
-//         {...defaultProps}
-//         validationMessage="Error message"
-//         status="error"
-//       />
-//     )
-//     screen.getByText("Description text")
-//   })
-//   it("validation message has the correct type", () => {
-//     const { container } = render(
-//       <TextField
-//         {...defaultProps}
-//         validationMessage="Error message"
-//         status="error"
-//       />
-//     )
-//     expect(
-//       container.querySelector(`#${defaultProps.id}-field-message`)
-//     ).toHaveClass("error")
-//   })
-// })
+const defaultProps = {
+  id: "text-field-test",
+  labelText: "Label",
+  description: "Description text",
+  icon: dateStart,
+  validationMessages,
+  isOpen: false,
+  onButtonClick: () => jest.fn(),
+  onKeyDown: () => jest.fn(),
+}
+
+describe("<DateInput />", () => {
+  it("renders a description when provided", () => {
+    render(<DateInput {...defaultProps} />)
+    screen.getByText("Description text")
+  })
+
+  it("renders a error validation message when provided", () => {
+    render(<DateInput {...defaultProps} status="error" />)
+    screen.getByText("This is an error message")
+  })
+
+  it("renders the correc validation message class", () => {
+    const { container } = render(<DateInput {...defaultProps} status="error" />)
+    expect(
+      container.querySelector(`#${defaultProps.id}-field-message`)
+    ).toHaveClass("error")
+  })
+})
