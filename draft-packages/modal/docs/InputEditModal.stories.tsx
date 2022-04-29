ecf757a0272e343215eac69b6c13c8daecf1ebb0
Cassandra <cassandra.tam@cultureamp.com>
2022-04-29 12:19:53 +1000

    docs(inputeditmodal): improve stories to showcase sample code (#2701)

--- draft-packages/modal/docs/InputEditModal.stories.tsx
+++ draft-packages/modal/docs/InputEditModal.stories.tsx
@@ -1,47 +1,26 @@
-import React from "react"
-import { Paragraph } from "@kaizen/typography"
-import { Button } from "@kaizen/button"
-import { Select } from "@kaizen/draft-select"
-import { InputEditModal, ModalAccessibleDescription } from "@kaizen/draft-modal"
+import React, { useState } from "react"
 import isChromatic from "chromatic/isChromatic"
+import { ComponentStory } from "@storybook/react"
 import { withDesign } from "storybook-addon-designs"
+import { Button } from "@kaizen/button"
+import { InputEditModal, ModalAccessibleDescription } from "@kaizen/draft-modal"
+import { Select } from "@kaizen/draft-select"
+import { Paragraph } from "@kaizen/typography"
 import { figmaEmbed } from "../../../storybook/helpers"
 import { CATEGORIES } from "../../../storybook/constants"
+
+const IS_CHROMATIC = isChromatic()
 
 // Add additional height to the stories when running in Chromatic only.
 // Modals have fixed position and would be cropped from snapshot tests.
 // Setting height to 100vh ensures we capture as much content of the
 // modal, as it's height responds to the content within it.
 const withMinHeight = Story => {
-  if (!isChromatic()) return <Story />
-  return (
-    <div style={{ minHeight: "100vh" }}>
-      <Story />
-    </div>
-  )
-}
-
-class ModalStateContainer extends React.Component<
-  {
-    isInitiallyOpen: boolean
-    children: (renderProps: {
-      open: () => void
-      close: () => void
-      isOpen: boolean
-    }) => React.ReactNode
-  },
-  { isOpen: boolean }
-> {
-  state = { isOpen: this.props.isInitiallyOpen }
-  open = () => this.setState({ isOpen: true })
-  close = () => this.setState({ isOpen: false })
-  render() {
-    return this.props.children({
-      open: this.open,
-      close: this.close,
-      isOpen: this.state.isOpen,
-    })
+  if (IS_CHROMATIC) {
+    return <div style={{ minHeight: "100vh" }}>{Story()}</div>
   }
+
+  return Story()
 }
 
 export default {
@@ -56,9 +35,7 @@ export default {
     docs: {
       description: {
         component:
-          "import { InputEditModal, " +
-          "ModalAccessibleDescription" +
-          ' } from "@kaizen/draft-modal"',
+          'import { InputEditModal, ModalAccessibleDescription } from "@kaizen/draft-modal"',
       },
     },
     ...figmaEmbed(
@@ -68,46 +45,58 @@ export default {
   decorators: [withDesign, withMinHeight],
 }
 
-const SELECT_OPTIONS = [
-  { value: "Mindy", label: "Mindy" },
-  { value: "Jaime", label: "Jaime", isDisabled: true },
-  { value: "Rafa", label: "Rafa" },
-  { value: "Elaine", label: "Elaine" },
-  { value: "Julio", label: "Julio" },
-  { value: "Priyanka", label: "Priyanka" },
-  { value: "Prince", label: "Prince" },
-  { value: "Charith", label: "Charith" },
-  { value: "Nick", label: "Nick" },
-]
+const InputEditModalTemplate: ComponentStory<typeof InputEditModal> = args => {
+  const [isOpen, setIsOpen] = useState<boolean>(IS_CHROMATIC)
 
-export const InputEditModals = args => (
-  <ModalStateContainer isInitiallyOpen={isChromatic()}>
-    {({ open, close, isOpen }) => (
-      <div>
-        <Button label="Open modal" onClick={open} />
-        <InputEditModal
-          isOpen={isOpen}
-          mood="positive"
-          title="Input-edit modal title"
-          onSubmit={close}
-          onDismiss={close}
-          submitLabel="Label"
-          {...args}
-        >
-          <ModalAccessibleDescription>
-            <Paragraph variant="body">
-              Instructive text to drive user selection goes here.
-            </Paragraph>
-          </ModalAccessibleDescription>
-          <Select
-            options={SELECT_OPTIONS}
-            placeholder="Placeholder"
-            isSearchable={false}
-            isDisabled={false}
-            defaultValue={SELECT_OPTIONS[0]}
-          />
-        </InputEditModal>
-      </div>
-    )}
-  </ModalStateContainer>
-)
+  const handleOpen = () => setIsOpen(true)
+  const handleClose = () => setIsOpen(false)
+
+  const SELECT_OPTIONS = [
+    { value: "Mindy", label: "Mindy" },
+    { value: "Jaime", label: "Jaime", isDisabled: true },
+    { value: "Rafa", label: "Rafa" },
+    { value: "Elaine", label: "Elaine" },
+    { value: "Julio", label: "Julio" },
+    { value: "Priyanka", label: "Priyanka" },
+    { value: "Prince", label: "Prince" },
+    { value: "Charith", label: "Charith" },
+    { value: "Nick", label: "Nick" },
+  ]
+
+  return (
+    <>
+      <Button label="Open modal" onClick={handleOpen} />
+
+      <InputEditModal
+        {...args}
+        isOpen={args.isOpen === undefined ? isOpen : args.isOpen}
+        onSubmit={handleClose}
+        onDismiss={handleClose}
+      >
+        <ModalAccessibleDescription>
+          <Paragraph variant="body">
+            Instructive text to drive user selection goes here.
+          </Paragraph>
+        </ModalAccessibleDescription>
+        <Select
+          options={SELECT_OPTIONS}
+          placeholder="Placeholder"
+          isSearchable={false}
+          isDisabled={false}
+          defaultValue={SELECT_OPTIONS[0]}
+        />
+      </InputEditModal>
+    </>
+  )
+}
+
+export const InputEditModalExample = InputEditModalTemplate.bind({})
+InputEditModalExample.storyName = "Input Edit Modal"
+InputEditModalExample.args = {
+  mood: "positive",
+  title: "Input-edit modal title",
+  submitLabel: "Submit label",
+}
+InputEditModalExample.parameters = {
+  docs: { source: { type: "code" } },
+}
