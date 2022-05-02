b7e7572a01f4973a43480788293dea70d858641a
Natalie Loyzaga <nloyzaga@gmail.com>
2022-05-02 10:00:39 +1000

    fix: update index.ts to be on root level of package

---
+++ packages/loading-skeleton/index.ts
@@ -0,0 +1,4 @@
+export * from "./src/LoadingSkeleton/components/LoadingParagraph"
+export * from "./src/LoadingSkeleton/components/LoadingHeading"
+export * from "./src/LoadingSkeleton/components/LoadingInput"
+export * from "./src/LoadingSkeleton/components/LoadingGraphic"
--- packages/loading-skeleton/src/LoadingSkeleton/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./components/LoadingParagraph"
-export * from "./components/LoadingHeading"
-export * from "./components/LoadingInput"
-export * from "./components/LoadingGraphic"
