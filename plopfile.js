4885a50efd3110d544ff5d4556f3385b78c98ae0
Cassandra Tam <cassandra.tam@cultureamp.com>
2022-05-06 18:08:46 +1000

    chore(plopjs): add support for adding subcomponents

--- plopfile.js
+++ plopfile.js
@@ -10,22 +10,42 @@ module.exports = (
         name: "componentName",
         message: "What is the component name?",
       },
-    ],
-    actions: [
-      // Package
       {
-        type: "addMany",
-        destination: "packages/{{kebabCase componentName}}",
-        base: "plop-templates/basic-component",
-        templateFiles: "plop-templates/basic-component/**/*.hbs",
-      },
-      // Site docs
-      {
-        type: "addMany",
-        destination: "site/docs/components",
-        base: "plop-templates/site/docs/components",
-        templateFiles: "plop-templates/site/docs/components/*.hbs",
+        type: "input",
+        name: "parentComponentName",
+        message:
+          "If this is a subcomponent, enter the parent component name (leave blank otherwise)",
       },
     ],
+    actions: ({ parentComponentName }) => {
+      if (parentComponentName) {
+        return [
+          {
+            type: "addMany",
+            destination:
+              "packages/{{kebabCase parentComponentName}}/src/{{pascalCase parentComponentName}}/components",
+            base: "plop-templates/basic-component/src",
+            templateFiles: "plop-templates/basic-component/src/**/*.hbs",
+          },
+        ]
+      }
+
+      return [
+        {
+          type: "addMany",
+          destination: "packages/{{kebabCase componentName}}",
+          base: "plop-templates/basic-component",
+          templateFiles: "plop-templates/basic-component/**/*.hbs",
+          skipIfExists: true,
+        },
+        {
+          type: "addMany",
+          destination: "site/docs/components",
+          base: "plop-templates/site/docs/components",
+          templateFiles: "plop-templates/site/docs/components/*.hbs",
+          skipIfExists: true,
+        },
+      ]
+    },
   })
 }
