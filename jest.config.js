3b4deb473ec7b662a9ddd47fcc5db5b56b056880
Doug MacKenzie <doug.mackenzie@cultureamp.com>
2022-05-16 17:01:05 +1000

    Add old version of testing-library/react and use on react 16 & 17 steps

--- jest.config.js
+++ jest.config.js
@@ -14,11 +14,22 @@ module.exports = {
 
 process.env.TZ = "UTC"
 
+if (process.env.USE_REACT_16 === "true") {
+  module.exports.cacheDirectory = ".cache/jest-cache-react-16"
+  module.exports.moduleNameMapper = {
+    ...module.exports.moduleNameMapper,
+    "^react-dom((\\/.*)?)$": "react-dom-16$1",
+    "^react((\\/.*)?)$": "react-16$1",
+    "^@testing-library/react((\\/.*)?)$": "@testing-library/react-12$1",
+  }
+}
+
 if (process.env.USE_REACT_17 === "true") {
   module.exports.cacheDirectory = ".cache/jest-cache-react-17"
   module.exports.moduleNameMapper = {
     ...module.exports.moduleNameMapper,
     "^react-dom((\\/.*)?)$": "react-dom-17$1",
     "^react((\\/.*)?)$": "react-17$1",
+    "^@testing-library/react((\\/.*)?)$": "@testing-library/react-12$1",
   }
 }
