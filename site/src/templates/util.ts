de2c3c31a4e23a92473352416e552df5f1dea1e4
Cassandra <cassandra.tam@cultureamp.com>
2022-06-21 10:43:23 +1000

    chore: remove gatsby site (#2802)
    * chore: remove gatsby site
    
    * chore: update yarn.lock after removing site
    
    * chore: remove gatsby from package.json and add carat back to svgo-load
    
    * chore: remove site from gitignore and prettierignore
    
    * chore: update js-audit-allow-list to remove references to gatsby
    
    * docs: update README to remove mentions of Gatsby
    
    * chore(deps): add raw-loader to root package.json
    
    * chore(storybook): add header and footer (adapted from site)
    
    * adds storybook to tsconfig

--- site/src/templates/util.ts
+++
@@ -1,8 +0,0 @@
-export const stripTrailingSlash = (str: string) => str.replace(/\/$/, "")
-
-export const sortSidebarTabs = tabs => {
-  const newTabs = [...tabs].sort((a, b) =>
-    a.node.frontmatter.navTitle > b.node.frontmatter.navTitle ? 1 : -1
-  )
-  return newTabs
-}
