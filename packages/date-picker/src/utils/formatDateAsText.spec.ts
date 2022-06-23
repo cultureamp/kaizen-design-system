985ee47da51b8a5eb78ef309e38a1a28388d5b37
Cassandra Tam <cassandra.tam@cultureamp.com>
2022-06-23 11:47:28 +1000

    test(date-picker): fix formatDateasText disabled test

--- packages/date-picker/src/utils/formatDateAsText.spec.ts
+++ packages/date-picker/src/utils/formatDateAsText.spec.ts
@@ -11,7 +11,7 @@ describe("formatDateAsText", () => {
   it("formats disabled date in numeral format", () => {
     const disabledDays = [{ before: new Date("2022-02-16") }]
     formatDateAsText(new Date("2022-01-16"), disabledDays, onFormat)
-    expect(onFormat).toHaveBeenCalledWith("Jan 16, 2022")
+    expect(onFormat).toHaveBeenCalledWith("01/16/2022")
   })
 
   it("formats invalid date as error message", () => {
