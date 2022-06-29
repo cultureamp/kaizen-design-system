776d953a8c5adc51be2767a06a4d1d6f16f873d9
Patrick Smith <pgwsmith@gmail.com>
2022-06-29 12:23:58 +1000

    feat: Bootstrap new <Listbox> component

---
+++ packages/listbox/src/Listbox/Listbox.tsx
@@ -0,0 +1,32 @@
+import React from "react"
+import classnames from "classnames"
+import styles from "./Listbox.scss"
+
+export type ListboxVariant = "default" | "filter"
+
+// TODO: decide which third-party headless library to do the heavy lifting of this component.
+
+export interface ListboxProps {
+  children: React.ReactNode
+  variant?: ListboxVariant
+}
+
+const ListboxRoot: React.FC<ListboxProps> = props => (
+  <div role="listbox" tabIndex={0} className={classnames([styles.listbox])}>
+    <ul role="presentation">{props.children}</ul>
+  </div>
+)
+ListboxRoot.displayName = "Listbox"
+
+const Option: React.FC<ListboxProps> = props => (
+  <div
+    role="option"
+    tabIndex={0}
+    className={classnames([styles.listbox])}
+    aria-selected={false}
+  />
+)
+ListboxRoot.displayName = "ListboxOption"
+
+// Pattern copied from HeadlessUI: https://github.com/tailwindlabs/headlessui/blob/6253aa52b3801e84331450a651bb41328ffa7239/packages/%40headlessui-react/src/components/listbox/listbox.tsx#L890
+export const Listbox = Object.assign(ListboxRoot, { Option })
