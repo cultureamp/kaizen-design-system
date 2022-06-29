e169176076b1d157ea15635dab6fc72386931610
Cassandra Tam <cassandra.tam@cultureamp.com>
2022-06-29 17:57:17 +1000

    refactor(date-picker): update type SupportedLocales to be string with suggestions

--- packages/date-picker/src/types.ts
+++ packages/date-picker/src/types.ts
@@ -1,3 +1,7 @@
 export type WeekStartsOn = 0 | 1 | 2 | 3 | 4 | 5 | 6 | undefined
 
-export type SupportedLocales = "en-US" | "en-AU"
+// This isn't recommended TS behaviour, however since we have a fallback in place
+// for handling other locales, we have a valid usage of suggesting supported locales.
+type StringSuggestions<T> = T | (string & Record<never, never>)
+
+export type SupportedLocales = StringSuggestions<"en-US" | "en-AU">
