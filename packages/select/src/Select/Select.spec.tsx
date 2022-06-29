9434eb4496d5d26c7885e67a74229606f8349db8
louiseliang <louise.liang@cultureamp.com>
2022-06-29 15:47:34 +1000

    Attempt to add select
    Co-authored-by: John W McCowan <jwmccowan@users.noreply.github.com>

---
+++ packages/select/src/Select/Select.spec.tsx
@@ -0,0 +1,36 @@
+import React from "react"
+import { render, screen } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
+import { Listbox } from "./Select"
+
+describe("<Listbox />", () => {
+  describe("A listbox with 3 options", () => {
+    beforeEach(() => {
+      render(
+        <Listbox>
+          <Listbox.Option>First</Listbox.Option>
+          <Listbox.Option>Second</Listbox.Option>
+          <Listbox.Option>Third</Listbox.Option>
+        </Listbox>
+      )
+    })
+
+    it("renders a visible listbox", () => {
+      expect(screen.getByRole("listbox")).toBeVisible()
+    })
+
+    it("renders 3 options", () => {
+      expect(screen.getAllByRole("option")).toHaveLength(3)
+    })
+
+    it("can be focused", () => {
+      userEvent.tab()
+      expect(screen.getByRole("listbox")).toHaveFocus()
+    })
+
+    it.todo("selects the current option when user presses Space")
+    it.todo("can toggle the current option when user presses Space")
+    it.todo("supports the down arrow")
+    it.todo("supports the up arrow")
+  })
+})
