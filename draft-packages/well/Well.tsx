02feda57e7ed88d6829c5253c7967295a6f51f9b
Cassandra Tam <cassandra.tam@cultureamp.com>
2022-07-26 20:33:31 +1000

    refactor(well): rename scss files to use .module.scss

--- draft-packages/well/Well.tsx
+++ draft-packages/well/Well.tsx
@@ -1,7 +1,7 @@
 import React, { HTMLAttributes } from "react"
 import classnames from "classnames"
 import { OverrideClassName } from "@kaizen/component-base"
-import styles from "./styles.scss"
+import styles from "./Well.module.scss"
 
 type VariantType =
   | "positive"
