c6083de33a68b531f0aae548df89e42beb8bfc2c
Cassandra Tam <cassandra.tam@cultureamp.com>
2022-07-26 17:28:43 +1000

    refactor(menu): rename scss files to use .module.scss

--- draft-packages/menu/KaizenDraft/Menu/MenuDropdown.tsx
+++ draft-packages/menu/KaizenDraft/Menu/MenuDropdown.tsx
@@ -1,7 +1,7 @@
 import classnames from "classnames"
 import React, { useCallback, useEffect, useState } from "react"
 import { usePopper } from "react-popper"
-import styles from "./styles.scss"
+import styles from "./MenuDropdown.module.scss"
 
 type MenuDropdownProps = {
   id?: string
