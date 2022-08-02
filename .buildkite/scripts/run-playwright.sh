5cc3ddd7302cf83b583ab3bcfd901f76de57d9ce
mcwinter07 <mcwinter07@gmail.com>
2022-08-02 15:02:33 +1000

    update 'commands:' to 'command' in pipleline yml

--- .buildkite/scripts/run-playwright.sh
+++ .buildkite/scripts/run-playwright.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 set -e
 
+# shellcheck source=setup-registry.sh
+. ".buildkite/scripts/helpers/setup-registry.sh"
+
+echo "--- :yarnpkg: Install dependencies"
+yarn install --frozen-lockfile
+
 KAIZEN_DEV_BRANCH=""
 
 
@@ -9,9 +15,7 @@ if [ "$KAIZEN_DOMAIN_NAME" = "dev.cultureamp.design" ]; then
     KAIZEN_DEV_BRANCH="${KAIZEN_DOMAIN_NAME}/${BUILDKITE_BRANCH}/"
     echo "Running playwright tests on ${KAIZEN_DEV_BRANCH}"
 
-    yarn install --frozen-lockfile
     yarn playwright
-
 else
     echo "Playwright tests not run on prod env. Move along, move along"
 fi
