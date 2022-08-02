be3063846037bdab46e85de66faf99ad1577d5b2
Jake Pitman <jakeseanp@gmail.com>
2022-08-02 21:58:25 +1000

    test(tooltip): add spec for Tooltip

---
+++ draft-packages/tooltip/KaizenDraft/Tooltip/Tooltip.spec.tsx
@@ -0,0 +1,33 @@
+import { cleanup, render } from "@testing-library/react"
+import * as React from "react"
+import { AnimationProvider } from "./AppearanceAnim"
+import { Tooltip } from "./index"
+import "@testing-library/jest-dom"
+jest.mock("./AppearanceAnim")
+
+afterEach(() => cleanup())
+
+describe("<Tooltip />", () => {
+  describe("When no animationDuration prop is given", () => {
+    test("animationDuration prop is passed to AnimationProvider as undefined", () => {
+      render(<Tooltip text="Example" />)
+      expect(AnimationProvider).toHaveBeenCalledWith(
+        expect.objectContaining({
+          animationDuration: undefined,
+        }),
+        {}
+      )
+    })
+  })
+  describe("When animationDuration prop is given", () => {
+    test("animationDuration prop is passed to AnimationProvider", () => {
+      render(<Tooltip text="Example" animationDuration={1000} />)
+      expect(AnimationProvider).toHaveBeenCalledWith(
+        expect.objectContaining({
+          animationDuration: 1000,
+        }),
+        {}
+      )
+    })
+  })
+})
