04fef1d81b35d24fcc3bc06067d06ca9b19aa9a8
Jake Pitman <jakeseanp@gmail.com>
2022-08-03 17:04:32 +1000

    test(appearanceanim): add spec to confirm animationDuration prop is used properly

---
+++ draft-packages/tooltip/KaizenDraft/Tooltip/AppearanceAnim.spec.tsx
@@ -0,0 +1,41 @@
+import { cleanup, render } from "@testing-library/react"
+import * as React from "react"
+import useDebounce from "use-debounce"
+import { AnimationProvider } from "./AppearanceAnim"
+import "@testing-library/jest-dom"
+jest.mock("use-debounce")
+const useDebouncedCallback = useDebounce.useDebouncedCallback as jest.Mock
+
+afterEach(() => cleanup())
+
+let mockReturnValue
+
+beforeEach(() => {
+  mockReturnValue = jest.fn()
+  mockReturnValue.cancel = () => {}
+  useDebouncedCallback.mockImplementation(() => mockReturnValue)
+})
+
+describe("<AnimationProvider />", () => {
+  describe("When no animationDuration prop is given", () => {
+    it("calls useDebouncedCallback with a 400 ms delay", () => {
+      render(<AnimationProvider isVisible />)
+      expect(useDebouncedCallback).toHaveBeenCalledWith(
+        expect.anything(),
+        400,
+        expect.anything()
+      )
+    })
+  })
+  describe("When an animationDuration prop is given", () => {
+    it("calls useDebouncedCallback with the animationDuration value", () => {
+      const expected = 1000
+      render(<AnimationProvider isVisible animationDuration={expected} />)
+      expect(useDebouncedCallback).toHaveBeenCalledWith(
+        expect.anything(),
+        expected,
+        expect.anything()
+      )
+    })
+  })
+})
