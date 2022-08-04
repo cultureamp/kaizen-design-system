1f60c122bf4ecf512d3d70f81dc020f0dbe3fe54
mcwinter07 <mcwinter07@gmail.com>
2022-08-04 14:19:44 +1000

    fix file name and update pipeline config

--- .buildkite/scripts/run-playwright.sh
+++ .buildkite/scripts/run-playwright.sh
@@ -13,9 +13,11 @@ KAIZEN_DEV_BRANCH=""
 if [ "$KAIZEN_DOMAIN_NAME" = "dev.cultureamp.design" ]; then
     echo "On dev branch for ${BUILDKITE_BRANCH}"
     KAIZEN_DEV_BRANCH="${KAIZEN_DOMAIN_NAME}/${BUILDKITE_BRANCH}/"
-    echo "Running playwright tests on ${KAIZEN_DEV_BRANCH}"
 
-    yarn playwright
+    echo "--- Running playwright tests on ${KAIZEN_DEV_BRANCH}"
+    npx playwright test
 else
-    echo "Playwright tests not run on prod env. Move along, move along"
+    echo "Playwright tests only run on dev env"
 fi
+
+echo "E2E Playwright tests complete!"
