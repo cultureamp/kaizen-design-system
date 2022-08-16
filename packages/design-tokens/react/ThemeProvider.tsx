4233bbb69205744aaa2361a74312a390cf1aad2e
Harry Ta <harry.ta@cultureamp.com>
2022-08-16 16:57:34 +1000

    add inter font variations and tiempos font

--- packages/design-tokens/react/ThemeProvider.tsx
+++ packages/design-tokens/react/ThemeProvider.tsx
@@ -54,7 +54,28 @@ export const ThemeProvider = ({
       />
       <link
         rel="preload"
+        href="https://d1e7r7b0lb8p4d.cloudfront.net/fonts/inter/inter-medium.woff2"
+        as="font"
+        type="font/woff2"
+        crossOrigin="crossorigin"
+      />
+      <link
+        rel="preload"
         href="https://d1e7r7b0lb8p4d.cloudfront.net/fonts/inter/inter-regular.woff2"
+        as="font"
+        type="font/woff2"
+        crossOrigin="crossorigin"
+      />
+      <link
+        rel="preload"
+        href="https://d1e7r7b0lb8p4d.cloudfront.net/fonts/inter/inter-light.woff2"
+        as="font"
+        type="font/woff2"
+        crossOrigin="crossorigin"
+      />
+      <link
+        rel="preload"
+        href="https://d1e7r7b0lb8p4d.cloudfront.net/fonts/tiempos/tiempos-headline-bold.woff2"
         as="font"
         type="font/woff2"
         crossOrigin="crossorigin"
