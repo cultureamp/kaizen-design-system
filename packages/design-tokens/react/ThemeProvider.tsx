f3c16aae52f10c53e9fe90df3d0a5dc696ce7839
cultureamp-ci <cultureamp-ci@cultureamp.com>
2022-08-17 05:04:17 +0000

    chore: release [skip ci]
     - @kaizen/rich-text-editor@1.17.0

--- packages/design-tokens/react/ThemeProvider.tsx
+++ packages/design-tokens/react/ThemeProvider.tsx
@@ -1,4 +1,3 @@
-import isChromatic from "chromatic"
 import React, { useState } from "react"
 import { ThemeManager, defaultTheme, Theme } from "../"
 
@@ -34,52 +33,46 @@ export const ThemeProvider = ({
     }
   }, [])
 
-  const injectFonts = !(isChromatic() || process.env.STORYBOOK !== undefined)
-
   return (
     <>
       <ThemeContext.Provider value={theme}>
         {props.children}
       </ThemeContext.Provider>
-      {injectFonts && (
-        <>
-          <link
-            rel="preload"
-            href="https://d1e7r7b0lb8p4d.cloudfront.net/fonts/inter/inter-bold.woff2"
-            as="font"
-            type="font/woff2"
-            crossOrigin="crossorigin"
-          />
-          <link
-            rel="preload"
-            href="https://d1e7r7b0lb8p4d.cloudfront.net/fonts/inter/inter-demi-bold.woff2"
-            as="font"
-            type="font/woff2"
-            crossOrigin="crossorigin"
-          />
-          <link
-            rel="preload"
-            href="https://d1e7r7b0lb8p4d.cloudfront.net/fonts/inter/inter-medium.woff2"
-            as="font"
-            type="font/woff2"
-            crossOrigin="crossorigin"
-          />
-          <link
-            rel="preload"
-            href="https://d1e7r7b0lb8p4d.cloudfront.net/fonts/inter/inter-regular.woff2"
-            as="font"
-            type="font/woff2"
-            crossOrigin="crossorigin"
-          />
-          <link
-            rel="preload"
-            href="https://d1e7r7b0lb8p4d.cloudfront.net/fonts/tiempos/tiempos-headline-bold.woff2"
-            as="font"
-            type="font/woff2"
-            crossOrigin="crossorigin"
-          />
-        </>
-      )}
+      <link
+        rel="preload"
+        href="https://d1e7r7b0lb8p4d.cloudfront.net/fonts/inter/inter-bold.woff2"
+        as="font"
+        type="font/woff2"
+        crossOrigin="crossorigin"
+      />
+      <link
+        rel="preload"
+        href="https://d1e7r7b0lb8p4d.cloudfront.net/fonts/inter/inter-demi-bold.woff2"
+        as="font"
+        type="font/woff2"
+        crossOrigin="crossorigin"
+      />
+      <link
+        rel="preload"
+        href="https://d1e7r7b0lb8p4d.cloudfront.net/fonts/inter/inter-medium.woff2"
+        as="font"
+        type="font/woff2"
+        crossOrigin="crossorigin"
+      />
+      <link
+        rel="preload"
+        href="https://d1e7r7b0lb8p4d.cloudfront.net/fonts/inter/inter-regular.woff2"
+        as="font"
+        type="font/woff2"
+        crossOrigin="crossorigin"
+      />
+      <link
+        rel="preload"
+        href="https://d1e7r7b0lb8p4d.cloudfront.net/fonts/tiempos/tiempos-headline-bold.woff2"
+        as="font"
+        type="font/woff2"
+        crossOrigin="crossorigin"
+      />
     </>
   )
 }
