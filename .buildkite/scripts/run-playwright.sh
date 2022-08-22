95fa2445d2bf5e7056c85b9cf90a4788efd7e39a
mcwinter07 <mcwinter07@gmail.com>
2022-08-22 16:25:43 +1000

    update pkg script

--- .buildkite/scripts/run-playwright.sh
+++ .buildkite/scripts/run-playwright.sh
@@ -13,7 +13,7 @@ if [ "$KAIZEN_DOMAIN_NAME" = "dev.cultureamp.design" ]; then
     KAIZEN_DEV_BRANCH="${KAIZEN_DOMAIN_NAME}/${BUILDKITE_BRANCH}/"
 
     echo "--- :playwright: Running playwright on ${KAIZEN_DEV_BRANCH}"
-    URL=${KAIZEN_DEV_BRANCH} yarn playwright test
+    URL=${KAIZEN_DEV_BRANCH} yarn playwright:ci
 else
     echo ":playwright: Playwright tests are only run on dev branches. Everything's good to go!"
 fi
