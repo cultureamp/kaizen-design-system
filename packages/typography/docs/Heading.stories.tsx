2a50930caf48973abfb941dd4675f7130d47fdbf
Natalie Loyzaga <nloyzaga@gmail.com>
2022-08-31 09:14:47 +1000

    docs: update to use const fontColour

--- packages/typography/docs/Heading.stories.tsx
+++ packages/typography/docs/Heading.stories.tsx
@@ -89,81 +89,85 @@ const Documentation = () => (
 
 const StickerSheetTemplate: Story<{ isReversed: boolean }> = ({
   isReversed,
-}) => (
-  <>
-    <StoryWrapper isReversed={isReversed}>
-      <StoryWrapper.Row rowTitle="Display 0">
-        <Heading variant="display-0" color={isReversed ? "white" : "dark"}>
-          Let's create a better world of work
-        </Heading>
-      </StoryWrapper.Row>
-      <StoryWrapper.Row rowTitle="Heading 1">
-        <Heading variant="heading-1" color={isReversed ? "white" : "dark"}>
-          Have the courage to be vulnerable.
-        </Heading>
-      </StoryWrapper.Row>
-      <StoryWrapper.Row rowTitle="Heading 2">
-        <Heading variant="heading-2" color={isReversed ? "white" : "dark"}>
-          Learn faster through feedback.
-        </Heading>
-      </StoryWrapper.Row>
-      <StoryWrapper.Row rowTitle="Heading 3">
-        <Heading variant="heading-3" color={isReversed ? "white" : "dark"}>
-          Trust people to make decisions.
-        </Heading>
-      </StoryWrapper.Row>
-      <StoryWrapper.Row rowTitle="Heading 4">
-        <Heading variant="heading-4" color={isReversed ? "white" : "dark"}>
-          Amplify others.
-        </Heading>
-      </StoryWrapper.Row>
-      <StoryWrapper.Row rowTitle="Heading 5">
-        <Heading variant="heading-5" color={isReversed ? "white" : "dark"}>
-          An employee experience that people love.
-        </Heading>
-      </StoryWrapper.Row>
-      <StoryWrapper.Row rowTitle="Heading 6">
-        <Heading variant="heading-6" color={isReversed ? "white" : "dark"}>
-          Discover the power of humanity at work.
-        </Heading>
-      </StoryWrapper.Row>
-    </StoryWrapper>
-    {!isReversed ? (
+}) => {
+  const fontColour = isReversed ? "white" : "dark"
+  return (
+    <>
       <StoryWrapper isReversed={isReversed}>
-        <StoryWrapper.RowHeader
-          headings={["Dark", "Dark Reduced Opacity", "Positive", "Negative"]}
-        />
-        <StoryWrapper.Row rowTitle="Colours">
-          <Heading variant="heading-6" color="dark">
-            Discover the power of humanity at work.
+        <StoryWrapper.Row rowTitle="Display 0">
+          <Heading variant="display-0" color={fontColour}>
+            Let's create a better world of work
           </Heading>
-          <Heading variant="heading-6" color="dark-reduced-opacity">
-            Discover the power of humanity at work.
+        </StoryWrapper.Row>
+        <StoryWrapper.Row rowTitle="Heading 1">
+          <Heading variant="heading-1" color={fontColour}>
+            Have the courage to be vulnerable.
           </Heading>
-          <Heading variant="heading-6" color="positive">
-            Discover the power of humanity at work.
+        </StoryWrapper.Row>
+        <StoryWrapper.Row rowTitle="Heading 2">
+          <Heading variant="heading-2" color={fontColour}>
+            Learn faster through feedback.
           </Heading>
-          <Heading variant="heading-6" color="negative">
+        </StoryWrapper.Row>
+        <StoryWrapper.Row rowTitle="Heading 3">
+          <Heading variant="heading-3" color={fontColour}>
+            Trust people to make decisions.
+          </Heading>
+        </StoryWrapper.Row>
+        <StoryWrapper.Row rowTitle="Heading 4">
+          <Heading variant="heading-4" color={fontColour}>
+            Amplify others.
+          </Heading>
+        </StoryWrapper.Row>
+        <StoryWrapper.Row rowTitle="Heading 5">
+          <Heading variant="heading-5" color={fontColour}>
+            An employee experience that people love.
+          </Heading>
+        </StoryWrapper.Row>
+        <StoryWrapper.Row rowTitle="Heading 6">
+          <Heading variant="heading-6" color={fontColour}>
             Discover the power of humanity at work.
           </Heading>
         </StoryWrapper.Row>
       </StoryWrapper>
-    ) : (
-      <StoryWrapper isReversed={isReversed}>
-        <StoryWrapper.RowHeader headings={["White", "White Reduced Opacity"]} />
-        <StoryWrapper.Row rowTitle="Colours">
-          <Heading variant="heading-6" color="white">
-            Discover the power of humanity at work.
-          </Heading>
-          <Heading variant="heading-6" color="white-reduced-opacity">
-            Discover the power of humanity at work.
-          </Heading>
-        </StoryWrapper.Row>
-      </StoryWrapper>
-    )}
-  </>
-)
-
+      {!isReversed ? (
+        <StoryWrapper isReversed={isReversed}>
+          <StoryWrapper.RowHeader
+            headings={["Dark", "Dark Reduced Opacity", "Positive", "Negative"]}
+          />
+          <StoryWrapper.Row rowTitle="Colours">
+            <Heading variant="heading-6" color="dark">
+              Discover the power of humanity at work.
+            </Heading>
+            <Heading variant="heading-6" color="dark-reduced-opacity">
+              Discover the power of humanity at work.
+            </Heading>
+            <Heading variant="heading-6" color="positive">
+              Discover the power of humanity at work.
+            </Heading>
+            <Heading variant="heading-6" color="negative">
+              Discover the power of humanity at work.
+            </Heading>
+          </StoryWrapper.Row>
+        </StoryWrapper>
+      ) : (
+        <StoryWrapper isReversed={isReversed}>
+          <StoryWrapper.RowHeader
+            headings={["White", "White Reduced Opacity"]}
+          />
+          <StoryWrapper.Row rowTitle="Colours">
+            <Heading variant="heading-6" color="white">
+              Discover the power of humanity at work.
+            </Heading>
+            <Heading variant="heading-6" color="white-reduced-opacity">
+              Discover the power of humanity at work.
+            </Heading>
+          </StoryWrapper.Row>
+        </StoryWrapper>
+      )}
+    </>
+  )
+}
 export const StickerSheetDefault = StickerSheetTemplate.bind({})
 StickerSheetDefault.storyName = "Sticker Sheet (Default)"
 StickerSheetDefault.parameters = {
