c39d4bd6794594a84b4de7c71f1249a258471d1e
Jake Pitman <jakeseanp@gmail.com>
2022-09-01 16:01:10 +1000

    test(filter-multi-select): update getTruncatedLabels spec

--- packages/select/src/FilterMultiSelect/utils/getTruncatedLabel/getTruncatedLabels.spec.ts
+++ packages/select/src/FilterMultiSelect/utils/getTruncatedLabel/getTruncatedLabels.spec.ts
@@ -1,39 +1,52 @@
 import { getTruncatedLabels } from "./getTruncatedLabel"
 
-const CHAR_LIMIT = 100
+const firstLabel = "optionhas11"
+const secondLabel = "optionwith12"
+const thirdLabel = "optioncontains16"
 
-const getMockOptionLabels = (count: number): string[] =>
-  Array.from(Array(count).keys()).map(item => `option${item + 1}-label-mock`)
+const labels = [firstLabel, secondLabel, thirdLabel]
 
 describe("getTruncatedLabels", () => {
-  describe("Given 3 selected labels", () => {
-    it("shows all the option labels", () => {
-      expect(getTruncatedLabels(getMockOptionLabels(3), CHAR_LIMIT)).toContain(
-        "option1-label-mock, option2-label-mock, option3-label-mock"
+  describe("With only one label", () => {
+    it("Always shows on its own, regardless of character limit", () => {
+      const limit = 5
+      const longLabel = "My super long label with a lot of characters"
+      const withOnlyOneLabel = [longLabel]
+      expect(getTruncatedLabels(withOnlyOneLabel, limit)).toEqual(longLabel)
+    })
+  })
+  describe("When the first label exceeds the character limit", () => {
+    it("Always shows", () => {
+      const limit = 5
+      const longLabel = "My super long label with a lot of characters"
+      const withLongFirstLabel = [longLabel, secondLabel]
+      expect(getTruncatedLabels(withLongFirstLabel, limit)).toEqual(
+        `${longLabel} + 1 more`
       )
     })
   })
-
-  describe("Given 4 selected labels", () => {
-    it("shows only the first 3 option labels", () => {
-      const result = getTruncatedLabels(getMockOptionLabels(4), CHAR_LIMIT)
-      expect(result).toContain(
-        "option1-label-mock, option2-label-mock, option3-label-mock"
+  describe("With all labels combined less than character limit", () => {
+    it("Shows all labels, connected with commas", () => {
+      const limit = 100
+      expect(getTruncatedLabels(labels, limit)).toEqual(
+        `${firstLabel}, ${secondLabel}, ${thirdLabel}`
       )
-
-      expect(result).not.toContain("option4-label-mock")
-    })
-
-    it("shows +1 more", () => {
-      const result = getTruncatedLabels(getMockOptionLabels(4), CHAR_LIMIT)
-      expect(result).toContain("+1 more")
     })
   })
-
-  describe("Given 6 selected labels", () => {
-    it("shows +3 more", () => {
-      const result = getTruncatedLabels(getMockOptionLabels(6), CHAR_LIMIT)
-      expect(result).toContain("+3 more")
+  describe("With last label exceeding character limit", () => {
+    it("Replaces last item with '+1 more'", () => {
+      const limit = 25
+      expect(getTruncatedLabels(labels, limit)).toEqual(
+        `${firstLabel}, ${secondLabel} + 1 more`
+      )
+    })
+  })
+  describe("With last two labels exceeding character limit", () => {
+    it("Replaces last two items with '+2 more'", () => {
+      const limit = 20
+      expect(getTruncatedLabels(labels, limit)).toEqual(
+        `${firstLabel} + 2 more`
+      )
     })
   })
 })
