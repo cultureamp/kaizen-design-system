1709ad99cdf8a2ad2519eb89d3b1e14cbe80f626
Josh Li <joshua.li@cultureamp.com>
2022-09-14 17:24:19 +1000

    style(KDS-754): move button styling to module.scss file

--- packages/time-picker/src/TimePicker/components/Button/Button.tsx
+++ packages/time-picker/src/TimePicker/components/Button/Button.tsx
@@ -1,27 +1,13 @@
 import React, { ForwardedRef } from "react"
 import { useButton, AriaButtonProps } from "@react-aria/button"
+import styles from "./Button.module.scss"
 
 export const Button = React.forwardRef(
   (props: AriaButtonProps, ref: ForwardedRef<HTMLButtonElement>) => {
     // @ts-expect-error not sure why how to fix below
     const { buttonProps } = useButton(props, ref)
     return (
-      <button
-        {...buttonProps}
-        ref={ref}
-        style={{
-          display: "flex",
-          position: "absolute",
-          right: 0,
-          width: 50,
-          top: 0,
-          bottom: 0,
-          alignItems: "center",
-          justifyContent: "center",
-          background: "none",
-          border: "none",
-        }}
-      >
+      <button {...buttonProps} ref={ref} className={styles.button}>
         {props.children}
       </button>
     )
