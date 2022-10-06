7575b365799c74dd0c8eab19033a20d19a279cfd
Jake Pitman <jakeseanp@gmail.com>
2022-10-06 11:46:00 +1000

    test: KDS-837 Add Playwright tests for FilterMultiSelect

---
+++ packages/select/src/FilterMultiSelect/FilterMultiSelect.playwright.spec.tsx
@@ -0,0 +1,48 @@
+import { expect, test } from "@playwright/test"
+
+const iframePath = "iframe.html?args=&id="
+const storyId =
+  "components-select-filter-multi-select--default-kaizen-site-demo"
+test.beforeEach(async ({ page }) => {
+  // Go to the starting url before each test.
+  await page.goto(`./${iframePath}${storyId}`)
+})
+
+test.describe("Selecting an unselected option", async () => {
+  test("Selects the option", async ({ page }) => {
+    // Click trigger button
+    await page.locator("text=Engineer: Front-End").click()
+
+    // const devOpsOption = await page.locator('[aria-label="Dev-ops"]')
+    const devOpsOption = page.locator('[aria-label="Dev-ops"]')
+    await devOpsOption.click()
+    expect(await devOpsOption.locator('svg[role="presentation"]').count()).toBe(
+      1
+    )
+  })
+})
+
+test.describe("Deselecting a selected option", async () => {
+  test("Selects the option", async ({ page }) => {
+    // Click trigger button
+    await page.locator("text=Engineer: Front-End").click()
+
+    const frontEndOption = await page.locator("text=Front-End1245 available")
+    await frontEndOption.click()
+    expect(
+      await frontEndOption.locator('svg[role="presentation"]').count()
+    ).toBe(0)
+  })
+})
+
+test.describe("Filtering by text", async () => {
+  test("Keeps text matches, and filters out non-matches", async ({ page }) => {
+    // Click trigger button
+    await page.locator("text=Engineer: Front-End").click()
+    // Enter text "dev" in search field
+    await page.locator('[placeholder="Search…"]').fill("dev")
+
+    expect(await page.locator('[aria-label="Dev-ops"]').count()).toBe(1)
+    expect(await page.locator('[aria-label="Others"]').count()).toBe(0)
+  })
+})
