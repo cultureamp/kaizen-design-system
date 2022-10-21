51c5bbe8e19628d64464b8e056d735c9fffb8cb2
Rachel Harris <rachel.harris@cultureamp.com>
2022-10-21 17:08:07 +1100

    Update test to check if children are rendered

--- draft-packages/collapsible/KaizenDraft/ExpertAdviceCollapsible/ExpertAdviceCollapsible.spec.tsx
+++ draft-packages/collapsible/KaizenDraft/ExpertAdviceCollapsible/ExpertAdviceCollapsible.spec.tsx
@@ -1,30 +1,36 @@
 import * as React from "react"
 import { render, screen } from "@testing-library/react"
+import { Paragraph } from "@kaizen/typography"
 import { ExpertAdviceCollapsible } from ".."
 
-it("renders children", () => {
+it("renders the title", () => {
   render(
     <ExpertAdviceCollapsible id="1" title="This title should be rendered">
       Expert advice collapsible component
     </ExpertAdviceCollapsible>
   )
-  screen.getByText("This title should be rendered")
+  expect(screen.getByText("This title should be rendered")).toBeInTheDocument()
 })
 
 it("renders an icon in the header", () => {
   render(
-    <ExpertAdviceCollapsible id="2" title="Expert advice collapsible icon">
+    <ExpertAdviceCollapsible id="1" title="Expert advice collapsible icon">
       Expert advice collapsible component
     </ExpertAdviceCollapsible>
   )
-  screen.getByRole("img", { name: "Collective Intelligence" })
+  expect(
+    screen.getByRole("img", { name: "Collective Intelligence" })
+  ).toBeInTheDocument()
 })
 
-it("renders children", () => {
+it("renders collapsible component and its children", () => {
   render(
-    <ExpertAdviceCollapsible id="2" title="Expert advice collapsible children">
-      Expert advice collapsible component
+    <ExpertAdviceCollapsible id="1" title="Expert advice collapsible children">
+      <Paragraph variant="body">Text Expert advice section content</Paragraph>
     </ExpertAdviceCollapsible>
   )
-  screen.getByRole("region")
+  expect(screen.getByLabelText("Toggle section")).toBeInTheDocument()
+  expect(
+    screen.getByText("Text Expert advice section content")
+  ).toBeInTheDocument()
 })
