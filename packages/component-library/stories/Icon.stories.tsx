d389bc4a133c00fb89c5728b2131441730c6eb22
Natalie Loyzaga <nloyzaga@gmail.com>
2022-10-25 15:09:46 +1000

    docs(icons): add story to display all icons

--- packages/component-library/stories/Icon.stories.tsx
+++ packages/component-library/stories/Icon.stories.tsx
@@ -1,7 +1,21 @@
 import React from "react"
 import { Icon } from "@kaizen/component-library"
-import configureIcon from "@kaizen/component-library/icons/configure.icon.svg"
+import { Story } from "@storybook/react"
+import { Heading, Paragraph } from "@kaizen/typography"
 import { CATEGORIES } from "../../../storybook/constants"
+import {
+  Logo,
+  Actions,
+  Informational,
+  Users,
+  Survey,
+  Views,
+  Directional,
+  Sentiment,
+  Performance,
+  Effectiveness,
+  Miscellaneous,
+} from "./Icons"
 
 export default {
   title: `${CATEGORIES.components}/Icon`,
@@ -18,9 +32,13 @@ export default {
 export const MeaningfulKaizenSiteDemo = () => (
   // the wrapper with the fixed with is to solve a problem when this is used
   // as a site demo: the iframe was getting a height of 0px in Firefox
-  <div style={{ width: "20px" }}>
+  <div
+    style={{
+      width: "20px",
+    }}
+  >
     <Icon
-      icon={configureIcon}
+      icon={Actions.configure}
       title="Warning"
       desc="Aliens approaching!"
       role="img"
@@ -28,16 +46,146 @@ export const MeaningfulKaizenSiteDemo = () => (
     />
   </div>
 )
-MeaningfulKaizenSiteDemo.storyName = "Meaningful (Kaizen Site Demo)"
+MeaningfulKaizenSiteDemo.storyName = "Icon"
 
-export const Presentational = () => (
-  <Icon icon={configureIcon} role="presentation" />
-)
-Presentational.parameters = { chromatic: { disable: false } }
-
-export const InheritSize = () => (
-  <div style={{ width: "100%" }}>
-    <Icon icon={configureIcon} role="presentation" inheritSize={true} />
+const IconExampleTile = ({ icon, name }) => (
+  <div
+    style={{
+      width: "150px",
+      display: "inline-flex",
+      flexWrap: "wrap",
+      flexDirection: "column",
+      alignItems: "center",
+      padding: "2rem",
+    }}
+  >
+    <div
+      style={{
+        paddingBottom: "2rem",
+      }}
+    >
+      <Icon icon={icon} role="presentation" />
+    </div>
+    <Paragraph variant="small">{name}</Paragraph>
   </div>
 )
-InheritSize.parameters = { chromatic: { disable: false } }
+
+const StickerSheetTemplate: Story = () => {
+  const logoIcons = [...Object.entries(Logo)]
+  const actionIcons = [...Object.entries(Actions)]
+  const formationalIcons = [...Object.entries(Informational)]
+  const usersIcons = [...Object.entries(Users)]
+  const surveyIcons = [...Object.entries(Survey)]
+  const viewsIcons = [...Object.entries(Views)]
+  const directionalIcons = [...Object.entries(Directional)]
+  const sentimentIcons = [...Object.entries(Sentiment)]
+  const performanceIcons = [...Object.entries(Performance)]
+  const effectivenessIcons = [...Object.entries(Effectiveness)]
+  const miscellaneousIcons = [...Object.entries(Miscellaneous)]
+  return (
+    <div>
+      <Heading variant="heading-3">Logo</Heading>
+      {logoIcons.map((props, i) => (
+        <IconExampleTile
+          icon={logoIcons[i][1]}
+          {...props}
+          name={logoIcons[i][0]}
+          key={logoIcons[i][0]}
+        />
+      ))}
+      <Heading variant="heading-3">Actions</Heading>
+      {actionIcons.map((props, i) => (
+        <IconExampleTile
+          icon={actionIcons[i][1]}
+          {...props}
+          name={actionIcons[i][0]}
+          key={actionIcons[i][0]}
+        />
+      ))}
+      <Heading variant="heading-3">Informational</Heading>
+      {formationalIcons.map((props, i) => (
+        <IconExampleTile
+          icon={formationalIcons[i][1]}
+          {...props}
+          name={formationalIcons[i][0]}
+          key={formationalIcons[i][0]}
+        />
+      ))}
+      <Heading variant="heading-3">Users</Heading>
+      {usersIcons.map((props, i) => (
+        <IconExampleTile
+          icon={usersIcons[i][1]}
+          {...props}
+          name={usersIcons[i][0]}
+          key={usersIcons[i][0]}
+        />
+      ))}
+      <Heading variant="heading-3">Survey</Heading>
+      {surveyIcons.map((props, i) => (
+        <IconExampleTile
+          icon={surveyIcons[i][1]}
+          {...props}
+          name={surveyIcons[i][0]}
+          key={surveyIcons[i][0]}
+        />
+      ))}
+      <Heading variant="heading-3">Views</Heading>
+      {viewsIcons.map((props, i) => (
+        <IconExampleTile
+          icon={viewsIcons[i][1]}
+          {...props}
+          name={viewsIcons[i][0]}
+          key={viewsIcons[i][0]}
+        />
+      ))}
+      <Heading variant="heading-3">Directional</Heading>
+      {directionalIcons.map((props, i) => (
+        <IconExampleTile
+          icon={directionalIcons[i][1]}
+          {...props}
+          name={directionalIcons[i][0]}
+          key={directionalIcons[i][0]}
+        />
+      ))}
+      <Heading variant="heading-3">Sentiment</Heading>
+      {sentimentIcons.map((props, i) => (
+        <IconExampleTile
+          icon={sentimentIcons[i][1]}
+          {...props}
+          name={sentimentIcons[i][0]}
+          key={sentimentIcons[i][0]}
+        />
+      ))}
+      <Heading variant="heading-3">Performance</Heading>
+      {performanceIcons.map((props, i) => (
+        <IconExampleTile
+          icon={performanceIcons[i][1]}
+          {...props}
+          name={performanceIcons[i][0]}
+          key={performanceIcons[i][0]}
+        />
+      ))}
+      <Heading variant="heading-3">Effectiveness</Heading>
+      {effectivenessIcons.map((props, i) => (
+        <IconExampleTile
+          icon={effectivenessIcons[i][1]}
+          {...props}
+          name={effectivenessIcons[i][0]}
+          key={effectivenessIcons[i][0]}
+        />
+      ))}
+      <Heading variant="heading-3">Miscellaneous</Heading>
+      {miscellaneousIcons.map((props, i) => (
+        <IconExampleTile
+          icon={miscellaneousIcons[i][1]}
+          {...props}
+          name={miscellaneousIcons[i][0]}
+          key={miscellaneousIcons[i][0]}
+        />
+      ))}
+    </div>
+  )
+}
+export const StickerSheetDefault = StickerSheetTemplate.bind({})
+StickerSheetDefault.storyName = "All Icons"
+StickerSheetDefault.parameters = { chromatic: { disable: false } }
---
+++ packages/component-library/stories/Icons.ts
@@ -0,0 +1,379 @@
+// LOGO
+import caMonogram from "@kaizen/component-library/icons/ca-monogram.icon.svg"
+
+// ACTIONS
+import draft from "@kaizen/component-library/icons/writing.icon.svg"
+import edit from "@kaizen/component-library/icons/edit.icon.svg"
+import trash from "@kaizen/component-library/icons/trash.icon.svg"
+import refresh from "@kaizen/component-library/icons/refresh.icon.svg"
+import sync from "@kaizen/component-library/icons/sync.icon.svg"
+import duplicate from "@kaizen/component-library/icons/duplicate.icon.svg"
+import undo from "@kaizen/component-library/icons/undo.icon.svg"
+import redo from "@kaizen/component-library/icons/redo.icon.svg"
+import send from "@kaizen/component-library/icons/send.icon.svg"
+import print from "@kaizen/component-library/icons/print.icon.svg"
+import configure from "@kaizen/component-library/icons/configure.icon.svg"
+import flagOn from "@kaizen/component-library/icons/flag-on.icon.svg"
+import flagOff from "@kaizen/component-library/icons/flag-off.icon.svg"
+import actionOn from "@kaizen/component-library/icons/action-on.icon.svg"
+import actionOff from "@kaizen/component-library/icons/action-off.icon.svg"
+import comment from "@kaizen/component-library/icons/comment.icon.svg"
+import commentAdd from "@kaizen/component-library/icons/comment-add.icon.svg"
+import commentDisabled from "@kaizen/component-library/icons/comment-disabled.icon.svg"
+import clear from "@kaizen/component-library/icons/clear.icon.svg"
+import add from "@kaizen/component-library/icons/add.icon.svg"
+import subtract from "@kaizen/component-library/icons/subtract.icon.svg"
+import pause from "@kaizen/component-library/icons/pause.icon.svg"
+import equal from "@kaizen/component-library/icons/equal.icon.svg"
+import launch from "@kaizen/component-library/icons/launch.icon.svg"
+import skip from "@kaizen/component-library/icons/skip.icon.svg"
+import importIcon from "@kaizen/component-library/icons/import.icon.svg"
+import exportIcon from "@kaizen/component-library/icons/export.icon.svg"
+import kebab from "@kaizen/component-library/icons/kebab.icon.svg"
+import meatballs from "@kaizen/component-library/icons/meatballs.icon.svg"
+// import sharing from "@kaizen/component-library/icons/report-sharing.icon.svg"
+import drag from "@kaizen/component-library/icons/drag.icon.svg"
+import support from "@kaizen/component-library/icons/support.icon.svg"
+import home from "@kaizen/component-library/icons/home.icon.svg"
+import settings from "@kaizen/component-library/icons/settings.icon.svg"
+import hamburger from "@kaizen/component-library/icons/hamburger.icon.svg"
+import academy from "@kaizen/component-library/icons/academy.icon.svg"
+import search from "@kaizen/component-library/icons/search.icon.svg"
+import filter from "@kaizen/component-library/icons/filter.icon.svg"
+import power from "@kaizen/component-library/icons/power.icon.svg"
+import reportSharing from "@kaizen/component-library/icons/report-sharing.icon.svg"
+import dateStart from "@kaizen/component-library/icons/date-start.icon.svg"
+import dateEnd from "@kaizen/component-library/icons/date-end.icon.svg"
+import dateRange from "@kaizen/component-library/icons/date-range.icon.svg"
+import tag from "@kaizen/component-library/icons/tag.icon.svg"
+import logOut from "@kaizen/component-library/icons/log-out.icon.svg"
+import externalLink from "@kaizen/component-library/icons/external-link.icon.svg"
+import close from "@kaizen/component-library/icons/close.icon.svg"
+import bookmarkOff from "@kaizen/component-library/icons/bookmark-off.icon.svg"
+import bookmarkOn from "@kaizen/component-library/icons/bookmark-on.icon.svg"
+import restore from "@kaizen/component-library/icons/restore.icon.svg"
+import bold from "@kaizen/component-library/icons/bold.icon.svg"
+import italics from "@kaizen/component-library/icons/italics.icon.svg"
+import underline from "@kaizen/component-library/icons/underline.icon.svg"
+import addLink from "@kaizen/component-library/icons/add-link.icon.svg"
+import removeLink from "@kaizen/component-library/icons/remove-link.icon.svg"
+import bulletList from "@kaizen/component-library/icons/bulletted-list.icon.svg"
+import numberedList from "@kaizen/component-library/icons/numbered-list.icon.svg"
+import numberedlistRTL from "@kaizen/component-library/icons/numbered-list-rtl.icon.svg"
+import increaseIndent from "@kaizen/component-library/icons/increase-indent.icon.svg"
+import decreseIndent from "@kaizen/component-library/icons/decrease-indent.icon.svg"
+import photoUpload from "@kaizen/component-library/icons/photo-upload.icon.svg"
+import minimize from "@kaizen/component-library/icons/minimize.icon.svg"
+import maximize from "@kaizen/component-library/icons/maximize.icon.svg"
+import repeats from "@kaizen/component-library/icons/repeats.icon.svg"
+// import connect from "@kaizen/component-library/icons/connect.icon.svg"
+// import disconnect from "@kaizen/component-library/icons/disconnect.icon.svg"
+import zoomIn from "@kaizen/component-library/icons/zoom-in.icon.svg"
+import zoomOut from "@kaizen/component-library/icons/zoom-out.icon.svg"
+
+// INFORMATIONAL
+import success from "@kaizen/component-library/icons/success.icon.svg"
+import information from "@kaizen/component-library/icons/information.icon.svg"
+import warning from "@kaizen/component-library/icons/exclamation.icon.svg"
+import promotion from "@kaizen/component-library/icons/promotion.icon.svg"
+import time from "@kaizen/component-library/icons/time.icon.svg"
+import question from "@kaizen/component-library/icons/question.icon.svg"
+import indicatorInactive from "@kaizen/component-library/icons/empty.icon.svg"
+import indicatorActive from "@kaizen/component-library/icons/full.icon.svg"
+import file from "@kaizen/component-library/icons/file.icon.svg"
+import report from "@kaizen/component-library/icons/report.icon.svg"
+import kiosk from "@kaizen/component-library/icons/kiosk.icon.svg"
+import minus from "@kaizen/component-library/icons/minus.icon.svg"
+import check from "@kaizen/component-library/icons/check.icon.svg"
+import spinner from "@kaizen/component-library/icons/spinner.icon.svg"
+import delta from "@kaizen/component-library/icons/delta-bare.icon.svg"
+import deltaNegative from "@kaizen/component-library/icons/delta-negative.icon.svg"
+import deltaNone from "@kaizen/component-library/icons/delta-flat.icon.svg"
+import deltaPositive from "@kaizen/component-library/icons/delta-positive.icon.svg"
+import guidance from "@kaizen/component-library/icons/guidance.icon.svg"
+import notification from "@kaizen/component-library/icons/notification.icon.svg"
+import communications from "@kaizen/component-library/icons/communications.icon.svg"
+import factors from "@kaizen/component-library/icons/factors.icon.svg"
+import lock from "@kaizen/component-library/icons/lock.icon.svg"
+import translation from "@kaizen/component-library/icons/translation.icon.svg"
+import permissions from "@kaizen/component-library/icons/permissions.icon.svg"
+import organization from "@kaizen/component-library/icons/organization.icon.svg"
+import securityTip from "@kaizen/component-library/icons/security-tip.icon.svg"
+import department from "@kaizen/component-library/icons/department.icon.svg"
+import informationReport from "@kaizen/component-library/icons/exclamation-octagon.icon.svg"
+import percentage from "@kaizen/component-library/icons/percentage.icon.svg"
+import branching from "@kaizen/component-library/icons/branching.icon.svg"
+import competencyLibrary from "@kaizen/component-library/icons/competency-library.icon.svg"
+
+// USERS
+import individual from "@kaizen/component-library/icons/person.icon.svg"
+import user from "@kaizen/component-library/icons/user.icon.svg"
+import users from "@kaizen/component-library/icons/users.icon.svg"
+import userAdd from "@kaizen/component-library/icons/user-add.icon.svg"
+import userSelect from "@kaizen/component-library/icons/user-select.icon.svg"
+import userDelete from "@kaizen/component-library/icons/user-delete.icon.svg"
+import userSettings from "@kaizen/component-library/icons/user-settings.icon.svg"
+import userExit from "@kaizen/component-library/icons/user-exit.icon.svg"
+import userUpdate from "@kaizen/component-library/icons/user-update.icon.svg"
+
+// SURVEYS
+import surveys from "@kaizen/component-library/icons/surveys.icon.svg"
+import surveyDraft from "@kaizen/component-library/icons/draft.icon.svg"
+import surveyOpen from "@kaizen/component-library/icons/open.icon.svg"
+import surveyClosed from "@kaizen/component-library/icons/closed.icon.svg"
+import surveyArchived from "@kaizen/component-library/icons/archived.icon.svg"
+import surveyBlank from "@kaizen/component-library/icons/blank.icon.svg"
+import surveyTemplate from "@kaizen/component-library/icons/template.icon.svg"
+import surveyUnattributed from "@kaizen/component-library/icons/unattributed.icon.svg"
+
+// VIEWS
+import gridView from "@kaizen/component-library/icons/grid-view.icon.svg"
+import listView from "@kaizen/component-library/icons/list-view.icon.svg"
+import visible from "@kaizen/component-library/icons/visible.icon.svg"
+import invisible from "@kaizen/component-library/icons/invisible.icon.svg"
+import hierarchy from "@kaizen/component-library/icons/hierarchy.icon.svg"
+import navigator from "@kaizen/component-library/icons/navigator.icon.svg"
+
+// DIRECTIONAL
+import arrowLeft from "@kaizen/component-library/icons/arrow-left.icon.svg"
+import arrowRight from "@kaizen/component-library/icons/arrow-right.icon.svg"
+import arrowDown from "@kaizen/component-library/icons/arrow-down.icon.svg"
+import arrowUp from "@kaizen/component-library/icons/arrow-up.icon.svg"
+import chevronDown from "@kaizen/component-library/icons/chevron-down.icon.svg"
+import chevronUp from "@kaizen/component-library/icons/chevron-up.icon.svg"
+import sortDescending from "@kaizen/component-library/icons/sort-descending.icon.svg"
+import sortAscending from "@kaizen/component-library/icons/sort-ascending.icon.svg"
+// import reverse from "@kaizen/component-library/icons/reverse.icon.svg"
+import start from "@kaizen/component-library/icons/start.icon.svg"
+import end from "@kaizen/component-library/icons/end.icon.svg"
+
+// Sentiment
+import verySatisfied from "@kaizen/component-library/icons/face-very-satisfied.icon.svg"
+import satisfied from "@kaizen/component-library/icons/face-satisfied.icon.svg"
+import dissatisfied from "@kaizen/component-library/icons/face-dissatisfied.icon.svg"
+import neutral from "@kaizen/component-library/icons/face-neutral.icon.svg"
+import veryDissatisfied from "@kaizen/component-library/icons/face-very-dissatisfied.icon.svg"
+import thumbsUp from "@kaizen/component-library/icons/thumbs-up.icon.svg"
+import thumbsDown from "@kaizen/component-library/icons/thumbs-down.icon.svg"
+
+// PERFORMANCE
+import leaderBoard from "@kaizen/component-library/icons/leaderboard.icon.svg"
+import favoriteOff from "@kaizen/component-library/icons/favorite-off.icon.svg"
+import favoriteOn from "@kaizen/component-library/icons/favorite-on.icon.svg"
+import starOff from "@kaizen/component-library/icons/star-off.icon.svg"
+import starrOn from "@kaizen/component-library/icons/star-on.icon.svg"
+import email from "@kaizen/component-library/icons/email.icon.svg"
+
+// EFFECTIVENESS
+import feedbackCompleted from "@kaizen/component-library/icons/feedback-completed.icon.svg"
+import processManager from "@kaizen/component-library/icons/process-manager.icon.svg"
+import participation from "@kaizen/component-library/icons/participation.icon.svg"
+import feedbackReport from "@kaizen/component-library/icons/feedback-report.icon.svg"
+import feedbackReview from "@kaizen/component-library/icons/feedback-review.icon.svg"
+import feedbackClassify from "@kaizen/component-library/icons/feedback-classify.icon.svg"
+import feedbackShare from "@kaizen/component-library/icons/feedback-share.icon.svg"
+
+// MISCELLANEOUS
+import custom from "@kaizen/component-library/icons/custom.icon.svg"
+import dashboard from "@kaizen/component-library/icons/dashboard.icon.svg"
+import experience from "@kaizen/component-library/icons/experience.icon.svg"
+import effectiveness from "@kaizen/component-library/icons/effectiveness.icon.svg"
+import heatmap from "@kaizen/component-library/icons/heatmap.icon.svg"
+import insight from "@kaizen/component-library/icons/insight.icon.svg"
+import tasks from "@kaizen/component-library/icons/tasks.icon.svg"
+import textAnalytics from "@kaizen/component-library/icons/text-analytics.icon.svg"
+
+export const Logo = {
+  caMonogram,
+}
+
+export const Actions = {
+  draft,
+  edit,
+  trash,
+  refresh,
+  sync,
+  duplicate,
+  undo,
+  redo,
+  send,
+  print,
+  configure,
+  flagOn,
+  flagOff,
+  actionOn,
+  actionOff,
+  comment,
+  commentAdd,
+  commentDisabled,
+  clear,
+  add,
+  subtract,
+  pause,
+  equal,
+  launch,
+  skip,
+  importIcon,
+  exportIcon,
+  kebab,
+  meatballs,
+  // sharing,
+  drag,
+  support,
+  home,
+  settings,
+  hamburger,
+  academy,
+  search,
+  filter,
+  power,
+  reportSharing,
+  dateStart,
+  dateEnd,
+  dateRange,
+  tag,
+  logOut,
+  externalLink,
+  close,
+  bookmarkOff,
+  bookmarkOn,
+  restore,
+  bold,
+  italics,
+  underline,
+  addLink,
+  removeLink,
+  bulletList,
+  numberedList,
+  numberedlistRTL,
+  increaseIndent,
+  decreseIndent,
+  photoUpload,
+  minimize,
+  maximize,
+  repeats,
+  // connect,
+  // disconnect,
+  zoomIn,
+  zoomOut,
+}
+
+export const Informational = {
+  success,
+  information,
+  warning,
+  promotion,
+  time,
+  question,
+  indicatorInactive,
+  indicatorActive,
+  file,
+  report,
+  kiosk,
+  minus,
+  check,
+  spinner,
+  delta,
+  deltaNegative,
+  deltaNone,
+  deltaPositive,
+  guidance,
+  notification,
+  communications,
+  factors,
+  lock,
+  translation,
+  permissions,
+  organization,
+  securityTip,
+  department,
+  informationReport,
+  percentage,
+  branching,
+  competencyLibrary,
+}
+
+export const Users = {
+  individual,
+  user,
+  users,
+  userAdd,
+  userSelect,
+  userDelete,
+  userSettings,
+  userExit,
+  userUpdate,
+}
+
+export const Survey = {
+  surveys,
+  surveyDraft,
+  surveyOpen,
+  surveyClosed,
+  surveyArchived,
+  surveyBlank,
+  surveyTemplate,
+  surveyUnattributed,
+}
+
+export const Views = {
+  gridView,
+  listView,
+  visible,
+  invisible,
+  hierarchy,
+  navigator,
+}
+
+export const Directional = {
+  arrowLeft,
+  arrowRight,
+  arrowDown,
+  arrowUp,
+  chevronDown,
+  chevronUp,
+  sortDescending,
+  sortAscending,
+  // reverse,
+  start,
+  end,
+}
+
+export const Sentiment = {
+  verySatisfied,
+  satisfied,
+  dissatisfied,
+  neutral,
+  veryDissatisfied,
+  thumbsUp,
+  thumbsDown,
+}
+
+export const Performance = {
+  leaderBoard,
+  favoriteOff,
+  favoriteOn,
+  starOff,
+  starrOn,
+  email,
+}
+export const Effectiveness = {
+  feedbackCompleted,
+  processManager,
+  participation,
+  feedbackReport,
+  feedbackReview,
+  feedbackClassify,
+  feedbackShare,
+}
+
+export const Miscellaneous = {
+  custom,
+  dashboard,
+  experience,
+  effectiveness,
+  heatmap,
+  insight,
+  tasks,
+  textAnalytics,
+}
