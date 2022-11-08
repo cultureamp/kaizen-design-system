b0c579780f91eaeaa23a4cd26ee6b02d1d6c6ff4
Natalie Loyzaga <nloyzaga@gmail.com>
2022-11-08 15:51:08 +1100

    test: add tests for providers
    fix: tests
    
    feat: remove all test

---
+++ packages/select/src/Select/provider/MenuTriggerProvider/MenuTriggerProvider.spec.tsx
@@ -0,0 +1,181 @@
+import React from "react"
+import { render, screen } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
+import { TriggerButtonBase } from "../../components/Trigger/TriggerButtonBase"
+import { FloatingSelectWrapper } from "../../components/FloatingSelectWrapper"
+import {
+  MenuTriggerProvider,
+  MenuTriggerProviderProps,
+} from "./MenuTriggerProvider"
+
+const MenuTriggerProviderWrapper = (
+  props: Partial<MenuTriggerProviderProps>
+) => (
+  <MenuTriggerProvider {...props}>
+    <TriggerButtonBase>trigger-display-label-mock</TriggerButtonBase>
+    <FloatingSelectWrapper>
+      <span>menu-content-mock</span>
+      <button>menu-content-button-mock</button>
+    </FloatingSelectWrapper>
+  </MenuTriggerProvider>
+)
+
+describe("<MenuTriggerProvider /> - Visual content", () => {
+  it("shows the trigger initially", () => {
+    render(<MenuTriggerProviderWrapper />)
+    const trigger = screen.getByRole("button", {
+      name: "trigger-display-label-mock",
+    })
+    expect(trigger).toBeVisible()
+  })
+
+  it("makes sure the menu to be labelled by trigger", () => {
+    render(<MenuTriggerProviderWrapper defaultOpen />)
+    const menu = screen.getByLabelText("trigger-display-label-mock")
+
+    expect(menu).toHaveTextContent("menu-content-mock")
+  })
+
+  describe("when uncontrolled", () => {
+    it("does not show the menu initially", () => {
+      render(<MenuTriggerProviderWrapper />)
+      const menu = screen.queryByText("menu-content-mock")
+      expect(menu).not.toBeInTheDocument()
+    })
+
+    it("shows the menu when defaultOpen is set to true", () => {
+      render(<MenuTriggerProviderWrapper defaultOpen />)
+      const menu = screen.queryByText("menu-content-mock")
+      expect(menu).toBeVisible()
+    })
+  })
+  describe("when controlled", () => {
+    it("shows the menu based on the isOpen prop", () => {
+      const { rerender } = render(<MenuTriggerProviderWrapper isOpen />)
+
+      const menu = screen.queryByText("menu-content-mock")
+      expect(menu).toBeVisible()
+
+      rerender(<MenuTriggerProviderWrapper isOpen={false} />)
+      expect(screen.queryByText("menu-content-mock")).not.toBeInTheDocument()
+    })
+
+    it("fires the onOpenChange callback when the trigger is interacted", () => {
+      const onOpenChange = jest.fn<void, [boolean]>()
+      render(<MenuTriggerProviderWrapper isOpen onOpenChange={onOpenChange} />)
+
+      const trigger = screen.getByRole("button", {
+        name: "trigger-display-label-mock",
+      })
+      userEvent.click(trigger)
+      expect(onOpenChange).toBeCalledTimes(1)
+      expect(onOpenChange).toBeCalledWith(false)
+    })
+  })
+})
+
+describe("<MenuTriggerProvider /> - Mouse interaction", () => {
+  describe("Given the menu is closed", () => {
+    it("is opened when user clicks on the trigger", () => {
+      render(<MenuTriggerProviderWrapper />)
+      const trigger = screen.getByRole("button", {
+        name: "trigger-display-label-mock",
+      })
+      userEvent.click(trigger)
+
+      expect(screen.queryByText("menu-content-mock")).toBeVisible()
+    })
+  })
+
+  describe("Given the menu is opened", () => {
+    it("is closed when user clicks on the trigger", () => {
+      render(<MenuTriggerProviderWrapper defaultOpen />)
+      const trigger = screen.getByRole("button", {
+        name: "trigger-display-label-mock",
+      })
+      userEvent.click(trigger)
+
+      expect(screen.queryByText("menu-content-mock")).not.toBeInTheDocument()
+    })
+
+    it("is closed when user clicks outside of the menu", () => {
+      render(<MenuTriggerProviderWrapper defaultOpen />)
+      userEvent.click(document.body)
+
+      expect(screen.queryByText("menu-content-mock")).not.toBeInTheDocument()
+    })
+
+    it("remains open when user clicks inside of the menu", () => {
+      render(<MenuTriggerProviderWrapper defaultOpen />)
+      const buttonInsideMenu = screen.getByRole("button", {
+        name: "menu-content-button-mock",
+      })
+      userEvent.click(buttonInsideMenu)
+
+      expect(screen.queryByText("menu-content-mock")).toBeVisible()
+    })
+  })
+})
+
+describe("<MenuTriggerProvider /> - Keyboard interaction", () => {
+  describe("Given the menu is closed", () => {
+    it("allows the user to tab to the trigger", () => {
+      render(<MenuTriggerProviderWrapper />)
+      const trigger = screen.getByRole("button", {
+        name: "trigger-display-label-mock",
+      })
+      userEvent.tab()
+      userEvent.tab()
+
+      expect(trigger).toHaveFocus()
+    })
+
+    it("opens the menu when hits enter key", () => {
+      render(<MenuTriggerProviderWrapper />)
+      const trigger = screen.getByRole("button", {
+        name: "trigger-display-label-mock",
+      })
+      trigger.focus()
+
+      userEvent.keyboard("{Enter}")
+
+      expect(screen.queryByText("menu-content-mock")).toBeVisible()
+    })
+  })
+
+  describe("Given the menu is opened", () => {
+    // TBD
+    it("moves the focus to the first focusable element inside the menu initially", () => {
+      render(<MenuTriggerProviderWrapper defaultOpen />)
+      expect(screen.queryByText("menu-content-mock")).toBeVisible()
+
+      expect(
+        screen.queryByRole("button", { name: "menu-content-button-mock" })
+      ).toHaveFocus()
+    })
+
+    it("is closed when hits the escape key", () => {
+      render(<MenuTriggerProviderWrapper defaultOpen />)
+
+      expect(
+        screen.queryByRole("button", { name: "menu-content-button-mock" })
+      ).toHaveFocus()
+
+      userEvent.keyboard("{Escape}")
+
+      expect(screen.queryByText("menu-content-mock")).not.toBeInTheDocument()
+    })
+
+    it("is closed when the focus is moved outside of the menu", () => {
+      render(<MenuTriggerProviderWrapper defaultOpen />)
+
+      expect(
+        screen.queryByRole("button", { name: "menu-content-button-mock" })
+      ).toHaveFocus()
+
+      userEvent.click(document.body)
+
+      expect(screen.queryByText("menu-content-mock")).not.toBeInTheDocument()
+    })
+  })
+})
---
+++ packages/select/src/Select/provider/SelectionProvider/SelectionProvider.spec.tsx
@@ -0,0 +1,334 @@
+import React, { Key, useState } from "react"
+import { render, screen } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
+import { ItemType } from "../../types"
+import { Select } from "../../Select"
+import { SelectionProvider, SelectionProviderProps } from "./SelectionProvider"
+
+const itemsMock: ItemType[] = [
+  {
+    label: "option-1-label-mock",
+    value: "option-1-value-mock",
+  },
+  {
+    label: "option-2-label-mock",
+    value: "option-2-value-mock",
+  },
+  {
+    label: "option-3-label-mock",
+    value: "option-3-value-mock",
+  },
+]
+
+const SelectionProviderWrapper = ({
+  items = itemsMock,
+  selectedKey,
+  onSelectionChange,
+  ...props
+}: Partial<SelectionProviderProps>) => {
+  const [selected, setSelected] = useState<Key | null>(selectedKey ?? null)
+
+  return (
+    <SelectionProvider
+      items={items}
+      label="selection-label-mock"
+      selectedKey={selected}
+      onSelectionChange={selection => {
+        setSelected(selection)
+        onSelectionChange && onSelectionChange(selection)
+      }}
+      {...props}
+    >
+      <Select.ListBox>
+        {({ allItems }) =>
+          allItems.map(item => <Select.Option key={item.key} item={item} />)
+        }
+      </Select.ListBox>
+    </SelectionProvider>
+  )
+}
+
+describe("<SelectionProviderWrapper /> - Visual content", () => {
+  describe("Given no selectedKeys", () => {
+    it("shows all the options unselected", () => {
+      render(<SelectionProviderWrapper />)
+      expect(
+        screen.getByRole("option", {
+          name: "option-1-label-mock",
+          selected: false,
+        })
+      ).toBeVisible()
+      expect(
+        screen.getByRole("option", {
+          name: "option-2-label-mock",
+          selected: false,
+        })
+      ).toBeVisible()
+      expect(
+        screen.getByRole("option", {
+          name: "option-3-label-mock",
+          selected: false,
+        })
+      ).toBeVisible()
+    })
+
+    test("The listbox is labelled by the provided label", () => {
+      render(<SelectionProviderWrapper />)
+      const listBox = screen.getByLabelText("selection-label-mock", {
+        selector: "ul",
+      })
+      expect(listBox).toBeInTheDocument()
+    })
+  })
+
+  describe("Given selectedKeys is [option-2-value-mock]", () => {
+    it("shows only option 2 is selected", () => {
+      render(<SelectionProviderWrapper selectedKey="option-2-value-mock" />)
+      expect(
+        screen.getByRole("option", {
+          name: "option-1-label-mock",
+          selected: false,
+        })
+      ).toBeVisible()
+      expect(
+        screen.getByRole("option", {
+          name: "option-2-label-mock",
+          selected: true,
+        })
+      ).toBeVisible()
+      expect(
+        screen.getByRole("option", {
+          name: "option-3-label-mock",
+          selected: false,
+        })
+      ).toBeVisible()
+    })
+  })
+})
+
+describe("<SelectionProviderWrapper /> - Mouse interaction", () => {
+  it("selects the option when clicks on a non-selected option", () => {
+    render(<SelectionProviderWrapper />)
+    const option1 = screen.getByRole("option", {
+      name: "option-1-label-mock",
+    })
+
+    userEvent.click(option1)
+
+    expect(
+      screen.getByRole("option", {
+        name: "option-1-label-mock",
+        selected: true,
+      })
+    ).toBeVisible()
+  })
+
+  it("fires onSelectionChange when clicks on a option", () => {
+    const spy = jest.fn()
+    render(<SelectionProviderWrapper onSelectionChange={spy} />)
+    const option1 = screen.getByRole("option", {
+      name: "option-1-label-mock",
+    })
+
+    userEvent.click(option1)
+    expect(spy).toHaveBeenCalledTimes(1)
+  })
+
+  it("selects all options when clicks on Select all button", () => {
+    render(<SelectionProviderWrapper />)
+    const selectAll = screen.getByRole("button", {
+      name: "Select all",
+    })
+
+    userEvent.click(selectAll)
+
+    expect(
+      screen.getByRole("option", {
+        name: "option-1-label-mock",
+        selected: true,
+      })
+    ).toBeVisible()
+
+    expect(
+      screen.getByRole("option", {
+        name: "option-2-label-mock",
+        selected: true,
+      })
+    ).toBeVisible()
+
+    expect(
+      screen.getByRole("option", {
+        name: "option-3-label-mock",
+        selected: true,
+      })
+    ).toBeVisible()
+  })
+
+  it("fires onSelectionChange when clicks on Select all button", () => {
+    const spy = jest.fn()
+    render(<SelectionProviderWrapper onSelectionChange={spy} />)
+    const selectAll = screen.getByRole("button", {
+      name: "Select all",
+    })
+
+    userEvent.click(selectAll)
+    expect(spy).toHaveBeenCalledTimes(1)
+  })
+
+  it("de-selects the option when clicks on a selected option", () => {
+    render(<SelectionProviderWrapper selectedKey="option-2-value-mock" />)
+    const option2 = screen.getByRole("option", {
+      name: "option-2-label-mock",
+      selected: true,
+    })
+
+    userEvent.click(option2)
+
+    expect(
+      screen.getByRole("option", {
+        name: "option-2-label-mock",
+        selected: false,
+      })
+    ).toBeVisible()
+  })
+})
+
+describe("<SelectionProviderWrapper /> - Keyboard interaction", () => {
+  describe("Given no selectedKeys", () => {
+    it("focuses on the frist option when tabs onto the list", () => {
+      render(<SelectionProviderWrapper />)
+      userEvent.tab()
+
+      expect(
+        screen.getByRole("option", { name: "option-1-label-mock" })
+      ).toHaveFocus()
+    })
+  })
+
+  describe("Given selectedKey is option-2-value-mock", () => {
+    it("focuses the frist selected option when tabs onto the list", () => {
+      render(<SelectionProviderWrapper selectedKey="option-2-value-mock" />)
+      userEvent.tab()
+
+      expect(
+        screen.getByRole("option", { name: "option-2-label-mock" })
+      ).toHaveFocus()
+    })
+  })
+
+  it("moves the focus down when hits arrow down key", () => {
+    render(<SelectionProviderWrapper />)
+    userEvent.tab()
+    userEvent.keyboard("{ArrowDown}")
+
+    expect(
+      screen.getByRole("option", { name: "option-3-label-mock" })
+    ).toHaveFocus()
+  })
+
+  it("keeps the focus at the last element when hits arrow down key on it", () => {
+    render(<SelectionProviderWrapper selectedKey="option-3-value-mock" />)
+    userEvent.tab()
+    userEvent.keyboard("{ArrowDown}")
+
+    expect(
+      screen.getByRole("option", { name: "option-3-label-mock" })
+    ).toHaveFocus()
+  })
+
+  it("moves the focus up when hits arrow up key", () => {
+    render(<SelectionProviderWrapper selectedKey="option-3-value-mock" />)
+    userEvent.tab()
+    userEvent.keyboard("{ArrowUp}")
+
+    expect(
+      screen.getByRole("option", { name: "option-2-label-mock" })
+    ).toHaveFocus()
+  })
+
+  it("keeps the focus ring at the first element when hits arrow up key on it", () => {
+    render(<SelectionProviderWrapper />)
+    userEvent.tab()
+    userEvent.keyboard("{ArrowUp}")
+
+    expect(
+      screen.getByRole("option", { name: "option-1-label-mock" })
+    ).toHaveFocus()
+  })
+
+  it("selects the option when hits enter on a non-selected option", () => {
+    render(<SelectionProviderWrapper />)
+
+    userEvent.tab()
+    userEvent.keyboard("{Enter}")
+
+    expect(
+      screen.getByRole("option", {
+        name: "option-1-label-mock",
+        selected: true,
+      })
+    ).toBeVisible()
+  })
+
+  it("de-selects the option when hits enter on a selected option", () => {
+    render(<SelectionProviderWrapper selectedKey="option-2-value-mock" />)
+
+    userEvent.tab()
+    userEvent.keyboard("{Enter}")
+
+    expect(
+      screen.getByRole("option", {
+        name: "option-2-label-mock",
+        selected: false,
+      })
+    ).toBeVisible()
+  })
+
+  it("fires onSelectionChange when hits enter on a option", () => {
+    const spy = jest.fn()
+    render(<SelectionProviderWrapper onSelectionChange={spy} />)
+
+    userEvent.tab()
+    userEvent.keyboard("{Enter}")
+
+    expect(spy).toHaveBeenCalledTimes(1)
+  })
+})
+
+describe("<SelectionProviderWrapper /> - controlling items from the consumer", () => {
+  it("renders only items passed", () => {
+    const { rerender } = render(<SelectionProviderWrapper />)
+    expect(
+      screen.getByRole("option", {
+        name: "option-1-label-mock",
+      })
+    ).toBeVisible()
+    expect(
+      screen.getByRole("option", {
+        name: "option-2-label-mock",
+      })
+    ).toBeVisible()
+    expect(
+      screen.getByRole("option", {
+        name: "option-3-label-mock",
+      })
+    ).toBeVisible()
+
+    rerender(<SelectionProviderWrapper items={itemsMock.slice(2)} />)
+    expect(
+      screen.queryByRole("option", {
+        name: "option-1-label-mock",
+      })
+    ).not.toBeInTheDocument()
+    expect(
+      screen.queryByRole("option", {
+        name: "option-2-label-mock",
+      })
+    ).not.toBeInTheDocument()
+    expect(
+      screen.getByRole("option", {
+        name: "option-3-label-mock",
+      })
+    ).toBeVisible()
+  })
+})
