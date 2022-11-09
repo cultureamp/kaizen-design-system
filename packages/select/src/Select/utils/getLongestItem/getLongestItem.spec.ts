01b8178216b8eacd96a41558ccc705cc63ab8b01
Natalie Loyzaga <nloyzaga@gmail.com>
2022-11-10 10:49:31 +1100

    feat: update getLongestItem test with new mock data

--- packages/select/src/Select/utils/getLongestItem/getLongestItem.spec.ts
+++ packages/select/src/Select/utils/getLongestItem/getLongestItem.spec.ts
@@ -11,9 +11,18 @@ const singleItems: ItemType[] = [
   { label: "SuperSameItem", value: "id-same" },
 ]
 
+const singleItemsOne: ItemType[] = [
+  { label: "Front-End", value: "id-fe" },
+  { label: "Back-End", value: "id-be" },
+  { label: "SRE", value: "id-sre" },
+  { label: "Dev-ops", value: "id-devops" },
+  { label: "Others", value: "id-others" },
+  { label: "SuperlongItem", value: "id-long" },
+]
+
 describe("getLongestItem", () => {
   it("returns the longest string", () => {
-    expect(getLongestItem(singleItems)).toEqual("SuperlongItem")
+    expect(getLongestItem(singleItemsOne)).toEqual("SuperlongItem")
   })
   it("returns one longest string when there are 2 of the same length", () => {
     expect(getLongestItem(singleItems)).toEqual("SuperSameItem")
