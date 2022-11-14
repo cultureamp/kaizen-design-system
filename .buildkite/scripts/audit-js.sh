41afc7d349f4e02329ab340bfc7a1159df774d75
Will Brennan <will.brennan@cultureamp.com>
2022-11-14 15:39:02 +1100

    fix(deps): remove @cultureamp/frontend-ci-scripts from dev deps

--- .buildkite/scripts/audit-js.sh
+++ .buildkite/scripts/audit-js.sh
@@ -4,8 +4,5 @@ set -e
 # shellcheck source=setup-registry.sh
 . .buildkite/scripts/helpers/setup-registry.sh
 
-echo "--- :yarnpkg: Install dependencies"
-yarn install --frozen-lockfile
-
 echo "--- :nodesecurity: Audit JS dependencies"
 npm_config_registry=https://npm.pkg.github.com/ npx -p @cultureamp/frontend-ci-scripts@latest --call frontend-audit-js-dependencies
