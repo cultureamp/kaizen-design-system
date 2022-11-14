592db4793b0de9414ae069469c871a6ae00025ab
Will Brennan <will.brennan@cultureamp.com>
2022-11-14 16:20:05 +1100

    chore: Remove audit js script

--- .buildkite/scripts/audit-js.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-
-# shellcheck source=setup-registry.sh
-. .buildkite/scripts/helpers/setup-registry.sh
-
-echo "--- :nodesecurity: Audit JS dependencies"
-npm_config_yes=true npm_config_registry=https://npm.pkg.github.com/ npx -p @cultureamp/frontend-ci-scripts@latest --call frontend-audit-js-dependencies
