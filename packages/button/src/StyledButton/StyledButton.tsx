45b324bc918ff27ed832f6f900f20c407fa8f2cf
Cassandra Tam <cassandra.tam@cultureamp.com>
2022-11-18 12:01:55 +1100

    wip

---
+++ packages/button/src/StyledButton/StyledButton.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+export interface StyledButtonProps {
+  children: React.ReactNode
+}
+
+export const StyledButton: React.VFC<StyledButtonProps> = ({ children }) => {
+  {React.Children.only(children, () => {
+
+  })}
+}
