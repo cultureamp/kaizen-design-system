457c519204d23890814c0c5b74c49fcae2b7633f
mcwinter07 <mcwinter07@gmail.com>
2022-11-23 13:32:09 +1100

    add #docs-root id to show tw classes in docs tabe for storybook

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,8 @@
 module.exports = {
   content: ["./**/*.{ts,tsx}"],
   presets: [require("@kaizen/design-tokens").TailwindPreset],
-  // important to add to the root to ensure that tailwind classes supersede component styles
-  important: "#root",
+  // important to add to the #root and #docs-root (for storybook) to ensure that tailwind classes supersede component styles
+  important: ["#root", "#docs-root"],
   theme: {
     extend: {},
   },
