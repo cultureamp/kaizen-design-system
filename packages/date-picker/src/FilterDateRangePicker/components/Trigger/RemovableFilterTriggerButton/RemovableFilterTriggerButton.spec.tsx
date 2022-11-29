4a9e415ad7a9de24eb7804f9ef0e26f157674d11
Cassandra Tam <cassandra.tam@cultureamp.com>
2022-11-29 16:14:20 +1100

    test(date-picker): add tests for removable filter button

--- packages/date-picker/src/FilterDateRangePicker/components/Trigger/RemovableFilterTriggerButton/RemovableFilterTriggerButton.spec.tsx
+++ packages/date-picker/src/FilterDateRangePicker/components/Trigger/RemovableFilterTriggerButton/RemovableFilterTriggerButton.spec.tsx
@@ -1,19 +1,76 @@
-import React from "react"
+import React, { useRef } from "react"
 import { render, screen } from "@testing-library/react"
-import { FilterTriggerButton, FilterTriggerButtonProps } from "."
+import userEvent from "@testing-library/user-event"
+import {
+  RemovableFilterTriggerButton,
+  RemovableFilterTriggerButtonProps,
+} from "."
 
-const FilterTriggerButtonWrapper = (
-  props: Partial<FilterTriggerButtonProps>
-) => <FilterTriggerButton label="Desserts" isOpen={false} {...props} />
+const RemovableFilterTriggerButtonWrapper = ({
+  triggerButtonProps,
+  removeButtonProps,
+  ...restProps
+}: Partial<RemovableFilterTriggerButtonProps>) => (
+  <RemovableFilterTriggerButton
+    triggerButtonProps={{
+      ...triggerButtonProps,
+      label: "Desserts",
+    }}
+    removeButtonProps={{
+      ...removeButtonProps,
+    }}
+    {...restProps}
+  />
+)
 
-describe("<FilterTriggerButton />", () => {
-  it("should only show the label when there is no value", () => {
-    render(<FilterTriggerButtonWrapper />)
-    expect(screen.getByRole("button", { name: "Desserts" })).toBeVisible()
+describe("<RemovableFilterTriggerButton />", () => {
+  it("should use fallback label for remove button if not specified", () => {
+    render(<RemovableFilterTriggerButtonWrapper />)
+    expect(
+      screen.getByRole("button", { name: "Remove filter - Desserts" })
+    ).toBeVisible()
   })
 
-  it("should show the label and selected value when defined", () => {
-    render(<FilterTriggerButtonWrapper selectedValue="Cake" />)
-    expect(screen.getByRole("button", { name: "Desserts: Cake" })).toBeVisible()
+  describe("Refs", () => {
+    it("correctly passes through both button refs", async () => {
+      const onClick = jest.fn<
+        void,
+        [string | null | undefined, string | null | undefined]
+      >()
+
+      const Wrapper = () => {
+        const triggerButtonRef = useRef<HTMLButtonElement>(null)
+        const removeButtonRef = useRef<HTMLButtonElement>(null)
+        const ref = useRef({ triggerButtonRef, removeButtonRef })
+
+        const handleClick = (): void =>
+          onClick(triggerButtonRef.current?.id, removeButtonRef.current?.id)
+
+        return (
+          <>
+            <RemovableFilterTriggerButton
+              ref={ref}
+              id="test__date-input-field--ref"
+              triggerButtonProps={{
+                label: "Desserts",
+                id: "test__trigger-button",
+              }}
+              removeButtonProps={{
+                id: "test__remove-button",
+              }}
+            />
+            <button onClick={handleClick}>Click me</button>
+          </>
+        )
+      }
+
+      render(<Wrapper />)
+
+      await userEvent.click(screen.getByText("Click me"))
+      expect(onClick).toBeCalledWith(
+        "test__trigger-button",
+        "test__remove-button"
+      )
+    })
   })
 })
