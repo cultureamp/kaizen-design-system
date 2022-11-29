307cc0988c09b3481dad74642efd06900ec03b04
Cassandra Tam <cassandra.tam@cultureamp.com>
2022-11-29 15:04:26 +1100

    chore(date-picker): rename filter stories

--- packages/date-picker/docs/filter-button-foundations/FilterButtonGroup.stories.tsx
+++ packages/date-picker/docs/filter-button-foundations/FilterButtonGroup.stories.tsx
@@ -1,15 +1,15 @@
 import React from "react"
 import { ComponentMeta, ComponentStory, Story } from "@storybook/react"
 import { withDesign } from "storybook-addon-designs"
-import { StoryWrapper } from "../../../storybook/components/StoryWrapper"
+import { StoryWrapper } from "../../../../storybook/components/StoryWrapper"
 import {
   CATEGORIES,
   SUB_CATEGORIES,
   SUB_COMPONENTS_FOLDER_NAME,
-} from "../../../storybook/constants"
-import { FilterBaseButton } from "../src/FilterDateRangePicker/components/Trigger/FilterBaseButton"
-import { FilterBaseTooltipButton } from "../src/FilterDateRangePicker/components/Trigger/FilterBaseTooltipButton"
-import { FilterButtonGroup } from "../src/FilterDateRangePicker/components/Trigger/FilterButtonGroup"
+} from "../../../../storybook/constants"
+import { FilterBaseButton } from "../../src/FilterDateRangePicker/components/Trigger/FilterBaseButton"
+import { FilterBaseTooltipButton } from "../../src/FilterDateRangePicker/components/Trigger/FilterBaseTooltipButton"
+import { FilterButtonGroup } from "../../src/FilterDateRangePicker/components/Trigger/FilterButtonGroup"
 
 export default {
   title: `${CATEGORIES.components}/${SUB_CATEGORIES.datePicker}/Filter Date Range Picker/${SUB_COMPONENTS_FOLDER_NAME}/Filter Button Group`,
