84856dbc749d31904a5abd0b49af7ab56d29c603
Cassandra Tam <cassandra.tam@cultureamp.com>
2022-12-12 23:41:14 +1100

    increase delays; test 1

--- draft-packages/tooltip/docs/Tooltip.stories.tsx
+++ draft-packages/tooltip/docs/Tooltip.stories.tsx
@@ -274,7 +274,7 @@ export const StickerSheet = props => (
   </div>
 )
 StickerSheet.parameters = {
-  chromatic: { disable: false, disableSnapshot: false, delay: 800 },
+  chromatic: { disable: false, disableSnapshot: false, delay: 1200 },
 }
 
 export const OverflowScroll = props => (
--- packages/date-picker/docs/filter-button-foundations/filter-base-buttons.stories.tsx
+++ packages/date-picker/docs/filter-button-foundations/filter-base-buttons.stories.tsx
@@ -66,6 +66,6 @@ const StickerSheetTemplate: Story = () => (
 export const StickerSheetDefault = StickerSheetTemplate.bind({})
 StickerSheetDefault.storyName = "Sticker Sheet (Default)"
 StickerSheetDefault.parameters = {
-  chromatic: { disable: false, disableSnapshot: false, delay: 1000 },
+  chromatic: { disable: false, disableSnapshot: false, delay: 1500 },
   controls: { disable: true },
 }
