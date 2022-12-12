18b7e8ab9456a8801f74c3a30d3c9eadff3c5aad
Cassandra <cassandra.tam@cultureamp.com>
2022-12-12 16:03:21 +1100

    refactor(storybook): update sticker sheet component (#3117)
    * feat(stickersheet): add StickerSheet component
    
    * docs(button): update sticker sheets
    
    * docs(split-button): update sticker sheets
    
    * docs(stickersheet): add table heading component and clean up
    
    * docs(stickersheet): update padding styles
    
    * fix(sticker-sheet): update table headings to align to start
    
    * docs(stickersheet): remove full width and allow control over column width
    
    * fix(stickersheet): move table alignment to table
    
    * fix(stickersheet): apply margin-bottom to all section headings

---
+++ storybook/components/StickerSheet/components/StickerSheetHeader/StickerSheetHeader.tsx
@@ -0,0 +1,37 @@
+import React, { HTMLAttributes } from "react"
+import { StickerSheetTableHeading } from "../StickerSheetTableHeading"
+
+export interface StickerSheetHeaderProps
+  extends HTMLAttributes<HTMLTableSectionElement> {
+  headings: string[]
+  headingsWidth?: number | string
+  isReversed?: boolean
+  hasVerticalHeadings?: boolean
+  verticalHeadingsWidth?: number | string
+}
+
+export const StickerSheetHeader: React.VFC<StickerSheetHeaderProps> = ({
+  headings,
+  headingsWidth,
+  isReversed = false,
+  hasVerticalHeadings = false,
+  verticalHeadingsWidth = "7rem",
+  ...restProps
+}) => (
+  <thead {...restProps}>
+    <tr>
+      {hasVerticalHeadings && <th style={{ width: verticalHeadingsWidth }} />}
+      {headings.map(heading => (
+        <StickerSheetTableHeading
+          key={heading}
+          isReversed={isReversed}
+          style={{ width: headingsWidth }}
+        >
+          {heading}
+        </StickerSheetTableHeading>
+      ))}
+    </tr>
+  </thead>
+)
+
+StickerSheetHeader.displayName = "StickerSheet.Header"
