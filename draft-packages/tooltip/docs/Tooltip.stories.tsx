9e79d3dce0e41b2ea86add5c34fe14c30345e5af
Cassandra Tam <cassandra.tam@cultureamp.com>
2022-12-13 12:27:23 +1100

    increase Tooltip delay only

--- draft-packages/tooltip/docs/Tooltip.stories.tsx
+++ draft-packages/tooltip/docs/Tooltip.stories.tsx
@@ -274,7 +274,7 @@ export const StickerSheet = props => (
   </div>
 )
 StickerSheet.parameters = {
-  chromatic: { disable: false, disableSnapshot: false, delay: 2000 },
+  chromatic: { disable: false, disableSnapshot: false, delay: 2200 },
 }
 
 export const OverflowScroll = props => (
