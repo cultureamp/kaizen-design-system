b0cf528e01a3ac810e784b45b0e8eb141d53a46b
Cassandra Tam <cassandra.tam@cultureamp.com>
2022-12-22 14:32:38 +1100

    docs(split-button): add return types

--- packages/split-button/src/SplitButton/components/DropdownButton/DropdownButton.spec.tsx
+++ packages/split-button/src/SplitButton/components/DropdownButton/DropdownButton.spec.tsx
@@ -1,8 +1,8 @@
 import React from "react"
-import { render } from "@testing-library/react"
+import { render, RenderResult } from "@testing-library/react"
 import { DropdownButton, DropdownButtonProps } from "./DropdownButton"
 
-const renderDropdownButton = (props?: Partial<DropdownButtonProps>) =>
+const renderDropdownButton = (props?: Partial<DropdownButtonProps>): RenderResult =>
   render(<DropdownButton {...props} />)
 
 describe("<DropdownButton />", () => {
