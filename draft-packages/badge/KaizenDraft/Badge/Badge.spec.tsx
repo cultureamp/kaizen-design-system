b5c1055c746e42b17dd49425c14d919eb9aa1d16
Cassandra Tam <cassandra.tam@cultureamp.com>
2023-01-04 13:51:37 +1100

    docs(draft-badge): add return types

--- draft-packages/badge/KaizenDraft/Badge/Badge.spec.tsx
+++ draft-packages/badge/KaizenDraft/Badge/Badge.spec.tsx
@@ -1,11 +1,10 @@
-import * as React from "react"
+import React from "react"
 import { cleanup, render, screen } from "@testing-library/react"
-
 import { Badge, BadgeProps } from "./Badge"
 
 afterEach(cleanup)
 
-const renderBadge = (props?: BadgeProps) => render(<Badge {...props} />)
+const renderBadge = (props?: BadgeProps): ReturnType<typeof render> => render(<Badge {...props} />)
 
 describe("<Badge />", () => {
   describe("default", () => {
