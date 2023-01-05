75641f6480bbf771bf5c0b1ad3f5bae8fa632da9
Natalie Loyzaga <nloyzaga@gmail.com>
2023-01-06 09:41:42 +1100

    WIP: feature modal story

--- packages/modal/docs/Modal.stories.tsx
+++ packages/modal/docs/Modal.stories.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react"
 import { ComponentStory } from "@storybook/react"
 import { Button } from "@kaizen/button"
+import { InformationReportOwnerByRule } from "@kaizen/draft-illustration"
 import { Modal } from "@kaizen/modal"
 import { Heading, Paragraph } from "@kaizen/typography"
 import { CATEGORIES } from "../../../storybook/constants"
@@ -198,5 +199,63 @@ DefaultStory.parameters = {
   docs: { source: { type: "code" } },
 }
 DefaultStory.args = {
+  size: "md",
+}
+
+export const FeatureModal: ComponentStory<typeof Modal> = props => {
+  const [modalOpen, setModalOpen] = useState<boolean>(true)
+  return (
+    <>
+      <Button label="Open modal" onClick={() => setModalOpen(true)} />
+      <Paragraph variant="body">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque at
+        tortor sit amet lorem sollicitudin placerat et quis mauris. Proin at
+        orci ullamcorper, volutpat erat eget, facilisis lacus. Pellentesque
+        habitant morbi tristique senectus et netus et malesuada fames ac turpis
+        egestas. Sed rutrum, libero malesuada ornare porttitor, metus diam
+        scelerisque mauris, sit amet mollis lorem tortor iaculis risus.
+        Vestibulum et fringilla sem. Cras placerat lectus a auctor facilisis.
+        Cras ac porta diam. Donec auctor risus non ligula scelerisque accumsan.
+        Mauris molestie tortor quis lectus euismod, non tincidunt ligula
+        egestas. Phasellus at nibh lobortis, porttitor lectus tempor, lobortis
+        augue. Quisque augue dolor, hendrerit tincidunt dapibus et, tempus eget
+        turpis. Suspendisse pharetra tincidunt lobortis.
+      </Paragraph>
+      <Modal
+        {...props}
+        isOpen={modalOpen}
+        onDismiss={() => setModalOpen(false)}
+        accessibleLabelId="my-modal-title"
+      >
+        <div style={{ backgroundColor: "#dfc9ea" }}>
+          <InformationReportOwnerByRule alt="" />
+        </div>
+        <Modal.Header>
+          <Heading variant="heading-3" tag="div" id="my-modal-title">
+            Introducing a new feature
+          </Heading>
+        </Modal.Header>
+        <Modal.Body>
+          <Paragraph variant="body">
+            Instructive text to drive user selection goes here.
+          </Paragraph>
+        </Modal.Body>
+        <Modal.Footer>
+          <Modal.Actions>
+            <Modal.ActionButton
+              primary
+              label="Done"
+              onClick={() => setModalOpen(false)}
+            />
+          </Modal.Actions>
+        </Modal.Footer>
+      </Modal>
+    </>
+  )
+}
+FeatureModal.parameters = {
+  docs: { source: { type: "code" } },
+}
+FeatureModal.args = {
   size: "md",
 }
