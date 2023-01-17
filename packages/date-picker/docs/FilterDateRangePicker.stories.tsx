e646b9d971887a9efb292d3dea5b4852efbfdfdf
Cassandra Tam <cassandra.tam@cultureamp.com>
2023-01-17 13:30:17 +1100

    fixup! docs(date-picker): fix FilterDRP snapshot layout

--- packages/date-picker/docs/FilterDateRangePicker.stories.tsx
+++ packages/date-picker/docs/FilterDateRangePicker.stories.tsx
@@ -109,7 +109,7 @@ const StickerSheetTemplate: Story = () => {
   })
 
   return (
-    <div style={{ paddingBottom: IS_CHROMATIC ? "34rem" : undefined }}>
+    <div style={{ paddingBottom: IS_CHROMATIC ? "36rem" : undefined }}>
       <StickerSheet heading="Default">
         <StickerSheet.Header
           headings={["Base", "With existing value"]}
