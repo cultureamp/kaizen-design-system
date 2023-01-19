6a2d99cbcc567fb2b850085f34cc072d7ae77fe5
Cassandra Tam <cassandra.tam@cultureamp.com>
2023-01-19 16:19:10 +1100

    feat: solution 2 with context

--- packages/date-picker/src/Filter/FilterSolution2Select.tsx
+++ packages/date-picker/src/Filter/FilterSolution2Select.tsx
@@ -3,10 +3,12 @@ import classnames from "classnames"
 import { FocusOn } from "react-focus-on"
 import { OverrideClassName } from "@kaizen/component-base"
 import { isRefObject } from "../utils/isRefObject"
+import { FilterTriggerButtonProps } from "./components"
 import { FilterPopover } from "./components/FilterPopover"
+import { FilterProvider } from "./context/useFilterContext"
 import styles from "./Filter.module.scss"
 
-export interface FilterSolution2SelectProps
+export interface FilterSolution2RefProps
   extends OverrideClassName<HTMLAttributes<HTMLDivElement>> {
   children: React.ReactNode
   // This ensures `ref` exists
@@ -14,44 +16,111 @@ export interface FilterSolution2SelectProps
   filterButton: React.FunctionComponentElement<any>
 }
 
-export const FilterSolution2Select = React.forwardRef<HTMLButtonElement, FilterSolution2SelectProps>(
-  ({ children, filterButton, classNameOverride, ...restProps }, ref) => {
+export const FilterSolution2Ref = React.forwardRef<
+  HTMLButtonElement,
+  FilterSolution2RefProps
+>(({ children, filterButton, classNameOverride, ...restProps }, ref) => {
+  // If the consumer uses the legacy `createRef`, it will not work
+  const consumerRef = isRefObject(ref) ? ref : null
+  const inbuiltButtonRef = useRef<HTMLButtonElement>(null)
+  const [isOpen, setIsOpen] = useState<boolean>(false)
+
+  return (
+    <div
+      className={classnames(styles.filter, classNameOverride)}
+      {...restProps}
+    >
+      {React.cloneElement(filterButton, {
+        ref: filterButton.ref ?? consumerRef ?? inbuiltButtonRef,
+        onClick: () => setIsOpen(!isOpen),
+        isOpen,
+        ...filterButton.props,
+      })}
+      {isOpen && (
+        <FocusOn
+          scrollLock={false}
+          onClickOutside={(): void => setIsOpen(false)}
+          onEscapeKey={(): void => setIsOpen(false)}
+        >
+          <FilterPopover
+            referenceElement={consumerRef?.current ?? inbuiltButtonRef.current}
+            // Does the popper need this or just the contents?
+            // aria-label={label}
+          >
+            {children}
+          </FilterPopover>
+        </FocusOn>
+      )}
+    </div>
+  )
+})
+FilterSolution2Ref.displayName = "FilterSolution2Select"
+
+export interface FilterSolution2ContextProps
+  extends OverrideClassName<HTMLAttributes<HTMLDivElement>> {
+  children: React.ReactNode
+  label: string
+  defaultSelectedValuesLabel?: FilterTriggerButtonProps["selectedValue"]
+  // This ensures `ref` exists
+  // The `any` can probably be scoped better
+  filterButton: React.FunctionComponentElement<any>
+}
+
+export const FilterSolution2Context = React.forwardRef<
+  HTMLButtonElement,
+  FilterSolution2ContextProps
+>(
+  (
+    {
+      children,
+      label,
+      defaultSelectedValuesLabel,
+      filterButton,
+      classNameOverride,
+      ...restProps
+    },
+    ref
+  ) => {
     // If the consumer uses the legacy `createRef`, it will not work
     const consumerRef = isRefObject(ref) ? ref : null
     const inbuiltButtonRef = useRef<HTMLButtonElement>(null)
     const [isOpen, setIsOpen] = useState<boolean>(false)
 
     return (
-      <div
-        className={classnames(
-          styles.filter,
-          classNameOverride
-        )}
-        {...restProps}
+      <FilterProvider
+        label={label}
+        defaultSelectedValuesLabel={defaultSelectedValuesLabel}
       >
-        {React.cloneElement(filterButton, {
-          ref: filterButton.ref ?? consumerRef ?? inbuiltButtonRef,
-          onClick: () => setIsOpen(!isOpen),
-          isOpen,
-          ...filterButton.props,
-        })}
-        {isOpen && (
-          <FocusOn
-            scrollLock={false}
-            onClickOutside={(): void => setIsOpen(false)}
-            onEscapeKey={(): void => setIsOpen(false)}
-          >
-            <FilterPopover
-              referenceElement={consumerRef?.current ?? inbuiltButtonRef.current}
-              // Does the popper need this or just the contents?
-              // aria-label={label}
+        <div
+          className={classnames(styles.filter, classNameOverride)}
+          {...restProps}
+        >
+          {React.cloneElement(filterButton, {
+            ref: filterButton.ref ?? consumerRef ?? inbuiltButtonRef,
+            onClick: () => setIsOpen(!isOpen),
+            isOpen,
+            ...filterButton.props,
+          })}
+          {isOpen && (
+            <FocusOn
+              scrollLock={false}
+              onClickOutside={(): void => setIsOpen(false)}
+              onEscapeKey={(): void => setIsOpen(false)}
             >
-              {children}
-            </FilterPopover>
-          </FocusOn>
-        )}
-      </div>
+              <FilterPopover
+                referenceElement={
+                  consumerRef?.current ?? inbuiltButtonRef.current
+                }
+                // Does the popper need this or just the contents?
+                // aria-label={label}
+              >
+                {children}
+              </FilterPopover>
+            </FocusOn>
+          )}
+        </div>
+      </FilterProvider>
     )
   }
 )
-FilterSolution2Select.displayName = "FilterSolution2Select"
+FilterSolution2Context.displayName = "FilterSolution2Context"
--- packages/date-picker/src/Filter/components/FilterTriggerButton/FilterTriggerButton.tsx
+++ packages/date-picker/src/Filter/components/FilterTriggerButton/FilterTriggerButton.tsx
@@ -5,6 +5,7 @@ import chevronDown from "@kaizen/component-library/icons/chevron-down.icon.svg"
 import chevronUp from "@kaizen/component-library/icons/chevron-up.icon.svg"
 import { isRefObject } from "../../../utils/isRefObject"
 import { FilterRef } from "../../FilterSolution2"
+import { useFilterContext } from "../../context/useFilterContext"
 import { FilterBaseButton, FilterBaseButtonProps } from "../FilterBaseButton"
 import styles from "./FilterTriggerButton.module.scss"
 
@@ -101,3 +102,48 @@ export const FilterTriggerButtonWithFilterRef = forwardRef<
 
 FilterTriggerButtonWithFilterRef.displayName =
   "FilterTriggerButtonWithFilterRef"
+
+export type FilterTriggerButtonContextProps = Omit<
+  FilterTriggerButtonProps,
+  "label"
+>
+
+export const FilterTriggerButtonContext = forwardRef<
+  HTMLButtonElement,
+  FilterTriggerButtonContextProps
+>(({ selectedValue, isOpen = false, classNameOverride, ...restProps }, ref) => {
+  const { label, selectedValuesLabel } = useFilterContext()
+  // Maybe we shouldn't check; would we ever receive an empty string?
+  // Maybe we would want to show it to show something went wrong?
+  // const hasSelectedValue = selectedValue && selectedValue !== ""
+
+  return (
+    <FilterBaseButton
+      ref={ref}
+      classNameOverride={classnames(
+        styles.filterTriggerButton,
+        classNameOverride
+      )}
+      aria-haspopup="true"
+      aria-expanded={isOpen}
+      {...restProps}
+    >
+      <span className={styles.labelContainer}>
+        {selectedValuesLabel ? (
+          <>
+            <span className={styles.hasSelectedValues}>
+              <span>{label}</span>
+              <span className={styles.labelSeparator}>:</span>
+            </span>
+            <span>{selectedValuesLabel}</span>
+          </>
+        ) : (
+          label
+        )}
+      </span>
+      <Icon icon={isOpen ? chevronUp : chevronDown} role="presentation" />
+    </FilterBaseButton>
+  )
+})
+
+FilterTriggerButtonContext.displayName = "FilterTriggerButtonContext"
