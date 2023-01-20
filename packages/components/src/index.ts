11c6939293cd9d2221643e809ed74edb130f4f12
Geoffrey Chong <gyf.chong@gmail.com>
2023-01-20 16:52:37 +1100

    re-export example

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -5,3 +5,30 @@ export * from "./KaizenProvider"
 
 // Example of re-export of existing kaizen components
 export * from "@kaizen/draft-select"
+export * from "@kaizen/draft-avatar"
+export * from "@kaizen/draft-badge"
+export * from "@kaizen/draft-card"
+export * from "@kaizen/draft-collapsible"
+export * from "@kaizen/draft-divider"
+export * from "@kaizen/draft-empty-state"
+export * from "@kaizen/draft-filter-menu-button"
+export * from "@kaizen/draft-form"
+export * from "@kaizen/draft-guidance-block"
+export * from "@kaizen/draft-hero-card"
+export * from "@kaizen/draft-illustration"
+export * from "@kaizen/draft-likert-scale-legacy"
+export * from "@kaizen/draft-menu"
+// Module "@kaizen/draft-form" has already exported a member named 'Radio'
+// export * from "@kaizen/draft-radio"
+export * from "@kaizen/draft-select"
+// Module "@kaizen/draft-form" has already exported a member named 'Slider'
+// export * from "@kaizen/draft-slider"
+export * from "@kaizen/draft-table"
+export * from "@kaizen/draft-tile"
+export * from "@kaizen/draft-title-block-zen"
+export * from "@kaizen/draft-tooltip"
+export * from "@kaizen/draft-vertical-progress-step"
+export * from "@kaizen/draft-well"
+
+// Module "@kaizen/draft-menu" has already exported a member named 'Menu'
+// export * from "@kaizen/draft-zen-navigation-bar"
