beb630364e2a83f41f9407831778a239f32bb7ec
Geoffrey Chong <gyf.chong@gmail.com>
2023-01-24 16:28:59 +1100

    missing cleanup

--- packages/components/src/__future__/ExampleComponent/ExampleComponent.spec.tsx
+++ packages/components/src/__future__/ExampleComponent/ExampleComponent.spec.tsx
@@ -1,7 +1,6 @@
 import React from "react"
 import { render } from "@testing-library/react"
 import { ExampleComponent } from "./ExampleComponent"
-import "@testing-library/jest-dom"
 
 describe("<ExampleComponent />", () => {
   it("renders", () => {
