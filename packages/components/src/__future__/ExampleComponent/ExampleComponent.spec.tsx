26ae45423979a3b1f637e00a7c33282cb82928fd
Geoffrey Chong <gyf.chong@gmail.com>
2023-01-24 15:24:18 +1100

    fixed react import

--- packages/components/src/__future__/ExampleComponent/ExampleComponent.spec.tsx
+++ packages/components/src/__future__/ExampleComponent/ExampleComponent.spec.tsx
@@ -1,4 +1,4 @@
-import * as React from "react"
+import React from "react"
 import { render } from "@testing-library/react"
 import { ExampleComponent } from "./ExampleComponent"
 import "@testing-library/jest-dom"
