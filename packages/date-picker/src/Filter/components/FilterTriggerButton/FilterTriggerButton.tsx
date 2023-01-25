edbef8284388dca572a7e369e6fd2224c8c7a5cc
Cassandra Tam <cassandra.tam@cultureamp.com>
2023-01-25 12:02:59 +1100

    solution 2 with flexi ref

--- packages/date-picker/src/Filter/components/FilterTriggerButton/FilterTriggerButton.tsx
+++ packages/date-picker/src/Filter/components/FilterTriggerButton/FilterTriggerButton.tsx
@@ -149,6 +149,55 @@ export const FilterTriggerButtonContext = forwardRef<
 
 FilterTriggerButtonContext.displayName = "FilterTriggerButtonContext"
 
+
+export type FilterTriggerButtonContextWithFilterRefProps = Omit<
+  FilterTriggerButtonProps,
+  "label"
+>
+
+export const FilterTriggerButtonContextWithFilterRef = forwardRef<
+  FilterRef,
+  FilterTriggerButtonContextWithFilterRefProps
+>(({ selectedValue, isOpen = false, classNameOverride, ...restProps }, ref) => {
+  const customRefObject = isRefObject(ref) ? ref.current : null
+  const triggerButtonRef = customRefObject?.triggerButtonRef
+
+  const { label, selectedValuesLabel } = useFilterContext()
+  // Maybe we shouldn't check; would we ever receive an empty string?
+  // Maybe we would want to show it to show something went wrong?
+  // const hasSelectedValue = selectedValue && selectedValue !== ""
+
+  return (
+    <FilterBaseButton
+      ref={triggerButtonRef}
+      classNameOverride={classnames(
+        styles.filterTriggerButton,
+        classNameOverride
+      )}
+      aria-haspopup="true"
+      aria-expanded={isOpen}
+      {...restProps}
+    >
+      <span className={styles.labelContainer}>
+        {selectedValuesLabel ? (
+          <>
+            <span className={styles.hasSelectedValues}>
+              <span>{label}</span>
+              <span className={styles.labelSeparator}>:</span>
+            </span>
+            <span>{selectedValuesLabel}</span>
+          </>
+        ) : (
+          label
+        )}
+      </span>
+      <Icon icon={isOpen ? chevronUp : chevronDown} role="presentation" />
+    </FilterBaseButton>
+  )
+})
+
+FilterTriggerButtonContextWithFilterRef.displayName = "FilterTriggerButtonContextWithFilterRef"
+
 export type FilterTriggerButtonSol3Props = Omit<
   FilterTriggerButtonProps,
   "label" | "isOpen"
