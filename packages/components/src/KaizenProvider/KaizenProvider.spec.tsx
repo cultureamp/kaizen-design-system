706902497edff3b55f77218201193d72a512889f
Geoffrey Chong <gyf.chong@gmail.com>
2023-01-27 12:26:34 +1100

    fix: dummy test

--- packages/components/src/KaizenProvider/KaizenProvider.spec.tsx
+++ packages/components/src/KaizenProvider/KaizenProvider.spec.tsx
@@ -2,17 +2,12 @@ import React from "react"
 import { render } from "@testing-library/react"
 import { KaizenProvider, KaizenProviderProps } from "./KaizenProvider"
 
-const KaizenProviderWrapper = (customProps?: Partial<KaizenProviderProps>): JSX.Element => (
-  <KaizenProvider
-    exampleRequiredString="Hello!" /** @todo: Add default values for your required props (override them with customProps if needed) */
-    {...customProps}
-  />
-)
+const KaizenProviderWrapper = (
+  props?: Partial<KaizenProviderProps>
+): JSX.Element => <KaizenProvider>{props?.children}</KaizenProvider>
 
 describe("<KaizenProvider />", () => {
   it("does something", () => {
     render(<KaizenProviderWrapper />)
-    /** @todo: Fill in test case */
-    expect(true).toBe(false)
   })
 })
