d8afd4df169d4cc995c4413379c42b46d2f240a9
jakepitman <jakeseanp@gmail.com>
2023-01-30 09:47:38 +1100

    docs(tailwind): fix media queries & pseudo states dir structure

--- docs/Tailwind/ClassnameReferences/TailwindTokens.stories.tsx
+++ docs/Tailwind/ClassnameReferences/TailwindTokens.stories.tsx
@@ -53,34 +53,3 @@ export const DefaultKaizenSiteDemo: Story = args => (
     </Card>
   </div>
 )
-
-DefaultKaizenSiteDemo.storyName = "Default (Kaizen Site Demo)"
-DefaultKaizenSiteDemo.args = { title: "Tailwind title example" }
-
-export const TailwindPseudoStates: Story = () => (
-  <div className="p-12">
-    <h1 className="flex flex-col items-center font-family-heading text-heading-1 font-weight-heading text-purple-700">
-      Tailwind Pseudo states
-    </h1>
-    <div className="flex">
-      <div className="inline-flex flex-col">
-        <h2 className="font-family-heading-2 font-weight-heading-2 mb-12 text-heading-2 text-purple-800">
-          Hover
-        </h2>
-        <button className=" rounded border-width-default rounded-default border-none bg-purple-600 p-12 font-family-paragraph text-paragraph font-weight-paragraph-bold text-white  hover:bg-purple-800">
-          Learn more
-        </button>
-      </div>
-      <div className="ml-12 inline-flex flex-col">
-        <h2 className="font-family-heading-2 font-weight-heading-2 mb-12 text-heading-2 text-purple-800">
-          Focus
-        </h2>
-        <button className="rounded rounded-default border-none bg-purple-600 p-12 font-family-paragraph text-paragraph font-weight-paragraph-bold text-white focus:bg-purple-800 focus:ring-2 focus:ring-offset-1">
-          Learn more
-        </button>
-      </div>
-    </div>
-  </div>
-)
-
-TailwindPseudoStates.storyName = "Tailwind pseudo selectors"
