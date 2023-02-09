a9cbf39314ae463b1720c85629f8fbc53c3915cf
Geoffrey Chong <gyf.chong@gmail.com>
2023-02-10 09:38:38 +1100

    fix: lint

--- packages/components/src/KaizenProvider/KaizenProvider.spec.tsx
+++ packages/components/src/KaizenProvider/KaizenProvider.spec.tsx
@@ -2,14 +2,16 @@ import React from "react"
 import { render } from "@testing-library/react"
 import { KaizenProvider, KaizenProviderProps } from "./KaizenProvider"
 
-const KaizenProviderWrapper = ({ children, ...restProps }: KaizenProviderProps): JSX.Element => (
+const KaizenProviderWrapper = ({
+  children,
+  ...restProps
+}: KaizenProviderProps): JSX.Element => (
   <KaizenProvider {...restProps}>{children}</KaizenProvider>
 )
 
 describe("<KaizenProvider />", () => {
   it("renders it's children", () => {
     const ReactApp = (): JSX.Element => <div>Hello App</div>
-
     render(
       <KaizenProviderWrapper>
         <ReactApp />
