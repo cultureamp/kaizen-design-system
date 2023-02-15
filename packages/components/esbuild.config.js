4f2ace55edb1d3b5fc0005b65165fd584357a0d2
Geoffrey Chong <gyfchong@users.noreply.github.com>
2023-02-15 12:01:30 +1100

    feat: 44-processing-tailwind-css (#3273)
    * wip: add tailwind processing to @kaizen/components
    
    * feat: add esbuild
    
    * change tailwind to tailwindcss
    
    * yarnlock
    
    * fix: css processing
    
    * feat: enable minify tailwind

---
+++ packages/components/esbuild.config.js
@@ -0,0 +1,20 @@
+// @ts-nocheck
+/* eslint-disable import/no-extraneous-dependencies */
+const postCssPlugin = require("@deanc/esbuild-plugin-postcss")
+const esbuild = require("esbuild")
+
+esbuild
+  .build({
+    entryPoints: ["./src/styles.css"],
+    outfile: "./dist/styles.css",
+    minify: true,
+    bundle: true,
+    plugins: [
+      postCssPlugin({
+        plugins: [require("tailwindcss"), require("autoprefixer")],
+      }),
+    ],
+  })
+  .catch(e => {
+    process.exit()
+  })
