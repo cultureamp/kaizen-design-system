96cc40c0fe5681094c45a68a39ffffa1d2d34c07
Geoffrey Chong <gyf.chong@gmail.com>
2023-02-16 11:08:33 +1100

    fix(components): esbuild bundling

--- packages/components/esbuild.config.js
+++ packages/components/esbuild.config.js
@@ -1,18 +1,74 @@
 // @ts-nocheck
 /* eslint-disable import/no-extraneous-dependencies */
-const postCssPlugin = require("@deanc/esbuild-plugin-postcss")
-const esbuild = require("esbuild")
+import { existsSync, mkdirSync, readdirSync, statSync, writeFileSync } from "fs"
+import { join } from "path"
+import postCssPlugin from "@deanc/esbuild-plugin-postcss"
+import autoprefixer from "autoprefixer"
+import esbuild from "esbuild"
+import tailwindcss from "tailwindcss"
+import glob from "tiny-glob"
 
+const dist = join(process.cwd(), "dist")
+
+if (!existsSync(dist)) {
+  mkdirSync(dist)
+}
+
+// const entryPoints = readdirSync(join(process.cwd(), "src"))
+//   .filter(
+//     file =>
+//       file.endsWith(".ts") &&
+//       statSync(join(process.cwd(), "src", file)).isFile()
+//   )
+//   .map(file => `src/${file}`)
+
+// esm output bundles with code splitting
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+;(async () => {
+  const entryPoints = await glob("./src/**/*.ts")
+  await esbuild
+    .build({
+      entryPoints,
+      outdir: "dist/esm",
+      bundle: true,
+      sourcemap: true,
+      minify: true,
+      splitting: true,
+      format: "esm",
+      define: { global: "window" },
+      target: ["esnext"],
+      loader: { ".scss": "copy" },
+    })
+    .catch(() => process.exit(1))
+})()
+
+// cjs output bundle
 esbuild
   .build({
-    entryPoints: ["./src/index.ts", "./src/future.ts"],
-    outdir: "./dist",
+    entryPoints: ["src/index.ts", "src/future.ts"],
+    outdir: "dist/cjs",
     bundle: true,
-    loader: { ".module.scss": "copy" },
+    sourcemap: true,
+    minify: true,
+    platform: "node",
+    target: ["node16"],
+    loader: { ".scss": "copy" },
   })
-  .catch(e => {
-    process.exit()
-  })
+  .catch(() => process.exit(1))
+
+// an entry file for cjs at the root of the bundle
+writeFileSync(join(dist, "index.js"), "export * from './esm/index.js';")
+writeFileSync(join(dist, "future.js"), "export * from './esm/future.js';")
+
+// an entry file for esm at the root of the bundle
+writeFileSync(
+  join(dist, "index.cjs.js"),
+  "module.exports = require('./cjs/index.cjs.js');"
+)
+writeFileSync(
+  join(dist, "future.cjs.js"),
+  "module.exports = require('./cjs/future.cjs.js');"
+)
 
 esbuild
   .build({
@@ -22,7 +78,7 @@ esbuild
     bundle: true,
     plugins: [
       postCssPlugin({
-        plugins: [require("tailwindcss"), require("autoprefixer")],
+        plugins: [tailwindcss, autoprefixer],
       }),
     ],
   })
