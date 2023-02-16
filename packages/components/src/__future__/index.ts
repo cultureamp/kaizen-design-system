98bdc4e93497fca425dfb354e3e7d6e86b0ee40d
Geoffrey Chong <gyf.chong@gmail.com>
2023-02-16 12:45:27 +1100

    feat: add exports to components in packages/

--- packages/components/src/__future__/index.ts
+++ packages/components/src/__future__/index.ts
@@ -1,1 +1,4 @@
 export * from "./ExampleComponent"
+
+// Re-exports which need to be moved into this package
+export * from "@kaizen/select"
--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -1,2 +1,30 @@
-// Example of exporting components to be consumed from main @kaizen/components
+// Components inside this package
 export * from "./KaizenProvider"
+
+// Re-exports which need to be moved into this package
+export * from "@kaizen/a11y"
+export * from "@kaizen/brand"
+export * from "@kaizen/brand-moment"
+export * from "@kaizen/button"
+export * from "@kaizen/component-base"
+export * from "@kaizen/date-picker"
+
+export {
+  makeCssVariableDefinitionsMap,
+  makeCSSVariableTheme,
+  mapLeafsOfObject,
+} from "@kaizen/design-tokens"
+export * from "@kaizen/design-tokens/src/themes"
+export * from "@kaizen/design-tokens/src/types"
+
+export * from "@kaizen/hosted-assets"
+export * from "@kaizen/loading-skeleton"
+export * from "@kaizen/loading-spinner"
+export * from "@kaizen/notification"
+export * from "@kaizen/pagination"
+export * from "@kaizen/progress-bar"
+export * from "@kaizen/responsive"
+export * from "@kaizen/rich-text-editor"
+
+export * from "@kaizen/split-button"
+export * from "@kaizen/tabs"
