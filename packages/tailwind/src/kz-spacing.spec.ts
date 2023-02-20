c48646b845e28665b07e22508e1703489ac29d1c
jakepitman <jakeseanp@gmail.com>
2023-02-20 10:25:06 +0900

    test(tailwind): make spec more readable

--- packages/tailwind/src/kz-spacing.spec.ts
+++ packages/tailwind/src/kz-spacing.spec.ts
@@ -1,24 +1,19 @@
 import { filterOutNonPixelTokens } from "./kz-spacing"
 
-const numericKeyPairs = {
-  0: "0",
-  1: ".0625rem",
-  2: ".125rem",
-}
-const nonNumericKeyPairs = {
-  xs: "0.375rem",
-  sm: "0.75rem",
-}
-
-const dummyData = {
-  ...numericKeyPairs,
-  ...nonNumericKeyPairs,
-}
-
 describe("filterOutNonPixelTokens", () => {
-  const result = filterOutNonPixelTokens(dummyData)
-
   it("Keeps all numeric keys", () => {
-    expect(result).toEqual(numericKeyPairs)
+    const result = filterOutNonPixelTokens({
+      0: "0",
+      xs: "0.375rem",
+      1: ".0625rem",
+      sm: "0.75rem",
+      2: ".125rem",
+    })
+
+    expect(result).toEqual({
+      0: "0",
+      1: ".0625rem",
+      2: ".125rem",
+    })
   })
 })
