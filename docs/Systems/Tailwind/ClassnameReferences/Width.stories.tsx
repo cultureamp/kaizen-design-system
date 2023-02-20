4e207558392ef13e99bcb787d942e093a12da789
jakepitman <jakeseanp@gmail.com>
2023-02-20 11:15:35 +0900

    docs(tailwind): add width docs

---
+++ docs/Systems/Tailwind/ClassnameReferences/Width.stories.tsx
@@ -0,0 +1,59 @@
+import React from "react"
+import { Story } from "@storybook/react"
+import { Divider } from "@kaizen/draft-divider"
+import { kaizenTailwindTheme } from "@kaizen/tailwind"
+import { StoryWrapper } from "../../../../storybook/components/StoryWrapper"
+import { CATEGORIES } from "../../../../storybook/constants"
+
+const prefix = "w-"
+const classKeyVal: string[][] = Object.entries(kaizenTailwindTheme?.width || [])
+
+export default {
+  title: `${CATEGORIES.tailwind}/Classname References/Spacing & layouts/Width`,
+  parameters: {
+    docs: {
+      description: {
+        component: `Use class "${prefix}\\$\\{modifier}", ie: className="${prefix}${classKeyVal[0][0]}"`,
+      },
+    },
+  },
+}
+
+const StickerSheetTemplate: Story<{ isReversed: boolean }> = ({
+  isReversed,
+}) => (
+  <>
+    <StoryWrapper isReversed={isReversed}>
+      <StoryWrapper.RowHeader headings={["Class", "Properties", "Example"]} />
+      {classKeyVal.map((presetData, index) => {
+        const [widthName, widthValue] = presetData
+
+        return (
+          <React.Fragment key={index}>
+            <Divider variant="canvas" />
+            <StoryWrapper.Row rowTitle="">
+              <p>w-{widthName}</p>
+              <p>{widthValue}</p>
+              <div className="w-100 border-solid rounded-default">
+                <div
+                  className="h-100 flex items-center bg-blue-400"
+                  style={{ width: widthValue }}
+                >
+                  {widthValue.includes("content") ? (
+                    <div className="font-family-paragraph bg-blue-100 p-4 border-dashed border-w-[1px]">
+                      Inner content
+                    </div>
+                  ) : null}
+                </div>
+              </div>
+            </StoryWrapper.Row>
+          </React.Fragment>
+        )
+      })}
+    </StoryWrapper>
+  </>
+)
+
+export const StickerSheetDefault = StickerSheetTemplate.bind({})
+StickerSheetDefault.storyName = "Width"
+StickerSheetDefault.parameters = { chromatic: { disable: false } }
