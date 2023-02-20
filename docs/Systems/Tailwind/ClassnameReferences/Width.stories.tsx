3a733f7a115356b214896da06aa99dd1e0e0c273
jakepitman <jakeseanp@gmail.com>
2023-02-21 07:49:44 +0900

    refactor(tailwind): refactor width story with UtilityClassTemplate

--- docs/Systems/Tailwind/ClassnameReferences/Width.stories.tsx
+++ docs/Systems/Tailwind/ClassnameReferences/Width.stories.tsx
@@ -1,18 +1,25 @@
 import React from "react"
 import { Story } from "@storybook/react"
 import { kaizenTailwindTheme } from "@kaizen/tailwind"
-import { StoryWrapper } from "../../../../storybook/components/StoryWrapper"
 import { CATEGORIES } from "../../../../storybook/constants"
+import { UtilityClassTemplate } from "../components/UtilityClassTemplate"
+import { utilityDescription } from "../helpers/utilityDescription"
 
 const prefix = "w-"
-const classKeyVal: string[][] = Object.entries(kaizenTailwindTheme?.width || [])
+const classEntries: Array<{ utilityClassName: string; cssProperty: string }> =
+  Object.entries(kaizenTailwindTheme?.width || []).map(
+    ([suffix, cssProperty]) => ({
+      utilityClassName: `${prefix}${suffix}`,
+      cssProperty,
+    })
+  )
 
 export default {
   title: `${CATEGORIES.tailwind}/Classname References/Spacing & layouts/Width`,
   parameters: {
     docs: {
       description: {
-        component: `Use class "${prefix}\\$\\{modifier}", ie: className="${prefix}${classKeyVal[0][0]}"`,
+        component: utilityDescription(prefix, classEntries[0].utilityClassName),
       },
     },
   },
@@ -21,35 +28,24 @@ export default {
 const StickerSheetTemplate: Story<{ isReversed: boolean }> = ({
   isReversed,
 }) => (
-  <>
-    <StoryWrapper hasRowDivider isReversed={isReversed}>
-      <StoryWrapper.RowHeader headings={["Class", "Properties", "Example"]} />
-      {classKeyVal.map((presetData, index) => {
-        const [widthName, widthValue] = presetData
-
-        return (
-          <React.Fragment key={index}>
-            <StoryWrapper.Row rowTitle="">
-              <p>w-{widthName}</p>
-              <p>{widthValue}</p>
-              <div className="w-100 border-solid rounded-default">
-                <div
-                  className="h-100 flex items-center bg-blue-400"
-                  style={{ width: widthValue }}
-                >
-                  {widthValue.includes("content") ? (
-                    <div className="font-family-paragraph bg-blue-100 p-4 border-dashed border-w-[1px]">
-                      Inner content
-                    </div>
-                  ) : null}
-                </div>
-              </div>
-            </StoryWrapper.Row>
-          </React.Fragment>
-        )
-      })}
-    </StoryWrapper>
-  </>
+  <UtilityClassTemplate
+    classKeyValues={classEntries}
+    renderExampleComponent={(cssProperty): React.ReactElement => (
+      <div className="w-100 border-solid rounded-default h-100">
+        <div
+          className="h-100 flex items-center bg-blue-400"
+          style={{ width: cssProperty }}
+        >
+          {cssProperty.includes("content") ? (
+            <div className="font-family-paragraph bg-blue-100 p-4 border-dashed border-w-[1px]">
+              Inner content
+            </div>
+          ) : null}
+        </div>
+      </div>
+    )}
+    isReversed={isReversed}
+  />
 )
 
 export const StickerSheetDefault = StickerSheetTemplate.bind({})
