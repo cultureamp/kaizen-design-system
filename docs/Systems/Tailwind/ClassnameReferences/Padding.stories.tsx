4c3a4e268a0f1b12c546b60499c1294067e8ac10
jakepitman <jakeseanp@gmail.com>
2023-02-21 10:09:06 +0900

    docs(tailwind): add Padding classNameReference story

---
+++ docs/Systems/Tailwind/ClassnameReferences/Padding.stories.tsx
@@ -0,0 +1,63 @@
+import React from "react"
+import { Story } from "@storybook/react"
+import { Card } from "@kaizen/draft-card"
+import { kaizenTailwindTheme } from "@kaizen/tailwind"
+import { CATEGORIES } from "../../../../storybook/constants"
+import { UtilityClassTemplate } from "../components/UtilityClassTemplate"
+import { utilityDescription } from "../helpers/utilityDescription"
+
+const prefix = "p-"
+const classEntries: Array<{ utilityClassName: string; cssProperty: string }> =
+  Object.entries(kaizenTailwindTheme?.spacing || []).map(
+    ([suffix, cssProperty]) => ({
+      utilityClassName: `${prefix}${suffix}`,
+      cssProperty,
+    })
+  )
+
+export default {
+  title: `${CATEGORIES.tailwind}/Classname References/Spacing & layouts/Padding`,
+  parameters: {
+    docs: {
+      description: {
+        component: utilityDescription(prefix, classEntries[0].utilityClassName),
+      },
+    },
+  },
+}
+
+const StickerSheetTemplate: Story<{ isReversed: boolean }> = ({
+  isReversed,
+}) => (
+  <div className="flex flex-col items-center">
+    <Card variant="informative" classNameOverride="mb-24">
+      <div className="font-family-paragraph max-w-[1000px] p-24">
+        <p>
+          The padding prefix 'p-' has been used in the examples in this
+          document, which compiles to the `padding` property in CSS.
+        </p>
+        <p>
+          Note that there are other prefixes (such as `pl-` for `padding-left`)
+          that can be used instead. Available padding prefixes can be referenced{" "}
+          <a href="https://tailwindcss.com/docs/padding#basic-usage">here</a>
+        </p>
+      </div>
+    </Card>
+    <UtilityClassTemplate
+      classKeyValues={classEntries}
+      renderExampleComponent={(cssProperty): React.ReactElement => (
+        <p
+          className="border-solid w-min rounded-default bg-blue-100"
+          style={{ padding: cssProperty }}
+        >
+          Padding
+        </p>
+      )}
+      isReversed={isReversed}
+    />
+  </div>
+)
+
+export const StickerSheetDefault = StickerSheetTemplate.bind({})
+StickerSheetDefault.storyName = "Padding"
+StickerSheetDefault.parameters = { chromatic: { disable: false } }
