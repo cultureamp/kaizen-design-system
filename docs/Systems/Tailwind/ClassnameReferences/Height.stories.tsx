6844329adc06d74c298c29f3ecc11b3409fb3c42
jakepitman <jakeseanp@gmail.com>
2023-02-21 12:56:57 +0900

    docs(tailwind): add classNameReferences story for Height

---
+++ docs/Systems/Tailwind/ClassnameReferences/Height.stories.tsx
@@ -0,0 +1,54 @@
+import React from "react"
+import { Story } from "@storybook/react"
+import { kaizenTailwindTheme } from "@kaizen/tailwind"
+import { CATEGORIES } from "../../../../storybook/constants"
+import { UtilityClassTemplate } from "../components/UtilityClassTemplate"
+import { utilityDescription } from "../helpers/utilityDescription"
+
+const prefix = "h-"
+const classEntries: Array<{ utilityClassName: string; cssProperty: string }> =
+  Object.entries(kaizenTailwindTheme?.height || []).map(
+    ([suffix, cssProperty]) => ({
+      utilityClassName: `${prefix}${suffix}`,
+      cssProperty,
+    })
+  )
+
+export default {
+  title: `${CATEGORIES.tailwind}/Classname References/Spacing & layouts/Height`,
+  parameters: {
+    docs: {
+      description: {
+        component: utilityDescription(prefix, classEntries[0].utilityClassName),
+      },
+    },
+  },
+}
+
+const StickerSheetTemplate: Story<{ isReversed: boolean }> = ({
+  isReversed,
+}) => (
+  <UtilityClassTemplate
+    compiledCssPropertyName="height"
+    classKeyValues={classEntries}
+    renderExampleComponent={(cssProperty): React.ReactElement => (
+      <div className="w-min flex flex-col-reverse border-solid rounded-default h-[200px]">
+        <div
+          className="w-[100px] h-100 flex justify-center items-center bg-blue-400"
+          style={{ height: cssProperty }}
+        >
+          {cssProperty.includes("content") ? (
+            <div className="w-min font-family-paragraph bg-blue-100 p-4 border-dashed border-w-[1px]">
+              Inner content
+            </div>
+          ) : null}
+        </div>
+      </div>
+    )}
+    isReversed={isReversed}
+  />
+)
+
+export const StickerSheetDefault = StickerSheetTemplate.bind({})
+StickerSheetDefault.storyName = "Height"
+StickerSheetDefault.parameters = { chromatic: { disable: false } }
