3451a1745634ff644b6e36027816edcc396d3afa
jakepitman <jakeseanp@gmail.com>
2023-02-21 14:09:42 +0900

    docs(tailwind): reformat utility class description
    Using ${} syntax falsely suggests that utilities can be dynamically constructed.

--- docs/Systems/Tailwind/helpers/utilityDescription.ts
+++ docs/Systems/Tailwind/helpers/utilityDescription.ts
@@ -2,4 +2,4 @@ export const utilityDescription = (
   prefix: string,
   utilityClassName: string
 ): string =>
-  `Use class "${prefix}\\$\\{suffix}", ie: className="${utilityClassName}"`
+  `Use class "${prefix}\\<SUFFIX>", ie: className="${utilityClassName}"`
