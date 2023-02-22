314a7355849819e6a5cb9cba4eefd5ac465b1b79
jakepitman <jakeseanp@gmail.com>
2023-02-22 21:45:45 +0900

    docs(tailwind): fix width in docs tab

--- docs/Systems/Tailwind/components/UtilityClassTemplate.tsx
+++ docs/Systems/Tailwind/components/UtilityClassTemplate.tsx
@@ -22,50 +22,52 @@ export const UtilityClassTemplate = ({
   const [copiedText, setCopiedText] = useState<null | string>(null)
 
   return (
-    <StickerSheet
-      className="border-none w-100"
-      isReversed={isReversed}
-      border={1}
-      frame={false}
-      rules="rows"
-    >
-      <StickerSheet.Header
-        className="border-transparent border-b-gray-400 border-solid"
-        headings={["Utility Class", "Compiled CSS", "Example"]}
-      />
-      {classKeyValues.map((presetData, index) => {
-        const { utilityClassName, cssProperty } = presetData
+    <div className="w-100">
+      <StickerSheet
+        className="border-none w-100"
+        isReversed={isReversed}
+        border={1}
+        frame={false}
+        rules="rows"
+      >
+        <StickerSheet.Header
+          className="border-transparent border-b-gray-400 border-solid"
+          headings={["Utility Class", "Compiled CSS", "Example"]}
+        />
+        {classKeyValues.map((presetData, index) => {
+          const { utilityClassName, cssProperty } = presetData
 
-        return (
-          <StickerSheet.Row rowTitle="" className="border-gray-400">
-            <div className="mr-32 min-w-max max-w-[300px]">
-              <CodeSnippet
-                text={utilityClassName}
-                onCopy={(text: string): void => setCopiedText(text)}
-              />
-            </div>
-            <p className="font-family-paragraph mr-32 w-max">
-              {compiledCssPropertyName}: {cssProperty}
-            </p>
-            <div className="font-family-paragraph">
-              {renderExampleComponent(cssProperty)}
-            </div>
-          </StickerSheet.Row>
-        )
-      })}
-      <div className="absolute top-0 right-0">
-        {copiedText && (
-          <InlineNotification
-            type="positive"
-            hideCloseIcon
-            autohide
-            onHide={(): void => setCopiedText(null)}
-            title="Copied to clipboard: "
-          >
-            {copiedText}
-          </InlineNotification>
-        )}
-      </div>
-    </StickerSheet>
+          return (
+            <StickerSheet.Row rowTitle="" className="border-gray-400">
+              <div className="mr-32 min-w-max max-w-[300px]">
+                <CodeSnippet
+                  text={utilityClassName}
+                  onCopy={(text: string): void => setCopiedText(text)}
+                />
+              </div>
+              <p className="font-family-paragraph mr-32 w-max">
+                {compiledCssPropertyName}: {cssProperty}
+              </p>
+              <div className="font-family-paragraph">
+                {renderExampleComponent(cssProperty)}
+              </div>
+            </StickerSheet.Row>
+          )
+        })}
+        <div className="absolute top-0 left-0">
+          {copiedText && (
+            <InlineNotification
+              type="positive"
+              hideCloseIcon
+              autohide
+              onHide={(): void => setCopiedText(null)}
+              title="Copied to clipboard: "
+            >
+              {copiedText}
+            </InlineNotification>
+          )}
+        </div>
+      </StickerSheet>
+    </div>
   )
 }
