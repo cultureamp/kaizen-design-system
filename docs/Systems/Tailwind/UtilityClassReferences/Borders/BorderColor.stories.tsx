77deb5be2a38ec280a30fd0ee196dd9325ea7586
JakePitman <jakeseanp@gmail.com>
2023-02-23 13:18:41 +0900

    docs: Add missing classNameReferences docs for Tailwind (#3302)
    * docs(tailwind): add width docs
    
    * docs(tailwind): replace Divider imports with StoryWrapper.hasRowDivider
    
    * docs(tailwind): make media-queries & width stories visually consistent
    
    * docs(tailwind): extract common rendering pattern into template
    
    * refactor(tailwind): refactor width story with UtilityClassTemplate
    
    * docs(tailwind): add Padding classNameReference story
    
    * docs(tailwind): add margin docs
    
    * docs(tailwind): display full css properties instead of just values
    
    * docs(tailwind): add classNameReferences story for Height
    
    * Create Modifiers dir & change titles to uppercase
    
    * docs(tailwind): rename 'Spacing & Layouts' to 'Spacing'
    
    * docs(tailwind): reformat utility class description
    
    Using ${} syntax falsely suggests that utilities can be dynamically constructed.
    
    * docs(tailwind): add border radius docs
    
    * docs(tailwind): make borders in stories wider by default
    
    * docs(tailwind): add background color docs
    
    * refactor(tailwind docs): rename ClassNameReferences to UtilityClassReferences, add subfolders
    
    * docs(tailwind): make utility classes copy-able
    
    * docs(tailwind): style code snippets
    
    * docs(tailwind): use code snippet in TextColor stories
    
    * refactor(tailwind docs): rename UtilityClass to CodeSnippet
    
    * docs(tailwind): add notification on code-snippet copy
    
    * docs(tailwind): use UtilityClassTemplate in TextColorStories
    
    * Align TextColorStories
    
    * docs(tailwind): use StickerSheet instead of StoryWrapper
    
    * docs(tailwind): fix borders
    
    * docs(tailwind): fix widths
    
    * docs(tailwind): fix width in docs tab
    
    * docs(tailwind): fix height in Width stories
    
    * docs(tailwind): fix notification position
    
    * Add landing page for UtilityClassReferences

---
+++ docs/Systems/Tailwind/UtilityClassReferences/Borders/BorderColor.stories.tsx
@@ -0,0 +1,45 @@
+import React from "react"
+import { Story } from "@storybook/react"
+import { kaizenTailwindTheme } from "@kaizen/tailwind"
+import { CATEGORIES } from "../../../../../storybook/constants"
+import { UtilityClassTemplate } from "../../components/UtilityClassTemplate"
+import { flattenEntries } from "../../helpers/flatten-entries"
+import { utilityDescription } from "../../helpers/utilityDescription"
+import styles from "../styles.module.scss"
+
+const prefix = "border-"
+const classEntries = flattenEntries(
+  prefix,
+  kaizenTailwindTheme?.borderColor || {}
+)
+
+export default {
+  title: `${CATEGORIES.tailwind}/Utility Class References/Borders/Border Color`,
+  parameters: {
+    docs: {
+      description: {
+        component: utilityDescription(prefix, classEntries[0].utilityClassName),
+      },
+    },
+  },
+}
+
+const StickerSheetTemplate: Story<{ isReversed: boolean }> = ({
+  isReversed,
+}) => (
+  <UtilityClassTemplate
+    compiledCssPropertyName="border-color"
+    classKeyValues={classEntries}
+    renderExampleComponent={(cssProperty): React.ReactElement => (
+      <div
+        style={{ borderColor: cssProperty }}
+        className={styles.boxWithBorder}
+      />
+    )}
+    isReversed={isReversed}
+  />
+)
+
+export const StickerSheetDefault = StickerSheetTemplate.bind({})
+StickerSheetDefault.storyName = "Border Color"
+StickerSheetDefault.parameters = { chromatic: { disable: false } }
