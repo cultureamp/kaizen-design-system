3e8dcdd7d00377bae06f53d5a572aa251a419b2d
Geoffrey Chong <gyf.chong@gmail.com>
2023-02-28 17:07:20 +1100

    remove example component

--- packages/components/src/ExampleComponent/ExampleComponent.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import styles from "./ExampleComponent.module.css"
-
-// Placeholder for illustration DO NOT COPY
-export const ExampleComponent = (): JSX.Element => (
-  <div className={`${styles.exampleComponent} bg-green-300`}>
-    ExampleComponent FUTURE
-  </div>
-)
