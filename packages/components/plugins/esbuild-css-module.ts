51331e4a0dc4fda7bf6467a326cbaef4dcd13560
Geoffrey Chong <gyf.chong@gmail.com>
2023-03-01 11:49:53 +1100

    fix: types

--- packages/components/plugins/esbuild-css-module.ts
+++ packages/components/plugins/esbuild-css-module.ts
@@ -4,20 +4,28 @@ import path from "path"
 import postcss from "postcss"
 import postcssModules from "postcss-modules"
 
+type ArgsType = {
+  resolveDir: string
+  path: string
+  pluginData: { css?: string; pathDir: string }
+}
+
 export default {
   name: "css-module",
   setup(build): void {
-    build.onResolve({ filter: /\.module\.css$/, namespace: "file" }, args => ({
-      path: `${args.path}#css-module`,
-      namespace: "css-module",
-      pluginData: {
-        pathDir: path.join(args.resolveDir, args.path),
-      },
-    }))
+    build.onResolve(
+      { filter: /\.module\.css$/, namespace: "file" },
+      (args: ArgsType) => ({
+        path: `${args.path}#css-module`,
+        namespace: "css-module",
+        pluginData: {
+          pathDir: path.join(args.resolveDir, args.path),
+        },
+      })
+    )
     build.onLoad(
       { filter: /#css-module$/, namespace: "css-module" },
-      async ({ pluginData }: { pluginData: { pathDir: string } }) => {
-
+      async ({ pluginData }: ArgsType) => {
         const source = await fs.promises.readFile(pluginData.pathDir, "utf8")
 
         let cssModule = {}
@@ -39,16 +47,16 @@ export default {
     )
     build.onResolve(
       { filter: /\.module\.css$/, namespace: "css-module" },
-      args => ({
+      (args: ArgsType) => ({
         path: path.join(args.resolveDir, args.path, "#css-module-data"),
         namespace: "css-module",
-        pluginData: args.pluginData as { css: string },
+        pluginData: args.pluginData,
       })
     )
     build.onLoad(
       { filter: /#css-module-data$/, namespace: "css-module" },
-      args => {
-        const contents = (args.pluginData as { css: string }).css
+      (args: ArgsType) => {
+        const contents = args.pluginData.css
 
         return {
           contents,
