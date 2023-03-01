f8ee3afc7bca425d0bb05981700fa555fcad039d
Geoffrey Chong <gyf.chong@gmail.com>
2023-03-01 12:02:52 +1100

    remove added storybook plugin

--- storybook/main.ts
+++ storybook/main.ts
@@ -45,7 +45,6 @@ module.exports = {
     "@storybook/addon-essentials",
     "@storybook/addon-a11y",
     "storybook-addon-designs",
-    "storybook-css-modules",
   ],
   presets: [path.resolve("./storybook/header-preset/preset")],
 }
