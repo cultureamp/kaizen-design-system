33dd9f53f59417aaff10e7f59bf02617ce2369cb
Geoffrey Chong <gyfchong@users.noreply.github.com>
2023-03-02 12:00:44 +1100

    fix: a11y and component-library package-locks + build (#3338)

--- packages/component-library/tsup.config.ts
+++ packages/component-library/tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "tsup"
 
 export default defineConfig({
   entry: ["./index.ts"],
-  format: ["cjs", "esm", "iife"],
+  format: ["cjs", "esm"],
   splitting: true,
   dts: true,
   clean: true,
