cd44772faccd44bf50942e7be5550ef3172e5808
jakepitman <jakeseanp@gmail.com>
2023-03-06 14:53:05 +0900

    docs(storybook config): style storybook config

--- storybook/theme.ts
+++ storybook/theme.ts
@@ -1,17 +1,34 @@
 /* eslint import/no-extraneous-dependencies: 0 */
-
 import { create } from "@storybook/theming/create"
 import { defaultTheme } from "@kaizen/design-tokens"
+const logo = require("./assets/kaizen-logo.png")
+
+const colors = defaultTheme.color
 
 export default create({
   base: "light",
-  brandTitle: "🌱 Storybook",
-
+  colorSecondary: colors.purple["600"],
+  // UI
+  appBg: colors.purple["100"],
+  appContentBg: colors.white,
+  appBorderColor: colors.gray["300"],
+  appBorderRadius: defaultTheme.border.solid.borderRadius,
   // Typography
   fontBase: '"Inter", "Noto Sans", Helvetica, Arial, sans-serif',
   fontCode: "monospace",
-
   // Text colors
-  // Doesn't seem to like CSS Variables wah
-  textColor: defaultTheme.color.purple["800"],
+  textColor: colors.purple["800"],
+  textInverseColor: colors.white,
+  textMutedColor: "rgba(110, 90, 125)",
+  // Toolbar default and active colors
+  barTextColor: colors.gray["600"],
+  barSelectedColor: colors.purple["600"],
+  barBg: colors.white,
+  // Form colors
+  inputBg: colors.white,
+  inputBorder: colors.gray["500"],
+  inputTextColor: colors.gray["600"],
+  inputBorderRadius: "20px",
+  brandTitle: "Kaizen Storybook",
+  brandImage: logo,
 })
