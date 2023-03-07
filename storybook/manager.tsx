f0f39ada5dde2a69b64afc3a8eadf57af88b28d3
jakepitman <jakeseanp@gmail.com>
2023-03-07 13:34:16 +0900

    docs(storybook theme): style subheadings with custom renderLabel prop

--- storybook/manager.tsx
+++ storybook/manager.tsx
@@ -1,11 +1,22 @@
 // This file is used by storybook to register installed addons.
 // See: https://storybook.js.org/docs/addons/using-addons/
+import React from "react"
 import { addons } from "@storybook/addons"
+import { defaultTheme } from "@kaizen/design-tokens"
 import KaizenTheme from "./theme"
+
+const colors = defaultTheme.color
 
 addons.setConfig({
   theme: KaizenTheme,
   analyticsGTM: {
     gtmId: "GTM-KS4VWLT",
+  },
+  sidebar: {
+    renderLabel: item => (
+      <div style={item.type === "root" ? { color: colors.purple["800"] } : {}}>
+        {item.name}
+      </div>
+    ),
   },
 })
