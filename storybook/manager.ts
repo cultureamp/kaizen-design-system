99ea6aa4d7428b5a36405d05f7e0f3a4e77a8bb5
Geoffrey Chong <gyf.chong@gmail.com>
2023-03-08 11:26:38 +1100

    abstract stickersheets to own category

--- storybook/manager.ts
+++ storybook/manager.ts
@@ -9,3 +9,23 @@ addons.setConfig({
     gtmId: "GTM-KS4VWLT",
   },
 })
+
+const CSS_TO_HIDE_TEST_SECTION_FROM_SIDEBAR = `
+  #stickersheets,
+  *[data-parent-id*="stickersheets"],
+  *[title*="Stickersheets"],
+  #aio,
+  *[data-parent-id*="aio"],
+  *[title*="aio"] {
+    display: none !important;
+  }
+`
+
+if (process.env.NODE_ENV === "development") {
+  const head = document.head || document.getElementsByTagName("head")[0]
+  const style = document.createElement("style")
+  head.appendChild(style)
+  style.appendChild(
+    document.createTextNode(CSS_TO_HIDE_TEST_SECTION_FROM_SIDEBAR)
+  )
+}
