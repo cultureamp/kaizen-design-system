5017f4bd4f464a77de7cebd3e523560672cd0837
Cassandra Tam <cassandra.tam@cultureamp.com>
2023-03-09 14:50:09 +1100

    chore(storybook): enable story filtering

--- storybook/main.ts
+++ storybook/main.ts
@@ -6,16 +6,16 @@ import path from "path"
  * Use `STORIES=path/to/specific/story.stories.tsx` to load a specific story only.
  * Leaving blank will return false, causing our defaultStoryPaths (for all stories in Kaizen) to be used.
  */
-// const getStoryPathsFromEnv = (): string[] | false => {
-//   if (!process.env.STORIES) return false
-//   const storyPath = path.join(__dirname, "../", process.env.STORIES)
-//   if (fs.existsSync(storyPath)) {
-//     if (fs.statSync(storyPath).isDirectory()) {
-//       return [path.join(storyPath, "**/*.stories.tsx")]
-//     }
-//   }
-//   return [storyPath]
-// }
+const getStoryPathsFromEnv = (): string[] | false => {
+  if (!process.env.STORIES) return false
+  const storyPath = path.join(__dirname, "../", process.env.STORIES)
+  if (fs.existsSync(storyPath)) {
+    if (fs.statSync(storyPath).isDirectory()) {
+      return [path.join(storyPath, "**/*.stories.tsx")]
+    }
+  }
+  return [storyPath]
+}
 
 const defaultStoryPaths = [
   "../draft-packages/**/*.stories.tsx",
@@ -28,9 +28,7 @@ const defaultStoryPaths = [
 
 module.exports = {
   mode: "production",
-  // @todo: test this still works
-  // stories: getStoryPathsFromEnv() || defaultStoryPaths,
-  stories: defaultStoryPaths,
+  stories: getStoryPathsFromEnv() || defaultStoryPaths,
   addons: ["@storybook/addon-essentials", "@storybook/addon-a11y"],
   presets: [path.resolve("./storybook/header-preset/preset")],
   framework: {
