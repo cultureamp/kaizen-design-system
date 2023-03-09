c9836ae44d5690062fff0cc574bde7362a745073
Geoffrey Chong <gyf.chong@gmail.com>
2023-03-09 12:15:11 +1100

    add production/dev classname for targeting styles

--- storybook/manager.ts
+++ storybook/manager.ts
@@ -1,3 +1,4 @@
+/* eslint-disable ssr-friendly/no-dom-globals-in-module-scope */
 // This file is used by storybook to register installed addons.
 // See: https://storybook.js.org/docs/addons/using-addons/
 import { addons } from "@storybook/addons"
@@ -10,22 +11,5 @@ addons.setConfig({
   },
 })
 
-const CSS_TO_HIDE_TEST_SECTION_FROM_SIDEBAR = `
-  #stickersheets,
-  *[data-parent-id*="stickersheets"],
-  *[title*="Stickersheets"],
-  #aio,
-  *[data-parent-id*="aio"],
-  *[title*="aio"] {
-    display: none !important;
-  }
-`
-
-if (process.env.NODE_ENV !== "development") {
-  const head = document.head || document.getElementsByTagName("head")[0]
-  const style = document.createElement("style")
-  head.appendChild(style)
-  style.appendChild(
-    document.createTextNode(CSS_TO_HIDE_TEST_SECTION_FROM_SIDEBAR)
-  )
-}
+const body = document.body || document.getElementsByTagName("body")[0]
+body.classList.add(process.env.NODE_ENV || "development")
