862c841e80b5ba56b527fb329ad6bf5f4f3163d6
Cassandra Tam <cassandra.tam@cultureamp.com>
2023-03-10 16:47:23 +1100

    chore: comment out header-preset as it no longer works

--- storybook/header-preset/preset.ts
+++ storybook/header-preset/preset.ts
@@ -1,37 +1,41 @@
-import path from "path"
-import { Configuration, NormalModuleReplacementPlugin } from "webpack"
-const webpackConfig = require("../webpack/rules")
+// import path from "path"
+// import { Configuration, NormalModuleReplacementPlugin } from "webpack"
+// const webpackConfig = require("../webpack/rules")
 
-module.exports = {
-  managerWebpack: (config, { configType }): Configuration => {
-    // don't add header and footer for local builds
-    if (configType !== "PRODUCTION") return config
+//
+// @NOTE: This isn't working in Storybook v7
+//
 
-    const replaceLayoutPlugin = new NormalModuleReplacementPlugin(
-      /^\.\/layout$/,
-      "custom-layout"
-    )
+// module.exports = {
+//   managerWebpack: (config, { configType }): Configuration => {
+//     // don't add header and footer for local builds
+//     if (configType !== "PRODUCTION") return config
 
-    return {
-      ...config,
-      plugins: [...config.plugins, replaceLayoutPlugin],
-      module: {
-        ...config.module,
-        rules: [
-          webpackConfig.babel,
-          webpackConfig.styles,
-          webpackConfig.svgs,
-          webpackConfig.svgIcons,
-        ].map(webpackConfig.excludeExternalModules),
-      },
-      resolve: {
-        ...config.resolve,
-        alias: {
-          ...config.resolve.alias,
-          "custom-layout": path.resolve("./storybook/header-preset/index.tsx"),
-        },
-        extensions: [...config.resolve.extensions, ".ts", ".tsx"],
-      },
-    }
-  },
-}
+//     const replaceLayoutPlugin = new NormalModuleReplacementPlugin(
+//       /^\.\/layout$/,
+//       "custom-layout"
+//     )
+
+//     return {
+//       ...config,
+//       plugins: [...config.plugins, replaceLayoutPlugin],
+//       module: {
+//         ...config.module,
+//         rules: [
+//           webpackConfig.babel,
+//           webpackConfig.styles,
+//           webpackConfig.svgs,
+//           webpackConfig.svgIcons,
+//         ].map(webpackConfig.excludeExternalModules),
+//       },
+//       resolve: {
+//         ...config.resolve,
+//         alias: {
+//           ...config.resolve.alias,
+//           "custom-layout": path.resolve("./storybook/header-preset/index.tsx"),
+//         },
+//         extensions: [...config.resolve.extensions, ".ts", ".tsx"],
+//       },
+//     }
+//   },
+// }
