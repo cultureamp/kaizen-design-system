ce9b0cc0d033809060aeee119ebe05560d4cb769
Cassandra Tam <cassandra.tam@cultureamp.com>
2023-03-10 17:13:04 +1100

    docs(expert-advice-collapsible): remove unused import

--- draft-packages/collapsible/docs/ExpertAdviceCollapsible.stories.tsx
+++ draft-packages/collapsible/docs/ExpertAdviceCollapsible.stories.tsx
@@ -1,6 +1,5 @@
 import React from "react"
 import { Meta, StoryFn } from "@storybook/react"
-import { Box } from "@kaizen/component-library"
 import { ExpertAdviceCollapsible } from "@kaizen/draft-collapsible"
 import { Paragraph } from "@kaizen/typography"
 
