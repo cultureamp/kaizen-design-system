0a7f3cd14623dc5a4f9bac832592bf4733eeec0a
nloyzaga <nloyzaga@gmail.com>
2023-03-13 14:55:25 +1100

    tests(Filter): Add tests for show content

--- packages/components/src/Filter/Filter.spec.tsx
+++ packages/components/src/Filter/Filter.spec.tsx
@@ -1,5 +1,6 @@
 import React from "react"
-import { render, screen } from "@testing-library/react"
+import { render, screen, waitFor } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
 import { Filter, FilterProps } from "./Filter"
 
 const FilterWrapper = (customProps?: Partial<FilterProps>): JSX.Element => {
@@ -24,9 +25,32 @@ describe("<Filter />", () => {
     expect(screen.queryByRole("dialog")).not.toBeInTheDocument()
   })
 
-  it("show content when isOpen is true", () => {
+  it("shows content when isOpen is true", () => {
     render(<FilterWrapper isOpen />)
     expect(screen.queryByRole("dialog")).toBeInTheDocument()
     expect(screen.queryByText("Filter Contents")).toBeInTheDocument()
+  })
+
+  it("shows content when trigger button is clicked", async () => {
+    render(<FilterWrapper />)
+    const filterButton = screen.getByRole("button")
+    await userEvent.click(filterButton)
+    await waitFor(() => {
+      expect(screen.queryByRole("dialog")).toBeInTheDocument()
+      expect(screen.queryByText("Filter Contents")).toBeInTheDocument()
+    })
+  })
+
+  it("shows content when trigger button on keydown enter", async () => {
+    render(<FilterWrapper />)
+    const filterButton = screen.getByRole("button")
+    await userEvent.tab()
+    expect(filterButton).toHaveFocus()
+    userEvent.keyboard("{Enter}")
+
+    await waitFor(() => {
+      expect(screen.queryByRole("dialog")).toBeInTheDocument()
+      expect(screen.queryByText("Filter Contents")).toBeInTheDocument()
+    })
   })
 })
