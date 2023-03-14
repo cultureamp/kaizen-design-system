7af61afdb9668cf7dba4bbaa5974ab51e8ad805c
nloyzaga <nloyzaga@gmail.com>
2023-03-15 09:42:28 +1100

    tests(Filter): Update test for ref

--- packages/components/src/Filter/Filter.spec.tsx
+++ packages/components/src/Filter/Filter.spec.tsx
@@ -74,55 +74,46 @@ describe("<Filter />", () => {
       expect(screen.queryByText("Filter Contents")).toBeInTheDocument()
     })
   })
-  describe("<RemovableFilterTriggerButton />", () => {
-    it("should use fallback label for remove button if not specified", () => {
-      render(<FilterTriggerButtonRemovableWrapper />)
-      expect(
-        screen.getByRole("button", { name: "Remove filter - Desserts" })
-      ).toBeVisible()
-    })
+  describe("Refs", () => {
+    it("correctly passes through both button refs", async () => {
+      const onClick = jest.fn<
+        void,
+        [string | null | undefined, string | null | undefined]
+      >()
 
-    describe("Refs", () => {
-      it("correctly passes through both button refs", async () => {
-        const onClick = jest.fn<
-          void,
-          [string | null | undefined, string | null | undefined]
-        >()
+      const Wrapper = (): JSX.Element => {
+        const triggerButtonRef = useRef<HTMLButtonElement>(null)
+        const removeButtonRef = useRef<HTMLButtonElement>(null)
+        const ref = useRef({ triggerButtonRef, removeButtonRef })
 
-        const Wrapper = (): JSX.Element => {
-          const triggerButtonRef = useRef<HTMLButtonElement>(null)
-          const removeButtonRef = useRef<HTMLButtonElement>(null)
-          const ref = useRef({ triggerButtonRef, removeButtonRef })
+        const handleClick = (): void =>
+          onClick(triggerButtonRef.current?.id, removeButtonRef.current?.id)
 
-          const handleClick = (): void =>
-            onClick(triggerButtonRef.current?.id, removeButtonRef.current?.id)
-
-          return (
-            <>
-              <FilterTriggerButtonRemovable
-                ref={ref}
-                id="test__date-input-field--ref"
-                triggerButtonProps={{
-                  label: "Desserts",
-                  id: "test__trigger-button",
-                }}
-                removeButtonProps={{
-                  id: "test__remove-button",
-                }}
-              />
-              <button onClick={handleClick}>Click me</button>
-            </>
-          )
-        }
-
-        render(<Wrapper />)
-
-        await userEvent.click(screen.getByText("Click me"))
-        expect(onClick).toBeCalledWith(
-          "test__trigger-button",
-          "test__remove-button"
+        return (
+          <>
+            <FilterTriggerButtonRemovable
+              ref={ref}
+              id="test__date-input-field--ref"
+              triggerButtonProps={{
+                label: "Desserts",
+                id: "test__trigger-button",
+              }}
+              removeButtonProps={{
+                id: "test__remove-button",
+              }}
+            />
+            <button onClick={handleClick}>Click me</button>
+          </>
         )
-      })
+      }
+
+      render(<Wrapper />)
+
+      await userEvent.click(screen.getByText("Click me"))
+      expect(onClick).toBeCalledWith(
+        "test__trigger-button",
+        "test__remove-button"
+      )
     })
   })
 })
