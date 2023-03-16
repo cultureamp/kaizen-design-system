cd41dfeae019624405a257e25d0b4a440754041b
Cassandra Tam <cassandra.tam@cultureamp.com>
2023-03-16 17:29:37 +1100

    docs(filter-button): update stories

--- packages/components/src/FilterButton/docs/FilterButton.stories.tsx
+++ packages/components/src/FilterButton/docs/FilterButton.stories.tsx
@@ -8,7 +8,7 @@ export default {
   parameters: {
     docs: {
       description: {
-        component: "Trigger button for Filter.",
+        component: "To be used with Filter.",
       },
     },
   },
