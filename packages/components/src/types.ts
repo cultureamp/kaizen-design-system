28b1bad6d8a9cc08c3d8151954c81b992044a0f8
Geoffrey Chong <gyf.chong@gmail.com>
2023-03-17 13:36:45 +1100

    cleanup

--- packages/components/src/types.ts
+++
@@ -1,4 +0,0 @@
-export type OverrideClassName<T extends Record<string, any>> =
-  | Omit<T, "className"> & { classNameOverride?: string }
-
-export type DataAttributes = { [key: `data-${string}`]: string | undefined }
