8e453838d42d2542cc6deedfc50a80dfedda3ecd
Doug MacKenzie <doug.mackenzie@cultureamp.com>
2023-03-20 21:44:14 +1100

    fix(form): Link RadioGroup/CheckboxGroup label to input group

--- draft-packages/form/KaizenDraft/Form/CheckboxGroup/CheckboxGroup.tsx
+++ draft-packages/form/KaizenDraft/Form/CheckboxGroup/CheckboxGroup.tsx
@@ -1,5 +1,6 @@
-import React, { HTMLAttributes } from "react"
+import React, { useState, HTMLAttributes } from "react"
 import classnames from "classnames"
+import { v4 } from "uuid"
 import { OverrideClassName } from "@kaizen/component-base"
 import { Label } from "../Primitives"
 import styles from "./CheckboxGroup.module.scss"
@@ -8,6 +9,7 @@ export interface CheckboxGroupProps
   extends OverrideClassName<HTMLAttributes<HTMLDivElement>> {
   children?: React.ReactNode
   labelText: string | React.ReactNode
+  labelId?: string
   noBottomMargin?: boolean
   reversed?: boolean
   /**
@@ -24,30 +26,39 @@ export interface CheckboxGroupProps
 export const CheckboxGroup = ({
   children,
   labelText,
+  labelId: propsLabelId,
   noBottomMargin = false,
   reversed = false,
   automationId,
   classNameOverride,
   ...restProps
-}: CheckboxGroupProps): JSX.Element => (
-  <div
-    data-automation-id={automationId && `${automationId}-field-checkbox-group`}
-    className={classnames(styles.checkboxGroupContainer, classNameOverride, {
-      [styles.noBottomMargin]: noBottomMargin,
-      [styles.reversed]: reversed,
-    })}
-    {...restProps}
-  >
-    <div className={styles.checkboxGroupLabel}>
-      <Label
-        automationId={`${automationId}-field-label`}
-        labelText={labelText}
-        labelType="text"
-        reversed={reversed}
-      />
+}: CheckboxGroupProps): JSX.Element => {
+  const [labelId] = useState<string>(propsLabelId || v4())
+  return (
+    <div
+      data-automation-id={
+        automationId && `${automationId}-field-checkbox-group`
+      }
+      className={classnames(styles.checkboxGroupContainer, classNameOverride, {
+        [styles.noBottomMargin]: noBottomMargin,
+        [styles.reversed]: reversed,
+      })}
+      role="group"
+      aria-labelledby={labelId}
+      {...restProps}
+    >
+      <div className={styles.checkboxGroupLabel}>
+        <Label
+          automationId={automationId && `${automationId}-field-label`}
+          id={labelId}
+          labelText={labelText}
+          labelType="text"
+          reversed={reversed}
+        />
+      </div>
+      {children}
     </div>
-    {children}
-  </div>
-)
+  )
+}
 
 CheckboxGroup.displayName = "CheckboxGroup"
