c57a3b13e2a8eb775fb56f17d5e85804aeb11513
Doug MacKenzie <doug.mackenzie@cultureamp.com>
2023-03-21 11:08:52 +1100

    Replace snapshot tests with tests for accessible name

--- draft-packages/form/KaizenDraft/Form/CheckboxGroup/CheckboxGroup.spec.tsx
+++ draft-packages/form/KaizenDraft/Form/CheckboxGroup/CheckboxGroup.spec.tsx
@@ -1,8 +1,6 @@
 import React from "react"
-import { cleanup, render } from "@testing-library/react"
+import { render, screen } from "@testing-library/react"
 import { CheckboxGroup, CheckboxGroupProps } from "./CheckboxGroup"
-
-afterEach(cleanup)
 
 const defaultCheckboxGroupProps: CheckboxGroupProps = {
   automationId: "CheckboxGroupAutomationId",
@@ -36,25 +34,14 @@ describe("<CheckboxGroup /> ", () => {
     expect(queryByText(title)).toBeTruthy()
   })
 
-  test("renders Checkbox Group with noBottomMargin attribute", () => {
-    const { container } = render(
-      <CheckboxGroup
-        automationId="CheckboxGroupAutomationId"
-        labelText="CheckboxGroup Label"
-        noBottomMargin
-      />
-    )
-
-    expect(container.firstChild).toMatchSnapshot()
-  })
-  test("renders Checkbox Group without noBottomMargin attribute", () => {
-    const { container } = render(
-      <CheckboxGroup
-        automationId="CheckboxGroupAutomationId"
-        labelText="CheckboxGroup Label"
-      />
-    )
-
-    expect(container.firstChild).toMatchSnapshot()
+  describe("accessibilty", () => {
+    it("should have an accessible name when not provided a labelId", () => {
+      render(<CheckboxGroup labelText="Label title" />)
+      expect(screen.getByRole("group", { name: "Label title" }))
+    })
+    it("should have an accessible name when provided a labelId", () => {
+      render(<CheckboxGroup labelText="Label title" labelId="some-custom-id" />)
+      expect(screen.getByRole("group", { name: "Label title" }))
+    })
   })
 })
--- draft-packages/form/KaizenDraft/Form/RadioGroup/RadioGroup.spec.tsx
+++ draft-packages/form/KaizenDraft/Form/RadioGroup/RadioGroup.spec.tsx
@@ -1,60 +1,8 @@
 import React from "react"
-import { cleanup, render } from "@testing-library/react"
-import { RadioField } from "@kaizen/draft-form"
+import { render, screen } from "@testing-library/react"
 import { RadioGroup } from "./index"
 
-afterEach(cleanup)
-
 describe("<RadioGroup /> ", () => {
-  describe("snapshots", () => {
-    test("renders RadioGroup in a column and with a label", () => {
-      const { container } = render(
-        <RadioGroup labelText="Label" children={null} />
-      )
-
-      expect(container.firstChild).toMatchSnapshot()
-    })
-
-    test("renders RadioGroup in a row and with a label", () => {
-      const { container } = render(
-        <RadioGroup labelText="Label" children={null} />
-      )
-
-      expect(container.firstChild).toMatchSnapshot()
-    })
-    test("renders RadioGroup with radios", () => {
-      const { container } = render(
-        <RadioGroup labelText="Label">
-          <RadioField
-            name="radio"
-            id="radio-1"
-            labelText="Label 1"
-            selectedStatus={false}
-            disabled={false}
-            value="radio-1"
-          />
-          <RadioField
-            name="radio"
-            id="radio-2"
-            labelText="Label 2"
-            selectedStatus={false}
-            disabled={false}
-            value="radio-2"
-          />
-        </RadioGroup>
-      )
-
-      expect(container.firstChild).toMatchSnapshot()
-    })
-
-    test("renders RadioGroup in a column and with a label", () => {
-      const { container } = render(
-        <RadioGroup labelText="Label" children={null} />
-      )
-
-      expect(container.firstChild).toMatchSnapshot()
-    })
-  })
   describe("presentational", () => {
     it("should render a title", () => {
       const title = "Label"
@@ -66,21 +14,13 @@ describe("<RadioGroup /> ", () => {
     })
   })
   describe("accessibilty", () => {
-    it("should markup aria label when supplied labelId", () => {
-      const labelId = "labelId"
-      const { getByText, getByRole } = render(
-        <RadioGroup
-          labelText="Label title"
-          labelId={labelId}
-          automationId="test"
-        />
-      )
-      expect(getByRole("radiogroup").getAttribute("aria-labelledby")).toEqual(
-        labelId
-      )
-      expect(
-        getByText("Label title").parentElement?.getAttribute("id")
-      ).toEqual(labelId)
+    it("should have an accessible name when not provided a labelId", () => {
+      render(<RadioGroup labelText="Label title" />)
+      expect(screen.getByRole("radiogroup", { name: "Label title" }))
+    })
+    it("should have an accessible name when provided a labelId", () => {
+      render(<RadioGroup labelText="Label title" labelId="some-custom-id" />)
+      expect(screen.getByRole("radiogroup", { name: "Label title" }))
     })
   })
 })
