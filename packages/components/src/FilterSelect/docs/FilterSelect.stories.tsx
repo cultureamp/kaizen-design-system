de84e4742dd9b25c6b794c1660d87f61b04f1779
Cassandra Tam <cassandra.tam@cultureamp.com>
2023-03-22 17:28:45 +1100

    docs(filter-select): add custom option example to sticker sheet

--- packages/components/src/FilterSelect/docs/FilterSelect.stories.tsx
+++ packages/components/src/FilterSelect/docs/FilterSelect.stories.tsx
@@ -1,11 +1,10 @@
 import React, { useState } from "react"
-import { action } from "@storybook/addon-actions"
 import { ComponentMeta, ComponentStory, Story } from "@storybook/react"
 import isChromatic from "chromatic"
 import { singleMockItems } from "@kaizen/select/docs/MockData"
 import { StickerSheet } from "../../../../../storybook/components/StickerSheet"
-import { FilterButton, FilterButtonRemovable } from "../../FilterButton"
-import { Filter, FilterContents } from "../../index"
+import { FilterButton } from "../../FilterButton"
+import { Filter } from "../../index"
 import { FilterSelect } from "../FilterSelect"
 
 const IS_CHROMATIC = isChromatic()
@@ -27,47 +26,80 @@ export const DefaultStory: ComponentStory<typeof Filter> = () => {
 
   return (
     <FilterSelect
+      label="Pancakes"
       isOpen={isOpen}
       setIsOpen={setIsOpen}
       renderTrigger={(triggerButtonProps): JSX.Element => (
         <FilterButton {...triggerButtonProps} />
       )}
-      label="Pancakes"
       items={singleMockItems}
     />
   )
 }
 DefaultStory.storyName = "Filter Select"
 
-// const StickerSheetTemplate: Story = () => {
-//   const [isOpen, setIsOpen] = React.useState(true)
+const StickerSheetTemplate: Story = () => {
+  const [isOpen, setIsOpen] = React.useState(true)
+  const [isCustomItemsOpen, setIsCustomItemsOpen] = React.useState(true)
 
-//   return (
-//     <StickerSheet
-//       heading="Filter"
-//       style={{ paddingBottom: IS_CHROMATIC ? "6rem" : undefined }}
-//     >
-//       <StickerSheet.Header headings={["Open"]} />
-//       <StickerSheet.Body>
-//         <StickerSheet.Row>
-//           <Filter
-//             isOpen={isOpen}
-//             setIsOpen={setIsOpen}
-//             renderTrigger={(triggerProps): JSX.Element => (
-//               <FilterButton label="Label" {...triggerProps} />
-//             )}
-//           >
-//             <FilterContents>Filter Contents</FilterContents>
-//           </Filter>
-//         </StickerSheet.Row>
-//       </StickerSheet.Body>
-//     </StickerSheet>
-//   )
-// }
+  return (
+    <StickerSheet
+      heading="Filter Select"
+      style={{ paddingBottom: IS_CHROMATIC ? "26rem" : undefined }}
+    >
+      <StickerSheet.Header headings={["Open", "Custom items"]} />
+      <StickerSheet.Body>
+        <StickerSheet.Row>
+          <div style={{ width: "250px" }}>
+            <FilterSelect
+              label="Label"
+              isOpen={isOpen}
+              setIsOpen={setIsOpen}
+              renderTrigger={(triggerProps): JSX.Element => (
+                <FilterButton {...triggerProps} />
+              )}
+              items={singleMockItems}
+            />
+          </div>
+          <div>
+            <FilterSelect
+              label="Label"
+              isOpen={isCustomItemsOpen}
+              setIsOpen={setIsCustomItemsOpen}
+              renderTrigger={(triggerProps): JSX.Element => (
+                <FilterButton {...triggerProps} />
+              )}
+              items={singleMockItems}
+            >
+              {({ items }): JSX.Element[] =>
+                items.map(item => (
+                  <FilterSelect.Option
+                    key={item.key}
+                    item={{
+                      ...item,
+                      rendered: (
+                        <div>
+                          <div style={{ fontSize: "0.75rem", color: "gray" }}>
+                            Customised in list!
+                          </div>
+                          <div>{item.rendered}</div>
+                        </div>
+                      ),
+                    }}
+                  />
+                ))
+              }
+            </FilterSelect>
+          </div>
+        </StickerSheet.Row>
+      </StickerSheet.Body>
+    </StickerSheet>
+  )
+}
 
-// export const StickerSheetDefault = StickerSheetTemplate.bind({})
-// StickerSheetDefault.storyName = "Sticker Sheet (Default)"
-// StickerSheetDefault.parameters = {
-//   chromatic: { disable: false },
-//   controls: { disable: true },
-// }
+export const StickerSheetDefault = StickerSheetTemplate.bind({})
+StickerSheetDefault.storyName = "Sticker Sheet (Default)"
+StickerSheetDefault.parameters = {
+  chromatic: { disable: false },
+  controls: { disable: true },
+}
