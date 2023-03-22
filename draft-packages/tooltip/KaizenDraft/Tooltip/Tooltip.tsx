b04c42330201b297918d514ee2d99035801b4113
Geoffrey Chong <gyf.chong@gmail.com>
2023-03-22 14:34:50 +1100

    fix: Tooltip named export

--- draft-packages/tooltip/KaizenDraft/Tooltip/Tooltip.tsx
+++ draft-packages/tooltip/KaizenDraft/Tooltip/Tooltip.tsx
@@ -148,7 +148,7 @@ const TooltipContent = ({
  * {@link https://cultureamp.design/components/tooltip/ Guidance} |
  * {@link https://cultureamp.design/storybook/?path=/docs/components-tooltip--default-kaizen-site-demo Storybook}
  */
-const Tooltip = ({
+export const Tooltip = ({
   children,
   text,
   inline,
