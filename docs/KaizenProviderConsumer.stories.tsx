dad832e60d8be43383bcedad8bc0a274c65a0739
jakepitman <jakeseanp@gmail.com>
2023-04-03 12:21:17 +1200

    WIP Use function for error boundary

--- docs/KaizenProviderConsumer.stories.tsx
+++ docs/KaizenProviderConsumer.stories.tsx
@@ -1,47 +1,36 @@
 import React from "react"
 import { ComponentStory, Story } from "@storybook/react"
-import { FormattedMessage } from "react-intl"
-import * as intl from "react-intl"
+import { useIntl } from "react-intl"
 
 type Props = {
   messageId: string
   defaultMessage: string
 }
 
-class TranslatedComponentWithFallback extends React.Component<Props> {
-  state = { hasError: false }
+const translateMessageSafely = (id: string, defaultMessage: string) => {
+  let message: string
 
-  static getDerivedStateFromError(error: unknown): { hasError: true } {
-    // Update state so the next render will show the fallback UI.
-    return { hasError: true }
+  try {
+    const intl = useIntl()
+    message = intl.formatMessage({
+      id,
+      defaultMessage,
+    })
+  } catch {
+    message = defaultMessage
   }
+  return message
+}
 
-  render(): React.ReactElement {
-    if (this.state.hasError) {
-      // Need to figure out how to return this as a text node
-      // because that's what FormattedMessage would have returned
-      return <p>{this.props.defaultMessage}</p>
-    }
-
-    return (
-      <FormattedMessage
-        id={this.props.messageId}
-        defaultMessage={this.props.defaultMessage}
-      />
-    )
-  }
+const TranslatedComponent = () => {
+  return <p>{translateMessageSafely("myMessage", "My default message")}</p>
 }
 
 export default {
   title: "My Translated Component",
-  component: TranslatedComponentWithFallback,
+  component: TranslatedComponent,
 }
 
-export const DefaultStory = (): JSX.Element => (
-  <TranslatedComponentWithFallback
-    messageId="myMessage"
-    defaultMessage="Default English translation"
-  />
-)
+export const DefaultStory = (): JSX.Element => <TranslatedComponent />
 
 DefaultStory.storyName = "KaizenProvider Consumer"
