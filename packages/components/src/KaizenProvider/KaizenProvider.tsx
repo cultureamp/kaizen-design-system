35187d781f604b93dbeadc31b486d9abe8b50912
jakepitman <jakeseanp@gmail.com>
2023-04-03 13:16:29 +1200

    feat(kaizenprovider): make KaizenProvider accept intl config object

--- packages/components/src/KaizenProvider/KaizenProvider.tsx
+++ packages/components/src/KaizenProvider/KaizenProvider.tsx
@@ -1,16 +1,21 @@
 import React from "react"
+import { IntlConfig, RawIntlProvider } from "react-intl"
 import { ThemeProvider, ThemeManager } from "./ThemeProvider"
 
 export interface KaizenProviderProps {
   children: React.ReactNode
+  intlConfig: IntlConfig
   themeManager?: ThemeManager
 }
 
 export const KaizenProvider = ({
   children,
+  intlConfig,
   themeManager,
 }: KaizenProviderProps): JSX.Element => (
-  <ThemeProvider themeManager={themeManager}>{children}</ThemeProvider>
+  <ThemeProvider themeManager={themeManager}>
+    <RawIntlProvider value={intlConfig}>{children}</RawIntlProvider>
+  </ThemeProvider>
 )
 
 KaizenProvider.displayName = "KaizenProvider"
