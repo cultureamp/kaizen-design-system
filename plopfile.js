2b444a5e81fbe2bd11ca069023021bcdd3dfa5f3
Cassandra Tam <cassandra.tam@cultureamp.com>
2023-04-04 15:02:05 +1000

    chore(plop): add support for adding components to AIO

--- plopfile.js
+++ plopfile.js
@@ -23,16 +23,64 @@ module.exports = (
         when: answers => answers.isSubcomponent,
       },
       {
+        type: "confirm",
+        name: "isAIO",
+        message: "Add this component to the AIO?",
+        default: true,
+      },
+      {
         type: "input",
         name: "packageName",
         message: "What is the package name?",
+        when: answers => answers.isAIO === false,
         default: ({ componentName, isSubcomponent, parentComponentName }) =>
           plop.getHelper("kebabCase")(
             isSubcomponent ? parentComponentName : componentName
           ),
       },
     ],
-    actions: ({ isSubcomponent }) => {
+    actions: ({ isSubcomponent, isAIO }) => {
+      if (isAIO) {
+        if (isSubcomponent) {
+          return [
+            {
+              type: "addMany",
+              destination:
+                "packages/components/src/{{pascalCase parentComponentName}}/subcomponents",
+              base: "plop-templates/aio-component",
+              // We only want the base src files and not any subfolders
+              templateFiles: "plop-templates/aio-component/*.hbs",
+            },
+          ]
+        }
+
+        return [
+          {
+            type: "addMany",
+            destination: "packages/components/src/{{pascalCase componentName}}",
+            base: "plop-templates/aio-component",
+            templateFiles: "plop-templates/aio-component/**/*.hbs",
+            data: {
+              packageName: "components",
+            },
+          },
+          {
+            type: "modify",
+            path: "packages/components/src/index.ts",
+            transform: (content, answers) => {
+              const componentName = plop.getHelper("pascalCase")(
+                answers.componentName
+              )
+              const exportStatement = `export * from "./${componentName}"`
+
+              if (content.includes(exportStatement)) return content
+
+              return `${content.trim()}\n${exportStatement}\n`
+            },
+          },
+        ]
+      }
+
       if (isSubcomponent) {
         return [
           {
