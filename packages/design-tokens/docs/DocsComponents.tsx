9c0429e8858eddcf3d491cc40ae7c6177df4406f
Cassandra Tam <cassandra.tam@cultureamp.com>
2023-04-06 13:34:43 +1000

    docs: fix relative paths

--- packages/design-tokens/docs/DocsComponents.tsx
+++ packages/design-tokens/docs/DocsComponents.tsx
@@ -1,9 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 
 import React from "react"
-import LinkTo from "@storybook/addon-links/react"
 import { Unstyled } from "@storybook/blocks"
-import { Meta } from "@storybook/react"
 import classNames from "classnames"
 import Highlight from "react-highlight"
 import { Box } from "@kaizen/component-library"
@@ -174,25 +172,6 @@ const sassBlocks: Array<
 
 export const SassVariablesCodeBlocks = (): JSX.Element => (
   <TabbedCodeBlocks blocks={sassBlocks} />
-)
-
-export const getStoryLinkName = (
-  storyTitle: string | undefined
-): string | undefined => storyTitle?.replace(/.*\//, "")
-
-export const LinkToStory = ({
-  storyModule,
-  children,
-  hash: _hash, // not used
-}: {
-  storyModule: Meta
-  hash?: string
-  /* Children can be used to override the Link text */
-  children?: React.ReactNode
-}): JSX.Element => (
-  <LinkTo kind={storyModule.title}>
-    {children || getStoryLinkName(storyModule.title!)}
-  </LinkTo>
 )
 
 export default {}
--- storybook/components/LinkTo/LinkTo.tsx
+++ storybook/components/LinkTo/LinkTo.tsx
@@ -4,16 +4,18 @@ import classnames from "classnames"
 
 interface LinkToProps extends HTMLAttributes<HTMLButtonElement> {
   pageId: string
+  sectionName?: string
 }
 
 export const LinkTo = ({
   children,
   pageId,
+  sectionName,
   className,
   ...restButtonAttributes
 }: LinkToProps): JSX.Element => (
   <button
-    onClick={linkTo(pageId)}
+    onClick={linkTo(pageId, sectionName)}
     className={classnames(
       "bg-transparent border-none text-blue-400 hover:underline cursor-pointer p-0",
       className
