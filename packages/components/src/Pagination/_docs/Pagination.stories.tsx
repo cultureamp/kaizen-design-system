e89e5fef7301e64eb49a158513875a4a87ab3df2
JakePitman <jakeseanp@gmail.com>
2023-04-06 17:44:58 +1200

    feat(components): Kzn 1111/pagination migrate (#3445)
    * Add Pagination component to components/src
    
    * Add changeset
    
    * Add missing export
    
    ---------
    
    Co-authored-by: Geoffrey Chong <gyfchong@users.noreply.github.com>

---
+++ packages/components/src/Pagination/_docs/Pagination.stories.tsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react"
+import { ComponentStory } from "@storybook/react"
+import { Pagination } from "@kaizen/pagination"
+import { CATEGORIES } from "../../../../../storybook/constants"
+
+export default {
+  title: `${CATEGORIES.components}/Pagination`,
+  component: Pagination,
+  parameters: {
+    docs: {
+      description: {
+        component: 'import { Pagination } from "@kaizen/pagination";',
+      },
+    },
+  },
+  decorators: [
+    (story, { globals: { textDirection } }): JSX.Element => (
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          padding: "1rem",
+        }}
+      >
+        {story({ isRTL: textDirection === "rtl" })}
+      </div>
+    ),
+  ],
+}
+
+export const Default: ComponentStory<typeof Pagination> = args => {
+  const [currentPage, setCurrentPage] = useState(1)
+
+  return (
+    <Pagination
+      {...args}
+      currentPage={currentPage}
+      ariaLabelNextPage="Next page"
+      ariaLabelPreviousPage="Previous page"
+      pageCount={10}
+      onPageChange={setCurrentPage}
+    />
+  )
+}
+Default.parameters = { chromatic: { disable: false } }
