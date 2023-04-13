1a9c458ee5b6f3c984f3a27224f4155fd1b1a100
jakepitman <jakeseanp@gmail.com>
2023-04-14 11:45:44 +1200

    Update GlobalNotification docs

--- packages/notification/docs/GlobalNotification.stories.tsx
+++ packages/notification/docs/GlobalNotification.stories.tsx
@@ -1,40 +1,67 @@
 import React from "react"
-import { Meta, StoryFn } from "@storybook/react"
+import { StoryFn, StoryObj } from "@storybook/react"
 import { GlobalNotification } from "@kaizen/notification"
-import { StoryWrapper } from "../../../storybook/components/StoryWrapper"
+import { ComponentDocsTemplate } from "../../../storybook/components/DocsContainer"
+import { StickerSheet } from "../../../storybook/components/StickerSheet"
 
-export default {
+const meta = {
   tags: ["autodocs"],
   title: "Components/Notification/Global Notification",
   component: GlobalNotification,
   parameters: {
     docs: {
-      description: {
-        component: 'import { GlobalNotification } from "@kaizen/notification";',
+      container: ComponentDocsTemplate,
+    },
+    installation: [
+      "npm install @kaizen/notification",
+      "import { GlobalNotication } from `@kaizen/notification`",
+    ],
+    resourceLinks: {
+      sourceCode:
+        "https://github.com/cultureamp/kaizen-design-system/tree/main/packages/notification/src",
+      figma:
+        "https://www.figma.com/file/ZRfnoNUXbGZv4eVWLbF4Az/%F0%9F%96%BC%EF%B8%8F-Component-Gallery?node-id=9-94185&t=pCbdJNgUOTYNCrEN-0",
+      designGuidelines:
+        "https://cultureamp.atlassian.net/wiki/spaces/DesignSystem/pages/3082060757",
+    },
+  },
+}
+
+export default meta
+
+/**
+ *
+ * A global notification is a message object used for system-level notifications
+ * that inform the user about system status such as logging in and background
+ * processes like data imports.
+ */
+export const Playground: StoryObj<typeof meta> = {
+  args: {
+    type: "positive",
+    persistent: true,
+    children: (
+      <>
+        Emails will be sent notifying coaches and inviting reviewers to give
+        their feedback. <a href="/">View all</a>
+      </>
+    ),
+  },
+  parameters: {
+    docs: {
+      canvas: {
+        sourceState: "shown",
       },
     },
   },
-} satisfies Meta<typeof GlobalNotification>
-
-export const DefaultStory: StoryFn<typeof GlobalNotification> = args => (
-  <GlobalNotification {...args}>
-    Emails will be sent notifying coaches and inviting reviewers to give their
-    feedback. <a href="/">View all</a>
-  </GlobalNotification>
-)
-DefaultStory.storyName = "Global Notification"
-DefaultStory.args = {
-  type: "positive",
-  persistent: true,
 }
 
 const StickerSheetTemplate: StoryFn<{ isReversed: boolean }> = ({
   isReversed,
 }) => (
   <>
-    <StoryWrapper isReversed={isReversed}>
-      <StoryWrapper.RowHeader headings={["Persistent"]} />
-      <StoryWrapper.Row rowTitle="Positive">
+    <StickerSheet isReversed={isReversed}>
+      <StickerSheet.Header headings={["Persistent"]} />
+      <StickerSheet.Row rowTitle="Positive" isReversed={isReversed}>
         <GlobalNotification
           type="positive"
           automationId="notification1"
@@ -43,8 +70,8 @@ const StickerSheetTemplate: StoryFn<{ isReversed: boolean }> = ({
           Emails will be sent notifying coaches and inviting reviewers to give
           their feedback. <a href="/">View all</a>
         </GlobalNotification>
-      </StoryWrapper.Row>
-      <StoryWrapper.Row rowTitle="Informative">
+      </StickerSheet.Row>
+      <StickerSheet.Row rowTitle="Informative" isReversed={isReversed}>
         <GlobalNotification
           type="informative"
           automationId="notification2"
@@ -53,8 +80,8 @@ const StickerSheetTemplate: StoryFn<{ isReversed: boolean }> = ({
           [Survey name]'s status has been changed to 'Archived'.{" "}
           <a href="/">View all</a>
         </GlobalNotification>
-      </StoryWrapper.Row>
-      <StoryWrapper.Row rowTitle="Cautionary">
+      </StickerSheet.Row>
+      <StickerSheet.Row rowTitle="Cautionary" isReversed={isReversed}>
         <GlobalNotification
           type="cautionary"
           automationId="notification3"
@@ -63,8 +90,8 @@ const StickerSheetTemplate: StoryFn<{ isReversed: boolean }> = ({
           The syncing process can take some time to complete. Keep this window
           open until complete. <a href="/">View all</a>
         </GlobalNotification>
-      </StoryWrapper.Row>
-      <StoryWrapper.Row rowTitle="Negative">
+      </StickerSheet.Row>
+      <StickerSheet.Row rowTitle="Negative" isReversed={isReversed}>
         <GlobalNotification
           type="negative"
           automationId="notification4"
@@ -73,35 +100,35 @@ const StickerSheetTemplate: StoryFn<{ isReversed: boolean }> = ({
           Something went wrong while validating and analyzing user data.{" "}
           <a href="/">View all</a>.
         </GlobalNotification>
-      </StoryWrapper.Row>
-    </StoryWrapper>
-    <StoryWrapper isReversed={isReversed}>
-      <StoryWrapper.RowHeader headings={["Dismissible"]} />
-      <StoryWrapper.Row rowTitle="Positive">
+      </StickerSheet.Row>
+    </StickerSheet>
+    <StickerSheet isReversed={isReversed}>
+      <StickerSheet.Header headings={["Dismissible"]} />
+      <StickerSheet.Row rowTitle="Positive" isReversed={isReversed}>
         <GlobalNotification type="positive" automationId="notification1">
           Emails will be sent notifying coaches and inviting reviewers to give
           their feedback. <a href="/">View all</a>
         </GlobalNotification>
-      </StoryWrapper.Row>
-      <StoryWrapper.Row rowTitle="Informative">
+      </StickerSheet.Row>
+      <StickerSheet.Row rowTitle="Informative" isReversed={isReversed}>
         <GlobalNotification type="informative" automationId="notification2">
           [Survey name]'s status has been changed to 'Archived'.{" "}
           <a href="/">View all</a>
         </GlobalNotification>
-      </StoryWrapper.Row>
-      <StoryWrapper.Row rowTitle="Cautionary">
+      </StickerSheet.Row>
+      <StickerSheet.Row rowTitle="Cautionary" isReversed={isReversed}>
         <GlobalNotification type="cautionary" automationId="notification3">
           The syncing process can take some time to complete. Keep this window
           open until complete. <a href="/">View all</a>
         </GlobalNotification>
-      </StoryWrapper.Row>
-      <StoryWrapper.Row rowTitle="Negative">
+      </StickerSheet.Row>
+      <StickerSheet.Row rowTitle="Negative" isReversed={isReversed}>
         <GlobalNotification type="negative" automationId="notification4">
           Something went wrong while validating and analyzing user data.{" "}
           <a href="/">View all</a>.
         </GlobalNotification>
-      </StoryWrapper.Row>
-    </StoryWrapper>
+      </StickerSheet.Row>
+    </StickerSheet>
   </>
 )
 export const StickerSheetDefault = StickerSheetTemplate.bind({})
