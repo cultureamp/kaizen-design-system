dfc1c39b43573be3a44e45584c54c2b333f8f4c8
jakepitman <jakeseanp@gmail.com>
2023-04-13 13:03:31 +1200

    Update LoadingHeading docs

--- packages/loading-skeleton/docs/LoadingHeading.stories.tsx
+++ packages/loading-skeleton/docs/LoadingHeading.stories.tsx
@@ -1,82 +1,101 @@
-import React from "react"
-import { StoryFn } from "@storybook/react"
 import { Heading } from "@kaizen/typography"
-import { StoryWrapper } from "../../../storybook/components/StoryWrapper"
+import { StoryFn, StoryObj } from "@storybook/react"
+import React from "react"
 import { LoadingHeading } from ".."
+import { ComponentDocsTemplate } from "../../../storybook/components/DocsContainer"
+import { StickerSheet } from "../../../storybook/components/StickerSheet"
 
-export default {
+const meta = {
   tags: ["autodocs"],
   title: "Components/Loading/Loading Headings",
   component: LoadingHeading,
   parameters: {
     docs: {
-      description: {
-        component: 'import { LoadingHeading } from "@kaizen/loading-skeleton"',
+      container: ComponentDocsTemplate,
+    },
+    installation: [
+      "npm install @kaizen/loading-skeleton",
+      "import { LoadingHeading } from `@kaizen/loading-skeleton`",
+    ],
+    sourceCodeLink:
+      "https://github.com/cultureamp/kaizen-design-system/tree/main/packages/loading-skeleton/src/LoadingHeading",
+    figmaLink:
+      "https://www.figma.com/file/ZRfnoNUXbGZv4eVWLbF4Az/%F0%9F%96%BC%EF%B8%8F-Component-Gallery?node-id=5-353&t=zbqic2Cy8SwmCR94-0",
+  },
+}
+
+export default meta
+
+/**
+ *  The Loading Heading can be displayed in place of a heading while it is being loaded.
+ */
+export const Playground: StoryObj<typeof meta> = {
+  args: {
+    variant: "display-0",
+  },
+  parameters: {
+    docs: {
+      canvas: {
+        sourceState: "shown",
       },
     },
   },
 }
 
-export const DefaultLoadingHeading: StoryFn<typeof LoadingHeading> = args => (
-  <LoadingHeading {...args} />
-)
-DefaultLoadingHeading.storyName = "Loading Heading"
-DefaultLoadingHeading.args = { variant: "heading-1" }
-
 const StickerSheetTemplate: StoryFn<{ isReversed: boolean }> = ({
   isReversed,
 }) => (
-  <StoryWrapper isReversed={isReversed}>
-    <StoryWrapper.RowHeader headings={["Loading Skeleton", "Example"]} />
-    <StoryWrapper.Row rowTitle="Display 0">
+  <StickerSheet isReversed={isReversed}>
+    <StickerSheet.Header headings={["Loading Skeleton", "Example"]} />
+    <StickerSheet.Row rowTitle="Display 0" isReversed>
       <LoadingHeading variant="display-0" isReversed={isReversed} />
       <Heading variant="display-0" color={isReversed ? "white" : "dark"}>
         Display-0
       </Heading>
-    </StoryWrapper.Row>
-    <StoryWrapper.Row rowTitle="Heading 1">
+    </StickerSheet.Row>
+    <StickerSheet.Row rowTitle="Heading 1" isReversed>
       <LoadingHeading variant="heading-1" isReversed={isReversed} />
       <Heading variant="heading-1" color={isReversed ? "white" : "dark"}>
         Heading
       </Heading>
-    </StoryWrapper.Row>
-    <StoryWrapper.Row rowTitle="Heading 2">
+    </StickerSheet.Row>
+    <StickerSheet.Row rowTitle="Heading 2" isReversed>
       <LoadingHeading variant="heading-2" isReversed={isReversed} />
       <Heading variant="heading-2" color={isReversed ? "white" : "dark"}>
         Heading
       </Heading>
-    </StoryWrapper.Row>
-    <StoryWrapper.Row rowTitle="Heading 3">
+    </StickerSheet.Row>
+    <StickerSheet.Row rowTitle="Heading 3" isReversed>
       <LoadingHeading variant="heading-3" isReversed={isReversed} />
       <Heading variant="heading-3" color={isReversed ? "white" : "dark"}>
         Heading
       </Heading>
-    </StoryWrapper.Row>
-    <StoryWrapper.Row rowTitle="Heading 4">
+    </StickerSheet.Row>
+    <StickerSheet.Row rowTitle="Heading 4" isReversed>
       <LoadingHeading variant="heading-4" isReversed={isReversed} />
       <Heading variant="heading-4" color={isReversed ? "white" : "dark"}>
         Heading
       </Heading>
-    </StoryWrapper.Row>
-    <StoryWrapper.Row rowTitle="Heading 5">
+    </StickerSheet.Row>
+    <StickerSheet.Row rowTitle="Heading 5" isReversed>
       <LoadingHeading variant="heading-5" isReversed={isReversed} />
       <Heading variant="heading-5" color={isReversed ? "white" : "dark"}>
         Heading
       </Heading>
-    </StoryWrapper.Row>
-    <StoryWrapper.Row rowTitle="Heading 6">
+    </StickerSheet.Row>
+    <StickerSheet.Row rowTitle="Heading 6" isReversed>
       <LoadingHeading variant="heading-6" isReversed={isReversed} />
       <Heading variant="heading-6" color={isReversed ? "white" : "dark"}>
         Heading
       </Heading>
-    </StoryWrapper.Row>
-    <StoryWrapper.Row rowTitle="Heading Link">
+    </StickerSheet.Row>
+    <StickerSheet.Row rowTitle="Heading Link" isReversed>
       <LoadingHeading variant="heading-1" isLink isReversed={isReversed} />
       <Heading variant="heading-1" color={isReversed ? "white" : "dark"}>
         Heading Link
       </Heading>
-    </StoryWrapper.Row>
-  </StoryWrapper>
+    </StickerSheet.Row>
+  </StickerSheet>
 )
 
 export const StickerSheetDefault = StickerSheetTemplate.bind({})
