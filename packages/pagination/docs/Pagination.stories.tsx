97b70c6dd8ceb28f47f2b7ce99b44f82f3d15b5e
jakepitman <jakeseanp@gmail.com>
2023-04-14 14:35:00 +1200

    Update Pagination docs

--- packages/pagination/docs/Pagination.stories.tsx
+++ packages/pagination/docs/Pagination.stories.tsx
@@ -1,16 +1,27 @@
 import React, { useState } from "react"
 import { StoryFn } from "@storybook/react"
 import { Pagination } from "@kaizen/pagination"
+import { ComponentDocsTemplate } from "../../../storybook/components/DocsContainer"
 
-export default {
+const meta = {
   tags: ["autodocs"],
   title: "Components/Pagination",
   component: Pagination,
   parameters: {
     docs: {
-      description: {
-        component: 'import { Pagination } from "@kaizen/pagination";',
-      },
+      container: ComponentDocsTemplate,
+    },
+    installation: [
+      "npm install @kaizen/pagination",
+      "import { Pagination } from `@kaizen/pagination`",
+    ],
+    resourceLinks: {
+      sourceCode:
+        "https://github.com/cultureamp/kaizen-design-system/tree/main/packages/pagination",
+      figma:
+        "https://www.figma.com/file/ZRfnoNUXbGZv4eVWLbF4Az/%F0%9F%96%BC%EF%B8%8F-Component-Gallery?node-id=9-39913&t=PsW56BTLlG11eXLS-0",
+      designGuidelines:
+        "https://cultureamp.atlassian.net/wiki/spaces/DesignSystem/pages/3082092975/Pagination",
     },
   },
   decorators: [
@@ -28,18 +39,32 @@ export default {
   ],
 }
 
-export const Default: StoryFn<typeof Pagination> = args => {
-  const [currentPage, setCurrentPage] = useState(1)
+export default meta
 
+/**
+ * Pagination separates large bodies of content into separate pages.
+ * You can access each page via a shared index of links.
+ */
+export const Playground: StoryFn<typeof Pagination> = args => {
+  const [currentPage, setCurrentPage] = useState<number>(1)
   return (
     <Pagination
       {...args}
       currentPage={currentPage}
-      ariaLabelNextPage="Next page"
-      ariaLabelPreviousPage="Previous page"
-      pageCount={10}
       onPageChange={setCurrentPage}
     />
   )
 }
-Default.parameters = { chromatic: { disable: false } }
+
+Playground.args = {
+  ariaLabelNextPage: "Next page",
+  ariaLabelPreviousPage: "Previous page",
+  pageCount: 10,
+}
+Playground.parameters = {
+  docs: {
+    canvas: {
+      sourceState: "shown",
+    },
+  },
+}
