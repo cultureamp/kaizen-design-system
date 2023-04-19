dd165e87c432577513e57df7403ec43582331640
Geoffrey Chong <gyf.chong@gmail.com>
2023-04-19 11:35:40 +1000

    testing BK pipeline env

--- .buildkite/scripts/build-storybook.sh
+++ .buildkite/scripts/build-storybook.sh
@@ -4,13 +4,13 @@ set -e
 # shellcheck source=setup-registry.sh
 . ".buildkite/scripts/helpers/setup-registry.sh"
 
-# if [ "$FORCE_STORYBOOK_DEPLOY" = "true" ]; then
-#   echo "Force is with you"
-# elif [ "$FORCE_STORYBOOK_DEPLOY" = "false" ]; then
-#   echo "No force for you"
-# else
-#   echo "not found"
-# fi
+if [ "$FORCE_STORYBOOK_DEPLOY" = true ]; then
+  echo "Force is with you"
+elif [ "$FORCE_STORYBOOK_DEPLOY" = false ]; then
+  echo "No force for you"
+else
+  echo "not found"
+fi
 
 yarn install --frozen-lockfile
 yarn workspace @kaizen/design-tokens prepublish
