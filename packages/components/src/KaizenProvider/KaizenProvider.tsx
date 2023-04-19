fa7b5c420bd68c935997bc0b11f610bcfd62a639
jakepitman <jakeseanp@gmail.com>
2023-04-19 15:01:07 +1200

    Remove intl from KZ provider

--- packages/components/src/KaizenProvider/KaizenProvider.tsx
+++ packages/components/src/KaizenProvider/KaizenProvider.tsx
@@ -1,21 +1,16 @@
 import React from "react"
-import { IntlShape, RawIntlProvider } from "react-intl"
 import { ThemeProvider, ThemeManager } from "./ThemeProvider"
 
 export interface KaizenProviderProps {
   children: React.ReactNode
-  intlConfig: IntlShape
   themeManager?: ThemeManager
 }
 
 export const KaizenProvider = ({
   children,
-  intlConfig,
   themeManager,
 }: KaizenProviderProps): JSX.Element => (
-  <ThemeProvider themeManager={themeManager}>
-    <RawIntlProvider value={intlConfig}>{children}</RawIntlProvider>
-  </ThemeProvider>
+  <ThemeProvider themeManager={themeManager}>{children}</ThemeProvider>
 )
 
 KaizenProvider.displayName = "KaizenProvider"
