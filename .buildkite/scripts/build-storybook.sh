cd086ab2c1e26605c70ae9c4e508da2a4b7225c7
Geoffrey Chong <gyf.chong@gmail.com>
2023-04-19 11:17:27 +1000

    testing BK pipeline env

--- .buildkite/scripts/build-storybook.sh
+++ .buildkite/scripts/build-storybook.sh
@@ -4,6 +4,10 @@ set -e
 # shellcheck source=setup-registry.sh
 . ".buildkite/scripts/helpers/setup-registry.sh"
 
+echo "FORCE_STORYBOOK_DEPLOY: ${FORCE_STORYBOOK_DEPLOY}"
+echo "FORCE_STORYBOOK_DEPLOY: ${FORCE_STORYBOOK_DEPLOY == true}"
+echo "FORCE_STORYBOOK_DEPLOY: ${FORCE_STORYBOOK_DEPLOY == false}"
+
 yarn install --frozen-lockfile
 yarn workspace @kaizen/design-tokens prepublish
 yarn workspace @kaizen/tailwind prepublish
