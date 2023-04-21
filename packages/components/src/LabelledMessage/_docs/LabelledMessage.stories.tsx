40a59458d3c8197ee97c2471122f70e2641d989b
Cassandra Tam <cassandra.tam@cultureamp.com>
2023-04-21 13:45:41 +1000

    docs(labelled-message): update docs format

--- packages/components/src/LabelledMessage/_docs/LabelledMessage.stories.tsx
+++ packages/components/src/LabelledMessage/_docs/LabelledMessage.stories.tsx
@@ -1,47 +1,41 @@
-import React from "react"
-import { ComponentStory, Story } from "@storybook/react"
-import { StickerSheet } from "../../../../../storybook/components/StickerSheet"
+import { Meta, StoryObj } from "@storybook/react"
+import { ComponentDocsTemplate } from "../../../../../storybook/components/DocsContainer"
 import { LabelledMessage } from "../LabelledMessage"
 
-export default {
+const meta = {
+  tags: ["autodocs"],
   title: "Components/Labelled Message",
   component: LabelledMessage,
+  args: {
+    label: "Label",
+    message: "Custom message here",
+  },
   parameters: {
     docs: {
-      description: {
-        component: 'import { LabelledMessage } from "@kaizen/components"',
+      container: ComponentDocsTemplate,
+    },
+    installation: [
+      "npm install @kaizen/components",
+      'import { LabelledMessage } from "@kaizen/components"',
+    ],
+    resourceLinks: {
+      sourceCode:
+        "https://github.com/cultureamp/kaizen-design-system/tree/main/packages/components/src/LabelledMessage",
+    },
+  },
+} satisfies Meta<typeof LabelledMessage>
+
+export default meta
+
+/**
+ * A label and message with a separator in between. This component supports `dir="ltr"`.
+ */
+export const Playground: StoryObj<typeof meta> = {
+  parameters: {
+    docs: {
+      canvas: {
+        sourceState: "shown",
       },
     },
   },
-}
-
-export const DefaultStory: ComponentStory<typeof LabelledMessage> = args => (
-  <LabelledMessage {...args} />
-)
-DefaultStory.parameters = {
-  docs: { source: { type: "code" } },
-}
-DefaultStory.args = {
-  label: "Label",
-  message: "Custom message here",
-}
-
-const StickerSheetTemplate: Story = () => (
-  <StickerSheet>
-    <StickerSheet.Body>
-      <StickerSheet.Row rowTitle="LTR" dir="ltr">
-        <LabelledMessage label="Label" message="Custom message here" />
-      </StickerSheet.Row>
-      <StickerSheet.Row rowTitle="RTL" dir="rtl">
-        <LabelledMessage label="Label" message="Custom message here" />
-      </StickerSheet.Row>
-    </StickerSheet.Body>
-  </StickerSheet>
-)
-
-export const StickerSheetDefault = StickerSheetTemplate.bind({})
-StickerSheetDefault.storyName = "Sticker Sheet (Default)"
-StickerSheetDefault.parameters = {
-  chromatic: { disable: false },
-  controls: { disable: true },
 }
