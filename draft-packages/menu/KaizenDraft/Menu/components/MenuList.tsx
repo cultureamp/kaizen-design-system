a0ee7c9860daa84b6830b97dfb9d4f7549780072
Doug MacKenzie <doug.mackenzie@cultureamp.com>
2023-05-02 23:12:28 +1000

    Allow custom heading props on MenuList

--- draft-packages/menu/KaizenDraft/Menu/components/MenuList.tsx
+++ draft-packages/menu/KaizenDraft/Menu/components/MenuList.tsx
@@ -1,21 +1,29 @@
 import React, { useState } from "react"
 import { v4 } from "uuid"
+import { Heading, HeadingProps } from "@kaizen/typography"
 import styles from "./MenuList.module.scss"
 
 export type MenuListProps = {
   heading?: React.ReactNode
+  headingProps?: Omit<Partial<HeadingProps>, "children">
   children: React.ReactNode
 }
 
 export const MenuList = (props: MenuListProps): JSX.Element => {
   const [listHeadingID] = useState<string>(v4())
-  const { heading, children } = props
+  const { heading, headingProps, children } = props
   return (
     <>
       {heading && (
-        <span className={styles.header} id={listHeadingID}>
+        <Heading
+          variant="heading-6"
+          tag="span"
+          classNameOverride={styles.header}
+          id={listHeadingID}
+          {...headingProps}
+        >
           {heading}
-        </span>
+        </Heading>
       )}
       <ul
         className={styles.menuSection}
