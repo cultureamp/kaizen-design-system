f09c085ee78c6dc79afc291faa6e284b333d1e93
Doug MacKenzie <doug.mackenzie@cultureamp.com>
2023-05-03 11:27:14 +1000

    Remove dependency on focus-visible polyfill (#3558)
    * Use native focus-visible instead of polyfill class
    
    * Restore global focus-visible class selector to support showing focus on stickersheets
    
    * Remove focus-visible polyfill dep
    
    * Add missing ampersands
    
    * Remove global focus-visible class selectors and leave just story specific ones
    
    * Add changeset
    
    * Add missing ampersands and a couple other tidy ups
    
    * Restore focus styling for Calendar sticker sheet

--- storybook/preview.tsx
+++ storybook/preview.tsx
@@ -7,10 +7,6 @@ import { DefaultDocsContainer } from "./components/DocsContainer"
 
 import "highlight.js/styles/a11y-light.css"
 
-// Polyfill for :focus-visible pseudo-selector
-// See: https://github.com/WICG/focus-visible
-import "focus-visible"
-
 // Standard base stylesheet used across Culture Amp products
 // See: https://github.com/necolas/normalize.css/
 import "normalize.css"
