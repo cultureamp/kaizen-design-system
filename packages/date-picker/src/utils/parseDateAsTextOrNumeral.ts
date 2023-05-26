f3f36b34a1208ea34277d5d84087afcd889a656b
Christian Klammer <christian.klammer@cultureamp.com>
2023-05-26 11:04:08 +1000

    fix(DatePicker): Fixes regression in DatePicker marking valid dates as invalid

---
+++ packages/date-picker/src/utils/parseDateAsTextOrNumeral.ts
@@ -0,0 +1,12 @@
+import { parseDateFromNumeralFormatValue } from "./parseDateFromNumeralFormatValue"
+import { parseDateFromTextFormatValue } from "./parseDateFromTextFormatValue"
+
+export const parseDateAsTextOrNumeral = (
+  value: string,
+  locale: Locale
+): Date => {
+  const parsedAsText = parseDateFromTextFormatValue(value, locale)
+  if (parsedAsText.toString() !== "Invalid Date") return parsedAsText
+
+  return parseDateFromNumeralFormatValue(value, locale)
+}
