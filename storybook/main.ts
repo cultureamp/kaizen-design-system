3b436b3a864227f3855b3486b5973df942d882b0
Geoffrey Chong <gyf.chong@gmail.com>
2023-05-26 12:37:02 +1000

    use storybook sddon styling defaults for css and scss modules

--- storybook/main.ts
+++ storybook/main.ts
@@ -35,20 +35,7 @@ const config = {
         sass: {
           implementation: require("node-sass"),
         },
-        scssBuildRule: {
-          test: /\.module.scss$/,
-          use: [
-            {
-              loader: "css-loader",
-              options: {
-                sourceMap: true,
-                modules: {
-                  localIdentName: "[folder]-[name]__[local]--[hash:base64:5]",
-                },
-              },
-            },
-          ],
-        },
+        cssModules: true,
         postCss: {
           implementation: require("postcss"),
         },
