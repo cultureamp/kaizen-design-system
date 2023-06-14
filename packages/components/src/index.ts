b6191cfcd399490dd95ea168d4c7e47ce87441b3
Cassandra Tam <cassandra.tam@cultureamp.com>
2023-06-14 14:45:17 +1000

    refactor(filter-bar): use KAIO FilterMultiSelect

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -3,12 +3,10 @@ export * from "./FilterBar"
 export * from "./FilterButton"
 export * from "./FilterDatePicker"
 export * from "./FilterDateRangePicker"
+export * from "./FilterMultiSelect"
 export * from "./FilterSelect"
 export * from "./KaizenProvider"
 export * from "./Workflow"
-
-// TODO: Remove when FilterMultiSelect is migrated to KAIO
-export { FilterMultiSelect } from "@kaizen/select"
 
 // TODO: Remove when Date Picker is migrated to KAIO
 export * from "./types/DatePicker"
