bf0377b5128564e7940289206e932ed70895a959
Geoffrey Chong <gyf.chong@gmail.com>
2023-06-15 09:56:41 +1000

    update npx to dlx

--- .buildkite/scripts/run-playwright.sh
+++ .buildkite/scripts/run-playwright.sh
@@ -6,12 +6,12 @@ set -e
 
 if [ "$KAIZEN_DOMAIN_NAME" = "dev.cultureamp.design" ]; then
     echo "On dev branch for ${BUILDKITE_BRANCH}"
-    npx playwright install
+    pnpm dlx playwright install
 
     KAIZEN_DEV_BRANCH="${KAIZEN_DOMAIN_NAME}/${BUILDKITE_BRANCH}/"
 
     echo "--- :playwright: Running playwright on ${KAIZEN_DEV_BRANCH}"
-    URL=${KAIZEN_DEV_BRANCH} npx playwright test
+    URL=${KAIZEN_DEV_BRANCH} pnpm dlx playwright test
 else
     echo ":playwright: Playwright tests are only run on dev branches. Everything's good to go!"
 fi
