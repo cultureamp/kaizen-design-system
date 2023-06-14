519789048e4af0774144969280cf6ffc915a9514
jakepitman <jakeseanp@gmail.com>
2023-06-14 15:53:01 +1200

    Lint fixes

--- packages/components/src/KaizenProvider/KaizenProvider.tsx
+++ packages/components/src/KaizenProvider/KaizenProvider.tsx
@@ -1,23 +1,23 @@
 import React from "react"
-//import { OptionalIntlProvider } from "./OptionalIntlProvider"
+// import { OptionalIntlProvider } from "./OptionalIntlProvider"
 import { ThemeProvider, ThemeManager } from "./ThemeProvider"
 
 export interface KaizenProviderProps {
   children: React.ReactNode
   themeManager?: ThemeManager
-  locale?: string
+  // locale?: string
 }
 
 export const KaizenProvider = ({
   children,
   themeManager,
-  locale = "en",
-}: KaizenProviderProps): JSX.Element => (
+}: // locale = "en",
+KaizenProviderProps): JSX.Element => (
   // TODO: Add this back in once UH's webpack plugin is capable of
-  //       merging translation files.
-  //<OptionalIntlProvider locale={locale}>
+  // merging translation files.
+  // <OptionalIntlProvider locale={locale}>
   <ThemeProvider themeManager={themeManager}>{children}</ThemeProvider>
-  //</OptionalIntlProvider>
+  // </OptionalIntlProvider>
 )
 
 KaizenProvider.displayName = "KaizenProvider"
