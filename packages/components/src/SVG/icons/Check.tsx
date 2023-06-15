4b3d995770bca56cc13aae6376674b909008fc6d
Geoffrey Chong <gyf.chong@gmail.com>
2023-06-15 10:37:13 +1000

    add check icon

---
+++ packages/components/src/SVG/icons/Check.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { SVG, SVGProps } from "~components/SVG"
+
+export const Check = (props: Omit<SVGProps, "children">): JSX.Element => (
+  <SVG {...props}>
+    <path
+      fill="currentColor"
+      fillRule="evenodd"
+      d="m8.333 14.342-4.166-4.167L5.342 9l2.991 2.983 6.325-6.325 1.175 1.184z"
+    />
+  </SVG>
+)
