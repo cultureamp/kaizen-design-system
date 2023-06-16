62c302774c981843613fc0f4c554345c70222350
Michael Winter <36558508+mcwinter07@users.noreply.github.com>
2023-06-16 13:58:15 +1000

    Remove dep on component library within KAIO (#3689)
    * run yarn and update lockfile
    
    * Remove component-lib icons and dep
    
    * update stroybook build script
    
    * remove component-library from root deps + pre-build script
    
    * fix SVG test
    
    * update changeset
    
    * update changeset
    
    * fix SVG tests
    
    * add KAIO Button version
    
    * add changeset
    
    * update yarn lock
    
    * add stickersheet to ensure the icons work + variants in KAIO button
    
    * fix button SCSS imports
    
    ---------
    
    Co-authored-by: Geoffrey Chong <gyf.chong@gmail.com>
    Co-authored-by: Geoffrey Chong <gyfchong@users.noreply.github.com>

---
+++ .changeset/kind-ears-mate.md
@@ -0,0 +1,6 @@
+---
+"@kaizen/components": patch
+---
+
+- Remove `component-library` as a dependency
+- Add `SVG` title to `aria-label` when it is meanigful.
---
+++ packages/components/src/SVG/icons/CloseIcon.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import { SVG, SVGProps } from "~components/SVG"
+
+export const CloseIcon = (props: Omit<SVGProps, "children">): JSX.Element => (
+  <SVG {...props}>
+    <path
+      fillRule="evenodd"
+      fill="currentColor"
+      d="M14.654 4.167 10 8.82 5.346 4.167l-1.18 1.18L8.823 10l-4.655 4.655 1.179 1.178L10 11.18l4.654 4.654 1.18-1.178L11.18 10l4.655-4.653z"
+    />
+  </SVG>
+)
+
+CloseIcon.displayName = "CloseIcon"
