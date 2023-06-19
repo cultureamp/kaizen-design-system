e3d362de9c3438042a271b2bc7bcb1d4e3b95839
Michael Winter <mcwinter07@gmail.com>
2023-06-19 11:17:35 +1000

    add test coverage for progress stepper

--- packages/components/src/Workflow/subcomponents/Footer/components/ProgressStepper/ProgressStepper.spec.tsx
+++ packages/components/src/Workflow/subcomponents/Footer/components/ProgressStepper/ProgressStepper.spec.tsx
@@ -1,11 +1,8 @@
-import React, { useState } from "react"
-import { render, screen, waitFor } from "@testing-library/react"
-import userEvent from "@testing-library/user-event"
-import { Step, ProgressStepper, ProgressStepperProps } from "./"
+import React from "react"
+import { render, screen } from "@testing-library/react"
+import { ProgressStepper, ProgressStepperProps } from "./"
 
 const defaultArgs = {
-  workflowName: "Create a self-reflection cycle",
-  currentStepId: "settings-step",
   steps: [
     { label: "Settings", id: "settings-step" },
     { label: "Questions", id: "questions-step" },
@@ -13,5 +10,36 @@ const defaultArgs = {
     { label: "Employees", id: "employees-step" },
     { label: "Schedule", id: "schedule-step" },
   ],
-  isComplete: false,
-}
+} satisfies Pick<ProgressStepperProps, "steps">
+
+describe("<ProgressStepperr />", () => {
+  it("renders the correct number of steps", () => {
+    render(<ProgressStepper currentStepId="settings-step" {...defaultArgs} />)
+    expect(screen.getAllByRole("listitem").length).toBe(5)
+  })
+  it("tracks the correct position of the active step", () => {
+    render(<ProgressStepper currentStepId="settings-step" {...defaultArgs} />)
+    screen.getByText("Current: Settings")
+    expect(
+      screen.getByText(`Step 1 of ${defaultArgs.steps.length}`)
+    ).toBeInTheDocument()
+  })
+  it("renders the correct number of completed steps with an accessible name", () => {
+    render(<ProgressStepper currentStepId="preview-step" {...defaultArgs} />)
+    expect(screen.getAllByText("Completed:", { exact: false }).length).toBe(2)
+  })
+  it("renders the correct number of steps not started with an accessible name", () => {
+    render(<ProgressStepper currentStepId="preview-step" {...defaultArgs} />)
+    expect(screen.getAllByText("Not started:", { exact: false }).length).toBe(2)
+  })
+  it("renders all steps as complete if the provided the `isComplete` prop", () => {
+    render(
+      <ProgressStepper
+        currentStepId="preview-step"
+        {...defaultArgs}
+        isComplete
+      />
+    )
+    expect(screen.getAllByText("Completed:", { exact: false }).length).toBe(5)
+  })
+})
