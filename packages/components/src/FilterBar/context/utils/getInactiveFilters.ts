81b7d633dac38715134b68d8ddf49c0191db9d05
Cassandra Tam <cassandra.tam@cultureamp.com>
2023-06-20 18:47:32 +1000

    fix(filter-bar): fix getInactiveFilters return type

--- packages/components/src/FilterBar/context/utils/getInactiveFilters.ts
+++ packages/components/src/FilterBar/context/utils/getInactiveFilters.ts
@@ -1,7 +1,7 @@
-import { FilterBarState, FilterState, FiltersValues } from "../types"
+import { FilterBarState, FilterBarStateFilters, FiltersValues } from "../types"
 
 export const getInactiveFilters = <ValuesMap extends FiltersValues>({
   filters,
   activeFilterIds,
-}: FilterBarState<ValuesMap>): Array<FilterState<keyof ValuesMap, ValuesMap>> =>
+}: FilterBarState<ValuesMap>): Array<FilterBarStateFilters<ValuesMap>> =>
   Object.values(filters).filter(({ id }) => !activeFilterIds.has(id))
