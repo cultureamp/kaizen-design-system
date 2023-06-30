c32837fafafd1c9e61e28ef1df098fea0fa15cdc
jakepitman <jakeseanp@gmail.com>
2023-06-30 16:18:05 +1200

    Translate input format message

--- packages/components/src/FilterDateRangePicker/subcomponents/DateInputDescription/DateInputDescription.tsx
+++ packages/components/src/FilterDateRangePicker/subcomponents/DateInputDescription/DateInputDescription.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import { LabelledMessage } from "~components/LabelledMessage"
 import { formatDescriptionInputFormat } from "./utils/formatDescriptionInputFormat"
 import styles from "./DateInputDescription.module.scss"
+import { useIntl } from "@cultureamp/i18n-react-intl"
 
 export interface DateInputDescriptionProps {
   description?: string | JSX.Element
@@ -12,9 +13,16 @@ export const DateInputDescription = ({
   description,
   locale,
 }: DateInputDescriptionProps): JSX.Element => {
+  const { formatMessage } = useIntl()
   const hasCustomDescription =
     (typeof description === "string" && description !== "") ||
     React.isValidElement(description)
+
+  const translatedInputFormatMessage = formatMessage({
+    id: "filterDateRangePicker.inputFormat",
+    defaultMessage: "Default date-from message",
+    description: "Description for date-from",
+  })
 
   if (hasCustomDescription) {
     return (
@@ -23,7 +31,7 @@ export const DateInputDescription = ({
         <span className={styles.dateInputFormatContainer}>
           (
           <LabelledMessage
-            label="Input format"
+            label={translatedInputFormatMessage}
             message={formatDescriptionInputFormat(locale)}
           />
           )
@@ -34,7 +42,7 @@ export const DateInputDescription = ({
 
   return (
     <LabelledMessage
-      label="Input format"
+      label={translatedInputFormatMessage}
       message={formatDescriptionInputFormat(locale)}
     />
   )
