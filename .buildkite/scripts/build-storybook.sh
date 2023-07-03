48cc530278a8e051e4b3cffee210736ce04fa55f
Geoffrey Chong <gyf.chong@gmail.com>
2023-07-03 12:05:42 +1000

    remove test commands

--- .buildkite/scripts/build-storybook.sh
+++ .buildkite/scripts/build-storybook.sh
@@ -4,21 +4,7 @@ set -e
 # shellcheck source=setup-registry.sh
 . ".buildkite/scripts/helpers/setup-registry.sh"
 
-setup_npm() {
-  npm config set update-notifier false
-  npm config set email service-npm@cultureamp.com
-  npm config set //registry.npmjs.org/:_auth cultureamp-user
-  npm config set //registry.npmjs.org/:_authToken "$NPM_TOKEN"
-  npm config set access public
-
-  echo "Checking npm authentication..."
-  echo "Logged in as: $(npm whoami)"
-}
-
-setup_npm
-
 yarn install --frozen-lockfile
-yarn build
 yarn workspace @kaizen/design-tokens prepublish
 yarn workspace @kaizen/tailwind prepublish
 yarn storybook:build
