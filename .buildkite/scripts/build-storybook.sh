dec96288aaa936812ea947bbfebab5b8df0bce9a
Geoffrey Chong <gyf.chong@gmail.com>
2023-07-03 11:50:34 +1000

    add test setup_npm to build

--- .buildkite/scripts/build-storybook.sh
+++ .buildkite/scripts/build-storybook.sh
@@ -4,6 +4,19 @@ set -e
 # shellcheck source=setup-registry.sh
 . ".buildkite/scripts/helpers/setup-registry.sh"
 
+setup_npm() {
+  npm config set update-notifier false
+  npm config set email service-npm@cultureamp.com
+  npm config set _auth cultureamp-user
+  npm config set //registry.npmjs.org/:_authToken "$NPM_TOKEN"
+  npm config set access public
+
+  echo "Checking npm authentication..."
+  echo "Logged in as: $(npm whoami)"
+}
+
+setup_npm
+
 yarn install --frozen-lockfile
 yarn workspace @kaizen/design-tokens prepublish
 yarn workspace @kaizen/tailwind prepublish
