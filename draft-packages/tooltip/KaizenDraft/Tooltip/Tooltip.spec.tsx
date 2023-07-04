fbc0ae55e42f7b16df4ef40c8b216917e66f3a31
Michael Winter <mcwinter07@gmail.com>
2023-07-04 10:08:25 +1000

    increase test coverage for aria-describedby scenarios

--- draft-packages/tooltip/KaizenDraft/Tooltip/Tooltip.spec.tsx
+++ draft-packages/tooltip/KaizenDraft/Tooltip/Tooltip.spec.tsx
@@ -1,24 +1,20 @@
 import React from "react"
 import { render, screen, waitFor } from "@testing-library/react"
 import { Button } from "@kaizen/button"
-import { TextArea } from "@kaizen/draft-form"
 import { Tooltip } from "./index"
 
 describe("<Tooltip />", () => {
-  describe("Accessible descriptions are passed only to interactive elements", () => {
+  describe("Accessible descriptions", () => {
     it("will render a button will have an accessible description", async () => {
       render(
-        <div id="portal-id" style={{ width: "500px", height: "500px" }}>
-          <Tooltip
-            text="Tooltip popup description for button"
-            display="inline"
-            isInitiallyVisible
-            position="below"
-          >
-            <Button label="More info" />
-          </Tooltip>
-          thing
-        </div>
+        <Tooltip
+          text="Tooltip popup description for button"
+          display="inline"
+          isInitiallyVisible
+          position="below"
+        >
+          <Button label="More info" />
+        </Tooltip>
       )
 
       await waitFor(() => {
@@ -27,41 +23,37 @@ describe("<Tooltip />", () => {
         ).toHaveAccessibleDescription("Tooltip popup description for button")
       })
     })
-    it("will render an input with an accessible description", async () => {
+
+    it("will render a button without an aria-describedby if the tooltip is not active", async () => {
       render(
-        <div id="portal-id" style={{ width: "500px", height: "500px" }}>
-          <Tooltip
-            text="Tooltip popup description for input"
-            display="inline"
-            isInitiallyVisible
-            position="below"
-          >
-            <TextArea />
-          </Tooltip>
-          thing
-        </div>
+        <Tooltip
+          text="Tooltip popup description for button"
+          display="inline"
+          position="below"
+        >
+          <Button label="More info" />
+        </Tooltip>
       )
 
       await waitFor(() => {
-        expect(screen.getByRole("textbox")).toHaveAccessibleDescription(
-          "Tooltip popup description for input"
-        )
+        expect(screen.queryByRole("tooltip")).toBe(null)
+        expect(
+          screen.getByRole("button", { name: "More info" })
+        ).not.toHaveAttribute("aria-describedby")
       })
     })
-    // Non-semantic elements without roles should not have aria-description on them - they will not read to screen readers
-    it("will render non-semantic elements without aria-describedby", async () => {
+
+    // Non-semantic elements without roles should not have aria-description on them - they will not read to screen readers and will throw an error
+    it("will render non-semantic elements without accessible descriptions", async () => {
       render(
-        <div id="portal-id" style={{ width: "500px", height: "500px" }}>
-          <Tooltip
-            text="Tooltip popup description for div"
-            display="inline"
-            isInitiallyVisible
-            position="below"
-          >
-            <div>Non somantic element</div>
-          </Tooltip>
-          thing
-        </div>
+        <Tooltip
+          text="Tooltip popup description for div"
+          display="inline"
+          isInitiallyVisible
+          position="below"
+        >
+          <div>Non somantic element</div>
+        </Tooltip>
       )
       await waitFor(() => {
         expect(screen.getByText("Non somantic element")).not.toHaveAttribute(
@@ -70,13 +62,22 @@ describe("<Tooltip />", () => {
       })
     })
   })
+
+  it("will render elements with semantic roles with accessible descriptions", async () => {
+    render(
+      <Tooltip
+        text="Tooltip popup description for div"
+        display="inline"
+        isInitiallyVisible
+        position="below"
+      >
+        <div role="textbox" contentEditable="true" aria-multiline="true"></div>
+      </Tooltip>
+    )
+    await waitFor(() => {
+      expect(screen.getByRole("textbox")).toHaveAccessibleDescription(
+        "Tooltip popup description for div"
+      )
+    })
+  })
 })
-// it("will render a button with the aria-describedby attribute with undefined when closed", () => {
-//   expect(true).toBe(true)
-// })
-// it("will render a component with an accessible role with the aria-describedby attribute when open", () => {
-//   expect(true).toBe(true)
-// })
-// it("will not render a non-semantic div with an aria-describedby", () => {
-//   expect(true).toBe(true)
-// })
