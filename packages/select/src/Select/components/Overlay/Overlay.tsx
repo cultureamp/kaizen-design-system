fd4037d654ecaeb1679a8b7eca445b131767d62e
Josh Li <joshua.li@cultureamp.com>
2023-07-06 09:25:15 +1000

    fix: remove Aria overlay, implement own popover

--- packages/select/src/Select/components/Overlay/Overlay.tsx
+++ packages/select/src/Select/components/Overlay/Overlay.tsx
@@ -1,11 +1,8 @@
 /* eslint-disable jsx-a11y/no-autofocus */
 import React from "react"
+import ReactDOM from "react-dom"
 import { FocusScope } from "@react-aria/focus"
-import {
-  Overlay as ReactAriaOverlay,
-  DismissButton,
-  usePopover,
-} from "@react-aria/overlays"
+import { DismissButton, usePopover, useOverlay } from "@react-aria/overlays"
 import { useSelectContext } from "../../context/SelectContext"
 import styles from "./Overlay.module.scss"
 
@@ -23,6 +20,7 @@ export const Overlay = ({
   const { state } = useSelectContext()
 
   const popoverRef = React.useRef<HTMLDivElement>(null)
+  const overlayRef = React.useRef<HTMLDivElement>(null)
   const { popoverProps } = usePopover(
     {
       triggerRef,
@@ -31,28 +29,38 @@ export const Overlay = ({
     },
     state
   )
+  const { overlayProps } = useOverlay(
+    {
+      isDismissable: true,
+      isOpen: state.isOpen,
+      onClose: state.close,
+      shouldCloseOnBlur: false,
+    },
+    overlayRef
+  )
   const { width } = triggerRef.current?.getBoundingClientRect() || {}
 
   // Wrap in <FocusScope> so that focus is restored back to the trigger when the menu is closed
   // and auto focus on the first focusable item after loading. (disable eslint no-autofocus error for it)
   // In addition, add hidden <DismissButton> components at the start and end of the list
   // to allow screen reader users to dismiss the popup easily.
-  return (
-    <ReactAriaOverlay>
-      <div
-        {...popoverProps}
-        ref={popoverRef}
-        style={{ ...popoverProps.style, width }}
-        className={styles.menuPopup}
-      >
-        <FocusScope contain autoFocus restoreFocus>
+  const contents = (
+    <div {...overlayProps} ref={overlayRef}>
+      <FocusScope contain autoFocus restoreFocus>
+        <div
+          {...popoverProps}
+          ref={popoverRef}
+          style={{ ...popoverProps.style, width }}
+          className={styles.menuPopup}
+        >
           <DismissButton onDismiss={state.close} />
           {children}
           <DismissButton onDismiss={state.close} />
-        </FocusScope>
-      </div>
-    </ReactAriaOverlay>
+        </div>
+      </FocusScope>
+    </div>
   )
+  return ReactDOM.createPortal(contents, document.body)
 }
 
 Overlay.displayName = "Overlay"
