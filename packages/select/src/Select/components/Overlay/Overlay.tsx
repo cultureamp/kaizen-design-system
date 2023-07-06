3251bcd8c128b771e818270ec8b477468124c4c1
Josh Li <joshua.li@cultureamp.com>
2023-07-06 13:47:13 +1000

    fix: swap order of wrappers to fix esc key event;

--- packages/select/src/Select/components/Overlay/Overlay.tsx
+++ packages/select/src/Select/components/Overlay/Overlay.tsx
@@ -2,7 +2,7 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import { FocusScope } from "@react-aria/focus"
-import { DismissButton, usePopover, useOverlay } from "@react-aria/overlays"
+import { DismissButton, usePopover } from "@react-aria/overlays"
 import { useSelectContext } from "../../context/SelectContext"
 import styles from "./Overlay.module.scss"
 
@@ -20,43 +20,32 @@ export const Overlay = ({
   const { state } = useSelectContext()
 
   const popoverRef = React.useRef<HTMLDivElement>(null)
-  const overlayRef = React.useRef<HTMLDivElement>(null)
   const { popoverProps } = usePopover(
     {
       triggerRef,
       popoverRef,
+      isNonModal: true,
       placement: "bottom left",
     },
     state
   )
-  const { overlayProps } = useOverlay(
-    {
-      isDismissable: true,
-      isOpen: state.isOpen,
-      onClose: state.close,
-      shouldCloseOnBlur: false,
-    },
-    overlayRef
-  )
-  const { width } = triggerRef.current?.getBoundingClientRect() || {}
 
+  const { width } = triggerRef.current?.getBoundingClientRect() || {}
   // Wrap in <FocusScope> so that focus is restored back to the trigger when the menu is closed
   // and auto focus on the first focusable item after loading. (disable eslint no-autofocus error for it)
   // In addition, add hidden <DismissButton> components at the start and end of the list
   // to allow screen reader users to dismiss the popup easily.
   const contents = (
-    <div {...overlayProps} ref={overlayRef}>
+    <div
+      {...popoverProps}
+      ref={popoverRef}
+      style={{ ...popoverProps.style, width }}
+      className={styles.menuPopup}
+    >
       <FocusScope contain autoFocus restoreFocus>
-        <div
-          {...popoverProps}
-          ref={popoverRef}
-          style={{ ...popoverProps.style, width }}
-          className={styles.menuPopup}
-        >
-          <DismissButton onDismiss={state.close} />
-          {children}
-          <DismissButton onDismiss={state.close} />
-        </div>
+        <DismissButton onDismiss={state.close} />
+        {children}
+        <DismissButton onDismiss={state.close} />
       </FocusScope>
     </div>
   )
