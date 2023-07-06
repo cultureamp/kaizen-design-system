c329629ec91cccb4e71957a804d65ba816efb3a3
Cassandra Tam <cassandra.tam@cultureamp.com>
2023-07-06 16:35:56 +1000

    docs(filter-bar): update code snippet for sibling dep filter story

--- packages/components/src/FilterBar/_docs/FilterBar.stories.tsx
+++ packages/components/src/FilterBar/_docs/FilterBar.stories.tsx
@@ -319,20 +319,20 @@ const ExampleFilterMultiSelect = (
 ): JSX.Element => (
   <FilterBar.MultiSelect {...props}>
     {(): JSX.Element => (
-      <>
-        <FilterMultiSelect.SearchInput />
-        <FilterMultiSelect.ListBox>
-          {({ allItems }): JSX.Element | JSX.Element[] =>
-            allItems.map(item => (
-              <FilterMultiSelect.Option key={item.key} item={item} />
-            ))
+      <FilterMultiSelect.ListBox>
+        {({ allItems }): JSX.Element | JSX.Element[] => {
+          if (allItems.length === 0) {
+            return (
+              <FilterMultiSelect.NoResults>
+                No results. Select a role first.
+              </FilterMultiSelect.NoResults>
+            )
           }
-        </FilterMultiSelect.ListBox>
-        <FilterMultiSelect.MenuFooter>
-          <FilterMultiSelect.SelectAllButton />
-          <FilterMultiSelect.ClearButton />
-        </FilterMultiSelect.MenuFooter>
-      </>
+          return allItems.map(item => (
+            <FilterMultiSelect.Option key={item.key} item={item} />
+          ))
+        }}
+      </FilterMultiSelect.ListBox>
     )}
   </FilterBar.MultiSelect>
 )
@@ -341,28 +341,6 @@ type ValuesSiblingDependent = {
   role: string[]
   person: string[]
   room: string
-}
-
-const FilterRole = (props: { id?: string }): JSX.Element => {
-  const data = [
-    { value: "designer", label: "Designer" },
-    { value: "engineer", label: "Engineer" },
-  ]
-
-  const [items, setItems] = useState<ItemType[]>(data)
-  const [searchTerm, setSearchTerm] = useState<string>("")
-
-  useEffect(() => {
-    setItems(data.filter(({ label }) => label.includes(searchTerm)))
-  }, [searchTerm])
-
-  return (
-    <ExampleFilterMultiSelect
-      id={props.id}
-      items={items}
-      onSearchInputChange={setSearchTerm}
-    />
-  )
 }
 
 const sleep = (ms: number): Promise<unknown> =>
@@ -393,9 +371,8 @@ const FilterPerson = (props: { id?: string }): JSX.Element => {
     },
   ]
 
-  const [allItems, setAllItems] = useState<ItemType[]>([])
+  const [items, setItems] = useState<ItemType[]>([])
   const [isLoading, setIsLoading] = useState<boolean>(true)
-  const [searchTerm, setSearchTerm] = useState<string>("")
 
   const { getFilterState } = useFilterBarContext<
     ValuesSiblingDependent["person"],
@@ -407,7 +384,7 @@ const FilterPerson = (props: { id?: string }): JSX.Element => {
   const loadItems = async (roles: string[] | undefined): Promise<void> => {
     await sleep(3000).then(() => {
       setIsLoading(false)
-      setAllItems(data.filter(({ role }) => role.find(r => roles?.includes(r))))
+      setItems(data.filter(({ role }) => role.find(r => roles?.includes(r))))
     })
   }
 
@@ -416,15 +393,12 @@ const FilterPerson = (props: { id?: string }): JSX.Element => {
     loadItems(roleFilter.value)
   }, [roleFilter.value])
 
-  const items = allItems.filter(({ label }) => label.includes(searchTerm))
-
   return (
     <ExampleFilterMultiSelect
       id={props.id}
       isLoading={isLoading}
       loadingSkeleton={<FilterMultiSelect.MenuLoadingSkeleton />}
       items={items}
-      onSearchInputChange={setSearchTerm}
     />
   )
 }
@@ -468,7 +442,14 @@ export const SiblingValueDependentFilter: StoryFn<typeof FilterBar> = () => {
     {
       id: "role",
       name: "Role",
-      Component: <FilterRole />,
+      Component: (
+        <ExampleFilterMultiSelect
+          items={[
+            { value: "designer", label: "Designer" },
+            { value: "engineer", label: "Engineer" },
+          ]}
+        />
+      ),
     },
     {
       id: "person",
@@ -496,6 +477,113 @@ export const SiblingValueDependentFilter: StoryFn<typeof FilterBar> = () => {
       <Highlight className="json">{JSON.stringify(values, null, 4)}</Highlight>
     </>
   )
+}
+const sourceCodeSiblingValueDependentFilter = `
+type Values = {
+  role: string[]
+  person: string[]
+  room: string
+}
+
+const sleep = (ms: number): Promise<unknown> => new Promise(resolve => setTimeout(resolve, ms))
+
+const FilterPerson = (props: { id?: string }): JSX.Element => {
+  const data = [
+    { value: "delete-it-g", label: "Delete-it G (Engineer)", role: ["engineer"] },
+    { value: "moustache-mackenzie", label: "Moustache MacKenzie (Engineer)", role: ["engineer"] },
+    { value: "jacon", label: "Jacon (Designer)", role: ["designer"] },
+    { value: "uppercase-winter", label: "Uppercase Winter (Engineer)", role: ["engineer"] },
+    { value: "unicorn", label: "Unicorn (Designer/Engineer)", role: ["designer", "engineer"] },
+  ]
+
+  const [items, setItems] = useState<ItemType[]>([])
+  const [isLoading, setIsLoading] = useState<boolean>(true)
+
+  const { getFilterState } = useFilterBarContext<Values["person"], Values>()
+
+  const roleFilter = getFilterState("role")
+
+  const loadItems = async (roles: string[] | undefined): Promise<void> => {
+    await sleep(3000).then(() => {
+      setIsLoading(false)
+      setItems(data.filter(({ role }) => role.find(r => roles?.includes(r))))
+    })
+  }
+
+  useEffect(() => {
+    setIsLoading(true)
+    loadItems(roleFilter.value)
+  }, [roleFilter.value])
+
+  return (
+    <FilterBar.MultiSelect
+      id={props.id}
+      isLoading={isLoading}
+      loadingSkeleton={<FilterMultiSelect.MenuLoadingSkeleton />}
+      items={items}
+    >{...renderChildren}</FilterBar.MultiSelect>
+  )
+}
+
+const FilterRoom = (props: { id?: string }): JSX.Element => {
+  type Item = SelectOption & { role: string }
+
+  const data = [
+    { value: "eng-1", label: "Engineering Space 1", role: "engineer" },
+    { value: "eng-2", label: "Engineering Space 2", role: "engineer" },
+    { value: "des-1", label: "Design Space 1", role: "designer" },
+  ]
+
+  const [items, setItems] = useState<Item[]>([])
+
+  const { getFilterState } = useFilterBarContext<Values["room"], Values>()
+
+  const roleFilter = getFilterState("role")
+
+  useEffect(() => {
+    setItems(data.filter(({ role }) => roleFilter.value?.includes(role)))
+  }, [roleFilter.value])
+
+  return <FilterBar.Select<Item> id={props.id} items={items} />
+}
+
+const CustomFilterBar = () => {
+  const filters = [
+    {
+      id: "role",
+      name: "Role",
+      Component: (<FilterBar.MultiSelect
+        items={[
+          { value: "designer", label: "Designer" },
+          { value: "engineer", label: "Engineer" },
+        ]}
+      >{...renderChildren}</FilterBar.MultiSelect>),
+    },
+    {
+      id: "person",
+      name: "Person",
+      Component: <FilterPerson />,
+    },
+    {
+      id: "room",
+      name: "Room",
+      Component: <FilterRoom />,
+      isRemovable: true,
+      isUsableWhen: state => state.role.value !== undefined,
+    },
+  ] satisfies Filters<Values>
+
+  const [values, setValues] = useState<Partial<Values>>({})
+
+  return <FilterBar<Values> filters={filters} values={values} onValuesChange={setValues} />
+}
+`
+SiblingValueDependentFilter.parameters = {
+  docs: {
+    source: {
+      code: sourceCodeSiblingValueDependentFilter,
+    },
+  },
 }
 
 export const ExternalEventValuesUpdate: StoryFn<typeof FilterBar> = () => {
