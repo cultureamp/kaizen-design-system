d0e373186207bc2811f36e942944231a6098cb0d
jakepitman <jakeseanp@gmail.com>
2023-07-07 16:54:57 +1200

    Add global mock for formatMessage

--- setupTests.ts
+++ setupTests.ts
@@ -2,3 +2,10 @@ import "@testing-library/jest-dom/extend-expect"
 
 /** @ts-ignore */
 global.IS_REACT_ACT_ENVIRONMENT = true
+
+jest.mock("@cultureamp/i18n-react-intl", () => ({
+  useIntl: () => ({
+    formatMessage: (options: { defaultMessage: string }) =>
+      options.defaultMessage,
+  }),
+}))
