a33c20dacaa46f682e01b679fa858d7566a80466
jakepitman <jakeseanp@gmail.com>
2023-07-07 17:03:14 +1200

    Add comment explaining global mock

--- setupTests.ts
+++ setupTests.ts
@@ -3,6 +3,8 @@ import "@testing-library/jest-dom/extend-expect"
 /** @ts-ignore */
 global.IS_REACT_ACT_ENVIRONMENT = true
 
+// This avoids errors related to our jest environment not having an Intl
+// object present in the app's context
 jest.mock("@cultureamp/i18n-react-intl", () => ({
   useIntl: () => ({
     formatMessage: (options: { defaultMessage: string }) =>
