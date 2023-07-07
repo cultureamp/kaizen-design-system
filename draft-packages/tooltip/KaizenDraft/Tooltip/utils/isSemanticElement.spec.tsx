979a85b0d7d0e82809a539263cc4bcca41578537
Michael Winter <mcwinter07@gmail.com>
2023-07-07 11:41:37 +1000

    update semantic element util

--- draft-packages/tooltip/KaizenDraft/Tooltip/utils/isSemanticElement.spec.tsx
+++ draft-packages/tooltip/KaizenDraft/Tooltip/utils/isSemanticElement.spec.tsx
@@ -1,39 +1,44 @@
 import React from "react"
 import { Button, IconButton } from "@kaizen/button"
-import { hasSemanticRole } from "./hasSemanticRole"
+import { isSemanticElement } from "./isSemanticElement"
 
-describe("hasSemanticRole", () => {
+describe("isSemanticElement", () => {
   it("will return true if provided a native element with a semantic role", () => {
     expect(
-      hasSemanticRole(
+      isSemanticElement(
         <button onClick={jest.fn} type="button">
           click
         </button>
       )
     ).toBe(true)
-    expect(hasSemanticRole(<a href="/">link</a>)).toBe(true)
+    expect(isSemanticElement(<a href="/">link</a>)).toBe(true)
   })
   it("will return false if provided a native element without a semantic role", () => {
-    expect(hasSemanticRole(<span>click</span>)).toBe(false)
-    expect(hasSemanticRole(<div>link</div>)).toBe(false)
+    expect(isSemanticElement(<span>click</span>)).toBe(false)
+    expect(isSemanticElement(<div>link</div>)).toBe(false)
   })
   it("will return true if provided a custom elements with a semantic role", () => {
-    expect(hasSemanticRole(<Button label="Click" />)).toBe(true)
-    expect(hasSemanticRole(<IconButton label="click" />)).toBe(true)
+    expect(isSemanticElement(<Button label="Click" />)).toBe(true)
+    expect(isSemanticElement(<IconButton label="click" />)).toBe(true)
   })
-  it("will return true if provided a non-semantic elements with a semantic role", () => {
+  it("will return true if provided a non-semantic element with a semantic role", () => {
     expect(
-      hasSemanticRole(
+      isSemanticElement(
         <span tabIndex={0} role="button" onKeyDown={jest.fn} onClick={jest.fn}>
           custom semantic el
         </span>
       )
     ).toBe(true)
     expect(
-      hasSemanticRole(
+      isSemanticElement(
         <div tabIndex={0} role="button" onKeyDown={jest.fn} onClick={jest.fn}>
           custom semantic el
         </div>
+      )
+    ).toBe(true)
+    expect(
+      isSemanticElement(
+        <div role="textbox" contentEditable="true" aria-multiline="true" />
       )
     ).toBe(true)
   })
