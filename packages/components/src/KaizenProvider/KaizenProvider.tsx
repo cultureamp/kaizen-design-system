4388ba0488eea74434b81bed4295142be9e2c30c
JakePitman <jakeseanp@gmail.com>
2023-07-10 14:19:45 +1200

    Revert "Remove optional intl provider and update docs (#3850)"
    This reverts commit 713427924bec6b2c7f81b90037360b62ae8ad8ce.

--- packages/components/src/KaizenProvider/KaizenProvider.tsx
+++ packages/components/src/KaizenProvider/KaizenProvider.tsx
@@ -1,16 +1,21 @@
 import React from "react"
+import { OptionalIntlProvider } from "./OptionalIntlProvider"
 import { ThemeProvider, ThemeManager } from "./ThemeProvider"
 
 export interface KaizenProviderProps {
   children: React.ReactNode
   themeManager?: ThemeManager
+  locale?: string
 }
 
 export const KaizenProvider = ({
   children,
   themeManager,
+  locale = "en",
 }: KaizenProviderProps): JSX.Element => (
-  <ThemeProvider themeManager={themeManager}>{children}</ThemeProvider>
+  <OptionalIntlProvider locale={locale}>
+    <ThemeProvider themeManager={themeManager}>{children}</ThemeProvider>
+  </OptionalIntlProvider>
 )
 
 KaizenProvider.displayName = "KaizenProvider"
