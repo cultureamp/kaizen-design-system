b1aa21917bd88188ad3b1aca1d284551e48e1914
Michael Winter <mcwinter07@gmail.com>
2023-07-12 16:53:43 +1000

    replace calendar icons

--- packages/date-picker/src/_subcomponents/Calendar/components/IconRight.tsx
+++ packages/date-picker/src/_subcomponents/Calendar/components/IconRight.tsx
@@ -1,9 +1,19 @@
 import React from "react"
-import { Icon } from "@kaizen/component-library"
-import arrowRight from "@kaizen/component-library/icons/arrow-right.icon.svg"
 
 export const IconRight = (): JSX.Element => (
-  <Icon icon={arrowRight} role="presentation" />
+  <svg
+    aria-hidden
+    role="presentation"
+    xmlns="http://www.w3.org/2000/svg"
+    width="20"
+    height="20"
+  >
+    <path
+      fill="currentColor"
+      fillRule="evenodd"
+      d="m12.5 5-1.179 1.179 2.987 2.987H2.5v1.667h11.808l-2.987 2.988L12.5 15l5-5.001z"
+    />
+  </svg>
 )
 
 IconRight.displayName = "Calendar__IconRight"
