c350310a8b0622d9075844dfe1b389c5e59a5677
Michael Winter <mcwinter07@gmail.com>
2023-07-20 12:55:46 +1000

    give aria-hidden a value on divider

--- packages/select/src/FilterMultiSelect/components/SectionDivider/SectionDivider.tsx
+++ packages/select/src/FilterMultiSelect/components/SectionDivider/SectionDivider.tsx
@@ -3,7 +3,7 @@ import { Divider } from "@kaizen/draft-divider"
 import styles from "./SectionDivider.module.scss"
 
 export const SectionDivider = (): JSX.Element => (
-  <li className={styles.divider} aria-hidden>
+  <li className={styles.divider} aria-hidden="true">
     <Divider variant="canvas" />
   </li>
 )
