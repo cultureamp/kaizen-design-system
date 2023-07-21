8f3fa78e9e189c305e1cd400534393501d3c1724
Michael Winter <36558508+mcwinter07@users.noreply.github.com>
2023-07-21 10:55:57 +1000

    update ListBoxSection and SectionDivider semantics (#3864)
    * update ListBoxSection and SectionDivider semantics
    
    * update structure following manual a11y test
    
    * update semantics of the listbox
    
    * update code comments and story name
    
    * add changeset
    
    * fix pascal case in storyname
    
    * update list semantics
    
    * run linting fix
    
    * update story to move sectionHeader to disabled inputs
    
    * give aria-hidden a value on divider
    
    * save uuid to state to avoid avoid regen on rerender and add VisuallyHidden wrapper instead of sr-only
    
    * remove uneeded useEffect

---
+++ .changeset/dry-snails-obey.md
@@ -0,0 +1,9 @@
+---
+"@kaizen/select": minor
+---
+
+- adds sectionHeader prop to ListBoxSection
+  - provide a way to add headers that describe the ListBoxSection
+- updates ListBoxSection semantic HTML
+  - this should address the issues with keyboard navigation and how the component is read to screen readers
+- adds story to showcase use of new prop usage
--- packages/select/src/FilterMultiSelect/components/ListBoxSection/ListBoxSection.tsx
+++ packages/select/src/FilterMultiSelect/components/ListBoxSection/ListBoxSection.tsx
@@ -1,7 +1,8 @@
-import React from "react"
+import React, { ReactNode, useState } from "react"
+import { v4 } from "uuid"
+import { VisuallyHidden } from "@kaizen/a11y"
 import { MultiSelectItem } from "../../../types"
 import styles from "./ListBoxSection.module.scss"
-
 export interface ListBoxSectionProps {
   items: MultiSelectItem[]
   /**
@@ -9,6 +10,8 @@ export interface ListBoxSectionProps {
    * unsighted users
    */
   sectionName: string
+  /** If provided, will override the aria-label for this group */
+  sectionHeader?: ReactNode
   children: (item: MultiSelectItem) => React.ReactNode
 }
 
@@ -16,9 +19,36 @@ export const ListBoxSection = ({
   items,
   sectionName,
   children,
-}: ListBoxSectionProps): JSX.Element => (
-  <div aria-label={sectionName} className={styles.listBoxSection}>
-    {Array.from(items).map(node => node != undefined && children(node))}
-  </div>
-)
+  sectionHeader,
+}: ListBoxSectionProps): JSX.Element => {
+  const [listSectionId] = useState<string>(v4())
+  return (
+    <li role="presentation">
+      <ul
+        className={styles.listBoxSection}
+        aria-label={sectionName}
+        aria-labelledby={sectionHeader ? listSectionId : undefined}
+        role="group"
+      >
+        {sectionHeader && (
+          <li
+            className={styles.listBoxSectionHeader}
+            id={listSectionId}
+            role="presentation"
+          >
+            <VisuallyHidden>{sectionName}. </VisuallyHidden>
+            {sectionHeader}
+          </li>
+        )}
+        {/*
+         * This seems semantically strange but most closely aligns to WCAGs suggested pattern:
+         https://www.w3.org/WAI/ARIA/apg/patterns/listbox/examples/listbox-grouped/
+         * the role="presentation" will throw an error a11y warning "Element has children which are not allowed".
+         This is expected and is an accepted issue / potential limitation with axe
+         */}
+        {Array.from(items).map(node => node != undefined && children(node))}
+      </ul>
+    </li>
+  )
+}
 ListBoxSection.displayName = "ListBoxSection"
