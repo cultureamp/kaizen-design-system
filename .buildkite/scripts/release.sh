bf54b8007c2b9d5100e76ed350113d405a7294c4
Geoffrey Chong <gyfchong@users.noreply.github.com>
2023-07-24 15:28:32 +1000

    removed unused github ssh key from buildkite release (#3902)
    * removed unused github ssh key from buildkite release
    
    * remove rogue export
    
    * remove ssh token
    
    * add empty changeset

--- .buildkite/scripts/release.sh
+++ .buildkite/scripts/release.sh
@@ -7,8 +7,6 @@ set -e
 # shellcheck source=setup-registry.sh
 . ".buildkite/scripts/helpers/setup-registry.sh"
 
-GITHUB_SSH_HOST_KEY="github.com ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCj7ndNxQowgcQnjshcLrqPEiiphnt+VTTvDP6mHBL9j1aNUkY4Ue1gvwnGLVlOhGeYrnZaMgRK6+PKCUXaDbC7qtbW8gIkhL7aGCsOr/C56SJMy/BCZfxd1nWzAOxSDPgVsmerOBYfNqltV9/hWCqBywINIR+5dIg6JTJ72pcEpEjcYgXkE2YEFXV1JHnsKgbLWNlhScqb2UmyRkQyytRLtL+38TGxkxCflmO+5Z8CSSNY7GidjMIZ7Q4zMjA2n1nGrlTDkzwDCsw+wqFPGQA179cnfGWOWRVruj16z6XyvxvjJwbz0wQZ75XK5tKSb7FNyeIEs4TT4jk+S4dhPeAUC5y+bDYirYgM4GC7uEnztnZyaVWQ7B381AK4Qdrwt51ZqExKbQpTUNn+EjqoTwvqNj4kqx5QUCI0ThS/YkOxJCXmPUWZbhjpCg56i+2aB6CmK2JGhn57K5mj0MNdBXA4/WnwH6XoPWJzK5Nyu2zB3nAZp+S5hpQs+p1vN1/wsjk="
-
 setup_github() {
   git config --global credential.helper cache
   git config --global user.email "cultureamp-ci@cultureamp.com"
@@ -16,12 +14,6 @@ setup_github() {
   git config --global user.password "$GH_TOKEN"
   git config --global commit.gpgsign false
   git config --global --add safe.directory /workspace
-
-  eval "$(ssh-agent -s)"
-  echo "$GH_SSH_KEY" | ssh-add -
-
-  echo "Adding GitHub host key to known hosts..."
-  echo "$GITHUB_SSH_HOST_KEY" | tr -d "\\n" >>/etc/ssh/ssh_known_hosts
 
   echo "Checking GitHub authentication..."
   ssh -T git@github.com || true # exits non-zero
@@ -56,10 +48,9 @@ release_canary() {
 }
 
 main() {
-  export GH_SSH_KEY GH_TOKEN NPM_TOKEN
+  export GH_TOKEN NPM_TOKEN
 
   printf "Fetching secrets... "
-  GH_SSH_KEY=$(get_secret "github-ssh-key") || exit $?
   GH_TOKEN=$(get_secret "github-api-token") || exit $?
   NPM_TOKEN=$(get_secret "npm-token") || exit $?
   echo "(done)"
@@ -90,7 +81,7 @@ main() {
 
   echo "All done!"
 
-  unset GH_SSH_KEY GH_TOKEN NPM_TOKEN
+  unset GH_TOKEN NPM_TOKEN
 }
 
 main
