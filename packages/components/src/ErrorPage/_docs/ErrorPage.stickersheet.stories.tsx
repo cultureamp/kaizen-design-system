1170c81895affeca54f6a61b470da0a1f568cfc1
Lisa Tassone <lisa.tassone@cultureamp.com>
2023-07-25 11:07:50 +1000

    run prettier

--- packages/components/src/ErrorPage/_docs/ErrorPage.stickersheet.stories.tsx
+++ packages/components/src/ErrorPage/_docs/ErrorPage.stickersheet.stories.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React from "react"
 import { Meta, StoryFn } from "@storybook/react"
 import { StickerSheet } from "../../../../../storybook/components/StickerSheet"
-import { ErrorPage } from "../ErrorPage";
+import { ErrorPage } from "../ErrorPage"
 
 export default {
   title: "Pages/Error Page",
@@ -13,80 +13,87 @@ export default {
 
 const StickerSheetTemplate: StoryFn<{ isReversed: boolean }> = ({
   isReversed,
-}) => <>
-  <StickerSheet heading="Error 400" isReversed={isReversed}>
-    <StickerSheet.Body>
-      <StickerSheet.Row >
-        <ErrorPage code="400"/>
-      </StickerSheet.Row>
-    </StickerSheet.Body>
-  </StickerSheet>
-  <StickerSheet heading="Error 401" isReversed={isReversed}>
-    <StickerSheet.Body>
-      <StickerSheet.Row >
-        <ErrorPage code="401"/>
-      </StickerSheet.Row>
-    </StickerSheet.Body>
-  </StickerSheet>
-  <StickerSheet heading="Error 403" isReversed={isReversed}>
-    <StickerSheet.Body>
-      <StickerSheet.Row >
-        <ErrorPage code="403"/>
-      </StickerSheet.Row>
-    </StickerSheet.Body>
-  </StickerSheet>
-  <StickerSheet heading="Error 404" isReversed={isReversed}>
-    <StickerSheet.Body>
-      <StickerSheet.Row >
-        <ErrorPage code="404"/>
-      </StickerSheet.Row>
-    </StickerSheet.Body>
-  </StickerSheet>
-  <StickerSheet heading="Error 422" isReversed={isReversed}>
-    <StickerSheet.Body>
-      <StickerSheet.Row >
-        <ErrorPage code="422"/>
-      </StickerSheet.Row>
-    </StickerSheet.Body>
-  </StickerSheet>
-  <StickerSheet heading="Error 500" isReversed={isReversed}>
-    <StickerSheet.Body>
-      <StickerSheet.Row >
-        <ErrorPage code="500"/>
-      </StickerSheet.Row>
-    </StickerSheet.Body>
-  </StickerSheet>
-  <StickerSheet heading="Error 502" isReversed={isReversed}>
-    <StickerSheet.Body>
-      <StickerSheet.Row >
-        <ErrorPage code="502"/>
-      </StickerSheet.Row>
-    </StickerSheet.Body>
-  </StickerSheet>
-  <StickerSheet heading="Error 503" isReversed={isReversed}>
-    <StickerSheet.Body>
-      <StickerSheet.Row >
-        <ErrorPage code="503"/>
-      </StickerSheet.Row>
-    </StickerSheet.Body>
-  </StickerSheet>
-  <StickerSheet heading="Error 504" isReversed={isReversed}>
-    <StickerSheet.Body>
-      <StickerSheet.Row >
-        <ErrorPage code="504"/>
-      </StickerSheet.Row>
-    </StickerSheet.Body>
-  </StickerSheet>
-  <StickerSheet heading="Custom error" isReversed={isReversed}>
-    <StickerSheet.Body>
-      <StickerSheet.Row >
-        <ErrorPage code={12839450} title="This is a custom title" message="This is a custom message" callToAction={{
-          onContactSupport: () => alert("Custom handler"),
-          homeHref: "/anewhome"
-        }}/>
-      </StickerSheet.Row>
-    </StickerSheet.Body>
-  </StickerSheet>
-</>
+}) => (
+  <>
+    <StickerSheet heading="Error 400" isReversed={isReversed}>
+      <StickerSheet.Body>
+        <StickerSheet.Row>
+          <ErrorPage code="400" />
+        </StickerSheet.Row>
+      </StickerSheet.Body>
+    </StickerSheet>
+    <StickerSheet heading="Error 401" isReversed={isReversed}>
+      <StickerSheet.Body>
+        <StickerSheet.Row>
+          <ErrorPage code="401" />
+        </StickerSheet.Row>
+      </StickerSheet.Body>
+    </StickerSheet>
+    <StickerSheet heading="Error 403" isReversed={isReversed}>
+      <StickerSheet.Body>
+        <StickerSheet.Row>
+          <ErrorPage code="403" />
+        </StickerSheet.Row>
+      </StickerSheet.Body>
+    </StickerSheet>
+    <StickerSheet heading="Error 404" isReversed={isReversed}>
+      <StickerSheet.Body>
+        <StickerSheet.Row>
+          <ErrorPage code="404" />
+        </StickerSheet.Row>
+      </StickerSheet.Body>
+    </StickerSheet>
+    <StickerSheet heading="Error 422" isReversed={isReversed}>
+      <StickerSheet.Body>
+        <StickerSheet.Row>
+          <ErrorPage code="422" />
+        </StickerSheet.Row>
+      </StickerSheet.Body>
+    </StickerSheet>
+    <StickerSheet heading="Error 500" isReversed={isReversed}>
+      <StickerSheet.Body>
+        <StickerSheet.Row>
+          <ErrorPage code="500" />
+        </StickerSheet.Row>
+      </StickerSheet.Body>
+    </StickerSheet>
+    <StickerSheet heading="Error 502" isReversed={isReversed}>
+      <StickerSheet.Body>
+        <StickerSheet.Row>
+          <ErrorPage code="502" />
+        </StickerSheet.Row>
+      </StickerSheet.Body>
+    </StickerSheet>
+    <StickerSheet heading="Error 503" isReversed={isReversed}>
+      <StickerSheet.Body>
+        <StickerSheet.Row>
+          <ErrorPage code="503" />
+        </StickerSheet.Row>
+      </StickerSheet.Body>
+    </StickerSheet>
+    <StickerSheet heading="Error 504" isReversed={isReversed}>
+      <StickerSheet.Body>
+        <StickerSheet.Row>
+          <ErrorPage code="504" />
+        </StickerSheet.Row>
+      </StickerSheet.Body>
+    </StickerSheet>
+    <StickerSheet heading="Custom error" isReversed={isReversed}>
+      <StickerSheet.Body>
+        <StickerSheet.Row>
+          <ErrorPage
+            code={12839450}
+            title="This is a custom title"
+            message="This is a custom message"
+            callToAction={{
+              onContactSupport: () => alert("Custom handler"),
+              homeHref: "/anewhome",
+            }}
+          />
+        </StickerSheet.Row>
+      </StickerSheet.Body>
+    </StickerSheet>
+  </>
+)
 
 export const StickerSheetDefault = StickerSheetTemplate.bind({})
