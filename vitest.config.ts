afc2adffbe93de73fc1b034cfd6b54ec3cf79743
Cassandra Tam <cassandra.tam@cultureamp.com>
2023-07-31 15:44:50 +1000

    chore(vitest): set up vitest base config

---
+++ vitest.config.ts
@@ -0,0 +1,21 @@
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vitest/config"
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: ["./vitest.setup.ts"],
+    include: ["?(draft-)packages/**/*.spec.ts?(x)"],
+  },
+  resolve: {
+    alias: {
+      "~types": path.resolve(__dirname, "packages/components/src/types"),
+      "~utils": path.resolve(__dirname, "packages/components/src/utils"),
+      "~components": path.resolve(__dirname, "packages/components/src"),
+      "~icons": path.resolve(__dirname, "packages/components/src/SVG/icons"),
+    },
+  },
+})
