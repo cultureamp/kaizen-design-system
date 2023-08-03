57804e49f6db97cfddd92487493c1826041d3cfd
Cassandra Tam <cassandra.tam@cultureamp.com>
2023-08-03 12:24:40 +1000

    test(tile): spilt snapshot tests only for jest

--- draft-packages/tile/KaizenDraft/Tile/Tile.spec.tsx
+++ draft-packages/tile/KaizenDraft/Tile/Tile.spec.tsx
@@ -1,5 +1,4 @@
-import * as React from "react"
-
+import React from "react"
 import { render } from "@testing-library/react"
 import {
   InformationTile,
@@ -7,7 +6,6 @@ import {
   TileAction,
   TileInformation,
 } from "@kaizen/draft-tile"
-import { GenericTileProps } from "./components/GenericTile"
 
 const primaryAction: TileAction = {
   label: "View",
@@ -18,14 +16,6 @@ const secondaryAction: TileAction = {
   label: "Preview",
   href: "href2",
 }
-const moods: Array<GenericTileProps["mood"]> = [
-  "positive",
-  "informative",
-  "cautionary",
-  "assertive",
-  "negative",
-  "prominent",
-]
 
 const information: TileInformation = {
   text:
@@ -38,132 +28,35 @@ const information: TileInformation = {
 }
 
 describe("<InformationTile />", () => {
-  describe("snapshots", () => {
-    it("renders InformationTile", () => {
-      const { container } = render(
-        <InformationTile
-          title="Title"
-          metadata="Metadata"
-          footer={<div>Hello world</div>}
-        />
-      )
+  it("renders InformationTile with a custom title tag", () => {
+    const { getByText } = render(
+      <InformationTile
+        title="custom title"
+        titleTag="div"
+        metadata="Metadata"
+        footer={<div>Hello world</div>}
+        information={information}
+      >
+        <div>Child content</div>
+      </InformationTile>
+    )
 
-      expect(container.firstChild).toMatchSnapshot()
-    })
-
-    it("renders InformationTile with children", () => {
-      const { container } = render(
-        <InformationTile
-          title="Title"
-          metadata="Metadata"
-          footer={<div>Hello world</div>}
-        >
-          <div>Child content</div>
-        </InformationTile>
-      )
-
-      expect(container.firstChild).toMatchSnapshot()
-    })
-
-    it("renders InformationTile with information data", () => {
-      const { container } = render(
-        <InformationTile
-          title="Title"
-          metadata="Metadata"
-          footer={<div>Hello world</div>}
-          information={information}
-        >
-          <div>Child content</div>
-        </InformationTile>
-      )
-
-      expect(container.firstChild).toMatchSnapshot()
-    })
-
-    it("renders InformationTile with a custom title tag", () => {
-      const { getByText } = render(
-        <InformationTile
-          title="custom title"
-          titleTag="div"
-          metadata="Metadata"
-          footer={<div>Hello world</div>}
-          information={information}
-        >
-          <div>Child content</div>
-        </InformationTile>
-      )
-
-      expect(getByText("custom title").tagName).toEqual("DIV")
-    })
-    moods.map(mood => {
-      it(`renders InformationTile with ${mood} mood`, () => {
-        const { container } = render(
-          <InformationTile
-            title="Title"
-            metadata="Metadata"
-            footer={<div>Hello world</div>}
-            mood={mood}
-          />
-        )
-        expect(container.firstChild).toMatchSnapshot()
-      })
-    })
+    expect(getByText("custom title").tagName).toEqual("DIV")
   })
 })
 
 describe("<MultiActionTile />", () => {
-  describe("snapshots", () => {
-    it("renders MultiActionTile", () => {
-      const { container } = render(
-        <MultiActionTile
-          title="Title"
-          metadata="Metadata"
-          primaryAction={primaryAction}
-        />
-      )
+  it("renders MultiActionTile with a custom title tag", () => {
+    const { getByText } = render(
+      <MultiActionTile
+        title="custom title"
+        titleTag="h6"
+        metadata="Metadata"
+        primaryAction={primaryAction}
+        secondaryAction={secondaryAction}
+      />
+    )
 
-      expect(container.firstChild).toMatchSnapshot()
-    })
-
-    it("renders MultiActionTile with secondary action", () => {
-      const { container } = render(
-        <MultiActionTile
-          title="Title"
-          metadata="Metadata"
-          primaryAction={primaryAction}
-          secondaryAction={secondaryAction}
-        />
-      )
-
-      expect(container.firstChild).toMatchSnapshot()
-    })
-
-    it("renders MultiActionTile with a custom title tag", () => {
-      const { getByText } = render(
-        <MultiActionTile
-          title="custom title"
-          titleTag="h6"
-          metadata="Metadata"
-          primaryAction={primaryAction}
-          secondaryAction={secondaryAction}
-        />
-      )
-
-      expect(getByText("custom title").tagName).toEqual("H6")
-    })
-    moods.map(mood => {
-      it(`renders MultiActionTile with ${mood} mood`, () => {
-        const { container } = render(
-          <MultiActionTile
-            title="Title"
-            metadata="Metadata"
-            primaryAction={primaryAction}
-            secondaryAction={secondaryAction}
-            mood={mood}
-          />
-        )
-        expect(container.firstChild).toMatchSnapshot()
-      })
-    })
+    expect(getByText("custom title").tagName).toEqual("H6")
   })
 })
