cffa819ffd54758e3ef0ee32a85e31637614a2a8
Cassandra Tam <cassandra.tam@cultureamp.com>
2023-08-03 11:55:17 +1000

    ci: run all jest/vitest tests from root

--- jest.config.js
+++ jest.config.js
@@ -2,15 +2,23 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "jsdom",
-  testMatch: ["**/*.spec?(.jest).ts?(x)"],
+  testMatch: [
+    "<rootDir>/packages/rich-text-editor/**/*.spec?(.jest).ts?(x)",
+    "**/*.spec.jest.ts?(x)",
+  ],
   setupFilesAfterEnv: ["jest-canvas-mock", "<rootDir>/setupTests.ts"],
   moduleNameMapper: {
     "\\.(jpe?g|png|webm|mp4)$": "jest-static-stubs/$1",
     "\\.s?css$": "identity-obj-proxy",
     "\\.svg$": require.resolve("@kaizen/component-library/mocks/svgMock"),
+    "~types/(.*)$": "<rootDir>/packages/components/src/types/$1",
+    "~utils/(.*)$": "<rootDir>/packages/components/src/utils/$1",
+    "~components/(.*)$": "<rootDir>/packages/components/src/$1",
+    "~icons/(.*)$": "<rootDir>/packages/components/src/SVG/icons/$1",
+    "^__@cultureamp/i18n-react-intl/locales/(.*)":
+      "<rootDir>/packages/components/locales/$1",
   },
   transformIgnorePatterns: ["[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs)$"],
-  modulePathIgnorePatterns: ["<rootDir>/packages/components"],
 }
 
 process.env.TZ = "UTC"
@@ -18,6 +26,7 @@ process.env.TZ = "UTC"
 if (process.env.USE_REACT_16 === "true") {
   console.log("=== React 16 tests ===")
   module.exports.cacheDirectory = ".cache/jest-cache-react-16"
+  module.exports.testMatch = ["**/*.spec?(.jest).ts?(x)"]
   module.exports.moduleNameMapper = {
     ...module.exports.moduleNameMapper,
     "^react-dom((\\/.*)?)$": "react-dom-16$1",
@@ -28,6 +37,7 @@ if (process.env.USE_REACT_16 === "true") {
 } else if (process.env.USE_REACT_17 === "true") {
   console.log("=== React 17 tests ===")
   module.exports.cacheDirectory = ".cache/jest-cache-react-17"
+  module.exports.testMatch = ["**/*.spec?(.jest).ts?(x)"]
   module.exports.moduleNameMapper = {
     ...module.exports.moduleNameMapper,
     "^react-dom((\\/.*)?)$": "react-dom-17$1",
