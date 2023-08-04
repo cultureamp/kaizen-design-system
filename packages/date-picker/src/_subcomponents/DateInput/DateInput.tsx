692444f1185f9e6118a3d4233a803c4c5cf702fe
Doug MacKenzie <doug.mackenzie@cultureamp.com>
2023-08-04 11:25:38 +1000

    Revert changes to datepicker subcomponents

--- packages/date-picker/src/_subcomponents/DateInput/DateInput.tsx
+++ packages/date-picker/src/_subcomponents/DateInput/DateInput.tsx
@@ -1,5 +1,4 @@
-import React, { useState } from "react"
-import { v4 } from "uuid"
+import React from "react"
 import { Input, InputProps, Label } from "@kaizen/draft-form"
 import { isRefObject } from "../../utils/isRefObject"
 import styles from "./DateInput.module.scss"
@@ -17,34 +16,29 @@ type OmittedInputProps =
   | "inputRef"
 
 export interface DateInputProps extends Omit<InputProps, OmittedInputProps> {
+  id: string
   labelText: React.ReactNode
   isReversed?: boolean
 }
 
 export const DateInput = React.forwardRef<HTMLInputElement, DateInputProps>(
-  (
-    { id: propsId, labelText, disabled, isReversed = false, ...inputProps },
-    ref
-  ) => {
-    const [id] = useState<string>(propsId || v4())
-    return (
-      <div className={styles.dateInput}>
-        <Label
-          htmlFor={id}
-          labelText={labelText}
-          reversed={isReversed}
-          disabled={disabled}
-        />
-        <Input
-          inputRef={isRefObject(ref) ? ref : undefined}
-          id={id}
-          autoComplete="off"
-          disabled={disabled}
-          reversed={isReversed}
-          {...inputProps}
-        />
-      </div>
-    )
-  }
+  ({ id, labelText, disabled, isReversed = false, ...inputProps }, ref) => (
+    <div className={styles.dateInput}>
+      <Label
+        htmlFor={id}
+        labelText={labelText}
+        reversed={isReversed}
+        disabled={disabled}
+      />
+      <Input
+        inputRef={isRefObject(ref) ? ref : undefined}
+        id={id}
+        autoComplete="off"
+        disabled={disabled}
+        reversed={isReversed}
+        {...inputProps}
+      />
+    </div>
+  )
 )
 DateInput.displayName = "DateInput"
