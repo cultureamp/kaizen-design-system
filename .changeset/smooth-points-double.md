327fbe92e1caa1ba714cc831e85e2d12543d43e0
Geoffrey Chong <gyfchong@users.noreply.github.com>
2023-08-11 08:06:08 +1000

    Fix-circular-dependencies (#3955)
    * fix filterdatepicker circ dep
    
    * fix button types circ dep
    
    * fix workflowheader circular types
    
    * add changeset
    
    * fix genericbutton test types
    
    * move all types to GenericButton
    
    * fix types in tests

---
+++ .changeset/smooth-points-double.md
@@ -0,0 +1,5 @@
+---
+"@kaizen/components": patch
+---
+
+Fix circular dependencies
--- packages/components/src/Workflow/subcomponents/Header/Header.tsx
+++ packages/components/src/Workflow/subcomponents/Header/Header.tsx
@@ -1,6 +1,12 @@
 import React from "react"
-import { HeaderRoot, Branding, Titles, Actions } from "./components"
-import { WorkflowActionsProps, WorkflowTitlesProps } from "./"
+import {
+  HeaderRoot,
+  Branding,
+  Titles,
+  Actions,
+  WorkflowTitlesProps,
+  WorkflowActionsProps,
+} from "./components"
 
 export type HeaderProps = WorkflowTitlesProps & WorkflowActionsProps
 
