d31bbd80d41d36f3f73f0972b9872dc17daddf84
jakepitman <jakeseanp@gmail.com>
2023-08-11 15:01:38 +1200

    Make plugin general enough to accept other modifications

--- packages/components/svgo.config.js
+++ packages/components/svgo.config.js
@@ -12,19 +12,22 @@ const attrKeysToReplace = [
   },
 ]
 
-// Recursively replace attributes found in svg and it's child elements
-const replaceAttrs = item => {
+const replaceAttrKeys = child => {
+  attrKeysToReplace.forEach(({ original, replacement }) => {
+    if (child.attributes.hasOwnProperty(original)) {
+      child.attributes[replacement] = child.attributes[original]
+      delete child.attributes[original]
+    }
+  })
+}
+
+// Recurses through all elements and child elements of root item
+const recurse = item => {
   // Note: first item is not an element, it's children are the elements.
   item.children.forEach(child => {
-    attrKeysToReplace.forEach(({ original, replacement }) => {
-      if (child.attributes.hasOwnProperty(original)) {
-        child.attributes[replacement] = child.attributes[original]
-        delete child.attributes[original]
-      }
-    })
+    replaceAttrKeys(child)
 
-    // Recurse
-    replaceAttrs(child)
+    recurse(child)
   })
 }
 
@@ -32,10 +35,10 @@ module.exports = {
   plugins: [
     "preset-default",
     {
-      name: "Replace attributes",
+      name: "Recurse",
       params: {},
       type: "perItem",
-      fn: replaceAttrs,
+      fn: recurse,
     },
   ],
 }
