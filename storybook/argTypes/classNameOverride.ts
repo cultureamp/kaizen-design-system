17afd561b67656359b004ee6a56c39d6d2735aa2
Cassandra Tam <cassandra.tam@cultureamp.com>
2023-08-11 13:40:33 +1000

    docs(storybook): remove config that adds classNameOverride to every component props table
    - create reusable arg type for classNameOverride

---
+++ storybook/argTypes/classNameOverride.ts
@@ -0,0 +1,7 @@
+export const classNameOverrideArgType = {
+  classNameOverride: {
+    type: "string",
+    description:
+      "Add extra classnames to the component. Try out some Tailwind classes (eg. `!mb-48`) to see!",
+  },
+} as const
