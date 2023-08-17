c58f22f937654cd9fae5f1c9e4bdef2b7e3b3e2c
Michael Winter <mcwinter07@gmail.com>
2023-08-17 11:53:06 +1000

    update example for MultiSelect search inside

--- packages/components/src/MultiSelect/_docs/LabelledMessage.stories.tsx
+++ packages/components/src/MultiSelect/_docs/LabelledMessage.stories.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { VisuallyHidden } from "@kaizen/a11y"
 import { Button, IconButton } from "@kaizen/button"
 import { TextField, CheckboxField } from "@kaizen/draft-form"
 
@@ -13,10 +14,11 @@ const Options = (): JSX.Element => (
   <>
     {/* we will always fieldset wrapping the options */}
     <fieldset
+      id="id--all-options"
       data-section="options container"
-      aria-describedby="id--options-desc"
+      aria-describedby="id--live-region"
     >
-      <span id="id--options-desc" aria-hidden="true">
+      <span id="id--live-region" aria-live="polite">
         3 options total
       </span>
       <div className=" flex focus-within:bg-blue-200" data-section="option">
@@ -50,12 +52,9 @@ const Options = (): JSX.Element => (
   </>
 )
 
-{
-  /* TODO: validate if the label can be separate as htmlFor  */
-}
 const GroupOptions = (): JSX.Element => (
-  <fieldset>
-    <span id="id--group-desc" aria-live="assertive">
+  <fieldset aria-describedby="id--live-region">
+    <span id="id--live-region" aria-live="assertive">
       9 options available
     </span>
     <fieldset
@@ -169,37 +168,42 @@ const GroupOptions = (): JSX.Element => (
   </fieldset>
 )
 
-const Tags = (): JSX.Element => (
+const Tag = ({ tagLabel }: { tagLabel: string }): JSX.Element => (
+  <li
+    id={`id--tag-${tagLabel}`}
+    className=" inline-flex border-solid border-green-300"
+  >
+    {tagLabel}
+    <button
+      type="button"
+      onClick={() => {
+        const thisTag = document.getElementById(`id--tag-${tagLabel}`)
+        if (thisTag) {
+          thisTag.remove()
+        }
+      }}
+    >
+      <VisuallyHidden>{tagLabel} - Remove option</VisuallyHidden>
+      <span aria-hidden="true">X</span>
+    </button>
+  </li>
+)
+
+const TagList = (): JSX.Element => (
   // role=list" gets around safari jank
   // eslint-disable-next-line
   <ul
     className=" flex gap-6 list-none p-0 m-0"
-    data-section="tags"
+    data-section="TagList"
     id="id--selected-options"
     aria-relevant="additions removals"
     role="list"
-    // TODO: aria-label would be constructed from its children
-    aria-label="Selected options: Tag 1, Tag 2, Tag 3"
-    aria-live="assertive"
+    aria-label="Pancake - selected options: Tag 1, Tag 2, Tag 3"
+    aria-atomic={true}
   >
-    <li data-section="tag" className="border-solid border-green-300">
-      Tag 1{/* would be constructed by map */}
-      <button aria-label="Remove option - Tag 1" type="button">
-        X
-      </button>
-    </li>
-    <li data-section="tag" className="border-solid border-green-300">
-      Tag 2
-      <button type="button" aria-label="Remove option - Tag 2">
-        X
-      </button>
-    </li>
-    <li data-section="tag" className="border-solid border-green-300">
-      Tag 3
-      <button type="button" aria-label="Remove option - Tag 3">
-        X
-      </button>
-    </li>
+    <Tag tagLabel="Tag 1" />
+    <Tag tagLabel="Tag 2" />
+    <Tag tagLabel="Tag 3" />
   </ul>
 )
 
@@ -216,10 +220,16 @@ const ClearButton = (): JSX.Element => (
   </button>
 )
 
+const ClearSearchInputButton = (): JSX.Element => (
+  <button aria-label="Clear search" type="button">
+    X
+  </button>
+)
+
 const ToggleButton = (props: any): JSX.Element => (
   <button
     {...props}
-    aria-label={`${props["aria-expanded"] ? "Close" : "Open"} menu: Coffee"`}
+    aria-label={`${props["aria-expanded"] ? "Close" : "Open"} menu: Pancakes`}
     aria-controls="id--popper"
     aria-expanded={props["aria-expanded"]}
     aria-haspopup="dialog"
@@ -242,7 +252,7 @@ const Combobox = (props: any): JSX.Element => (
     placeholder="🔍"
     id="id--combo-1"
     data-section="search input / combobox"
-    // TODO: tie the tags to the a11y label
+    // TODO: tie the TagList to the a11y label
     type="text"
   />
 )
@@ -253,38 +263,85 @@ const Popover = (props: any): JSX.Element => (
     role="dialog"
     aria-modal="true"
     className=" border-solid border-blue-300"
+    aria-label="Pancake options"
   >
     {props.children}
   </div>
 )
 
 export const SearchOutside = (): JSX.Element => (
-  <div>
-    <Label htmlFor="id--combo-1" />
-    <div
-      className=" flex border-solid border-red-300"
-      data-section="combobox container"
+  <>
+    <div>
+      <Label htmlFor="id--combo-1" />
+      <div
+        className=" flex border-solid border-red-300"
+        data-section="combobox container"
+      >
+        <TagList />
+        <Combobox aria-expanded={true} />
+        <ClearButton />
+        <ToggleButton aria-expanded={true} />
+      </div>
+      <div id="id--combo-desc">I am a descriptions!</div>
+      <div id="id--error-message" data-section="error">
+        I am an erar! RAWR!
+      </div>
+      <Popover>
+        <Options />
+        {/* <Options /> */}
+      </Popover>
+    </div>
+    <button
+      type="button"
+      onClick={() => {
+        const liveRegion = document.getElementById("id--live-region")
+        if (liveRegion) {
+          liveRegion.innerText = "2 options available"
+        }
+      }}
     >
-      <Tags />
-      <Combobox aria-expanded={true} />
-      <ClearButton />
-      <ToggleButton aria-expanded={true} />
-    </div>
-    <div id="id--combo-desc">I am a descriptions!</div>
-    <div id="id--error-message" data-section="error">
-      I am an erar! RAWR!
-    </div>
-    <Popover>
-      <Options />
-      {/* <Options /> */}
-    </Popover>
-  </div>
+      Update options count live region
+    </button>
+    <button
+      type="button"
+      onClick={() => {
+        const taglist = document.getElementById("id--selected-options")
+        if (taglist) {
+          taglist.childNodes[0].remove()
+        }
+      }}
+    >
+      Remove tag from live region
+    </button>
+    <button
+      type="button"
+      onClick={() => {
+        const taglist = document.getElementById("id--selected-options")
+        if (taglist) {
+          taglist.insertAdjacentHTML(
+            "afterbegin",
+            `<li
+          class="border-solid border-green-300"
+          aria-label="Mocha"
+        >
+          Mocha
+          <button aria-label="Remove option - Mocha" type="button" >
+            X
+          </button>
+        </li>`
+          )
+        }
+      }}
+    >
+      Add tag to live region
+    </button>
+  </>
 )
 
-export const SearchAndTagsOutside = (): JSX.Element => (
+export const SearchAndTagListOutside = (): JSX.Element => (
   <div>
     <Label htmlFor="id--combo-1" />
-    <Tags />
+    <TagList />
     <div
       data-section="combobox container"
       className=" border-solid border-red-300"
@@ -303,23 +360,48 @@ export const SearchAndTagsOutside = (): JSX.Element => (
   </div>
 )
 
-export const SearchInside = (): JSX.Element => (
-  <div>
-    <div
-      data-section="combobox container"
-      className=" flex border-solid border-red-300"
-    >
-      <Tags />
-      <ClearButton />
-      <ToggleButton aria-expanded={true} />
+export const SearchInside = (): JSX.Element => {
+  const [isOpen, setIsOpen] = React.useState<boolean>(false)
+  return (
+    <div>
+      <Label htmlFor="id--inline-tag-search" />
+      {/* Disabling these a11y linting errors because there is an IconButton that mitigates these concerns. The onClick here is just an additional layer. */}
+      {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events,
+    jsx-a11y/no-static-element-interactions */}
+      <div className=" flex rounded-default border-solid gap-6 border-red-300">
+        <div className="grow">
+          <TagList />
+        </div>
+        <ClearButton />
+        <ToggleButton
+          aria-expanded={true}
+          onClick={() => {
+            setIsOpen(!isOpen)
+          }}
+        />
+      </div>
+      <div id="id--combo-desc">I am a descriptions!</div>
+      <div className="bg-red-100" id="id--error-message" data-section="error">
+        I am an erar! RAWR!
+      </div>
+      <Popover data-section="popover" className=" border-solid border-blue-300">
+        {isOpen && (
+          <>
+            <div className="flex w-full">
+              <input
+                id="id--inline-tag-search"
+                className="grow m-2"
+                type="search"
+                aria-controls="id--all-options"
+                value=""
+                placeholder="🔍"
+              />
+              <ClearSearchInputButton />
+            </div>
+            <Options />
+          </>
+        )}
+      </Popover>
     </div>
-    <div id="id--combo-desc">I am a descriptions!</div>
-    <div id="id--error-message" data-section="error">
-      I am an erar! RAWR!
-    </div>
-    <Popover data-section="popover" className=" border-solid border-blue-300">
-      <input data-section="search input / combobox" type="text" />
-      <Options />
-    </Popover>
-  </div>
-)
+  )
+}
