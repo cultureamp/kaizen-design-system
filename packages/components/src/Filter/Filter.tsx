dbd0b198550010f233fc7652ecc83fbb532694c0
Michael Winter <mcwinter07@gmail.com>
2023-08-21 14:47:53 +1000

    test floating lib with filter base

--- packages/components/src/Filter/Filter.tsx
+++ packages/components/src/Filter/Filter.tsx
@@ -1,4 +1,5 @@
 import React, { HTMLAttributes, useEffect, useRef, useState } from "react"
+import { useFloating } from "@floating-ui/react-dom"
 import { FocusOn } from "react-focus-on"
 import { OverrideClassName } from "~types/OverrideClassName"
 import { FilterPopover } from "./components/FilterPopover"
@@ -20,50 +21,30 @@ export const Filter = ({
   children,
   isOpen,
   setIsOpen,
-  renderTrigger,
+  // renderTrigger,
   classNameOverride,
-  onMount,
+  // onMount,
   ...restProps
 }: FilterProps): JSX.Element => {
-  const [isRefLoaded, setIsRefLoaded] = useState<boolean>(false)
+  const [anchor, setAnchor] = useState<HTMLButtonElement | null>(null)
 
-  const trigger = renderTrigger({
-    onClick: (): void => setIsOpen(!isOpen),
-    isOpen,
-  })
-
-  const inbuiltButtonRef = useRef<HTMLButtonElement>(null)
-  const inbuiltRef = useRef<FilterTriggerRef>({
-    triggerRef: inbuiltButtonRef,
-  })
-  const filterButtonRef = trigger.ref ?? inbuiltRef
-
-  useEffect(() => {
-    if (filterButtonRef.current?.triggerRef?.current) {
-      setIsRefLoaded(true)
-      onMount?.(filterButtonRef.current.triggerRef)
-    }
-  }, [filterButtonRef.current?.triggerRef?.current, onMount])
+  // const { refs, elements } = useFloating()
 
   return (
     <div className={classNameOverride} {...restProps}>
-      {React.cloneElement(trigger, {
-        ref: filterButtonRef,
-      })}
-      {isRefLoaded && isOpen && (
-        <FocusOn
-          scrollLock={false}
-          onClickOutside={(): void => setIsOpen(false)}
-          onEscapeKey={(): void => setIsOpen(false)}
-        >
-          <FilterPopover
-            referenceElement={
-              filterButtonRef.current?.triggerRef?.current || null
-            }
-          >
-            {children}
-          </FilterPopover>
-        </FocusOn>
+      <button
+        onClick={() => setIsOpen(!isOpen)}
+        ref={setAnchor}
+        // ref={refs.setReference}
+        type="button"
+      >
+        Pancakes!
+      </button>
+      {isOpen && (
+        <FilterPopover referenceElement={anchor}>{children}</FilterPopover>
+        // <FilterPopover ref={refs.setFloating}  elements={elements}>
+        //   {children}
+        // </FilterPopover>
       )}
     </div>
   )
--- packages/components/src/Filter/components/FilterPopover/FilterPopover.tsx
+++ packages/components/src/Filter/components/FilterPopover/FilterPopover.tsx
@@ -1,4 +1,5 @@
 import React, { HTMLAttributes, useState } from "react"
+import { useFloating, offset, autoPlacement } from "@floating-ui/react-dom"
 import { Options } from "@popperjs/core"
 import classnames from "classnames"
 import { usePopper } from "react-popper"
@@ -8,51 +9,52 @@ import styles from "./FilterPopover.module.scss"
 export interface FilterPopoverProps
   extends OverrideClassName<HTMLAttributes<HTMLDivElement>> {
   children: React.ReactNode
-  referenceElement: HTMLElement | null
-  popperOptions?: Partial<Options>
+  referenceElement?: HTMLElement | null
+  // popperOptions?: Partial<Options>
+  elements?: unknown
 }
 
-export const FilterPopover = ({
-  children,
-  referenceElement,
-  popperOptions,
-  classNameOverride,
-  ...restProps
-}: FilterPopoverProps): JSX.Element => {
-  const [popperElement, setPopperElement] = useState<HTMLDivElement | null>(
-    null
-  )
-
-  const { styles: popperStyles, attributes: popperAttributes } = usePopper(
-    referenceElement,
-    popperElement,
+export const FilterPopover =
+  // React.forwardRef(
+  (
     {
-      modifiers: [
-        {
-          name: "offset",
-          options: {
-            offset: [0, 15],
-          },
-        },
-      ],
+      children,
+      referenceElement,
+      // elements,
+      // popperOptions,
+      classNameOverride,
+      // refs,
+      ...restProps
+    }: FilterPopoverProps
+    // ref
+  ): JSX.Element => {
+    const { refs, floatingStyles } = useFloating({
+      elements: {
+        reference: referenceElement,
+      },
       placement: "bottom-start",
-      ...popperOptions,
-    }
-  )
+      middleware: [offset(15)],
+    })
+    // const { floatingStyles } = useFloating({
+    //   elements,
+    //   placement: "bottom-start",
+    //   middleware: [offset(15), autoPlacement()],
+    // })
 
-  return (
-    <div
-      ref={setPopperElement}
-      style={popperStyles?.popper}
-      {...popperAttributes?.popper}
-      className={classnames(styles.filterPopover, classNameOverride)}
-      role="dialog"
-      aria-modal="true"
-      {...restProps}
-    >
-      {children}
-    </div>
-  )
-}
-
+    return (
+      <div
+        ref={refs.setFloating}
+        // ref={ref}
+        style={floatingStyles}
+        // {...popperAttributes?.popper}
+        className={classnames(styles.filterPopover, classNameOverride)}
+        role="dialog"
+        aria-modal="true"
+        {...restProps}
+      >
+        {children}
+      </div>
+    )
+  }
+// )
 FilterPopover.displayName = "FilterPopover"
