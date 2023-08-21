7a57f75a3f5f4334ad981b28f98294cfa49974a3
jakepitman <jakeseanp@gmail.com>
2023-08-21 16:32:22 +1200

    Autogenerate index.ts for /Icons

--- packages/components/src/Icons/AcademyIcon.tsx
+++ packages/components/src/Icons/AcademyIcon.tsx
@@ -9,6 +9,6 @@ export const AcademyIcon = (props: Omit<SVGProps, "children">): JSX.Element => (
         d="M10 2 0 7.455l10 5.454 8.182-4.464v6.281H20V7.455L10 2Zm0 12.726-6.364-3.471v3.636L10 18.364l6.364-3.473v-3.636L10 14.726Z"
       />
     </defs>
-    <use fill="currentColor" fillRule="evenodd" href="#academy.icon.svg" />
+    <use fill="currentColor" fill-rule="evenodd" href="#academy.icon.svg" />
   </SVG>
 )
