4dea3391e12d41bed545cfa4cd88a4aa3fdbebd6
Michael Winter <mcwinter07@gmail.com>
2023-08-23 16:29:38 +1000

    Port over and refactor the checkbox from @kaizen/draft-form

--- packages/components/src/Checkbox/Checkbox.tsx
+++ packages/components/src/Checkbox/Checkbox.tsx
@@ -1,63 +1,77 @@
-import React, { InputHTMLAttributes } from "react"
+import React, { useEffect, useRef, useState, InputHTMLAttributes } from "react"
 import classnames from "classnames"
 import { CheckIcon } from "~icons/CheckIcon"
 import { MinusIcon } from "~icons/MinusIcon"
 import { OverrideClassName } from "~types/OverrideClassName"
 import styles from "./Checkbox.module.scss"
 
-export type CheckedStatus = "on" | "off" | "mixed"
+export type CheckedStatus = "checked" | "unchecked" | "indeterminate"
 
 export interface CheckboxProps
   extends OverrideClassName<
     Omit<InputHTMLAttributes<HTMLInputElement>, "type" | "checked">
   > {
-  checkedStatus?: CheckedStatus
-  reversed?: boolean
+  checkedStatus: CheckedStatus
   value?: string
 }
 
-const renderCheckOrMixedIcon = (
-  status: CheckedStatus,
-  reversed: boolean
-): React.ReactNode => {
-  if (status === "off") return
-  const Icon = status === "on" ? CheckIcon : MinusIcon
+const renderIcon = (status: CheckedStatus): JSX.Element | null => {
+  switch (status) {
+    case "indeterminate":
+      return <MinusIcon role="presentation" classNameOverride={styles.icon} />
+    case "checked":
+      return <CheckIcon role="presentation" classNameOverride={styles.icon} />
+    default:
+      return null
+  }
+}
+
+export const Checkbox = ({
+  checkedStatus,
+  onChange,
+  classNameOverride,
+  ...restProps
+}: CheckboxProps): JSX.Element => {
+  const checkboxRef = useRef<HTMLInputElement>(null)
+  // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/checkbox#value
+  // The `checked` attribute is representing the default checked state and never changes
+  const [defaultChecked] = useState<boolean>(checkedStatus === "checked")
+
+  useEffect(() => {
+    if (checkboxRef.current) {
+      if (checkedStatus === "checked") {
+        checkboxRef.current.checked = true
+        checkboxRef.current.indeterminate = false
+      } else if (checkedStatus === "unchecked") {
+        checkboxRef.current.checked = false
+        checkboxRef.current.indeterminate = false
+      } else if (checkedStatus === "indeterminate") {
+        checkboxRef.current.checked = false
+        checkboxRef.current.indeterminate = true
+      }
+    }
+  }, [checkedStatus])
+
   return (
-    <span className={classnames(styles.icon, reversed && styles.reversed)}>
-      <Icon role="presentation" />
+    <span
+      className={classnames(
+        styles.checkbox,
+        checkedStatus !== "unchecked" && styles.selected,
+        classNameOverride
+      )}
+    >
+      <input
+        ref={checkboxRef}
+        type="checkbox"
+        className={styles.nativeCheckbox}
+        checked={defaultChecked}
+        onChange={onChange}
+        readOnly={onChange === undefined}
+        {...restProps}
+      />
+      <span className={styles.iconContainer}>{renderIcon(checkedStatus)}</span>
     </span>
   )
 }
-
-const getCheckedFromStatus = (checkedStatus: CheckedStatus): boolean =>
-  checkedStatus === "on"
-
-export const Checkbox = ({
-  checkedStatus = "off",
-  onChange,
-  reversed = false,
-  value,
-  classNameOverride,
-  ...restProps
-}: CheckboxProps): JSX.Element => (
-  <span className={styles.container}>
-    <input
-      type="checkbox"
-      className={classnames(
-        styles.checkbox,
-        classNameOverride,
-        reversed && styles.reversed
-      )}
-      checked={getCheckedFromStatus(checkedStatus)}
-      onChange={onChange}
-      value={value || checkedStatus}
-      readOnly={onChange === undefined}
-      {...restProps}
-    />
-    <span className={classnames(styles.box, reversed && styles.reversed)}>
-      {renderCheckOrMixedIcon(checkedStatus, reversed)}
-    </span>
-  </span>
-)
 
 Checkbox.displayName = "Checkbox"
--- packages/components/src/Checkbox/_docs/Checkbox.stories.tsx
+++ packages/components/src/Checkbox/_docs/Checkbox.stories.tsx
@@ -1,35 +1,13 @@
 import React from "react"
-import { Meta, StoryFn, StoryObj } from "@storybook/react"
-import { ComponentDocsTemplate } from "../../../../../storybook/components/DocsContainer"
-import { StickerSheet } from "../../../../../storybook/components/StickerSheet"
+import { Meta, StoryObj } from "@storybook/react"
 import { Checkbox } from "../index"
+import styles from "./Checkbox.stories.module.scss"
 
 const meta = {
-  tags: ["autodocs"],
   title: "Components/Checkbox",
   component: Checkbox,
-  parameters: {
-    docs: {
-      container: ComponentDocsTemplate,
-    },
-    installation: [
-      "npm install @kaizen/components",
-      "import { Checkbox } from `@kaizen/components`",
-    ],
-    resourceLinks: {
-      /** @todo: Add Github link (adjust as needed) */
-      sourceCode:
-        "https://github.com/cultureamp/kaizen-design-system/tree/master/packages/components/src/Checkbox",
-      /** @todo (optional): Add Figma link */
-      figma: "Add Figma link here",
-      /** @todo (optional): Add Confluence link */
-      designGuidelines: "Add Confluence link here",
-    },
-    /**
-     * @todo (optional): Add alternate components
-     * Provide the storybook docs id
-     */
-    alternatives: ["components-component-name-1--docs", "components-component-name-2--docs"],
+  args: {
+    checkedStatus: "checked",
   },
 } satisfies Meta<typeof Checkbox>
 
@@ -46,46 +24,30 @@ export const Playground: StoryObj<typeof meta> = {
       },
     },
   },
+  render: args => {
+    const [status, setStatus] = React.useState(args.checkedStatus)
+
+    const handleClick = e => {
+      if (status === "unchecked") {
+        setStatus("indeterminate")
+      } else if (status === "indeterminate") {
+        setStatus("checked")
+      } else if (status === "checked") {
+        setStatus("unchecked")
+      }
+      args.onClick?.(e)
+    }
+
+    return (
+      <Checkbox
+        {...args}
+        onClick={handleClick}
+        checkedStatus={status}
+        // classNameOverride={styles.test}
+      />
+    )
+  },
+  // args: {
+  //   onChange: undefined,
+  // },
 }
-
-/** @todo: Add extra stories to showcase props which don't appear in sticker sheets - Delete the unused example stories below */
-
-const VariantsTemplate: StoryFn<{ isReversed?: boolean }> = ({
-  isReversed,
-}) => (
-  <StickerSheet>
-    <StickerSheet.Body>
-      <StickerSheet.Row>
-        /** Change `variant` to the relevant prop name */
-        <Checkbox variant="variantName" isReversed={isReversed} />
-        <Checkbox variant="variantName" isReversed={isReversed} />
-      </StickerSheet.Row>
-    </StickerSheet.Body>
-  </StickerSheet>
-)
-
-/**
- * @todo: Description here
- */
-export const Variants = VariantsTemplate.bind({})
-
-export const Reversed = VariantsTemplate.bind({})
-Reversed.args = { isReversed: true }
-Reversed.parameters = {
-  backgrounds: { default: "Purple 700" },
-}
-
-/**
- * @todo: Description here
- */
-export const Disabled: StoryObj<typeof meta> = {
-  args: { disabled: true },
-}
-
-/**
- * @todo: Description here
- * @note: This will likely be a Loading Skeleton component
- */
-export const Loading: StoryFn = () => (
-  <div></div>
-)
