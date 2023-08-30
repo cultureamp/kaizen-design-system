88da71e223f4aa134ade44e00b76fec40171a0bf
Cassandra Tam <cassandra.tam@cultureamp.com>
2023-08-30 17:57:56 +1000

    docs(filter): update format of stories

--- packages/components/src/Filter/_docs/Filter.stories.tsx
+++ packages/components/src/Filter/_docs/Filter.stories.tsx
@@ -1,11 +1,11 @@
 import React, { useState } from "react"
 import { action } from "@storybook/addon-actions"
-import { Meta, StoryFn } from "@storybook/react"
+import { Meta, StoryObj } from "@storybook/react"
 import { FilterButton, FilterButtonRemovable } from "~components/FilterButton"
 import { classNameOverrideArgType } from "~storybook/argTypes"
 import { Filter, FilterContents } from ".."
 
-export default {
+const meta = {
   title: "Components/Filter Base",
   component: Filter,
   argTypes: {
@@ -16,43 +16,49 @@ export default {
     onMount: { control: "disabled" },
     ...classNameOverrideArgType,
   },
+  args: {
+    children: <FilterContents>Filter Contents</FilterContents>,
+    renderTrigger: (triggerProps): JSX.Element => (
+      <FilterButton label="Label" {...triggerProps} />
+    ),
+    // Open state is set by FilterTemplate
+    isOpen: undefined,
+    setIsOpen: undefined,
+  },
 } satisfies Meta<typeof Filter>
 
-export const SimpleFilter: StoryFn<typeof Filter> = args => {
-  const [isOpen, setIsOpen] = useState(false)
-  return (
-    <Filter
-      {...args}
-      isOpen={isOpen}
-      setIsOpen={setIsOpen}
-      renderTrigger={(triggerProps): JSX.Element => (
-        <FilterButton label="Label" {...triggerProps} />
-      )}
-    >
-      <FilterContents>Filter Contents</FilterContents>
-    </Filter>
-  )
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+const FilterTemplate: Story = {
+  render: ({ children, ...args }) => {
+    const [isOpen, setIsOpen] = useState(false)
+    return (
+      <Filter {...args} isOpen={isOpen} setIsOpen={setIsOpen}>
+        {children}
+      </Filter>
+    )
+  },
 }
 
-export const RemovableFilter: StoryFn = () => {
-  const [isOpen, setIsOpen] = useState(false)
-  return (
-    <Filter
-      isOpen={isOpen}
-      setIsOpen={setIsOpen}
-      renderTrigger={(triggerProps): JSX.Element => (
-        <FilterButtonRemovable
-          triggerButtonProps={{
-            label: "Label",
-            ...triggerProps,
-          }}
-          removeButtonProps={{
-            onClick: action("remove button clicked"),
-          }}
-        />
-      )}
-    >
-      <FilterContents>Filter Contents</FilterContents>
-    </Filter>
-  )
+export const SimpleFilter: Story = {
+  ...FilterTemplate,
+}
+
+export const RemovableFilter: Story = {
+  ...FilterTemplate,
+  args: {
+    renderTrigger: (triggerProps): JSX.Element => (
+      <FilterButtonRemovable
+        triggerButtonProps={{
+          label: "Label",
+          ...triggerProps,
+        }}
+        removeButtonProps={{
+          onClick: action("remove button clicked"),
+        }}
+      />
+    ),
+  },
 }
