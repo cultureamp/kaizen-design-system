9c99b44786b0130c6c15cb7ff71644e874b289a5
Cassandra <cassandra.tam@cultureamp.com>
2023-09-05 11:05:55 +1000

    feat(multi-select): add Checkbox subcomponent (#4041)
    * Port over and refactor the checkbox from @kaizen/draft-form
    
    ---------
    
    Co-authored-by: Michael Winter <mcwinter07@gmail.com>
    Co-authored-by: JakePitman <jakeseanp@gmail.com>

---
+++ packages/components/src/MultiSelect/subcomponents/Checkbox/Checkbox.tsx
@@ -0,0 +1,79 @@
+import React, { useEffect, useRef, InputHTMLAttributes } from "react"
+import classnames from "classnames"
+import { CheckIcon, MinusIcon } from "~components/Icons"
+import { OverrideClassName } from "~types/OverrideClassName"
+import styles from "./Checkbox.module.scss"
+
+export type CheckedStatus = "checked" | "unchecked" | "indeterminate"
+
+type ReadOnly = {
+  readOnly: true
+  onChange?: never
+}
+
+type Controlled = {
+  readOnly?: false
+  onChange: Required<InputHTMLAttributes<HTMLInputElement>["onChange"]>
+}
+
+export type CheckboxProps = {
+  checkedStatus: CheckedStatus
+} & (ReadOnly | Controlled) &
+  OverrideClassName<
+    // `checked` is omitted as it conflicts with controlled checkboxes
+    Omit<InputHTMLAttributes<HTMLInputElement>, "type" | "checked">
+  >
+
+const renderIcon = (status: CheckedStatus): JSX.Element | null => {
+  switch (status) {
+    case "indeterminate":
+      return <MinusIcon role="presentation" classNameOverride={styles.icon} />
+    case "checked":
+      return <CheckIcon role="presentation" classNameOverride={styles.icon} />
+    default:
+      return null
+  }
+}
+
+export const Checkbox = ({
+  checkedStatus,
+  classNameOverride,
+  ...restProps
+}: CheckboxProps): JSX.Element => {
+  const checkboxRef = useRef<HTMLInputElement>(null)
+
+  useEffect(() => {
+    if (checkboxRef.current) {
+      if (checkedStatus === "checked") {
+        checkboxRef.current.checked = true
+        checkboxRef.current.indeterminate = false
+      } else if (checkedStatus === "unchecked") {
+        checkboxRef.current.checked = false
+        checkboxRef.current.indeterminate = false
+      } else if (checkedStatus === "indeterminate") {
+        checkboxRef.current.checked = false
+        checkboxRef.current.indeterminate = true
+      }
+    }
+  }, [checkedStatus])
+
+  return (
+    <span
+      className={classnames(
+        styles.checkbox,
+        checkedStatus !== "unchecked" && styles.selected,
+        classNameOverride
+      )}
+    >
+      <input
+        ref={checkboxRef}
+        type="checkbox"
+        className={styles.nativeCheckbox}
+        {...restProps}
+      />
+      <span className={styles.iconContainer}>{renderIcon(checkedStatus)}</span>
+    </span>
+  )
+}
+
+Checkbox.displayName = "Checkbox"
---
+++ packages/components/src/MultiSelect/subcomponents/Checkbox/_docs/Checkbox.stickersheet.stories.tsx
@@ -0,0 +1,61 @@
+import React from "react"
+import { Meta, StoryObj } from "@storybook/react"
+import { StickerSheet } from "~storybook/components/StickerSheet"
+import { Checkbox, CheckedStatus } from "../index"
+
+const meta = {
+  title: "Components/MultiSelect/Checkbox",
+  parameters: {
+    chromatic: { disable: false },
+    controls: { disable: true },
+  },
+} satisfies Meta<typeof Checkbox>
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+const STATUS_ROWS = [
+  { title: "Unchecked", status: "unchecked" },
+  { title: "Checked", status: "checked" },
+  { title: "Indeterminate", status: "indeterminate" },
+] satisfies Array<{ title: string; status: CheckedStatus }>
+
+const StickerSheetTemplate: Story = {
+  render: () => (
+    <StickerSheet>
+      <StickerSheet.Header
+        headings={["Default", "Hover", "Focus"]}
+        hasVerticalHeadings
+      />
+      <StickerSheet.Body>
+        {STATUS_ROWS.map(({ title, status }) => (
+          <StickerSheet.Row key={title} rowTitle={title}>
+            <Checkbox
+              aria-label="Read only label"
+              checkedStatus={status}
+              readOnly
+            />
+            <Checkbox
+              aria-label="Hover label"
+              classNameOverride="story__checkbox--hover"
+              checkedStatus={status}
+              readOnly
+            />
+            <Checkbox
+              aria-label="Focus label"
+              classNameOverride="story__checkbox--focus"
+              checkedStatus={status}
+              readOnly
+            />
+          </StickerSheet.Row>
+        ))}
+      </StickerSheet.Body>
+    </StickerSheet>
+  ),
+}
+
+export const StickerSheetDefault = {
+  ...StickerSheetTemplate,
+  name: "Sticker Sheet (Default)",
+}
