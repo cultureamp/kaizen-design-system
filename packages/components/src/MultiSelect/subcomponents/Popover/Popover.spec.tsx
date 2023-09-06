0ed872920adc77223bb5418c875122c490818d9f
jakepitman <jakeseanp@gmail.com>
2023-09-06 16:56:56 +1200

    WIP: Add test for portal functionality

--- packages/components/src/MultiSelect/subcomponents/Popover/Popover.spec.tsx
+++ packages/components/src/MultiSelect/subcomponents/Popover/Popover.spec.tsx
@@ -1,18 +1,38 @@
 import React from "react"
-import { render } from "@testing-library/react"
-import { Popover, PopoverProps } from "./Popover"
+import { render, waitFor } from "@testing-library/react"
+import { Popover, PopoverProps, useFloating } from "./"
 
-const PopoverWrapper = (customProps?: Partial<PopoverProps>): JSX.Element => (
-  <Popover
-    exampleRequiredString="Hello!" /** @todo: Add default values for your required props (override them with customProps if needed) */
-    {...customProps}
-  />
-)
+const PopoverWrapper = (customProps?: Partial<PopoverProps>): JSX.Element => {
+  const { refs } = useFloating()
+  return (
+    <Popover {...customProps} refs={refs}>
+      Hello
+    </Popover>
+  )
+}
 
-describe("<Popover />", () => {
-  it("does something", () => {
-    render(<PopoverWrapper />)
+describe("portalContainer prop", () => {
+  it("does something", async () => {
+    const portalContainer = document.createElement("div")
+    portalContainer.id = "portal-container"
+    //@ts-ignore
+    portalContainer["data-testid"] = "portal-container"
+    document.body.append(portalContainer)
+
+    const { getByTestId } = render(
+      <>
+        <PopoverWrapper
+          portalContainer={document.getElementById("portal-container")}
+        >
+          Hello
+        </PopoverWrapper>
+      </>,
+      { container: portalContainer }
+    )
     /** @todo: Fill in test case */
-    expect(true).toBe(false)
+
+    await waitFor(() => {
+      expect(getByTestId("portal-container")).toHaveTextContent("Hello")
+    })
   })
 })
