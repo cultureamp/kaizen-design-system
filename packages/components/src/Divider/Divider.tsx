6d61441660eeb9835434cd1892e6944b6823d581
Geoffrey Chong <gyf.chong@gmail.com>
2023-09-11 16:26:33 +1000

    cleanup

--- packages/components/src/Divider/Divider.tsx
+++ packages/components/src/Divider/Divider.tsx
@@ -10,7 +10,7 @@ export type DividerProps = {
 
 /**
  * {@link https://cultureamp.atlassian.net/wiki/spaces/DesignSystem/pages/3082061035/Divider Guidance} |
- * {@link https://cultureamp.design/?path=/docs/components-divider--default-story Storybook}
+ * {@link https://cultureamp.design/?path=/docs/components-divider--docs Storybook}
  */
 export const Divider = ({
   variant,
