26acc7894a1646affbb508a422b0582e3b6926b0
Geoffrey Chong <gyf.chong@gmail.com>
2023-09-18 15:49:52 +1000

    a working radio example

--- packages/components/src/Radio/RadioGroup/_docs/RadioGroup.stories.tsx
+++ packages/components/src/Radio/RadioGroup/_docs/RadioGroup.stories.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import { Meta, StoryObj } from "@storybook/react"
 import { RadioField } from "~components/Radio/RadioField"
 import { RadioGroup } from "../index"
@@ -16,29 +16,43 @@ export default meta
 type Story = StoryObj<typeof meta>
 
 export const Playground: Story = {
-  render: args => (
-    <RadioGroup {...args}>
-      <RadioField
-        labelText="Label"
-        name="radio"
-        value="radio-1"
-        reversed={args?.reversed}
-        selectedStatus
-      />
-      <RadioField
-        labelText="Label"
-        name="radio"
-        value="radio-2"
-        reversed={args?.reversed}
-      />
-      <RadioField
-        labelText="Label"
-        name="radio"
-        value="radio-3"
-        reversed={args?.reversed}
-      />
-    </RadioGroup>
-  ),
+  render: args => {
+    const [selected, setSelected] = useState("radio-1")
+    return (
+      <RadioGroup {...args}>
+        <RadioField
+          labelText="Label"
+          name="radio"
+          value="radio-1"
+          reversed={args?.reversed}
+          onChange={event => {
+            setSelected(event.target.value)
+          }}
+          selectedStatus={selected === "radio-1"}
+        />
+        <RadioField
+          labelText="Label"
+          name="radio"
+          value="radio-2"
+          reversed={args?.reversed}
+          onChange={event => {
+            setSelected(event.target.value)
+          }}
+          selectedStatus={selected === "radio-2"}
+        />
+        <RadioField
+          labelText="Label"
+          name="radio"
+          value="radio-3"
+          reversed={args?.reversed}
+          onChange={event => {
+            setSelected(event.target.value)
+          }}
+          selectedStatus={selected === "radio-3"}
+        />
+      </RadioGroup>
+    )
+  },
   parameters: {
     docs: {
       canvas: {
