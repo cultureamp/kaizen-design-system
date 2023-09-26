56c82d5d741593a569c76096ad10ca37e1beaec0
Cassandra Tam <cassandra.tam@cultureamp.com>
2023-09-26 17:47:34 +1000

    docs(input-search): add stickersheets

---
+++ packages/components/src/InputSearch/_docs/InputSearch.stickersheet.stories.tsx
@@ -0,0 +1,69 @@
+import React from "react"
+import { Meta } from "@storybook/react"
+import {
+  StickerSheet,
+  StickerSheetStory,
+} from "~storybook/components/StickerSheet"
+import { InputSearch } from "../index"
+
+export default {
+  title: "Components/InputSearch",
+  parameters: {
+    chromatic: { disable: false },
+    controls: { disable: true },
+  },
+} satisfies Meta
+
+const StickerSheetTemplate: StickerSheetStory = {
+  render: ({ isReversed }) => (
+    <StickerSheet isReversed={isReversed}>
+      <StickerSheet.Header headings={["Default", "Hover", "Focus"]} />
+      <StickerSheet.Body>
+        <StickerSheet.Row>
+          <InputSearch id="input-search--default" reversed={isReversed} />
+          <InputSearch
+            id="input-search--hover"
+            reversed={isReversed}
+            classNameOverride="story__input-search--hover"
+          />
+          <InputSearch
+            id="input-search--focus"
+            reversed={isReversed}
+            classNameOverride="story__input-search--focus"
+          />
+        </StickerSheet.Row>
+      </StickerSheet.Body>
+    </StickerSheet>
+  ),
+  parameters: {
+    pseudo: {
+      hover: ".story__input-search--hover",
+      focusWithin: ".story__input-search--focus",
+      focus: ".story__input-search--focus > input",
+    },
+  },
+}
+
+export const StickerSheetDefault: StickerSheetStory = {
+  ...StickerSheetTemplate,
+  name: "Sticker Sheet (Default)",
+}
+
+export const StickerSheetReversed: StickerSheetStory = {
+  ...StickerSheetTemplate,
+  name: "Sticker Sheet (Reversed)",
+  parameters: {
+    ...StickerSheetTemplate.parameters,
+    backgrounds: { default: "Purple 700" },
+  },
+  args: { isReversed: true },
+}
+
+export const StickerSheetRTL: StickerSheetStory = {
+  ...StickerSheetTemplate,
+  name: "Sticker Sheet (RTL)",
+  parameters: {
+    ...StickerSheetTemplate.parameters,
+    textDirection: "rtl",
+  },
+}
