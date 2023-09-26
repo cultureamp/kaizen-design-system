453e6a06be969e47ff61a0098fc4a54153569390
Michael Winter <mcwinter07@gmail.com>
2023-09-26 10:03:58 +1000

    feat(FieldMessage): migrate and update test formats

--- packages/components/src/FieldMessage/FieldMessage.spec.tsx
+++ packages/components/src/FieldMessage/FieldMessage.spec.tsx
@@ -2,17 +2,75 @@ import React from "react"
 import { render } from "@testing-library/react"
 import { FieldMessage, FieldMessageProps } from "./FieldMessage"
 
-const FieldMessageWrapper = (customProps?: Partial<FieldMessageProps>): JSX.Element => (
+const defaultFieldMessageProps = {
+  id: "someFieldMessageId",
+  message: "Some FieldMessage.",
+}
+
+const FieldMessageWrapper = (
+  props: Partial<FieldMessageProps>
+): JSX.Element => (
   <FieldMessage
-    exampleRequiredString="Hello!" /** @todo: Add default values for your required props (override them with customProps if needed) */
-    {...customProps}
+    id={defaultFieldMessageProps.id}
+    message={defaultFieldMessageProps.message}
+    {...props}
   />
 )
 
 describe("<FieldMessage />", () => {
-  it("does something", () => {
-    render(<FieldMessageWrapper />)
-    /** @todo: Fill in test case */
-    expect(true).toBe(false)
+  it("renders a message within a <p> tag when given a string", () => {
+    const { getByText } = render(
+      <FieldMessageWrapper message="Hello I am a message" />
+    )
+    const labelContainer = getByText("Hello I am a message")
+
+    expect(labelContainer).toBeInTheDocument()
+    expect(labelContainer?.tagName === "P").toBeTruthy()
+  })
+
+  it("renders a message within a <div> tag when not given node other than string", () => {
+    const { getByText } = render(
+      <FieldMessageWrapper
+        message={<span>Hello I am a message within a span</span>}
+      />
+    )
+
+    const labelContainer = getByText("Hello I am a message within a span")
+
+    expect(labelContainer).toBeInTheDocument()
+    expect(labelContainer?.tagName === "SPAN").toBeTruthy()
+    expect(labelContainer?.parentElement?.tagName === "DIV").toBeTruthy()
+  })
+
+  it("renders an `id` attribute", () => {
+    const { getByTestId } = render(
+      <FieldMessageWrapper data-testid="id--fieldMessage" id="id--test" />
+    )
+
+    expect(getByTestId("id--fieldMessage")).toHaveAttribute("id", "id--test")
+  })
+
+  it("renders a `reversed` field message", () => {
+    const { container } = render(<FieldMessageWrapper reversed />)
+
+    expect(container.querySelector(".reversed")).toBeTruthy()
+  })
+
+  it("renders a warning icon with an error status", () => {
+    const { container, getByLabelText } = render(
+      <FieldMessageWrapper status="error" />
+    )
+
+    expect(container.querySelector(".warningIcon")).toBeTruthy()
+    expect(getByLabelText("error message")).toBeInTheDocument()
+  })
+
+  it("renders a warning icon with an error status", () => {
+    const { container, getByLabelText } = render(
+      <FieldMessageWrapper status="caution" />
+    )
+
+    expect(container.querySelector(".warningIcon")).toBeTruthy()
+    expect(getByLabelText("caution message")).toBeInTheDocument()
   })
 })
