a3e11f430e1b2143043acbd89ae6930fd81f7d79
Cassandra Tam <cassandra.tam@cultureamp.com>
2023-09-28 17:06:02 +1000

    docs(select): add docs

--- packages/components/src/Select/_docs/Select.stories.tsx
+++ packages/components/src/Select/_docs/Select.stories.tsx
@@ -1,10 +1,12 @@
 import React from "react"
 import { Meta, StoryObj } from "@storybook/react"
-import Highlight from "react-highlight"
-import { Text } from "~components/Text"
 import { Select } from "../Select"
-// import { SelectOption } from "../types"
-import { groupedMockItems, singleMockItems } from "./mockData"
+import { SelectOption } from "../types"
+import {
+  groupedMockItems,
+  mixedMockItemsDisabled,
+  singleMockItems,
+} from "./mockData"
 
 const meta = {
   title: "Components/Select",
@@ -37,69 +39,105 @@ export default meta
 type Story = StoryObj<typeof meta>
 
 export const Playground: Story = {
+  parameters: {
+    docs: {
+      canvas: {
+        sourceState: "shown",
+      },
+    },
+  },
+}
+
+export const SingleItems: Story = {
+  args: {
+    items: singleMockItems,
+  },
+}
+
+export const GroupedItems: Story = {
+  args: {
+    items: groupedMockItems,
+  },
+}
+
+export const DisabledItems: Story = {
+  args: {
+    items: mixedMockItemsDisabled,
+  },
+}
+
+const sourceCodeCustomiseTrigger = `
+<Select
+  trigger={props => <Select.TriggerButton {...props} id="select--custom-trigger" />}
+/>
+`
+export const CustomiseTrigger: Story = {
+  args: {
+    trigger: props => (
+      <Select.TriggerButton {...props} id="select--custom-trigger" />
+    ),
+  },
+  parameters: {
+    docs: {
+      source: {
+        code: sourceCodeCustomiseTrigger,
+      },
+    },
+  },
+}
+
+export const AdditionalProperties: Story = {
+  render: args => (
+    <Select<SelectOption & { isFruit: boolean }>
+      {...args}
+      label="Custom"
+      items={[
+        { label: "Bubblegum", value: "bubblegum", isFruit: false },
+        { label: "Strawberry", value: "strawberry", isFruit: true },
+        { label: "Chocolate", value: "chocolate", isFruit: false },
+        { label: "Apple", value: "apple", isFruit: true },
+        { label: "Lemon", value: "lemon", isFruit: true },
+      ]}
+    >
+      {({ items }): JSX.Element[] =>
+        items.map(item =>
+          item.type === "item" ? (
+            <Select.Option
+              key={item.key}
+              item={{
+                ...item,
+                rendered: item.value?.isFruit
+                  ? `${item.rendered} (Fruit)`
+                  : item.rendered,
+              }}
+            />
+          ) : (
+            <Select.ItemDefaultRender key={item.key} item={item} />
+          )
+        )
+      }
+    </Select>
+  ),
   parameters: { docs: { source: { type: "code" } } },
 }
 
-export const WithSections: Story = {
-  args: {
-    items: groupedMockItems,
-    placeholder: "Placeholder",
-  },
-  decorators: [
-    Story => (
-      <>
-        <Story />
-        <div className="mt-4">
-          <Text variant="body">Items: </Text>
-          <Highlight className="json">
-            {JSON.stringify(groupedMockItems, null, 2)}
-          </Highlight>
-        </div>
-      </>
-    ),
-  ],
+export const Validation: Story = {
+  render: args => (
+    <div className="flex gap-16">
+      <Select
+        {...args}
+        status="error"
+        validationMessage="This is an error message"
+      />
+      <Select
+        {...args}
+        status="caution"
+        validationMessage="This is a cautionary message"
+      />
+    </div>
+  ),
 }
 
-// /**
-//  * Extend the option type to have additional properties to use for rendering.
-//  */
-// export const AdditionalProperties: Story = {
-//   render: args => {
-//     const [isOpen, setIsOpen] = useState<boolean>(false)
-
-//     return (
-//       <Select<SelectOption & { isFruit: boolean }>
-//         {...args}
-//         label="Custom"
-//         isOpen={isOpen}
-//         setIsOpen={setIsOpen}
-//         items={[
-//           { label: "Bubblegum", value: "bubblegum", isFruit: false },
-//           { label: "Strawberry", value: "strawberry", isFruit: true },
-//           { label: "Chocolate", value: "chocolate", isFruit: false },
-//           { label: "Apple", value: "apple", isFruit: true },
-//           { label: "Lemon", value: "lemon", isFruit: true },
-//         ]}
-//       >
-//         {({ items }): JSX.Element[] =>
-//           items.map(item =>
-//             item.type === "item" ? (
-//               <Select.Option
-//                 key={item.key}
-//                 item={{
-//                   ...item,
-//                   rendered: item.value?.isFruit
-//                     ? `${item.rendered} (Fruit)`
-//                     : item.rendered,
-//                 }}
-//               />
-//             ) : (
-//               <Select.ItemDefaultRender key={item.key} item={item} />
-//             )
-//           )
-//         }
-//       </Select>
-//     )
-//   },
-//   name: "Additional option properties",
-// }
+export const FullWidth: Story = {
+  args: { isFullWidth: true },
+}
