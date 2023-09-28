afe555a3941619265d319a77f836fd7cabe580af
Cassandra Tam <cassandra.tam@cultureamp.com>
2023-09-28 20:26:42 +1000

    docs(select): add stickersheet for options

--- packages/components/src/Select/_docs/Select.stickersheet.stories.tsx
+++ packages/components/src/Select/_docs/Select.stickersheet.stories.tsx
@@ -1,11 +1,20 @@
 import React from "react"
 import { Meta } from "@storybook/react"
+import isChromatic from "chromatic"
 import {
   StickerSheet,
   StickerSheetStory,
 } from "~storybook/components/StickerSheet"
 import { Select } from "../index"
-import { singleMockItems } from "./mockData"
+import {
+  groupedMockItems,
+  mixedMockItemsDisabled,
+  mixedMockItemsUngroupedFirst,
+  mixedMockItemsUnordered,
+  singleMockItems,
+} from "./mockData"
+
+const IS_CHROMATIC = isChromatic()
 
 export default {
   title: "Components/Select",
@@ -194,6 +203,263 @@ export const StickerSheetRTL: StickerSheetStory = {
   name: "Sticker Sheet (RTL)",
   parameters: {
     ...StickerSheetTemplate.parameters,
+    textDirection: "rtl",
+  },
+}
+
+const StickerSheetOptionsTemplate: StickerSheetStory = {
+  render: () => (
+    <>
+      <StickerSheet
+        heading="Default options"
+        style={{ paddingBottom: IS_CHROMATIC ? "26rem" : undefined }}
+      >
+        <StickerSheet.Header
+          headings={["Single items", "Groups", "Existing value", "Disabled"]}
+        />
+        <StickerSheet.Body>
+          <StickerSheet.Row>
+            <StickerSheet.Cell style={{ width: "250px" }}>
+              <Select
+                label="Label"
+                defaultOpen={IS_CHROMATIC}
+                items={singleMockItems}
+              />
+            </StickerSheet.Cell>
+            <StickerSheet.Cell style={{ width: "250px" }}>
+              <Select
+                label="Label"
+                defaultOpen={IS_CHROMATIC}
+                items={groupedMockItems}
+              />
+            </StickerSheet.Cell>
+            <StickerSheet.Cell style={{ width: "250px" }}>
+              <Select
+                label="Label"
+                defaultOpen={IS_CHROMATIC}
+                items={singleMockItems}
+                selectedKey="mocha"
+              />
+            </StickerSheet.Cell>
+            <StickerSheet.Cell style={{ width: "250px" }}>
+              <Select
+                label="Label"
+                defaultOpen={IS_CHROMATIC}
+                items={mixedMockItemsDisabled}
+              />
+            </StickerSheet.Cell>
+          </StickerSheet.Row>
+        </StickerSheet.Body>
+      </StickerSheet>
+
+      <StickerSheet
+        heading="Customised options"
+        style={{ paddingTop: IS_CHROMATIC ? "26rem" : undefined }}
+      >
+        <StickerSheet.Header
+          headings={[
+            "All options",
+            "Partial customisation",
+            "Section dividers",
+            "Specific item divider",
+          ]}
+        />
+        <StickerSheet.Body>
+          <StickerSheet.Row>
+            <StickerSheet.Cell style={{ width: "250px" }}>
+              <Select
+                label="Label"
+                defaultOpen={IS_CHROMATIC}
+                items={singleMockItems}
+              >
+                {({ items }): JSX.Element[] =>
+                  items.map(item => {
+                    if (item.type === "item") {
+                      return (
+                        <Select.Option
+                          key={item.key}
+                          item={{
+                            ...item,
+                            rendered: (
+                              <div>
+                                <div
+                                  style={{
+                                    fontSize: "0.75rem",
+                                    color: "gray",
+                                  }}
+                                >
+                                  Customised in list!
+                                </div>
+                                <div>{item.rendered}</div>
+                              </div>
+                            ),
+                          }}
+                        />
+                      )
+                    }
+
+                    return (
+                      <Select.ItemDefaultRender key={item.key} item={item} />
+                    )
+                  })
+                }
+              </Select>
+            </StickerSheet.Cell>
+
+            <StickerSheet.Cell style={{ width: "250px" }}>
+              <Select
+                label="Label"
+                defaultOpen={IS_CHROMATIC}
+                items={mixedMockItemsUngroupedFirst}
+              >
+                {({ items }): JSX.Element[] =>
+                  items.map(item => {
+                    if (item.type === "item" && item.key === "batch-brew") {
+                      return (
+                        <Select.Option
+                          key={item.key}
+                          item={{
+                            ...item,
+                            rendered: <div>++ {item.rendered}</div>,
+                          }}
+                        />
+                      )
+                    }
+
+                    if (item.type === "section" && item.key === "Syrup") {
+                      return (
+                        <Select.Section
+                          key={item.key}
+                          section={{
+                            ...item,
+                            childNodes: Array.from(item.childNodes).map(
+                              child => ({
+                                ...child,
+                                rendered: <div>-- {child.rendered}</div>,
+                              })
+                            ),
+                          }}
+                        />
+                      )
+                    }
+
+                    return (
+                      <Select.ItemDefaultRender key={item.key} item={item} />
+                    )
+                  })
+                }
+              </Select>
+            </StickerSheet.Cell>
+
+            <StickerSheet.Cell style={{ width: "250px" }}>
+              <Select
+                label="Label"
+                defaultOpen={IS_CHROMATIC}
+                items={mixedMockItemsUnordered}
+              >
+                {({ items }): JSX.Element[] =>
+                  items.map(item => (
+                    <React.Fragment key={item.key}>
+                      {item.type === "section" && <Select.SectionDivider />}
+                      <Select.ItemDefaultRender item={item} />
+                      {item.type === "section" && <Select.SectionDivider />}
+                    </React.Fragment>
+                  ))
+                }
+              </Select>
+            </StickerSheet.Cell>
+
+            <StickerSheet.Cell style={{ width: "250px" }}>
+              <Select
+                label="Label"
+                defaultOpen={IS_CHROMATIC}
+                items={[
+                  { label: "Customise...", value: "custom" },
+                  ...singleMockItems,
+                ]}
+              >
+                {({ items }): JSX.Element[] =>
+                  items.map(item => {
+                    if (item.type === "item" && item.key === "custom") {
+                      return (
+                        <React.Fragment key={item.key}>
+                          <Select.Option item={item} />
+                          <Select.SectionDivider />
+                        </React.Fragment>
+                      )
+                    }
+
+                    return (
+                      <Select.ItemDefaultRender key={item.key} item={item} />
+                    )
+                  })
+                }
+              </Select>
+            </StickerSheet.Cell>
+          </StickerSheet.Row>
+        </StickerSheet.Body>
+      </StickerSheet>
+
+      <StickerSheet
+        heading="Min/Max"
+        style={{ paddingTop: IS_CHROMATIC ? "26rem" : undefined }}
+      >
+        <StickerSheet.Header headings={["Min size", "Max size"]} />
+        <StickerSheet.Body>
+          <StickerSheet.Row>
+            <StickerSheet.Cell style={{ width: "250px" }}>
+              <Select
+                label="Label"
+                defaultOpen={IS_CHROMATIC}
+                items={[{ value: "a", label: "A" }]}
+              />
+            </StickerSheet.Cell>
+            <StickerSheet.Cell>
+              <Select
+                label="Label"
+                defaultOpen={IS_CHROMATIC}
+                items={[
+                  {
+                    value: "long-1",
+                    label:
+                      "Super long option where the container is fixed width and the selected option goes multiline",
+                  },
+                  {
+                    value: "long-2",
+                    label:
+                      "Another super long option where the container is fixed width and the selected option goes multiline",
+                  },
+                  ...singleMockItems,
+                ]}
+              />
+            </StickerSheet.Cell>
+          </StickerSheet.Row>
+        </StickerSheet.Body>
+      </StickerSheet>
+    </>
+  ),
+}
+
+export const StickerSheetOptionsDefault: StickerSheetStory = {
+  ...StickerSheetOptionsTemplate,
+  name: "Sticker Sheet - Options (Default)",
+}
+
+export const StickerSheetOptionsReversed: StickerSheetStory = {
+  ...StickerSheetOptionsTemplate,
+  name: "Sticker Sheet - Options (Reversed)",
+  parameters: {
+    ...StickerSheetOptionsTemplate.parameters,
+    backgrounds: { default: "Purple 700" },
+  },
+  args: { isReversed: true },
+}
+
+export const StickerSheetOptionsRTL: StickerSheetStory = {
+  ...StickerSheetOptionsTemplate,
+  name: "Sticker Sheet - Options (RTL)",
+  parameters: {
+    ...StickerSheetOptionsTemplate.parameters,
     textDirection: "rtl",
   },
 }
