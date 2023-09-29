164ed6d4a1e0f7cdaed189e97fa02da3ea518980
Geoffrey Chong <gyf.chong@gmail.com>
2023-09-29 16:49:57 +1000

    updated Checkbox playground

--- packages/components/src/Checkbox/Checkbox/_docs/Checkbox.stories.tsx
+++ packages/components/src/Checkbox/Checkbox/_docs/Checkbox.stories.tsx
@@ -1,9 +1,14 @@
+import React, { useEffect, useState } from "react"
 import { Meta, StoryObj } from "@storybook/react"
+import { CheckboxFieldProps } from "@kaizen/draft-form"
 import { Checkbox } from "../index"
 
 const meta = {
   title: "KAIO-staging/Checkbox controls/Checkbox",
   component: Checkbox,
+  args: {
+    checkedStatus: "off",
+  },
   parameters: {
     a11y: {
       config: {
@@ -24,6 +29,27 @@ export default meta
 type Story = StoryObj<typeof meta>
 
 export const Playground: Story = {
+  render: ({ onCheck, checkedStatus, ...props }) => {
+    const [status, setStatus] =
+      useState<CheckboxFieldProps["checkedStatus"]>(checkedStatus)
+
+    const handleChange: React.ChangeEventHandler<HTMLInputElement> = e => {
+      if (status === "off") {
+        setStatus("mixed")
+      } else if (status === "mixed") {
+        setStatus("on")
+      } else if (status === "on") {
+        setStatus("off")
+      }
+      onCheck?.(e)
+    }
+
+    useEffect(() => {
+      setStatus(checkedStatus)
+    }, [checkedStatus])
+
+    return <Checkbox {...props} checkedStatus={status} onCheck={handleChange} />
+  },
   parameters: {
     docs: {
       canvas: {
