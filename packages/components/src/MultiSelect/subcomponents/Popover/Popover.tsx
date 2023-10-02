099d6434f485da5dde2bc5d2ee7bdf11026f0532
Cassandra Tam <cassandra.tam@cultureamp.com>
2023-10-02 21:18:38 +1100

    fix(popover): use flip middleware

--- packages/components/src/MultiSelect/subcomponents/Popover/Popover.tsx
+++ packages/components/src/MultiSelect/subcomponents/Popover/Popover.tsx
@@ -1,12 +1,12 @@
 import React, { HTMLAttributes } from "react"
 import { createPortal } from "react-dom"
 import {
-  useFloating,
+  autoUpdate,
+  flip,
   offset,
+  useFloating,
   UseFloatingReturn,
   UseFloatingOptions,
-  autoPlacement,
-  autoUpdate,
 } from "@floating-ui/react-dom"
 import classnames from "classnames"
 import { FocusOn } from "react-focus-on"
@@ -40,28 +40,7 @@ export const Popover = ({
       floating: refs.floating.current,
     },
     placement: "bottom-start",
-    middleware: [
-      offset(15),
-      autoPlacement(state =>
-        state.platform.isRTL?.(state.elements.reference)
-          ? {
-              allowedPlacements: [
-                "bottom-end",
-                "bottom-start",
-                "top-end",
-                "top-start",
-              ],
-            }
-          : {
-              allowedPlacements: [
-                "bottom-start",
-                "bottom-end",
-                "top-start",
-                "top-end",
-              ],
-            }
-      ),
-    ],
+    middleware: [offset(15), flip()],
     whileElementsMounted: autoUpdate,
     ...floatingOptions,
   })
