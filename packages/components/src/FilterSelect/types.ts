4f561ac1864e4643faaab7eaedd1a99a1152cf51
Cassandra Tam <cassandra.tam@cultureamp.com>
2023-10-02 21:03:09 +1100

    chore: add comment for FilterSelect types

--- packages/components/src/FilterSelect/types.ts
+++ packages/components/src/FilterSelect/types.ts
@@ -1,1 +1,3 @@
+// @todo: Remove this and use the types from Select when it is moved out from future.
+// This is currently here to allow consumers access to the types from the entrypoint.
 export * from "~components/__future__/Select/types"
