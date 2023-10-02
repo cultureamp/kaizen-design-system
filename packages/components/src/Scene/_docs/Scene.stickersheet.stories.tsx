927ccc1dfb47457e88fe41c8dfd85537cd4b3b85
jakepitman <jakeseanp@gmail.com>
2023-10-02 15:03:40 +1300

    Remove spec and fill in stickersheet

--- packages/components/src/Scene/_docs/Scene.stickersheet.stories.tsx
+++ packages/components/src/Scene/_docs/Scene.stickersheet.stories.tsx
@@ -4,7 +4,53 @@ import {
   StickerSheet,
   StickerSheetStory,
 } from "~storybook/components/StickerSheet"
-import { Scene } from "../index"
+import {
+  EmptyStatesAction,
+  EmptyStatesInformative,
+  EmptyStatesNegative,
+  EmptyStatesNeutral,
+  EmptyStatesPositive,
+  Information360Upgrade,
+  InformationDemographicFocus,
+  InformationEmergingTrends,
+  InformationEmployeeLifecycle,
+  InformationReportOwner,
+  InformationReportOwnerByRule,
+  InformationTurnoverCalculator,
+  InformationTurnoverForecast,
+  Collaboration,
+  Communication,
+  CompanyValues,
+  ConnectTheDots,
+  CultureLab,
+  TermsAgreement,
+  Programs,
+  PerformanceCompanySettings,
+  EngagementSurveySummaryFemale,
+  EngagementSurveySummaryMale,
+  BrandMomentNewAccountOnboarding,
+  BrandMomentUploadEmployeeData,
+  BrandMomentPositiveOutro,
+  BrandMomentLogin,
+  BrandMomentError,
+  BrandMomentStarterKit,
+  SkillsCoach1On1Meetings,
+  SkillsCoachCoaching,
+  SkillsCoachEmployeeDevelopment,
+  SkillsCoachEssentialFeedback,
+  SkillsCoachEssentialProductivity,
+  SkillsCoachEssentialResilience,
+  SkillsCoachInfluentialCommunication,
+  SkillsCoachLeadingChange,
+  SkillsCoachFeedback,
+  SkillsCoachManagerHub,
+  SkillsCoachProductivity,
+  SkillsCoachRemoteManager,
+  SkillsCoachResilience,
+  SkillsCoachStrategy,
+  SurveyGetStarted,
+  SurveyOverviewClosed,
+} from ".."
 
 export default {
   title: "Components/Scene",
@@ -18,18 +64,146 @@ const StickerSheetTemplate: StickerSheetStory = {
   render: ({ isReversed }) => (
     /** @note: If you have multiple StickerSheets to display, you can add a `heading` */
     <StickerSheet isReversed={isReversed}>
-      {/* @note: Header is optional */}
-      <StickerSheet.Header headings={["COLUMN 1", "COLUMN 2"]} />
+      <StickerSheet.Header headings={["Scene"]} />
       <StickerSheet.Body>
         <StickerSheet.Row>
-          {/** @todo: Add column 1 + row 1 props */}
-          <Scene exampleRequiredString="One" isReversed={isReversed} />
-          {/** @todo: Add column 2 + row 1 props */}
-          <Scene exampleRequiredString="Two" isReversed={isReversed} />
+          <EmptyStatesAction />
+        </StickerSheet.Row>
+        <StickerSheet.Row>
+          <EmptyStatesInformative />
+        </StickerSheet.Row>
+        <StickerSheet.Row>
+          <EmptyStatesNegative />
+        </StickerSheet.Row>
+        <StickerSheet.Row>
+          <EmptyStatesNeutral />
+        </StickerSheet.Row>
+        <StickerSheet.Row>
+          <EmptyStatesPositive />
+        </StickerSheet.Row>
+        <StickerSheet.Row>
+          <Information360Upgrade />
+        </StickerSheet.Row>
+        <StickerSheet.Row>
+          <InformationDemographicFocus />
+        </StickerSheet.Row>
+        <StickerSheet.Row>
+          <InformationEmergingTrends />
+        </StickerSheet.Row>
+        <StickerSheet.Row>
+          <InformationEmployeeLifecycle />
+        </StickerSheet.Row>
+        <StickerSheet.Row>
+          <InformationReportOwner />
+        </StickerSheet.Row>
+        <StickerSheet.Row>
+          <InformationReportOwnerByRule />
+        </StickerSheet.Row>
+        <StickerSheet.Row>
+          <InformationTurnoverCalculator />
+        </StickerSheet.Row>
+        <StickerSheet.Row>
+          <InformationTurnoverForecast />
+        </StickerSheet.Row>
+        <StickerSheet.Row>
+          <Collaboration />
+        </StickerSheet.Row>
+        <StickerSheet.Row>
+          <Communication />
+        </StickerSheet.Row>
+        <StickerSheet.Row>
+          <CompanyValues />
+        </StickerSheet.Row>
+        <StickerSheet.Row>
+          <ConnectTheDots />
+        </StickerSheet.Row>
+        <StickerSheet.Row>
+          <CultureLab />
+        </StickerSheet.Row>
+        <StickerSheet.Row>
+          <TermsAgreement />
+        </StickerSheet.Row>
+        <StickerSheet.Row>
+          <Programs />
+        </StickerSheet.Row>
+        <StickerSheet.Row>
+          <PerformanceCompanySettings />
+        </StickerSheet.Row>
+        <StickerSheet.Row>
+          <EngagementSurveySummaryFemale />
+        </StickerSheet.Row>
+        <StickerSheet.Row>
+          <EngagementSurveySummaryMale />
+        </StickerSheet.Row>
+        <StickerSheet.Row>
+          <BrandMomentNewAccountOnboarding />
+        </StickerSheet.Row>
+        <StickerSheet.Row>
+          <BrandMomentUploadEmployeeData />
+        </StickerSheet.Row>
+        <StickerSheet.Row>
+          <BrandMomentPositiveOutro />
+        </StickerSheet.Row>
+        <StickerSheet.Row>
+          <BrandMomentLogin />
+        </StickerSheet.Row>
+        <StickerSheet.Row>
+          <BrandMomentError />
+        </StickerSheet.Row>
+        <StickerSheet.Row>
+          <BrandMomentStarterKit />
+        </StickerSheet.Row>
+        <StickerSheet.Row>
+          <SkillsCoach1On1Meetings />
+        </StickerSheet.Row>
+        <StickerSheet.Row>
+          <SkillsCoachCoaching />
+        </StickerSheet.Row>
+        <StickerSheet.Row>
+          <SkillsCoachEmployeeDevelopment />
+        </StickerSheet.Row>
+        <StickerSheet.Row>
+          <SkillsCoachEssentialFeedback />
+        </StickerSheet.Row>
+        <StickerSheet.Row>
+          <SkillsCoachEssentialProductivity />
+        </StickerSheet.Row>
+        <StickerSheet.Row>
+          <SkillsCoachEssentialResilience />
+        </StickerSheet.Row>
+        <StickerSheet.Row>
+          <SkillsCoachInfluentialCommunication />
+        </StickerSheet.Row>
+        <StickerSheet.Row>
+          <SkillsCoachLeadingChange />
+        </StickerSheet.Row>
+        <StickerSheet.Row>
+          <SkillsCoachFeedback />
+        </StickerSheet.Row>
+        <StickerSheet.Row>
+          <SkillsCoachManagerHub />
+        </StickerSheet.Row>
+        <StickerSheet.Row>
+          <SkillsCoachProductivity />
+        </StickerSheet.Row>
+        <StickerSheet.Row>
+          <SkillsCoachRemoteManager />
+        </StickerSheet.Row>
+        <StickerSheet.Row>
+          <SkillsCoachResilience />
+        </StickerSheet.Row>
+        <StickerSheet.Row>
+          <SkillsCoachStrategy />
+        </StickerSheet.Row>
+        <StickerSheet.Row>
+          <SurveyGetStarted />
+        </StickerSheet.Row>
+        <StickerSheet.Row>
+          <SurveyOverviewClosed />
         </StickerSheet.Row>
       </StickerSheet.Body>
     </StickerSheet>
-  )
+  ),
 }
 
 export const StickerSheetDefault: StickerSheetStory = {
