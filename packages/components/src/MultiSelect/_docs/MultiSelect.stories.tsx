f38c4937e2bad4ef5f6e07c141aacec7c2d54740
Cassandra Tam <cassandra.tam@cultureamp.com>
2023-10-03 12:19:31 +1100

    docs(multi-select): fix docs page

--- packages/components/src/MultiSelect/_docs/MultiSelect.stories.tsx
+++ packages/components/src/MultiSelect/_docs/MultiSelect.stories.tsx
@@ -5,8 +5,26 @@ import { MultiSelect, MultiSelectProps } from "../index"
 const meta = {
   title: "Components/MultiSelect",
   component: MultiSelect,
+  argTypes: {
+    selectedValues: {
+      options: ["None", "Single", "Multiple"],
+      control: {
+        type: "select",
+        labels: {
+          None: "new Set()",
+          Single: 'new Set(["waffle"])',
+          Multiple: 'new Set(["waffle", "pancakes"])',
+        },
+      },
+      mapping: {
+        None: new Set(),
+        Single: new Set(["waffle"]),
+        Multiple: new Set(["waffle", "pancakes"]),
+      },
+    },
+  },
   args: {
-    label: "Jalapeno",
+    label: "Breakfast order",
     selectedValues: new Set(),
     isOpen: false,
     items: [
@@ -41,6 +59,10 @@ const MultiSelectTemplate: Story = {
       setIsOpen(args.isOpen)
     }, [args.isOpen])
 
+    useEffect(() => {
+      setSelectedValues(args.selectedValues)
+    }, [args.selectedValues])
+
     return (
       <MultiSelect
         {...args}
@@ -62,4 +84,62 @@ export const Playground: Story = {
       },
     },
   },
+}
+
+const sourceCodeItems = `
+<MultiSelect
+  items={[
+    { label: "Pancakes", value: "pancakes" },
+    { label: "Waffles", value: "waffles" },
+    { label: "Toastie", value: "toastie" },
+  ]}
+  {...props}
+/>
+`
+
+export const Items: Story = {
+  ...MultiSelectTemplate,
+  parameters: { docs: { source: { code: sourceCodeItems } } },
+}
+
+const sourceCodeSelectedValues = `
+const [selectedValues, setSelectedValues] = useState<MultiSelectProps["selectedValues"]>(new Set(["waffle"]))
+
+return (
+  <MultiSelect
+    items={[
+      { label: "Pancakes", value: "pancakes" },
+      { label: "Waffles", value: "waffles" },
+      { label: "Toastie", value: "toastie" },
+    ]}
+    selectedValues={selectedValues}
+    onSelectedValuesChange={setSelectedValues}
+    {...props}
+  />
+)
+`
+
+export const SelectedValues: Story = {
+  ...MultiSelectTemplate,
+  args: {
+    selectedValues: new Set(["waffle"]),
+  },
+  parameters: { docs: { source: { code: sourceCodeSelectedValues } } },
+}
+
+const sourceCodeOpenState = `
+const [isOpen, setIsOpen] = useState<boolean>(false)
+
+return (
+  <MultiSelect
+    isOpen={isOpen}
+    onOpenChange={setIsOpen}
+    {...props}
+  />
+)
+`
+
+export const OpenState: Story = {
+  ...MultiSelectTemplate,
+  parameters: { docs: { source: { code: sourceCodeOpenState } } },
 }
