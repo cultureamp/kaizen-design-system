cebe37050b57891c7ec380bbf9a24743df5846d7
Michael Winter <mcwinter07@gmail.com>
2023-10-04 17:16:21 +1100

    fix(multi-select-option-field): fix long unbroken text wrap on option labels

--- packages/components/src/MultiSelect/subcomponents/MultiSelectOptionField/_docs/MultiSelectOptionField.stickersheet.stories.tsx
+++ packages/components/src/MultiSelect/subcomponents/MultiSelectOptionField/_docs/MultiSelectOptionField.stickersheet.stories.tsx
@@ -27,47 +27,78 @@ const STATUS_ROWS = [
 
 const StickerSheetTemplate: Story = {
   render: () => (
-    <StickerSheet>
-      <StickerSheet.Header
-        headings={["Default", "Hover", "Focus"]}
-        hasVerticalHeadings
-      />
-      <StickerSheet.Body>
-        {STATUS_ROWS.map(({ title, status }) => (
-          <StickerSheet.Row key={title} rowTitle={title}>
-            <MultiSelectOptionField
-              id="jaffle--id"
-              onChange={action("jaffle clicked")}
-              option={{
-                label: "Jaffle",
-                value: "jaffle",
-              }}
-              checkedStatus={status}
-            />
-            <MultiSelectOptionField
-              id="waffle--id"
-              onChange={action("waffle clicked")}
-              option={{
-                label: "Waffle",
-                value: "waffle",
-              }}
-              data-sb-pseudo-styles="hover"
-              checkedStatus={status}
-            />
-            <MultiSelectOptionField
-              id="flapjack--id"
-              onChange={action("flapjack clicked")}
-              option={{
-                label: "Flapjack",
-                value: "flapjack",
-              }}
-              data-sb-pseudo-styles="focus"
-              checkedStatus={status}
-            />
+    <>
+      <StickerSheet heading="MultiSelectOptionField">
+        <StickerSheet.Header
+          headings={["Default", "Hover", "Focus"]}
+          hasVerticalHeadings
+        />
+        <StickerSheet.Body>
+          {STATUS_ROWS.map(({ title, status }) => (
+            <StickerSheet.Row key={title} rowTitle={title}>
+              <MultiSelectOptionField
+                id="id--jaffle"
+                onChange={action("jaffle clicked")}
+                option={{
+                  label: "Jaffle",
+                  value: "jaffle",
+                }}
+                checkedStatus={status}
+              />
+              <MultiSelectOptionField
+                id="id--waffle"
+                onChange={action("waffle clicked")}
+                option={{
+                  label: "Waffle",
+                  value: "waffle",
+                }}
+                data-sb-pseudo-styles="hover"
+                checkedStatus={status}
+              />
+              <MultiSelectOptionField
+                id="id--flapjack"
+                onChange={action("flapjack clicked")}
+                option={{
+                  label: "Flapjack",
+                  value: "flapjack",
+                }}
+                data-sb-pseudo-styles="focus"
+                checkedStatus={status}
+              />
+            </StickerSheet.Row>
+          ))}
+        </StickerSheet.Body>
+      </StickerSheet>
+
+      <StickerSheet heading="Long text wrap">
+        <StickerSheet.Body>
+          <StickerSheet.Row>
+            <div className=" w-280 border-solid border-gray-500 border-default">
+              <MultiSelectOptionField
+                id="id--john-long-name"
+                onChange={action("long name option clicked")}
+                option={{
+                  label:
+                    "johnTheJaffleEaterAndDevourerOfPancakesWithBlackPuddingAndASideOfBeans@yahoo.com",
+                  value: "email",
+                }}
+                checkedStatus="unchecked"
+              />
+              <MultiSelectOptionField
+                id="id--long-sentence"
+                onChange={action("long sentence option clicked")}
+                option={{
+                  label:
+                    "John was a jaffle eater and devourer of pancakes with black pudding and a side of beans",
+                  value: "sentence",
+                }}
+                checkedStatus="unchecked"
+              />
+            </div>
           </StickerSheet.Row>
-        ))}
-      </StickerSheet.Body>
-    </StickerSheet>
+        </StickerSheet.Body>
+      </StickerSheet>
+    </>
   ),
   parameters: {
     pseudo: {
