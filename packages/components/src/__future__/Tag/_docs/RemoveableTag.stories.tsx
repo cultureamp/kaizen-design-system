a334b9613ec1963285debb7d639b7de5764f284f
jakepitman <jakeseanp@gmail.com>
2023-10-04 16:54:54 +1300

    Add mdx file

--- packages/components/src/__future__/Tag/_docs/RemoveableTag.stories.tsx
+++ packages/components/src/__future__/Tag/_docs/RemoveableTag.stories.tsx
@@ -1,6 +1,8 @@
 import React from "react"
 import { Meta, StoryObj } from "@storybook/react"
 import { RemoveableTag } from "../RemoveableTag"
+import { AcademyIcon, ActionOffIcon, AddIcon, TagIcon } from "~components/Icons"
+import { TagColorKeys } from "../types"
 
 const meta = {
   title: "Components/RemoveableTag",
@@ -25,5 +27,71 @@ export const Playground: Story = {
         sourceState: "shown",
       },
     },
+  },
+}
+
+export const Children: StoryObj = {
+  args: { children: "This text is the children" },
+}
+
+export const Color: Story = {
+  render: () => (
+    <>
+      {TagColorKeys.map(color => (
+        <RemoveableTag
+          color={color}
+          icon={<TagIcon role="presentation" />}
+          key={color}
+          removeButtonProps={{
+            "aria-label": "close",
+            onClick: () => console.log("Clicked"),
+          }}
+        >
+          {color}
+        </RemoveableTag>
+      ))}
+    </>
+  ),
+}
+
+export const Icon: StoryObj = {
+  render: () => (
+    <>
+      <RemoveableTag
+        icon={<AcademyIcon role="presentation" />}
+        removeButtonProps={{
+          "aria-label": "close",
+          onClick: () => console.log("Clicked"),
+        }}
+      >
+        AcademyIcon
+      </RemoveableTag>
+      <RemoveableTag
+        color="yellow"
+        icon={<ActionOffIcon role="presentation" />}
+        removeButtonProps={{
+          "aria-label": "close",
+          onClick: () => console.log("Clicked"),
+        }}
+      >
+        ActionOffIcon
+      </RemoveableTag>
+      <RemoveableTag
+        color="green"
+        icon={<AddIcon role="presentation" />}
+        removeButtonProps={{
+          "aria-label": "close",
+          onClick: () => console.log("Clicked"),
+        }}
+      >
+        AddIcon
+      </RemoveableTag>
+    </>
+  ),
+}
+
+export const ClassNameOverride: Story = {
+  args: {
+    classNameOverride: "border-red-500 border-solid border-",
   },
 }
