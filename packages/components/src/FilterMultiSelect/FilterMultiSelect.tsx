2a1067a00e2e3892fc5f352e620096a304d517d6
Geoffrey Chong <gyf.chong@gmail.com>
2023-10-11 14:35:04 +1100

    fix dot notation for FMS

--- packages/components/src/FilterMultiSelect/FilterMultiSelect.tsx
+++ packages/components/src/FilterMultiSelect/FilterMultiSelect.tsx
@@ -1,35 +1,110 @@
+import React from "react"
+import { SelectionMode, Selection } from "@react-types/shared"
+import { ClearButton } from "@kaizen/draft-form"
+import {
+  MenuTriggerConsumer,
+  MenuTriggerProvider,
+  MenuTriggerProviderContextType,
+  MenuTriggerProviderProps,
+  SelectionConsumer,
+  SelectionProvider,
+  SelectionProviderContextType,
+} from "./context"
 import { ListBox } from "./subcomponents/ListBox"
 import { ListBoxSection } from "./subcomponents/ListBoxSection"
 import { LoadMoreButton } from "./subcomponents/LoadMoreButton"
 import { MenuFooter, MenuLoadingSkeleton } from "./subcomponents/MenuLayout"
+import { MenuPopup, MenuPopupProps } from "./subcomponents/MenuPopup"
 import { MultiSelectOption } from "./subcomponents/MultiSelectOption"
 import { NoResults } from "./subcomponents/NoResults"
-import { Root, RootProps } from "./subcomponents/Root"
-import { SearchInput } from "./subcomponents/SearchInput/SearchInput"
+import { SearchInput } from "./subcomponents/SearchInput"
 import { SectionDivider } from "./subcomponents/SectionDivider"
-import {
-  ClearButton,
-  SelectAllButton,
-} from "./subcomponents/SelectionControlButton"
+import { SelectAllButton } from "./subcomponents/SelectionControlButton"
 import {
   FilterTriggerButton,
   RemovableFilterTrigger,
 } from "./subcomponents/Trigger"
+import { ItemType } from "./types"
 
-export type FilterMultiSelectProps = RootProps
+type SelectionProps = {
+  label: string // provide A11y context for listbox
+  items: ItemType[]
+  selectedKeys?: Selection
+  defaultSelectedKeys?: Selection
+  onSelectionChange?: (keys: Selection) => void
+  selectionMode?: SelectionMode
+  onSearchInputChange?: (searchInput: string) => void
+}
 
-export const FilterMultiSelect = Object.assign(Root, {
-  TriggerButton: FilterTriggerButton,
-  RemovableTrigger: RemovableFilterTrigger,
-  SearchInput,
-  ListBox,
-  ListBoxSection,
-  SectionDivider,
-  Option: MultiSelectOption,
-  SelectAllButton,
-  ClearButton,
-  MenuFooter, // For layout
-  MenuLoadingSkeleton, // Menu Loading Skeleton example
-  LoadMoreButton,
-  NoResults,
-})
+export type FilterMultiSelectProps = {
+  trigger: (value?: MenuTriggerProviderContextType) => React.ReactNode
+  children: (value?: SelectionProviderContextType) => React.ReactNode // the content of the menu
+} & Omit<MenuPopupProps, "children"> &
+  Omit<MenuTriggerProviderProps, "children"> &
+  SelectionProps
+
+export const FilterMultiSelect = (
+  {
+    trigger,
+    children,
+    isOpen,
+    defaultOpen,
+    onOpenChange,
+    isLoading,
+    loadingSkeleton,
+    label,
+    items,
+    selectedKeys,
+    defaultSelectedKeys,
+    onSelectionChange,
+    selectionMode = "multiple",
+    onSearchInputChange,
+  }: FilterMultiSelectProps
+): JSX.Element => {
+  const menuTriggerProps = { isOpen, defaultOpen, onOpenChange }
+  const menuPopupProps = { isLoading, loadingSkeleton }
+  const disabledKeys: Selection = new Set(
+    items
+      ?.filter(item => item.isDisabled === true)
+      .map(disabledItem => disabledItem.value)
+  )
+  const selectionProps = {
+    label,
+    items,
+    selectedKeys,
+    defaultSelectedKeys,
+    onSelectionChange,
+    selectionMode,
+    disabledKeys,
+    onSearchInputChange,
+  }
+
+  return (
+    <MenuTriggerProvider {...menuTriggerProps}>
+      <div>
+        <MenuTriggerConsumer>{trigger}</MenuTriggerConsumer>
+        <MenuPopup {...menuPopupProps}>
+          <SelectionProvider {...selectionProps}>
+            <SelectionConsumer>{children}</SelectionConsumer>
+          </SelectionProvider>
+        </MenuPopup>
+      </div>
+    </MenuTriggerProvider>
+  )
+}
+
+FilterMultiSelect.displayName = "FilterMultiSelect"
+
+FilterMultiSelect.TriggerButton = FilterTriggerButton
+FilterMultiSelect.RemovableTrigger = RemovableFilterTrigger
+FilterMultiSelect.SearchInput = SearchInput
+FilterMultiSelect.ListBox = ListBox
+FilterMultiSelect.ListBoxSection = ListBoxSection
+FilterMultiSelect.SectionDivider = SectionDivider
+FilterMultiSelect.Option = MultiSelectOption
+FilterMultiSelect.SelectAllButton = SelectAllButton
+FilterMultiSelect.ClearButton = ClearButton
+FilterMultiSelect.MenuFooter = MenuFooter // For layout
+FilterMultiSelect.MenuLoadingSkeleton = MenuLoadingSkeleton // Menu Loading Skeleton example
+FilterMultiSelect.LoadMoreButton = LoadMoreButton
+FilterMultiSelect.NoResults = NoResults
