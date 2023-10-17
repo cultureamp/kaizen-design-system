dd2b8f86fa27ad30db2a5af6c6f89c155f5e2ac7
Geoffrey Chong <gyf.chong@gmail.com>
2023-10-17 16:12:54 +1100

    add chromatic

--- packages/components/src/Modal/InputEditModal/_docs/InputEditModal.stories.tsx
+++ packages/components/src/Modal/InputEditModal/_docs/InputEditModal.stories.tsx
@@ -1,14 +1,17 @@
 import React, { useState } from "react"
 import { Meta, StoryObj } from "@storybook/react"
+import isChromatic from "chromatic"
 import { ModalDescription } from "~components/Modal/GenericModal/subcomponents/ModalDescription"
 import { Text } from "~components/Text"
 import { TextField } from "~components/TextField"
 import { InputEditModal, InputEditModalProps } from "../index"
 
+const IS_CHROMATIC = isChromatic()
+
 const InputEditModalWithState = (
   args: Omit<InputEditModalProps, "onDismiss" | "onSubmit">
 ): JSX.Element => {
-  const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(IS_CHROMATIC)
 
   const handleOpen = (): void => setIsOpen(true)
   const handleClose = (): void => setIsOpen(false)
@@ -44,6 +47,13 @@ const ExampleForm = (): JSX.Element => (
 const meta = {
   title: "Components/Modals/Input Edit Modal",
   component: InputEditModal,
+  parameters: {
+    chromatic: {
+      disable: false,
+      delay: 400, // match MODAL_TRANSITION_TIMEOUT in modals + 50ms
+      pauseAnimationAtEnd: true,
+    },
+  },
   args: {
     isOpen: false,
     title: "Your input is valuable",
@@ -56,6 +66,23 @@ const meta = {
       control: false,
     },
   },
+  decorators: [
+    // Add additional height to the stories when running in Chromatic only.
+    // Modals have fixed position and would be cropped from snapshot tests.
+    // Setting height to 100vh ensures we capture as much content of the
+    // modal, as it's height responds to the content within it.
+    Story => {
+      if (IS_CHROMATIC) {
+        return (
+          <div style={{ minHeight: "100vh" }}>
+            <Story />
+          </div>
+        )
+      }
+
+      return <Story />
+    },
+  ],
 } satisfies Meta<typeof InputEditModal>
 
 export default meta
