ed3559af3bb002c4bbe18b5b1f974ef23f07497f
Geoffrey Chong <gyf.chong@gmail.com>
2023-10-18 10:54:22 +1100

    align storybook settings

--- packages/components/src/Modal/ContextModal/_docs/ContextModal.stories.tsx
+++ packages/components/src/Modal/ContextModal/_docs/ContextModal.stories.tsx
@@ -8,61 +8,6 @@ import { ContextModal, ContextModalProps } from "../index"
 
 const IS_CHROMATIC = isChromatic()
 
-const ContextModalWithState = (args: ContextModalProps): JSX.Element => {
-  const [isOpen, setIsOpen] = useState(IS_CHROMATIC)
-
-  const handleOpen = (): void => setIsOpen(true)
-  const handleClose = (): void => setIsOpen(false)
-
-  return (
-    <>
-      <button type="button" onClick={handleOpen}>
-        Open Modal
-      </button>
-      <ContextModal
-        {...args}
-        isOpen={isOpen}
-        onConfirm={handleClose}
-        onDismiss={handleClose}
-        image={
-          <AddImage
-            classNameOverride={
-              args.layout === "landscape" ? "!w-auto h-200" : undefined
-            }
-            alt="placeholder"
-          />
-        }
-      />
-    </>
-  )
-}
-
-const ExampleContent = (): JSX.Element => (
-  <>
-    <ModalDescription>
-      <Text variant="body">
-        Intro defining what the modal is trying to explain or depict. Intro
-        defining what the modal is trying to explain or depict.
-      </Text>
-    </ModalDescription>
-    <ul>
-      <li>
-        <Text variant="body">Key point to the benefits of the feature</Text>
-      </li>
-      <li>
-        <Text variant="body">Key point to the benefits of the feature</Text>
-      </li>
-      <li>
-        <Text variant="body">Key point to the benefits of the feature</Text>
-      </li>
-    </ul>
-    <Text variant="body">
-      More information to conclude can go here. More information to conclude can
-      go here. More information to conclude can go here.
-    </Text>
-  </>
-)
-
 // Add additional height to the stories when running in Chromatic only.
 // Modals have fixed position and would be cropped from snapshot tests.
 // Setting height to 100vh ensures we capture as much content of the
@@ -79,6 +24,76 @@ const HeightDecorator: Decorator<ContextModalProps> = Story => {
   return <Story />
 }
 
+const chromaticModalSettings = {
+  parameters: {
+    chromatic: {
+      disable: false,
+      delay: 400, // match MODAL_TRANSITION_TIMEOUT in modals + 50ms
+      pauseAnimationAtEnd: true,
+    },
+  },
+  decorators: [HeightDecorator],
+}
+
+const ContextModalTemplate: Story = {
+  render: args => {
+    const [isOpen, setIsOpen] = useState(IS_CHROMATIC)
+
+    const handleOpen = (): void => setIsOpen(true)
+    const handleClose = (): void => setIsOpen(false)
+
+    return (
+      <>
+        <button type="button" onClick={handleOpen}>
+          Open Modal
+        </button>
+        <ContextModal
+          {...args}
+          isOpen={isOpen}
+          onConfirm={handleClose}
+          onDismiss={handleClose}
+          image={
+            <AddImage
+              classNameOverride={
+                args.layout === "landscape" ? "!w-auto h-200" : undefined
+              }
+              alt="placeholder"
+            />
+          }
+        >
+          <ModalDescription>
+            <Text variant="body">
+              Intro defining what the modal is trying to explain or depict.
+              Intro defining what the modal is trying to explain or depict.
+            </Text>
+          </ModalDescription>
+          <ul>
+            <li>
+              <Text variant="body">
+                Key point to the benefits of the feature
+              </Text>
+            </li>
+            <li>
+              <Text variant="body">
+                Key point to the benefits of the feature
+              </Text>
+            </li>
+            <li>
+              <Text variant="body">
+                Key point to the benefits of the feature
+              </Text>
+            </li>
+          </ul>
+          <Text variant="body">
+            More information to conclude can go here. More information to
+            conclude can go here. More information to conclude can go here.
+          </Text>
+        </ContextModal>
+      </>
+    )
+  },
+}
+
 const meta = {
   title: "Components/Modals/Context Modal",
   component: ContextModal,
@@ -92,7 +107,7 @@ const meta = {
   args: {
     isOpen: false,
     title: "Context modal title",
-    children: <ExampleContent />,
+    children: undefined,
   },
   argTypes: {
     children: {
@@ -106,7 +121,7 @@ export default meta
 type Story = StoryObj<typeof meta>
 
 export const Playground: Story = {
-  render: ContextModalWithState,
+  ...ContextModalTemplate,
   parameters: {
     docs: {
       canvas: {
@@ -117,13 +132,13 @@ export const Playground: Story = {
 }
 
 export const Potrait: Story = {
-  render: ContextModalWithState,
+  ...ContextModalTemplate,
   args: { layout: "portrait" },
-  decorators: [HeightDecorator],
+  ...chromaticModalSettings,
 }
 
 export const Landscape: Story = {
-  render: ContextModalWithState,
+  ...ContextModalTemplate,
   args: { layout: "landscape" },
-  decorators: [HeightDecorator],
+  ...chromaticModalSettings,
 }
