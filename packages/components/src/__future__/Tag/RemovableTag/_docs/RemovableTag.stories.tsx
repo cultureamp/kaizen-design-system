7420a45651b9fb87f372f1085a485bb58625adf4
jakepitman <jakeseanp@gmail.com>
2023-10-24 11:13:54 +1300

    Lint fix

--- packages/components/src/__future__/Tag/RemovableTag/_docs/RemovableTag.stories.tsx
+++ packages/components/src/__future__/Tag/RemovableTag/_docs/RemovableTag.stories.tsx
@@ -1,6 +1,5 @@
-import React from "react"
 import { Meta, StoryObj } from "@storybook/react"
-import { AcademyIcon, ActionOffIcon, AddIcon, TagIcon } from "~components/Icon"
+// import { AcademyIcon, ActionOffIcon, AddIcon, TagIcon } from "~components/Icon"
 // import { TagColorKeys } from "../../Tag/types"
 import { RemovableTag } from "../RemovableTag"
 
@@ -22,18 +21,18 @@ type Story = StoryObj<typeof meta>
 
 export const Playground: Story = {
   // This can be added when we add Icon support
-  //argTypes: {
-  //icon: {
-  //options: ["AcademyIcon", "ActionOffIcon", "AddIcon", "TagIcon"],
-  //control: { type: "radio" },
-  //mapping: {
-  //AcademyIcon: <AcademyIcon role="presentation" />,
-  //ActionOffIcon: <ActionOffIcon role="presentation" />,
-  //AddIcon: <AddIcon role="presentation" />,
-  //TagIcon: <TagIcon role="presentation" />,
-  //},
-  //},
-  //},
+  // argTypes: {
+  // icon: {
+  // options: ["AcademyIcon", "ActionOffIcon", "AddIcon", "TagIcon"],
+  // control: { type: "radio" },
+  // mapping: {
+  // AcademyIcon: <AcademyIcon role="presentation" />,
+  // ActionOffIcon: <ActionOffIcon role="presentation" />,
+  // AddIcon: <AddIcon role="presentation" />,
+  // TagIcon: <TagIcon role="presentation" />,
+  // },
+  // },
+  // },
   parameters: {
     docs: {
       canvas: {
@@ -73,41 +72,41 @@ export const Children: Story = {
 // },
 // },
 // }
-//export const Icon: Story = {
-//render: () => (
-//<>
-//<RemovableTag
-//icon={<AcademyIcon role="presentation" />}
-//removeButtonProps={{
-//ariaLabel: "close",
-//onClick: () => alert("Clicked"),
-//}}
-//>
-//AcademyIcon
-//</RemovableTag>
-//<RemovableTag
-//// color="yellow"
-//icon={<ActionOffIcon role="presentation" />}
-//removeButtonProps={{
-//ariaLabel: "close",
-//onClick: () => alert("Clicked"),
-//}}
-//>
-//ActionOffIcon
-//</RemovableTag>
-//<RemovableTag
-//// color="green"
-//icon={<AddIcon role="presentation" />}
-//removeButtonProps={{
-//ariaLabel: "close",
-//onClick: () => alert("Clicked"),
-//}}
-//>
-//AddIcon
-//</RemovableTag>
-//</>
-//),
-//}
+// export const Icon: Story = {
+// render: () => (
+// <>
+// <RemovableTag
+// icon={<AcademyIcon role="presentation" />}
+// removeButtonProps={{
+// ariaLabel: "close",
+// onClick: () => alert("Clicked"),
+// }}
+// >
+// AcademyIcon
+// </RemovableTag>
+// <RemovableTag
+/// / color="yellow"
+// icon={<ActionOffIcon role="presentation" />}
+// removeButtonProps={{
+// ariaLabel: "close",
+// onClick: () => alert("Clicked"),
+// }}
+// >
+// ActionOffIcon
+// </RemovableTag>
+// <RemovableTag
+/// / color="green"
+// icon={<AddIcon role="presentation" />}
+// removeButtonProps={{
+// ariaLabel: "close",
+// onClick: () => alert("Clicked"),
+// }}
+// >
+// AddIcon
+// </RemovableTag>
+// </>
+// ),
+// }
 
 export const ClassNameOverride: Story = {
   args: {
