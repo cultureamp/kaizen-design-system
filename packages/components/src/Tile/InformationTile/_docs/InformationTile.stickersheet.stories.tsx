6ca1c20a65b95c24d00ab9355c17e8a37d4bc119
Geoffrey Chong <gyf.chong@gmail.com>
2023-10-24 13:16:30 +1100

    add InformationTile

---
+++ packages/components/src/Tile/InformationTile/_docs/InformationTile.stickersheet.stories.tsx
@@ -0,0 +1,40 @@
+import React from "react"
+import { Meta } from "@storybook/react"
+import {
+  StickerSheet,
+  StickerSheetStory,
+} from "~storybook/components/StickerSheet"
+import { moodsList } from "../../subcomponents/GenericTile/types"
+import { InformationTile } from "../index"
+
+export default {
+  title: "Components/InformationTile",
+  parameters: {
+    chromatic: { disable: false },
+    controls: { disable: true },
+  },
+} satisfies Meta
+
+const StickerSheetTemplate: StickerSheetStory = {
+  render: () => (
+    <StickerSheet>
+      <StickerSheet.Body>
+        {moodsList.map(mood => (
+          <StickerSheet.Row key={mood} rowTitle={mood}>
+            <InformationTile
+              mood={mood}
+              title="Title"
+              metadata="metadata"
+              footer={<>Footer</>}
+            />
+          </StickerSheet.Row>
+        ))}
+      </StickerSheet.Body>
+    </StickerSheet>
+  ),
+}
+
+export const StickerSheetDefault: StickerSheetStory = {
+  ...StickerSheetTemplate,
+  name: "Sticker Sheet (Default)",
+}
