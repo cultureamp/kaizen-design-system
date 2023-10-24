8d47026eca714c104fd0c7f820c5cf334ac200c8
Geoffrey Chong <gyf.chong@gmail.com>
2023-10-24 15:34:20 +1100

    un-deprecate the use of JSON tokens directly

--- packages/design-tokens/tokens/animation.json.d.ts
+++ packages/design-tokens/tokens/animation.json.d.ts
@@ -1,11 +1,5 @@
 import { DeepMapObjectLeafs, Theme } from "../src/types"
-/**
- * @deprecated
- * The preferred way to use Kaizen tokens in TypeScript/JavaScript is to use the React hook {@link useTheme} paired with a {@link ThemeProvider}
- */
+
 export const animation: DeepMapObjectLeafs<Theme["animation"], string>
-/**
- * @deprecated
- * The preferred way to use Kaizen tokens in TypeScript/JavaScript is to use the React hook {@link useTheme} paired with a {@link ThemeProvider}
- */
+
 export default { animation }
