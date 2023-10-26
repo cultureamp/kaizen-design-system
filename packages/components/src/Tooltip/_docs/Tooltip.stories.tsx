dcdcd2769ac40cc4ae1d99d3ae3d2ee342a03b88
Doug MacKenzie <doug.mackenzie@cultureamp.com>
2023-10-26 18:54:18 +1300

    Adjust typing on OverflowScroll story

--- packages/components/src/Tooltip/_docs/Tooltip.stories.tsx
+++ packages/components/src/Tooltip/_docs/Tooltip.stories.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { Meta, StoryFn, StoryObj } from "@storybook/react"
+import { Meta, StoryObj } from "@storybook/react"
 import { Tag } from "../../Tag"
 import { Button, InformationIcon, Text } from "../../index"
 import { Tooltip } from "../index"
@@ -44,98 +44,100 @@ export const Playground: Story = {
   ],
 }
 
-export const OverflowScroll: StoryFn<typeof Tooltip> = props => (
-  <>
-    <p>
-      Default Placement is &apos;above&apos;. Scroll horizontally or vertically
-      to view the Tooltip &quot;flip&quot; and move according to the space of
-      the viewport. Ensuring the Tooltip does not get cut off.
-    </p>
+export const OverflowScroll: Story = {
+  render: props => (
+    <>
+      <p>
+        Default Placement is &apos;above&apos;. Scroll horizontally or
+        vertically to view the Tooltip &quot;flip&quot; and move according to
+        the space of the viewport. Ensuring the Tooltip does not get cut off.
+      </p>
 
-    <div
-      style={{
-        display: "flex",
-        width: "300px",
-        maxHeight: "700px",
-        overflow: "scroll",
-        border: "solid black 2px",
-        flexDirection: "column",
-      }}
-    >
       <div
         style={{
-          width: "500px",
-          marginLeft: "200px",
-          marginTop: "400px",
+          display: "flex",
+          width: "300px",
+          maxHeight: "700px",
+          overflow: "scroll",
+          border: "solid black 2px",
+          flexDirection: "column",
         }}
       >
         <div
           style={{
-            width: "300px",
-            height: "200px",
-            textAlign: "center",
-            position: "relative",
+            width: "500px",
+            marginLeft: "200px",
+            marginTop: "400px",
           }}
         >
-          <Tooltip {...props} display="inline-block" text="Tooltip label">
-            <Button label="Default" />
-          </Tooltip>
-        </div>
-      </div>
-      <div
-        style={{
-          width: "500px",
-          marginLeft: "200px",
-        }}
-      >
-        <div
-          style={{
-            width: "300px",
-            height: "100px",
-            textAlign: "center",
-            position: "relative",
-          }}
-        >
-          <Tooltip {...props} display="inline" text="Tooltip label">
-            <InformationIcon role="img" aria-label="Info" />
-          </Tooltip>
-        </div>
-      </div>
-      <div
-        style={{
-          width: "500px",
-          marginLeft: "200px",
-          marginBottom: "500px",
-        }}
-      >
-        <div
-          style={{
-            width: "300px",
-            height: "200px",
-            textAlign: "center",
-            position: "relative",
-          }}
-        >
-          <Text tag="div" variant="body">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
-            nulla quas corporis? Perspiciatis, ratione voluptas{" "}
+          <div
+            style={{
+              width: "300px",
+              height: "200px",
+              textAlign: "center",
+              position: "relative",
+            }}
+          >
             <Tooltip {...props} display="inline-block" text="Tooltip label">
-              <Tag>ad veniam sapiente</Tag>
-            </Tooltip>{" "}
-            Maxime harum, ducimus maiores itaque pariatur quod vel porro
-            mollitia. Lorem ipsum dolor sit{" "}
-            <Tooltip {...props} display="inline" text="Open in new tab">
-              {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-              <a href="#">
-                amet consectetur adipisicing elit Itaque obcaecati maxime
-                molestiae blanditiis pariatur
-              </a>
+              <Button label="Default" />
             </Tooltip>
-            . Magni perspiciatis assumenda in adipisci, eaque commodi quidem
-            dolore, tempore provident animi{" "}
-          </Text>
+          </div>
+        </div>
+        <div
+          style={{
+            width: "500px",
+            marginLeft: "200px",
+          }}
+        >
+          <div
+            style={{
+              width: "300px",
+              height: "100px",
+              textAlign: "center",
+              position: "relative",
+            }}
+          >
+            <Tooltip {...props} display="inline" text="Tooltip label">
+              <InformationIcon role="img" aria-label="Info" />
+            </Tooltip>
+          </div>
+        </div>
+        <div
+          style={{
+            width: "500px",
+            marginLeft: "200px",
+            marginBottom: "500px",
+          }}
+        >
+          <div
+            style={{
+              width: "300px",
+              height: "200px",
+              textAlign: "center",
+              position: "relative",
+            }}
+          >
+            <Text tag="div" variant="body">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
+              nulla quas corporis? Perspiciatis, ratione voluptas{" "}
+              <Tooltip {...props} display="inline-block" text="Tooltip label">
+                <Tag>ad veniam sapiente</Tag>
+              </Tooltip>{" "}
+              Maxime harum, ducimus maiores itaque pariatur quod vel porro
+              mollitia. Lorem ipsum dolor sit{" "}
+              <Tooltip {...props} display="inline" text="Open in new tab">
+                {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                <a href="#">
+                  amet consectetur adipisicing elit Itaque obcaecati maxime
+                  molestiae blanditiis pariatur
+                </a>
+              </Tooltip>
+              . Magni perspiciatis assumenda in adipisci, eaque commodi quidem
+              dolore, tempore provident animi{" "}
+            </Text>
+          </div>
         </div>
       </div>
-    </div>
-  </>
-)
+    </>
+  ),
+}
