4a24f447fc37f0e2e76388e7f5a5b8e90f362148
Geoffrey Chong <gyf.chong@gmail.com>
2023-10-26 15:15:22 +1100

    fix RTL Button stickersheet flakiness?

--- packages/components/src/Button/Button/_docs/Button.stickersheet.stories.tsx
+++ packages/components/src/Button/Button/_docs/Button.stickersheet.stories.tsx
@@ -281,6 +281,9 @@ export const StickerSheetRTL: StickerSheetStory = {
   name: "Sticker Sheet (RTL)",
   parameters: {
     ...StickerSheetTemplate.parameters,
+    chromatic: {
+      delay: 400,
+    },
     textDirection: "rtl",
   },
 }
