9a252f9a7bcccb7bdddd5259245fd7e72f4cfe2e
Geoffrey Chong <gyf.chong@gmail.com>
2023-10-26 13:03:57 +1100

    fix lint

--- packages/components/src/TitleBlockZen/subcomponents/MobileActions.spec.tsx
+++ packages/components/src/TitleBlockZen/subcomponents/MobileActions.spec.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import { render, screen, waitFor } from "@testing-library/react"
 import userEvent from "@testing-library/user-event"
-import MobileActions from "./MobileActions"
+import { MobileActions } from "./MobileActions"
 
 const user = userEvent.setup()
 
