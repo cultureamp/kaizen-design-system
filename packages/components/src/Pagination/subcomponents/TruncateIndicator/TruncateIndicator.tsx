3c87879ed8317af5d8b2fafeeb359da4733716b0
Geoffrey Chong <gyf.chong@gmail.com>
2023-10-30 09:10:45 +1100

    Fix truncate style import

--- packages/components/src/Pagination/subcomponents/TruncateIndicator/TruncateIndicator.tsx
+++ packages/components/src/Pagination/subcomponents/TruncateIndicator/TruncateIndicator.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { EllipsisIcon } from "~components/Icon"
-import styles from "./TruncateIndicators.module.scss"
+import styles from "./TruncateIndicator.module.scss"
 
 export const TruncateIndicator = (): JSX.Element => (
   <div
