b9e5445978d0fca1c442fd673e0c618e7d147cfb
Cassandra Tam <cassandra.tam@cultureamp.com>
2023-11-03 16:33:19 +1100

    chore(RTE): add missing return types for LinkManager

--- packages/components/src/RichTextEditor/utils/plugins/LinkManager/LinkManager.tsx
+++ packages/components/src/RichTextEditor/utils/plugins/LinkManager/LinkManager.tsx
@@ -52,7 +52,7 @@ class LinkManager {
     }, 15)
   }
 
-  update(view: CAEditorView, lastState: EditorState | null) {
+  update(view: CAEditorView, lastState: EditorState | null): void {
     const { state } = view
 
     // Don’t do anything if the document/TextSelection didn't change
@@ -81,12 +81,12 @@ class LinkManager {
     this.updateElement(view)
   }
 
-  destroy() {
+  destroy(): void {
     this.tooltipTarget?.destroy()
     window.removeEventListener("resize", this.onResize)
   }
 
-  createElement(view: CAEditorView) {
+  createElement(view: CAEditorView): void {
     if (!view.dom.parentElement) return
     this.tooltipTarget = createReactTooltipWrapper(
       view.dom.parentElement,
@@ -96,11 +96,11 @@ class LinkManager {
     window.addEventListener("resize", this.onResize)
   }
 
-  updateElement(view: CAEditorView) {
+  updateElement(view: CAEditorView): void {
     this.tooltipTarget?.update(this.getEditorProps(view))
   }
 
-  destroyElement(view: CAEditorView) {
+  destroyElement(view: CAEditorView): void {
     view.dispatch(this.validateLinks)
     this.tooltipTarget?.destroy()
     this.tooltipTarget = null
@@ -164,7 +164,7 @@ export function createLinkManager({
     }
     return attrs.href && attrs.href !== ""
   },
-}: CreateLinkManagerArgs) {
+}: CreateLinkManagerArgs): Plugin {
   return new Plugin({
     view(editorView) {
       return new LinkManager(
