c77512148d5f618e7ddbe0559aa3524738328155
Geoffrey Chong <gyf.chong@gmail.com>
2023-11-03 15:00:09 +1100

    revert token changes

--- packages/tailwind/src/tailwind-presets.ts
+++ packages/tailwind/src/tailwind-presets.ts
@@ -1,5 +1,5 @@
 import { ThemeConfig } from "tailwindcss/types/config"
-import { tokens } from "@kaizen/design-tokens/js"
+import { heartTheme } from "@kaizen/design-tokens/"
 import { kzSpacing } from "./kz-spacing"
 
 export type KaizenTailwindTheme = Partial<ThemeConfig>
@@ -7,28 +7,28 @@ export type KaizenTailwindPreset = {
   theme: KaizenTailwindTheme
 }
 
-// Note: changing any token will require to to run build:ts from the root `design-tokens`
+// Note: changing any token will require to to run build:ts from the root `design-heartTheme`
 export const kaizenTailwindTheme: KaizenTailwindTheme = {
   extend: {
     maxWidth: {
-      paragraph: `${tokens.typography.paragraphBody.maxWidth}`,
-      "paragraph-lede": `${tokens.typography.paragraphIntroLede.maxWidth}`,
-      "paragraph-sm": `${tokens.typography.paragraphSmall.maxWidth}`,
-      "paragraph-xs": `${tokens.typography.paragraphExtraSmall.maxWidth}`,
+      paragraph: `${heartTheme.typography.paragraphBody.maxWidth}`,
+      "paragraph-lede": `${heartTheme.typography.paragraphIntroLede.maxWidth}`,
+      "paragraph-sm": `${heartTheme.typography.paragraphSmall.maxWidth}`,
+      "paragraph-xs": `${heartTheme.typography.paragraphExtraSmall.maxWidth}`,
     },
   },
   colors: {
     transparent: "transparent",
     current: "currentColor",
     inherit: "inherit",
-    ...tokens.color,
-    ...tokens.dataViz,
+    ...heartTheme.color,
+    ...heartTheme.dataViz,
   },
   spacing: kzSpacing,
   boxShadow: {
     none: "none",
-    sm: tokens.shadow.small.boxShadow,
-    lg: tokens.shadow.large.boxShadow,
+    sm: heartTheme.shadow.small.boxShadow,
+    lg: heartTheme.shadow.large.boxShadow,
   },
   borderRadius: {
     DEFAULT: "7px",
@@ -42,35 +42,35 @@ export const kaizenTailwindTheme: KaizenTailwindTheme = {
     "focus-ring": "2px",
   },
   borderColor: {
-    "default-color": `${tokens.border.solid.borderColor}`,
-    transparent: `${tokens.border.borderless.borderColor}`,
-    "focus-ring": tokens.color.blue[600],
-    ...tokens.color,
+    "default-color": `${heartTheme.border.solid.borderColor}`,
+    transparent: `${heartTheme.border.borderless.borderColor}`,
+    "focus-ring": heartTheme.color.blue[600],
+    ...heartTheme.color,
   },
   fontFamily: {
-    "family-paragraph": [`${tokens.typography.paragraphBody.fontFamily}`],
-    "family-heading": [`${tokens.typography.heading1.fontFamily}`],
-    "family-data": [`${tokens.typography.dataLarge.fontFamily}`],
-    "family-display": [`${tokens.typography.display0.fontFamily}`],
+    "family-paragraph": [`${heartTheme.typography.paragraphBody.fontFamily}`],
+    "family-heading": [`${heartTheme.typography.heading1.fontFamily}`],
+    "family-data": [`${heartTheme.typography.dataLarge.fontFamily}`],
+    "family-display": [`${heartTheme.typography.display0.fontFamily}`],
   },
   fontSize: {
-    display: `${tokens.typography.display0.fontSize}`,
-    "heading-1": `${tokens.typography.heading1.fontSize}`,
-    "heading-2": `${tokens.typography.heading2.fontSize}`,
-    "heading-3": `${tokens.typography.heading3.fontSize}`,
-    "heading-4": `${tokens.typography.heading4.fontSize}`,
-    "heading-5": `${tokens.typography.heading5.fontSize}`,
-    "heading-6": `${tokens.typography.heading6.fontSize}`,
-    paragraph: `${tokens.typography.paragraphBody.fontSize}`,
-    "paragraph-lede": `${tokens.typography.paragraphIntroLede.fontSize}`,
-    "paragraph-sm": `${tokens.typography.paragraphSmall.fontSize}`,
-    "paragraph-xs": `${tokens.typography.paragraphExtraSmall.fontSize}`,
-    "data-lg": `${tokens.typography.dataLarge.fontSize}`,
-    "data-md": `${tokens.typography.dataMedium.fontSize}`,
-    "data-sm": `${tokens.typography.dataSmall.fontSize}`,
-    "data-units-lg": `${tokens.typography.dataLargeUnits.fontSize}`,
-    "data-units-md": `${tokens.typography.dataMediumUnits.fontSize}`,
-    "data-units-sm": `${tokens.typography.dataSmallUnits.fontSize}`,
+    display: `${heartTheme.typography.display0.fontSize}`,
+    "heading-1": `${heartTheme.typography.heading1.fontSize}`,
+    "heading-2": `${heartTheme.typography.heading2.fontSize}`,
+    "heading-3": `${heartTheme.typography.heading3.fontSize}`,
+    "heading-4": `${heartTheme.typography.heading4.fontSize}`,
+    "heading-5": `${heartTheme.typography.heading5.fontSize}`,
+    "heading-6": `${heartTheme.typography.heading6.fontSize}`,
+    paragraph: `${heartTheme.typography.paragraphBody.fontSize}`,
+    "paragraph-lede": `${heartTheme.typography.paragraphIntroLede.fontSize}`,
+    "paragraph-sm": `${heartTheme.typography.paragraphSmall.fontSize}`,
+    "paragraph-xs": `${heartTheme.typography.paragraphExtraSmall.fontSize}`,
+    "data-lg": `${heartTheme.typography.dataLarge.fontSize}`,
+    "data-md": `${heartTheme.typography.dataMedium.fontSize}`,
+    "data-sm": `${heartTheme.typography.dataSmall.fontSize}`,
+    "data-units-lg": `${heartTheme.typography.dataLargeUnits.fontSize}`,
+    "data-units-md": `${heartTheme.typography.dataMediumUnits.fontSize}`,
+    "data-units-sm": `${heartTheme.typography.dataSmallUnits.fontSize}`,
   },
   fontWeight: {
     "weight-paragraph": "400",
@@ -80,31 +80,31 @@ export const kaizenTailwindTheme: KaizenTailwindTheme = {
     "weight-display": "800",
   },
   lineHeight: {
-    display: `${tokens.typography.display0.lineHeight}`,
-    "heading-1": `${tokens.typography.heading1.lineHeight}`,
-    "heading-2": `${tokens.typography.heading2.lineHeight}`,
-    "heading-3": `${tokens.typography.heading3.lineHeight}`,
-    "heading-4": `${tokens.typography.heading4.lineHeight}`,
-    "heading-5": `${tokens.typography.heading5.lineHeight}`,
-    "heading-6": `${tokens.typography.heading6.lineHeight}`,
-    paragraph: `${tokens.typography.paragraphBody.lineHeight}`,
-    "paragraph-lede": `${tokens.typography.paragraphIntroLede.lineHeight}`,
-    "paragraph-sm": `${tokens.typography.paragraphSmall.lineHeight}`,
-    "paragraph-xs": `${tokens.typography.paragraphExtraSmall.lineHeight}`,
-    "data-lg": `${tokens.typography.dataLarge.lineHeight}`,
-    "data-md": `${tokens.typography.dataMedium.lineHeight}`,
-    "data-sm": `${tokens.typography.dataSmall.lineHeight}`,
-    "data-units-lg": `${tokens.typography.dataLargeUnits.lineHeight}`,
-    "data-units-md": `${tokens.typography.dataMediumUnits.lineHeight}`,
-    "data-units-sm": `${tokens.typography.dataSmallUnits.lineHeight}`,
+    display: `${heartTheme.typography.display0.lineHeight}`,
+    "heading-1": `${heartTheme.typography.heading1.lineHeight}`,
+    "heading-2": `${heartTheme.typography.heading2.lineHeight}`,
+    "heading-3": `${heartTheme.typography.heading3.lineHeight}`,
+    "heading-4": `${heartTheme.typography.heading4.lineHeight}`,
+    "heading-5": `${heartTheme.typography.heading5.lineHeight}`,
+    "heading-6": `${heartTheme.typography.heading6.lineHeight}`,
+    paragraph: `${heartTheme.typography.paragraphBody.lineHeight}`,
+    "paragraph-lede": `${heartTheme.typography.paragraphIntroLede.lineHeight}`,
+    "paragraph-sm": `${heartTheme.typography.paragraphSmall.lineHeight}`,
+    "paragraph-xs": `${heartTheme.typography.paragraphExtraSmall.lineHeight}`,
+    "data-lg": `${heartTheme.typography.dataLarge.lineHeight}`,
+    "data-md": `${heartTheme.typography.dataMedium.lineHeight}`,
+    "data-sm": `${heartTheme.typography.dataSmall.lineHeight}`,
+    "data-units-lg": `${heartTheme.typography.dataLargeUnits.lineHeight}`,
+    "data-units-md": `${heartTheme.typography.dataMediumUnits.lineHeight}`,
+    "data-units-sm": `${heartTheme.typography.dataSmallUnits.lineHeight}`,
   },
   letterSpacing: {
     "letter-spacing-normal": "normal",
   },
   // A mix of layout styles
   screens: {
-    md: tokens.layout.breakpoints.medium, // => @media (min-width: 768px) { ... }
-    lg: tokens.layout.breakpoints.large, // => @media (min-width: 1080px) { ... }
+    md: heartTheme.layout.breakpoints.medium, // => @media (min-width: 768px) { ... }
+    lg: heartTheme.layout.breakpoints.large, // => @media (min-width: 1080px) { ... }
   },
 }
 
