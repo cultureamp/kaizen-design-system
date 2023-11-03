7860c0d3e71deeea0168ebdac2b02f7e3e49fff0
Cassandra Tam <cassandra.tam@cultureamp.com>
2023-11-03 16:43:40 +1100

    refactor(RTE): add return types to state utils

--- packages/components/src/RichTextEditor/utils/core/state.ts
+++ packages/components/src/RichTextEditor/utils/core/state.ts
@@ -7,36 +7,23 @@ import { Doc, DocContent } from "./types"
  * Create a ProseMirror EditorState. Wrapper for EditorState.create to make the
  * API obvious to consumers
  */
-export function createEditorState(
+export const createEditorState = (
   schema: Schema,
   doc: Node | undefined,
   plugins: Plugin[] = []
-): EditorState {
-  return EditorState.create({
-    doc,
-    schema,
-    plugins,
-  })
-}
+): EditorState => EditorState.create({ doc, schema, plugins })
 
 /**
  * Create a ProseMirror doc node from the combination of a schema and the object
  * representation of a document
  */
-export function createDocNode(schema: Schema, docObject: Doc) {
-  return Node.fromJSON(schema, docObject)
-}
+export const createDocNode = (schema: Schema, docObject: Doc): Node => Node.fromJSON(schema, docObject)
 
 /**
  * Create a ProseMirror doc node from the combination of a schema and the doc’s
  * content array (rather than the top-level doc node).
  */
-export function createDocNodeFromContent(
+export const createDocNodeFromContent = (
   schema: Schema,
   docContent: DocContent
-) {
-  return createDocNode(schema, {
-    type: "doc",
-    content: docContent,
-  })
-}
+): Node => createDocNode(schema, { type: "doc", content: docContent })
