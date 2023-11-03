48f659f9591efff12df6b00af71481171b9ea34a
Cassandra Tam <cassandra.tam@cultureamp.com>
2023-11-03 16:44:58 +1100

    refactor(RTE): add return types to inputrules utils

--- packages/components/src/RichTextEditor/utils/core/inputrules.ts
+++ packages/components/src/RichTextEditor/utils/core/inputrules.ts
@@ -1,22 +1,19 @@
-import { wrappingInputRule } from "prosemirror-inputrules"
+import { InputRule, wrappingInputRule } from "prosemirror-inputrules"
 import { NodeType } from "prosemirror-model"
 
 // Given a list node type, returns an input rule that turns a number
 // followed by a dot at the start of a textblock into an ordered list.
 // Copied from https://github.com/ProseMirror/prosemirror-example-setup/blob/master/src/inputrules.js
-export function orderedListRule(nodeType: NodeType) {
-  return wrappingInputRule(
+export const orderedListRule = (nodeType: NodeType): InputRule =>
+  wrappingInputRule(
     /^(\d+)\.\s$/,
     nodeType,
     match => ({ order: +match[1] }),
     (match, node) => node.childCount + node.attrs.order == +match[1]
   )
-}
 
 // Given a list node type, returns an input rule that turns a bullet
 // (dash, plush, or asterisk) at the start of a textblock into a
 // bullet list.
 // Copied from https://github.com/ProseMirror/prosemirror-example-setup/blob/master/src/inputrules.js
-export function bulletListRule(nodeType: NodeType) {
-  return wrappingInputRule(/^\s*([-+*])\s$/, nodeType)
-}
+export const bulletListRule = (nodeType: NodeType): InputRule => wrappingInputRule(/^\s*([-+*])\s$/, nodeType)
