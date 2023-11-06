f3d144e7cef9278e9616ae0bbdff97182bf513f8
Doug MacKenzie <doug.mackenzie@cultureamp.com>
2023-11-06 12:40:58 +1100

    Use same children across all tabs stories

--- packages/components/src/Tabs/_docs/Tabs.stories.tsx
+++ packages/components/src/Tabs/_docs/Tabs.stories.tsx
@@ -8,7 +8,29 @@ import { Tabs, TabList, Tab, TabPanels, TabPanel } from "../index"
 const meta = {
   title: "Components/Tabs",
   component: Tabs,
-  args: {},
+  args: {
+    children: (
+      <>
+        <TabList aria-label="Tabs">
+          <Tab>Tab 1</Tab>
+          <Tab>Tab 2</Tab>
+          <Tab badge="3">Tab 3</Tab>
+          <Tab disabled>Disabled Tab</Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel classNameOverride="p-24">
+            <Text variant="body">Content 1</Text>
+          </TabPanel>
+          <TabPanel classNameOverride="p-24">
+            <Text variant="body">Content 2</Text>
+          </TabPanel>
+          <TabPanel classNameOverride="p-24">
+            <Text variant="body">Content 3</Text>
+          </TabPanel>
+        </TabPanels>
+      </>
+    ),
+  },
 } satisfies Meta<typeof Tabs>
 
 export default meta
@@ -27,61 +49,10 @@ export const Playground: Story = {
     defaultIndex: 1,
     // eslint-disable-next-line no-console
     onChange: (index): void => console.log("Tab changed to ", index),
-    children: (
-      <>
-        <TabList aria-label="Tabs">
-          <Tab>Tab 1</Tab>
-          <Tab>Tab 2</Tab>
-          <Tab badge="3">Tab 3</Tab>
-          <Tab disabled>Disabled Tab</Tab>
-        </TabList>
-        <TabPanels>
-          <TabPanel classNameOverride="p-24">
-            <Text variant="body">Content 1</Text>
-          </TabPanel>
-          <TabPanel classNameOverride="p-24">
-            <Text variant="body">
-              Uncontrolled is where you (optionally) specify a default selected
-              tab, but the state is otherwise uncontrolled from the consuming
-              end and controlled under the hood instead.
-            </Text>
-          </TabPanel>
-          <TabPanel classNameOverride="p-24">
-            <Text variant="body">Content 3</Text>
-          </TabPanel>
-        </TabPanels>
-      </>
-    ),
   },
 }
 
 export const Controlled: Story = {
-  args: {
-    children: (
-      <>
-        <TabList aria-label="Tabs">
-          <Tab>Tab 1</Tab>
-          <Tab>Tab 2</Tab>
-          <Tab>Tab 3</Tab>
-        </TabList>
-        <TabPanels>
-          <TabPanel classNameOverride="p-24">
-            <Text variant="body">
-              Controlled is where the selected tab state is held at the
-              consuming level and passed into the component. This allows you to
-              change the tab by some other means, such as this button.
-            </Text>
-          </TabPanel>
-          <TabPanel classNameOverride="p-24">
-            <Text variant="body">Content 2</Text>
-          </TabPanel>
-          <TabPanel classNameOverride="p-24">
-            <Text variant="body">Content 3</Text>
-          </TabPanel>
-        </TabPanels>
-      </>
-    ),
-  },
   render: args => {
     const [selectedIndex, setSelectedIndex] = useState<number>(0)
     return (
@@ -90,8 +61,7 @@ export const Controlled: Story = {
           {...args}
           selectedIndex={selectedIndex}
           onChange={setSelectedIndex}
-        ></Tabs>
-
+        />
         <Button
           label="Switch to tab 2"
           onClick={(): void => setSelectedIndex(1)}
@@ -102,29 +72,6 @@ export const Controlled: Story = {
 }
 
 export const UsageInCard: Story = {
-  args: {
-    children: (
-      <>
-        <TabList aria-label="Tabs">
-          <Tab>Tab 1</Tab>
-          <Tab>Tab 2</Tab>
-          <Tab>Tab 3</Tab>
-          <Tab disabled>Disabled Tab</Tab>
-        </TabList>
-        <TabPanels>
-          <TabPanel classNameOverride="p-24">
-            <Text variant="body">Content 1</Text>
-          </TabPanel>
-          <TabPanel classNameOverride="p-24">
-            <Text variant="body">Content 2</Text>
-          </TabPanel>
-          <TabPanel classNameOverride="p-24">
-            <Text variant="body">Content 3</Text>
-          </TabPanel>
-        </TabPanels>
-      </>
-    ),
-  },
   parameters: {
     backgrounds: { default: "Gray 100" },
   },
