fb630b2c5d557d6a7e9a7142fcb567d858c5f220
Geoffrey Chong <gyf.chong@gmail.com>
2023-11-07 09:39:58 +1100

    lint

--- packages/components/src/FieldGroup/_docs/FieldGroup.stickersheet.stories.tsx
+++ packages/components/src/FieldGroup/_docs/FieldGroup.stickersheet.stories.tsx
@@ -25,11 +25,21 @@ const FieldGroupTemplate = ({
   <div>
     <FieldGroup inline={inline} classNameOverride="mr-6">
       <Label htmlFor={`id--field-${id}`}>Email</Label>
-      <input className="border border-gray-500" placeholder="Native text input..." type="text" id="id--field-2" />
+      <input
+        className="border border-gray-500"
+        placeholder="Native text input..."
+        type="text"
+        id="id--field-2"
+      />
     </FieldGroup>
     <FieldGroup inline={inline}>
       <Label htmlFor={`id--field-${id}`}>Username</Label>
-      <input className="border border-gray-500" placeholder="Native text input..." type="text" id="id--field-2" />
+      <input
+        className="border border-gray-500"
+        placeholder="Native text input..."
+        type="text"
+        id="id--field-2"
+      />
     </FieldGroup>
   </div>
 )
