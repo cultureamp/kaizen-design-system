9df2d082b176d0ace7da37d5247255d9ad69f830
Geoffrey Chong <gyf.chong@gmail.com>
2023-11-09 10:37:07 +1100

    cleanup

--- storybook/preview.tsx
+++ storybook/preview.tsx
@@ -5,13 +5,6 @@ import isChromatic from "chromatic"
 import { KaizenProvider } from "~components/KaizenProvider"
 import { backgrounds } from "./backgrounds"
 import { DefaultDocsContainer } from "./components/DocsContainer"
-
-import "highlight.js/styles/a11y-light.css"
-
-// Standard base stylesheet used across Culture Amp products
-// See: https://github.com/necolas/normalize.css/
-import "normalize.css"
-import "@kaizen/component-library/styles/fonts.scss"
 import { globalA11yRules } from "./global-a11y-rules"
 
 const IS_CHROMATIC = isChromatic()
