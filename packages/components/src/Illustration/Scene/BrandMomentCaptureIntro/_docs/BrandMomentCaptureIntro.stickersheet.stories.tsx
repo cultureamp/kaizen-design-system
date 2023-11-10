0a1207f24ec0cd6a5ccc10864358e04500141d93
Michael Winter <mcwinter07@gmail.com>
2023-11-10 12:07:08 +1100

    Migrate BandMomentCaptureIntro from kaizen-legacy

---
+++ packages/components/src/Illustration/Scene/BrandMomentCaptureIntro/_docs/BrandMomentCaptureIntro.stickersheet.stories.tsx
@@ -0,0 +1,38 @@
+import React from "react"
+import { Meta } from "@storybook/react"
+import {
+  StickerSheet,
+  StickerSheetStory,
+} from "~storybook/components/StickerSheet"
+import { BrandMomentCaptureIntro } from "../index"
+
+export default {
+  title: "Components/Illustrations/BrandMomentCaptureIntro",
+  parameters: {
+    chromatic: { disable: false },
+    controls: { disable: true },
+  },
+} satisfies Meta
+
+const StickerSheetTemplate: StickerSheetStory = {
+  render: ({ isReversed }) => (
+    <StickerSheet isReversed={isReversed}>
+      <StickerSheet.Header headings={["Default", "Autoplay (hover)"]} />
+      <StickerSheet.Body>
+        <StickerSheet.Row>
+          <BrandMomentCaptureIntro />
+        </StickerSheet.Row>
+      </StickerSheet.Body>
+    </StickerSheet>
+  ),
+  parameters: {
+    pseudo: {
+      hover: true,
+    },
+  },
+}
+
+export const StickerSheetDefault: StickerSheetStory = {
+  ...StickerSheetTemplate,
+  name: "Sticker Sheet (Default)",
+}
--- packages/components/src/Illustration/Scene/BrandMomentCaptureIntro/index.ts
+++ packages/components/src/Illustration/Scene/BrandMomentCaptureIntro/index.ts
@@ -1,2 +1,1 @@
-export * from "./Scene"
 export * from "./BrandMomentCaptureIntro"
