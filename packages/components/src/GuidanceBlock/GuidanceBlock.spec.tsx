f09c3964e348fdd651ec32722e0b42515c014099
Geoffrey Chong <gyfchong@users.noreply.github.com>
2023-11-14 09:20:37 +1100

    KDS-1914-migrate-guidance-block-to-kaio (#4272)
    * fleshed out stories
    
    * fix lint
    
    * add secondary example to stickersheets
    
    * pass persistent
    
    * fix test
    
    * alphatise
    
    * add changeset
    
    * remove reverse sheet
    
    * fix story types
    
    * remove default text
    
    * removed empty h3s from text
    
    * update delay
    
    * separated working buttons to stickersheet
    
    * lint fix
    
    * remove persistent
    
    * remove persistent
    
    * remove unused tests
    
    * cleanup
    
    * missing image fix
    
    * revert width fix
    
    * fix some .. paths
    
    * fix some .. paths
    
    * revert
    
    * remove useless test

---
+++ packages/components/src/GuidanceBlock/GuidanceBlock.spec.tsx
@@ -0,0 +1,82 @@
+import React from "react"
+import { cleanup, render, waitFor } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
+import { Informative } from "~components/Illustration"
+import { GuidanceBlock } from "./GuidanceBlock"
+
+const user = userEvent.setup()
+
+// eslint-disable-next-line ssr-friendly/no-dom-globals-in-module-scope
+window.matchMedia = jest.fn().mockImplementation(() => ({
+  matches: false,
+  media: "",
+  onchange: null,
+  addListener: jest.fn(),
+  removeListener: jest.fn(),
+}))
+
+describe("GuidanceBlock", () => {
+  afterEach(cleanup)
+
+  it("calls the action function when action button is clicked", async () => {
+    const onAction = jest.fn()
+    const { container } = render(
+      <GuidanceBlock
+        illustration={<Informative alt="" />}
+        text={{
+          title: "This is the call to action title",
+          description:
+            "Mussum Ipsum, cacilds vidis litro abertis. Suco de cevadiss, é um leite divinis.",
+        }}
+        actions={{
+          primary: { label: "Action!", onClick: onAction },
+        }}
+      />
+    )
+    const actionButton = container.querySelector("button")
+    actionButton && (await user.click(actionButton))
+
+    await waitFor(() => {
+      expect(onAction).toHaveBeenCalledTimes(1)
+    })
+  })
+
+  it("has a default title tag of h3", () => {
+    const { getByRole } = render(
+      <GuidanceBlock
+        illustration={<Informative alt="" />}
+        text={{
+          title: "This is the call to action title",
+          description:
+            "Mussum Ipsum, cacilds vidis litro abertis. Suco de cevadiss, é um leite divinis.",
+        }}
+      />
+    )
+    expect(
+      getByRole("heading", {
+        level: 3,
+        name: "This is the call to action title",
+      })
+    ).toBeInTheDocument()
+  })
+
+  it("can allow the user to override the title tag", () => {
+    const { getByRole } = render(
+      <GuidanceBlock
+        illustration={<Informative alt="" />}
+        text={{
+          title: "This is the call to action title",
+          description:
+            "Mussum Ipsum, cacilds vidis litro abertis. Suco de cevadiss, é um leite divinis.",
+          titleTag: "h2",
+        }}
+      />
+    )
+    expect(
+      getByRole("heading", {
+        level: 2,
+        name: "This is the call to action title",
+      })
+    ).toBeInTheDocument()
+  })
+})
