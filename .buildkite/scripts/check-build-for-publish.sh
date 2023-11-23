cd637cc6fa95ba4986e8da2e6aee167902409cdc
Michael Winter <36558508+mcwinter07@users.noreply.github.com>
2023-11-23 16:21:52 +1100

    ci(buildkite): update shell script (#4344)

--- .buildkite/scripts/check-build-for-publish.sh
+++ .buildkite/scripts/check-build-for-publish.sh
@@ -2,3 +2,21 @@
 set -e
 
 echo "Query Github with the current commit $GITHUB_CURRENT_BUILD"
+
+VALID_LABEL="release"
+
+# Stores the labels object in a variable
+LABELS=$(curl --request GET \
+  --url "https://api.github.com/repos/cultureamp/kaizen-design-system/commits/$GITHUB_CURRENT_BUILD/pulls?=" \
+  --header "Authorization: Bearer $GITHUB_REGISTRY_TOKEN" | jq '[] | .labels.name')
+
+echo "Latest commit labels: $LABELS"
+
+if [ -n "$LABELS" ]; then
+    echo "I have labels, let's see it they match!"
+    # Does something when labels exist
+    exit 0  # Return true
+else
+    echo "I have no labels"
+    exit 1  # Return false
+fi
