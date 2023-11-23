c0562fa360d7cb7c187251dd280158349cf7c220
Michael Winter <mcwinter07@gmail.com>
2023-11-24 10:23:25 +1100

    arbitary update

--- .buildkite/scripts/check-build-for-publish.sh
+++ .buildkite/scripts/check-build-for-publish.sh
@@ -10,9 +10,9 @@ LABELS=$(curl --request GET \
   --url "https://api.github.com/repos/cultureamp/kaizen-design-system/commits/${CURRENT_COMMIT}/pulls?=" \
   --header "Authorization: Bearer ${GITHUB_REGISTRY_TOKEN}" | jq '[] | .labels.name')
 
-echo "Latest commit labels: $LABELS"
+echo "Latest commit labels: ${LABELS}"
 
-if [ -n "$LABELS" ]; then
+if [ -n "${LABELS}" ]; then
     echo "I have labels, let's see it they match!!!"
     # Does something when labels exist
     exit 0  # Return true
