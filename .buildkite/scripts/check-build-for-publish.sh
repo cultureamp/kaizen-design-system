79d7594e79b963722c2487040b5517d6a1b52793
Michael Winter <36558508+mcwinter07@users.noreply.github.com>
2023-11-24 10:27:42 +1100

    Update buildkite config (#4349)
    * ci(buildkite): update shell script
    
    * ci(BK): arbitary change to test branch merge
    
    * ci(buildkit): update shell script execution permisions
    
    * ci(Buildkite): add prepublish docker image and update pipeline
    
    * arbitary update

--- .buildkite/scripts/check-build-for-publish.sh
+++ .buildkite/scripts/check-build-for-publish.sh
@@ -1,20 +1,19 @@
 #!/bin/sh
 set -e
 
-echo "Query Github with the current commit $GITHUB_CURRENT_BUILD"
+echo "Query Github with the current commit ${GITHUB_CURRENT_BUILD}"
 
 VALID_LABEL="release"
 
 # Stores the labels object in a variable
 LABELS=$(curl --request GET \
-  --url "https://api.github.com/repos/cultureamp/kaizen-design-system/commits/$GITHUB_CURRENT_BUILD/pulls?=" \
-  --header "Authorization: Bearer $GITHUB_REGISTRY_TOKEN" | jq '[] | .labels.name')
+  --url "https://api.github.com/repos/cultureamp/kaizen-design-system/commits/${GITHUB_CURRENT_BUILD}/pulls?=" \
+  --header "Authorization: Bearer ${GITHUB_REGISTRY_TOKEN}" | jq '[] | .labels.name')
 
-echo "Latest commit labels: $LABELS"
+echo "Latest commit labels: ${LABELS}"
 
-if [ -n "$LABELS" ]; then
+if [ -n "${LABELS}" ]; then
     echo "I have labels, let's see it they match!!!"
-
     # Does something when labels exist
     exit 0  # Return true
 else
