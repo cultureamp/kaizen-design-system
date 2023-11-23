9b3a9aa2ed1493ec1b0304cd0084fbd5502eb315
Michael Winter <36558508+mcwinter07@users.noreply.github.com>
2023-11-24 10:53:58 +1100

    Update buildkite config env vars (#4352)
    * ci(buildkite): update shell script
    
    * ci(BK): arbitary change to test branch merge
    
    * arbitary update
    
    * update script variabels
    
    * update script env vars

--- .buildkite/scripts/check-build-for-publish.sh
+++ .buildkite/scripts/check-build-for-publish.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 set -e
 
-echo "Query Github with the current commit ${GITHUB_CURRENT_BUILD}"
+echo "Query Github with the current commit ${CURRENT_COMMIT}"
 
-VALID_LABEL="release"
+VALID_LABEL="pipeline test label"
 
 # Stores the labels object in a variable
 LABELS=$(curl --request GET \
-  --url "https://api.github.com/repos/cultureamp/kaizen-design-system/commits/${GITHUB_CURRENT_BUILD}/pulls?=" \
+  --url "https://api.github.com/repos/cultureamp/kaizen-design-system/commits/${CURRENT_COMMIT}/pulls?=" \
   --header "Authorization: Bearer ${GITHUB_REGISTRY_TOKEN}" | jq '[] | .labels.name')
 
 echo "Latest commit labels: ${LABELS}"
