086234ee44043225a5a03022e2704392c78afbd1
Michael Winter <36558508+mcwinter07@users.noreply.github.com>
2023-11-24 16:44:09 +1100

    Allow a soft fail on the bk shell script (#4357)
    * update pipeline and script to allow for soft fail

--- .buildkite/scripts/check-build-for-publish.sh
+++ .buildkite/scripts/check-build-for-publish.sh
@@ -3,11 +3,11 @@ set -e
 
 VALID_LABEL="pipeline test label"
 
-echo "⏩ Querying Github for current commit: ${CURRENT_COMMIT} and retriving labels"
+echo "⏩ Querying Github for current commit: ${BUILDKITE_COMMIT} and retriving labels"
 
 # Stores the labels object in a variable
 LABELS=$(curl --request GET \
-  --url "https://api.github.com/repos/cultureamp/kaizen-design-system/commits/${CURRENT_COMMIT}/pulls?=" \
+  --url "https://api.github.com/repos/cultureamp/kaizen-design-system/commits/${BUILDKITE_COMMIT}/pulls?=" \
   --header "Authorization: Bearer ${GITHUB_REGISTRY_TOKEN}" | jq 'try .[0] .labels [] .name catch .')
 
 
@@ -15,7 +15,8 @@ if [ -n "${LABELS}" ]; then
     echo "🔍 Labels were found! Checking for \"${VALID_LABEL}\" label"
     if [[ $LABELS =~ "${VALID_LABEL}" ]]; then
         echo "🔮 \"${VALID_LABEL}\" was found! Commencing build..."
-      exit 0  
+        SHOULD_PUBLISH=true
+      exit 0
     else
       echo "🤷‍♀️ \"${VALID_LABEL}\" label was not found. Exiting build"
       exit 1  
