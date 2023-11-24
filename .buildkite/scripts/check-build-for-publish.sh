23c40573864c0cf30c44e121fe588cd53b5326d3
Michael Winter <36558508+mcwinter07@users.noreply.github.com>
2023-11-24 14:01:05 +1100

    Update bk shell script to catch and handle errors (#4355)
    * ci(buildkite): update shell script
    
    * ci(BK): arbitary change to test branch merge
    
    * arbitary update
    
    * update docker compose to add in missing env var
    
    * update build script to catch and exit

--- .buildkite/scripts/check-build-for-publish.sh
+++ .buildkite/scripts/check-build-for-publish.sh
@@ -1,22 +1,26 @@
 #!/bin/sh
 set -e
 
-echo "Query Github with the current commit ${CURRENT_COMMIT}"
-
 VALID_LABEL="pipeline test label"
+
+echo "⏩ Querying Github for current commit: ${CURRENT_COMMIT} and retriving labels"
 
 # Stores the labels object in a variable
 LABELS=$(curl --request GET \
   --url "https://api.github.com/repos/cultureamp/kaizen-design-system/commits/${CURRENT_COMMIT}/pulls?=" \
-  --header "Authorization: Bearer ${GITHUB_REGISTRY_TOKEN}" | jq '[] | .labels.name')
+  --header "Authorization: Bearer ${GITHUB_REGISTRY_TOKEN}" | jq 'try .[0] .labels [] .name catch .')
 
-echo "Latest commit labels: ${LABELS}"
 
 if [ -n "${LABELS}" ]; then
-    echo "I have labels, let's see it they match!!!"
-    # Does something when labels exist
-    exit 0  # Return true
+    echo "🔍 Labels were found! Checking for \"${VALID_LABEL}\" label"
+    if [[ $LABELS =~ "${VALID_LABEL}" ]]; then
+        echo "🔮 \"${VALID_LABEL}\" was found! Commencing build..."
+      exit 0  
+    else
+      echo "🤷‍♀️ \"${VALID_LABEL}\" label was not found. Exiting build"
+      exit 1  
+    fi
 else
-    echo "I have no labels"
-    exit 1  # Return false
+    echo "⛔️ No labels were found in this commit. Exiting build"
+    exit 1  
 fi
