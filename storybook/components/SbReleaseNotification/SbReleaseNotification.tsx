553f01698b2382f13f58592c9744de8c810b6702
Michael Winter <mcwinter07@gmail.com>
2023-12-07 10:49:15 +1100

    remove props from component

--- storybook/components/SbReleaseNotification/SbReleaseNotification.tsx
+++ storybook/components/SbReleaseNotification/SbReleaseNotification.tsx
@@ -1,21 +1,11 @@
-import React, { ReactNode } from "react"
-import {
-  InlineNotification,
-  InlineNotificationProps,
-} from "~components/Notification"
+import React from "react"
+import { InlineNotification } from "~components/Notification"
 
-export type SbReleaseNotificationProps = {
-  content: ReactNode
-} & InlineNotificationProps
-
-export const SbReleaseNotification = ({
-  content,
-  type = "cautionary",
-  persistent = true,
-  ...otherProps
-}: SbReleaseNotificationProps): JSX.Element => (
-  <InlineNotification persistent={persistent} type={type} {...otherProps}>
-    {content}
+export const SbReleaseNotification = (): JSX.Element => (
+  <InlineNotification persistent type="negative">
+    This component has not been officially released. Please wait for the an
+    announcement in <code>#update_design_system</code> and for the design
+    guidance to be published to Figma and Confluence before consuming.
   </InlineNotification>
 )
 
