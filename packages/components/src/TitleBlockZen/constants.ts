b9c15749ecfb3d09b42cc011e977d17241114f94
louiseliang <louise.liang@cultureamp.com>
2024-01-10 16:24:46 +1100

    chore: prettier

--- packages/components/src/TitleBlockZen/constants.ts
+++ packages/components/src/TitleBlockZen/constants.ts
@@ -1,2 +1,3 @@
-export const TITLE_BLOCK_ZEN_SECONDARY_MENU_HTML_ID = "title-block-secondary-menu"
+export const TITLE_BLOCK_ZEN_SECONDARY_MENU_HTML_ID =
+  "title-block-secondary-menu"
 export const TITLE_BLOCK_ZEN_OTHER_ACTIONS_HTML_ID = "title-block-other-actions"
