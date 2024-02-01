ea83dfb28f06602b865cf2f8d51b3dbad934f0cd
Cassandra <cassandra.tam@cultureamp.com>
2024-02-01 16:35:21 +1100

    chore: optimise storybook (#4511)
    * ci: add test flag to storybook build
    
    * chore: remove babel (keep ones required for rollup)
    
    * chore(deps): replace @storybook/jest and @storybook/testing-library with @storybook/test

--- packages/components/src/Filter/FilterDatePicker/FilterDatePicker.spec.stories.tsx
+++ packages/components/src/Filter/FilterDatePicker/FilterDatePicker.spec.stories.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react"
-import { expect } from "@storybook/jest"
 import { StoryObj } from "@storybook/react"
-import { userEvent, within } from "@storybook/testing-library"
+import { expect, userEvent, within } from "@storybook/test"
 import {
   FilterButton,
   FilterButtonProps,
