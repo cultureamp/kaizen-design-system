5f72bebed834d9b44cb0c761548ecb1d25a640a8
Cassandra Tam <cassandra.tam@cultureamp.com>
2024-02-01 19:02:07 +1100

    chore(vite): get storybook working
    Note: Tailwind not working

--- storybook/backgrounds.ts
+++ storybook/backgrounds.ts
@@ -1,22 +1,6 @@
-/* eslint import/no-extraneous-dependencies: 0 */
-
-import { defaultTheme } from "@kaizen/design-tokens"
 import colors from "@kaizen/design-tokens/tokens/color.json"
 
 export const backgrounds = [
-  { name: "White", value: defaultTheme.color.white },
-  { name: "Gray 100", value: defaultTheme.color.gray["100"] },
-  { name: "Gray 300", value: defaultTheme.color.gray["300"] },
-  { name: "Purple 700", value: defaultTheme.color.purple["700"] },
-  { name: "Blue 500", value: defaultTheme.color.blue["500"] },
-  { name: "Green 500", value: defaultTheme.color.green["500"] },
-  { name: "Yellow 500", value: defaultTheme.color.yellow["500"] },
-  { name: "Orange 500", value: defaultTheme.color.orange["500"] },
-  { name: "Red 500", value: defaultTheme.color.red["500"] },
-]
-
-// This is a temporary duplication until we find a way for storybook to like the above ^^ tokens with css variables rather than concretes.
-export const cssVarBackgrounds = [
   { name: "White", value: colors.color.white },
   { name: "Gray 100", value: colors.color.gray["100"] },
   { name: "Gray 300", value: colors.color.gray["300"] },
--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,35 @@
 import path from "path"
-import type { UserConfig } from "vite"
+import { defineConfig, mergeAlias } from "vite"
 
-export default {
+export default defineConfig({
   resolve: {
-    alias: {
-      "~storybook": path.resolve(__dirname, "storybook"),
-      "~types": path.resolve(__dirname, "packages/components/src/types"),
-      "~utils": path.resolve(__dirname, "packages/components/src/utils"),
-      "~components": path.resolve(__dirname, "packages/components/src"),
-      "~design-tokens": path.resolve(__dirname, "packages/design-tokens/src"),
-      // i18n-react-intl package attempts to import locales from this path.
-      // When rollup attempts to import from the 'find' path, it will be
-      // redirected to import from the replacement path (Same as KAIO rollup config).
-      "__@cultureamp/i18n-react-intl/locales": path.resolve(
-        __dirname,
-        "packages/components/locales"
-      ),
-    },
+    alias: mergeAlias(
+      [
+        {
+          // this is required for the SCSS modules
+          find: /^~(.*)$/,
+          replacement: "$1",
+        },
+        {
+          // monorepo workspace aliases
+          find: /^\@kaizen(.*)$/,
+          replacement: path.resolve(__dirname, "packages$1"),
+        },
+      ],
+      {
+        "~storybook": path.resolve(__dirname, "storybook"),
+        "~types": path.resolve(__dirname, "packages/components/src/types"),
+        "~utils": path.resolve(__dirname, "packages/components/src/utils"),
+        "~components": path.resolve(__dirname, "packages/components/src"),
+        "~design-tokens": path.resolve(__dirname, "packages/design-tokens/src"),
+        // i18n-react-intl package attempts to import locales from this path.
+        // When rollup attempts to import from the 'find' path, it will be
+        // redirected to import from the replacement path (Same as KAIO rollup config).
+        "__@cultureamp/i18n-react-intl/locales": path.resolve(
+          __dirname,
+          "packages/components/locales"
+        ),
+      }
+    ),
   },
-} satisfies UserConfig
+})
