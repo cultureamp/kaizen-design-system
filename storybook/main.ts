88c80fe1968da1f050534c9a72bd805dd36d148d
Cassandra Tam <cassandra.tam@cultureamp.com>
2024-02-09 16:57:04 +1100

    chore(storybook): remove filtering as it no longer works

--- storybook/main.ts
+++ storybook/main.ts
@@ -1,34 +1,10 @@
-import fs from "fs"
-import path from "path"
 import { StorybookConfig } from "@storybook/react-vite"
 
-/**
- * Use `STORIES=path/to/package` environment variable to load all `*.stories.tsx` stories in that folder.
- * Use `STORIES=path/to/specific/story.stories.tsx` to load a specific story only.
- * Leaving blank will return false, causing our defaultStoryPaths (for all stories in Kaizen) to be used.
- */
-const getStoryPathsFromEnv = (): string[] | false => {
-  if (!process.env.STORIES) return false
-
-  const storyPath = path.join(__dirname, "../", process.env.STORIES)
-  if (fs.existsSync(storyPath)) {
-    if (fs.statSync(storyPath).isDirectory()) {
-      return [
-        path.join(storyPath, "**/*.stories.tsx"),
-        path.join(storyPath, "**/*.mdx"),
-      ]
-    }
-  }
-  return [storyPath]
-}
-
-const defaultStoryPaths = [
-  "../{docs,packages}/**/*.mdx",
-  "../{docs,packages}/**/*.stories.tsx",
-]
-
 const config = {
-  stories: getStoryPathsFromEnv() || defaultStoryPaths,
+  stories: [
+    "../{docs,packages}/**/*.mdx",
+    "../{docs,packages}/**/*.stories.tsx",
+  ],
   core: {
     builder: "@storybook/builder-vite",
   },
