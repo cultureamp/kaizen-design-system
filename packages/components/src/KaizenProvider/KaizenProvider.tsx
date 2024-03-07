2d6a426cfc8a31cf01adfc5362ff46cc68be5e8c
Jay Linsell <holla@jaylinsell.com>
2024-03-08 10:00:04 +1100

    removing toastnotificationlist

--- packages/components/src/KaizenProvider/KaizenProvider.tsx
+++ packages/components/src/KaizenProvider/KaizenProvider.tsx
@@ -29,7 +29,7 @@ export const KaizenProvider = ({
     <OptionalIntlProvider locale={locale}>
       <>
         {/* <ToastNotificationProvider> */}
-          {notificationsList}
+          {/* {notificationsList} */}
           {children}
         {/* </ToastNotificationProvider> */}
         <FontDefinitions />
