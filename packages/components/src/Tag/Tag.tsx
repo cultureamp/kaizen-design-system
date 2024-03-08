d0523e5491d04c5b380ee73a02fdcb1ffcee870f
Jay Linsell <holla@jaylinsell.com>
2024-03-08 12:14:44 +1100

    went too gung ho, forgot about TS and props.

--- packages/components/src/Tag/Tag.tsx
+++ packages/components/src/Tag/Tag.tsx
@@ -1,5 +1,31 @@
 import React from "react"
+import { AvatarProps } from "~components/Avatar"
+import { TagVariants } from "./types"
 
-export const Tag = (): JSX.Element => <div>Testing Tag component</div>
+export type TagWithAvatarProps = Omit<DefaultTagProps, "variant"> & {
+  variant: "profile"
+  avatar: JSX.Element | AvatarProps
+}
+
+type Variant = (typeof TagVariants)[number]
+
+export interface DefaultTagProps {
+  variant?: Variant
+  children: React.ReactNode
+  size?: "medium" | "small"
+  inline?: boolean
+  dismissible?: boolean
+  onDismiss?: React.MouseEventHandler<HTMLSpanElement>
+  onMouseDown?: React.MouseEventHandler<HTMLSpanElement>
+  onMouseLeave?: React.MouseEventHandler<HTMLSpanElement>
+  truncateWidth?: number
+}
+
+export type TagProps = DefaultTagProps | TagWithAvatarProps
+
+export const Tag = (props: TagProps): JSX.Element => {
+  const { variant = "default" } = props
+  return <div>Testing Tag component {variant}</div>
+}
 
 Tag.displayName = "Tag"
