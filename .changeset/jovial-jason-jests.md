c6718306b97f5408d7d2e4ac05320addc3d14909
Doug MacKenzie <doug.mackenzie@cultureamp.com>
2024-03-13 10:29:03 +1100

    Export useMediaQueries hook (#4570)

---
+++ .changeset/jovial-jason-jests.md
@@ -0,0 +1,5 @@
+---
+"@kaizen/components": patch
+---
+
+Export useMediaQueries hook
---
+++ packages/components/src/utils/index.ts
@@ -0,0 +1,2 @@
+// Not all utils are exported because most are intended for internal use only
+export * from "./useMediaQueries"
