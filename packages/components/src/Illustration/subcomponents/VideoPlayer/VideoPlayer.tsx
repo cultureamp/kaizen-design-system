33559ed78e4f12ac1ae0789c37efe8f297438371
Cassandra Tam <cassandra.tam@cultureamp.com>
2024-04-04 11:54:47 +1100

    fixup! fix(VideoPlayer): fix source for browsers without webm support

--- packages/components/src/Illustration/subcomponents/VideoPlayer/VideoPlayer.tsx
+++ packages/components/src/Illustration/subcomponents/VideoPlayer/VideoPlayer.tsx
@@ -51,7 +51,6 @@ export const VideoPlayer = ({
   const videoRef = useRef<HTMLVideoElement>(null)
   const [prefersReducedMotion, setPrefersReducedMotion] =
     React.useState<boolean>(true)
-  const [sourceEl, setSourceEl] = React.useState<React.ReactNode>()
 
   useEffect(() => {
     /**
@@ -150,24 +149,6 @@ export const VideoPlayer = ({
     }
   }, [videoRef])
 
-  useEffect(() => {
-    /**
-     * This seems counter-intuitive, but webm support is codec specific.
-     * Only offer webm if we are positive the browser supports it.
-     * Reference: https://bugs.webkit.org/show_bug.cgi?id=216652#c1
-     */
-    setSourceEl(
-      canPlayWebm() ? (
-        <>
-          <source src={assetUrl(`${source}.webm`)} type="video/webm" />
-          <source src={assetUrl(`${source}.mp4`)} type="video/mp4" />
-        </>
-      ) : (
-        <source src={assetUrl(`${source}.mp4`)} type="video/mp4" />
-      )
-    )
-  }, [])
-
   const pausePlay = usePausePlay(videoRef)
 
   return (
@@ -191,7 +172,10 @@ export const VideoPlayer = ({
         playsInline={true}
         tabIndex={-1}
       >
-        {sourceEl}
+        {canPlayWebm() && (
+          <source src={assetUrl(`${source}.webm`)} type="video/webm" />
+        )}
+        <source src={assetUrl(`${source}.mp4`)} type="video/mp4" />
       </video>
       <IconButton
         onClick={(): void => pausePlay.toggle()}
