a2becc02ea4d060a1e58bab87a70e8ffe81ed280
Cassandra Tam <cassandra.tam@cultureamp.com>
2024-04-11 15:55:24 +1000

    style: clean up

--- packages/components/bin/markStyleInjectAsPure.ts
+++ packages/components/bin/markStyleInjectAsPure.ts
@@ -1,7 +1,4 @@
 import replace from "replace-in-file"
-// import replace from "replace-in-file"
-
-// const replace = require("replace-in-file");
 
 const options = {
   files: "dist/esm/**/*.module.scss.mjs",
