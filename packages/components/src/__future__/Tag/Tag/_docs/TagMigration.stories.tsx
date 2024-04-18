9fce96b7d83ae955f247c2b42bbb20e2d7463e61
Michael Winter <mcwinter07@gmail.com>
2024-04-19 09:48:00 +1000

    update migration documentation

--- packages/components/src/__future__/Tag/Tag/_docs/TagMigration.stories.tsx
+++ packages/components/src/__future__/Tag/Tag/_docs/TagMigration.stories.tsx
@@ -1,14 +1,16 @@
-import React from "react"
+import React, { useState } from "react"
 import { Meta, StoryObj } from "@storybook/react"
+import { fn } from "@storybook/test"
+import { Avatar } from "~components/Avatar"
 import {
-  AcademyIcon,
-  ActionOffIcon,
-  AddIcon,
-  TagIcon,
+  CautionIcon,
+  ExclamationIcon,
+  InformationIcon,
+  SuccessIcon,
   LiveIcon,
 } from "~components/Icon"
-import { TagColorKeys } from "../types"
-import { Tag } from ".."
+
+import { Tag, RemovableTag } from "../../"
 import LegacyStyles from "./TagLegacy.module.scss"
 
 const meta = {
@@ -17,10 +19,18 @@ const meta = {
   args: {
     children: "My tag",
   },
+  parameters: {
+    docs: {
+      source: {
+        type: "dynamic",
+      },
+    },
+  },
 } satisfies Meta<typeof Tag>
 
 export default meta
 
+/** * This is a stand-in component for the legacy Tag's bake in LiveIcon - we should consider adding this as an actual component or replacing it */
 const LiveIconComponent = (): JSX.Element => (
   <span className={LegacyStyles.liveIcon}>
     <LiveIcon
@@ -63,7 +73,7 @@ export const LiveIconComponentStory: StoryObj = {
   parameters: {
     docs: {
       source: {
-        type: "hidden",
+        type: "dynamic",
         code: `
   // component with styled with CSS modules
   const LiveIconComponent = (): JSX.Element => (
@@ -115,7 +125,7 @@ export const LiveIconComponentStory: StoryObj = {
 
 export const StatusMigration: StoryObj = {
   render: () => (
-    <div className="flex gap-16">
+    <div className="flex gap-12">
       <Tag classNameOverride="gap-4" color="green">
         <span>Tag</span>
         <LiveIconComponent />
@@ -125,9 +135,167 @@ export const StatusMigration: StoryObj = {
       <Tag color="orange">Tag</Tag>
     </div>
   ),
-  parameters: {
-    docs: {
-      source: { type: "dynamic" },
-    },
-  },
+}
+
+export const ValidationMigration: StoryObj = {
+  render: () => (
+    <div className="flex gap-12">
+      <Tag
+        color="green"
+        icon={<SuccessIcon role="img" aria-label="Success," />}
+      >
+        Tag
+      </Tag>
+      <Tag
+        color="blue"
+        icon={<InformationIcon role="img" aria-label="Note," />}
+      >
+        Tag
+      </Tag>
+      <Tag
+        color="red"
+        icon={<ExclamationIcon role="img" aria-label="Error," />}
+      >
+        Tag
+      </Tag>
+      <Tag
+        color="yellow"
+        icon={<CautionIcon role="img" aria-label="Warning," />}
+      >
+        Tag
+      </Tag>
+    </div>
+  ),
+}
+
+export const SentimentsMigration: StoryObj = {
+  render: () => (
+    <div className="flex gap-12">
+      <Tag color="green">Tag</Tag>
+      <Tag color="gray">Tag</Tag>
+      <Tag color="red">Tag</Tag>
+    </div>
+  ),
+}
+
+export const SentimentNone: StoryObj = {
+  render: () => (
+    <Tag
+      color="gray"
+      classNameOverride="bg-white border-default-color border-solid border"
+    >
+      Tag
+    </Tag>
+  ),
+}
+
+export const DismissableMigration: StoryObj = {
+  render: () => (
+    <RemovableTag
+      removeButtonProps={{
+        ariaLabel: "Remove this tag",
+        onClick: fn(),
+      }}
+    >
+      Tag
+    </RemovableTag>
+  ),
+}
+
+export const AvatarMigration: StoryObj = {
+  render: () => (
+    <div className="flex gap-12">
+      <Tag classNameOverride="ps-4">
+        <span className="flex gap-6 items-center">
+          <Avatar size="small" />
+          Tag
+        </span>
+      </Tag>
+      <Tag classNameOverride="ps-4">
+        <span className="flex gap-6 items-center">
+          <Avatar size="small" fullName="Reed Richards" />
+          Reed Richards
+        </span>
+      </Tag>
+      <Tag classNameOverride="ps-4">
+        <span className="flex gap-6 items-center">
+          <Avatar
+            size="small"
+            fullName="Sue Storm"
+            avatarSrc="https://www.cultureampcom-preview-1.usw2.wp-dev-us.cultureamp-cdn.com/assets/slices/main/assets/public/media/chapters-card-1@2x.05e547444387f29f14df0b82634bf2b6.png"
+          />
+          Susan Storm
+        </span>
+      </Tag>
+    </div>
+  ),
+}
+
+export const AvatarRemovableMigration: StoryObj = {
+  render: () => (
+    <div className="flex gap-12">
+      <RemovableTag
+        removeButtonProps={{
+          ariaLabel: "Remove user from *this context*",
+          onClick: fn(),
+        }}
+        classNameOverride="ps-4"
+      >
+        <span className="flex gap-6 items-center">
+          <Avatar size="small" />
+          Tag
+        </span>
+      </RemovableTag>
+      <RemovableTag
+        removeButtonProps={{
+          ariaLabel: "Remove Reed Richards from *this context*",
+          onClick: fn(),
+        }}
+        classNameOverride="ps-4"
+      >
+        <span className="flex gap-6 items-center">
+          <Avatar size="small" fullName="Reed Richards" />
+          Tag
+        </span>
+      </RemovableTag>
+      <RemovableTag
+        removeButtonProps={{
+          ariaLabel: "Remove Sue Storm from *this context*",
+          onClick: fn(),
+        }}
+        classNameOverride="ps-4"
+      >
+        <span className="flex gap-6 items-center">
+          <Avatar
+            size="small"
+            fullName="Sue Storm"
+            avatarSrc="https://www.cultureampcom-preview-1.usw2.wp-dev-us.cultureamp-cdn.com/assets/slices/main/assets/public/media/chapters-card-1@2x.05e547444387f29f14df0b82634bf2b6.png"
+          />
+          Tag
+        </span>
+      </RemovableTag>
+    </div>
+  ),
+}
+
+export const InlineMigration: StoryObj = {
+  render: () => (
+    <>
+      <div className="mb-6">
+        <Tag>Tag</Tag>
+        <Tag>Tag</Tag>
+        <Tag>Tag</Tag>
+      </div>
+      <div className="flex gap-12 mb-6">
+        <Tag>Tag</Tag>
+        <Tag>Tag</Tag>
+        <Tag>Tag</Tag>
+      </div>
+      <div>
+        <Tag classNameOverride=" me-12">Tag</Tag>
+        <Tag classNameOverride=" me-12">Tag</Tag>
+        <Tag>Tag</Tag>
+      </div>
+    </>
+  ),
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,7 @@ module.exports = {
     "./storybook/components/**/*.tsx",
   ],
   presets: [Preset],
+  important: "body",
   corePlugins: {
     preflight: false,
   },
