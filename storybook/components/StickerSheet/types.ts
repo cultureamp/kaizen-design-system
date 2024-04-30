52d6bb3c9e9ea83507cbb72c8722fa33b6065e5f
Rafael Oliveira <rafael.oliveira@cultureamp.com>
2024-04-30 13:30:19 +1000

    Create a blue variant for the Likert Scale
    We have some big customers threating to churn based them
    being unhappy with the red color used in the traditional LikertScale
    
    We created a blue variant that seems made them happy. The red version
    called "classical" on this work, is still the default one.

--- storybook/components/StickerSheet/types.ts
+++ storybook/components/StickerSheet/types.ts
@@ -1,7 +1,9 @@
 import { StoryObj } from "@storybook/react"
+import {ColorSchema} from "~components/LikertScaleLegacy/types";
 
 export type StickerSheetArgs = {
   isReversed?: boolean
+  colorSchema?: ColorSchema
 }
 
 export type StickerSheetStory = StoryObj<StickerSheetArgs>
