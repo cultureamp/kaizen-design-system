0cc6d41f854b775b96ac70b51f62a7337d375da8
Cassandra Tam <cassandra.tam@cultureamp.com>
2024-05-10 16:56:46 +1000

    refactor: relative paths for tailwind inject

--- packages/package-bundler/src/presets/ui-library/bin/addTailwindStylesImport.ts
+++ packages/package-bundler/src/presets/ui-library/bin/addTailwindStylesImport.ts
@@ -1,38 +1,21 @@
 import fs from "fs"
 import path from "path"
-import util from "util"
 import { getArgs } from "./getArgs.js"
 
 const args = getArgs()
 
-const readFileAsync = util.promisify(fs.readFile)
+const DIST_STYLES_PATH = path.resolve(
+  args.packagePath,
+  "./dist/tailwind.css.js"
+)
 
-const getPackageName = async (): Promise<string | null> => {
-  // Assuming the package.json file is in the root directory of the consuming repo
-  // const packageJsonPath = path.resolve(process.cwd(), "package.json")
-  const packageJsonPath = path.resolve(args.packagePath, "package.json")
-
-  try {
-    const data = await readFileAsync(packageJsonPath, "utf8")
-    const packageJson = JSON.parse(data)
-    return packageJson.name
-  } catch (error) {
-    // eslint-disable-next-line
-    console.error("Error reading package.json:", error)
-    return null
-  }
-}
-
-const addGlobalStylesImport = async (
-  stylesPath: string,
-  dirOrFile: string
-): Promise<void> => {
+const addGlobalStylesImport = async (dirOrFile: string): Promise<void> => {
   if (fs.statSync(dirOrFile).isDirectory()) {
     const dirContent = await fs.promises.readdir(dirOrFile)
 
     dirContent.forEach(content => {
       const contentPath = path.join(dirOrFile, content)
-      addGlobalStylesImport(stylesPath, contentPath)
+      addGlobalStylesImport(contentPath)
     })
     return
   }
@@ -46,6 +29,7 @@ const addGlobalStylesImport = async (
       return
     }
 
+    const stylesPath = path.relative(path.dirname(filePath), DIST_STYLES_PATH)
     const fileContent = fs.readFileSync(filePath).toString()
 
     const isESM = dirOrFile.includes("dist/esm")
@@ -74,9 +58,7 @@ const DIST_DIRS = ["dist/cjs", "dist/esm"]
 DIST_DIRS.forEach(dir => {
   ;(async () => {
     try {
-      const packageName = await getPackageName()
-      const distStylesPath = `${packageName}/dist/tailwind.css.js`
-      addGlobalStylesImport(distStylesPath, `${args.packagePath}/${dir}`)
+      addGlobalStylesImport(`${args.packagePath}/${dir}`)
     } catch (e) {
       // eslint-disable-next-line no-console
       console.error("Directory not found", e)
