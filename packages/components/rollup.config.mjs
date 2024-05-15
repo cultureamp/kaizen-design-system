4eefe4f593af79b27a82f55a4ecff409727fa1ff
Cassandra Tam <cassandra.tam@cultureamp.com>
2024-05-15 17:36:20 +1000

    chore: increase linting scope

--- packages/components/rollup.config.mjs
+++ packages/components/rollup.config.mjs
@@ -1,13 +1,13 @@
 import { pluginsSharedUi, rollupConfig } from "@kaizen/package-bundler";
 
 export default rollupConfig({
-    input: { index: "./src/index.ts", future: "./src/__future__/index.ts" },
-    plugins: pluginsSharedUi,
-    alias: {
-      entries: [
-        { find: "~types", replacement: "src/types" },
-        { find: "~utils", replacement: "src/utils" },
-        { find: "~components", replacement: "src" },
-      ],
-    },
-  })
+  input: { index: "./src/index.ts", future: "./src/__future__/index.ts" },
+  plugins: pluginsSharedUi,
+  alias: {
+    entries: [
+      { find: "~types", replacement: "src/types" },
+      { find: "~utils", replacement: "src/utils" },
+      { find: "~components", replacement: "src" },
+    ],
+  },
+})
