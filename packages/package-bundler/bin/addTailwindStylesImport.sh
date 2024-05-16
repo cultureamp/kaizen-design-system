d4f5e0fa3bf4aa7b6707094abed1d5d630597871
Geoffrey Chong <gyf.chong@gmail.com>
2024-05-16 16:15:46 +1000

    cleaned up recursive scripts

--- packages/package-bundler/bin/addTailwindStylesImport.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-npm explore @kaizen/package-bundler -- node ./dist/presets/shared-ui/bin/addTailwindStylesImport.js --packagePath="$PWD"
--- packages/package-bundler/bin/build-commands.sh
+++ packages/package-bundler/bin/build-commands.sh
@@ -6,39 +6,58 @@ NC='\033[0m' # No Color
 
 SECONDS=0
 
+clean() {
+    echo -e "${GREEN}Remove existing build output folder...${NC}"
+    rm -rf dist
+    echo -e "${GREEN}------${NC}"
+}
+
+bundle() {
+    echo -e "${GREEN}Compile and bundle source code...${NC}"
+    rollup -c
+    echo -e "${GREEN}------${NC}"
+}
+
+compile_types() {
+    echo -e "${GREEN}Compile typescript types...${NC}"
+    tsc --project tsconfig.types.json --declarationDir dist/types
+    echo -e "${GREEN}------{NC}"
+}
+
+inject_tailwind_imports() {
+    echo -e "${GREEN}Running Tailwind Styles Import command..."
+    npm explore @kaizen/package-bundler -- node ./dist/presets/shared-ui/bin/injectTailwindImports.js --packagePath="$PWD"
+    echo -e "${GREEN}------${NC}"
+}
+
+apply_pure_to_style_inject() {
+    echo -e "${GREEN}Purify style inject for treeshaking...${NC}"
+    npm explore @kaizen/package-bundler -- node ./dist/presets/shared-ui/bin/applyPureToStyleInject.js --packagePath="$PWD"
+    echo -e "${GREEN}------${NC}"
+}
+
+elapsed_time() {
+    echo -e "${GREEN}Built in: ${BOLD}${GREEN}$SECONDS${NC} ${GREEN}seconds${NC}"
+}
+
+build() {
+    clean
+    bundle
+    compile_types
+}
+
 case "$1" in
     build)
         echo "Running build command..."
-        echo -e "${GREEN}Remove existing build output folder...${NC}"
-        echo "rm -rf dist"
-        rm -rf dist
-        echo -e "${GREEN}Compile and bundle source code...${NC}"
-        echo "rollup -c"
-        rollup -c
-        echo -e "${GREEN}Compile typescript types...${NC}"
-        echo "tsc --project tsconfig.types.json --declarationDir dist/types"
-        tsc --project tsconfig.types.json --declarationDir dist/types
-        elapsed_time=$SECONDS
-        echo -e "${GREEN}Built in: ${BOLD}${GREEN}$elapsed_time${NC} ${GREEN}seconds${NC}"
+        build
+        elapsed_time
         ;;
     build-shared-ui)
         echo "Running build-shared-ui command..."
-        echo -e "${GREEN}Remove existing build output folder...${NC}"
-        echo "rm -rf dist"
-        rm -rf dist
-        echo -e "${GREEN}Compile and bundle source code...${NC}"
-        echo "rollup -c"
-        rollup -c
-        echo -e "${GREEN}Compile typescript types...${NC}"
-        echo "tsc --project tsconfig.types.json --declarationDir dist/types"
-        tsc --project tsconfig.types.json --declarationDir dist/types
-        echo "Running Tailwind Styles Import command..."
-        npx --no -c addTailwindStylesImport
-        npx package-bundler style-inject
-        echo -e "${GREEN}Purify style inject for treeshaking...${NC}"
-        npx --no -c markStyleInjectAsPure
-        elapsed_time=$SECONDS
-        echo -e "${GREEN}Built in: ${BOLD}${GREEN}$elapsed_time${NC} ${GREEN}seconds${NC}"
+        build
+        inject_tailwind_imports
+        apply_pure_to_style_inject
+        elapsed_time
         ;;
     help)
         echo -e "${GREEN}Usage: $0 {build|build-shared-ui}${NC}"
