44d5e1cd66f964f766c981232219e7d6051e8c16
Geoffrey Chong <gyf.chong@gmail.com>
2024-05-16 15:15:30 +1000

    refactor self-references in build-commands.sh

--- packages/package-bundler/bin/build-commands.sh
+++ packages/package-bundler/bin/build-commands.sh
@@ -9,35 +9,36 @@ SECONDS=0
 case "$1" in
     build)
         echo "Running build command..."
-        npx package-bundler clean
-        npx package-bundler rollup
-        npx package-bundler types
+        echo -e "${GREEN}Remove existing build output folder...${NC}"
+        echo "rm -rf dist"
+        rm -rf dist
+        echo -e "${GREEN}Compile and bundle source code...${NC}"
+        echo "rollup -c"
+        rollup -c
+        echo -e "${GREEN}Compile typescript types...${NC}"
+        echo "tsc --project tsconfig.types.json --declarationDir dist/types"
+        tsc --project tsconfig.types.json --declarationDir dist/types
         elapsed_time=$SECONDS
         echo -e "${GREEN}Built in: ${BOLD}${GREEN}$elapsed_time${NC} ${GREEN}seconds${NC}"
         ;;
     build-shared-ui)
         echo "Running build-shared-ui command..."
-        npx package-bundler build
+        echo -e "${GREEN}Remove existing build output folder...${NC}"
+        echo "rm -rf dist"
+        rm -rf dist
+        echo -e "${GREEN}Compile and bundle source code...${NC}"
+        echo "rollup -c"
+        rollup -c
+        echo -e "${GREEN}Compile typescript types...${NC}"
+        echo "tsc --project tsconfig.types.json --declarationDir dist/types"
+        tsc --project tsconfig.types.json --declarationDir dist/types
+        echo "Running Tailwind Styles Import command..."
         npx --no -c addTailwindStylesImport
+        npx package-bundler style-inject
         echo -e "${GREEN}Purify style inject for treeshaking...${NC}"
         npx --no -c markStyleInjectAsPure
         elapsed_time=$SECONDS
         echo -e "${GREEN}Built in: ${BOLD}${GREEN}$elapsed_time${NC} ${GREEN}seconds${NC}"
-        ;;
-    clean)
-        echo -e "${GREEN}Remove existing build output folder...${NC}"
-        echo "rm -rf dist"
-        rm -rf dist
-        ;;
-    rollup)
-        echo -e "${GREEN}Compile and bundle source code...${NC}"
-        echo "rollup -c"
-        rollup -c
-        ;;
-    types)
-        echo -e "${GREEN}Compile typescript types...${NC}"
-        echo "tsc --project tsconfig.types.json --declarationDir dist/types"
-        tsc --project tsconfig.types.json --declarationDir dist/types
         ;;
     help)
         echo -e "${GREEN}Usage: $0 {build|build-shared-ui|clean|rollup|types|help}${NC}"
