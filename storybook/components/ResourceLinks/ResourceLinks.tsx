8e7db2ff147a6d6d0e6b8ce662e8c0e1c3fd8137
Michael Winter <mcwinter07@gmail.com>
2024-06-20 12:03:01 +1000

    add a sbText component to reduce dependance on Unstyled tags

--- storybook/components/ResourceLinks/ResourceLinks.tsx
+++ storybook/components/ResourceLinks/ResourceLinks.tsx
@@ -10,22 +10,20 @@ type ResourceLinkProps = {
 
 const ResourceLink = ({ href, text }: ResourceLinkProps): JSX.Element => (
   <li>
-    <Unstyled>
-      <a
-        href={href}
-        target="_blank"
-        rel="noopener noreferrer nofollow"
-        className="border rounded inline-flex items-center p-8 text-blue-500"
-      >
-        <Text variant="small" tag="span" classNameOverride="text-inherit">
-          {text}
-        </Text>
-        <ExternalLinkIcon
-          role="presentation"
-          classNameOverride="ml-4 w-16 h-16"
-        />
-      </a>
-    </Unstyled>
+    <a
+      href={href}
+      target="_blank"
+      rel="noopener noreferrer nofollow"
+      className="border rounded inline-flex items-center p-8 text-blue-400 no-underline"
+    >
+      <Text variant="small" tag="span" classNameOverride="text-inherit">
+        {text}
+      </Text>
+      <ExternalLinkIcon
+        role="presentation"
+        classNameOverride="ml-4 w-16 h-16"
+      />
+    </a>
   </li>
 )
 
@@ -33,20 +31,25 @@ export type ResourceLinksProps = HTMLAttributes<HTMLUListElement> & {
   sourceCode: string
   figma?: string
   designGuidelines?: string
+  apiSpecification?: string
 }
 
 export const ResourceLinks = ({
   sourceCode,
   figma,
   designGuidelines,
+  apiSpecification,
   ...attributes
 }: ResourceLinksProps): JSX.Element => (
   <Unstyled>
-    <ul className="mb-40 list-none m-0 p-0 flex gap-8" {...attributes}>
+    <ul {...attributes} className="mb-40 list-none m-0 p-0 flex gap-8">
       <ResourceLink href={sourceCode} text="Source Code" />
       {figma && <ResourceLink href={figma} text="Figma" />}
       {designGuidelines && (
         <ResourceLink href={designGuidelines} text="Usage Guidelines" />
+      )}
+      {apiSpecification && (
+        <ResourceLink href={apiSpecification} text="API specification" />
       )}
     </ul>
   </Unstyled>
---
+++ storybook/components/SbText/SbText.tsx
@@ -0,0 +1,16 @@
+import React from "react"
+import { Unstyled } from "@storybook/blocks"
+import { Text, TextProps } from "~components/Text"
+
+/** A sensible defaulft and convenience wrapper for Storybook text so we don't have to wrap it in an unstyled all the time */
+export const SbText = ({
+  tag = "span",
+  variant = "body",
+  ...otherProps
+}: TextProps): JSX.Element => (
+  <Unstyled>
+    <Text variant={variant} tag={tag} {...otherProps} />
+  </Unstyled>
+)
+
+SbText.displayName = "SbText"
