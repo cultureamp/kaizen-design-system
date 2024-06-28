ca3b4607b087c4aa90ef75fcc8328203e78fb403
Geoffrey Chong <gyf.chong@gmail.com>
2024-06-28 11:10:15 +1000

    fix merge conflict

--- packages/components/codemods/codemod-migrate-v1-to-v2.spec.ts
+++ packages/components/codemods/codemod-migrate-v1-to-v2.spec.ts
@@ -1,49 +1,21 @@
-<<<<<<< HEAD
-import fs from "fs";
-import path from "path";
-import ts from "typescript";
-import { splitIntoFamilies } from "./codemod-migrate-v1-to-v2";
-describe("codemod-migrate-v1-to-v2", () => {
-    it("should migrate v1 to v2", () => {
-        const fileContent = fs.readFileSync(path.join(__dirname, "./mockFile.tsx"), { encoding: "utf8" });
-        const sourceFile = ts.createSourceFile("./mockFile.tsx", fileContent, ts.ScriptTarget.Latest, true);
-        splitIntoFamilies(sourceFile);
-        expect(true).toBe(true);
-        // expect(result).toBe("import { Modal } from '@kaizen/components/overlays'")
-    });
-});
-=======
-// import { js } from "@ast-grep/napi"
-// import { splitIntoFamilies } from "./codemod-migrate-v1-to-v2"
-import { inspect } from "util"
+import fs from "fs"
+import path from "path"
 import ts from "typescript"
-import {
-  codemodRunner,
-  getImportFromAstSourcefile,
-} from "./codemod-migrate-v1-to-v2"
-
+import { splitIntoFamilies } from "./codemod-migrate-v1-to-v2"
 describe("codemod-migrate-v1-to-v2", () => {
   it("should migrate v1 to v2", () => {
-    const result = codemodRunner("mockRoot.ts")
-
-    const imports = getImportFromAstSourcefile(result)
-
-    // console.log(imports)
-
-    // let indent = 0
-
-    // function printTree(node: any) {
-    //   console.log(new Array(indent + 1).join(" ") + ts.SyntaxKind[node.kind])
-    //   indent++
-    //   ts.forEachChild(node, printTree)
-    //   indent--
-    // }
-    // printTree(result)
-
-    // inspect(result, true, 1000)
-    // console.log(JSON.parse(result.kind.toString()))
-
-    expect(true).toBe(false)
+    const fileContent = fs.readFileSync(
+      path.join(__dirname, "./mockFile.tsx"),
+      { encoding: "utf8" }
+    )
+    const sourceFile = ts.createSourceFile(
+      "./mockFile.tsx",
+      fileContent,
+      ts.ScriptTarget.Latest,
+      true
+    )
+    splitIntoFamilies(sourceFile)
+    expect(true).toBe(true)
+    // expect(result).toBe("import { Modal } from '@kaizen/components/overlays'")
   })
 })
->>>>>>> ecd4c843c07b48b900ec727ca07a230b3b667ef1
