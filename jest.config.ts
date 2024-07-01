76a89e2cd594b874edf0af88b84c7f046332b809
Cassandra Tam <cassandra.tam@cultureamp.com>
2024-07-01 16:16:43 +1000

    chore(jest): remove uuid module map

--- jest.config.ts
+++ jest.config.ts
@@ -8,9 +8,6 @@ const jestConfig: JestConfigWithTsJest = {
   moduleNameMapper: {
     "\\.(jpe?g|png|webm|mp4)$": "jest-static-stubs/$1",
     "\\.s?css$": "identity-obj-proxy",
-    // Force module uuid to resolve with the CJS entry point, because Jest does not support package.json.exports. See https://github.com/uuidjs/uuid/issues/451
-    // Should be resolved in v9, but @storybook/test-runner has deps which use and bring in v8
-    uuid: require.resolve("uuid"),
   },
   transformIgnorePatterns: ["[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs)$"],
 }
