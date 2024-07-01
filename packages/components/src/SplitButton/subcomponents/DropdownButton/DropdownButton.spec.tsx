fc7f0e076d552193376af74a12495d7952285fd6
Cassandra Tam <cassandra.tam@cultureamp.com>
2024-07-01 10:15:21 +1000

    test(DropdownButton): fix tests with intl provider

--- packages/components/src/SplitButton/subcomponents/DropdownButton/DropdownButton.spec.tsx
+++ packages/components/src/SplitButton/subcomponents/DropdownButton/DropdownButton.spec.tsx
@@ -1,22 +1,27 @@
 import React from "react"
-import { render, screen } from "@testing-library/react"
+import { screen, waitFor } from "@testing-library/react"
 import { DropdownButton, DropdownButtonProps } from "./DropdownButton"
+import { renderWithIntl } from "~tests"
 
 const DropdownButtonWrapper = (
   props?: Partial<DropdownButtonProps>
 ): JSX.Element => <DropdownButton {...props} />
 
 describe("<DropdownButton />", () => {
-  it("renders icon with default aria-label", () => {
-    render(<DropdownButtonWrapper />)
-    const button = screen.getByRole("button", { name: "Additional actions" })
-    expect(button.getAttribute("aria-label")).toBe("Additional actions")
-    expect(button.firstChild?.nodeName).toEqual("svg")
+  it("renders icon with default aria-label", async () => {
+    renderWithIntl(<DropdownButtonWrapper />)
+    await waitFor(() => {
+      const button = screen.getByRole("button", { name: "Additional actions" })
+      expect(button.getAttribute("aria-label")).toBe("Additional actions")
+      expect(button.firstChild?.nodeName).toEqual("svg")
+    })
   })
 
-  it("renders custom aria-label", () => {
-    render(<DropdownButtonWrapper aria-label="Custom aria label" />)
-    const button = screen.getByRole("button", { name: "Custom aria label" })
-    expect(button.getAttribute("aria-label")).toBe("Custom aria label")
+  it("renders custom aria-label", async () => {
+    renderWithIntl(<DropdownButtonWrapper aria-label="Custom aria label" />)
+    await waitFor(() => {
+      const button = screen.getByRole("button", { name: "Custom aria label" })
+      expect(button.getAttribute("aria-label")).toBe("Custom aria label")
+    })
   })
 })
