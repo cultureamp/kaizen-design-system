34b8d4f114632906615c0048dab58c5a9fb39a9a
Cassandra Tam <cassandra.tam@cultureamp.com>
2024-07-15 17:39:17 +1000

    docs(Notification): fix titles

--- packages/components/src/Notification/InlineNotification/_docs/InlineNotification.stories.tsx
+++ packages/components/src/Notification/InlineNotification/_docs/InlineNotification.stories.tsx
@@ -16,7 +16,7 @@ const DEFAULT_PROPS: Partial<InlineNotificationProps> = {
 }
 
 const meta = {
-  title: "Components/Notifications/Inline Notification",
+  title: "Components/Notifications/InlineNotification",
   component: InlineNotification,
   args: {
     ...DEFAULT_PROPS,
