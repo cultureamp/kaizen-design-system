babb559e84ec8fb32c574ae642244da7c6e94d21
Geoffrey Chong <gyfchong@users.noreply.github.com>
2024-07-16 14:03:10 +1000

    KZN-2506-add-assignment-icon (#4834)
    * added assignment icon
    
    * added: changeset
    
    ---------
    
    Co-authored-by: Geoffrey Chong <gyf.chong@gmail.com>

--- packages/components/src/Icon/EditIcon.tsx
+++ packages/components/src/Icon/EditIcon.tsx
@@ -12,7 +12,7 @@ export const EditIcon = (props: IconProps): JSX.Element => {
       <defs>
         <path
           id={uniqueId}
-          d="M1.556 14.666V18h3.519l10.383-9.837-3.519-3.334zM18.17 5.594a.858.858 0 0 0 0-1.258L15.979 2.26a.978.978 0 0 0-1.328 0l-1.717 1.627 3.519 3.334z"
+          d="M1.556 14.666V18h3.519l10.383-9.837-3.519-3.334zM18.17 5.594a.86.86 0 0 0 0-1.258L15.979 2.26a.98.98 0 0 0-1.328 0l-1.717 1.627 3.519 3.334z"
         />
       </defs>
       <use fill="currentColor" href={`#${uniqueId}`} fillRule="evenodd" />
