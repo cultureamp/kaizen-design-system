5e1237d7a5b2156ba0630a96a8aafeda19569eb4
Cassandra Tam <cassandra.tam@cultureamp.com>
2024-07-31 10:09:55 +1000

    feat: update usages of BrandMoment

--- packages/components/src/ErrorPage/ErrorPage.tsx
+++ packages/components/src/ErrorPage/ErrorPage.tsx
@@ -64,7 +64,7 @@ export const ErrorPage = ({
           </>
         }
         illustration={<BrandMomentError isAnimated loop />}
-        mood="negative"
+        variant="warning"
         primaryAction={{
           ...actions.primary,
           icon: <ArrowRightIcon role="presentation" />,
