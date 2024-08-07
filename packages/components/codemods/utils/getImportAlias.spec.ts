deeb7a936dfeb629ca1a0a971ab29dba118df927
Michael Winter <mcwinter07@gmail.com>
2024-08-07 14:22:39 +1000

    remove redundant code from importAlias tests

--- packages/components/codemods/utils/getImportAlias.spec.ts
+++ packages/components/codemods/utils/getImportAlias.spec.ts
@@ -3,24 +3,21 @@ import { getImportAlias } from "./getImportAlias"
 
 describe("getImportAlias", () => {
   it("returns the import name if it matches the target specifier", () => {
-    const input = parseJsx(`
-      import { Well } from "@kaizen/components"
-      export const TestComponent = () => <div><Well>Test</Well></div>`)
+    const input = parseJsx('import { Well } from "@kaizen/components"')
     const importAlias = getImportAlias(input, "Well")
     expect(importAlias).toBe("Well")
   })
+
   it("returns the import alias if it matches the target specifier", () => {
-    const input = parseJsx(`
-      import { Well as KaizenWell } from "@kaizen/components"
-      export const TestComponent = () => <div><KaizenWell>Test</KaizenWell></div>`)
+    const input = parseJsx(
+      'import { Well as KaizenWell } from "@kaizen/components"'
+    )
     const importAlias = getImportAlias(input, "Well")
     expect(importAlias).toBe("KaizenWell")
   })
+
   it("returns undefined if there is no match to the target specifier", () => {
-    const input = parseJsx(`
-      import { Tag } from "@kaizen/components"
-      import { Well } from "@kaizen/well"
-      export const TestComponent = () => <div><Well>Test</Well></div>`)
+    const input = parseJsx('import { Well } from "@kaizen/well"')
     const importAlias = getImportAlias(input, "Well")
     expect(importAlias).toBe(undefined)
   })
