05c145200d8bb308d439cf1b475a6c90792ff702
Cassandra Tam <cassandra.tam@cultureamp.com>
2024-08-08 18:29:59 +1000

    feat(codemods): add codemod to migrate InformationTile mood to variant

---
+++ packages/components/codemods/migrateInformationTileMoodToVariant/transformInformationTileMoodToVariant.ts
@@ -0,0 +1,22 @@
+import type { InformationTileProps } from "~components/Tile"
+import { migrateStringProp } from "../utils/migrateStringProp"
+
+const OLD_PROP_NAME = "mood"
+const NEW_PROP_NAME = "variant"
+
+const getNewVariantValue = (
+  oldValue: Exclude<InformationTileProps[typeof OLD_PROP_NAME], undefined>
+): Exclude<InformationTileProps[typeof NEW_PROP_NAME], undefined> => {
+  switch (oldValue) {
+    case "prominent":
+      return "expert-advice"
+    default:
+      return "default"
+  }
+}
+
+export const transformInformationTileMoodToVariant = migrateStringProp(
+  OLD_PROP_NAME,
+  NEW_PROP_NAME,
+  getNewVariantValue
+)
