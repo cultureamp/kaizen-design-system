28dfb216b8e58ce90a98aaaaa6390d641d3a94d7
Geoffrey Chong <gyf.chong@gmail.com>
2024-08-12 11:27:29 +1000

    fix prettier

--- packages/components/codemods/migrateCardVariantToColor/index.ts
+++ packages/components/codemods/migrateCardVariantToColor/index.ts
@@ -3,9 +3,7 @@ import { transformCardVariantToColor } from "./transformCardVariantToColor"
 
 const migrateCardVariantToColor = (): void => {
   // eslint-disable-next-line no-console
-  console.log(
-    " ~(-_- ~) Running Card variant to color transformer (~ -_-)~"
-  )
+  console.log(" ~(-_- ~) Running Card variant to color transformer (~ -_-)~")
   const targetDir = process.argv[2]
   if (!targetDir) {
     process.exit(1)
