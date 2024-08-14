5688c0a5d62dfa00a87f303d3a08fe953ffd7207
Cassandra Tam <cassandra.tam@cultureamp.com>
2024-08-14 13:11:40 +1000

    add more test cases

--- packages/components/src/IconPoc/IconPoc.tsx
+++ packages/components/src/IconPoc/IconPoc.tsx
@@ -2,35 +2,81 @@ import React, { HTMLAttributes } from "react"
 import classnames from "classnames"
 import styles from "./IconPoc.module.scss"
 
-// @note: CDN must be set up:
-// - in preview-head.html for SB
-// - in <head> of apps
-//
-// This cannot be done in plain HTML, so we either need to:
-// 1. get the consumers to insert manually
-// 2. add an injection into KaizenProvider
-// 3. use react-helmet
-// 4. ??
+/**
+ * @note: CDN must be set up:
+ * - in preview-head.html for SB
+ * - in <head> of apps
+ *
+ * This cannot be done in plain HTML, so we either need to:
+ * 1. get the consumers to insert manually
+ * 2. add an injection into KaizenProvider
+ * 3. use react-helmet
+ * 4. ??
+ */
 
-export type IconPocProps = {
+/**
+ * Design questions:
+ * - Do we want sizes to be relative to text it sits next to or static?
+ *   - Notes:
+ *     - MUI default is 24px
+ *     - Inheriting size must either be our baked in default or done manually by the consumer
+ *   1. Scale with text - no `size` prop
+ *     - Pro: Simpler implementation
+ *     - Con: Optical size cannot be baked in
+ *     - Allowance: CSS vars are available for customisation by consumer
+ *   2. `size` prop - static sizes
+ *     - Pro: Can control optical size; more visually pleasing
+ *     - Pro: Consistent scaling
+ *     - Con: More thought required for implementation
+ *     - Allowance: CSS vars are available for customisation by consumer
+ *   3. [X - scratch this option] `opticalSize` prop - relative sizes
+ *     - Inherits the size of the parent text
+ *     - Pro: Assists with known values
+ *     - Con: Can control with CSS vars already - rather unnecessary
+ *
+ * - Font weight (declare font-weight as opposed to wght in font-variation-settings)
+ *   1. Inherit font weight
+ *     - Pro: Weight will scale to match parent
+ *     - Con: May not be what we want
+ *   2. Baked in font weight
+ *     - Pro: We can create cohesion for controlled sizes
+ *     - Con: Consumer will need to manually set uncontrolled scaling
+ *
+ * - Expectations for RTL? Should we try to bake it in, or rely on the consumer?
+ *   1. Bake it in - have a list of names which we know to apply a mirror
+ *   2. Add a prop - something like `shouldMirrorInRTL`
+ *   3. Do nothing - Leave it to the consumer to handle the mirror
+ *
+ * - Any missed test cases?
+ */
+
+/**
+ * Technical questions:
+ * - Can we bake in vertical alignment?
+ * - Intellisense for name?
+ * - A11y
+ * - `name` or `children`?
+ */
+
+export type IconPocNoSizeProps = {
   name: string
   isFilled?: boolean
-  size?: "small" | "medium" | "large"
+  shouldInheritWeight?: boolean
 } & HTMLAttributes<HTMLSpanElement>
 
-export const IconPoc = ({
+export const IconPocNoSize = ({
   name,
   isFilled,
-  size = "medium",
+  shouldInheritWeight,
   className,
   ...restProps
-}: IconPocProps): JSX.Element => (
+}: IconPocNoSizeProps): JSX.Element => (
   <span
     className={classnames(
       "material-symbols-outlined",
       styles.iconPoc,
       isFilled && styles.filled,
-      styles[size],
+      shouldInheritWeight && styles.inheritWeight,
       className
     )}
     {...restProps}
@@ -39,4 +85,21 @@ export const IconPoc = ({
   </span>
 )
 
-IconPoc.displayName = "IconPoc"
+export type IconPocProps = IconPocNoSizeProps & {
+  /** @default inherit */
+  size?: "small" | "medium" | "large" | "inherit"
+}
+
+export const IconPoc = ({
+  size = "inherit",
+  className,
+  ...restProps
+}: IconPocProps): JSX.Element => (
+  <IconPocNoSize
+    className={classnames(
+      styles[size],
+      className
+    )}
+    {...restProps}
+  />
+)
--- packages/components/src/IconPoc/_docs/IconPoc.stickersheet.stories.tsx
+++ packages/components/src/IconPoc/_docs/IconPoc.stickersheet.stories.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import { Meta } from "@storybook/react"
+import { Heading } from "~components/Heading"
 import { Text } from "~components/Text"
 import { Button, IconButton } from "~components/__actions__/v2"
 import { Button as Buttonv3 } from "~components/__actions__/v3"
@@ -7,7 +8,7 @@ import {
   StickerSheet,
   StickerSheetStory,
 } from "~storybook/components/StickerSheet"
-import { IconPoc, IconPocProps } from "../index"
+import { IconPoc, IconPocNoSize, IconPocProps } from "../index"
 
 export default {
   title: "Components/IconPoc",
@@ -25,84 +26,208 @@ const StickerSheetTemplate: StickerSheetStory = {
       "invalid",
     ]
 
-    const customProps = [
-      { className: "text-purple-500" },
-      { size: "small" },
-      { size: "medium" },
-      { size: "large" },
-    ] satisfies Array<Partial<IconPocProps>>
+    const buttons = [
+      {
+        rowTitle: "size inherit",
+        iconProps: {}
+      },
+      {
+        rowTitle: "size small",
+        iconProps: { size: "small" }
+      },
+      {
+        rowTitle: "size medium",
+        iconProps: { size: "medium" }
+      },
+      {
+        rowTitle: "inherit size + weight",
+        iconProps: { shouldInheritWeight: true }
+      },
+    ] satisfies Array<{ rowTitle: string, iconProps: Partial<IconPocProps> }>
 
     return (
       <>
-    <StickerSheet isReversed={isReversed}>
+      <div>
+        <p>Notes:</p>
+        <ul>
+          <li>MUI default is 24px</li>
+          <li>`size` prop currently set to `inherit`</li>
+          <li>Existing button sizes do not change font size - future will</li>
+          <li>
+            Tailwind limitation for `font-weight: inherit` if we do not want it as default/prop.
+            `style` prop works though
+          </li>
+        </ul>
+      </div>
+    <StickerSheet isReversed={isReversed} heading="Icon POC">
       <StickerSheet.Header headings={[
-        // "Default", "Filled",
+        "Default",
+        "Filled",
         "Color",
-        "Small (16px/20opsz)", "Medium (20px/24opsz)", "Large (24px/40opsz)"
-        // "Size 16", "Size 20", "Size 24"
       ]}
       hasVerticalHeadings
       />
       <StickerSheet.Body>
         {names.map(name => (
           <StickerSheet.Row key={name} rowTitle={name}>
-          {customProps.map((props, index) => (
-            <StickerSheet.Cell key={index}>
-              <>
-                <IconPoc name={name} {...props} />
-                <IconPoc name={name} {...props} isFilled />
-              </>
-            </StickerSheet.Cell>
-          ))}
+            <IconPoc name={name} />
+            <IconPoc name={name} isFilled />
+            <IconPoc name={name} className="text-red-500" />
         </StickerSheet.Row>
         ))}
       </StickerSheet.Body>
     </StickerSheet>
 
-    <StickerSheet isReversed={isReversed}>
+    <StickerSheet isReversed={isReversed} heading="Sizes (weight 400/inherit)">
       <StickerSheet.Header headings={[
-          "Button v2", "Button v3", "Browser button", "IconButton v2", "Button v3 icon only"
-        ]}
-        hasVerticalHeadings
+        "Small (16px)", "Medium (20px)", "Large (24px)",
+        "Default (24px / inherit)",
+        "Default in Heading 1",
+      ]}
+      hasVerticalHeadings
       />
       <StickerSheet.Body>
-        <StickerSheet.Row rowTitle="Buttons">
-          <Button
-            label="I am the sun!"
-            icon={<IconPoc name="clear_day" />}
-            />
-          <Buttonv3>
-            <IconPoc name="clear_day" /> I am the sun!
-          </Buttonv3>
-          <button type="button">
-            <IconPoc name="clear_day" /> I am the sun!
-          </button>
-          <IconButton
-            label="I am the sun!"
-            icon={<IconPoc name="clear_day" />}
-            primary
-          />
-          <Buttonv3>
-            <IconPoc name="clear_day" />
-          </Buttonv3>
+        <StickerSheet.Row rowTitle="Without prop (use className)">
+          <StickerSheet.Cell>
+            <IconPocNoSize name="delete" className="text-[16px]"/>
+            <IconPocNoSize name="delete" className="text-[16px]" isFilled />
+            <IconPocNoSize name="delete" className="text-[16px] ml-8 pl-8 border-l-1 border-gray-400" shouldInheritWeight />
+            <IconPocNoSize name="delete" className="text-[16px]" isFilled shouldInheritWeight />
+          </StickerSheet.Cell>
+          <StickerSheet.Cell>
+            <IconPocNoSize name="delete" className="text-[20px]" />
+            <IconPocNoSize name="delete" className="text-[20px]" isFilled />
+            <IconPocNoSize name="delete" className="text-[20px] ml-8 pl-8 border-l-1 border-gray-400" shouldInheritWeight />
+            <IconPocNoSize name="delete" className="text-[20px]" isFilled shouldInheritWeight/>
+          </StickerSheet.Cell>
+          <StickerSheet.Cell>
+            <IconPocNoSize name="delete" className="text-[24px]" />
+            <IconPocNoSize name="delete" className="text-[24px]" isFilled />
+            <IconPocNoSize name="delete" className="text-[24px] ml-8 pl-8 border-l-1 border-gray-400" shouldInheritWeight/>
+            <IconPocNoSize name="delete" className="text-[24px]" isFilled shouldInheritWeight/>
+          </StickerSheet.Cell>
+          <StickerSheet.Cell>
+            <IconPocNoSize name="delete" />
+            <IconPocNoSize name="delete" isFilled />
+            <IconPocNoSize name="delete" className="ml-8 pl-8 border-l-1 border-gray-400" shouldInheritWeight/>
+            <IconPocNoSize name="delete" isFilled shouldInheritWeight/>
+          </StickerSheet.Cell>
+          <StickerSheet.Cell>
+            <Heading variant="heading-1" style={{ display: "inline"}}><IconPocNoSize name="delete" /> Heading 1</Heading>
+            <Heading variant="heading-1" style={{ display: "inline"}}><IconPocNoSize name="delete" className="ml-16 pl-16 border-l-1 border-gray-400" shouldInheritWeight /> Heading 1</Heading>
+          </StickerSheet.Cell>
+        </StickerSheet.Row>
+      </StickerSheet.Body>
+      <StickerSheet.Body>
+        <StickerSheet.Row rowTitle="With prop (baked in optical sizing; sans inherit)">
+          <StickerSheet.Cell>
+            <IconPoc name="delete" size="small" />
+            <IconPoc name="delete" size="small" isFilled />
+            <IconPoc name="delete" size="small" className="ml-8 pl-8 border-l-1 border-gray-400" shouldInheritWeight />
+            <IconPoc name="delete" size="small" isFilled shouldInheritWeight/>
+          </StickerSheet.Cell>
+          <StickerSheet.Cell>
+            <IconPoc name="delete" size="medium" />
+            <IconPoc name="delete" size="medium" isFilled />
+            <IconPoc name="delete" size="medium"  className="ml-8 pl-8 border-l-1 border-gray-400" shouldInheritWeight/>
+            <IconPoc name="delete" size="medium" isFilled shouldInheritWeight/>
+          </StickerSheet.Cell>
+          <StickerSheet.Cell>
+            <IconPoc name="delete" size="large" />
+            <IconPoc name="delete" size="large" isFilled />
+            <IconPoc name="delete" size="large"  className="ml-8 pl-8 border-l-1 border-gray-400" shouldInheritWeight/>
+            <IconPoc name="delete" size="large" isFilled shouldInheritWeight/>
+          </StickerSheet.Cell>
+          <StickerSheet.Cell>
+            <IconPoc name="delete" />
+            <IconPoc name="delete" isFilled />
+            <IconPoc name="delete" className="ml-8 pl-8 border-l-1 border-gray-400" shouldInheritWeight />
+            <IconPoc name="delete" isFilled shouldInheritWeight/>
+          </StickerSheet.Cell>
+          <StickerSheet.Cell>
+            <Heading variant="heading-1" style={{ display: "inline"}}><IconPoc name="delete" /> Heading 1</Heading>
+            <Heading variant="heading-1" style={{ display: "inline"}}><IconPoc name="delete" className="ml-16 pl-16 border-l-1 border-gray-400" shouldInheritWeight /> Heading 1</Heading>
+          </StickerSheet.Cell>
         </StickerSheet.Row>
       </StickerSheet.Body>
     </StickerSheet>
 
-    <StickerSheet isReversed={isReversed}>
+    <StickerSheet isReversed={isReversed} heading="Buttons">
       <StickerSheet.Header headings={[
-          "div", "p", "Text", "Wrapped text"
+          "Button v2", "Button v2 small",
+          "Button v3", "Button v3 small",
+          "Browser button",
+          "IconButton v2", "Button v3 icon only",
         ]}
         hasVerticalHeadings
       />
       <StickerSheet.Body>
-        <StickerSheet.Row rowTitle="Text">
+        {buttons.map(({ rowTitle, iconProps }) => (
+          <StickerSheet.Row key={rowTitle} rowTitle={rowTitle}>
+            <Button
+              label="I am the sun!"
+              icon={<IconPoc name="clear_day" {...iconProps} />}
+              />
+            <Button
+              label="I am the sun!"
+              icon={<IconPoc name="clear_day" {...iconProps} />}
+              size="small"
+              />
+            <Buttonv3>
+              <IconPoc name="clear_day" {...iconProps} /> I am the sun!
+            </Buttonv3>
+            <Buttonv3 size="small">
+              <IconPoc name="clear_day" {...iconProps} /> I am the sun!
+            </Buttonv3>
+            <button type="button">
+              <IconPoc name="clear_day" {...iconProps} /> I am the sun!
+            </button>
+            <IconButton
+              label="I am the sun!"
+              icon={<IconPoc name="clear_day" {...iconProps} />}
+              primary
+            />
+            <Buttonv3>
+              <IconPoc name="clear_day" {...iconProps} />
+            </Buttonv3>
+          </StickerSheet.Row>
+        ))}
+      </StickerSheet.Body>
+    </StickerSheet>
+
+    <StickerSheet isReversed={isReversed} heading="Simulated future Button">
+      <StickerSheet.Header headings={[
+          "Large", "Medium", "Small",
+        ]}
+        hasVerticalHeadings
+      />
+      <StickerSheet.Body>
+        {buttons.map(({ rowTitle, iconProps }) => (
+          <StickerSheet.Row key={rowTitle} rowTitle={rowTitle}>
+            <Buttonv3 className="border-1 h-[48px] text-[16px] min-h-[unset]" style={{ fontWeight: 500 }}>
+              <IconPoc name="clear_day" {...iconProps} /> I am the sun!
+            </Buttonv3>
+            <Buttonv3 className="border-1 h-[40px] text-[16px] min-h-[unset]" style={{ fontWeight: 500 }}>
+              <IconPoc name="clear_day" {...iconProps} /> I am the sun!
+            </Buttonv3>
+            <Buttonv3 className="border-1 h-[32px] text-[12px] min-h-[unset]" style={{ fontWeight: 500 }}>
+              <IconPoc name="clear_day" {...iconProps} /> I am the sun!
+            </Buttonv3>
+          </StickerSheet.Row>
+        ))}
+      </StickerSheet.Body>
+    </StickerSheet>
+
+    <StickerSheet isReversed={isReversed} heading="Text">
+      <StickerSheet.Header headings={[
+          "div", "Text", "Wrapped text"
+        ]}
+      />
+      <StickerSheet.Body>
+        <StickerSheet.Row>
           <div>
             <IconPoc name="clear_day" /> I am the sun!
           </div>
-          <p>
-            <IconPoc name="clear_day" /> I am the sun!
-          </p>
           <Text variant="body">
             <IconPoc name="clear_day" /> I am the sun!
           </Text>
@@ -110,6 +235,23 @@ const StickerSheetTemplate: StickerSheetStory = {
             <Text variant="body">
               <IconPoc name="clear_day" /> I should wrap around in my container!
             </Text>
+          </div>
+        </StickerSheet.Row>
+      </StickerSheet.Body>
+    </StickerSheet>
+
+    <StickerSheet isReversed={isReversed} heading="Direction">
+      <StickerSheet.Header headings={[
+          "LTR", "RTL",
+        ]}
+      />
+      <StickerSheet.Body>
+        <StickerSheet.Row>
+          <div dir="ltr">
+            <IconPoc name="arrow_forward" />
+          </div>
+          <div dir="rtl">
+            <IconPoc name="arrow_forward" />
           </div>
         </StickerSheet.Row>
       </StickerSheet.Body>
