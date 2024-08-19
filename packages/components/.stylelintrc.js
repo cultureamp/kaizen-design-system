4e175a358f43030a35e6d86b35c73609bb0d679e
Geoffrey Chong <gyf.chong@gmail.com>
2024-08-20 08:22:24 +1000

    remove postcss from root

--- packages/components/.stylelintrc.js
+++ packages/components/.stylelintrc.js
@@ -1,5 +1,5 @@
 /** @type {import('stylelint').Config} */
-
+// For CSS linting only, SCSS linting is handled in the root .stylelintrc.json
 export default {
   extends: "stylelint-config-standard",
 }
