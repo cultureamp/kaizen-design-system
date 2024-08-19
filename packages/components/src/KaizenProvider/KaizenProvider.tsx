020ddd02ec4758c44d1963bb1228cd8cd9a039a5
Cassandra Tam <cassandra.tam@cultureamp.com>
2024-08-19 16:08:00 +1000

    test material-symbols

--- packages/components/src/KaizenProvider/KaizenProvider.tsx
+++ packages/components/src/KaizenProvider/KaizenProvider.tsx
@@ -3,6 +3,7 @@ import { ToastNotificationsList } from "~components/Notification"
 import { ToastNotificationProvider } from "~components/Notification/ToastNotification/context/ToastNotificationContext"
 import { FontDefinitions } from "./subcomponents/FontDefinitions"
 import { OptionalIntlProvider } from "./subcomponents/OptionalIntlProvider"
+// import "material-symbols/outlined.css";
 
 export type KaizenProviderProps = {
   children: React.ReactNode
