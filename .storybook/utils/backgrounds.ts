ed08d5cc5b878bd0c3c38b4f17c5fb3745c7b198
Geoffrey Chong <gyf.chong@gmail.com>
2024-08-20 12:04:40 +1000

    fix design-tokens distribution

--- .storybook/utils/backgrounds.ts
+++ .storybook/utils/backgrounds.ts
@@ -1,13 +1,13 @@
-import colors from "@kaizen/design-tokens/tokens/color.json"
+import { tokens } from "@kaizen/design-tokens/js"
 
 export const backgrounds = [
-  { name: "White", value: colors.color.white },
-  { name: "Gray 100", value: colors.color.gray["100"] },
-  { name: "Gray 300", value: colors.color.gray["300"] },
-  { name: "Purple 700", value: colors.color.purple["700"] },
-  { name: "Blue 500", value: colors.color.blue["500"] },
-  { name: "Green 500", value: colors.color.green["500"] },
-  { name: "Yellow 500", value: colors.color.yellow["500"] },
-  { name: "Orange 500", value: colors.color.orange["500"] },
-  { name: "Red 500", value: colors.color.red["500"] },
+  { name: "White", value: tokens.color.white },
+  { name: "Gray 100", value: tokens.color.gray["100"] },
+  { name: "Gray 300", value: tokens.color.gray["300"] },
+  { name: "Purple 700", value: tokens.color.purple["700"] },
+  { name: "Blue 500", value: tokens.color.blue["500"] },
+  { name: "Green 500", value: tokens.color.green["500"] },
+  { name: "Yellow 500", value: tokens.color.yellow["500"] },
+  { name: "Orange 500", value: tokens.color.orange["500"] },
+  { name: "Red 500", value: tokens.color.red["500"] },
 ]
