df16927f386898b8c625b0ab54602653f6b4981f
Cassandra <cassandra.tam@cultureamp.com>
2024-08-21 16:46:49 +1000

    chore(Popover): deprecate `variant` and `customIcon` props (#4941)
    * chore(Popover): deprecate variant and customIcon props
    
    * fix(Popover): fix RTL styles
    
    * feat(codemods): add codemod to remove Popover props variant and customIcon
    
    * refactor(codemods): update readme and remove alias references

---
+++ packages/components/codemods/utils/removeProps.spec.ts
@@ -0,0 +1,65 @@
+import { parseJsx } from "../__tests__/utils"
+import { transformSource, printAst } from "../utils"
+import { removeProps } from "./removeProps"
+
+describe("removeProps()", () => {
+  it("removes single specified prop", () => {
+    const inputAst = parseJsx(`
+      export const TestComponent = () => <Pancakes topping="butter" />
+    `)
+    const outputAst = parseJsx(`
+      export const TestComponent = () => <Pancakes />
+    `)
+    const transformed = transformSource({
+      sourceFile: inputAst,
+      astTransformer: removeProps(["topping"]),
+      tagName: "Pancakes",
+    })
+    expect(transformed).toEqual(printAst(outputAst))
+  })
+
+  it("removes multiple specified props", () => {
+    const inputAst = parseJsx(`
+      export const TestComponent = () => <Pancakes topping="butter" fruit="blueberries" />
+    `)
+    const outputAst = parseJsx(`
+      export const TestComponent = () => <Pancakes />
+    `)
+    const transformed = transformSource({
+      sourceFile: inputAst,
+      astTransformer: removeProps(["topping", "fruit"]),
+      tagName: "Pancakes",
+    })
+    expect(transformed).toEqual(printAst(outputAst))
+  })
+
+  it("handles multiple attributes and removes only specified props", () => {
+    const inputAst = parseJsx(`
+      export const TestComponent = () => <Pancakes topping="jam" id="123"/>
+    `)
+    const outputAst = parseJsx(`
+      export const TestComponent = () => <Pancakes id="123"/>
+    `)
+    const transformed = transformSource({
+      sourceFile: inputAst,
+      astTransformer: removeProps(["topping"]),
+      tagName: "Pancakes",
+    })
+    expect(transformed).toBe(printAst(outputAst))
+  })
+
+  it("transforms multiple Pancakess", () => {
+    const inputAst = parseJsx(`
+      export const TestComponent = () => <div><Pancakes topping="butter"/><Pancakes topping="jam"/></div>
+    `)
+    const outputAst = parseJsx(`
+      export const TestComponent = () => <div><Pancakes /><Pancakes /></div>
+    `)
+    const transformed = transformSource({
+      sourceFile: inputAst,
+      astTransformer: removeProps(["topping"]),
+      tagName: "Pancakes",
+    })
+    expect(transformed).toBe(printAst(outputAst))
+  })
+})
