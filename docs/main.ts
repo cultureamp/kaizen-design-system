6a098be7594bd78ad086ad87269e3d8d2355f646
Geoffrey Chong <gyf.chong@gmail.com>
2024-08-21 16:53:15 +1000

    fix pseudo storybook addon

--- docs/main.ts
+++ docs/main.ts
@@ -18,10 +18,12 @@ const config: StorybookConfig = {
     "./pages/**/*.stories.tsx",
   ],
   addons: [
-    getAbsolutePath("@storybook/addon-onboarding"),
-    getAbsolutePath("@storybook/addon-links"),
+    getAbsolutePath("@storybook/addon-a11y"),
     getAbsolutePath("@storybook/addon-essentials"),
+    getAbsolutePath("@storybook/addon-onboarding"),
     getAbsolutePath("@storybook/addon-interactions"),
+    getAbsolutePath("@storybook/addon-links"),
+    "storybook-addon-pseudo-states",
   ],
   framework: {
     name: getAbsolutePath("@storybook/react-vite"),
--- packages/tailwind/types.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "*.css" {
-  const classes: Record<string, string>
-  export default classes
-}
-
-declare module "*.scss" {
-  const classes: Record<string, string>
-  export default classes
-}
