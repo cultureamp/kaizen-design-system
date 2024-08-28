5ffc2dde667782eef751b0aaa7ac022afe4fb516
Doug MacKenzie <doug.mackenzie@cultureamp.com>
2024-08-28 16:17:56 +1000

    Add deprecate docblocks to v1 and v2

--- packages/components/src/__layout__/Workflow/v1/index.ts
+++ packages/components/src/__layout__/Workflow/v1/index.ts
@@ -1,1 +1,10 @@
-export * from "./Workflow"
+// Since we can't add a deprecation flag on a * export
+import {
+  Workflow as WorkflowV1,
+  WorkflowProps as WorkflowPropsV1,
+} from "./Workflow"
+
+/** * @deprecated upgrade to v3 for the latest release */
+export const Workflow = WorkflowV1
+/** * @deprecated upgrade to v3 for the latest release */
+export type WorkflowProps = WorkflowPropsV1
--- packages/components/src/__layout__/Workflow/v2/index.ts
+++ packages/components/src/__layout__/Workflow/v2/index.ts
@@ -1,1 +1,10 @@
-export * from "./Workflow"
+// Since we can't add a deprecation flag on a * export
+import {
+  Workflow as WorkflowV2,
+  WorkflowProps as WorkflowPropsV2,
+} from "./Workflow"
+
+/** * @deprecated upgrade to v3 for the latest release */
+export const Workflow = WorkflowV2
+/** * @deprecated upgrade to v3 for the latest release */
+export type WorkflowProps = WorkflowPropsV2
