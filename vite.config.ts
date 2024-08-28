bdf3f66eb75f4d532b2bf99c14120c8a9ceb959a
Geoffrey Chong <gyf.chong@gmail.com>
2024-08-29 09:49:24 +1000

    revert usage of tsconfig paths as alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,44 @@
 /** @type {import('vite').UserConfig} */
 import path from "path"
-import tsconfigPaths from "vite-tsconfig-paths"
+import { mergeAlias } from "vite"
 
 export default {
-  plugins: [tsconfigPaths()],
+  optimizeDeps: {
+    include: ["@cultureamp/i18n-react-intl"],
+  },
+  resolve: {
+    alias: mergeAlias(
+      [
+        {
+          // this is required for the SCSS modules
+          find: /^~(.*)$/,
+          replacement: "$1",
+        },
+        {
+          // monorepo workspace aliases
+          find: /^\@kaizen(.*)$/,
+          replacement: path.resolve(__dirname, "./packages$1"),
+        },
+      ],
+      {
+        "~storybook": path.resolve(__dirname, "./docs"),
+        "~components": path.resolve(__dirname, "./packages/components/src"),
+        "~tests": path.resolve(__dirname, "./packages/components/__tests__"),
+        "~design-tokens": path.resolve(
+          __dirname,
+          "./packages/design-tokens/src"
+        ),
+        "~tailwind": path.resolve(__dirname, "./packages/tailwind/src"),
+        // i18n-react-intl package attempts to import locales from this path.
+        // When rollup attempts to import from the 'find' path, it will be
+        // redirected to import from the replacement path (Same as KAIO rollup config).
+        "__@cultureamp/i18n-react-intl/locales": path.resolve(
+          __dirname,
+          "./packages/components/locales"
+        ),
+      }
+    ),
+  },
   test: {
     include: ["**/*.spec.ts?(x)", "!**/*.jest.spec.ts?(x)"],
     environment: "jsdom",
