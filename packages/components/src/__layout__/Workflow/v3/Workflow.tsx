2d34341ea9e6d8c28af80645e6bef3cd60cb8f8e
Doug MacKenzie <doug.mackenzie@cultureamp.com>
2024-08-30 09:03:53 +1000

    feat(Workflow): Create v3 Workflow (#4978)
    * feat(Workflow): Create v3 Workflow
    
    * Update story names to include Workflow name alongside version
    
    * Add package.json files for new layout category
    
    * Add deprecate docblocks to v1 and v2
    
    * Update page title to use composable-header-title Heading variant
    
    * Adjust status prop in Header to allow ReactNode
    
    * Convert all sass to css in v3
    
    * Rename status prop to statusTag
    
    * Update import statements
    
    * Delete KaioLegacyDocsNotification
    
    * Remove background decorator on v3 footer story

---
+++ packages/components/src/__layout__/Workflow/v3/Workflow.tsx
@@ -0,0 +1,55 @@
+import React, { HTMLAttributes } from "react"
+import { OverrideClassName } from "~components/types/OverrideClassName"
+import {
+  Footer,
+  FooterProps,
+  Header,
+  Main,
+  HeaderProps,
+  ProgressStepper,
+  Wrapper,
+} from "./subcomponents"
+
+export type WorkflowProps = OverrideClassName<HTMLAttributes<HTMLDivElement>> &
+  FooterProps &
+  Pick<HeaderProps, "workflowName" | "statusTag" | "headerActions">
+
+export const Workflow = ({
+  steps,
+  currentStepId,
+  isComplete,
+  workflowName,
+  statusTag,
+  headerActions,
+  children,
+  nextAction,
+  previousAction,
+  classNameOverride,
+  ...restProps
+}: WorkflowProps): JSX.Element => {
+  const currentStep = steps.find(step => step.id === currentStepId)
+  return (
+    <Workflow.Wrapper classNameOverride={classNameOverride} {...restProps}>
+      <Workflow.Header
+        workflowName={workflowName}
+        stepName={currentStep!.label}
+        statusTag={statusTag}
+        headerActions={headerActions}
+      />
+      <Workflow.Main>{children}</Workflow.Main>
+      <Workflow.Footer
+        currentStepId={currentStepId}
+        steps={steps}
+        isComplete={isComplete}
+        nextAction={nextAction}
+        previousAction={previousAction}
+      />
+    </Workflow.Wrapper>
+  )
+}
+
+Workflow.Header = Header
+Workflow.Footer = Footer
+Workflow.Main = Main
+Workflow.ProgressStepper = ProgressStepper
+Workflow.Wrapper = Wrapper
---
+++ packages/components/src/__layout__/Workflow/v3/subcomponents/Header/components/index.ts
@@ -0,0 +1,4 @@
+export * from "./Titles"
+export * from "./Actions"
+export * from "./Branding"
+export * from "./Root"
