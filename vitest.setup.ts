264419548dc868d0dd0de4d96f4446081779ae29
Geoffrey Chong <gyf.chong@gmail.com>
2024-08-29 12:32:47 +1000

    remove extend matchers

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,12 +1,10 @@
 import * as JestDomMatchers from "@testing-library/jest-dom/matchers"
 import { cleanup } from "@testing-library/react"
 import { expect, afterEach, beforeEach } from "vitest"
-import * as AxeMatchers from "vitest-axe/matchers"
 import "vitest-axe/extend-expect"
 import { reactIntlMock } from "~tests"
 
 expect.extend(JestDomMatchers)
-expect.extend(AxeMatchers)
 
 beforeEach(() => {
   reactIntlMock()
