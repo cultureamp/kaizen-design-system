2f9847caad67fda7412aefcfe65e3aee8cbab84c
Cassandra Tam <cassandra.tam@cultureamp.com>
2024-09-03 11:42:30 +1000

    docs(DosAndDonts): update styles so items are the same height

--- docs/components/DosAndDonts/DosAndDonts.tsx
+++ docs/components/DosAndDonts/DosAndDonts.tsx
@@ -11,7 +11,10 @@ export const DosAndDonts = ({
   ...otherProps
 }: HTMLAttributes<HTMLDivElement>): JSX.Element => (
   <div
-    className={classnames(className, "grid grid-cols-1 gap-16 lg:grid-cols-2")}
+    className={classnames(
+      className,
+      "relative grid grid-cols-1 gap-16 lg:grid-cols-2"
+    )}
     {...otherProps}
   >
     {children}
@@ -29,31 +32,29 @@ export const DoOrDont = ({
   isDont?: boolean
   story: any
 } & HTMLAttributes<HTMLDivElement>): JSX.Element => (
-  <div className={classnames("relative [&>*]:m-0", className)} {...otherProps}>
-    <div className="relative [&>*]:m-0">
-      <div
-        className={classnames(
-          "absolute top-6 left-6 z-10 py-4 px-6 font-bold text-white rounded flex gap-2 items-center justify-center",
-          isDont ? "bg-red-500" : "bg-green-500"
-        )}
-      >
-        {isDont ? (
-          <CloseIcon role="presentation" />
-        ) : (
-          <CheckIcon role="presentation" />
-        )}
-        <SbContent>
-          <span className="text-white">
-            <strong>{isDont ? "Don't" : "Do"}</strong>
-          </span>
-        </SbContent>
-      </div>
-      <Canvas
-        sourceState="none"
-        className="[&>*]:bg-gray-100 rounded-none shadow-none border-none"
-        of={story}
-      />
+  <div className={classnames("relative", className)} {...otherProps}>
+    <div
+      className={classnames(
+        "absolute top-6 left-6 z-10 py-4 px-6 font-bold text-white rounded flex gap-2 items-center justify-center",
+        isDont ? "bg-red-500" : "bg-green-500"
+      )}
+    >
+      {isDont ? (
+        <CloseIcon role="presentation" />
+      ) : (
+        <CheckIcon role="presentation" />
+      )}
+      <SbContent>
+        <span className="text-white">
+          <strong>{isDont ? "Don't" : "Do"}</strong>
+        </span>
+      </SbContent>
     </div>
+    <Canvas
+      sourceState="none"
+      className="h-full m-0 bg-gray-100 rounded-none shadow-none border-none"
+      of={story}
+    />
   </div>
 )
 
