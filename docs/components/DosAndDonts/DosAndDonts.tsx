8d525ab4ddc2e54c89a9ad12835cb38fdb1dd6db
Cassandra Tam <cassandra.tam@cultureamp.com>
2024-09-11 17:19:10 +1000

    chore(DosAndDonts): update to use new icon

--- docs/components/DosAndDonts/DosAndDonts.tsx
+++ docs/components/DosAndDonts/DosAndDonts.tsx
@@ -1,9 +1,9 @@
 import React, { HTMLAttributes } from "react"
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { Canvas } from "@storybook/blocks"
+import { Canvas, Unstyled } from "@storybook/blocks"
 import classnames from "classnames"
-import { CheckIcon, CloseIcon } from "~components/Icon"
-import { SbContent } from "../SbContent"
+import { Text } from "~components/Text"
+import { Icon } from "~components/__future__/Icon"
 import styles from "./DosAndDonts.module.css"
 
 export const DosAndDonts = ({
@@ -30,20 +30,16 @@ export const DoOrDont = ({
   <div className={classnames("relative", className)} {...otherProps}>
     <div
       className={classnames(
-        "absolute top-6 left-6 z-10 py-4 px-6 font-bold text-white rounded flex gap-2 items-center justify-center",
+        "absolute top-6 left-6 z-10 py-2 ps-4 pe-8 font-bold text-white rounded flex gap-2 items-center justify-center",
         isDont ? "bg-red-500" : "bg-green-500"
       )}
     >
-      {isDont ? (
-        <CloseIcon role="presentation" />
-      ) : (
-        <CheckIcon role="presentation" />
-      )}
-      <SbContent>
-        <span className="text-white">
+      <Unstyled style={{ display: "contents" }}>
+        <Icon name={isDont ? "close" : "check"} isPresentational />
+        <Text variant="body" color="white">
           <strong>{isDont ? "Don't" : "Do"}</strong>
-        </span>
-      </SbContent>
+        </Text>
+      </Unstyled>
     </div>
     <Canvas
       sourceState="none"
