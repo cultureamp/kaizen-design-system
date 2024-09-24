7b1a639ad48d4888cb49fab90b566a2c00349016
Doug MacKenzie <doug.mackenzie@cultureamp.com>
2024-09-24 11:44:19 +1000

    Allow passing a ref to InlineNotification

--- packages/components/src/Notification/subcomponents/GenericNotification/GenericNotification.tsx
+++ packages/components/src/Notification/subcomponents/GenericNotification/GenericNotification.tsx
@@ -1,4 +1,10 @@
-import React, { HTMLAttributes, useEffect, useRef, useState } from "react"
+import React, {
+  forwardRef,
+  HTMLAttributes,
+  useEffect,
+  useRef,
+  useState,
+} from "react"
 import classnames from "classnames"
 import { HeadingProps } from "~components/Heading"
 import {
@@ -6,6 +12,7 @@ import {
   NotificationVariant,
 } from "~components/Notification/types"
 import { OverrideClassName } from "~components/types/OverrideClassName"
+import { isRefObject } from "~components/utils/isRefObject"
 import { CancelButton } from "../CancelButton"
 import { NotificationHeading } from "../NotificationHeading"
 import {
@@ -49,89 +56,102 @@ export type GenericNotificationVariant = {
 export type GenericNotificationProps = GenericNotificationBase &
   (GenericNotificationType | GenericNotificationVariant)
 
-export const GenericNotification = ({
-  type,
-  variant,
-  style,
-  children,
-  title,
-  persistent = false,
-  onHide,
-  noBottomMargin,
-  forceMultiline,
-  headingProps,
-  classNameOverride,
-  ...restProps
-}: GenericNotificationProps): JSX.Element | null => {
-  const [isHidden, setIsHidden] = useState<boolean>(true)
-  const [isRemoved, setIsRemoved] = useState<boolean>(false)
+export const GenericNotification = forwardRef<
+  HTMLDivElement,
+  GenericNotificationProps
+>(
+  (
+    {
+      type,
+      variant,
+      style,
+      children,
+      title,
+      persistent = false,
+      onHide,
+      noBottomMargin,
+      forceMultiline,
+      headingProps,
+      classNameOverride,
+      ...restProps
+    },
+    ref
+  ): JSX.Element | null => {
+    const [isHidden, setIsHidden] = useState<boolean>(true)
+    const [isRemoved, setIsRemoved] = useState<boolean>(false)
 
-  const containerRef = useRef<HTMLDivElement>(null)
+    const containerRef = isRefObject(ref) ? ref : useRef<HTMLDivElement>(null)
 
-  useEffect(() => {
-    requestAnimationFrame(() => {
-      if (containerRef.current) {
-        setIsHidden(false)
+    useEffect(() => {
+      requestAnimationFrame(() => {
+        if (containerRef.current) {
+          setIsHidden(false)
+        }
+      })
+    }, [])
+
+    const getMarginTop = (): string => {
+      if (isHidden && containerRef.current) {
+        return -containerRef.current.clientHeight + "px"
       }
-    })
-  }, [])
-
-  const getMarginTop = (): string => {
-    if (isHidden && containerRef.current) {
-      return -containerRef.current.clientHeight + "px"
+      return "0"
     }
-    return "0"
-  }
 
-  const onTransitionEnd = (e: React.TransitionEvent<HTMLDivElement>): void => {
-    // Be careful: this assumes the final CSS property to be animated is "margin-top".
-    if (isHidden && e.propertyName === "margin-top") {
-      setIsRemoved(true)
-      onHide?.()
+    const onTransitionEnd = (
+      e: React.TransitionEvent<HTMLDivElement>
+    ): void => {
+      // Be careful: this assumes the final CSS property to be animated is "margin-top".
+      if (isHidden && e.propertyName === "margin-top") {
+        setIsRemoved(true)
+        onHide?.()
+      }
     }
-  }
 
-  if (isRemoved) {
-    return null
-  }
+    if (isRemoved) {
+      return null
+    }
 
-  return (
-    <div
-      ref={containerRef}
-      className={classnames(
-        styles.notification,
-        variant ? styles[variant] : styles[type],
-        styles[style],
-        isHidden && styles.hidden,
-        noBottomMargin && styles.noBottomMargin,
-        classNameOverride,
-        persistent && styles.persistent
-      )}
-      style={{ marginTop: getMarginTop() }}
-      onTransitionEnd={onTransitionEnd}
-      {...restProps}
-    >
-      <div className={styles.icon}>
-        {type ? (
-          <NotificationIconType type={type} />
-        ) : (
-          <NotificationIconVariant variant={variant} />
-        )}
-      </div>
+    return (
       <div
+        ref={containerRef}
         className={classnames(
-          styles.textContainer,
-          forceMultiline && styles.forceMultiline
+          styles.notification,
+          variant ? styles[variant] : styles[type],
+          styles[style],
+          isHidden && styles.hidden,
+          noBottomMargin && styles.noBottomMargin,
+          classNameOverride,
+          persistent && styles.persistent
         )}
+        style={{ marginTop: getMarginTop() }}
+        onTransitionEnd={onTransitionEnd}
+        {...restProps}
       >
-        {style !== "global" && (
-          <NotificationHeading titleProp={title} headingProps={headingProps} />
-        )}
-        {children && <div className={styles.text}>{children}</div>}
+        <div className={styles.icon}>
+          {type ? (
+            <NotificationIconType type={type} />
+          ) : (
+            <NotificationIconVariant variant={variant} />
+          )}
+        </div>
+        <div
+          className={classnames(
+            styles.textContainer,
+            forceMultiline && styles.forceMultiline
+          )}
+        >
+          {style !== "global" && (
+            <NotificationHeading
+              titleProp={title}
+              headingProps={headingProps}
+            />
+          )}
+          {children && <div className={styles.text}>{children}</div>}
+        </div>
+        {!persistent && <CancelButton onClick={() => setIsHidden(true)} />}
       </div>
-      {!persistent && <CancelButton onClick={() => setIsHidden(true)} />}
-    </div>
-  )
-}
+    )
+  }
+)
 
 GenericNotification.displayName = "GenericNotification"
