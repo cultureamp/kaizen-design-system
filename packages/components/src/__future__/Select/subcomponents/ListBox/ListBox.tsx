15cdc8908cb19c0492c9cf9eb333ae5a9ca6c8c7
Michael Winter <mcwinter07@gmail.com>
2024-10-03 14:14:10 +1000

    update to use the useIsClientReady hook

--- packages/components/src/__future__/Select/subcomponents/ListBox/ListBox.tsx
+++ packages/components/src/__future__/Select/subcomponents/ListBox/ListBox.tsx
@@ -1,7 +1,8 @@
-import React, { HTMLAttributes, Key, useEffect } from "react"
+import React, { HTMLAttributes, Key, useEffect, useRef, ReactNode } from "react"
 import { AriaListBoxOptions, useListBox } from "@react-aria/listbox"
 import { SelectState } from "@react-stately/select"
 import classnames from "classnames"
+import { useIsClientReady } from "~components/__utilities__/isClientReady"
 import { OverrideClassName } from "~components/types/OverrideClassName"
 import { useSelectContext } from "../../context"
 import { SelectOption, SelectItem } from "../../types"
@@ -10,7 +11,7 @@ import styles from "./ListBox.module.scss"
 export type SingleListBoxProps<Option extends SelectOption> = OverrideClassName<
   HTMLAttributes<HTMLUListElement>
 > & {
-  children: React.ReactNode
+  children: ReactNode
   /** Props for the popup. */
   menuProps: AriaListBoxOptions<SelectItem<Option>>
 }
@@ -30,15 +31,26 @@ const getOptionKeyFromCollection = (
   return state.collection.getFirstKey()
 }
 
+// This makes the use of query selector less brittle in instances where a failed selector is passed in
+const safeQuerySelector = (selector: string): HTMLElement | null => {
+  try {
+    return document.querySelector(selector)
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error("QuerySelector failed:", error)
+    return null
+  }
+}
+
 export const ListBox = <Option extends SelectOption>({
   children,
   menuProps,
   classNameOverride,
   ...restProps
 }: SingleListBoxProps<Option>): JSX.Element => {
+  const isClientReady = useIsClientReady()
   const { state } = useSelectContext<Option>()
-  const ref = React.useRef<HTMLUListElement>(null)
-  const [isListboxReady, setListboxReady] = React.useState(false)
+  const ref = useRef<HTMLUListElement>(null)
   const { listBoxProps } = useListBox(
     {
       ...menuProps,
@@ -51,24 +63,18 @@ export const ListBox = <Option extends SelectOption>({
   )
 
   /**
-   * This is a slightly hacky way to ensure the Listbox is aware of its position without using timeout.
-   * This solves the page from refocusing to the top of the DOM when it is opened for the first time with keyboard.
+   * This uses the new useIsClientReady to ensure document exists before trying to querySelector and give the time to focus to the correct element
    */
   useEffect(() => {
-    setListboxReady(true)
-  }, [])
-
-  useEffect(() => {
-    if (isListboxReady) {
+    if (isClientReady) {
       const optionKey = getOptionKeyFromCollection(state)
-      const focusToElement = document.querySelector(
-        `[data-key="${optionKey}"]`
-      ) as HTMLElement
+      const focusToElement = safeQuerySelector(`[data-key='${optionKey}']`)
+
       if (focusToElement) {
         focusToElement.focus()
       }
     }
-  }, [isListboxReady])
+  }, [isClientReady])
 
   return (
     // eslint-disable-next-line jsx-a11y/no-noninteractive-element-interactions
