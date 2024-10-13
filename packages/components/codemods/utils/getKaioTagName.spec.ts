2c7b01337ef67afa302c9fe185253471400dddef
Cassandra <cassandra.tam@cultureamp.com>
2024-10-14 10:24:02 +1100

    feat(codemod): add codemod to upgrade Icon to Future Icon (#5080)
    * refactor: rename getTagName to getKaioTagName and abstract traversing
    
    * feat(codemod): add util getKaioTagNamesByRegex
    
    * refactor(transformSource): make tag name generic
    
    * feat(updateJsxElementWithNewProps): add arg to update tag name
    
    * feat(codemod): add codemod for upgrading icons
    
    * feat(upgradeIconV1): transform CaMonogramIcon to Brand
    
    * feat(updateKaioImport): add util to update imports
    
    * feat(upgradeIconV1): transform SpinnerIcon to LoadingSpinner

---
+++ packages/components/codemods/utils/getKaioTagName.spec.ts
@@ -0,0 +1,85 @@
+import { parseJsx } from "../__tests__/utils"
+import { getKaioTagName, getKaioTagNamesByRegex } from "./getKaioTagName"
+
+describe("getKaioTagName", () => {
+  it("returns the import name if it matches the target specifier", () => {
+    const input = parseJsx('import { Well } from "@kaizen/components"')
+    const tagName = getKaioTagName(input, "Well")
+    expect(tagName).toBe("Well")
+  })
+
+  it("returns the import alias if it matches the target specifier", () => {
+    const input = parseJsx(
+      'import { Well as KaizenWell } from "@kaizen/components"'
+    )
+    const tagName = getKaioTagName(input, "Well")
+    expect(tagName).toBe("KaizenWell")
+  })
+
+  it("returns undefined if there is no match to the target specifier", () => {
+    const input = parseJsx('import { Well } from "@kaizen/well"')
+    const tagName = getKaioTagName(input, "Well")
+    expect(tagName).toBe(undefined)
+  })
+})
+
+describe("getKaioTagNamesByRegex", () => {
+  it("returns the import names if it matches the regex target specifier", () => {
+    const input = parseJsx(
+      'import { AddIcon, ArrowDownIcon, Well } from "@kaizen/components"'
+    )
+    const tagNames = getKaioTagNamesByRegex(input, "Icon")
+    expect(tagNames).toEqual(
+      new Map([
+        [
+          "@kaizen/components",
+          new Map([
+            ["AddIcon", "AddIcon"],
+            ["ArrowDownIcon", "ArrowDownIcon"],
+          ]),
+        ],
+      ])
+    )
+  })
+
+  it("returns the import alias if it matches the target specifier", () => {
+    const input = parseJsx(
+      'import { AddIcon as KzAddIcon, ArrowDownIcon, Well } from "@kaizen/components"'
+    )
+    const tagNames = getKaioTagNamesByRegex(input, "Icon")
+    expect(tagNames).toEqual(
+      new Map([
+        [
+          "@kaizen/components",
+          new Map([
+            ["KzAddIcon", "AddIcon"],
+            ["ArrowDownIcon", "ArrowDownIcon"],
+          ]),
+        ],
+      ])
+    )
+  })
+
+  it("returns matching import names from different KAIO imports", () => {
+    const input = parseJsx(`
+      import { AddIcon, Well } from "@kaizen/components"
+      import { Icon } from "@kaizen/components/future"
+    `)
+    const tagNames = getKaioTagNamesByRegex(input, "Icon$")
+    expect(tagNames).toEqual(
+      new Map([
+        ["@kaizen/components", new Map([["AddIcon", "AddIcon"]])],
+        ["@kaizen/components/future", new Map([["Icon", "Icon"]])],
+      ])
+    )
+  })
+
+  it("returns undefined if there is no match to the target specifier", () => {
+    const input = parseJsx(`
+      import { Well } from "@kaizen/components"
+      import { AddIcon } from "@kaizen/icons"
+    `)
+    const tagNames = getKaioTagNamesByRegex(input, "Icon")
+    expect(tagNames).toBe(undefined)
+  })
+})
--- packages/components/codemods/utils/updateJsxElementWithNewProps.ts
+++ packages/components/codemods/utils/updateJsxElementWithNewProps.ts
@@ -2,19 +2,20 @@ import ts from "typescript"
 
 export const updateJsxElementWithNewProps = (
   node: ts.Node,
-  newAttributes: ts.JsxAttributeLike[]
+  newAttributes: ts.JsxAttributeLike[],
+  newTagName?: string
 ): ts.Node => {
   if (ts.isJsxOpeningElement(node)) {
     return ts.factory.updateJsxOpeningElement(
       node,
-      node.tagName,
+      newTagName ? ts.factory.createIdentifier(newTagName) : node.tagName,
       node.typeArguments,
       ts.factory.createJsxAttributes(newAttributes)
     )
   } else if (ts.isJsxSelfClosingElement(node)) {
     return ts.factory.updateJsxSelfClosingElement(
       node,
-      node.tagName,
+      newTagName ? ts.factory.createIdentifier(newTagName) : node.tagName,
       node.typeArguments,
       ts.factory.createJsxAttributes(newAttributes)
     )
