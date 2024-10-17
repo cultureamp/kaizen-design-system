ad2e75d9ea12bc402ed8ea138aff4f56e5a58226
Cassandra Tam <cassandra.tam@cultureamp.com>
2024-10-17 18:30:07 +1100

    docs: update SB links

--- packages/components/src/__actions__/Button/v1/Button/_docs/Button.stories.tsx
+++ packages/components/src/__actions__/Button/v1/Button/_docs/Button.stories.tsx
@@ -63,7 +63,8 @@ export const Disabled: Story = {
 }
 
 const sourceCodeIcon = `
-import { Button, AddIcon } from "@kaizen/components"
+import { Button } from "@kaizen/components"
+import { Icon } from "@kaizen/components/future"
 
 <Button label="Label" icon={<Icon name="add" isPresentational />} />
 `
