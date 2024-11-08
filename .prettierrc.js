2b1a096b0c52f642745a6b671292fff084973771
Cassandra Tam <cassandra.tam@cultureamp.com>
2024-11-08 17:32:17 +1100

    chore(deps): upgrade to eslint v9

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+export default {
+  printWidth: 100,
+  semi: false,
+  singleQuote: true,
+  quoteProps: "consistent",
+  trailingComma: "all",
+  arrowParens: "always",
+  experimentalTernaries: false,
+  tabWidth: 2,
+}
