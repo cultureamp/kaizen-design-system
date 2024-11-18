d16161beeec9c324f891f25b3ff00d4f1419a7e3
Cassandra Tam <cassandra.tam@cultureamp.com>
2024-11-18 13:48:54 +1100

    docs: add new StickerSheet using grid

---
+++ docs/components/_future/StickerSheet/components/StickerSheetHeader/StickerSheetHeader.tsx
@@ -0,0 +1,28 @@
+import React, { HTMLAttributes } from "react"
+import classnames from "classnames"
+import styles from "./StickerSheetHeader.module.css"
+
+export type StickerSheetHeaderProps = {
+  children: React.ReactNode
+  isReversed?: boolean
+} & HTMLAttributes<HTMLDivElement>
+
+export const StickerSheetHeader = ({
+  children,
+  isReversed = false,
+  className,
+  ...restProps
+}: StickerSheetHeaderProps): JSX.Element => (
+  <div
+    className={classnames(
+      styles.stickerSheetHeader,
+      isReversed && styles.isReversed,
+      className
+    )}
+    {...restProps}
+  >
+    {children}
+  </div>
+)
+
+StickerSheetHeader.displayName = "StickerSheet.Header"
---
+++ docs/components/_future/StickerSheet/components/StickerSheetRow/StickerSheetRow.tsx
@@ -0,0 +1,35 @@
+import React, { HTMLAttributes } from "react"
+import classnames from "classnames"
+import { StickerSheetCell } from "../StickerSheetCell"
+import { StickerSheetHeader } from "../StickerSheetHeader"
+import styles from "./StickerSheetRow.module.css"
+
+export type StickerSheetRowProps = {
+  children: React.ReactNode
+  header?: string
+  isReversed?: boolean
+} & HTMLAttributes<HTMLDivElement>
+
+export const StickerSheetRow = ({
+  children,
+  header,
+  isReversed = false,
+  dir,
+  className,
+  ...restProps
+}: StickerSheetRowProps): JSX.Element => (
+  <div className={classnames(styles.stickerSheetRow, className)} {...restProps}>
+    {header && (
+      <StickerSheetHeader isReversed={isReversed}>{header}</StickerSheetHeader>
+    )}
+    {React.Children.map(children, child => {
+      if (React.isValidElement(child) && child.type === StickerSheetCell) {
+        return child
+      }
+
+      return <StickerSheetCell dir={dir}>{child}</StickerSheetCell>
+    })}
+  </div>
+)
+
+StickerSheetRow.displayName = "StickerSheet.Row"
