27f35b9e36177072adb7425bf470ef040badc842
Cassandra Tam <cassandra.tam@cultureamp.com>
2024-11-20 16:44:44 +1100

    docs(DatePicker): update stickersheet

--- packages/components/src/DatePicker/_docs/DatePicker.stickersheet.stories.tsx
+++ packages/components/src/DatePicker/_docs/DatePicker.stickersheet.stories.tsx
@@ -22,119 +22,117 @@ const StickerSheetTemplate: StickerSheetStory = {
     const [selectedDate, setValueDate] = useState<Date | undefined>()
     return (
       <>
-        <StickerSheet isReversed={isReversed} heading="DatePicker">
-          <StickerSheet.Header
-            headings={[
-              "Default",
-              "Selected Value",
-              "Custom Description",
-              "Disabled",
-            ]}
-          />
-          <StickerSheet.Body>
-            <StickerSheet.Row style={{ verticalAlign: "top" }}>
-              <DatePicker
-                labelText="Label"
-                selectedDay={selectedDate}
-                onDayChange={setValueDate}
-                isReversed={isReversed}
-              />
-              <DatePicker
-                labelText="Label"
-                selectedDay={new Date(2022, 1, 5)}
-                onDayChange={() => undefined}
-                isReversed={isReversed}
-              />
-              <DatePicker
-                labelText="Label"
-                selectedDay={undefined}
-                onDayChange={() => undefined}
-                isReversed={isReversed}
-                description={
-                  <>
-                    <Text
-                      tag="span"
-                      variant="small"
-                      color={isReversed ? "white" : "dark"}
-                    >
-                      My <strong>Custom</strong> Description
-                    </Text>
-                  </>
-                }
-              />
-              <DatePicker
-                labelText="Label"
-                selectedDay={undefined}
-                onDayChange={() => undefined}
-                isReversed={isReversed}
-                disabled
-              />
-            </StickerSheet.Row>
-          </StickerSheet.Body>
+        <StickerSheet
+          isReversed={isReversed}
+          title="DatePicker"
+          headers={[
+            "Default",
+            "Selected Value",
+            "Custom Description",
+            "Disabled",
+          ]}
+        >
+          <StickerSheet.Row style={{ verticalAlign: "top" }}>
+            <DatePicker
+              labelText="Label"
+              selectedDay={selectedDate}
+              onDayChange={setValueDate}
+              isReversed={isReversed}
+            />
+            <DatePicker
+              labelText="Label"
+              selectedDay={new Date(2022, 1, 5)}
+              onDayChange={() => undefined}
+              isReversed={isReversed}
+            />
+            <DatePicker
+              labelText="Label"
+              selectedDay={undefined}
+              onDayChange={() => undefined}
+              isReversed={isReversed}
+              description={
+                <>
+                  <Text
+                    tag="span"
+                    variant="small"
+                    color={isReversed ? "white" : "dark"}
+                  >
+                    My <strong>Custom</strong> Description
+                  </Text>
+                </>
+              }
+            />
+            <DatePicker
+              labelText="Label"
+              selectedDay={undefined}
+              onDayChange={() => undefined}
+              isReversed={isReversed}
+              disabled
+            />
+          </StickerSheet.Row>
         </StickerSheet>
 
-        <StickerSheet isReversed={isReversed} heading="Pseudo states">
-          <StickerSheet.Header
-            headings={["Hover", "Focus"]}
-            hasVerticalHeadings
-          />
-          <StickerSheet.Body>
-            <StickerSheet.Row rowTitle="Container">
-              <DatePicker
-                isReversed={isReversed}
-                labelText="Date"
-                selectedDay={undefined}
-                onDayChange={() => undefined}
-                data-sb-pseudo-styles="hover"
-              />
-              <DatePicker
-                isReversed={isReversed}
-                labelText="Date"
-                selectedDay={undefined}
-                onDayChange={() => undefined}
-                data-sb-pseudo-styles="focus"
-              />
-            </StickerSheet.Row>
+        <StickerSheet
+          isReversed={isReversed}
+          title="Pseudo states"
+          headers={["Hover", "Focus"]}
+        >
+          <StickerSheet.Row header="Container">
+            <DatePicker
+              isReversed={isReversed}
+              labelText="Date"
+              selectedDay={undefined}
+              onDayChange={() => undefined}
+              data-sb-pseudo-styles="hover"
+            />
+            <DatePicker
+              isReversed={isReversed}
+              labelText="Date"
+              selectedDay={undefined}
+              onDayChange={() => undefined}
+              data-sb-pseudo-styles="focus"
+            />
+          </StickerSheet.Row>
 
-            <StickerSheet.Row rowTitle="Icon button">
-              <DatePicker
-                isReversed={isReversed}
-                labelText="Date"
-                selectedDay={undefined}
-                onDayChange={() => undefined}
-                classNameOverride="story__date-input-single-field--hover"
-              />
-              <DatePicker
-                isReversed={isReversed}
-                labelText="Date"
-                selectedDay={undefined}
-                onDayChange={() => undefined}
-                classNameOverride="story__date-input-single-field--focus"
-              />
-            </StickerSheet.Row>
-          </StickerSheet.Body>
+          <StickerSheet.Row header="Icon button">
+            <DatePicker
+              isReversed={isReversed}
+              labelText="Date"
+              selectedDay={undefined}
+              onDayChange={() => undefined}
+              classNameOverride="story__date-input-single-field--hover"
+            />
+            <DatePicker
+              isReversed={isReversed}
+              labelText="Date"
+              selectedDay={undefined}
+              onDayChange={() => undefined}
+              classNameOverride="story__date-input-single-field--focus"
+            />
+          </StickerSheet.Row>
         </StickerSheet>
 
-        <StickerSheet isReversed={isReversed} heading="Validation">
-          <StickerSheet.Header headings={["Error", "Caution"]} />
-          <StickerSheet.Body>
-            <StickerSheet.Row>
-              <DatePicker
-                labelText="Label"
-                selectedDay={new Date("potato")}
-                onDayChange={() => undefined}
-                isReversed={isReversed}
-              />
-              <DatePicker
-                labelText="Label"
-                selectedDay={undefined}
-                onDayChange={() => undefined}
-                isReversed={isReversed}
-                status="caution"
-                validationMessage="Custom cautionary message"
-              />
-            </StickerSheet.Row>
-          </StickerSheet.Body>
+        <StickerSheet
+          isReversed={isReversed}
+          title="Validation"
+          headers={["Error", "Caution"]}
+        >
+          <StickerSheet.Row>
+            <DatePicker
+              labelText="Label"
+              selectedDay={new Date("potato")}
+              onDayChange={() => undefined}
+              isReversed={isReversed}
+            />
+            <DatePicker
+              labelText="Label"
+              selectedDay={undefined}
+              onDayChange={() => undefined}
+              isReversed={isReversed}
+              status="caution"
+              validationMessage="Custom cautionary message"
+            />
+          </StickerSheet.Row>
         </StickerSheet>
       </>
     )
@@ -185,40 +183,34 @@ export const StickerSheetLocales: StickerSheetStory = {
   name: "Sticker Sheet (Locales)",
   render: () => (
     <>
-      <StickerSheet heading="Localisation">
-        <StickerSheet.Header headings={["en-AU", "en-US"]} />
-        <StickerSheet.Body>
-          <StickerSheet.Row>
-            <DatePicker
-              labelText="Label"
-              selectedDay={new Date("2022, 1, 5")}
-              onDayChange={() => undefined}
-            />
-            <DatePicker
-              labelText="Label"
-              selectedDay={new Date("2022, 1, 5")}
-              onDayChange={() => undefined}
-              locale="en-US"
-            />
-          </StickerSheet.Row>
-        </StickerSheet.Body>
+      <StickerSheet title="Localisation" headers={["en-AU", "en-US"]}>
+        <StickerSheet.Row>
+          <DatePicker
+            labelText="Label"
+            selectedDay={new Date("2022, 1, 5")}
+            onDayChange={() => undefined}
+          />
+          <DatePicker
+            labelText="Label"
+            selectedDay={new Date("2022, 1, 5")}
+            onDayChange={() => undefined}
+            locale="en-US"
+          />
+        </StickerSheet.Row>
       </StickerSheet>
 
-      <StickerSheet>
-        <StickerSheet.Header headings={["fr-CA"]} />
-        <StickerSheet.Body>
-          <StickerSheet.Row>
-            <StaticIntlProvider locale="fr-CA">
-              <DatePicker
-                labelText="Label"
-                selectedDay={new Date("2022, 1, 5")}
-                onDayChange={() => undefined}
-                locale="fr-CA"
-                data-testid="id--dp-fr-ca"
-              />
-            </StaticIntlProvider>
-          </StickerSheet.Row>
-        </StickerSheet.Body>
+      <StickerSheet headers={["fr-CA"]}>
+        <StickerSheet.Row>
+          <StaticIntlProvider locale="fr-CA">
+            <DatePicker
+              labelText="Label"
+              selectedDay={new Date("2022, 1, 5")}
+              onDayChange={() => undefined}
+              locale="fr-CA"
+              data-testid="id--dp-fr-ca"
+            />
+          </StaticIntlProvider>
+        </StickerSheet.Row>
       </StickerSheet>
     </>
   ),
