e97331caad1b365ccab6ca22460edfd1243e5bb8
Michael Winter <mcwinter07@gmail.com>
2024-11-21 12:01:00 +1100

    create LinkButton component and stories and update button exports

---
+++ packages/components/src/__actions__/LinkButton/v3/_docs/LinkButton.doc.stories.tsx
@@ -0,0 +1,131 @@
+import React from "react"
+// import { action } from "@storybook/addon-actions"
+import { Meta, StoryObj } from "@storybook/react"
+import { Badge } from "~components/Badge"
+import { Icon } from "~components/__future__"
+import { ReversedColors } from "~components/__utilities__/v3"
+import { LinkButton } from "../index"
+
+const meta = {
+  title: "Actions/LinkButton/LinkButton (v3)",
+  component: LinkButton,
+  args: {
+    children: "Label",
+    href: "#link-button-clicked",
+    target: "_blank",
+  },
+} satisfies Meta<typeof LinkButton>
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Playground: Story = {}
+
+export const LinkButtonVariants: Story = {
+  render: args => (
+    <>
+      <LinkButton {...args} variant="primary" />
+      <LinkButton {...args} variant="secondary" />
+      <LinkButton {...args} variant="tertiary" />
+    </>
+  ),
+  decorators: [
+    Story => (
+      <div className="flex gap-8">
+        <Story />
+      </div>
+    ),
+  ],
+}
+
+export const LinkButtonVariantsReversed: Story = {
+  render: args => (
+    <ReversedColors isReversed={true}>
+      <LinkButton {...args} variant="primary" />
+      <LinkButton {...args} variant="secondary" />
+      <LinkButton {...args} variant="tertiary" />
+    </ReversedColors>
+  ),
+  parameters: {
+    reverseColors: true,
+  },
+  decorators: [
+    Story => (
+      <div className="flex gap-8 bg-purple-700 p-16">
+        <Story />
+      </div>
+    ),
+  ],
+}
+
+export const LinkButtonSizes: Story = {
+  render: args => (
+    <>
+      <LinkButton {...args} size="small" />
+      <LinkButton {...args} size="medium" />
+      <LinkButton {...args} size="large" />
+    </>
+  ),
+  decorators: [
+    Story => (
+      <div className="[&>*]:ms-8">
+        <Story />
+      </div>
+    ),
+  ],
+}
+
+export const LinkButtonWithIconStart: Story = {
+  args: {
+    icon: <Icon isPresentational name="delete" />,
+  },
+}
+
+export const LinkButtonWithIconEnd: Story = {
+  args: {
+    icon: <Icon isPresentational name="arrow_forward" shouldMirrorInRTL />,
+    iconPosition: "end",
+  },
+}
+
+export const IconLinkButton: Story = {
+  args: {
+    children: "Remove highlights from: May 8, 2024",
+    icon: <Icon isPresentational name="delete" />,
+    hasHiddenLabel: true,
+  },
+}
+
+export const ReversedLinkButton: Story = {
+  parameters: {
+    reverseColors: true,
+    docs: {
+      source: {
+        code: `<ReversedColors isReversed={true}>
+          <LinkButton>Label</LinkButton>
+        </ReversedColors>
+      `,
+      },
+    },
+  },
+}
+
+export const LinkButtonFullWidth: Story = {
+  args: {
+    isFullWidth: true,
+  },
+}
+
+export const LinkButtonWithBadge: Story = {
+  args: {
+    children: (
+      <>
+        Label
+        <Badge classNameOverride="ms-8" size="small">
+          3
+        </Badge>
+      </>
+    ),
+  },
+}
