8bf1435857b3817fee83a219a1c88b58c8b6f2f6
Christian Moore <33382564+Zystix@users.noreply.github.com>
2024-12-03 11:28:10 +1100

    Fix Tile flip focus (#5336)
    * initial commit
    
    * Add refocus behaviour with useEffect
    
    * Fix focus theft on initial render
    
    * Add tests
    
    * Update metadata to be more specific
    
    * Update state naming to be consistent

--- packages/components/src/Tile/TileGrid/_docs/TileGrid.stories.tsx
+++ packages/components/src/Tile/TileGrid/_docs/TileGrid.stories.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import { Meta, StoryObj } from "@storybook/react"
+import { expect, waitFor, within } from "@storybook/test"
 import { InformationTile } from "~components/Tile"
 import { TileGrid } from "../index"
 
@@ -10,21 +11,21 @@ const meta = {
     children: (
       <>
         <InformationTile
-          title="Title"
+          title="Title A"
           metadata="Side A"
-          information="Side B"
+          information="Side A - Back"
           footer={<>Footer</>}
         />
         <InformationTile
-          title="Title"
-          metadata="Side A"
-          information="Side B"
+          title="Title B"
+          metadata="Side B"
+          information="Side B - Back"
           footer={<>Footer</>}
         />
         <InformationTile
-          title="Title"
-          metadata="Side A"
-          information="Side B"
+          title="Title C"
+          metadata="Side C"
+          information="Side C - Back"
           footer={<>Footer</>}
         />
       </>
@@ -43,5 +44,37 @@ export const Playground: Story = {
         sourceState: "shown",
       },
     },
+  },
+}
+
+// Test for multiple tiles, flipping one doesn't flip others
+export const FlipOneNotOthers: Story = {
+  play: async ({ canvasElement, step }) => {
+    const canvas = within(canvasElement)
+
+    await step("initial render complete", async () => {
+      await waitFor(() => {
+        canvas.getByRole("button", {
+          name: "View more information: Title A",
+        })
+      })
+    })
+
+    await step("Can focus to button", async () => {
+      await waitFor(() => {
+        const buttonWithInfoLabel = canvas.getByRole("button", {
+          name: "View more information: Title A",
+        })
+        buttonWithInfoLabel.click()
+      })
+    })
+
+    await step("Check other tiles", async () => {
+      await waitFor(() => {
+        expect(canvas.getByText("Side A - Back")).toBeInTheDocument()
+        expect(canvas.getByText("Title B")).toBeInTheDocument()
+        expect(canvas.getByText("Title C")).toBeInTheDocument()
+      })
+    })
   },
 }
