03b56a422b183d4398006a9a8cd41b38d59d1e39
Doug MacKenzie <doug.mackenzie@cultureamp.com>
2024-12-10 10:09:59 +1100

    Add unique ids for stickersheet examples

--- packages/components/src/__future__/Tabs/_docs/Tabs.stickersheet.stories.tsx
+++ packages/components/src/__future__/Tabs/_docs/Tabs.stickersheet.stories.tsx
@@ -12,36 +12,36 @@ export default {
   },
 } satisfies Meta
 
-const ExampleTabs = (): JSX.Element => (
+const ExampleTabs = ({ id }: { id: string }): JSX.Element => (
   <Tabs>
     <TabList aria-label="Tabs">
-      <Tab id="one">Tab 1</Tab>
-      <Tab id="two">Tab 2</Tab>
-      <Tab id="three" badge="3">
+      <Tab id={`${id}-1`}>Tab 1</Tab>
+      <Tab id={`${id}-2`}>Tab 2</Tab>
+      <Tab id={`${id}-3`} badge="3">
         Tab 3
       </Tab>
-      <Tab id="disabled" isDisabled>
+      <Tab id={`${id}-disabled`} isDisabled>
         Disabled Tab
       </Tab>
-      <Tab id="four">Tab 4</Tab>
-      <Tab id="five">Tab 5</Tab>
+      <Tab id={`${id}-4`}>Tab 4</Tab>
+      <Tab id={`${id}-5`}>Tab 5</Tab>
     </TabList>
-    <TabPanel id="one" className="p-24">
+    <TabPanel id={`${id}-1`} className="p-24">
       <Text variant="body">Content 1</Text>
     </TabPanel>
-    <TabPanel id="two" className="p-24">
+    <TabPanel id={`${id}-2`} className="p-24">
       <Text variant="body">Content 2</Text>
     </TabPanel>
-    <TabPanel id="three" className="p-24">
+    <TabPanel id={`${id}-3`} className="p-24">
       <Text variant="body">Content 3</Text>
     </TabPanel>
-    <TabPanel id="disabled" className="p-24">
+    <TabPanel id={`${id}-disabled`} className="p-24">
+      <Text variant="body">Disabled content</Text>
+    </TabPanel>
+    <TabPanel id={`${id}-4`} className="p-24">
       <Text variant="body">Content 4</Text>
     </TabPanel>
-    <TabPanel id="four" className="p-24">
-      <Text variant="body">Content 4</Text>
-    </TabPanel>
-    <TabPanel id="five" className="p-24">
+    <TabPanel id={`${id}-5`} className="p-24">
       <Text variant="body">Content 5</Text>
     </TabPanel>
   </Tabs>
@@ -53,14 +53,14 @@ const StickerSheetTemplate: StickerSheetStory = {
       <>
         <StickerSheet title="Tabs" layout="stretch">
           <StickerSheet.Row>
-            <ExampleTabs />
+            <ExampleTabs id="fullwidth" />
           </StickerSheet.Row>
         </StickerSheet>
 
         <StickerSheet title="Overflow (container 500px)" layout="stretch">
           <StickerSheet.Row>
             <div style={{ maxWidth: '500px' }}>
-              <ExampleTabs />
+              <ExampleTabs id="overflow" />
             </div>
           </StickerSheet.Row>
         </StickerSheet>
