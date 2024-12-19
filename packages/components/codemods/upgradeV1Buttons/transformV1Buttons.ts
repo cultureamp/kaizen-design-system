2886bcea5268aed526ce7bc6df728d3e123f8046
Cassandra Tam <cassandra.tam@cultureamp.com>
2024-12-19 12:22:21 +1100

    feat(codemods): add codemod to upgrade V1 Buttons (not complete)

---
+++ packages/components/codemods/upgradeV1Buttons/transformV1Buttons.ts
@@ -0,0 +1,71 @@
+import ts from 'typescript'
+import { createJsxElementWithChildren, createProp, createStringProp } from '../utils'
+
+/**
+ * @returns
+ * - `ts.JsxAttribute` if the prop should be transformed
+ * - `null` if the prop should be removed
+ * - `undefined` if the prop should be kept as is
+ */
+const transformProp = (
+  propName: string,
+  propValue: ts.JsxAttributeValue | undefined,
+): ts.JsxAttribute | null | undefined => {
+  switch (propName) {
+    case 'onClick':
+      return createProp('onPress', propValue)
+    case 'reversed':
+      return createProp('isReversed', propValue)
+    case 'classNameOverride':
+      return createProp('className', propValue)
+    case 'data-automation-id':
+      return createProp('data-testid', propValue)
+    case 'disabled':
+      return createProp('isDisabled', propValue)
+    default:
+      return undefined
+  }
+}
+
+export const transformV1Buttons = (
+  node: ts.JsxSelfClosingElement,
+  kaioComponentName: string,
+  tagName: string = 'Button',
+): ts.Node => {
+  let childrenValue: ts.JsxAttributeValue | undefined
+
+  const newAttributes = node.attributes.properties.reduce<ts.JsxAttributeLike[]>((acc, attr) => {
+    if (ts.isJsxAttribute(attr)) {
+      const propName = attr.name.getText()
+
+      if (propName === 'label') {
+        childrenValue = attr.initializer
+        return acc
+      }
+
+      if (propName === 'newTabAndIUnderstandTheAccessibilityImplications') {
+        acc.push(createStringProp('target', '_blank'))
+        acc.push(createStringProp('rel', 'noopener noreferrer'))
+        return acc
+      }
+
+      const newProp = transformProp(propName, attr.initializer)
+
+      if (newProp === null) return acc
+
+      if (newProp) {
+        acc.push(newProp)
+        return acc
+      }
+    }
+
+    acc.push(attr)
+    return acc
+  }, [])
+
+  if (kaioComponentName === 'IconButton') {
+    newAttributes.push(createProp('hasHiddenLabel'))
+  }
+
+  return createJsxElementWithChildren(tagName, newAttributes, childrenValue)
+}
---
+++ packages/components/codemods/upgradeV1Buttons/upgradeV1Buttons.spec.ts
@@ -0,0 +1,292 @@
+import { parseJsx } from '../__tests__/utils'
+import {
+  getKaioTagNamesMapByComponentName,
+  printAst,
+  transformSource,
+  type TransformSourceArgs,
+} from '../utils'
+import { upgradeV1Buttons } from './upgradeV1Buttons'
+
+const transformIcons = (sourceFile: TransformSourceArgs['sourceFile']): string => {
+  const kaioTagNamesMap = getKaioTagNamesMapByComponentName(sourceFile, ['IconButton', 'Button'])
+  return transformSource({
+    sourceFile,
+    transformers: [upgradeV1Buttons(kaioTagNamesMap!)],
+  })
+}
+
+describe('upgradeV1Buttons()', () => {
+  it('transforms both IconButton and Button v1 to Button v3 in the same iteration', () => {
+    const inputAst = parseJsx(`
+      import { Button, IconButton } from "@kaizen/components"
+      export const TestComponent = () => (
+        <>
+          <Button label="Hello" />
+          <IconButton icon={<Icon isPresentational name="more_horiz"/>} label="More pls" />
+        </>
+      )
+    `)
+    const outputAst = parseJsx(`
+      import { Button } from "@kaizen/components/v3/actions"
+      export const TestComponent = () => (
+        <>
+          <Button>Hello</Button>
+          <Button icon={<Icon isPresentational name="more_horiz"/>} hasHiddenLabel>More pls</Button>
+        </>
+      )
+    `)
+
+    expect(transformIcons(inputAst)).toEqual(printAst(outputAst))
+  })
+
+  it('transforms Button v1 to Button v3 when href and component prop are not set', () => {
+    const inputAst = parseJsx(`
+      import { Button } from "@kaizen/components"
+      export const TestComponent = () => <Button label="Hello" onClick={handleClick} />
+    `)
+    const outputAst = parseJsx(`
+      import { Button } from "@kaizen/components/v3/actions"
+      export const TestComponent = () => <Button onPress={handleClick}>Hello</Button>
+    `)
+
+    expect(transformIcons(inputAst)).toEqual(printAst(outputAst))
+  })
+
+  it('transforms IconButton to Button v3 when href and component prop are not set', () => {
+    const inputAst = parseJsx(`
+      import { IconButton } from "@kaizen/components"
+      export const TestComponent = () => <IconButton icon={<Icon isPresentational name="more_horiz"/>} label="More pls" onClick={handleClick} />
+    `)
+    const outputAst = parseJsx(`
+      import { Button } from "@kaizen/components/v3/actions"
+      export const TestComponent = () => <Button icon={<Icon isPresentational name="more_horiz"/>} onPress={handleClick} hasHiddenLabel>More pls</Button>
+    `)
+
+    expect(transformIcons(inputAst)).toEqual(printAst(outputAst))
+  })
+
+  it('transforms aliased V1 Buttons to Button when href and component prop are not set', () => {
+    const inputAst = parseJsx(`
+      import { IconButton as KzIconButton, Button as KzButton } from "@kaizen/components"
+      export const TestComponent = () => (
+        <><KzButton label="Waffle" /><KzIconButton label="Pancake" /></>
+      )
+    `)
+    const outputAst = parseJsx(`
+      import { Button } from "@kaizen/components/v3/actions"
+      export const TestComponent = () => (
+        <><Button>Waffle</Button><Button hasHiddenLabel>Pancake</Button></>
+      )
+    `)
+    expect(transformIcons(inputAst)).toEqual(printAst(outputAst))
+  })
+
+  it('transforms V1 Buttons to aliased Button', () => {
+    const inputAst = parseJsx(`
+      import { IconButton, Button } from "@kaizen/components"
+      import { Button as ButtonAlias } from "@kaizen/components/v3/actions"
+      export const TestComponent = () => (
+        <><Button label="Waffle" /><IconButton label="Pancake" /></>
+      )
+    `)
+    const outputAst = parseJsx(`
+      import { Button as ButtonAlias } from "@kaizen/components/v3/actions"
+      export const TestComponent = () => (
+        <><ButtonAlias>Waffle</ButtonAlias><ButtonAlias hasHiddenLabel>Pancake</ButtonAlias></>
+      )
+    `)
+    expect(transformIcons(inputAst)).toEqual(printAst(outputAst))
+  })
+
+  // @todo
+  // it('does not transform IconButton when href prop is set', () => {
+  //   const inputAst = parseJsx(`
+  //     import { IconButton } from "@kaizen/components"
+  //     export const TestComponent = () => <IconButton href="#" />
+  //   `)
+  //   const outputAst = parseJsx(`
+  //     import { IconButton } from "@kaizen/components"
+  //     export const TestComponent = () => <IconButton href="#" />
+  //   `)
+  //   expect(transformIcons(inputAst)).toEqual(printAst(outputAst))
+  // })
+
+  // @todo
+  // it('does not transform IconButton when component prop is set', () => {
+  //   const inputAst = parseJsx(`
+  //     import { IconButton } from "@kaizen/components"
+  //     export const TestComponent = () => <IconButton component={Component} />
+  //   `)
+  //   const outputAst = parseJsx(`
+  //     import { IconButton } from "@kaizen/components"
+  //     export const TestComponent = () => <IconButton component={Component} />
+  //   `)
+  //   expect(transformIcons(inputAst)).toEqual(printAst(outputAst))
+  // })
+
+  describe('import statements', () => {
+    it('updates V1 Buttons from @kaizen/components', () => {
+      const inputAst = parseJsx(`
+        import { Button, IconButton } from "@kaizen/components"
+        export const TestComponent = () => (
+          <><Button label="Pancakes" /><IconButton label="Waffles" /></>
+        )
+      `)
+      const outputAst = parseJsx(`
+        import { Button } from "@kaizen/components/v3/actions"
+        export const TestComponent = () => (
+          <><Button>Pancakes</Button><Button hasHiddenLabel>Waffles</Button></>
+        )
+      `)
+      expect(transformIcons(inputAst)).toEqual(printAst(outputAst))
+    })
+
+    it('updates V1 Buttons from @kaizen/components/v1/actions', () => {
+      const inputAst = parseJsx(`
+        import { Button, IconButton } from "@kaizen/components/v1/actions"
+        export const TestComponent = () => (
+          <><Button label="Pancakes" /><IconButton label="Waffles" /></>
+        )
+      `)
+      const outputAst = parseJsx(`
+        import { Button } from "@kaizen/components/v3/actions"
+        export const TestComponent = () => (
+          <><Button>Pancakes</Button><Button hasHiddenLabel>Waffles</Button></>
+        )
+      `)
+      expect(transformIcons(inputAst)).toEqual(printAst(outputAst))
+    })
+
+    it('updates IconButton from @kaizen/components/v2/actions', () => {
+      const inputAst = parseJsx(`
+        import { Button, IconButton } from "@kaizen/components/v2/actions"
+        export const TestComponent = () => (
+          <><Button label="Pancakes" /><IconButton label="Waffles" /></>
+        )
+      `)
+      const outputAst = parseJsx(`
+        import { Button } from "@kaizen/components/v3/actions"
+        export const TestComponent = () => (
+          <><Button>Pancakes</Button><Button hasHiddenLabel>Waffles</Button></>
+        )
+      `)
+      expect(transformIcons(inputAst)).toEqual(printAst(outputAst))
+    })
+
+    it('updates aliased V1 Buttons to Button', () => {
+      const inputAst = parseJsx(`
+        import { Button as KzButton, IconButton as KzIconButton } from "@kaizen/components"
+        export const TestComponent = () => (
+          <><KzButton label="Pancakes" /><KzIconButton label="Waffles" /></>
+        )
+      `)
+      const outputAst = parseJsx(`
+        import { Button } from "@kaizen/components/v3/actions"
+        export const TestComponent = () => (
+          <><Button>Pancakes</Button><Button hasHiddenLabel>Waffles</Button></>
+        )
+      `)
+      expect(transformIcons(inputAst)).toEqual(printAst(outputAst))
+    })
+
+    it('updates imports of multiple V1 Buttons from different KAIO imports', () => {
+      const inputAst = parseJsx(`
+        import { Button as KzButton, IconButton as KzIconButton } from "@kaizen/components"
+        import { Button as ButtonV1, IconButton as IconButtonV1 } from "@kaizen/components/v1/actions"
+        export const TestComponent = () => (
+          <>
+            <KzButton label="Pancakes" />
+            <ButtonV1 label="Waffles" />
+            <KzIconButton label="Toasties" />
+            <IconButtonV1 label="Scones" />
+          </>
+        )
+      `)
+      const outputAst = parseJsx(`
+        import { Button } from "@kaizen/components/v3/actions"
+        export const TestComponent = () => (
+          <>
+            <Button>Pancakes</Button>
+            <Button>Waffles</Button>
+            <Button hasHiddenLabel>Toasties</Button>
+            <Button hasHiddenLabel>Scones</Button>
+          </>
+        )
+      `)
+      expect(transformIcons(inputAst)).toEqual(printAst(outputAst))
+    })
+
+    it('does not duplicate Button import if it already exists', () => {
+      const inputAst = parseJsx(`
+        import { IconButton, Button as KzButton } from "@kaizen/components"
+        import { Button } from "@kaizen/components/v3/actions"
+        export const TestComponent = () => (
+          <>
+            <IconButton label="Pancakes" />
+            <KzButton label="Scones" />
+            <Button>Waffles</Button>
+          </>
+        )
+      `)
+      const outputAst = parseJsx(`
+        import { Button } from "@kaizen/components/v3/actions"
+        export const TestComponent = () => (
+          <>
+            <Button hasHiddenLabel>Pancakes</Button>
+            <Button>Scones</Button>
+            <Button>Waffles</Button>
+          </>
+        )
+      `)
+      expect(transformIcons(inputAst)).toEqual(printAst(outputAst))
+    })
+
+    it('does not add Button if aliased Button exists', () => {
+      const inputAst = parseJsx(`
+        import { Button, IconButton } from "@kaizen/components"
+        import { Button as ButtonAlias } from "@kaizen/components/v3/actions"
+        export const TestComponent = () => (
+          <>
+            <Button label="Pancakes" />
+            <IconButton label="Scones" />
+            <ButtonAlias>Waffles</ButtonAlias>
+          </>
+        )
+      `)
+      const outputAst = parseJsx(`
+        import { Button as ButtonAlias } from "@kaizen/components/v3/actions"
+        export const TestComponent = () => (
+          <>
+            <ButtonAlias>Pancakes</ButtonAlias>
+            <ButtonAlias hasHiddenLabel>Scones</ButtonAlias>
+            <ButtonAlias>Waffles</ButtonAlias>
+          </>
+        )
+      `)
+      expect(transformIcons(inputAst)).toEqual(printAst(outputAst))
+    })
+
+    it('does not update import of irrelevant KAIO components', () => {
+      const inputAst = parseJsx(`
+        import { IconButton, FilterButton } from "@kaizen/components"
+        export const TestComponent = () => (
+          <>
+            <IconButton label="Pancakes" />
+            <FilterButton />
+          </>
+        )
+      `)
+      const outputAst = parseJsx(`
+        import { FilterButton } from "@kaizen/components"
+        import { Button } from "@kaizen/components/v3/actions"
+        export const TestComponent = () => (
+          <>
+            <Button hasHiddenLabel>Pancakes</Button>
+            <FilterButton />
+          </>
+        )
+      `)
+      expect(transformIcons(inputAst)).toEqual(printAst(outputAst))
+    })
+  })
+})
