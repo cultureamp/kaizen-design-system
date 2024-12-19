fda4e14fbb5d011684062e2fed5cdee9c2d7ee33
Christian Moore <christian.moore@cultureamp.com>
2024-12-20 08:24:53 +1100

    initial commit

--- packages/components/src/utils/convertBtnProps.ts
+++ packages/components/src/utils/convertBtnProps.ts
@@ -1,10 +1,39 @@
-import {type ButtonProps} from '../Button';
+import { type GenericButtonProps } from '../Button/GenericButton'
+import { type ButtonProps } from '../__rc__/Button/Button'
 
+export const convertBtnProps = (oldProps: GenericButtonProps): ButtonProps => {
+  // How to handle workingLabel, workingLabelHidden, and type?
 
-export const convertBtnProps(ButtonProps: GenericButtonProps): ButtonProps => {
-  const { mood, variant, ...restProps } = ButtonProps;
-  if (mood) {
-    return { ...restProps, variant: mood };
+  const {
+    label,
+    primary,
+    destructive,
+    secondary,
+    disabled,
+    size,
+    badge,
+    type,
+    reversed,
+    fullWidth,
+    working,
+    workingLabel,
+    workingLabelHidden,
+  } = oldProps
+
+  return {
+    variant: primary || destructive ? 'primary' : secondary ? 'tertiary' : 'primary',
+    size: size === 'regular' ? 'large' : size === 'small' ? 'medium' : size,
+    className: '',
+    children: { label, badge },
+    isDisabled: disabled,
+    isFullWidth: fullWidth,
+    icon: undefined,
+    iconPosition: undefined,
+    hasHiddenLabel: false,
+    isPending: working ?? false,
+    hasHiddenPendingLabel: workingLabelHidden ?? undefined,
+    pendingLabel: workingLabel ?? undefined,
+    isReversed: reversed,
   }
-  return ButtonProps;
 }
+>>>>>>> 158018f8a (initial commit)
