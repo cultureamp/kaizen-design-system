f7bea2413386ab941aa28a33c954fc0c4fa2718f
Viet Anh Tran <viet.anhtran@cultureamp.com>
2025-01-06 18:15:28 +1300

    Revert change to stories and ensure that container is set

--- packages/components/src/Notification/ToastNotification/ToastNotificationsList/ToastNotificationsList.tsx
+++ packages/components/src/Notification/ToastNotification/ToastNotificationsList/ToastNotificationsList.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { createPortal } from 'react-dom'
 import { useToastNotificationContext } from '../context/ToastNotificationContext'
 import { ToastNotificationsMap } from './subcomponents/ToastNotificationsMap'
@@ -6,17 +6,26 @@ import styles from './ToastNotificationsList.module.scss'
 
 export const ToastNotificationsList = (): JSX.Element => {
   const { notifications, removeToastNotification } = useToastNotificationContext()
+  const [toastContainer, setToastContainer] = useState<Element | null>(null)
 
-  const containers = document.querySelectorAll('[data-testid="toast-notifications-list"')
+  useEffect(() => {
+    const containers = document.querySelectorAll('[data-testid="toast-notifications-list"')
+    if (toastContainer && containers.length === 1) {
+      return
+    }
 
-  if (containers) {
-    // Remove any duplicate instances
-    // (eg. Storybook docs page has multiple stories each with their own context)
-    containers.forEach((c, i) => {
-      if (i === 0) return
-      c.remove()
-    })
-  }
+    if (containers) {
+      // Remove any duplicate instances
+      // (eg. Storybook docs page has multiple stories each with their own context)
+      containers.forEach((c, i) => {
+        if (i === 0) {
+          setToastContainer(c)
+          return
+        }
+        c.remove()
+      })
+    }
+  }, [toastContainer])
 
   return createPortal(
     <div
@@ -27,7 +36,7 @@ export const ToastNotificationsList = (): JSX.Element => {
       <ToastNotificationsMap
         notifications={notifications}
         onHide={removeToastNotification}
-        container={containers[0]}
+        container={toastContainer}
       />
     </div>,
     document.body,
