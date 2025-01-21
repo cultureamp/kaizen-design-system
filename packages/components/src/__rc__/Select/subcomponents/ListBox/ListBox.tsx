928e9754140e6fa5f313b262cb87e5b71fa15288
Michael Winter <mcwinter07@gmail.com>
2025-01-21 16:30:32 +1100

    Add new util to check for grouped sections

--- packages/components/src/__rc__/Select/subcomponents/ListBox/ListBox.tsx
+++ packages/components/src/__rc__/Select/subcomponents/ListBox/ListBox.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useRef, type HTMLAttributes, type Key, type ReactNode
 import { useListBox, type AriaListBoxOptions } from '@react-aria/listbox'
 import { type SelectState } from '@react-stately/select'
 import classnames from 'classnames'
+import { useIsClientReady } from '~components/__utilities__/useIsClientReady'
 import { type OverrideClassName } from '~components/types/OverrideClassName'
 import { useSelectContext } from '../../context'
 import { useHasStableYPosition } from '../../hooks/useHasStableYPosition'
@@ -16,6 +17,30 @@ export type SingleListBoxProps<Option extends SelectOption> = OverrideClassName<
   menuProps: AriaListBoxOptions<SelectItem<Option>>
 }
 
+// TODO: This accounts for the grouping of elements in a listbox - there's still a cleaner way to do this I'm sure but this is something that can be finessed
+const getOptionKey = (
+  optionKey: SelectOption['value'] | null,
+  state: SelectState<SelectItem<any>>,
+): Key | null => {
+  if (!optionKey) {
+    return null
+  }
+  const option = state.collection.getItem(optionKey)
+  const optionType = option?.type
+  // const optionChildren = option
+
+  console.log('optionType', option)
+  console.log('optionType', optionType)
+
+  if (optionType === 'section') {
+    const firstChildOption = option?.value.options[0]
+    console.log(firstChildOption)
+    // return the first key of the section options
+    return firstChildOption.value
+  }
+  return optionKey
+}
+
 /** A util to retrieve the key of the correct focusable items based of the focus strategy
  * This is used to determine which element from the collection to focus to on open base on the keyboard event
  * ie: UpArrow will set the focusStrategy to "last"
@@ -24,9 +49,10 @@ const getOptionKeyFromCollection = (state: SelectState<SelectItem<any>>): Key | 
   if (state.selectedItem) {
     return state.selectedItem.key
   } else if (state.focusStrategy === 'last') {
-    return state.collection.getLastKey()
+    // return state.collection.getLastKey()
+    return getOptionKey(state.collection.getLastKey(), state)
   }
-  return state.collection.getFirstKey()
+  return getOptionKey(state.collection.getFirstKey(), state)
 }
 
 /** This makes the use of query selector less brittle in instances where a failed selector is passed in
@@ -47,7 +73,8 @@ export const ListBox = <Option extends SelectOption>({
   classNameOverride,
   ...restProps
 }: SingleListBoxProps<Option>): JSX.Element => {
-  const { state } = useSelectContext<Option>()
+  const isClientReady = useIsClientReady()
+  const { state, section } = useSelectContext<Option>()
   const ref = useRef<HTMLUListElement>(null)
   const hasStableYPosition = useHasStableYPosition(ref)
   const { listBoxProps } = useListBox(
@@ -65,11 +92,18 @@ export const ListBox = <Option extends SelectOption>({
    * This uses the hasStableYPosition to determine if the position is stable within the window
    */
   useEffect(() => {
-    if (hasStableYPosition) {
+    if (isClientReady && hasStableYPosition) {
       const optionKey = getOptionKeyFromCollection(state)
       const focusToElement = safeQuerySelector(`[data-key='${optionKey}']`)
+      // console.log(
+      //   'state.collection',
+      //   state.collection.getItem(state.collection.getFirstKey())?.type === 'section',
+      // )
+      // console.log(optionKey)
+      // console.log(focusToElement)
 
       if (focusToElement) {
+        console.log('focusToElement', focusToElement)
         focusToElement.focus()
       } else {
         ref.current?.focus()
@@ -77,7 +111,7 @@ export const ListBox = <Option extends SelectOption>({
     }
     // Only run this effect for checking the first successful render
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [hasStableYPosition])
+  }, [isClientReady, hasStableYPosition])
 
   return (
     <ul
