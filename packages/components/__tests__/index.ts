48d0e5c8f723a0be631099a04849896114d92ef8
Christian Moore <christian.moore@cultureamp.com>
2025-01-23 17:02:41 +1100

    Lift mockMedia logic to own function/file

--- packages/components/__tests__/index.ts
+++ packages/components/__tests__/index.ts
@@ -1,1 +1,2 @@
+export * from './mockMatchMedia'
 export * from './reactIntlMock'
--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,27 +1,14 @@
 import * as JestDomMatchers from '@testing-library/jest-dom/matchers'
 import { cleanup } from '@testing-library/react'
-import { afterEach, beforeEach, expect, vi } from 'vitest'
+import { afterEach, beforeEach, expect } from 'vitest'
 import 'vitest-axe/extend-expect'
-import { reactIntlMock } from '~tests'
+import { mockMatchMedia, reactIntlMock } from '~tests'
 
 expect.extend(JestDomMatchers)
 
 beforeEach(() => {
   reactIntlMock()
-
-  Object.defineProperty(window, 'matchMedia', {
-    writable: true,
-    value: vi.fn().mockImplementation((query) => ({
-      matches: false,
-      media: query,
-      onchange: null,
-      addListener: vi.fn(), // deprecated
-      removeListener: vi.fn(), // deprecated
-      addEventListener: vi.fn(),
-      removeEventListener: vi.fn(),
-      dispatchEvent: vi.fn(),
-    })),
-  })
+  mockMatchMedia()
 })
 
 afterEach(() => {
