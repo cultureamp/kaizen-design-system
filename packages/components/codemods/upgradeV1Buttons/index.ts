be64cfa2078024286d609d7ada7ee64e4be00453
Michael Winter <mcwinter07@gmail.com>
2025-02-12 10:18:47 +1100

    add button migration guide and update outputs for codemod

--- packages/components/codemods/upgradeV1Buttons/index.ts
+++ packages/components/codemods/upgradeV1Buttons/index.ts
@@ -2,6 +2,8 @@ import { transformComponentsInDir } from '../utils'
 import { upgradeV1Buttons } from './upgradeV1Buttons'
 
 const run = (): void => {
+  console.log('It is recommended that the `upgradeIconV1` codemod be run prior to this')
+  console.log('---')
   console.log('~(-_- ~) Running V1 Buttons upgrade (~ -_-)~')
 
   const targetDir = process.argv[2]
