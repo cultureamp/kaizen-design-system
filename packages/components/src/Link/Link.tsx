de25f4c7d010682a71764aeadb8a65caabdf8152
Christian Moore <christian.moore@cultureamp.com>
2025-02-17 09:23:43 +1100

    Default iconPosition is now 'end', icon names in usage guidelines match actual icon names

--- packages/components/src/Link/Link.tsx
+++ packages/components/src/Link/Link.tsx
@@ -43,7 +43,7 @@ export const Link = forwardRef(
       variant = 'primary',
       size = 'body',
       icon,
-      iconPosition = 'start',
+      iconPosition = 'end',
       isInline = false,
       isDisabled,
       className,
