29f7da098a060d062f3d7f0807f6f6a3fe19336a
Doug MacKenzie <doug.mackenzie@cultureamp.com>
2025-04-02 09:40:16 +1100

    Use tokens for animation

--- packages/components/src/Modal/GenericModal/animations.ts
+++ packages/components/src/Modal/GenericModal/animations.ts
@@ -1,16 +1,18 @@
+import { tokens } from '@kaizen/design-tokens/src/js'
+
 export const backdropMotion = {
   hidden: {
     opacity: 0,
   },
   visible: {
     opacity: 0.5,
-    animationTimingFunction: 'cubic-bezier(0.485, 0.155, 0.24, 1.245)', // $animation-easing-function-bounce-in
-    animationDuration: '300ms', // $animation-duration-fast
+    animationTimingFunction: tokens.animation.easingFunction.bounceIn,
+    animationDuration: tokens.animation.duration.fast,
   },
   exit: {
     opacity: 0,
-    animationTimingFunction: 'cubic-bezier(0.485, 0.155, 0.515, 0.845)', // $animation-easing-function-bounce-out
-    animationDuration: '201ms', // $animation-duration-rapid
+    animationTimingFunction: tokens.animation.easingFunction.bounceOut,
+    animationDuration: tokens.animation.duration.rapid,
   },
 }
 
@@ -22,13 +24,13 @@ export const modalMotion = {
   visible: {
     opacity: 1,
     scale: 1,
-    animationTimingFunction: 'cubic-bezier(0.485, 0.155, 0.24, 1.245)', // $animation-easing-function-bounce-in
-    animationDuration: '300ms', // $animation-duration-fast
+    animationTimingFunction: tokens.animation.easingFunction.bounceIn,
+    animationDuration: tokens.animation.duration.fast,
   },
   exit: {
     opacity: 0,
     scale: 0.5,
-    animationTimingFunction: 'cubic-bezier(0.485, 0.155, 0.515, 0.845)', // $animation-easing-function-bounce-out
-    animationDuration: '200ms', // $animation-duration-rapid
+    animationTimingFunction: tokens.animation.easingFunction.bounceOut,
+    animationDuration: tokens.animation.duration.rapid,
   },
 }
