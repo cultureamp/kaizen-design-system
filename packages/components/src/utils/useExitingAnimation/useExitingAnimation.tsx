cd4424aca2bb6159ec92f3cb48d424443eec3e1d
Michael Winter <mcwinter07@gmail.com>
2025-04-07 15:13:13 +1000

    update useExitingAnimation hook to use more generic variable names

--- packages/components/src/utils/useExitingAnimation/useExitingAnimation.tsx
+++ packages/components/src/utils/useExitingAnimation/useExitingAnimation.tsx
@@ -8,11 +8,11 @@ export const useExitingAnimation = (
   exitDuration: number = 1000,
 ): string | undefined => {
   const [animationClass, setAnimationClass] = useState('')
-  const prevIsAiLoading = React.useRef<boolean | undefined>(undefined)
+  const previousIsLoading = React.useRef<boolean | undefined>(undefined)
 
   useEffect(() => {
     if (shouldAnimate !== undefined) {
-      if (prevIsAiLoading.current === true && shouldAnimate === false) {
+      if (previousIsLoading.current === true && shouldAnimate === false) {
         setAnimationClass(exitingClass)
 
         const timeout = setTimeout(() => {
@@ -25,7 +25,7 @@ export const useExitingAnimation = (
         setAnimationClass(animatingClass)
       }
     }
-    prevIsAiLoading.current = shouldAnimate
+    previousIsLoading.current = shouldAnimate
 
     return undefined
   }, [shouldAnimate, animatingClass, exitingClass, exitDuration])
