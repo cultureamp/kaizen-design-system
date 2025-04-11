91ec1558fb2e792fe432c1bf5fbbc5b7b0008a74
Michael Winter <mcwinter07@gmail.com>
2025-04-11 11:38:08 +1000

    add AI moment props, styles and docs to Card

--- packages/components/src/Card/_docs/Card.stickersheet.stories.tsx
+++ packages/components/src/Card/_docs/Card.stickersheet.stories.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { type Meta } from '@storybook/react'
+import { Text } from '~components/Text'
 import { StickerSheet, type StickerSheetStory } from '~storybook/components/StickerSheet'
 import { Card, type CardProps } from '../index'
 
@@ -35,22 +36,45 @@ const variants = [
   'highlight',
 ] satisfies CardProps['variant'][]
 
+const CardWithContent = (props: CardProps): JSX.Element => (
+  <Card {...props}>
+    <div className="p-6">
+      <Text variant="body">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas accusantium doloribus
+        dicta odio recusandae repudiandae tenetur! Fugiat vero architecto quasi rem culpa vel
+        asperiores, sit, quas suscipit, ea deleniti dolorum.
+      </Text>
+    </div>
+  </Card>
+)
+
 const StickerSheetTemplate: StickerSheetStory = {
   render: ({ isReversed, ...args }) => (
     <>
       <StickerSheet isReversed={isReversed} title="Colors">
         {colors.map((color) => (
           <StickerSheet.Row key={color} header={color}>
-            <Card {...args} color={color} />
+            <CardWithContent {...args} color={color} />
           </StickerSheet.Row>
         ))}
       </StickerSheet>
       <StickerSheet isReversed={isReversed} title="Variants (deprecated)">
         {variants.map((variant) => (
           <StickerSheet.Row key={variant} header={variant}>
-            <Card {...args} variant={variant} />
+            <CardWithContent {...args} variant={variant} />
           </StickerSheet.Row>
         ))}
+      </StickerSheet>
+      <StickerSheet isReversed={isReversed} title="AI Moments">
+        <StickerSheet.Row header="AI is loaded">
+          <CardWithContent {...args} isAiLoading={false} />
+        </StickerSheet.Row>
+        <StickerSheet.Row header="AI is loading">
+          <CardWithContent {...args} isAiLoading />
+        </StickerSheet.Row>
+        <StickerSheet.Row header="AI is undefined">
+          <CardWithContent {...args} isAiLoading={undefined} />
+        </StickerSheet.Row>
       </StickerSheet>
     </>
   ),
