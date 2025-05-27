99e1c810568650cd846a740b7f6b0b8abccf464c
nancy_nguyenpham <nancy.pham@cultureamp.com>
2025-05-27 18:12:33 +1000

    feat: adjust scrollbar

--- packages/components/src/Filter/FilterMultiSelect/subcomponents/MenuPopup/MenuPopup.tsx
+++ packages/components/src/Filter/FilterMultiSelect/subcomponents/MenuPopup/MenuPopup.tsx
@@ -44,7 +44,7 @@ export const MenuPopup = ({
       size({
         apply({ availableHeight, availableWidth, elements }) {
           Object.assign(elements.floating.style, {
-            maxHeight: `${Math.max(availableHeight - 25, 350)}px`,
+            maxHeight: `${Math.max(availableHeight - 25, 352)}px`,
             maxWidth: `${availableWidth}px`,
           })
         },
