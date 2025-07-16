8c147773fc4d5245ec04e10c9aae05fb0fe05112
Kitty Allen <kitty.allen@cultureamp.com>
2025-07-16 15:25:45 +1000

    add tags to components based on status

--- docs/.storybook/manager.tsx
+++ docs/.storybook/manager.tsx
@@ -22,24 +22,66 @@ const colors = tokens.color
 addons.setConfig({
   theme: KaizenTheme,
   sidebar: {
-    renderLabel: (item): JSX.Element =>
-      item.type === 'root' ? (
-        <span
-          style={{
-            color: colors.purple['800'],
-            textTransform: 'capitalize',
-            fontSize: '13px',
-            letterSpacing: 'normal',
-            fontWeight: 600,
-          }}
-        >
-          <span aria-hidden style={{ marginRight: tokens.spacing[6] }}>
-            {CATEGORIES_ICON[item.name]}
+    renderLabel: (item): JSX.Element => {
+      const statusTag =
+        item.type === 'group' || item.type === 'component'
+          ? item.tags?.find((tag) => ['alpha', 'next', 'deprecated'].includes(tag))
+          : undefined
+
+      const statusStyles: Record<string, React.CSSProperties> = {
+        alpha: {
+          backgroundColor: colors.blue['200'],
+          color: colors.blue['800'],
+        },
+        next: {
+          backgroundColor: colors.green['200'],
+          color: colors.green['800'],
+        },
+        deprecated: {
+          backgroundColor: colors.red['200'],
+          color: colors.red['800'],
+        },
+      }
+
+      if (item.type === 'root') {
+        return (
+          <span
+            style={{
+              color: colors.purple['800'],
+              textTransform: 'capitalize',
+              fontSize: '13px',
+              letterSpacing: 'normal',
+              fontWeight: 600,
+            }}
+          >
+            <span aria-hidden style={{ marginRight: tokens.spacing[6] }}>
+              {CATEGORIES_ICON[item.name]}
+            </span>
+            {item.name}
           </span>
-          {item.name}
-        </span>
-      ) : (
-        <span style={{ margin: '1px 0' }}>{item.name}</span>
-      ),
+        )
+      }
+
+      if (statusTag) {
+        return (
+          <span style={{ display: 'flex', alignItems: 'center', gap: '6px' }}>
+            <span>{item.name}</span>
+            <span
+              style={{
+                ...statusStyles[statusTag],
+                fontSize: '10px',
+                padding: '2px 6px',
+                borderRadius: '999px',
+                fontWeight: 500,
+                textTransform: 'uppercase',
+              }}
+            >
+              {statusTag}
+            </span>
+          </span>
+        )
+      }
+      return <span style={{ margin: '1px 0' }}>{item.name}</span>
+    },
   },
 })
