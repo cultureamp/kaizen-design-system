aa2d9fa0b27cb9297daaeaceede5060ac62ea50d
Michael Winter <mcwinter07@gmail.com>
2025-08-14 15:31:08 +1000

    move RA and RAC into a libs folder to resolve local linting and build issues

--- packages/components/rollup.config.mjs
+++ packages/components/rollup.config.mjs
@@ -5,8 +5,8 @@ export default rollupConfig({
     index: './src/index.ts',
     next: './src/__next__/index.ts',
     alpha: './src/__alpha__/index.ts',
-    reactAria: './src/__react-aria__/index.ts',
-    reactAriaComponents: './src/__react-aria-components__/index.ts',
+    reactAriaKZ: './src/__libs__/react-aria/index.ts',
+    reactAriaComponentsKZ: './src/__libs__/react-aria-components/index.ts',
   },
   plugins: pluginsSharedUi,
 })
