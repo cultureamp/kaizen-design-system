19ab770b10792e0cab3f1732a4cae10bd25eea78
Michael Winter <36558508+mcwinter07@users.noreply.github.com>
2025-08-15 14:23:05 +1000

    Remove old entry points (#5991)
    * feat: remove old entry points
    
    * fix: remove utils entry point and update workspace to fix linting
    
    * move RA and RAC into a libs folder to resolve local linting and build issues
    
    * fix exports path
    
    * update files list in package.json

--- packages/components/rollup.config.mjs
+++ packages/components/rollup.config.mjs
@@ -3,20 +3,10 @@ import { pluginsSharedUi, rollupConfig } from '@cultureamp/package-bundler'
 export default rollupConfig({
   input: {
     index: './src/index.ts',
-    future: './src/__next__/index.ts',
     next: './src/__next__/index.ts',
-    actionsV1: './src/v1-actions.ts',
-    actionsV2: './src/v2-actions.ts',
-    actionsV3: './src/v3-actions.ts',
-    overlaysV1: './src/v1-overlays.ts',
-    overlaysV2: './src/v2-overlays.ts',
-    overlaysV3: './src/v3-overlays.ts',
+    alpha: './src/__alpha__/index.ts',
     reactAriaKZ: './src/__libs__/react-aria/index.ts',
     reactAriaComponentsKZ: './src/__libs__/react-aria-components/index.ts',
-    utilitiesV3: './src/index.ts',
-    reactAriaV3: './src/__react-aria__/index.ts',
-    reactAriaComponentsV3: './src/__react-aria-components__/index.ts',
-    alpha: './src/__alpha__/index.ts',
   },
   plugins: pluginsSharedUi,
 })
