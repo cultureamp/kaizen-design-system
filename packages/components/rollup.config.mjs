69cff8c74511b404253303d446fc6b51a7cadd76
Michael Winter <36558508+mcwinter07@users.noreply.github.com>
2025-09-01 12:45:13 +1000

    re-add v3 RA library exports to v2 (#6031)
    * re-add v3 RA library exports to v2

--- packages/components/rollup.config.mjs
+++ packages/components/rollup.config.mjs
@@ -7,6 +7,8 @@ export default rollupConfig({
     alpha: './src/__alpha__/index.ts',
     reactAriaKZ: './src/__libs__/react-aria/index.ts',
     reactAriaComponentsKZ: './src/__libs__/react-aria-components/index.ts',
+    reactAriaV3: './src/__react-aria__/index.ts',
+    reactAriaComponentsV3: './src/__react-aria-components__/index.ts',
   },
   plugins: pluginsSharedUi,
 })
