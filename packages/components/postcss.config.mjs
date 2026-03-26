430bda2697566a7b7bbe476d09c14234d78d60f8
AliceMenzie <alicemenzie@cultureamp.com>
2026-03-27 08:39:38 +1100

    revert bundler changes

--- packages/components/postcss.config.mjs
+++ packages/components/postcss.config.mjs
@@ -1,6 +1,6 @@
-import cssnano from 'cssnano'
-import postcssImport from 'postcss-import'
-
 export default {
-  plugins: [postcssImport, cssnano({ preset: 'default' })],
+  plugins: {
+    'postcss-import': {},
+    'cssnano': {},
+  },
 }
