ef668d44b4ae6db2ef05cbc8c041d5d1d2ee82ff
= <48108358+CrowBe@users.noreply.github.com>
2026-06-17 16:24:53 +1000

    chore: resolve PR comments and add a patch changeset

---
+++ .changeset/few-squids-guess.md
@@ -0,0 +1,5 @@
+---
+'@kaizen/components': patch
+---
+
+Updated the tooltip Focusable tag to accept an optional tag prop that determines the html element wrapping the passed in children
--- packages/components/src/Focusable/Focusable.tsx
+++ packages/components/src/Focusable/Focusable.tsx
@@ -3,25 +3,25 @@ import classnames from 'classnames'
 import { useFocusable, type FocusableOptions } from 'react-aria'
 import styles from './Focusable.module.css'
 
-export type FocusableProps<T extends ElementType = 'span'> = {
+export type FocusableProps<T extends ElementType = 'div'> = {
   children: ReactNode
   /**
-   * The HTML element to render as. Defaults to `span` to avoid block-in-inline
-   * hydration errors when Focusable wraps content inside a `<p>` or other inline context.
+   * The HTML element to render as. Defaults to `div` to avoid an api change
+   * but should be set to 'span' if rendering within something like a 'p' tag to avoid invalid HTML.
    */
-  as?: T
+  tag?: T
 } & FocusableOptions &
-  HTMLAttributes<HTMLElement>
+  HTMLAttributes<HTMLDivElement>
 
-export const Focusable = <T extends ElementType = 'span'>({
+export const Focusable = <T extends ElementType = 'div'>({
   children,
   className,
-  as,
+  tag,
   ...props
 }: FocusableProps<T>): JSX.Element => {
-  const ref = useRef<HTMLElement>(null)
+  const ref = useRef<HTMLDivElement>(null)
   const { focusableProps } = useFocusable(props, ref)
-  const Element = as ?? 'span'
+  const Element = tag ?? 'div'
 
   return (
     <Element
