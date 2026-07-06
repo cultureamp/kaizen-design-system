7db584a1e628db30bf7bd2010c8597fa4f32e1a5
AliceMenzie <alice.menzie@cultureamp.com>
2026-07-06 13:33:45 +1000

    update: rename icon type

--- packages/components/src/Icon/CustomIcons.ts
+++ packages/components/src/Icon/CustomIcons.ts
@@ -1,8 +1,8 @@
 import type { FC, SVGProps } from 'react'
 import _UserSettingsIcon from '../../assets/svgs/custom-icons/user-settings.svg'
 
-type CustomIconsProps = FC<SVGProps<SVGSVGElement> & { color?: string; size?: number }>
+type CustomIcon = FC<SVGProps<SVGSVGElement> & { color?: string; size?: number }>
 
 // Cast forces tsc to emit the concrete FC type in .d.ts rather than a .svg path reference,
 // which consumers can't resolve (the declare module '*.svg' in types.d.ts is not included in the package output).
-export const UserSettingsIcon = _UserSettingsIcon as CustomIconsProps
+export const UserSettingsIcon = _UserSettingsIcon as CustomIcon
